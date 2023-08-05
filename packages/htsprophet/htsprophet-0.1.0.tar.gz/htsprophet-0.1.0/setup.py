2f187549ba068abfa5f5a8836ef5979179f00658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:49 +0000

    Add package htsprophet/0.1.0/htsprophet-0.1.0.tar.gz

---
+++ packages/htsprophet/htsprophet-0.1.0.tar.gz/htsprophet-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name = 'htsprophet',
+      version = '0.1.0',
+      description = "Creates Hierarchical Time Series Forecasts with Facebook's Prophet tool",
+      url = "https://github.com/CollinRooney12/htsprophet",
+      author = "Collin Rooney",
+      author_email = 'CollinRooney12@gmail.com',
+      license = 'MIT',
+      keywords='hts time series hierarchy forecast Prophet',
+      packages = ['htsprophet'],
+      zip_safe = False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      install_requires = [
+              'matplotlib',
+              'pandas>=0.18.1',
+              'numpy',
+              'fbprophet',
+              'scikit-learn>=0.18'],
+       )
