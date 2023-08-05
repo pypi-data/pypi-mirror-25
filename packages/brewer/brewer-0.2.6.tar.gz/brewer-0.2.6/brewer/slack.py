ad2193cd9531b44ca5464188a86c4720a928b067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:43 +0000

    Add package brewer/0.2.6/brewer-0.2.6.tar.gz

---
+++ packages/brewer/brewer-0.2.6.tar.gz/brewer-0.2.6/brewer/slack.py
@@ -0,0 +1,18 @@
+from slacker import Slacker
+import os
+
+class BrewerBot(Slacker):
+    def __init__(self):
+        self.token_file = os.path.expanduser("~") + "/slack.token"
+        self.slacker = Slacker(self.get_token_from_file())
+        self.channel = "@luke"
+
+    def get_token_from_file(self):
+        with open(self.token_file, "r") as file:
+            # FIXME: Boot up brewer_network if there's no token??
+            return file.readlines()[0].rstrip()
+
+    def send(self, message):
+        # I don't want to mess around with 'super'. It's confusing and I don't have time.
+        self.slacker.chat.post_message(self.channel, message, username = "Beer Bot")
+        return True
