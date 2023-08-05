34dd09a60ec289b1c93f5a92813c874ced3a4cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package simplyrestful/0.1.2/simplyrestful-0.1.2-py2.py3-none-any.whl

---
+++ packages/simplyrestful/simplyrestful-0.1.2-py2.py3-none-any.whl/simplyrestful/settings.py
@@ -0,0 +1,9 @@
+DATABASE = 'postgresql://postgres:postgres@localhost/simply_restful'
+HOST = '0.0.0.0'
+PORT = 5000
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+DEFAULT_AUTHENTICATION = ['authenticators.BasicAuthenticator']
+DEFAULT_AUTHORIZATION = []
+
+DEFAULT_PAGE_SIZE = 10
+MAX_PAGE_SIZE = 100
