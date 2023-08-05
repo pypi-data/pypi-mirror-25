6ba1e60425f652fcfeec8cd5383c416523d96c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:55 +0000

    Add package syy123nester/1.0.0/syy123nester-1.0.0.tar.gz

---
+++ packages/syy123nester/syy123nester-1.0.0.tar.gz/syy123nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'syy123nester',
+    version         = '1.0.0',
+    py_modules      = ['syy123nester'],
+    author          = 'SmileVictor',
+    author_email    = 'suyaoyun@qq.com',
+    url             = 'http://www.headfirstlabs.com',
+    description     = 'A simple printer of nested lists',
+    )
