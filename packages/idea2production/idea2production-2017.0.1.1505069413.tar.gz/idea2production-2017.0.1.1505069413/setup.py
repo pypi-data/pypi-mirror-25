c88ad4638e2356a36b9c74e3ee9fa123dc61a225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:02 +0000

    Add package idea2production/2017.0.1.1505069413/idea2production-2017.0.1.1505069413.tar.gz

---
+++ packages/idea2production/idea2production-2017.0.1.1505069413.tar.gz/idea2production-2017.0.1.1505069413/setup.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import shlex
+import sys
+
+import io
+import os
+from setuptools import find_packages, setup, Command
+from setuptools.command.test import test as TestCommand
+from shutil import rmtree
+
+with open('requirements.txt') as fd:
+    requirements = [line.rstrip() for line in fd]
+
+with open('requirements_test.txt') as fd:
+    test_requirements = [line.rstrip() for line in fd]
+
+
+
+# Package meta-data.
+NAME = 'idea2production'
+DESCRIPTION = 'From Idea to Production on Python Projects'
+URL = 'https://github.com/me/myproject'
+EMAIL = 'development@hrl-upm.es'
+AUTHOR = 'Harbor Research Laboratory UPM'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'requests', 'twine',
+]
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
+
+
+class PublishCommand(Command):
+    """Support setup.py publish."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except FileNotFoundError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
+class PyTest(TestCommand):
+
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ''
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+
+    install_requires=REQUIRED,
+    tests_require=test_requirements,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+
+    # $ setup.py publish support.
+    cmdclass={
+        'publish': PublishCommand,
+        'test': PyTest,
+    },
+)
