78d1596931632d722ac61eff88f69c64982cd89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:35 +0000

    Add package pymchelper/0.7.0/pymchelper-0.7.0-py2.py3-none-any.whl

---
+++ packages/pymchelper/pymchelper-0.7.0-py2.py3-none-any.whl/pymchelper/run.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+import glob
+import logging
+import sys
+
+import argparse
+
+from pymchelper.detector import merge_list, merge_many, Converters, ErrorEstimate
+from pymchelper.writers.plots import ImageWriter
+
+logger = logging.getLogger(__name__)
+
+
+def set_logger_level(args):
+    if args.quiet:
+        if args.quiet == 1:
+            level = "WARNING"
+        if args.quiet == 2:
+            level = "ERROR"
+        else:
+            level = "CRITICAL"
+    elif args.verbose:
+        level = "DEBUG"
+    else:
+        level = "INFO"
+    logging.basicConfig(level=level)
+
+
+def add_default_options(parser):
+    import pymchelper
+    parser.add_argument('input', help='input filename, file list or pattern', type=str)
+    parser.add_argument('output', help='output filename or directory', nargs='?')
+    parser.add_argument('-j', '--jobs', help='number of parallel jobs to use (-1 means all CPUs)', default=-1, type=int)
+    parser.add_argument('--many', help='automatically merge data from various sources', action="store_true")
+    parser.add_argument('-a', '--nan', help='ignore NaN in averaging', action="store_true")
+    parser.add_argument('-e', '--error',
+                        help='type of error estimate to add (default: ' + ErrorEstimate.stderr.name + ')',
+                        default=ErrorEstimate.stderr.name,
+                        choices=[x.name for x in ErrorEstimate],
+                        type=str)
+    parser.add_argument('-n', '--nscale', help='scale with number of primaries N.', default=1, type=float)
+    parser.add_argument('-v',
+                        '--verbose',
+                        action='count',
+                        default=0,
+                        help='give more output. Option is additive, and can be used up to 3 times')
+    parser.add_argument('-q', '--quiet', action='count', default=0, help='be silent')
+    parser.add_argument('-V', '--version', action='version', version=pymchelper.__version__)
+
+
+def main(args=sys.argv[1:]):
+    import pymchelper
+    import os
+
+    _progname = os.path.basename(sys.argv[0])
+    _helptxt = 'Universal converter for FLUKA and SHIELD-HIT12A generated files.'
+    _epitxt = '''Type '{:s} <converter> --help' for help on a specific converter.'''.format(_progname)
+
+    parser = argparse.ArgumentParser(description=_helptxt, epilog=_epitxt)
+
+    # subparsers = parser.add_subparsers(title='available converters', metavar='...')
+    subparsers = parser.add_subparsers(dest='command', metavar='converter')
+
+    parser_txt = subparsers.add_parser(Converters.txt.name, help='converts to plain txt file')
+    add_default_options(parser_txt)
+
+    parser_image = subparsers.add_parser(Converters.image.name, help='converts to PNG images')
+    add_default_options(parser_image)
+    parser_image.add_argument("--colormap",
+                              help='image color map, see http://matplotlib.org/users/colormaps.html '
+                                   'for list of possible options (default: ' + ImageWriter.default_colormap + ')',
+                              default=ImageWriter.default_colormap,
+                              type=str)
+
+    parser_excel = subparsers.add_parser(Converters.excel.name, help='converts to MS Excel file')
+    add_default_options(parser_excel)
+
+    parser_plotdata = subparsers.add_parser(Converters.plotdata.name, help='converts to gnuplot data')
+    add_default_options(parser_plotdata)
+
+    parser_gnuplot = subparsers.add_parser(Converters.gnuplot.name, help='converts to gnuplot script')
+    add_default_options(parser_gnuplot)
+
+    parser_sparse = subparsers.add_parser(Converters.sparse.name, help='converts to sparse matrix format')
+    add_default_options(parser_sparse)
+    parser_sparse.add_argument("--threshold",
+                               help='only values greater than threshold are saved',
+                               type=float,
+                               default=0.0)
+
+    parser_tripcube = subparsers.add_parser(Converters.tripcube.name, help='converts to trip98 data cube')
+    add_default_options(parser_tripcube)
+
+    parser_tripddd = subparsers.add_parser(Converters.tripddd.name, help='converts to trip98 ddd file')
+    add_default_options(parser_tripddd)
+    parser_tripddd.add_argument("--energy",
+                                help='energy of the beam [MeV/amu]',
+                                type=float)
+    parser_tripddd.add_argument("--ngauss",
+                                help='number of Gauss curves to fit (default: 2)',
+                                choices=(0, 1, 2),
+                                default=2,
+                                type=int)
+
+    parser.add_argument('-V', '--version', action='version', version=pymchelper.__version__)
+
+    parsed_args = parser.parse_args(args)
+
+    if parsed_args.command is not None:
+        set_logger_level(parsed_args)
+
+        # check if output directory exists
+        if parsed_args.output is not None:
+            output_dir = os.path.dirname(parsed_args.output)
+            if output_dir and not os.path.exists(output_dir):
+                raise IOError("Directory {}/ does not exist.".format(output_dir))
+
+        # TODO add filename discovery
+        files = sorted(glob.glob(parsed_args.input))
+        if not files:
+            logger.error('File does not exist: ' + parsed_args.input)
+
+        parsed_args.error = ErrorEstimate[parsed_args.error]
+
+        # check required options for tripddd parser
+        if parsed_args.command == Converters.tripddd.name and not parsed_args.energy:
+            logger.error("Option --energy is required, provide an energy value")
+            return 1
+
+        if parsed_args.many:
+            merge_many(files, parsed_args.output, parsed_args, parsed_args.jobs)
+        else:
+            merge_list(files, parsed_args.output, parsed_args)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
---
+++ packages/pymchelper/pymchelper-0.7.0-py2.py3-none-any.whl/pymchelper/writers/plots.py
@@ -0,0 +1,214 @@
+import os
+import logging
+
+import numpy as np
+
+logger = logging.getLogger(__name__)
+
+
+class PlotDataWriter:
+    def __init__(self, filename, options):
+        self.filename = filename
+        if not self.filename.endswith(".dat"):
+            self.filename += ".dat"
+
+    def write(self, detector):
+        logger.info("Writing: " + self.filename)
+
+        data = np.array(detector.data)
+        error = np.array(detector.error)
+
+        # change units for LET from MeV/cm to keV/um
+        from pymchelper.shieldhit.detector.detector_type import SHDetType
+        if detector.dettyp in (SHDetType.dlet, SHDetType.dletg, SHDetType.tlet, SHDetType.tletg):
+            data *= np.float64(0.1)  # 1 MeV / cm = 0.1 keV / um
+            if np.any(error):
+                error *= np.float64(0.1)  # 1 MeV / cm = 0.1 keV / um
+
+        axis_data_column = [list(detector.axis_values(i, plotting_order=True)) for i in range(detector.dimension)]
+
+        fmt = "%g" + " %g" * detector.dimension
+        data_to_save = axis_data_column + [data.ravel()]  # ravel needed to change arrays like [[1]] to [1]
+
+        # if error information is present save it as additional column
+        if detector.error is not None:
+            fmt += " %g"
+            data_to_save += [error.ravel()]
+
+        # transpose from rows to columns
+        data_columns = np.transpose(data_to_save)
+
+        # save space-delimited text file
+        np.savetxt(self.filename, data_columns, fmt=fmt, delimiter=' ')
+
+
+class GnuplotDataWriter:
+    def __init__(self, filename, options):
+        self.data_filename = filename
+        self.script_filename = filename
+        self.plot_filename = filename
+
+        if not self.plot_filename.endswith(".png"):
+            self.plot_filename += ".png"
+        if not self.script_filename.endswith(".plot"):
+            self.script_filename += ".plot"
+        if not self.data_filename.endswith(".dat"):
+            self.data_filename += ".dat"
+
+        dirname = os.path.split(self.script_filename)[0]
+        self.awk_script_filename = os.path.join(dirname, "addblanks.awk")
+
+    _awk_2d_script_content = """/^[[:blank:]]*#/ {next} # ignore comments (lines starting with #)
+NF < 3 {next} # ignore lines which don't have at least 3 columns
+$2 != prev {printf \"\\n\"; prev=$2} # print blank line
+{print} # print the line
+    """
+
+    _header = """set term png
+set output \"{plot_filename}\"
+set title \"{title}\"
+set xlabel \"{xlabel}\"
+set ylabel \"{ylabel}\"
+"""
+
+    _error_plot_command = "'./{data_filename}' u 1:(max($2-$3,0.0)):($2+$3) w filledcurves " \
+                          "fs transparent solid 0.2 lc 3 title '1-sigma confidence', "
+
+    _plotting_command = {
+        1: """max(x,y) = (x > y) ? x : y
+plot {error_plot} './{data_filename}' u 1:2 w l lt 1 lw 2 lc -1 title 'mean value'
+        """,
+        2: """set view map
+splot \"<awk -f addblanks.awk '{data_filename}'\" u 1:2:3 with pm3d
+"""
+    }
+
+    def write(self, detector):
+        # skip plotting 0-D and 3-D data
+        if detector.dimension in (0, 3):
+            return
+
+        # set labels
+        x_axis_number = detector.axis_data(0, plotting_order=True).number
+        xlabel = detector.units[x_axis_number]
+        if detector.dimension == 1:
+            ylabel = ImageWriter._make_label(detector.units[4], detector.title)
+        elif detector.dimension == 2:
+            y_axis_number = detector.axis_data(1, plotting_order=True).number
+            ylabel = detector.units[y_axis_number]
+
+            # for 2-D plots writte additional awk script to convert data
+            # as described in gnuplot faq: http://www.gnuplot.info/faq/faq.html#x1-320003.9
+            with open(self.awk_script_filename, 'w') as script_file:
+                logger.info("Writing: " + self.awk_script_filename)
+                script_file.write(self._awk_2d_script_content)
+
+        # save gnuplot script
+        with open(self.script_filename, 'w') as script_file:
+            logger.info("Writing: " + self.script_filename)
+            script_file.write(self._header.format(plot_filename=self.plot_filename, xlabel=xlabel, ylabel=ylabel,
+                                                  title=detector.title))
+            plt_cmd = self._plotting_command[detector.dimension]
+
+            # add error plot if error data present
+            err_cmd = ""
+            if np.any(detector.error):
+                err_cmd = self._error_plot_command.format(data_filename=self.data_filename)
+
+            script_file.write(plt_cmd.format(data_filename=self.data_filename, error_plot=err_cmd))
+
+
+class ImageWriter:
+    def __init__(self, filename, options):
+        self.plot_filename = filename
+        if not self.plot_filename.endswith(".png"):
+            self.plot_filename += ".png"
+        self.colormap = options.colormap
+
+    default_colormap = 'gnuplot2'
+
+    @staticmethod
+    def _make_label(unit, name):
+        return name + " " + "[" + unit + "]"
+
+    def _save_2d_error_plot(self, detector, xlist, ylist, elist):
+        import matplotlib
+        matplotlib.use('Agg')
+        import matplotlib.pyplot as plt
+
+        plt.pcolormesh(xlist, ylist, elist.clip(0.0), cmap=self.colormap)
+        y_axis_number = detector.axis_data(1, plotting_order=True).number
+        y_axis_name = detector.units[6 + y_axis_number]
+        plt.ylabel(self._make_label(detector.units[y_axis_number], y_axis_name))
+        cbar = plt.colorbar()
+        cbar.set_label(detector.units[4], rotation=270, verticalalignment='bottom')
+        base_name, _ = os.path.splitext(self.plot_filename)
+        plt.savefig(base_name + "_error.png")
+        plt.close()
+
+    def write(self, detector):
+        import matplotlib
+        matplotlib.use('Agg')
+        import matplotlib.pyplot as plt
+
+        # skip plotting 0-D and 3-D data
+        if detector.dimension in (0, 3):
+            return
+
+        data = np.array(detector.data)
+        error = np.array(detector.error)
+
+        # change units for LET from MeV/cm to keV/um
+        from pymchelper.shieldhit.detector.detector_type import SHDetType
+        if detector.dettyp in (SHDetType.dlet, SHDetType.dletg, SHDetType.tlet, SHDetType.tletg):
+            data *= np.float64(0.1)  # 1 MeV / cm = 0.1 keV / um
+            if np.any(error):
+                error *= np.float64(0.1)  # 1 MeV / cm = 0.1 keV / um
+
+        logger.info("Writing: " + self.plot_filename)
+
+        x_axis_number = detector.axis_data(0, plotting_order=True).number
+        x_axis_name = detector.units[6 + x_axis_number]
+        plt.xlabel(self._make_label(detector.units[x_axis_number], x_axis_name))
+        xlist = list(detector.axis_values(0, plotting_order=True))  # make list of values from generator
+
+        # 1-D plotting
+        if detector.dimension == 1:
+
+            # add optional error area
+            if np.any(detector.error):
+                plt.fill_between(xlist,
+                                 (data - error).clip(0.0),
+                                 (data + error).clip(0.0, 1.05 * (detector.v.max())),
+                                 alpha=0.2, edgecolor='#CC4F1B', facecolor='#FF9848', antialiased=True)
+            plt.ylabel(self._make_label(detector.units[4], detector.title))
+            plt.plot(xlist, data)
+        elif detector.dimension == 2:
+            ylist = list(detector.axis_values(1, plotting_order=True))   # make list of values from generator
+
+            xn = detector.axis_data(0, plotting_order=True).n
+            yn = detector.axis_data(1, plotting_order=True).n
+
+            shape_tuple = (yn, xn)
+            xlist = np.asarray(xlist).reshape(shape_tuple)
+            ylist = np.asarray(ylist).reshape(shape_tuple)
+            zlist = data.reshape(shape_tuple)
+
+            # add error plot if error data present
+            if np.any(detector.error):
+                elist = error.reshape(shape_tuple)
+                self._save_2d_error_plot(detector, xlist, ylist, elist)
+
+            x_axis_number = detector.axis_data(0, plotting_order=True).number
+            x_axis_name = detector.units[6 + x_axis_number]
+            plt.xlabel(self._make_label(detector.units[x_axis_number], x_axis_name))
+
+            y_axis_number = detector.axis_data(1, plotting_order=True).number
+            y_axis_name = detector.units[6 + y_axis_number]
+            plt.ylabel(self._make_label(detector.units[y_axis_number], y_axis_name))
+
+            plt.pcolormesh(xlist, ylist, zlist, cmap=self.colormap)
+            cbar = plt.colorbar()
+            cbar.set_label(detector.units[4], rotation=270, verticalalignment='bottom')
+        plt.savefig(self.plot_filename)
+        plt.close()
