d760948f2efaf3573d6d46e08cf19c650cfdba2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:07 +0000

    Add package wcpan.model/1.0.0.dev2/wcpan.model-1.0.0.dev2.tar.gz

---
+++ packages/wcpan.model/wcpan.model-1.0.0.dev2.tar.gz/wcpan.model-1.0.0.dev2/wcpan/model/__init__.py
@@ -0,0 +1,4 @@
+from .models import Model
+from .fields import (BaseField, StringField, IntegerField, FloatField,
+                    BooleanField, DateTimeField, DateField, TimeField,
+                    ModelField, ModelCollectionField, FieldCollectionField)
