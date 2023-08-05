2ff51f781636c99f50945817d8e237e1cbc7ef82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:08 +0000

    Add package mgn/1.1.2/mgn-1.1.2.tar.gz

---
+++ packages/mgn/mgn-1.1.2.tar.gz/mgn-1.1.2/README.rst
@@ -0,0 +1,8 @@
+mgn
+=====
+
+A SageMath package for computations on the moduli space of stable curves.
+
+This package is distributed on PyPI. `Click here`_ to find it there and see installation and use instructions.
+
+.. _Click here: https://pypi.python.org/pypi/mgn/
---
+++ packages/mgn/mgn-1.1.2.tar.gz/mgn-1.1.2/setup.py
@@ -0,0 +1,30 @@
+# nodoctest
+
+from distutils.core import setup
+
+
+
+try:
+    from sage.env import SAGE_SRC, SAGE_VERSION
+except ImportError:
+    import warnings
+    warnings.warn("This package only works with SageMath!")
+
+version = "1.1.2"
+
+print "This is version", version
+
+with open("description.rst") as f:
+    long_description = f.read()
+    
+setup(
+    name='mgn',
+    version=version,
+    packages=['strataalgebra', 'topintersections'],
+    url='https://github.com/uberparagon/mgn',
+    license='MIT',
+    author='Drew Johnson',
+    author_email='werd2.718@gmail.com',
+    description='A Sage program for computing products, Faber-Zagier relations, and top intersections on the moduli space of stable curves.',
+    long_description = long_description
+)
