276ca991665ee68888659ef171cff4a1a9a1fe25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:57 +0000

    Add package lsd/0.3.0/lsd-0.3.0.tar.gz

---
+++ packages/lsd/lsd-0.3.0.tar.gz/lsd-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lsd',
+    version='0.3.0',
+    description='Pounds, shillings and pence conversion',
+    url='http://github.com/jwg4/lsd',
+    author='Jack Grahl',
+    author_email='jack.grahl@gmail.com',
+    license='MIT',
+    packages=['lsd'],
+    zip_safe=False,
+    test_suite="tests",
+    tests_require=["hypothesis"]
+)
