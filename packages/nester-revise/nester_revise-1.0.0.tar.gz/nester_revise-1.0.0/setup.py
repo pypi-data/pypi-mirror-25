eb66e21fc6fb3f9f690c2ccfbc2dca8dd7205c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:43 +0000

    Add package nester-revise/1.0.0/nester_revise-1.0.0.tar.gz

---
+++ packages/nester-revise/nester_revise-1.0.0.tar.gz/nester_revise-1.0.0/setup.py
@@ -0,0 +1,12 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup (
+        name='nester_revise',
+        version='1.0.0',
+        py_modules=['nest_revise'],
+        author='jr',
+        author_email='jr001@me.com',
+        url='http://nanoalmighty.com',
+        description='A simple printer of lists',
+        )
