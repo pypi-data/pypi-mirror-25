484eeec8b8f723f60d17608674a95d168e508ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:48 +0000

    Add package launchpad-rtmidi-py/0.7.0/launchpad_rtmidi_py-0.7.0-py2.7.egg

---
+++ packages/launchpad-rtmidi-py/launchpad_rtmidi_py-0.7.0-py2.7.egg/launchpad_py/__init__.py
@@ -0,0 +1,7 @@
+from launchpad import Launchpad
+from launchpad import LaunchpadMk2
+from launchpad import LaunchpadPro
+from launchpad import LaunchControlXL
+from launchpad import LaunchKeyMini
+from launchpad import Dicer
+import charset
