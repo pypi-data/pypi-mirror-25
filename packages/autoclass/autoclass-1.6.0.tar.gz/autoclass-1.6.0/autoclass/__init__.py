b0ae43f650c30a338a4d639d1bd6acb9297fbe62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:16 +0000

    Add package autoclass/1.6.0/autoclass-1.6.0.tar.gz

---
+++ packages/autoclass/autoclass-1.6.0.tar.gz/autoclass-1.6.0/autoclass/__init__.py
@@ -0,0 +1,8 @@
+from autoclass.autoargs import *
+from autoclass.autoprops import *
+from autoclass.autodict import *
+from autoclass.var_checker import *
+from autoclass.validate import *
+from autoclass.booleans import *
+
+__all__ = ['autoargs', 'autoprops', 'autodict', 'validate', 'var_checker', 'booleans']
