e4837098b2e075a1bd09f427e2a2cb08875a3b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:06 +0000

    Add package bitlist/0.1.0.0/bitlist-0.1.0.0.tar.gz

---
+++ packages/bitlist/bitlist-0.1.0.0.tar.gz/bitlist-0.1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name             = 'bitlist',
+    version          = '0.1.0.0',
+    packages         = ['bitlist',],
+    install_requires = [],
+    license          = 'MIT',
+	url              = 'https://github.com/lapets/bitlist',
+	author           = 'Andrei Lapets',
+	author_email     = 'a@lapets.io',
+    description      = 'Minimal Python library for working with little-endian list representation of bit strings.',
+    long_description = open('README.rst').read(),
+    test_suite       = 'nose.collector',
+    tests_require    = ['nose'],
+)
