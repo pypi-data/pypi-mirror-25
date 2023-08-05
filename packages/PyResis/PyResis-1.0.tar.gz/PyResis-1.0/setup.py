8a6ab1c879b9c4aeb7c84a8e9f3bbc7d4812d152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package PyResis/1.0/PyResis-1.0.tar.gz

---
+++ packages/PyResis/PyResis-1.0.tar.gz/PyResis-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyResis',
+    version='1.0',
+    packages=['PyResis'],
+    url='https://github.com/MaritimeRenewable/PyResis',
+    license='MIT',
+    author='Yu Cao',
+    author_email='tsaoyu@gmail.com',
+    description='Python ship Resistance estimation package'
+)
