6c0bac637c96790589f3f2bc006b5ef37b7ecc7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:00 +0000

    Add package HeartbeatMaker/0.9/HeartbeatMaker-0.9.tar.gz

---
+++ packages/HeartbeatMaker/HeartbeatMaker-0.9.tar.gz/HeartbeatMaker-0.9/test/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from redis import StrictRedis
+from HeartbeatMaker import HeartbeatMaker
+import arrow
+
+
+def test(it):
+    print('%s:%s:心跳' % (arrow.now(), it))
+
+
+maker = HeartbeatMaker('redis://localhost:6379/0', 'test-beat', test)
+
+# maker.clean()
+# maker.beat_it('bac', 6,'bac-par')
+# maker.beat_it('shawn', 2,'par')
+# maker.omit_it('jack')
+maker.beat_it('jack', 5,'par')
+
+# maker.start()
