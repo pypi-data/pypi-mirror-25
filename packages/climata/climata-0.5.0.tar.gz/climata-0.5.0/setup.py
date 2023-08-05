50c8bec612439ae0eed7379ddcdd3e727ece9ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:48 +0000

    Add package climata/0.5.0/climata-0.5.0.tar.gz

---
+++ packages/climata/climata-0.5.0.tar.gz/climata-0.5.0/setup.py
@@ -0,0 +1,72 @@
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+
+SERVICES = (
+    "ACIS (NOAA RCCs)",
+    "CoCoRaHS",
+    "Hydromet (USBR)",
+    "SNOTEL AWDB (NRCS)",
+    "NWIS (USGS)",
+)
+SVC_STR = ", ".join(SERVICES[:-1]) + " and " + SERVICES[-1]
+
+DESCRIPTION = "Loads climate and hydrology data from %s." % SVC_STR
+
+LONG_DESCRIPTION = """
+A pythonic library for iterating over climate time series data from %s.
+Powered by wq.io.
+""" % SVC_STR
+
+
+def long_description():
+    """Return long description from README.rst if it's present
+    because it doesn't get installed."""
+    try:
+        return open(join(dirname(__file__), 'README.rst')).read()
+    except IOError:
+        return LONG_DESCRIPTION
+
+
+def get_version():
+    version = open(join(dirname(__file__), "climata", "version.py")).read()
+    version = version.strip().replace("VERSION = ", '')
+    version = version.replace('"', '')
+    return version
+
+setup(
+    name='climata',
+    version=get_version(),
+    author='HEI Geo',
+    author_email='gis@houstoneng.com',
+    url='https://github.com/heigeo/climata',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    description=DESCRIPTION,
+    long_description=long_description(),
+    install_requires=[
+        'wq.io>=0.7.0',
+        'owslib>=0.9',
+        "suds-jurko",
+    ],
+    scripts=['climata/bin/acis_sites.py', 'climata/bin/acis_data.py'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Natural Language :: English',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: System :: Networking :: Monitoring',
+    ],
+    test_suite='tests',
+    tests_require=['django'],
+    # tests_require=['iso8601'],  # FIXME: Requires wq.io==1.0.1
+)
