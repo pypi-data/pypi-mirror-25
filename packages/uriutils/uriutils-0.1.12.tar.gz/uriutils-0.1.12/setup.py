217f4c4a5b1e4c0aefd8f2e9b1b99e608c444a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package uriutils/0.1.12/uriutils-0.1.12.tar.gz

---
+++ packages/uriutils/uriutils-0.1.12.tar.gz/uriutils-0.1.12/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='uriutils',
+    version='0.1.12',
+    description='Easily read and write to different storage platforms in Python.',
+    long_description=open('README.rst', 'r').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet',
+        'Topic :: System :: Filesystems',
+    ],
+    keywords='boto3 s3 google cloud filesystem file uri url http ftp',
+    url='http://github.com/skylander86/uriutils',
+    author='Yanchuan Sim',
+    author_email='yanchuan@outlook.com',
+    license='Apache License 2.0',
+    packages=['uriutils'],
+    zip_safe=True,
+)
