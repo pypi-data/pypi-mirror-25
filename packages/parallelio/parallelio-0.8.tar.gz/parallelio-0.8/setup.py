f4623f51ab8e47a217988989cf1bc30cc7f93ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package parallelio/0.8/parallelio-0.8.tar.gz

---
+++ packages/parallelio/parallelio-0.8.tar.gz/parallelio-0.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'parallelio',
+  packages = ['parallelio'],
+  version = '0.8',
+  description = 'Basic tools for working with natural language text data',
+  author = 'Signal N',
+  author_email = 'parallelio@signaln.com',
+  url = 'https://github.com/signaln/parallelio',
+  download_url = 'https://github.com/signaln/parallelio/archive/0.8.tar.gz',
+  keywords = ['text', 'data', 'files', 'io', 'natural language processing'],
+  classifiers = [],
+)
