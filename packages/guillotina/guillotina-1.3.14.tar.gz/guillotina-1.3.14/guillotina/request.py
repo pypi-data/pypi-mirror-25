7dcf640f2eb87b16dc0440b7da8d4fe2bc1d06ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:46 +0000

    Add package guillotina/1.3.14/guillotina-1.3.14.tar.gz

---
+++ packages/guillotina/guillotina-1.3.14.tar.gz/guillotina-1.3.14/guillotina/request.py
@@ -0,0 +1,59 @@
+from aiohttp import web_request
+from guillotina.interfaces import IDefaultLayer
+from guillotina.interfaces import IRequest
+from zope.interface import implementer
+
+import asyncio
+
+
+@implementer(IRequest, IDefaultLayer)
+class Request(web_request.Request):
+    """
+    Guillotina specific request type.
+    We store potentially a lot of state onto the request
+    object as it is essential our poor man's thread local model
+    """
+
+#    _db_id = None
+#    _tm = None
+#    _txn = None
+#    _container_id = None
+#    container = None
+#    container_settings = None
+#    tail = None
+#    resource = None
+#    security = None
+
+    _db_write_enabled = True
+    _futures = None
+
+    application = None
+    exc = None
+    view_name = None
+    found_view = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._futures = {}
+
+    def add_future(self, name, fut):
+        if self._futures is None:
+            self._futures = {}
+        self._futures[name] = fut
+
+    def get_future(self, name):
+        try:
+            return self._futures[name]
+        except (AttributeError, KeyError):
+            return
+
+    async def execute_futures(self):
+        if self._futures is None:
+            return
+        futures = []
+        for fut in self._futures.values():
+            if not asyncio.iscoroutine(fut):
+                fut = fut()
+            futures.append(fut)
+        await asyncio.gather(*futures)
+        self._futures = {}
