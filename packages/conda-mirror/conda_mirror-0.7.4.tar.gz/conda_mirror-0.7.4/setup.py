244b2207a0970b194164616410d5632613884a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:29 +0000

    Add package conda-mirror/0.7.4/conda_mirror-0.7.4.tar.gz

---
+++ packages/conda-mirror/conda_mirror-0.7.4.tar.gz/conda_mirror-0.7.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+import versioneer
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    name='conda_mirror',
+    author="Eric Dill",
+    packages=find_packages(),
+    author_email='eric.dill@maxpoint.com',
+    description='mirror an upstream conda channel to a local directory',
+    url='https://github.com/maxpoint/conda-mirror',
+    platforms='Cross platform (Linux, Mac OSX, Windows)',
+    license='BSD 3-Clause',
+    install_requires=[
+        'requests',
+        'pyyaml',
+    ],
+    entry_points={
+        "console_scripts": [
+            'conda-mirror = conda_mirror.conda_mirror:cli'
+        ]
+    }
+)
