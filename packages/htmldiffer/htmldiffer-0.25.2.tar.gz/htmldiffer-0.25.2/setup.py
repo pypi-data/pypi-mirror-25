d6aed95dba8703d935d56934dbb7eebfbca67280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:08 +0000

    Add package htmldiffer/0.25.2/htmldiffer-0.25.2.tar.gz

---
+++ packages/htmldiffer/htmldiffer-0.25.2.tar.gz/htmldiffer-0.25.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="htmldiffer",
+    version="0.25.2",
+    author="Anastasia Aizman",
+    author_email="anastasia.aizman@gmail.com",
+    description=("HTML diff"),
+    license="MIT",
+    keywords="html diff",
+    url="https://github.com/anastasia/htmldiffer",
+    packages=['htmldiffer', 'tests'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=2.7'
+)
+
