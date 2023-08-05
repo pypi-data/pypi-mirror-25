11de4f99a44f79085845060f912146ee9e429a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:43 +0000

    Add package mettle_protocol/1.0.3/mettle_protocol-1.0.3.tar.gz

---
+++ packages/mettle_protocol/mettle_protocol-1.0.3.tar.gz/mettle_protocol-1.0.3/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import setuptools
+
+setup_params = dict(
+    name='mettle_protocol',
+    version='1.0.3',
+    author='Y Team',
+    author_email=', '.join([
+        'alejandro.rivera@yougov.com',
+        'fernando.gutierrez@yougov.com',
+        'brent.tubbs@gmail.com',
+    ]),
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'isodate==0.5.0',
+        'pika==0.9.14',
+        'utc==0.0.3',
+    ],
+    description='A robust framework for scheduling and executing ETL jobs.',
+)
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_params)
