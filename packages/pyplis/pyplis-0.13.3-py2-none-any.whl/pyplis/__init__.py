5a69aba6a9fcfc7825074718e90e94fac717f761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:18 +0000

    Add package pyplis/0.13.3/pyplis-0.13.3-py2-none-any.whl

---
+++ packages/pyplis/pyplis-0.13.3-py2-none-any.whl/pyplis/__init__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from os.path import abspath, dirname
+from pkg_resources import get_distribution
+from matplotlib import rcParams
+
+rcParams["mathtext.default"] = u"regular"
+
+__dir__ = abspath(dirname(__file__))
+__version__ = get_distribution('pyplis').version
+_LIBDIR = __dir__ #from older version
+
+URL_TESTDATA = ("https://folk.nilu.no/~gliss/pyplis_testdata/"
+                "pyplis_etna_testdata.zip")
+
+try:
+    import pydoas
+    PYDOASAVAILABLE =True
+except:
+    PYDOASAVAILABLE = False
+
+try:
+    import geonum
+    GEONUMAVAILABLE = 1
+except:
+    GEONUMAVAILABLE = 0
+
+from inout import download_test_data, find_test_data
+from geometry import MeasGeometry
+from utils import Filter, DarkOffsetInfo
+from image import Img
+import custom_image_import
+from dataset import Dataset
+from imagelists import ImgList, CellImgList
+from plumebackground import PlumeBackgroundModel
+from cellcalib import CellCalibData, CellCalibEngine
+from doascalib import DoasCalibData, DoasFOV, DoasFOVEngine
+from plumespeed import find_signal_correlation, OptflowFarneback,\
+    FarnebackSettings, LocalPlumeProperties
+from processing import LineOnImage, ImgStack, ProfileTimeSeriesImg,\
+    PixelMeanTimeSeries
+from dilutioncorr import DilutionCorr
+from fluxcalc import EmissionRateAnalysis, EmissionRates,\
+    EmissionRateSettings
+from optimisation import PolySurfaceFit, MultiGaussFit
+
+from setupclasses import MeasSetup, Camera, Source
+
+
+#==============================================================================
+# import model_functions 
+# import helpers
+# import exceptions
+# import glob
+#==============================================================================
+
+
+
+
+#import gui_features as gui_features
---
+++ packages/pyplis/pyplis-0.13.3-py2-none-any.whl/pyplis/exceptions.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Module containing pyplis custom exceptions"""
+class CellSearchError(Exception):
+    pass
+
+class FileNameInfoAccessError(Exception):
+    pass
+
+class ImgMetaError(Exception):
+    pass
+
+class ImgModifiedError(Exception):
+    pass
+
+class MetaAccessError(Exception):
+    pass
