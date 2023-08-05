a12090f2a2631ddeec8ac1583dbc189caca12b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package AnchorHarbor/0.1.0/AnchorHarbor-0.1.0.tar.gz

---
+++ packages/AnchorHarbor/AnchorHarbor-0.1.0.tar.gz/AnchorHarbor-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='AnchorHarbor',
+
+    version='0.1.0',
+
+    description='A small event emitter for Python',
+
+    author='Srishan Bhattarai',
+    author_email='srishanbhattarai@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers'
+    ],
+
+    py_modules=['Anchor']
+)
