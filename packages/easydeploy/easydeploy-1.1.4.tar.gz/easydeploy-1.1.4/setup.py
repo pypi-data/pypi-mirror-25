b547e841faa8ab49ecd0232294b18ade254c3e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:20 +0000

    Add package easydeploy/1.1.4/easydeploy-1.1.4.tar.gz

---
+++ packages/easydeploy/easydeploy-1.1.4.tar.gz/easydeploy-1.1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easydeploy',
+    version='1.1.4',
+    install_requires=[
+        'Fabric>=1.8.2',
+        'fabtools>=0.19.0',
+        'PyYAML>=3.11'
+      ],
+    entry_points={
+        "console_scripts": [
+            "ed = easydeploy.main:main",
+        ]
+    },
+    url='https://github.com/rodrigorodriguescosta/easydeploy.git',
+    license='',
+    author='Rodrigo Rodrigues',
+    author_email='rodrigorodriguescosta@gmail.com',
+    description=''
+)
