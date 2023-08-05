a485e5c818337aa6ecf7421b2c40408fb866c46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:48 +0000

    Add package jenkins-publisher-bitbucket/0.0.1/jenkins-publisher-bitbucket-0.0.1.tar.gz

---
+++ packages/jenkins-publisher-bitbucket/jenkins-publisher-bitbucket-0.0.1.tar.gz/jenkins-publisher-bitbucket-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='jenkins-publisher-bitbucket',
+    version='0.0.1',
+    description='Jenkins Job Builder Bitbucket Notifier',
+    url='https://github.com/bobtfish/jenkins-publisher-bitbucket',
+    author='Tomas Doran',
+    author_email='bobtfish@bobtfish.net',
+    license='MIT license',
+    install_requires=[],
+    entry_points={
+        'jenkins_jobs.publishers': [
+            'bitbucket = jenkins_publisher_bitbucket.bitbucket:bitbucket_publisher']},
+    packages=['jenkins_publisher_bitbucket'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'])
