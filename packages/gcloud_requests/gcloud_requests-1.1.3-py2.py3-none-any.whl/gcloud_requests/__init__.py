5885c6d78b00dd90f50c831a3b60edb4b9a41019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:10 +0000

    Add package gcloud_requests/1.1.3/gcloud_requests-1.1.3-py2.py3-none-any.whl

---
+++ packages/gcloud_requests/gcloud_requests-1.1.3-py2.py3-none-any.whl/gcloud_requests/__init__.py
@@ -0,0 +1,6 @@
+from .proxy import RequestsProxy  # noqa
+from .datastore import DatastoreRequestsProxy, enter_transaction, exit_transaction  # noqa
+from .pubsub import PubSubRequestsProxy  # noqa
+from .storage import CloudStorageRequestsProxy  # noqa
+
+__version__ = "1.1.3"
