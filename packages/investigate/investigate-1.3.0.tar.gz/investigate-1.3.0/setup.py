0ac01f32ef42455292b7a0d881054b1dc21092ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:18 +0000

    Add package investigate/1.3.0/investigate-1.3.0.tar.gz

---
+++ packages/investigate/investigate-1.3.0.tar.gz/investigate-1.3.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+
+setup(
+    name='investigate',
+    version='1.3.0',
+    description='Python interface for the OpenDNS Investigate API',
+    url='https://github.com/opendns/pyinvestigate',
+    author='Skyler Hawthorne, Thibault Reuille',
+    author_email='skylerhawthorne@gmail.com, thibault@opendns.com',
+    license='MIT',
+    keywords='opendns investigate',
+    packages=find_packages(),
+    install_requires=['requests'],
+)
