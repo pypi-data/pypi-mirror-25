48bf44a6b5045517b046980203309a9c64f564b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:12 +0000

    Add package revscoring/1.3.19/revscoring-1.3.19-py2.py3-none-any.whl

---
+++ packages/revscoring/revscoring-1.3.19-py2.py3-none-any.whl/revscoring/languages/__init__.py
@@ -0,0 +1,114 @@
+"""
+This module implements a set of languages as collections of features that are
+language specific.
+
+feature collections
++++++++++++++++++++
+Languages implement a subset of feature collections (e.g.
+:class:`~revscoring.languages.features.Dictionary`,
+:class:`~revscoring.languages.features.Stopwords`,
+:class:`~revscoring.languages.features.Stemmed` and
+:class:`~revscoring.languages.features.RegexMatches`) based on what
+language assets are available. See :mod:`revscoring.languages.features`.
+
+albanian
+++++++++++
+.. automodule:: revscoring.languages.albanian
+    :members:
+
+bengali
+++++++++++
+.. automodule:: revscoring.languages.bengali
+    :members:
+
+dutch
++++++
+.. automodule:: revscoring.languages.arabic
+    :members:
+
+dutch
++++++
+.. automodule:: revscoring.languages.dutch
+    :members:
+
+english
++++++++
+.. automodule:: revscoring.languages.english
+    :members:
+
+estonian
+++++++++
+.. automodule:: revscoring.languages.estonian
+    :members:
+
+finnish
+++++++++++
+.. automodule:: revscoring.languages.finnish
+    :members:
+
+french
+++++++
+.. automodule:: revscoring.languages.french
+    :members:
+
+german
+++++++
+.. automodule:: revscoring.languages.german
+    :members:
+
+greek
++++++++
+.. automodule:: revscoring.languages.greek
+    :members:
+
+hebrew
+++++++
+.. automodule:: revscoring.languages.hebrew
+    :members:
+
+indonesianbengali
+++++++++++
+.. automodule:: revscoring.languages.indonesian
+    :members:
+
+italian
++++++++
+.. automodule:: revscoring.languages.italian
+    :members:
+
+japanese
++++++++bengali
+.. automodule:: revscoring.languages.japanese
+    :members:
+
+persian
++++++++
+.. automodule:: revscoring.languages.persian
+    :members:
+
+portuguese
+++++++++++
+.. automodule:: revscoring.languages.portuguese
+    :members:
+
+spanish
++++++++
+.. automodule:: revscoring.languages.spanish
+    :members:
+
+turkish
++++++++
+.. automodule:: revscoring.languages.turkish
+    :members:
+
+ukrainian
++++++++++
+.. automodule:: revscoring.languages.ukrainian
+    :members:
+
+vietnamese
+++++++++++
+.. automodule:: revscoring.languages.vietnamese
+    :members:
+
+"""
---
+++ packages/revscoring/revscoring-1.3.19-py2.py3-none-any.whl/revscoring/utilities/__init__.py
@@ -0,0 +1,39 @@
+"""
+This module implements a set of utilities for extracting features and
+train/testing :class:`revscoring.scorer_models.MLScorerModel` from the
+command-line.  When the revscoring python package is installed, a `revscoring`
+utility should be available from the commandline.  Run `revscoring -h` for more
+information:
+
+cv_train
+++++++++
+.. automodule:: revscoring.utilities.cv_train
+
+extract
++++++++
+.. automodule:: revscoring.utilities.extract
+
+fit
++++
+.. automodule:: revscoring.utilities.fit
+
+model_info
+++++++++++
+.. automodule:: revscoring.utilities.model_info
+
+score
++++++
+.. automodule:: revscoring.utilities.score
+
+train_model
++++++++++++
+.. automodule:: revscoring.utilities.train_model
+
+test_model
+++++++++++
+.. automodule:: revscoring.utilities.test_model
+
+tune
+++++
+.. automodule:: revscoring.utilities.tune
+"""
