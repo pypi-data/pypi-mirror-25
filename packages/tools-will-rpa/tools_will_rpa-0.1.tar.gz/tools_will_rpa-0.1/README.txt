8f30e31f8224a792a0be8a1cac353f937d67c7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:00 +0000

    Add package tools-will-rpa/0.1/tools_will_rpa-0.1.tar.gz

---
+++ packages/tools-will-rpa/tools_will_rpa-0.1.tar.gz/tools_will_rpa-0.1/README.txt
@@ -0,0 +1,2 @@
+test pypi
+download_url='https://github.com/wiflore/rpa_test/archive/0.1.tar.gz',  # I'll explain this in a second
---
+++ packages/tools-will-rpa/tools_will_rpa-0.1.tar.gz/tools_will_rpa-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tools_will_rpa',
+    version='0.1',
+    packages=['rpa_bot'],
+    url='https://github.com/wiflore/rpa_test/tree/master/mypackage',
+    license='APACHE 2.0',
+    author='JamesAir',
+    author_email='williamflorezmaestre@gmail.com',
+    description='testing'
+)
