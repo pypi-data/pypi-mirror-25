b5953f1b571209ab6524b5a977cb420620201db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:51 +0000

    Add package django-appmail/0.7/django-appmail-0.7.tar.gz

---
+++ packages/django-appmail/django-appmail-0.7.tar.gz/django-appmail-0.7/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="django-appmail",
+    version="0.7",
+    packages=find_packages(),
+    install_requires=[
+        'Django>1.8',
+        'psycopg2>=2.7.1',
+        'six'
+    ],
+    include_package_data=True,
+    description='Django app for managing localised email templates.',
+    long_description=README,
+    url='https://github.com/yunojuno/django-appmail',
+    author='YunoJuno',
+    author_email='code@yunojuno.com',
+    maintainer='YunoJuno',
+    maintainer_email='code@yunojuno.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
