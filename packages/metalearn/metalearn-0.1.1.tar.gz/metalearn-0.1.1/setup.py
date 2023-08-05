4607b2810aec7022105a44b8f06aeded0c92f2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:16 +0000

    Add package metalearn/0.1.1/metalearn-0.1.1.tar.gz

---
+++ packages/metalearn/metalearn-0.1.1.tar.gz/metalearn-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'metalearn',
+  packages = find_packages(),
+  version = '0.1.1',
+  description = 'A package to retrieve metafeatures from datasets',
+  author = 'Casey Davis',
+  author_email = 'caseykdavis@gmail.com',
+  url = 'https://github.com/byu-dml/metalearn',
+  download_url = 'https://github.com/byu-dml/metalearn/archive/0.1.tar.gz',
+  keywords = ['metalearning', 'machine learning', 'darpa', 'metalearn'],
+  classifiers = [],
+)
