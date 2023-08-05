bd2df794994dfe724c054d6a76c4fb8ab6c1bd22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:35 +0000

    Add package storagex/0.3/storagex-0.3.tar.gz

---
+++ packages/storagex/storagex-0.3.tar.gz/storagex-0.3/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+long_description = 'a library to storage file to cyber space'
+
+
+setup(
+    name='storagex',
+    version='0.3',
+    description='just storagex',
+    long_description=long_description,
+    author='RainX<Jing Xu>',
+    author_email='i@rainx.cc',
+    url='https://github.com/rainx/storagex',
+    packages=find_packages(),
+    install_requires=[
+            'click',
+            'six',
+            'requests',
+            'cryptography',
+            'Pillow'
+    ],
+    entry_points={
+          'console_scripts': [
+              'storagex=storagex.commandline:storagex'
+          ]
+      },
+    )
+
---
+++ packages/storagex/storagex-0.3.tar.gz/storagex-0.3/storagex/commandline.py
@@ -0,0 +1,32 @@
+#coding: utf-8
+
+import click
+from storagex.storage import Storage
+
+
+@click.group()
+def storagex():
+    pass
+
+@storagex.command()
+#@click.option('-f', '--filename', help="file to upload")
+@click.argument('filename')
+def upload(filename):
+    storage = Storage(1024, 768)
+    storage.verbose = True
+    ret = storage.put_file(filename)
+    if ret is not False and ret[0] is not None:
+        click.secho("Completely uploaded! YOUR DOWNLOAD KEY IS : {}".format(ret[0]), fg="green")
+
+
+@storagex.command()
+# @click.option('-k', '--key', help="Download Key")
+# @click.option('-f', '--filename', help="file name of download file")
+@click.argument("key")
+@click.argument("filename", default=None, required=False)
+def download(key, filename):
+    storage = Storage(1024, 768)
+    storage.verbose = True
+    filename_out = storage.download_file(meta_info_key=key, file_name=filename)
+    if filename_out is not False:
+        click.secho("donwload done! the file is {}".format(filename_out), fg='green')
