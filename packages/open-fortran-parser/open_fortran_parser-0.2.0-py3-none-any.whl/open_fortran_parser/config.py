f48808876f3823b34d2960715d2ee1763802c8fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:57 +0000

    Add package open-fortran-parser/0.2.0/open_fortran_parser-0.2.0-py3-none-any.whl

---
+++ packages/open-fortran-parser/open_fortran_parser-0.2.0-py3-none-any.whl/open_fortran_parser/config.py
@@ -0,0 +1,13 @@
+"""Default configuration for open_fortran_parser package."""
+
+import pathlib
+
+JAVA = {
+    'executable': pathlib.Path('java'),
+    'classpath': None,
+    'ofp_class': 'fortran.ofp.FrontEnd',
+    'ofp_xml_class': 'fortran.ofp.XMLPrinter'}
+
+OFC = {
+    'executable': pathlib.Path('ofc'),
+    'path': None}
