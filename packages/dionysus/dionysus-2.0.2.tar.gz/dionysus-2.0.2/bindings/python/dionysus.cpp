f4ae7745f761e4adf338378f448c9402fdb50894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:16 +0000

    Add package dionysus/2.0.2/dionysus-2.0.2.tar.gz

---
+++ packages/dionysus/dionysus-2.0.2.tar.gz/dionysus-2.0.2/bindings/python/dionysus.cpp
@@ -0,0 +1,33 @@
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+void init_simplex(py::module&);
+void init_filtration(py::module&);
+void init_rips(py::module&);
+void init_freudenthal(py::module&);
+
+void init_field(py::module&);
+void init_persistence(py::module&);
+void init_omnifield_persistence(py::module&);
+void init_cohomology_persistence(py::module&);
+void init_zigzag_persistence(py::module&);
+void init_diagram(py::module&);
+void init_distances(py::module&);
+
+PYBIND11_MODULE(_dionysus, m)
+{
+    m.doc() = "Dionysus python bindings";
+
+    init_simplex(m);
+    init_filtration(m);
+    init_rips(m);
+    init_freudenthal(m);
+
+    init_field(m);
+    init_persistence(m);
+    init_cohomology_persistence(m);
+    init_omnifield_persistence(m);
+    init_zigzag_persistence(m);
+    init_diagram(m);
+    init_distances(m);
+}
---
+++ packages/dionysus/dionysus-2.0.2.tar.gz/dionysus-2.0.2/bindings/python/filtration.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <dionysus/filtration.h>
+
+#include "simplex.h"
+
+using PyFiltration = dionysus::Filtration<PySimplex>;
