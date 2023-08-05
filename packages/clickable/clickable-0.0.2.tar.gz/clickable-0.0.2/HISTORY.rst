d33ce354f5c4939b4726ab39dde3fa34fb69b1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:02 +0000

    Add package clickable/0.0.2/clickable-0.0.2.tar.gz

---
+++ packages/clickable/clickable-0.0.2.tar.gz/clickable-0.0.2/HISTORY.rst
@@ -0,0 +1,14 @@
+=======
+History
+=======
+
+0.0.1 (2017-09-10)
+------------------
+
+* First release on PyPI.
+
+
+0.0.2.dev4 (2017-09-10)
+-----------------------
+
+* Fixed ignored excludes in lftp_sync
---
+++ packages/clickable/clickable-0.0.2.tar.gz/clickable-0.0.2/clickable/coloredlogs/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import print_function  # pylint: disable=unused-import
+
+import logging
+import sys
+
+import coloredlogs
+
+
+def bootstrap():
+    logger_name = sys.modules[__name__].__package__.split('.')[0]
+    logger = logging.getLogger(logger_name)
+    stdout = logging.getLogger('.'.join(['stdout', logger_name]))
+    logging.getLogger().addFilter(filter)
+    logger_format = '*** %(name)s %(levelname)-7s %(message)s'
+    stdout_format = '* %(levelname)-7s %(message)s'
+    coloredlogs.install(level='WARNING', logger=logger, fmt=logger_format)
+    coloredlogs.install(level='INFO', logger=stdout, fmt=stdout_format)
