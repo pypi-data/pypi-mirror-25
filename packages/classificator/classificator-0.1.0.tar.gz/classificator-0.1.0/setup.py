96e835b37bbf2bf42d2f6263e13b8ac174b8441d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:30 +0000

    Add package classificator/0.1.0/classificator-0.1.0.tar.gz

---
+++ packages/classificator/classificator-0.1.0.tar.gz/classificator-0.1.0/setup.py
@@ -0,0 +1,22 @@
+"""
+Run setup
+"""
+
+from distutils.core import setup
+
+setup(name='classificator',
+      version='0.1.0',
+      description='Text classification automation tool',
+      url='https://github.com/denver1117/classificator',
+      download_url='https://github.com/denver1117/classificator/archive/0.1.tar.gz',
+      author='Evan Harris',
+      author_email='emitchellh@gmail.com',
+      license='MIT',
+      packages=['classificator'],
+      install_requires=[
+          'pandas>=0.18.0',
+          'numpy>=1.13.1',
+          'scipy>=0.17.0',
+          'scikit-learn>=0.18.2',
+          'boto3>=1.4.0'
+      ])
