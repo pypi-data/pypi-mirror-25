fff25f3065cf9b58cd4f44bff22cceebe87a6cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:58 +0000

    Add package django-reviewing/0.0.6/django-reviewing-0.0.6.tar.gz

---
+++ packages/django-reviewing/django-reviewing-0.0.6.tar.gz/django-reviewing-0.0.6/reviews/managers.py
@@ -0,0 +1,32 @@
+from django.db.models import Manager, Avg
+from django.db.models.query import QuerySet
+
+from django.contrib.contenttypes.models import ContentType
+
+
+class ActiveMixin(object):
+    """
+    An extended manager to return active objects.
+    """
+    def active(self):
+        return self.filter(active=True)
+
+
+class ActiveQuerySet(ActiveMixin, QuerySet):
+    pass
+
+
+class ActiveManager(ActiveMixin, Manager):
+    def get_query_set(self):
+        return ActiveQuerySet(self.model, using=self._db)
+
+    def get_average(self, instance):
+        queryset = ActiveQuerySet(self.model, using=self._db)
+        queryset = queryset.filter(object_id=instance.id, content_type=ContentType.objects.get_for_model(instance))
+        avg_dict = queryset.aggregate(average=Avg('score'))
+        return avg_dict.get('average', 0)
+
+    def average(self):
+        queryset = ActiveQuerySet(self.model, using=self._db)
+        avg_dict = queryset.aggregate(average=Avg('score'))
+        return avg_dict.get('average', 0)
