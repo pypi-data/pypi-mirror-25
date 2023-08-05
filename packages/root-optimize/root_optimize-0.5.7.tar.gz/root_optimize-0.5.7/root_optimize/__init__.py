7db831cbd27edd212d58b1461fe05e5da19ac2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:23 +0000

    Add package root-optimize/0.5.7/root_optimize-0.5.7.tar.gz

---
+++ packages/root-optimize/root_optimize-0.5.7.tar.gz/root_optimize-0.5.7/root_optimize/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-,
+from __future__ import absolute_import
+from __future__ import print_function
+
+__version__ = '0.5.7'
+__all__ = ['json_encoder',
+           'utils']
+
+# Set up ROOT
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+ROOT.gROOT.SetBatch(True)
+
+import logging
+from .utils import TqdmLoggingHandler
+logger = logging.getLogger("root_optimize")
+logger.addHandler(TqdmLoggingHandler())
