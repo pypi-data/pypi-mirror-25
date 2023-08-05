16596f2b31173dc1e102e10d7ed5cd7d48cb0e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:35 +0000

    Add package c2p/2017.0.1.1505289747/c2p-2017.0.1.1505289747.tar.gz

---
+++ packages/c2p/c2p-2017.0.1.1505289747.tar.gz/c2p-2017.0.1.1505289747/c2p/main.py
@@ -0,0 +1,13 @@
+
+class Operations:
+    def __init__(self):
+        self.name = "Basic Calculator Example"
+
+    def add(self, x, y):
+        return x + y
+
+    def divide(self, x, y):
+        return x/y
+
+    def multiply(self, x, y):
+        return x * y
