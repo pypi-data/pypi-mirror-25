b3f5450403808aa58b7ad0b2b9b14bfc859e9196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:33 +0000

    Add package testingpythonsdk/0.1/testingpythonsdk-0.1.tar.gz

---
+++ packages/testingpythonsdk/testingpythonsdk-0.1.tar.gz/testingpythonsdk-0.1/README.txt
@@ -0,0 +1,1 @@
+hey i am testing just for fu
---
+++ packages/testingpythonsdk/testingpythonsdk-0.1.tar.gz/testingpythonsdk-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'testingpythonsdk',
+  packages = ['testingpythonsdk'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Deepanshu kartikey',
+  author_email = 'deepanshu.kartikey@clickpost.in',
+  url = 'https://github.com/kartikey406/testingpythonsdk', # use the URL to the github repo
+  download_url = 'https://github.com/kartikey406/testingpythonsdk/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
