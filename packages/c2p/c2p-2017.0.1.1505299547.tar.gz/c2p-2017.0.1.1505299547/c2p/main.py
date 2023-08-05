137103d4b511e355c99171390c390dfea1486062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:07 +0000

    Add package c2p/2017.0.1.1505299547/c2p-2017.0.1.1505299547.tar.gz

---
+++ packages/c2p/c2p-2017.0.1.1505299547.tar.gz/c2p-2017.0.1.1505299547/c2p/main.py
@@ -0,0 +1,25 @@
+
+class Operations:
+    def __init__(self):
+        self.name = "Basic Calculator Example"
+
+    def add(self, x: int, y: int)-> int:
+        """
+        Esta funcion se encargar de sumar dos valores enteros.
+        :param x: int. Primer valor de entrada entero
+        :param y: int. Segundo valor de entrada entero.
+        :return: int. Devuelve la operacion suma x + y
+        """
+        return  x + y
+
+    def divide(self, x, y):
+        pass
+
+    def multiply(self, x, y):
+        pass
+
+    def log_e(self, x):
+        pass
+
+    def exponencial(self, x, y):
+        pass
