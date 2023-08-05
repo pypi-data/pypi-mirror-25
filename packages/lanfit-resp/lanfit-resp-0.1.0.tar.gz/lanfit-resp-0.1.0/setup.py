c2bf3bc6c763ce2f7ea974d2d3c9157e54cb8aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:18 +0000

    Add package lanfit-resp/0.1.0/lanfit-resp-0.1.0.tar.gz

---
+++ packages/lanfit-resp/lanfit-resp-0.1.0.tar.gz/lanfit-resp-0.1.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='lanfit-resp',
+    version='0.1.0',
+    description='requests to flask response with Link header.',
+    long_description=read('README.rst'),
+    author='ll1l11',
+    author_email='ll1l11@qq.com',
+    url='',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['Flask', 'lanfit-exceptions'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ]
+)
