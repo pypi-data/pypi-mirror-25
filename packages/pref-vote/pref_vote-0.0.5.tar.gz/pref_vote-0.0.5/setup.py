2cb15ae60bac429cc8524141bf2925ba03ddb518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:49 +0000

    Add package pref-vote/0.0.5/pref_vote-0.0.5.tar.gz

---
+++ packages/pref-vote/pref_vote-0.0.5.tar.gz/pref_vote-0.0.5/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Setup file for pref_vote.
+
+    This file was generated with PyScaffold 2.5.8, a tool that easily
+    puts up a scaffold for your new Python project. Learn more under:
+    http://pyscaffold.readthedocs.org/
+"""
+
+from setuptools import setup
+
+
+setup(name='pref_vote',
+      packages=['pref_vote'],
+      description='Preferential voting tool',
+      version='0.0.5',
+      url='https://github.com/jmhossler/pref_vote',
+      author='John Hossler',
+      author_email='john.m.hossler@gmail.com',
+)
