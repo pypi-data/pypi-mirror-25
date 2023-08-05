566e014377c3d66981318ede0bc56a2002cee414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:33 +0000

    Add package nyf-nester/1.1.0/nyf_nester-1.1.0.tar.gz

---
+++ packages/nyf-nester/nyf_nester-1.1.0.tar.gz/nyf_nester-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from  distutils.core import setup
+
+
+setup(
+     name          ='nyf_nester',
+	 version       ='1.1.0',
+	 py_modules    =['nyf_nester'],
+	 author        ='nyf1213',
+	 author_email  ='1040541774@qq.com',
+	 description   ='just a test',
+
+
+)
