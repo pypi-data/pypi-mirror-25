9d7d1d203f6eb4f6359ea46ec64abd96ff6be277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:52 +0000

    Add package madliar/0.1a1.dev1019/madliar-0.1a1.dev1019.tar.gz

---
+++ packages/madliar/madliar-0.1a1.dev1019.tar.gz/madliar-0.1a1.dev1019/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages  
+  
+setup(  
+    name='madliar',
+    version='0.1a1.dev1019',
+    description='A tiny WSGI freamwork.',
+    classifiers=[  
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',  
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    # install_requires=['jinja2'],
+    entry_points={
+        'console_scripts': [
+            'madliar-manage.py=madliar:main',
+        ],
+    },
+    author='caoliang',
+    url='https://github.com/cl-ei',
+    author_email='i@caoliang.net',
+    license='MIT',  
+    packages=find_packages(),
+    include_package_data=False,  
+    zip_safe=True,  
+)
+
