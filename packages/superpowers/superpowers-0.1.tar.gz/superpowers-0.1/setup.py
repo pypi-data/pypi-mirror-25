55a02f1ecf31b8e1f9416ca71aa69f9bea0372ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package superpowers/0.1/superpowers-0.1.tar.gz

---
+++ packages/superpowers/superpowers-0.1.tar.gz/superpowers-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import codecs
+
+setup(
+    name='superpowers',
+    description=(
+        'Scala-like map for Python, do [1, 2, 3].map(_*3)'
+    ),
+    version='0.1',
+    license='BSD',
+    author='Teemu',
+    author_email='teemu.on.ihminen@gmail.com',
+    py_modules=['superpowers'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
