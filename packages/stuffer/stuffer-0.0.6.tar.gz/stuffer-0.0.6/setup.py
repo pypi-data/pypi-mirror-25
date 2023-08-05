eda8d5c3a9532703a7a0c8c1022b314ab10bc866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:14 +0000

    Add package stuffer/0.0.6/stuffer-0.0.6.tar.gz

---
+++ packages/stuffer/stuffer-0.0.6.tar.gz/stuffer-0.0.6/setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+import sys
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+
+readme_note = """\
+.. note:: For the latest source, please visit the
+   `Bitbucket repository <https://bitbucket.org/mapflat/stuffer>`_
+   
+
+"""
+
+long_description = readme_note + Path(sys.argv[0]).with_name('README.rst').read_text()
+
+setup(
+    name="stuffer",
+    description = 'Simplified, container-friendly provisioning',
+    long_description = long_description,
+    maintainer="Lars Albertsson",
+    maintainer_email="lalle@mapflat.com",
+    url="http://bitbucket.org/mapflat/stuffer",
+    version='0.0.6',
+    packages=find_packages(exclude=["tests"]),
+    entry_points="""
+      [console_scripts]
+      stuffer=stuffer.main:cli
+    """,
+    install_requires=['click']
+)
