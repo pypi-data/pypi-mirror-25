fd095ad4bd5641ff8f2f9a9fcfd1b2c1a5ea3a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:14 +0000

    Add package prolog/0.1.1/prolog-0.1.1.tar.gz

---
+++ packages/prolog/prolog-0.1.1.tar.gz/prolog-0.1.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+'''
+Simple, robust features for expediting application logging configuration
+'''
+import os
+import sys
+from setuptools import find_packages, setup
+
+
+
+long_description = ''
+readme = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')
+if os.path.exists(readme):
+    with open(readme, 'r') as f:
+        long_description = f.read()
+
+VERSION = __import__('prolog').__version_string__
+
+setup(
+    name='prolog',
+    version=VERSION,
+    description=__doc__.strip(),
+    long_description=long_description,
+    author='David Krauth',
+    author_email='dakrauth@gmail.com',
+    url='https://github.com/dakrauth/prolog',
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    license='MIT License',
+    entry_points={'console_scripts': ['prolog=prolog.__main__:main']},
+    install_requires=['appdirs>=1.4.3'],
+    classifiers=[
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Topic :: System :: Logging',
+        'Topic :: Utilities',
+    ],
+)
