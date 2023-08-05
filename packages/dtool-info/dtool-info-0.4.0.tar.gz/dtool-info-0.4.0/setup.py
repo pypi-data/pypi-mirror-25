a07a2a5a9bec963edea85829295d69415b653e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:19 +0000

    Add package dtool-info/0.4.0/dtool-info-0.4.0.tar.gz

---
+++ packages/dtool-info/dtool-info-0.4.0.tar.gz/dtool-info-0.4.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+url = "https://github.com/jic-dtool/dtool-info"
+version = "0.4.0"
+readme = open('README.rst').read()
+
+setup(
+    name="dtool-info",
+    packages=["dtool_info"],
+    version=version,
+    description="Dtool plugin for getting information about datasets",
+    long_description=readme,
+    include_package_data=True,
+    author="Tjelvar Olsson",
+    author_email="tjelvar.olsson@jic.ac.uk",
+    url=url,
+    install_requires=[
+        "click",
+        "dtoolcore>=2.8.0",
+        "dtool_cli",
+        "pygments",
+    ],
+    entry_points={
+        "dtool.cli": [
+            "diff=dtool_info.dataset:diff",
+            "ls=dtool_info.dataset:ls",
+            "summary=dtool_info.dataset:summary",
+            "item=dtool_info.dataset:item",
+            "identifiers=dtool_info.dataset:identifiers",
+            "verify=dtool_info.dataset:verify",
+        ],
+    },
+    download_url="{}/tarball/{}".format(url, version),
+    license="MIT"
+)
