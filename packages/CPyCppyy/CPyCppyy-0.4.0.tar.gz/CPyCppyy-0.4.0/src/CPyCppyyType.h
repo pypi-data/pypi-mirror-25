8784562374401e7c22190f438bcd3a8fbb8099fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:21 +0000

    Add package CPyCppyy/0.4.0/CPyCppyy-0.4.0.tar.gz

---
+++ packages/CPyCppyy/CPyCppyy-0.4.0.tar.gz/CPyCppyy-0.4.0/src/CPyCppyyType.h
@@ -0,0 +1,57 @@
+#ifndef CPYCPPYY_CPYCPPYYTYPE_H
+#define CPYCPPYY_CPYCPPYYTYPE_H
+
+#if PY_MAJOR_VERSION == 2 && PY_MINOR_VERSION == 2
+
+// In p2.2, PyHeapTypeObject is not yet part of the interface
+#include "structmember.h"
+
+typedef struct {
+   PyTypeObject type;
+   PyNumberMethods as_number;
+   PySequenceMethods as_sequence;
+   PyMappingMethods as_mapping;
+   PyBufferProcs as_buffer;
+   PyObject *name, *slots;
+   PyMemberDef members[1];
+} PyHeapTypeObject;
+
+#endif
+
+
+namespace CPyCppyy {
+
+/** Type object to hold class reference (this is only semantically a presentation
+    of CPyCppyyType instances, not in a C++ sense)
+      @author  WLAV
+      @date    07/06/2017
+      @version 1.0
+ */
+
+    class CPyCppyyClass {
+    public:
+        PyHeapTypeObject fType;
+        Cppyy::TCppType_t fCppType;
+
+    private:
+        CPyCppyyClass() = delete;
+    };
+
+//- metatype type and type verification --------------------------------------
+    extern PyTypeObject CPyCppyyType_Type;
+
+    template<typename T>
+    inline bool CPyCppyyType_Check(T* object)
+    {
+        return object && PyObject_TypeCheck(object, &CPyCppyyType_Type);
+    }
+
+    template<typename T>
+    inline bool CPyCppyyType_CheckExact(T* object)
+    {
+        return object && Py_TYPE(object) == &CPyCppyyType_Type;
+    }
+
+} // namespace CPyCppyy
+
+#endif // !CPYCPPYY_CPYCPPYYTYPE_H
---
+++ packages/CPyCppyy/CPyCppyy-0.4.0.tar.gz/CPyCppyy-0.4.0/src/TConstructorHolder.h
@@ -0,0 +1,28 @@
+#ifndef CPYCPPYY_TCONSTRUCTORHOLDER_H
+#define CPYCPPYY_TCONSTRUCTORHOLDER_H
+
+// Bindings
+#include "TMethodHolder.h"
+
+
+namespace CPyCppyy {
+
+    class TConstructorHolder : public TMethodHolder {
+    public:
+        using TMethodHolder::TMethodHolder;
+
+    public:
+        virtual PyObject* GetDocString();
+        virtual PyCallable* Clone() { return new TConstructorHolder(*this); }
+
+    public:
+        virtual PyObject* Call(
+            ObjectProxy*& self, PyObject* args, PyObject* kwds, TCallContext* ctxt = 0);
+
+    protected:
+        virtual bool InitExecutor_(TExecutor*&, TCallContext* ctxt = 0);
+    };
+
+} // namespace CPyCppyy
+
+#endif // !CPYCPPYY_TCONSTRUCTORHOLDER_H
