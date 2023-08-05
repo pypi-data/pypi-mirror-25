1f8be3dfafa70e78b8441050beaf6ae005f973a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:12 +0000

    Add package pippo/1.0.0/pippo-1.0.0.tar.gz

---
+++ packages/pippo/pippo-1.0.0.tar.gz/pippo-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+       name         = 'pippo',
+       version      = '1.0.0',
+       py_modules   = ['pippo'],
+       author       = 'scardu',
+       author_email = 'scardu@acme.com',
+       url          = 'http://www.acme.com',
+       description  = 'A simple printer of nested lists',
+    )
