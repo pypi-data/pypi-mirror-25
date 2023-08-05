44f39fd4ebfef277ae34be995df233a99575eb2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:35 +0000

    Add package airflow-xplenty/0.2.1/airflow_xplenty-0.2.1.tar.gz

---
+++ packages/airflow-xplenty/airflow_xplenty-0.2.1.tar.gz/airflow_xplenty-0.2.1/airflow_xplenty/job_manager.py
@@ -0,0 +1,25 @@
+import os
+import time
+from client_factory import ClientFactory
+
+class JobManager:
+    FINAL_STATUSES = ['completed', 'failed', 'stopped']
+
+    def __init__(self, client, poll_interval=10):
+        self.client = client
+        self.poll_interval = poll_interval
+
+    def run(self, cluster_id, package_id):
+        job = self.client.add_job(cluster_id, package_id, {})
+        self.job_id = job.id
+
+        while job.status not in self.FINAL_STATUSES:
+            time.sleep(self.poll_interval)
+            job = self.client.get_job(self.job_id)
+
+        if job.status == 'failed':
+            raise Exception('Job for package_id %d failed: %s' % (self.package_id, job.errors))
+
+    def stop(self):
+        if self.job_id is not None:
+            self.client.terminate_job(self.job_id)
---
+++ packages/airflow-xplenty/airflow_xplenty-0.2.1.tar.gz/airflow_xplenty-0.2.1/airflow_xplenty/operators/xplenty_job_operator.py
@@ -0,0 +1,40 @@
+import time
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow_xplenty.client_factory import ClientFactory
+from airflow_xplenty.cluster_factory import ClusterFactory
+from airflow_xplenty.job_manager import JobManager
+from airflow_xplenty.package_finder import PackageFinder
+
+"""Operator to run an Xplenty package on a cluster
+
+Spin-up or re-use a cluster in the given environment ('sandbox' or 'production')
+and execute the named package as a job. If the job completes normally or is
+stopped, this operator will finish successfully. If the job fails, then this
+operator will fail.
+"""
+class XplentyJobOperator(BaseOperator):
+    @apply_defaults
+    def __init__(self, package_name, env='sandbox', **kwargs):
+        self.package_name = package_name
+
+        # Setting all of these instance variables in the constructor,
+        # rather than where they are used to facilitate testing.
+        self.client = ClientFactory().client()
+        self.cluster_factory = ClusterFactory(self.client, env)
+        self.package_finder = PackageFinder(self.client)
+        self.job_manager = JobManager(self.client)
+
+        super(XplentyJobOperator, self).__init__(**kwargs)
+
+    def execute(self, context):
+        cluster = self.cluster_factory.find_or_start()
+        package = self.package_finder.find(self.package_name)
+
+        if package is None:
+            raise Exception('Package %s not found' % self.package_name)
+
+        self.job_manager.run(cluster.id, package.id)
+
+    def on_kill(self):
+        self.job_manager.stop()
