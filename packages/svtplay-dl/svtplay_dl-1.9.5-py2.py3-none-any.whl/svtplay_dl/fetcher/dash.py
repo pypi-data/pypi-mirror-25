d01ce3bb65793d934884ecd9649381f95707c8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:14 +0000

    Add package svtplay-dl/1.9.5/svtplay_dl-1.9.5-py2.py3-none-any.whl

---
+++ packages/svtplay-dl/svtplay_dl-1.9.5-py2.py3-none-any.whl/svtplay_dl/fetcher/dash.py
@@ -0,0 +1,217 @@
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+from __future__ import absolute_import
+import copy
+import xml.etree.ElementTree as ET
+import os
+import re
+
+
+from svtplay_dl.output import progress_stream, output, ETA, progressbar
+from svtplay_dl.utils.urllib import urljoin
+from svtplay_dl.error import UIException, ServiceError
+from svtplay_dl.fetcher import VideoRetriever
+
+
+class DASHException(UIException):
+    def __init__(self, url, message):
+        self.url = url
+        super(DASHException, self).__init__(message)
+
+
+class LiveDASHException(DASHException):
+    def __init__(self, url):
+        super(LiveDASHException, self).__init__(
+            url, "This is a live DASH stream, and they are not supported.")
+
+
+def templateelemt(element, filename, idnumber):
+    files = []
+
+    init = element.attrib["initialization"]
+    media = element.attrib["media"]
+    if "startNumber" in element.attrib:
+        start = int(element.attrib["startNumber"])
+    else:
+        start = 0
+    timeline = element.find("{urn:mpeg:dash:schema:mpd:2011}SegmentTimeline")
+    rvalue = timeline.findall(".//{urn:mpeg:dash:schema:mpd:2011}S[@r]")
+    selements = timeline.findall(".//{urn:mpeg:dash:schema:mpd:2011}S")
+    selements.pop()
+    if rvalue:
+        total = int(rvalue[0].attrib["r"]) + len(selements) + 1
+
+    name = media.replace("$RepresentationID$", idnumber)
+    files.append(urljoin(filename, init.replace("$RepresentationID$", idnumber)))
+
+    if "$Time$" in media:
+        time = []
+        time.append(0)
+        for n in selements:
+            time.append(int(n.attrib["d"]))
+        match = re.search("\$Time\$", name)
+        if match:
+            number = 0
+            if len(selements) < 3:
+                for n in range(start, start + total):
+                    new = name.replace("$Time$", str(n * int(rvalue[0].attrib["d"])))
+                    files.append(urljoin(filename, new))
+            else:
+                for n in time:
+                    number += int(n)
+                    new = name.replace("$Time$", str(number))
+                    files.append(urljoin(filename, new))
+    if "$Number" in name:
+        if re.search("\$Number(\%\d+)d\$", name):
+            vname = name.replace("$Number", "").replace("$", "")
+            for n in range(start, start + total):
+                files.append(urljoin(filename, vname % n))
+        else:
+            for n in range(start, start + total):
+                newname = name.replace("$Number$", str(n))
+                files.append(urljoin(filename, newname))
+    return files
+
+
+def adaptionset(element, url, baseurl=None):
+    streams = {}
+
+    dirname = os.path.dirname(url) + "/"
+    if baseurl:
+        dirname = urljoin(dirname, baseurl)
+
+    template = element[0].find("{urn:mpeg:dash:schema:mpd:2011}SegmentTemplate")
+    represtation = element[0].findall(".//{urn:mpeg:dash:schema:mpd:2011}Representation")
+
+    for i in represtation:
+        files = []
+        segments = False
+        filename = dirname
+        bitrate = int(int(i.attrib["bandwidth"]) / 1000)
+        idnumber = i.attrib["id"]
+
+        if i.find("{urn:mpeg:dash:schema:mpd:2011}BaseURL") is not None:
+            filename = urljoin(filename, i.find("{urn:mpeg:dash:schema:mpd:2011}BaseURL").text)
+
+        if i.find("{urn:mpeg:dash:schema:mpd:2011}SegmentBase") is not None:
+            files.append(filename)
+        if template is not None:
+            segments = True
+            files = templateelemt(template, filename, idnumber)
+        elif i.find("{urn:mpeg:dash:schema:mpd:2011}SegmentTemplate"):
+            segments = True
+            files = templateelemt(i.find("{urn:mpeg:dash:schema:mpd:2011}SegmentTemplate"), filename, idnumber)
+
+        streams[bitrate] = {"segments": segments, "files": files}
+
+    return streams
+
+
+def dashparse(options, res, url):
+    streams = {}
+
+    if not res:
+        return None
+
+    if res.status_code >= 400:
+        streams[0] = ServiceError("Can't read DASH playlist. {0}".format(res.status_code))
+        return streams
+    xml = ET.XML(res.text)
+
+    temp = xml.findall('.//{urn:mpeg:dash:schema:mpd:2011}AdaptationSet[@mimeType="audio/mp4"]')
+    audiofiles = adaptionset(temp, url)
+    temp = xml.findall('.//{urn:mpeg:dash:schema:mpd:2011}AdaptationSet[@mimeType="video/mp4"]')
+    videofiles = adaptionset(temp, url)
+
+    for i in videofiles.keys():
+        bitrate = (int(i) + int(list(audiofiles.keys())[0])) / 1000
+        options.other = "mp4"
+        options.segments = videofiles[i]["segments"]
+        streams[int(bitrate)] = DASH(copy.copy(options), url, bitrate, cookies=res.cookies, audio=audiofiles[list(audiofiles.keys())[0]]["files"], files=videofiles[i]["files"])
+
+    return streams
+
+
+class DASH(VideoRetriever):
+    def name(self):
+        return "dash"
+
+    def download(self):
+        if self.options.live and not self.options.force:
+            raise LiveDASHException(self.url)
+
+        if self.options.segments:
+            if self.audio:
+                self._download2(self.audio, audio=True)
+            self._download2(self.files)
+        else:
+            if self.audio:
+                self._download(self.audio, audio=True)
+            self._download(self.url)
+
+    def _download(self, url, audio=False):
+        cookies = self.kwargs["cookies"]
+        data = self.http.request("get", url, cookies=cookies, headers={'Range': 'bytes=0-8192'})
+        try:
+            total_size = data.headers['Content-Range']
+            total_size = total_size[total_size.find("/")+1:]
+        except KeyError:
+            total_size = 0
+        total_size = int(total_size)
+        bytes_so_far = 8192
+        if audio:
+            file_d = output(copy.copy(self.options), "m4a")
+        else:
+            file_d = output(self.options, self.options.other)
+        if hasattr(file_d, "read") is False:
+            return
+        file_d.write(data.content)
+        eta = ETA(total_size)
+        while bytes_so_far < total_size:
+            old = bytes_so_far + 1
+            bytes_so_far = old + 1000000
+            if bytes_so_far > total_size:
+                bytes_so_far = total_size
+
+            bytes_range = "bytes=%s-%s" % (old, bytes_so_far)
+
+            data = self.http.request("get", url, cookies=cookies, headers={'Range': bytes_range})
+            file_d.write(data.content)
+            if self.options.output != "-" and not self.options.silent:
+                eta.update(old)
+                progressbar(total_size, old, ''.join(["ETA: ", str(eta)]))
+
+        if self.options.output != "-":
+            file_d.close()
+            progressbar(bytes_so_far, total_size, "ETA: complete")
+            progress_stream.write('\n')
+            self.finished = True
+
+    def _download2(self, files, audio=False):
+        cookies = self.kwargs["cookies"]
+
+        if audio:
+            file_d = output(copy.copy(self.options), "m4a")
+        else:
+            file_d = output(self.options, self.options.other)
+        if hasattr(file_d, "read") is False:
+            return
+        eta = ETA(len(files))
+        n = 1
+        for i in files:
+            if self.options.output != "-" and not self.options.silent:
+                eta.increment()
+                progressbar(len(files), n, ''.join(['ETA: ', str(eta)]))
+                n += 1
+            data = self.http.request("get", i, cookies=cookies)
+
+            if data.status_code == 404:
+                break
+            data = data.content
+            file_d.write(data)
+
+        if self.options.output != "-":
+            file_d.close()
+            if not self.options.silent:
+                progress_stream.write('\n')
+            self.finished = True
---
+++ packages/svtplay-dl/svtplay_dl-1.9.5-py2.py3-none-any.whl/svtplay_dl/service/dplay.py
@@ -0,0 +1,184 @@
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+from __future__ import absolute_import
+import re
+import copy
+import json
+import time
+import os
+
+from svtplay_dl.service import Service
+from svtplay_dl.fetcher.hds import hdsparse
+from svtplay_dl.fetcher.hls import hlsparse
+from svtplay_dl.subtitle import subtitle
+from svtplay_dl.utils.urllib import quote, urlparse
+from svtplay_dl.error import ServiceError
+from svtplay_dl.utils import filenamify, is_py2
+from svtplay_dl.log import log
+
+class Dplay(Service):
+    supported_domains = ['dplay.se', 'dplay.dk', "dplay.no"]
+
+    def get(self):
+        data = self.get_urldata()
+        premium = False
+        parse = urlparse(self.url)
+        domain = re.search(r"(dplay\.\w\w)", parse.netloc).group(1)
+
+        match = re.search(r"<link rel='shortlink' href='[^']+/\?p=(\d+)", data)
+        if not match:
+            match = re.search(r'data-video-id="([^"]+)"', data)
+            if not match:
+                yield ServiceError("Can't find video id")
+                return
+        vid = match.group(1)
+        data = self.http.request("get", "http://%s/api/v2/ajax/videos?video_id=%s" % (parse.netloc, vid)).text
+        dataj = json.loads(data)
+        if dataj["data"] is None:
+            yield ServiceError("Cant find video. wrong url without video?")
+            return
+        if self.options.username and self.options.password:
+            premium = self._login(self.options)
+            if not premium:
+                yield ServiceError("Wrong username or password")
+                return
+
+        what = self._playable(dataj["data"][0], premium)
+        if what == 1:
+            yield ServiceError("Premium content")
+            return
+        if what == 2:
+            yield ServiceError("DRM protected. Can't do anything")
+            return
+
+        if self.options.output_auto:
+            directory = os.path.dirname(self.options.output)
+            self.options.service = "dplay"
+            name = self._autoname(dataj)
+            if name is None:
+                yield ServiceError("Cant find vid id for autonaming")
+                return
+            title = "%s-%s-%s" % (name, vid, self.options.service)
+            if len(directory):
+                self.options.output = os.path.join(directory, title)
+            else:
+                self.options.output = title
+
+        if self.exclude():
+            yield ServiceError("Excluding video")
+            return
+
+        subt = "subtitles_%s_srt" % self._country2lang()
+        suburl = dataj["data"][0][subt]
+        if len(suburl) > 0:
+            yield subtitle(copy.copy(self.options), "raw", suburl)
+
+        data = self.http.request("get", "http://geo.%s/geo.js" % domain).text
+        dataj = json.loads(data)
+        geo = dataj["countryCode"]
+        timestamp = (int(time.time())+3600)*1000
+        cookie = {"dsc-geo": quote('{"countryCode":"%s","expiry":%s}' % (geo, timestamp))}
+        if self.options.cookies:
+            self.options.cookies.update(cookie)
+        else:
+            self.options.cookies = cookie
+        data = self.http.request("get", "https://secure.%s/secure/api/v2/user/authorization/stream/%s?stream_type=hds" % (domain, vid), cookies=self.options.cookies)
+        if data.status_code == 403 or data.status_code == 401:
+            yield ServiceError("Geoblocked video")
+            return
+        dataj = json.loads(data.text)
+        if "hds" in dataj:
+            streams = hdsparse(copy.copy(self.options), self.http.request("get", dataj["hds"], params={"hdcore": "3.8.0"}), dataj["hds"])
+            if streams:
+                for n in list(streams.keys()):
+                    yield streams[n]
+        data = self.http.request("get", "https://secure.%s/secure/api/v2/user/authorization/stream/%s?stream_type=hls" % (domain, vid), cookies=self.options.cookies)
+        dataj = json.loads(data.text)
+        if "hls" in dataj:
+            streams = hlsparse(self.options, self.http.request("get", dataj["hls"]), dataj["hls"])
+            if streams:
+                for n in list(streams.keys()):
+                    yield streams[n]
+
+    def _autoname(self, jsondata):
+        show = jsondata["data"][0]["video_metadata_show"]
+        season = jsondata["data"][0]["season"]
+        episode = jsondata["data"][0]["episode"]
+        title = jsondata["data"][0]["title"]
+        if is_py2:
+            show = filenamify(show).encode("latin1")
+            title = filenamify(title).encode("latin1")
+        else:
+            show = filenamify(show)
+            title = filenamify(title)
+        return filenamify("{0}.s{1:02d}e{2:02d}.{3}".format(show, int(season), int(episode), title))
+
+    def _login(self, options):
+        parse = urlparse(self.url)
+        domain = re.search(r"(dplay\.\w\w)", parse.netloc).group(1)
+        data = self.http.request("get", "https://secure.%s/login/" % domain, cookies={})
+        options.cookies = data.cookies
+        match = re.search('realm_code" value="([^"]+)"', data.text)
+        postdata = {"username" : options.username, "password": options.password, "remember_me": "true", "realm_code": match.group(1)}
+        data = self.http.request("post", "https://secure.%s/secure/api/v1/user/auth/login" % domain, data=postdata, cookies=options.cookies)
+        if data.status_code == 200:
+            options.cookies = data.cookies
+            return True
+        else:
+            return False
+
+    def _country2lang(self):
+        parse = urlparse(self.url)
+        domain = re.search(r"dplay\.(\w\w)", parse.netloc).group(1)
+        country = {"se": "sv", "no": "no", "dk": "da"}
+        if domain and domain in country:
+            return country[domain]
+        else:
+            return "sv"
+
+    def _playable(self, dataj, premium):
+        if dataj["content_info"]["package_label"]["value"] == "Premium" and not premium:
+            return 1
+
+        if dataj["video_metadata_drmid_playready"] != "none":
+            return 2
+
+        if dataj["video_metadata_drmid_flashaccess"] != "none":
+            return 2
+        return 0
+
+    def find_all_episodes(self, options):
+        data = self.get_urldata()
+        parse = urlparse(self.url)
+        domain = re.search(r"(dplay\.\w\w)", parse.netloc).group(1)
+        match = re.search('data-show-id="([^"]+)"', data)
+        if not match:
+            log.error("Cant find show id")
+            return None
+
+        premium = None
+        if options.username and options.password:
+            premium = self._login(options)
+
+        url = "http://www.%s/api/v2/ajax/shows/%s/seasons/?items=9999999&sort=episode_number_desc&page=" % (domain, match.group(1))
+        episodes = []
+        page = 0
+        data = self.http.request("get", "%s%s" % (url, page)).text
+        dataj = json.loads(data)
+        for i in dataj["data"]:
+            what = self._playable(i, premium)
+            if what == 0:
+                episodes.append(i["url"])
+        pages = dataj["total_pages"]
+        for n in range(1, pages):
+            data = self.http.request("get", "%s%s" % (url, n)).text
+            dataj = json.loads(data)
+            for i in dataj["data"]:
+                what = self._playable(i, premium)
+                if what == 0:
+                    episodes.append(i["url"])
+        if len(episodes) == 0:
+            log.error("Cant find any playable files")
+        if options.all_last > 0:
+            return episodes[:options.all_last]
+        return episodes
