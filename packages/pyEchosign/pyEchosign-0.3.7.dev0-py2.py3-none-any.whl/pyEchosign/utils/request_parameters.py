8b72929d42453d6f9eff31305f42ba9bf84f89d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:34 +0000

    Add package pyEchosign/0.3.7.dev0/pyEchosign-0.3.7.dev0-py2.py3-none-any.whl

---
+++ packages/pyEchosign/pyEchosign-0.3.7.dev0-py2.py3-none-any.whl/pyEchosign/utils/request_parameters.py
@@ -0,0 +1,23 @@
+def get_headers(access_token, api_user_email=None, content_type='application/json'):
+    """
+    Generates the headers for a request to the API - can specify which user the API call should be made under.
+    Args:
+        access_token: The access token of the account making the request
+        api_user_email: If the access token covers more than one user, enter the email of the user the call should be made under
+
+    Returns: A dictionary to be used as the headers argument in requests
+
+    """
+    headers = {'Access-Token': access_token}
+
+    if content_type is not None:
+        headers.update({'Content-Type': content_type})
+    if api_user_email is not None:
+        headers.update({'x-user-email': api_user_email})
+
+    return headers
+
+
+def account_headers(account, content_type='application/json'):
+    """ Creates the headers automatically from the EchosignAccount details """
+    return get_headers(account.access_token, api_user_email=account.user_email, content_type=content_type)
