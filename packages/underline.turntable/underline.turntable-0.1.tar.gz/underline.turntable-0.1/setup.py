8a26d65665f8aa24ebfd8939f37abd65bef25d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:24 +0000

    Add package underline.turntable/0.1/underline.turntable-0.1.tar.gz

---
+++ packages/underline.turntable/underline.turntable-0.1.tar.gz/underline.turntable-0.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from setuptools import setup, find_packages
+
+setup(name = "underline.turntable",
+      version = "0.1",
+      description = "Speed and direction control of turntable.",
+
+      author = "",
+      author_email = "",
+      maintainer = "Yeison Cardona",
+      maintainer_email = "yeisoneng@gmail.com",
+
+      url = "http://yeisoncardona.com",
+      download_url = "",
+
+      license = "GPLv3",
+      keywords = "Raspberry Pi 3, Stepper motor",
+
+      #list of classifiers in https://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=['Development Status :: 1 - Planning',
+                   'Environment :: X11 Applications',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+                   'Programming Language :: Python :: 3 :: Only',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Topic :: System :: Hardware',
+                   ],
+
+                   packages = find_packages(),
+      include_package_data = True,
+
+      install_requires = ['RPi.GPIO',
+                          ],
+      #scripts = [
+          #"cmd/command",
+                 #],
+
+      zip_safe = False,
+
+      )
