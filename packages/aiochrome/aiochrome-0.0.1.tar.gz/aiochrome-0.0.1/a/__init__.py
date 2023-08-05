731db744145900bb58dd094ac53cd6d5d7562963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:14 +0000

    Add package aiochrome/0.0.1/aiochrome-0.0.1.tar.gz

---
+++ packages/aiochrome/aiochrome-0.0.1.tar.gz/aiochrome-0.0.1/a/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+__email__ = 'fate0@fatezero.org'
+__author__ = 'fate0'
+__version__ = '0.0.1'
---
+++ packages/aiochrome/aiochrome-0.0.1.tar.gz/aiochrome-0.0.1/a/cli.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for a."""
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for a."""
+    click.echo("Replace this message by putting your code into "
+               "a.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+
+
+if __name__ == "__main__":
+    main()
