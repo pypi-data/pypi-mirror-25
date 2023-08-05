35f08f1359665fcb2a815d544306910b79f3f758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:15 +0000

    Add package resdk/1.10.0/resdk-1.10.0.tar.gz

---
+++ packages/resdk/resdk-1.10.0.tar.gz/resdk-1.10.0/resdk/resources/base.py
@@ -0,0 +1,194 @@
+"""Constants and abstract classes."""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import copy
+import logging
+import operator
+
+import six
+import slumber
+
+
+class BaseResource(object):
+    """Abstract resource.
+
+    One and only one of the identifiers (slug, id or model_data)
+    should be given.
+
+    :param str slug: Resource slug
+    :param int id: Resource ID
+    :param dict model_data: Resource model data
+    :param resolwe: Resolwe instance
+    :type resolwe: Resolwe object
+
+    """
+
+    endpoint = None
+    query_endpoint = None
+    query_method = 'GET'
+
+    WRITABLE_FIELDS = ('slug', 'name', 'permissions')
+    UPDATE_PROTECTED_FIELDS = ('contributor', )
+    READ_ONLY_FIELDS = ('id', 'created', 'modified')
+
+    def __init__(self, slug=None, id=None,  # pylint: disable=redefined-builtin
+                 model_data=None, resolwe=None):
+        """Verify that only a single attribute of slug, id or model_data given."""
+        if len([attr for attr in (slug, id, model_data) if attr is not None]) > 1:
+            raise ValueError("Only one of slug, id or model_data allowed")
+
+        if id:
+            if not isinstance(id, int):
+                raise ValueError("id should be an integer.")
+        elif slug:
+            if not isinstance(slug, six.string_types):
+                raise ValueError("slug should be a string.")
+        elif model_data:
+            if not isinstance(model_data, dict):
+                raise ValueError("model_data should be a dict.")
+
+        self._original_values = {}
+
+        def initialize_field(field_name, value):
+            """Initialize field and ignore those initialized in subclass."""
+            if not hasattr(self, field_name):  # check if field was initialized in subclass
+                setattr(self, field_name, value)
+
+        for field_name in self.fields():
+            initialize_field(field_name, None)
+
+        #: a descriptive name of the resource
+        self.name = None
+        #: unique identifier
+        self.id = id  # pylint: disable=invalid-name
+        #: human-readable unique identifier
+        self.slug = slug
+        #: user id of the contributor
+        self.contributor = None
+        #: date of creation
+        self.created = None
+        #: date of latest modification
+        self.modified = None
+        #: permissions - (view/download/add/edit/share/owner for user/group/public)
+        self.permissions = None
+
+        self.api = operator.attrgetter(self.endpoint)(resolwe.api)
+        self.resolwe = resolwe
+
+        if id:
+            try:
+                self._update_fields(self.api(id).get())
+            except slumber.exceptions.HttpNotFoundError:
+                raise ValueError("ID '{}' does not exist or you do not "
+                                 "have access permission.".format(id))
+        elif slug:
+            resources = self.api.get(slug=slug)  # pylint: disable=no-member
+
+            if len(resources) > 1:
+                # Return the latest version
+                resources.sort(key=lambda resource: resource['version'], reverse=True)
+            elif len(resources) < 1:
+                raise ValueError("Slug '{}' does not exist or you do not "
+                                 "have access permission.".format(slug))
+
+            self._update_fields(resources[0])
+        elif model_data:
+            self._update_fields(model_data)
+
+        self.logger = logging.getLogger(__name__)
+
+    def fields(self):
+        """Resource fields."""
+        return self.WRITABLE_FIELDS + self.UPDATE_PROTECTED_FIELDS + self.READ_ONLY_FIELDS
+
+    def _update_fields(self, payload):
+        """Update fields of the local resource based on the server values.
+
+        :param dict payload: Resource field values
+
+        """
+        self._original_values = copy.deepcopy(payload)
+        for field_name in self.fields():
+            setattr(self, field_name, payload.get(field_name, None))
+
+    def update(self):
+        """Update resource fields from the server."""
+        response = self.api(self.id).get()
+        self._update_fields(response)
+
+    def _dehydrate_resources(self, obj):
+        """Iterate through object and replace all objects with their ids."""
+        if isinstance(obj, BaseResource):
+            return obj.id
+        if isinstance(obj, list):
+            return [self._dehydrate_resources(element) for element in obj]
+        if isinstance(obj, dict):
+            return {key: self._dehydrate_resources(value) for key, value in six.iteritems(obj)}
+        return obj
+
+    def save(self):
+        """Save resource to the server."""
+        def field_changed(field_name):
+            """Check if local field value is different from the server."""
+            return getattr(self, field_name) != self._original_values.get(field_name)
+
+        def assert_fields_changed(field_names):
+            """Check if local field value is different from the server."""
+            changed_fields = [field_name for field_name in field_names if
+                              field_changed(field_name)]
+
+            if changed_fields:
+                msg = "Not allowed to change read only fields {}".format(', '.join(changed_fields))
+                raise ValueError(msg)
+
+        if self.id:  # update resource
+            assert_fields_changed(self.READ_ONLY_FIELDS + self.UPDATE_PROTECTED_FIELDS)
+
+            payload = {}
+            for field_name in self.WRITABLE_FIELDS:
+                if field_changed(field_name):
+                    payload[field_name] = self._dehydrate_resources(getattr(self, field_name))
+
+            if payload:
+                response = self.api(self.id).patch(payload)
+                self._update_fields(response)
+
+        else:  # create resource
+            assert_fields_changed(self.READ_ONLY_FIELDS)
+
+            field_names = self.WRITABLE_FIELDS + self.UPDATE_PROTECTED_FIELDS
+            payload = {field_name: self._dehydrate_resources(getattr(self, field_name))
+                       for field_name in field_names if getattr(self, field_name) is not None}
+
+            response = self.api.post(payload)
+            self._update_fields(response)
+
+    def delete(self, force=False):
+        """Delete the resource object from the server."""
+        if force is not True:
+            user_input = six.moves.input('Do you really want to delete {}?[yN] '.format(self))
+
+            if user_input.strip().lower() != 'y':
+                return
+
+        self.api(self.id).delete()
+
+    def __setattr__(self, name, value):
+        """Detect changes of read only fields.
+
+        This method detects changes of scalar fields and references. A
+        more comprehensive check is called before save.
+
+        """
+        if (hasattr(self, '_original_values') and
+                name in self._original_values and
+                name in self.READ_ONLY_FIELDS and
+                value != self._original_values[name]):
+            raise ValueError("Can not change read only field {}".format(name))
+
+        super(BaseResource, self).__setattr__(name, value)
+
+    def __repr__(self):
+        """Format resource name."""
+        return "{} <id: {}, slug: '{}', name: '{}'>".format(self.__class__.__name__,
+                                                            self.id, self.slug, self.name)
---
+++ packages/resdk/resdk-1.10.0.tar.gz/resdk-1.10.0/resdk/tests/unit/test_base.py
@@ -0,0 +1,250 @@
+"""
+Unit tests for resdk/resolwe.py file.
+"""
+# pylint: disable=missing-docstring, protected-access
+
+import unittest
+
+import six
+import slumber
+from mock import MagicMock, call, patch
+
+from resdk.resources import Collection
+from resdk.resources.base import BaseResource
+
+
+class TestBaseResource(unittest.TestCase):
+
+    @patch('resdk.resolwe.Resolwe')
+    def setUp(self, resolwe_mock):  # pylint: disable=arguments-differ
+        self.resolwe_mock = resolwe_mock
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_not_only_one_input(self, base_mock):
+        message = "Only one of slug, id or model_data allowed"
+        with six.assertRaisesRegex(self, ValueError, message):
+            BaseResource.__init__(base_mock, id=1, slug="a", resolwe=self.resolwe_mock)
+        with six.assertRaisesRegex(self, ValueError, message):
+            BaseResource.__init__(base_mock, id=1, model_data={'a': 1}, resolwe=self.resolwe_mock)
+        with six.assertRaisesRegex(self, ValueError, message):
+            BaseResource.__init__(base_mock, slug="a", model_data={'a': 1},
+                                  resolwe=self.resolwe_mock)
+        with six.assertRaisesRegex(self, ValueError, message):
+            BaseResource.__init__(base_mock, id=1, slug="a", model_data={'a': 1},
+                                  resolwe=self.resolwe_mock)
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_wrong_type_input(self, base_mock):
+        message = r"\w+ should be a\w? \w+."
+        with six.assertRaisesRegex(self, ValueError, message):
+            BaseResource.__init__(base_mock, id="bad_type_id", resolwe=self.resolwe_mock)
+        with six.assertRaisesRegex(self, ValueError, message):
+            BaseResource.__init__(base_mock, slug={'a': 1}, resolwe=self.resolwe_mock)
+        with six.assertRaisesRegex(self, ValueError, message):
+            BaseResource.__init__(base_mock, model_data="a", resolwe=self.resolwe_mock)
+
+    @patch('operator.attrgetter')
+    def test_field_constraints(self, attrgetter_mock):
+        base_resource = BaseResource(resolwe=self.resolwe_mock)
+
+        base_resource.WRITABLE_FIELDS = ('writable_scalar', )
+        base_resource.READ_ONLY_FIELDS = ('read_only_scalar', )
+        base_resource._original_values = {'writable_scalar': None, 'read_only_scalar': None}
+        base_resource.writable_scalar = None
+        base_resource.read_only_scalar = None
+
+        message = 'Can not change read only field read_only_scalar'
+        with six.assertRaisesRegex(self, ValueError, message):
+            base_resource.read_only_scalar = 42
+
+        base_resource.writable_scalar = '42'
+        self.assertEqual(base_resource.writable_scalar, '42')
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_id_http_error(self, base_mock):
+        base_mock.configure_mock(endpoint='resource_endpoint')
+        base_mock._update_fields.side_effect = slumber.exceptions.HttpNotFoundError
+        message = r"ID '\d+' does not exist or you do not have access permission."
+        with six.assertRaisesRegex(self, ValueError, message):
+            BaseResource.__init__(base_mock, id=1, resolwe=self.resolwe_mock)
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_id_ok(self, base_mock):
+        base_mock.configure_mock(endpoint='resource_endpoint')
+        BaseResource.__init__(base_mock, id=1, resolwe=self.resolwe_mock)
+        self.assertEqual(base_mock._update_fields.call_count, 1)
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_slug_value_error(self, base_mock):
+        base_mock.configure_mock(endpoint='resource_endpoint')
+        message = r"Slug '\w+' does not exist or you do not have access permission."
+        with six.assertRaisesRegex(self, ValueError, message):
+            BaseResource.__init__(base_mock, slug="a", resolwe=self.resolwe_mock)
+        base_mock._update_fields.assert_not_called()
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_slug_ok(self, base_mock):
+        base_mock.configure_mock(endpoint='resource_endpoint')
+        self.resolwe_mock.configure_mock(**{'api.resource_endpoint.get.return_value': [123]})
+        BaseResource.__init__(base_mock, slug="a", resolwe=self.resolwe_mock)
+        base_mock._update_fields.assert_called_once_with(123)
+
+        self.resolwe_mock.configure_mock(
+            **{'api.resource_endpoint.get.return_value': [{'version': 1}, {'version': 2}]}
+        )
+
+        BaseResource.__init__(base_mock, slug="a", resolwe=self.resolwe_mock)
+        self.assertEqual(base_mock._update_fields.call_count, 2)
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_model_data(self, base_mock):
+        base_mock.configure_mock(endpoint='resource_endpoint')
+        mdata = {'a': 1, 'b': 2}
+        BaseResource.__init__(base_mock, model_data=mdata, resolwe=self.resolwe_mock)
+        base_mock._update_fields.assert_called_once_with(mdata)
+
+    @patch('resdk.resources.base.logging')
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_init_all_ok(self, base_mock, log_mock):
+        base_mock.configure_mock(endpoint='resource_endpoint')
+        BaseResource.__init__(base_mock, id=1, resolwe=self.resolwe_mock)
+        self.assertEqual(log_mock.getLogger.call_count, 1)
+
+    @patch('operator.attrgetter')
+    def test_fields(self, attrgetter_mock):
+        base_resource = BaseResource(resolwe=self.resolwe_mock)
+        base_resource.WRITABLE_FIELDS = ('writable', )
+        base_resource.UPDATE_PROTECTED_FIELDS = ('update_protected', )
+        base_resource.READ_ONLY_FIELDS = ('read_only', )
+        self.assertEqual(base_resource.fields(), ('writable', 'update_protected', 'read_only'))
+
+    def test_dehydrate_resources(self):
+        # `Collection` is used because it is easier to use
+        collection = Collection(id=100, resolwe=MagicMock())
+        obj_1 = Collection(id=1, resolwe=MagicMock())
+        obj_1.id = 1  # this is overriden when initialized
+
+        self.assertEqual(collection._dehydrate_resources(obj_1), 1)
+        self.assertEqual(collection._dehydrate_resources([obj_1]), [1])
+        self.assertEqual(collection._dehydrate_resources({'key': obj_1}), {'key': 1})
+        self.assertEqual(collection._dehydrate_resources({'key': [obj_1]}), {'key': [1]})
+
+
+class TestBaseMethods(unittest.TestCase):
+
+    @patch('resdk.resources.base.setattr')
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_update_fields(self, base_mock, setattr_mock):
+        fields = {'id': 1, 'slug': 'testobj'}
+        base_mock.fields.return_value = ('id', 'slug')
+        BaseResource._update_fields(base_mock, fields)
+        setattr_mock.assert_has_calls([call(base_mock, 'id', 1),
+                                       call(base_mock, 'slug', 'testobj')],
+                                      any_order=True)
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_update(self, base_mock):
+        base_mock.configure_mock(api=MagicMock(), id=1)
+        BaseResource.update(base_mock)
+        self.assertEqual(base_mock._update_fields.call_count, 1)
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_save_post(self, base_mock):
+        base_mock.configure_mock(id=None, slug='test')
+        base_mock.WRITABLE_FIELDS = ('slug', )
+        base_mock.api = MagicMock(**{'post.return_value': {'id': 1, 'slug': 'test'}})
+        BaseResource.save(base_mock)
+        self.assertEqual(base_mock._update_fields.call_count, 1)
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_save_post_read_only(self, base_mock):
+        base_mock.configure_mock(id=None, slug='test', read_only_dict=None, _original_values={})
+        base_mock.READ_ONLY_FIELDS = ('id', 'read_only_dict')
+        base_mock.UPDATE_PROTECTED_FIELDS = ()
+        base_mock.WRITABLE_FIELDS = ('slug', )
+        base_mock.fields.return_value = ('id', 'slug', 'read_only_dict')
+
+        base_mock.read_only_dict = {'change': 'change-not-allowed'}
+
+        message = 'Not allowed to change read only fields read_only_dict'
+        with six.assertRaisesRegex(self, ValueError, message):
+            BaseResource.save(base_mock)
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_save_post_update_protected(self, base_mock):
+        base_mock.configure_mock(id=None, slug='test', update_protected_dict=None,
+                                 _original_values={})
+        base_mock.READ_ONLY_FIELDS = ('id', )
+        base_mock.UPDATE_PROTECTED_FIELDS = ('update_protected_dict', )
+        base_mock.WRITABLE_FIELDS = ('slug', )
+        base_mock.fields.return_value = ('id', 'slug', 'update_protected_dict')
+
+        base_mock.update_protected_dict = {'create': 'create-allowed'}
+        base_mock.api = MagicMock(**{'post.return_value': {
+            'id': 1, 'slug': 'test', 'update_protected_dict': {'create': 'create-allowed'}}})
+
+        BaseResource.save(base_mock)
+        self.assertEqual(base_mock.update_protected_dict, {'create': 'create-allowed'})
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_save_post_client_error(self, base_mock):
+        base_mock.configure_mock(id=None, slug='test')
+        base_mock.api = MagicMock(**{'post.side_effect': slumber.exceptions.HttpClientError(
+            message='', content='', response='')})
+
+        with self.assertRaises(slumber.exceptions.HttpClientError):
+            BaseResource.save(base_mock)
+        self.assertEqual(base_mock._update_fields.call_count, 0)
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_save_patch(self, base_mock):
+        base_mock.configure_mock(id=1, slug='test', _original_values={'slug': 'test-old'})
+        base_mock.WRITABLE_FIELDS = ('slug', )
+
+        base_mock.api = MagicMock(**{'patch.return_value': {'id': 1, 'slug': 'test'}})
+        BaseResource.save(base_mock)
+        self.assertEqual(base_mock._update_fields.call_count, 1)
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_save_patch_read_only(self, base_mock):
+        base_mock.READ_ONLY_FIELDS = ('id', 'read_only_dict')
+        base_mock.UPDATE_PROTECTED_FIELDS = ()
+        base_mock.WRITABLE_FIELDS = ('slug', )
+        base_mock.fields.return_value = ('id', 'slug', 'read_only_dict')
+        read_only_dict = {}
+        BaseResource._update_fields(base_mock, {'id': 1,
+                                                'slug': 'test',
+                                                'read_only_dict': read_only_dict})
+        base_mock.read_only_dict['change'] = 'change-not-allowed'
+
+        message = 'Not allowed to change read only fields read_only_dict'
+        with six.assertRaisesRegex(self, ValueError, message):
+            BaseResource.save(base_mock)
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_save_patch_update_protect(self, base_mock):
+        base_mock.READ_ONLY_FIELDS = ('id', )
+        base_mock.UPDATE_PROTECTED_FIELDS = ('update_protected_dict', )
+        base_mock.WRITABLE_FIELDS = ('slug', )
+        base_mock.fields.return_value = ('id', 'slug', 'update_protected_dict')
+        update_protected_dict = {}
+        BaseResource._update_fields(base_mock, {'id': 1,
+                                                'slug': 'test',
+                                                'update_protected_dict': update_protected_dict})
+        base_mock.update_protected_dict['change'] = 'change-not-allowed'
+
+        message = 'Not allowed to change read only fields update_protected_dict'
+        with six.assertRaisesRegex(self, ValueError, message):
+            BaseResource.save(base_mock)
+
+        base_mock.UPDATE_PROTECTED_FIELDS = ('id', 'update_protected_dict')
+
+    @patch('resdk.resources.base.BaseResource', spec=True)
+    def test_repr(self, base_mock):
+        base_mock.configure_mock(id=1, slug='a', name='b')
+        out = BaseResource.__repr__(base_mock)
+        self.assertEqual(out, 'BaseResource <id: 1, slug: \'a\', name: \'b\'>')
+
+
+if __name__ == '__main__':
+    unittest.main()
