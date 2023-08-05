d9e42edab29e0243b52efde1e08274c3c112af0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:10 +0000

    Add package bdgenomics.workflows/0.1.0/bdgenomics.workflows-0.1.0-py2.7.egg

---
+++ packages/bdgenomics.workflows/bdgenomics.workflows-0.1.0-py2.7.egg/bdgenomics/workflows/tools/aligners.py
@@ -0,0 +1,188 @@
+import os
+import subprocess
+
+from toil.lib.docker import dockerCall
+
+from toil_lib.urls import download_url
+
+
+def run_star(job, r1_id, r2_id, star_index_url, wiggle=False, sort=True):
+    """
+    Performs alignment of fastqs to bam via STAR
+
+    --limitBAMsortRAM step added to deal with memory explosion when sorting certain samples.
+    The value was chosen to complement the recommended amount of memory to have when running STAR (60G)
+
+    :param JobFunctionWrappingJob job: passed automatically by Toil
+    :param str r1_id: FileStoreID of fastq (pair 1)
+    :param str r2_id: FileStoreID of fastq (pair 2 if applicable, else pass None)
+    :param str star_index_url: STAR index tarball
+    :param bool wiggle: If True, will output a wiggle file and return it
+    :return: FileStoreID from RSEM
+    :rtype: str
+    """
+    work_dir = job.fileStore.getLocalTempDir()
+    download_url(job, url=star_index_url, name='starIndex.tar.gz', work_dir=work_dir)
+    subprocess.check_call(['tar', '-xvf', os.path.join(work_dir, 'starIndex.tar.gz'), '-C', work_dir])
+    os.remove(os.path.join(work_dir, 'starIndex.tar.gz'))
+    # Determine tarball structure - star index contains are either in a subdir or in the tarball itself
+    star_index = os.path.join('/data', os.listdir(work_dir)[0]) if len(os.listdir(work_dir)) == 1 else '/data'
+    # Parameter handling for paired / single-end data
+    parameters = ['--runThreadN', str(job.cores),
+                  '--genomeDir', star_index,
+                  '--outFileNamePrefix', 'rna',
+                  '--outSAMunmapped', 'Within',
+                  '--quantMode', 'TranscriptomeSAM',
+                  '--outSAMattributes', 'NH', 'HI', 'AS', 'NM', 'MD',
+                  '--outFilterType', 'BySJout',
+                  '--outFilterMultimapNmax', '20',
+                  '--outFilterMismatchNmax', '999',
+                  '--outFilterMismatchNoverReadLmax', '0.04',
+                  '--alignIntronMin', '20',
+                  '--alignIntronMax', '1000000',
+                  '--alignMatesGapMax', '1000000',
+                  '--alignSJoverhangMin', '8',
+                  '--alignSJDBoverhangMin', '1',
+                  '--sjdbScore', '1',
+                  '--limitBAMsortRAM', '49268954168']
+    # Modify paramaters based on function arguments
+    if sort:
+        parameters.extend(['--outSAMtype', 'BAM', 'SortedByCoordinate'])
+        aligned_bam = 'rnaAligned.sortedByCoord.out.bam'
+    else:
+        parameters.extend(['--outSAMtype', 'BAM', 'Unsorted'])
+        aligned_bam = 'rnaAligned.out.bam'
+    if wiggle:
+        parameters.extend(['--outWigType', 'bedGraph',
+                           '--outWigStrand', 'Unstranded',
+                           '--outWigReferencesPrefix', 'chr'])
+    if r1_id and r2_id:
+        job.fileStore.readGlobalFile(r1_id, os.path.join(work_dir, 'R1.fastq'))
+        job.fileStore.readGlobalFile(r2_id, os.path.join(work_dir, 'R2.fastq'))
+        parameters.extend(['--readFilesIn', '/data/R1.fastq', '/data/R2.fastq'])
+    else:
+        job.fileStore.readGlobalFile(r1_id, os.path.join(work_dir, 'R1.fastq'))
+        parameters.extend(['--readFilesIn', '/data/R1.fastq'])
+    # Call: STAR Mapping
+    dockerCall(job=job, tool='quay.io/ucsc_cgl/star:2.4.2a--bcbd5122b69ff6ac4ef61958e47bde94001cfe80',
+               workDir=work_dir, parameters=parameters)
+    # Check output bam isnt size zero if sorted
+    aligned_bam_path = os.path.join(work_dir, aligned_bam)
+    if sort:
+        assert(os.stat(aligned_bam_path).st_size > 0, 'Aligned bam failed to sort. Ensure sufficient memory is free.')
+    # Write to fileStore
+    aligned_id = job.fileStore.writeGlobalFile(aligned_bam_path)
+    transcriptome_id = job.fileStore.writeGlobalFile(os.path.join(work_dir, 'rnaAligned.toTranscriptome.out.bam'))
+    log_id = job.fileStore.writeGlobalFile(os.path.join(work_dir, 'rnaLog.final.out'))
+    sj_id = job.fileStore.writeGlobalFile(os.path.join(work_dir, 'rnaSJ.out.tab'))
+    if wiggle:
+        wiggle_id = job.fileStore.writeGlobalFile(os.path.join(work_dir, 'rnaSignal.UniqueMultiple.str1.out.bg'))
+        return transcriptome_id, aligned_id, wiggle_id, log_id, sj_id
+    else:
+        return transcriptome_id, aligned_id, log_id, sj_id
+
+
+def run_bwakit(job, config, sort=True, trim=False, mark_secondary=False):
+    """
+    Runs BWA-Kit to align single or paired-end fastq files or realign SAM/BAM files.
+
+    :param JobFunctionWrappingJob job: Passed by Toil automatically
+    :param Namespace config: A configuration object that holds strings as attributes.
+        The attributes must be accessible via the dot operator.
+        The config must have:
+        config.r1               FileStoreID for FASTQ file, or None if realigning SAM/BAM
+        config.r2               FileStoreID for paired FASTQ file, or None if single-ended
+        config.bam              FileStoreID for BAM file to be realigned, or None if aligning fastq
+        config.sam              FileStoreID for SAM file to be realigned, or None if aligning fastq
+        config.ref              FileStoreID for the reference genome
+        config.fai              FileStoreID for the reference index file
+        config.amb              FileStoreID for the reference amb file
+        config.ann              FileStoreID for the reference ann file
+        config.bwt              FileStoreID for the reference bwt file
+        config.pac              FileStoreID for the reference pac file
+        config.sa               FileStoreID for the reference sa file
+        config.alt              FileStoreID for the reference alt (or None)
+        config.rg_line          The read group value to use (or None -- see below)
+        config.library          Read group attribute: library
+        config.platform         Read group attribute: platform
+        config.program_unit     Read group attribute: program unit
+        config.uuid             Read group attribute: sample ID
+
+        If specifying config.rg_line, use the following format:
+            BAM read group header line (@RG), as defined on page 3 of the SAM spec.
+            Tabs should be escaped, e.g., @RG\\tID:foo\\tLB:bar...
+            for the read group "foo" from sequencing library "bar".
+            Multiple @RG lines can be defined, but should be split by an escaped newline \\n,
+            e.g., @RG\\tID:foo\\t:LB:bar\\n@RG\\tID:santa\\tLB:cruz.
+
+    :param bool sort: If True, sorts the BAM
+    :param bool trim: If True, performs adapter trimming
+    :param bool mark_secondary: If True, mark shorter split reads as secondary
+    :return: FileStoreID of BAM
+    :rtype: str
+    """
+    work_dir = job.fileStore.getLocalTempDir()
+    rg = None
+    inputs = {'ref.fa': config.ref,
+              'ref.fa.fai': config.fai,
+              'ref.fa.amb': config.amb,
+              'ref.fa.ann': config.ann,
+              'ref.fa.bwt': config.bwt,
+              'ref.fa.pac': config.pac,
+              'ref.fa.sa': config.sa}
+    samples = []
+    realignment = False
+    # If a fastq pair was provided
+    if getattr(config, 'r1', None):
+        inputs['input.1.fq.gz'] = config.r1
+        samples.append('input.1.fq.gz')
+    if getattr(config, 'r2', None):
+        inputs['input.2.fq.gz'] = config.r2
+        samples.append('input.2.fq.gz')
+    if getattr(config, 'bam', None):
+        inputs['input.bam'] = config.bam
+        samples.append('input.bam')
+        realignment = True
+    if getattr(config, 'sam', None):
+        inputs['input.sam'] = config.sam
+        samples.append('input.sam')
+        realignment = True
+    # If an alt file was provided
+    if getattr(config, 'alt', None):
+        inputs['ref.fa.alt'] = config.alt
+    for name, fileStoreID in inputs.iteritems():
+        job.fileStore.readGlobalFile(fileStoreID, os.path.join(work_dir, name))
+    # If a read group line was provided
+    if getattr(config, 'rg_line', None):
+        rg = config.rg_line
+    # Otherwise, generate a read group line to place in the BAM.
+    elif all(getattr(config, elem, None) for elem in ['library', 'platform', 'program_unit', 'uuid']):
+        rg = "@RG\\tID:{0}".format(config.uuid)  # '\' character is escaped so bwakit gets passed '\t' properly
+        rg_attributes = [config.library, config.platform, config.program_unit, config.uuid]
+        for tag, info in zip(['LB', 'PL', 'PU', 'SM'], rg_attributes):
+            rg += '\\t{0}:{1}'.format(tag, info)
+    # If realigning, then bwakit can use pre-existing read group data
+    elif realignment:
+        rg = None
+
+    # BWA Options
+    opt_args = []
+    if sort:
+        opt_args.append('-s')
+    if trim:
+        opt_args.append('-a')
+    if mark_secondary:
+        opt_args.append('-M')
+    # Call: bwakit
+    parameters = ['-t', str(job.cores)] + opt_args + ['-o', '/data/aligned', '/data/ref.fa']
+    if rg is not None:
+        parameters = ['-R', rg] + parameters
+    for sample in samples:
+        parameters.append('/data/{}'.format(sample))
+
+    dockerCall(job=job, tool='quay.io/ucsc_cgl/bwakit:0.7.12--c85ccff267d5021b75bb1c9ccf5f4b79f91835cc',
+               parameters=parameters, workDir=work_dir)
+
+    # Either write file to local output directory or upload to S3 cloud storage
+    job.fileStore.logToMaster('Aligned sample: {}'.format(config.uuid))
+    return job.fileStore.writeGlobalFile(os.path.join(work_dir, 'aligned.aln.bam'))
---
+++ packages/bdgenomics.workflows/bdgenomics.workflows-0.1.0-py2.7.egg/bdgenomics/workflows/tools/spark_tools.py
@@ -0,0 +1,275 @@
+"""
+Functions for calling raw tools in the UCSC Computational Genomics Lab
+ADAM/Spark pipeline
+
+@author Audrey Musselman-Brown, almussel@ucsc.edu
+@author Frank Austin Nothaft, fnothaft@berkeley.
+"""
+
+import os.path
+from subprocess import check_call, check_output
+
+from toil.lib.docker import dockerCall
+
+from toil_lib import require
+
+SPARK_MASTER_PORT = "7077"
+HDFS_MASTER_PORT = "8020"
+
+
+class MasterAddress(str):
+    """
+    A string containing the hostname or IP of the Spark/HDFS master. The Spark master expects its own address to
+    match what the client uses to connect to it. For example, if the master is configured with a host name,
+    the driver can't use an IP address to connect to it, and vice versa. This class works around by distinguishing
+    between the notional master address (self) and the actual one (self.actual) and adds support for the special
+    master address "auto" in order to implement auto-discovery of the master of a standalone.
+
+    >>> foo = MasterAddress('foo')
+    >>> foo == 'foo'
+    True
+    >>> foo.actual == 'foo'
+    True
+    >>> foo.actual == foo
+    True
+    """
+    def __init__(self, master_ip):
+        super(MasterAddress, self).__init__(master_ip)
+        self.actual = self
+
+    def docker_parameters(self, docker_parameters=None):
+        """
+        Augment a list of "docker run" arguments with those needed to map the  notional Spark master address to the
+        real one, if they are different.
+        """
+        if self != self.actual:
+            add_host_option = '--add-host=spark-master:' + self.actual
+            if docker_parameters is None:
+                docker_parameters = [add_host_option]
+            else:
+                docker_parameters.append(add_host_option)
+        return docker_parameters
+
+def _make_parameters(master_ip, default_parameters, memory, arguments, override_parameters):
+    """
+    Makes a Spark Submit style job submission line.
+
+    :param masterIP: The Spark leader IP address.
+    :param default_parameters: Application specific Spark configuration parameters.
+    :param memory: The memory to allocate to each Spark driver and executor.
+    :param arguments: Arguments to pass to the submitted job.
+    :param override_parameters: Parameters passed by the user, that override our defaults.
+    
+    :type masterIP: MasterAddress
+    :type default_parameters: list of string
+    :type arguments: list of string
+    :type memory: int or None
+    :type override_parameters: list of string or None
+    """
+
+    # python doesn't support logical xor?
+    # anywho, exactly one of memory or override_parameters must be defined
+    require((override_parameters is not None or memory is not None) and
+            (override_parameters is None or memory is None),
+            "Either the memory setting must be defined or you must provide Spark configuration parameters.")
+    
+    # if the user hasn't provided overrides, set our defaults
+    parameters = []
+    if memory is not None:
+        parameters = ["--conf", "spark.driver.memory=%sg" % memory,
+                      "--conf", "spark.executor.memory=%sg" % memory]
+    else:
+        parameters.extend(override_parameters)
+
+    if master_ip:
+        parameters.extend(["--master", "spark://%s:%s" % (master_ip, SPARK_MASTER_PORT),
+                           "--conf", ("spark.hadoop.fs.default.name=hdfs://%s:%s" % (master_ip, HDFS_MASTER_PORT))])
+
+    # add the tool specific spark parameters
+    parameters.extend(default_parameters)
+
+    # spark submit expects a '--' to split the spark conf arguments from tool arguments
+    parameters.append('--')
+
+    # now add the tool arguments and return
+    parameters.extend(arguments)
+
+    return parameters        
+    
+
+def call_conductor(job, master_ip, src, dst, memory=None, override_parameters=None):
+    """
+    Invokes the Conductor container to copy files between S3 and HDFS and vice versa.
+    Find Conductor at https://github.com/BD2KGenomics/conductor.
+
+    :param toil.Job.job job: The Toil Job calling this function
+    :param masterIP: The Spark leader IP address.
+    :param src: URL of file to copy.
+    :param src: URL of location to copy file to.
+    :param memory: Gigabytes of memory to provision for Spark driver/worker.
+    :param override_parameters: Parameters passed by the user, that override our defaults.
+
+    :type masterIP: MasterAddress
+    :type src: string
+    :type dst: string
+    :type memory: int or None
+    :type override_parameters: list of string or None
+    """
+
+    arguments = ["-C", src, dst]
+
+    docker_parameters = ['--log-driver', 'none', master_ip.docker_parameters(["--net=host"])]
+    dockerCall(job=job,
+                tool="quay.io/ucsc_cgl/conductor",
+                parameters=_make_parameters(master_ip,
+                                            [], # no conductor specific spark configuration
+                                            memory,
+                                            arguments,
+                                            override_parameters),
+               dockerParameters=docker_parameters)
+
+
+def call_adam(job, master_ip, arguments,
+              memory=None,
+              override_parameters=None,
+              run_local=False,
+              native_adam_path=None):
+    """
+    Invokes the ADAM container. Find ADAM at https://github.com/bigdatagenomics/adam.
+
+    :param toil.Job.job job: The Toil Job calling this function
+    :param masterIP: The Spark leader IP address.
+    :param arguments: Arguments to pass to ADAM.
+    :param memory: Gigabytes of memory to provision for Spark driver/worker.
+    :param override_parameters: Parameters passed by the user, that override our defaults.
+    :param native_adam_path: Path to ADAM executable. If not provided, Docker is used.
+    :param run_local: If true, runs Spark with the --master local[*] setting, which uses
+      all cores on the local machine. The master_ip will be disregarded.
+
+    :type masterIP: MasterAddress
+    :type arguments: list of string
+    :type memory: int or None
+    :type override_parameters: list of string or None
+    :type native_adam_path: string or None
+    :type run_local: boolean
+    """
+    if run_local:
+        master = ["--master", "local[*]"]
+    else:
+        master = ["--master",
+                  ("spark://%s:%s" % (master_ip, SPARK_MASTER_PORT)),
+                  "--conf", ("spark.hadoop.fs.default.name=hdfs://%s:%s" % (master_ip, HDFS_MASTER_PORT)),]
+
+    default_params = (master + [
+            # set max result size to unlimited, see #177
+            "--conf", "spark.driver.maxResultSize=0",
+            # these memory tuning parameters were derived in the course of running the
+            # experiments for the ADAM sigmod paper:
+            #
+            # Nothaft, Frank Austin, et al. "Rethinking data-intensive science using scalable
+            # analytics systems." Proceedings of the 2015 ACM SIGMOD International Conference
+            # on Management of Data. ACM, 2015.
+            #
+            # the memory tunings reduce the amount of memory dedicated to caching, which we don't
+            # take advantage of, and the network timeout flag reduces the number of job failures
+            # caused by heavy gc load
+            "--conf", "spark.storage.memoryFraction=0.3",
+            "--conf", "spark.storage.unrollFraction=0.1",
+            "--conf", "spark.network.timeout=300s"])
+
+    # are we running adam via docker, or do we have a native path?
+    if native_adam_path is None:
+        docker_parameters = ['--log-driver', 'none', master_ip.docker_parameters(["--net=host"])]
+        dockerCall(job=job,
+                    tool="quay.io/ucsc_cgl/adam:962-ehf--6e7085f8cac4b9a927dc9fb06b48007957256b80",
+                    dockerParameters=docker_parameters,
+                    parameters=_make_parameters(master_ip,
+                                                default_params,
+                                                memory,
+                                                arguments,
+                                                override_parameters))
+    else:
+        check_call([os.path.join(native_adam_path, "bin/adam-submit")] +
+                   default_params +
+                   arguments)
+
+def call_deca(job, master_ip, arguments,
+              memory=None,
+              override_parameters=None,
+              work_dir=None,
+              run_local=False,
+              aws_access_key_id=None,
+              aws_secret_access_key=None):
+    """
+    Invokes the DECA container. Find DECA at https://github.com/bigdatagenomics/deca.
+
+    :param toil.Job.job job: The Toil Job calling this function
+    :param masterIP: The Spark leader IP address.
+    :param arguments: Arguments to pass to ADAM.
+    :param memory: Gigabytes of memory to provision for Spark driver/worker.
+    :param override_parameters: Parameters passed by the user, that override our defaults.
+    :param run_local: If true, runs Spark with the --master local[*] setting, which uses
+      all cores on the local machine. The master_ip will be disregarded.
+
+    :type masterIP: MasterAddress
+    :type arguments: list of string
+    :type memory: int or None
+    :type override_parameters: list of string or None
+    :type native_adam_path: string or None
+    :type run_local: boolean
+    """
+    if run_local:
+        master = ["--master", "local[*]"]
+    else:
+        '''
+        hostname = check_output(["hostname", "-i"])[:-1]
+        master = ["--conf", "spark.driver.host=%s" % hostname]
+        '''
+        master = []
+        pass
+
+    default_params = (master + [
+        # set max result size to unlimited, see #177
+        "--conf", "spark.driver.maxResultSize=0",
+        "--conf", "spark.hadoop.hadoopbam.bam.enable-bai-splitter=true",
+        "--packages", "com.amazonaws:aws-java-sdk-pom:1.10.34,org.apache.hadoop:hadoop-aws:2.7.4",
+        "--conf", "spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem"])
+
+    docker_parameters = ['--log-driver', 'none',
+                         '--net=host']
+
+    if aws_access_key_id:
+
+        require(aws_secret_access_key,
+                'If AWS access key is passed, secret key must be defined')
+        
+        docker_parameters.extend(['-e', 'AWS_ACCESS_KEY_ID=%s' % aws_access_key_id,
+                                  '-e', 'AWS_SECRET_ACCESS_KEY=%s' % aws_secret_access_key])
+
+        default_params.extend(
+            ["--packages", "com.amazonaws:aws-java-sdk-pom:1.10.34,org.apache.hadoop:hadoop-aws:2.7.4",
+             "--conf", "spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem"])
+
+        for scheme in ['s3', 's3n']:
+            default_params.extend([
+                "--conf", "spark.hadoop.fs.%s.awsAccessKeyId=%s" % (scheme, aws_access_key_id),
+                "--conf", "spark.hadoop.fs.%s.awsSecretAccessKey=%s" % (scheme, aws_secret_access_key)])
+
+        default_params.extend([
+            "--conf", "spark.hadoop.fs.s3a.access.key=%s" % aws_access_key_id,
+            "--conf", "spark.hadoop.fs.s3a.secret.key=%s" % aws_secret_access_key,
+            "--conf", "spark.executorEnv.AWS_ACCESS_KEY_ID=%s" % aws_access_key_id,
+            "--conf", "spark.executorEnv.AWS_SECRET_ACCESS_KEY=%s" % aws_secret_access_key])
+
+    if work_dir:
+        docker_parameters.extend(['-v', '%s:/data' % work_dir])
+    
+    dockerCall(job=job,
+               tool="quay.io/ucsc_cgl/deca:0.1.0--7d13833a1220001481c4de0489e893c93ee3310f",
+               dockerParameters=docker_parameters,
+               parameters=_make_parameters(master_ip,
+                                           default_params,
+                                           memory,
+                                           arguments,
+                                           override_parameters))
+
