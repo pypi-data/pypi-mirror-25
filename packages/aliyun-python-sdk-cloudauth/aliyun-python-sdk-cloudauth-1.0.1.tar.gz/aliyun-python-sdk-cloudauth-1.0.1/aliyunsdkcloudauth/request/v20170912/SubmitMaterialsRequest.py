86a3e366b648d3d1ebc4f4225d22f7c6c2d86038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:14 +0000

    Add package aliyun-python-sdk-cloudauth/1.0.1/aliyun-python-sdk-cloudauth-1.0.1.tar.gz

---
+++ packages/aliyun-python-sdk-cloudauth/aliyun-python-sdk-cloudauth-1.0.1.tar.gz/aliyun-python-sdk-cloudauth-1.0.1/aliyunsdkcloudauth/request/v20170912/SubmitMaterialsRequest.py
@@ -0,0 +1,40 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+class SubmitMaterialsRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Cloudauth', '2017-09-12', 'SubmitMaterials','cloudauth')
+		self.set_protocol_type('https');
+
+	def get_Materials(self):
+		return self.get_query_params().get('Materials')
+
+	def set_Materials(self,Materials):
+		for i in range(len(Materials)):	
+			self.add_query_param('Material.' + bytes(i + 1) + '.MaterialType' , Materials[i].get('MaterialType'))
+			self.add_query_param('Material.' + bytes(i + 1) + '.Value' , Materials[i].get('Value'))
+
+
+	def get_VerifyToken(self):
+		return self.get_query_params().get('VerifyToken')
+
+	def set_VerifyToken(self,VerifyToken):
+		self.add_query_param('VerifyToken',VerifyToken)
