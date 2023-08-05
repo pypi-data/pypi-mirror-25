f60cd1695c110867f0a0b960fcf6175e41adf71c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:21 +0000

    Add package matmeta/0.1.0/matmeta-0.1.0.tar.gz

---
+++ packages/matmeta/matmeta-0.1.0.tar.gz/matmeta-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='matmeta',
+    version='0.1.0',
+    url='https://github.com/jasonthiese/CommonMetadata',
+    description='Common support for meta-data',
+    author='Jason Thiese',
+    author_email="jasonthiese@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        'pypif'
+    ]
+)
