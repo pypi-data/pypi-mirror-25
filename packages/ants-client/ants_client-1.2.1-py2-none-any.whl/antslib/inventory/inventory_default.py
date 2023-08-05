2f55dff8a22fc3e2cd5fd81c7c783aa27f242779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:52 +0000

    Add package ants-client/1.2.1/ants_client-1.2.1-py2-none-any.whl

---
+++ packages/ants-client/ants_client-1.2.1-py2-none-any.whl/antslib/inventory/inventory_default.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+"""inventory_default
+===================
+
+
+Default inventory script.
+
+This is a dummy inventory script to bootstrap ants.
+It will return localhost and fqdn as members of
+the ants-common group.
+
+These values can be used to assign roles in local.yml but
+using a dynamic inventory script like inventory_ad.py
+is the prefered way of running ants.
+"""
+
+from antslib.inventory import helper
+
+
+def main():
+    """Print default inventory in JSON."""
+    output = dict()
+    fqdn = antslib.inventory.helper.get_hostname()
+    output['ants-common'] = ([fqdn])
+    print antslib.inventory.helper.format_output(output)
+
+
+if __name__ == '__main__':
+    main()
