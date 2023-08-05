88d5285a022d8e686cf550febd67d83e310ef598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:23 +0000

    Add package quickstep/0.0.6/quickstep-0.0.6.tar.gz

---
+++ packages/quickstep/quickstep-0.0.6.tar.gz/quickstep-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='quickstep',
+    description='Quickstep GRPC API',
+    author='Robert Claus',
+    author_email='robertclaus@gmail.com',
+    version='0.0.6',
+    py_modules=['quickstep','proto.NetworkCli_pb2','proto.NetworkCli_pb2_grpc'],
+    license='Apache'
+)
