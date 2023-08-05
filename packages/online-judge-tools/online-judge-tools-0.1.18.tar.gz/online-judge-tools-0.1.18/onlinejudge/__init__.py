0d6b669928b03afb04cdbbf392159304f2801e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:44 +0000

    Add package online-judge-tools/0.1.18/online-judge-tools-0.1.18.tar.gz

---
+++ packages/online-judge-tools/online-judge-tools-0.1.18.tar.gz/online-judge-tools-0.1.18/onlinejudge/__init__.py
@@ -0,0 +1,10 @@
+# Python Version: 3.x
+from onlinejudge.implementation.version import *
+import onlinejudge.atcoder
+import onlinejudge.yukicoder
+import onlinejudge.anarchygolf
+import onlinejudge.codeforces
+import onlinejudge.hackerrank
+import onlinejudge.aoj
+import onlinejudge.csacademy
+import onlinejudge.dispatch
