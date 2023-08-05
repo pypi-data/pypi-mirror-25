eeb72d9a46e2d004c12a1d62396aad6ccbceb2ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:28 +0000

    Add package pyrvtools/1.0.0/pyrvtools-1.0.0.tar.gz

---
+++ packages/pyrvtools/pyrvtools-1.0.0.tar.gz/pyrvtools-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='pyrvtools',
+    version='1.0.0',
+    packages=['pyrvtools'],
+    install_requires=['xlrd'],
+    url='https://github.com/jbrt/pyrvtools',
+    license='GPL',
+    author='Julien B.',
+    author_email='julien@toshokan.fr',
+    description='Extract useful information from an RVTools ESX inventory file',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
