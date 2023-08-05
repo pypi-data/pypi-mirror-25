052ef332970f89e9d76c1855dc658f2aef86364c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:28 +0000

    Add package plagly/0.2/plagly-0.2.tar.gz

---
+++ packages/plagly/plagly-0.2.tar.gz/plagly-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='plagly',
+      version='0.2',
+      description='Insert description here',
+      url='http://github.com/plagly',
+      author='Plagly',
+      author_email='support@plagly.com',
+      license='MIT',
+      scripts=['plagly'],
+      zip_safe=False)
