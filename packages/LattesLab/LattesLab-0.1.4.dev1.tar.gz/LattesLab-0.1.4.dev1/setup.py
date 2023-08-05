6ddbb9e6c2464dce3519da0fbbb4a557a14cc3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:16 +0000

    Add package LattesLab/0.1.4.dev1/LattesLab-0.1.4.dev1.tar.gz

---
+++ packages/LattesLab/LattesLab-0.1.4.dev1.tar.gz/LattesLab-0.1.4.dev1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='LattesLab',
+      version='0.1.4.dev1',
+      description='Set of tools to generate data products originated by Lattes CV files.',
+      url='https://github.com/tcodingprojects/LattesLab',
+      author='Thiago Santana',
+      author_email='thiagoluis@gmail.com',
+      license='MIT',
+      packages=['LattesLab'],
+	  install_requires=[
+          'wordcloud',
+		  'datetime',
+		  'matplotlib',
+		  'sklearn',
+		  'networkx',
+		  'numpy',
+		  'pandas',
+		  'scikit-fuzzy'
+      ],
+      zip_safe=False)
