9360a686f9a198c06e98f7ad2ce5b9694f16a776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:41 +0000

    Add package BitcoinExchangeFH/0.2.1/BitcoinExchangeFH-0.2.1.tar.gz

---
+++ packages/BitcoinExchangeFH/BitcoinExchangeFH-0.2.1.tar.gz/BitcoinExchangeFH-0.2.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='BitcoinExchangeFH',
+    version='0.2.1',
+    author='Gavin Chan',
+    author_email='gavincyi@gmail.com',
+    packages=['befh'],
+    url='http://pypi.python.org/pypi/BitcoinExchangeFH/',
+    license='LICENSE.txt',
+    description='Cryptocurrency exchange market data feed handler.',
+    entry_points={
+            'console_scripts': ['bitcoinexchangefh=befh.bitcoinexchangefh:main']
+        },
+    install_requires=[
+            'pymysql',
+            'websocket-client',
+            'numpy',
+            'qpython',
+            'pyzmq'
+        ]
+    )
