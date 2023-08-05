28ad69a94c2acd56eff03b785b51c9ebea6c2cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:16 +0000

    Add package modoboa-pdfcredentials/1.1.2/modoboa-pdfcredentials-1.1.2.tar.gz

---
+++ packages/modoboa-pdfcredentials/modoboa-pdfcredentials-1.1.2.tar.gz/modoboa-pdfcredentials-1.1.2/modoboa_pdfcredentials/documents.py
@@ -0,0 +1,81 @@
+"""The code used to generate PDF files (based on Reportlab."""
+
+from io import BytesIO
+import os
+
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.units import cm
+from reportlab.lib.enums import TA_CENTER
+from reportlab.lib import utils
+from reportlab.platypus import (
+    SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, Frame,
+    Image
+)
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+
+from django.utils.translation import ugettext as _
+from django.conf import settings
+
+from django.contrib.sites.models import Site
+
+from . import lib
+
+styles = getSampleStyleSheet()
+styles.add(ParagraphStyle(
+    name='Footer', alignment=TA_CENTER, fontName="Helvetica-Oblique",
+    textColor=colors.lightgrey
+))
+
+
+def resized_image(path, width=1*cm):
+    img = utils.ImageReader(path)
+    iw, ih = img.getSize()
+    aspect = ih / float(iw)
+    return Image(path, width=width, height=(width * aspect))
+
+
+def credentials(account, password):
+    """Generate a PDF document containing account credentials."""
+
+    def page_template(canvas, doc):
+        canvas.setTitle(_("Personal account information"))
+        canvas.setAuthor(account.fullname)
+        canvas.setCreator("Modoboa")
+        footer = [Paragraph(_("Powered by Modoboa - Mail hosting made simple"),
+                            styles["Footer"])]
+        Frame(0, 0, 21 * cm, 4 * cm).addFromList(footer, canvas)
+
+    filename = lib.get_creds_filename(account)
+    buff = BytesIO()
+    doc = SimpleDocTemplate(buff, pagesize=A4)
+    story = []
+    story.append(resized_image(lib.get_document_logo(), 6*cm))
+    story.append(Spacer(1, 1 * cm))
+    story.append(Paragraph(_("Personal account information"), styles["Title"]))
+    story.append(Spacer(1, 2 * cm))
+    story.append(Paragraph(_("""
+Dear %s, this document contains the credentials you will need
+to connect to Modoboa. Learn the content and destroy
+the document as soon as possible.
+""") % account.fullname, styles["Normal"]))
+    url = "http(s)://{}{}".format(
+        Site.objects.get_current().domain, settings.LOGIN_URL)
+    data = [
+        ["URL", url],
+        [_("Username"), account.username],
+        [_("Password"), password]
+    ]
+    table = Table(data)
+    table.setStyle(TableStyle([
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+        ('BACKGROUND', (0, 0), (0, -1), colors.lightgrey),
+        # ('ALIGN', (0, 0), (-1, 0), 'CENTER'),
+    ]))
+    story.append(Spacer(1, 2 * cm))
+    story.append(table)
+
+    doc.build(story, onFirstPage=page_template, onLaterPages=page_template)
+    length = len(buff.getvalue())
+    buff.seek(0)
+    lib.crypt_and_save_to_file(buff, filename, length)
---
+++ packages/modoboa-pdfcredentials/modoboa-pdfcredentials-1.1.2.tar.gz/modoboa-pdfcredentials-1.1.2/modoboa_pdfcredentials/lib.py
@@ -0,0 +1,105 @@
+"""Internal library."""
+
+from io import BytesIO
+import os
+import random
+import struct
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+from django.conf import settings
+from django.utils.encoding import force_bytes
+from django.utils.translation import ugettext as _
+
+from modoboa.lib.exceptions import InternalError
+from modoboa.parameters import tools as param_tools
+
+
+def init_storage_dir():
+    """Create the directory whare documents will be stored."""
+    storage_dir = param_tools.get_global_parameter("storage_dir")
+    if os.path.exists(storage_dir):
+        return
+    try:
+        os.mkdir(storage_dir)
+    except (OSError, IOError) as inst:
+        raise InternalError(
+            _("Failed to create the directory that will contain "
+              "PDF documents (%s)") % inst
+        )
+
+
+def get_creds_filename(account):
+    """Return the full path of a document."""
+    storage_dir = param_tools.get_global_parameter("storage_dir")
+    return os.path.join(storage_dir, account.username + ".pdf")
+
+
+def delete_credentials(account):
+    """Try to delete a local file."""
+    fname = get_creds_filename(account)
+    if not os.path.exists(fname):
+        return
+    try:
+        os.remove(fname)
+    except OSError:
+        pass
+
+
+def _get_cipher(iv):
+    """Return ready-to-user Cipher."""
+    key = param_tools.get_global_parameter("secret_key", app="core")
+    backend = default_backend()
+    return Cipher(
+        algorithms.AES(force_bytes(key)),
+        modes.CBC(force_bytes(iv)),
+        backend=backend
+    )
+
+
+def crypt_and_save_to_file(content, filename, length, chunksize=64*1024):
+    """Crypt content and save it to a file."""
+    iv = "".join(chr(random.randint(0, 0xFF)) for i in range(16))
+    cipher = _get_cipher(iv)
+    encryptor = cipher.encryptor()
+    with open(filename, 'wb') as fp:
+        fp.write(struct.pack('<Q', length))
+        fp.write(iv)
+        while True:
+            chunk = content.read(chunksize)
+            if not len(chunk):
+                break
+            elif len(chunk) % 16:
+                chunk += ' ' * (16 - len(chunk) % 16)
+            fp.write(
+                encryptor.update(force_bytes(chunk)) + encryptor.finalize())
+
+
+def decrypt_file(filename, chunksize=24*1024):
+    """Decrypt the content of a file and return it."""
+    buff = BytesIO()
+    with open(filename, 'rb') as fp:
+        origsize = struct.unpack('<Q', fp.read(struct.calcsize('Q')))[0]
+        iv = fp.read(16)
+        cipher = _get_cipher(iv)
+        decryptor = cipher.decryptor()
+        while True:
+            chunk = fp.read(chunksize)
+            if not len(chunk):
+                break
+            buff.write(decryptor.update(chunk))
+        buff.write(decryptor.finalize())
+        buff.truncate(origsize)
+    return buff.getvalue()
+
+
+def get_document_logo():
+    """Retrieve path to logo."""
+    try:
+        logo = settings.MODOBOA_CUSTOM_LOGO
+        logo = os.path.join(settings.MEDIA_ROOT, os.path.basename(logo))
+    except AttributeError:
+        logo = os.path.join(settings.STATIC_ROOT, "css/modoboa.png")
+    finally:
+        return logo
