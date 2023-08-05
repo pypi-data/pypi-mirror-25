4112af48725de5e19a4e3b27772857df829bddd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:40 +0000

    Add package mytools/1.1.1/mytools-1.1.1.tar.gz

---
+++ packages/mytools/mytools-1.1.1.tar.gz/mytools-1.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 15:32:21 2016
+
+@author: 
+Maximilian N. Guenther
+Battcock Centre for Experimental Astrophysics,
+Cavendish Laboratory,
+JJ Thomson Avenue
+Cambridge CB3 0HE
+Email: mg719@cam.ac.uk
+"""
+
+
+from setuptools import setup
+
+setup(
+    name = 'mytools',      # The name of the PyPI-package.
+    packages = ['mytools'],
+    version = '1.1.1',    # Update the version number for new releases
+    #scripts=['ngtsio'],  # The name of the included script(s), and also the command used for calling it
+    description = 'Set of tools for photometric time series data and plotting',
+    author = 'Maximilian N. Guenther',
+    author_email = 'mg719@cam.ac.uk',
+    url = 'https://github.com/MNGuenther/mytools',
+    download_url = 'https://github.com/MNGuenther/mytools/releases',
+    classifiers = [],
+    install_requires=['numpy>=1.10','matplotlib>0.1','scipy>=0.1','pandas>=0.1']
+    )
+
+
+
