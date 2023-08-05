b60d692f172477dcaf424029f302c9c2b1681d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:29 +0000

    Add package cidaas_interceptor/0.1/cidaas_interceptor-0.1.tar.gz

---
+++ packages/cidaas_interceptor/cidaas_interceptor-0.1.tar.gz/cidaas_interceptor-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="cidaas_interceptor",
+      version="0.1",
+      description="Python cidaas interceptor",
+      url="http://github.com/cidaas/cidaasinterceptor",
+      author="cidaas",
+      author_email="cidaasmanagement@gmail.com",
+      packages=["cidaas_interceptor"],
+      zip_safe=False)
