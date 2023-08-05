c6a3c96e687afacac0bc8482a70f7f4f32c7c2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:02 +0000

    Add package django-utils-plus/0.4.0/django-utils-plus-0.4.0.tar.gz

---
+++ packages/django-utils-plus/django-utils-plus-0.4.0.tar.gz/django-utils-plus-0.4.0/utils_plus/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+
+
+def return_path_view(request, *args, **kwargs):
+    """
+        utility view for quick test. return the path and arguments as string
+    Args:
+        request:
+        *args:
+        **kwargs:
+
+    Returns:
+        HttpResponse:
+    Usage:
+    >>> reqf = getfixture('rf')
+    >>> req = reqf.get('/')
+    >>> return_path_view(req, 'arg1', 'arg2', kwarg=1).content
+    b"Requested / with Args: ('arg1', 'arg2') {'kwarg': 1}"
+    """
+    return HttpResponse("Requested " + request.path + " with Args: {} {}".format(args, kwargs))
