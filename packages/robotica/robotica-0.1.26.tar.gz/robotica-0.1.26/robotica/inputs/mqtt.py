d219ef49ebb8050b63555d3de1ceeab96f2f3ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:44 +0000

    Add package robotica/0.1.26/robotica-0.1.26.tar.gz

---
+++ packages/robotica/robotica-0.1.26.tar.gz/robotica-0.1.26/robotica/inputs/mqtt.py
@@ -0,0 +1,118 @@
+import asyncio
+import json
+import logging
+import platform
+
+import yaml
+
+from typing import Any, Optional, Dict
+
+from hbmqtt.client import MQTTClient, ClientException, QOS_0
+
+from robotica.executor import Executor
+from robotica.inputs import Input
+from robotica.schedule import Schedule
+
+logger = logging.getLogger(__name__)
+
+JsonType = Any
+
+
+TOPICS = [
+    ('/execute/', QOS_0),
+    ('/schedule/', QOS_0),
+]
+
+
+class MqttInput(Input):
+    def __init__(
+            self, loop: asyncio.AbstractEventLoop,
+            config: Dict,
+            executor: Executor,
+            schedule: Schedule) -> None:
+        self._loop = loop
+        self._config = config
+        self._disabled = self._config['disabled']
+        self._broker_url = self._config['broker_url']
+        self._executor = executor
+        self._schedule = schedule
+        self._task = None  # type: Optional[asyncio.Task]
+        self._client = MQTTClient()
+
+    def start(self) -> None:
+        if not self._disabled:
+            self._task = self._loop.create_task(self._mqtt())
+
+    def stop(self) -> None:
+        if not self._disabled and self._task is not None:
+            self._task.cancel()
+            try:
+                self._loop.run_until_complete(self._task)
+            except asyncio.CancelledError:
+                pass
+
+    async def _execute(self, data: JsonType) -> None:
+
+        reply_topic = data.get('reply_topic', None)
+        server = platform.node()
+
+        async def reply(data: JsonType) -> None:
+            client = self._client
+            raw_data = json.dumps(data).encode('UTF8')
+            if reply_topic is not None:
+                await client.publish(reply_topic, raw_data, qos=QOS_0)
+
+        try:
+            locations = data['locations']
+            actions = data['actions']
+        except KeyError:
+            logger.error("Required value missing.")
+            await reply({'status': 'error', 'server': server, })
+            return
+
+        try:
+            await reply({'status': 'processing', 'server': server, })
+            await self._executor.do_actions(locations, actions)
+            await reply({'status': 'success', 'server': server, })
+        except Exception as e:
+            logger.exception("Error in _execute")
+            await reply({'status': 'error', 'server': server, })
+
+        return
+
+    async def _process_schedule(self, data: JsonType) -> None:
+         await self._schedule.set_schedule(data)
+         self._schedule.save_schedule()
+
+    async def _process(self, topic: str, data: JsonType) -> None:
+        if topic.startswith("/execute/"):
+            await self._execute(data)
+        if topic.startswith("/schedule/"):
+            await self._process_schedule(data)
+
+    async def _mqtt(self) -> None:
+        client = self._client
+        await client.connect(self._broker_url)
+        await client.subscribe(TOPICS)
+
+        while True:
+            try:
+                message = await client.deliver_message()
+                packet = message.publish_packet
+                topic = packet.variable_header.topic_name
+                raw_data = bytes(packet.payload.data).decode('UTF8')
+
+                try:
+                    data = json.loads(raw_data)
+                    await self._process(topic, data)
+                except json.JSONDecodeError as e:
+                    logger.error("JSON Error %s" % e)
+
+            except asyncio.CancelledError:
+                await client.unsubscribe([t[0] for t in TOPICS])
+                await client.disconnect()
+                raise
+            except ClientException as e:
+                logger.error("Client exception: %s" % e)
+            except Exception as e:
+                logger.exception("Unknown exception.")
---
+++ packages/robotica/robotica-0.1.26.tar.gz/robotica-0.1.26/robotica/outputs/lifx.py
@@ -0,0 +1,97 @@
+import asyncio
+import logging
+from typing import List, Union, Set, Dict
+
+import yaml
+from aiolifxc.aiolifx import Lights, Light, Color, DeviceOffline
+
+from robotica.outputs import Output
+from robotica.types import Action
+
+logger = logging.getLogger(__name__)
+
+
+class LifxOutput(Output):
+    def __init__(self, loop: asyncio.AbstractEventLoop, config: Dict) -> None:
+        self._loop = loop
+        self._config = config
+        self._disabled = self._config['disabled']
+        self._lights = Lights(loop=self._loop)
+        self._locations = self._config.get('locations', {}) or {}
+
+    def start(self) -> None:
+        if not self._disabled:
+            logger.debug("LIFX enabled.")
+            self._task = self._lights.start_discover()
+
+    def stop(self) -> None:
+        if not self._disabled:
+            self._task.cancel()
+
+    def _get_labels_for_location(self, location: str) -> Set[str]:
+        labels = set(self._locations.get(location, []))
+        return labels
+
+    def is_action_required_for_location(self, location: str, action: Action) -> bool:
+        labels = self._get_labels_for_location(location)
+        if len(labels) == 0:
+            return False
+
+        if 'lights' in action:
+            return True
+
+        return False
+
+    async def execute(self, location: str, action: Action) -> None:
+        if 'lights' in action:
+            lights = action['lights']
+
+            lights_action = lights['action']
+            if lights_action == "flash":
+                await self.flash(location=location)
+            elif lights_action == "wake_up":
+                await self.wake_up(location=location)
+            elif lights_action == "turn_off":
+                await self.turn_off(location=location)
+            else:
+                logger.error("Unknown action '%s'.", action)
+
+    def _get_lights_from_location(self, location: str) -> Lights:
+        labels = self._get_labels_for_location(location)
+        lights = self._lights.get_by_lists(labels=list(labels))  # type: Lights
+        return lights
+
+    async def wake_up(self, location: str) -> None:
+        async def single_device(device: Light) -> None:
+            try:
+                power = await device.get_power()
+                if not power:
+                    await device.set_color(
+                        Color(hue=0, saturation=0, brightness=0, kelvin=2500))
+                await device.set_power(True)
+                await device.set_color(
+                    Color(hue=0, saturation=0, brightness=100, kelvin=2500),
+                    duration=60000)
+            except DeviceOffline:
+                logger.error("Light is offline %s.", device)
+
+        lights = self._get_lights_from_location(location)
+        logger.info("Lifx wakeup for lights %s.", lights)
+        await lights.do_for_every_device(Light, single_device)
+
+    async def flash(self, location: str) -> None:
+        lights = self._get_lights_from_location(location)
+        logger.info("Lifx flash for lights %s.", lights)
+        await lights.set_waveform(
+            color=Color(hue=0, saturation=100, brightness=100, kelvin=3500),
+            transient=1,
+            period=1000,
+            cycles=2,
+            duty_cycle=0,
+            waveform=0,
+        )
+
+    async def turn_off(self, location: str) -> None:
+        lights = self._get_lights_from_location(location)
+        logger.info("Lifx flash for lights %s.", lights)
+        await lights.set_light_power(False)
