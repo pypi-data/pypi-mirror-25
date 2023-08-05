3ea88df9249f94fd688aff0827184d99d436e981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:27 +0000

    Add package madliar/0.1a1.dev1211/madliar-0.1a1.dev1211.tar.gz

---
+++ packages/madliar/madliar-0.1a1.dev1211.tar.gz/madliar-0.1a1.dev1211/madliar/__init__.py
@@ -0,0 +1,15 @@
+"""
+    madliar
+    ~~~~~~~
+
+    Madliar is a wsgi web framework written in pure Python.
+    For more information - see `README.md`. A demo website
+    is <https://www.madliar.com>.
+
+    Caoliang
+    --------
+
+    :copyright: (c) 2017 MIT License
+"""
+
+__version__ = '0.1a1.dev1211'
