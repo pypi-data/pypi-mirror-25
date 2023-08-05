6f46c59de5bdb63702807b01960c14bfe8df6679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:56 +0000

    Add package persper/0.0.0/persper-0.0.0.tar.gz

---
+++ packages/persper/persper-0.0.0.tar.gz/persper-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='persper',
+  packages=['persper'],
+  version='0.0.0',
+  description='Persper Library',
+  author='ultimatebeaver',
+  author_email='yinhezheng1995@gmail.com',
+  url='https://github.com/UltimateBeaver/persper',
+  keywords=['open source', 'intellectual shareholding'],
+  classifiers=[],
+)
