defc5a309abb8cd8db9015164655dd0b256ea855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:53 +0000

    Add package waterbear/1.1.6/waterbear-1.1.6.tar.gz

---
+++ packages/waterbear/waterbear-1.1.6.tar.gz/waterbear-1.1.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='waterbear',
+      description='A utility that makes it easy to use dot notation with python dictionaries',
+      version='1.1.6',
+      url='https://github.com/episodeyang/waterbear',
+      author='Ge Yang',
+      author_email='yangge1987@gmail.com',
+      license=None,
+      keywords=['waterbear', 'dict', 'dot-notation'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python :: 3'
+      ],
+      packages=['waterbear'],
+      install_requires=[]
+      )
