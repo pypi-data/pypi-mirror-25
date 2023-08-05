a89f5a2f91bbefbca4a950e65f990c45465b06f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:59 +0000

    Add package extract-zip/1.0.0/extract_zip-1.0.0.tar.gz

---
+++ packages/extract-zip/extract_zip-1.0.0.tar.gz/extract_zip-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name 		= 'extract_zip',
+	version		= '1.0.0',
+	py_modules	= ['Extract'],
+	author		= 'ayush571995',
+	author_email	= 'ayuchitsaluja8@gmail.com',
+	url		= 'https://github.com/ayush571995/Extract_zip',
+	description	= 'With this you can extract all zip , tar ,tar.gz files which can further contain zip files just by instantiating the class and passing the argument of the path in the python module'
+
+     ) 
+ 
