ab4bb0f9a55b62116a0c600c51a0b9967f0ab9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:58 +0000

    Add package policyuniverse/1.0.7.6/policyuniverse-1.0.7.6.tar.gz

---
+++ packages/policyuniverse/policyuniverse-1.0.7.6.tar.gz/policyuniverse-1.0.7.6/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# from distutils.core import setup
+from setuptools import setup
+
+setup(name='policyuniverse',
+      version='1.0.7.6',
+      description='AWS IAM Policy Utilities',
+      author='Patrick Kelley',
+      author_email='pkelley@netflix.com',
+      url='https://github.com/Netflix-Skunkworks/policyuniverse',
+      keywords = ['iam', 'policy', 'wildcard'],
+      packages=['policyuniverse'],
+      package_data={
+          'policyuniverse': [
+              'master_permissions.json',
+          ]
+      },
+      include_package_data=True,
+      zip_safe=False
+     )
