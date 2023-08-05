bb8930febc214990def72becfd71c478f6700e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:05 +0000

    Add package gaft/0.2.4/gaft-0.2.4.zip

---
+++ packages/gaft/gaft-0.2.4.zip/gaft-0.2.4/tests/gaft_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+''' Module for all test in ga.py
+'''
+
+import unittest
+
+from individual_test import IndividualTest
+from population_test import PopulationTest
+from roulette_wheel_selection_test import RouletteWheelSelectionTest
+from uniform_crossover_test import UniformCrossoverTest
+from flip_bit_mutation_test import FlipBitMutationTest
+from mpiutil_test import MPIUtilTest
+from engine_test import GAEngineTest
+
+def suite():
+    test_suite = unittest.TestSuite([
+        unittest.TestLoader().loadTestsFromTestCase(IndividualTest),
+        unittest.TestLoader().loadTestsFromTestCase(PopulationTest),
+        unittest.TestLoader().loadTestsFromTestCase(RouletteWheelSelectionTest),
+        unittest.TestLoader().loadTestsFromTestCase(UniformCrossoverTest),
+        unittest.TestLoader().loadTestsFromTestCase(FlipBitMutationTest),
+        unittest.TestLoader().loadTestsFromTestCase(MPIUtilTest),
+        unittest.TestLoader().loadTestsFromTestCase(GAEngineTest)
+    ])
+
+    return test_suite
+
+if '__main__' == __name__:
+    result = unittest.TextTestRunner(verbosity=2).run(suite())
+
+    if result.errors or result.failures:
+        raise ValueError('Get erros and failures')
+
