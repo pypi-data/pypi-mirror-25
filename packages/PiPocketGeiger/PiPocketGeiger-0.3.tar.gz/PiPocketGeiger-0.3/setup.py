e25659da5f0717dc4275cd55fbc420df9c5dc76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:51 +0000

    Add package PiPocketGeiger/0.3/PiPocketGeiger-0.3.tar.gz

---
+++ packages/PiPocketGeiger/PiPocketGeiger-0.3.tar.gz/PiPocketGeiger-0.3/setup.py
@@ -0,0 +1,51 @@
+"""
+==============
+PiPocketGeiger
+==============
+
+Radiation Watch Pocket Geiger Type 5 library for Raspberry Pi.
+
+Usage
+=====
+::
+
+    from PiPocketGeiger import RadiationWatch
+    import time
+
+    with RadiationWatch(24, 23) as radiationWatch:
+        while 1:
+            print(radiationWatch.status())
+            time.sleep(5)
+
+
+See GitHub repository for complete documentation.
+"""
+import sys
+import re
+import ast
+from setuptools import setup
+
+setup(
+    name='PiPocketGeiger',
+    version='0.3',
+    url='https://github.com/MonsieurV/PiPocketGeiger',
+    license='MIT',
+    author='Yoan Tournade',
+    author_email='yoan@ytotech.com',
+    description='A library for monitoring radiation with the Radiation Watch '
+                'Pocket Geiger.',
+    long_description=__doc__,
+    packages=['PiPocketGeiger'],
+    include_package_data=True,
+    zip_safe=True,
+    platforms='any',
+    install_requires=[
+        'RPi.GPIO>=0.5.0a',
+    ],
+    extras_require={
+        'dev': [
+            'flake8',
+            'pylint'
+        ]
+    }
+)
