f7d33a1471a97d9e67d825385f24b99e7f5478a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:07 +0000

    Add package pines/2.65/pines-2.65-py2.py3-none-any.whl

---
+++ packages/pines/pines-2.65-py2.py3-none-any.whl/pines/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+__version__ = '2.65'
+
+def info():
+    import sys, os
+    print( f"┌── PINES TOOLKIT {__version__} " + "─"*(57-len(__version__)) )
+    v = '\n│'.join(sys.version.split('\n'))
+    print(f"│Python {v}")
+    print(f"│EXE ─ {sys.executable}")
+    print(f"│CWD ─ {os.getcwd()}", )
+    for p in sys.path[:1]:
+        print(f"│PTH ┬ {p}")
+    for p in sys.path[1:-1]:
+        print(f"│    ├ {p}")
+    for p in sys.path[-1:]:
+        print(f"│    └ {p}")
+    print("└───────────────────────────────────────────────────────────────────────────")
+
+
+info()
+
+
+
+## most common items here
+
+from .attribute_dict import fdict, quickdot
+from .codex import phash
