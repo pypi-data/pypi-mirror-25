b48cedebe9172003236ce048afc1a67c1f868fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:36 +0000

    Add package deeperlib/0.1b0/deeperlib-0.1b0.tar.gz

---
+++ packages/deeperlib/deeperlib-0.1b0.tar.gz/deeperlib-0.1b0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="deeperlib",
+    version="0.1.b",
+    packages=find_packages(),
+    author='yongjunhe',
+    author_email='141250047@smail.nju.edu.cn',
+    maintainer='yongjunhe',
+    maintainer_email='141250047@smail.nju.edu.cn',
+    url = 'https://github.com/sfu-db/deeper',
+    platforms = ["all"],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'pqdict>=1.0.0',
+        'requests>=2.18.4',
+        'simplejson>=3.11.1',
+        'rauth>=0.7.3',
+    ]
+)
