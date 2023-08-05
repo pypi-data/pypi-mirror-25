b38e168f2d4275a2738eb63520ad0de302ad59c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:25 +0000

    Add package pandas2/0.0.0/pandas2-0.0.0.tar.gz

---
+++ packages/pandas2/pandas2-0.0.0.tar.gz/pandas2-0.0.0/pandas2-setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+LONG_DESCRIPTION = """
+Placeholder for pandas2
+"""
+
+setup(
+    name='pandas2',
+    packages=['pandas2'],
+    version='0.0.0',
+    description="pandas2",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+    ],
+    url='http://github.com/pandas-dev/pandas2',
+    license='Apache License, Version 2.0',
+    maintainer="Wes McKinney",
+    maintainer_email="wesmckinn@gmail.com"
+)
