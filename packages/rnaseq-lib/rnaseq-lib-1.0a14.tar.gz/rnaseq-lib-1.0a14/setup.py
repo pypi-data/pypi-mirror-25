715cee0774244273ac2bebd6b56acf170b6e42ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:23 +0000

    Add package rnaseq-lib/1.0a14/rnaseq-lib-1.0a14.tar.gz

---
+++ packages/rnaseq-lib/rnaseq-lib-1.0a14.tar.gz/rnaseq-lib-1.0a14/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='rnaseq-lib',
+      version='1.0a14',
+      description='Library of convenience functions related to current research',
+      url='http://github.com/jvivian/rnaseq-lib',
+      author='John Vivian',
+      author_email='jtvivian@gmail.com',
+      license='MIT',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      package_data={'rnaseq_lib.utils': ['data/*']},
+      install_requires=['pandas',
+                        'numpy',
+                        'seaborn',
+                        'requests',
+                        'sklearn',
+                        'progressbar',
+                        'holoviews'])
---
+++ packages/rnaseq-lib/rnaseq-lib-1.0a14.tar.gz/rnaseq-lib-1.0a14/src/rnaseq_lib/utils/tissues.py
@@ -0,0 +1,140 @@
+import os
+import pickle
+
+import numpy as np
+import pandas as pd
+
+from rnaseq_lib.plotting import plot_boxplot
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+
+def return_samples():
+    """
+    Returns sample dictionary which maps TCGA and GTEx samples to a tissue.
+    Synapse ID: syn10296681
+
+    :return: Tissues are keys are list of samples are values
+    :rtype: dict(str, list(str))
+    """
+    return pickle.load(open(os.path.join(__location__, 'data/samples.pickle'), 'rb'))
+
+
+def get_gene_map():
+    """
+    Dictionary mapping gene ID to gene name
+
+    :return: Gene map
+    :rtype: dict
+    """
+    return pickle.load(open(os.path.join(__location__, 'data/gene_map.pickle'), 'rb'))
+
+
+def map_genes(df):
+    gene_map = get_gene_map()
+    genes = [gene_map[x.split('.')[0]] for x in df.index]
+    df.index = genes
+    return df
+
+
+def get_mab_targets():
+    """
+    Returns sorted list of MAB cancer drug targets
+
+    :return: Sorted gene list
+    :rtype: list
+    """
+    path = os.path.join(__location__, 'data/cancer-MAB-gene-targets.txt')
+    return sorted([x.strip() for x in open(path, 'r').readlines()])
+
+
+def get_ucsf_genes():
+    """
+    Returns sorted list of UCSF genes
+
+    :return: Sorted gene list
+    :rtype: list
+    """
+    path = os.path.join(__location__, 'data/UCSF-genes.csv')
+    return sorted([x.strip() for x in open(path, 'r').readlines()])
+
+
+def get_civic_genes():
+    """
+    Returns sorted list of genes from CIViC
+
+    :return: Sorted gene list
+    :rtype: list
+    """
+    path = os.path.join(__location__, 'data/civic-genes.txt')
+    return sorted([x.strip() for x in open(path, 'r').readlines()])
+
+
+def get_ucsf_subset(df):
+    """
+    Subset UCSF dataframe and return.
+
+    :param pd.DataFrame df: Input Dataframe in the format of "Genes by Samples"
+    :return: Subset of Dataframe that only includes UCSF genes
+    :rtype: pd.DataFrame
+    """
+    df = map_genes(df)
+
+    ucsf_genes = get_ucsf_genes()
+    ucsf_genes = [x for x in ucsf_genes if x in df.index]
+
+    return df.loc[ucsf_genes]
+
+
+def get_tumor_samples(tissue):
+    """
+    Returns TCGA tumor samples for a tissue
+    :param str tissue:
+    :return: List of tumor samples
+    :rtype: list
+    """
+    samples = return_samples()
+    return [x for x in samples[tissue] if x.endswith('-01')]
+
+
+def get_gtex_samples(tissue):
+    """
+    Returns GTEx samples for a tissue
+
+    :param str tissue:
+    :return: List of GTEx samples
+    :rtype: list
+    """
+    samples = return_samples()
+    return [x for x in samples[tissue] if not x.startswith('TCGA')]
+
+
+def get_normal_samples(tissue):
+    """
+    Returns TCGA normal samples for a tissue
+    :param str tissue:
+    :return: List of TCGA normal samples
+    :rtype: list
+    """
+    samples = return_samples()
+    return [x for x in samples[tissue] if x.endswith('-11')]
+
+
+def gene_expression_boxplot(df, tissue, gene, normal=False):
+    """
+    Returns a holoviews box and whisker object of a gene for a tissue
+
+    :param pd.DataFrame df: Gene by Samples DataFrame
+    :param str tissue: Tissue to use
+    :param str gene: Gene to use
+    :param bool normal: If True, will include TCGA normal samples
+    :return: Boxplot of gene for tissue
+    :rtype: hv.BoxWhisker
+    """
+    plot_info = {'GTEx': get_gtex_samples(tissue),
+                 'Tumor': get_tumor_samples(tissue)}
+
+    if normal:
+        plot_info['Normal'] = get_normal_samples(tissue)
+
+    return plot_boxplot(df, plot_info=plot_info, feature=gene, norm_func=lambda x: np.log2(x + 1))
