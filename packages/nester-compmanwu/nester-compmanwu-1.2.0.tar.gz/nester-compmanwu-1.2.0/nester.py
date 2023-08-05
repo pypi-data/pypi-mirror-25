651ae8fdb47e4acc6feb56513251e5570c86e935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:29 +0000

    Add package nester-compmanwu/1.2.0/nester-compmanwu-1.2.0.tar.gz

---
+++ packages/nester-compmanwu/nester-compmanwu-1.2.0.tar.gz/nester-compmanwu-1.2.0/nester.py
@@ -0,0 +1,9 @@
+def print_lol2(the_list,level=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol2(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                 print("\t",end=' ')
+            print(each_item)
+                
