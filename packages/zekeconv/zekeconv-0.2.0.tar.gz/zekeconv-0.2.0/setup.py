f9e9b5efd433756cfc594f6bdcd22ff6216f5afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:22 +0000

    Add package zekeconv/0.2.0/zekeconv-0.2.0.tar.gz

---
+++ packages/zekeconv/zekeconv-0.2.0.tar.gz/zekeconv-0.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='zekeconv',
+    version='0.2.0',
+    description='converter between zeke dumps and directory structures',
+    author='Stanislav Klenin',
+    author_email='stanislav.klenin@gmail.com',
+    license='Apache Software License',
+    packages=['zekeconv'],
+    entry_points={
+        'console_scripts': [
+            'zekeconv = zekeconv.zekeconv:main'
+        ]
+    },
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
