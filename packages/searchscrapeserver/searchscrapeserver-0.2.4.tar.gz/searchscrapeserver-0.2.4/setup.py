9cec14b298231b4d6b59746bd734b48ed3886d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:27 +0000

    Add package searchscrapeserver/0.2.4/searchscrapeserver-0.2.4.tar.gz

---
+++ packages/searchscrapeserver/searchscrapeserver-0.2.4.tar.gz/searchscrapeserver-0.2.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='searchscrapeserver',
+      version='0.2.4',
+      description='Server implementation scraping a number of popular servers',
+      url='https://github.com/EdmundMartin/SearchScraperAPI',
+      download_url = 'https://github.com/EdmundMartin/SearchScraperAPI/archive/0.2.3.tar.gz',
+      author='Edmund Martin',
+      author_email='edmartin101@googlemail.com',
+      license='GPL-3.0',
+      packages=['searchscrapeserver', 'searchscrapeserver.server', 'searchscrapeserver.scraping', 'searchscrapeserver.parsing', 
+      'searchscrapeserver.common', 'searchscrapeserver.schemas'],
+      install_requires=[
+      	'aiohttp',
+      	'lxml',
+      	'bs4',
+      	'marshmallow',
+      ],
+      zip_safe=False)
