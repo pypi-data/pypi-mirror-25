c808b39dcd682d11a8ca57717c205c72d675cbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:27 +0000

    Add package django-loaderio-thelab/1.0.0/django-loaderio-thelab-1.0.0.tar.gz

---
+++ packages/django-loaderio-thelab/django-loaderio-thelab-1.0.0.tar.gz/django-loaderio-thelab-1.0.0/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-loaderio-thelab',
+    version='1.0.0',
+    packages=['loaderio', 'loaderio.migrations'],
+    include_package_data=True,
+    license='MIT License',
+    description='An app for adding loader.io validation tokens',
+    long_description=README,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
