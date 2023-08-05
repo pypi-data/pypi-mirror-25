4851cf61c121c17bb8a377952be5e8a2df34e77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:02 +0000

    Add package tsrc/0.3.0/tsrc-0.3.0.tar.gz

---
+++ packages/tsrc/tsrc-0.3.0.tar.gz/tsrc-0.3.0/tsrc/cli/status.py
@@ -0,0 +1,67 @@
+""" Entry point for tsrc status """
+
+import attr
+import ui
+
+import tsrc.cli
+
+
+# pylint: disable=too-few-public-methods
+@attr.s
+class Status:
+    src = attr.ib()
+    branch = attr.ib()
+    dirty = attr.ib()
+
+
+def collect_statuses(workspace):
+    errors = list()
+    result = list()
+    repos = workspace.get_repos()
+
+    if not repos:
+        return errors, result
+
+    num_repos = len(repos)
+    max_len = max((len(x.src) for x in repos))
+    for i, repo, full_path in workspace.enumerate_repos():
+        ui.info_count(i, num_repos,
+                      "Checking", repo.src.ljust(max_len + 1), end="\r")
+        try:
+            branch = tsrc.git.get_current_branch(full_path)
+        except tsrc.git.GitError as e:
+            errors.append((repo.src, e))
+            continue
+
+        dirty = tsrc.git.is_dirty(full_path)
+
+        result.append(Status(src=repo.src, branch=branch, dirty=dirty))
+
+    ui.info("")
+    return result, errors
+
+
+def display_statuses(statuses, errors):
+    if not statuses:
+        return
+    max_src = max((len(x.src) for x in statuses))
+    for status in statuses:
+        message = (ui.green, "*", ui.reset, ui.bold, status.src.ljust(max_src),
+                   ui.reset, ui.green, status.branch)
+        if status.dirty:
+            message = message + (ui.reset, ui.brown, "(dirty)")
+        ui.info(*message)
+
+    if errors:
+        ui.info()
+        ui.error("Errors when getting branch")
+        for src, error in errors:
+            ui.info("*", ui.bold, src, ui.reset, error.output)
+        ui.info()
+
+
+def main(args):
+    workspace = tsrc.cli.get_workspace(args)
+    workspace.load_manifest()
+    statuses, errors = collect_statuses(workspace)
+    display_statuses(statuses, errors)
---
+++ packages/tsrc/tsrc-0.3.0.tar.gz/tsrc-0.3.0/tsrc/git.py
@@ -0,0 +1,125 @@
+""" git tools """
+
+
+import os
+import subprocess
+
+import path
+import ui
+
+import tsrc
+
+
+class GitError(tsrc.Error):
+    pass
+
+
+class GitCommandError(GitError):
+    def __init__(self, working_path, cmd, *, output=None):
+        self.cmd = cmd
+        self.working_path = working_path
+        self.output = output
+        message = "`git {cmd}` from {working_path} failed"
+        message = message.format(cmd=" ".join(cmd), working_path=working_path)
+        if output:
+            message += "\n" + output
+        super().__init__(message)
+
+
+class WorktreeNotFound(GitError):
+    def __init__(self, working_path):
+        super().__init__("'{}' is not inside a git repository".format(working_path))
+
+
+def run_git(working_path, *cmd, raises=True):
+    """ Run git `cmd` in given `working_path`
+
+    If `raises` is True and git return code is non zero, raise
+    an exception. Otherwise, return a tuple (returncode, out)
+
+    """
+    git_cmd = list(cmd)
+    git_cmd.insert(0, "git")
+    options = dict()
+    if not raises:
+        options["stdout"] = subprocess.PIPE
+        options["stderr"] = subprocess.STDOUT
+
+    ui.debug(ui.lightgray, working_path, "$", ui.reset, *git_cmd)
+    process = subprocess.Popen(git_cmd, cwd=working_path, **options)
+
+    if raises:
+        process.wait()
+    else:
+        out, _ = process.communicate()
+        out = out.decode("utf-8")
+
+    returncode = process.returncode
+    if raises:
+        if returncode != 0:
+            raise GitCommandError(working_path, cmd)
+    else:
+        if out.endswith('\n'):
+            out = out.strip('\n')
+        ui.debug(ui.lightgray, "[%i]" % returncode, ui.reset, out)
+        return returncode, out
+
+
+def get_current_branch(working_path):
+    cmd = ("rev-parse", "--abbrev-ref", "HEAD")
+    status, output = run_git(working_path, *cmd, raises=False)
+    if status != 0:
+        raise GitCommandError(working_path, cmd, output=output)
+
+    return output
+
+
+def get_current_ref(working_path):
+    cmd = ("rev-parse", "HEAD")
+    status, output = run_git(working_path, *cmd, raises=False)
+    if status != 0:
+        raise GitCommandError(working_path, cmd, output=output)
+
+    return output
+
+
+def get_repo_root():
+    working_path = path.Path(os.getcwd())
+    cmd = ("rev-parse", "--show-toplevel")
+    status, output = run_git(working_path, *cmd, raises=False)
+    if status != 0:
+        raise WorktreeNotFound(working_path)
+    return path.Path(output)
+
+
+def is_dirty(working_path):
+    status, _ = run_git(working_path, "diff-index", "--quiet", "HEAD", raises=False)
+    return status != 0
+
+
+def find_ref(repo, candidate_refs):
+    """ Find the first reference that exists in the given repo """
+    run_git(repo, "fetch", "--all", "--prune")
+    for candidate_ref in candidate_refs:
+        code, _ = run_git(repo, "rev-parse", candidate_ref, raises=False)
+        if code == 0:
+            return candidate_ref
+    ref_list = ", ".join(candidate_refs)
+    raise GitError("Could not find any of:", ref_list, "in repo", repo)
+
+
+def reset(repo, ref):
+    ui.info_2("Resetting", repo, "to", ref)
+    run_git(repo, "reset", "--hard", ref)
+
+
+def get_status(working_path):
+    _, out = run_git(working_path, "status", "--porcelain", raises=False)
+    for line in out.splitlines():
+        if line.startswith("??"):
+            return "untracked files"
+        if line.startswith(" M"):
+            return "modified files"
+        if line.startswith("A "):
+            return "non-committed files"
+    return "clean"
