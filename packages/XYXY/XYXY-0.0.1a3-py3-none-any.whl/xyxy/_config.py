1377795f93abd6c81a5c04450ba9327f60a06f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:05 +0000

    Add package XYXY/0.0.1a3/XYXY-0.0.1a3-py3-none-any.whl

---
+++ packages/XYXY/XYXY-0.0.1a3-py3-none-any.whl/xyxy/_config.py
@@ -0,0 +1,17 @@
+import os
+
+TYPE = 'dict'
+BATCH_SIZE = 32
+
+DATA_FILE_NAME = 'data.json'
+
+API_URL = 'http://api.xyxy.io/' if not os.environ.get('XYXY') else os.environ.get('XYXY_API_URL')
+BASE_PATH = '~/.xyxy' if not os.environ.get('XYXY_HOME_PATH') else os.environ.get('XYXY_HOME_PATH')
+
+MONGO_URI = None
+MONGO_DBNAME = 'xyxy_datasets'
+
+MONGO_DB = None
+
+MAX_INMEMORY_FILE_SIZE = 1024  # MB
+MAX_AWAIT_TIME = 128  # SEC
