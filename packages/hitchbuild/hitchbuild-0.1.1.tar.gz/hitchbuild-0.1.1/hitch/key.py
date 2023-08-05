9041a409b4563b983f05146a39e75dc3e8ddec58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:05 +0000

    Add package hitchbuild/0.1.1/hitchbuild-0.1.1.tar.gz

---
+++ packages/hitchbuild/hitchbuild-0.1.1.tar.gz/hitchbuild-0.1.1/hitch/key.py
@@ -0,0 +1,214 @@
+from hitchstory import StoryCollection, StorySchema, BaseEngine, exceptions, validate
+from hitchrun import hitch_maintenance, expected, DIR
+from pathquery import pathq
+from strictyaml import MapPattern, Str, Int
+from commandlib import python, Command
+import hitchpython
+import strictyaml
+import hitchtest
+from hitchrunpy import ExamplePythonCode
+
+
+class Engine(BaseEngine):
+    schema = StorySchema(
+        preconditions={
+            "files": MapPattern(Str(), Str()),
+            "variables": MapPattern(Str(), Str()),
+            "python version": Str(),
+            "build.py": Str(),
+            "sourcefile.txt": Str(),
+            "setup": Str(),
+        },
+        params={
+            "python version": Str(),
+        },
+    )
+
+    def __init__(self, paths, settings):
+        self.path = paths
+        self.settings = settings
+
+    def set_up(self):
+        self.path.state = self.path.gen.joinpath("state")
+
+        if self.path.state.exists():
+            self.path.state.rmtree(ignore_errors=True)
+        self.path.state.mkdir()
+
+        for filename in ["build.py", "sourcefile.txt", ]:
+            if filename in self.preconditions:
+                filepath = self.path.state.joinpath(filename)
+                if not filepath.dirname().exists():
+                    filepath.dirname().mkdir()
+                filepath.write_text(str(self.preconditions[filename]))
+
+        self.path.key.joinpath("code_that_does_things.py").copy(self.path.state)
+
+        self.python_package = hitchpython.PythonPackage(
+            self.preconditions.get('python_version', self.preconditions['python version'])
+        )
+        self.python_package.build()
+
+        self.pip = self.python_package.cmd.pip
+        self.python = self.python_package.cmd.python
+
+        with hitchtest.monitor([self.path.key.joinpath("debugrequirements.txt")]) as changed:
+            if changed:
+                self.pip("install", "-r", "debugrequirements.txt").in_dir(self.path.key).run()
+
+        self.pip("uninstall", "hitchbuild", "-y").ignore_errors().run()
+        self.pip("install", ".").in_dir(self.path.project).run()
+
+    def run_code(self, code):
+        ExamplePythonCode(code).with_setup_code(self.preconditions.get('setup', ''))\
+                               .run(self.path.state, self.python)
+
+    def exception_raised_with(self, code, exception_type, message):
+        result = ExamplePythonCode(code).with_setup_code(self.preconditions.get('setup', ''))\
+                                        .expect_exceptions()\
+                                        .run(self.path.state, self.python)
+        result.exception_was_raised(exception_type, message)
+
+    def run(self, command):
+        self.ipython_step_library.run(command)
+
+    def touch_file(self, filename):
+        self.path.state.joinpath(filename).write_text("\nfile touched!", append=True)
+
+    def _will_be(self, content, text, reference, changeable=None):
+        if text is not None:
+            if content.strip() == text.strip():
+                return
+            else:
+                raise RuntimeError("Expected to find:\n{0}\n\nActual output:\n{1}".format(
+                    text,
+                    content,
+                ))
+
+        artefact = self.path.key.joinpath(
+            "artefacts", "{0}.txt".format(reference.replace(" ", "-").lower())
+        )
+
+        from simex import DefaultSimex
+        simex = DefaultSimex(
+            open_delimeter="(((",
+            close_delimeter=")))",
+        )
+
+        simex_contents = content
+
+        if changeable is not None:
+            for replacement in changeable:
+                simex_contents = simex.compile(replacement).sub(replacement, simex_contents)
+
+        if not artefact.exists():
+            artefact.write_text(simex_contents)
+        else:
+            if self.settings.get('overwrite artefacts'):
+                artefact.write_text(simex_contents)
+                print(content)
+            else:
+                if simex.compile(artefact.bytes().decode('utf8')).match(content) is None:
+                    raise RuntimeError("Expected to find:\n{0}\n\nActual output:\n{1}".format(
+                        artefact.bytes().decode('utf8'),
+                        content,
+                    ))
+                else:
+                    print(content)
+
+    def file_contents_will_be(self, filename, text=None, reference=None, changeable=None):
+        output_contents = self.path.state.joinpath(filename).bytes().decode('utf8')
+        self._will_be(output_contents, text, reference, changeable)
+
+    def output_will_be(self, text=None, reference=None, changeable=None):
+        output_contents = self.path.state.joinpath("output.txt").bytes().decode('utf8')
+        self._will_be(output_contents, text, reference, changeable)
+
+    @validate(seconds=Int())
+    def sleep(self, seconds):
+        import time
+        time.sleep(int(seconds))
+
+
+@expected(strictyaml.exceptions.YAMLValidationError)
+@expected(exceptions.HitchStoryException)
+def tdd(*words):
+    """
+    Run test with words.
+    """
+    print(
+        StoryCollection(
+            pathq(DIR.key).ext("story"), Engine(DIR, {"overwrite artefacts": True})
+        ).shortcut(*words).play().report()
+    )
+
+
+def regression():
+    """
+    Run all stories.
+    """
+    lint()
+    print(
+        StoryCollection(
+            pathq(DIR.key).ext("story"), Engine(DIR, {})
+        ).ordered_by_name().play().report()
+    )
+
+
+def hitch(*args):
+    """
+    Use 'h hitch --help' to get help on these commands.
+    """
+    hitch_maintenance(*args)
+
+
+def lint():
+    """
+    Lint all code.
+    """
+    python("-m", "flake8")(
+        DIR.project.joinpath("hitchbuild"),
+        "--max-line-length=100",
+        "--exclude=__init__.py",
+    ).run()
+    python("-m", "flake8")(
+        DIR.key.joinpath("key.py"),
+        "--max-line-length=100",
+    ).run()
+    print("Lint success!")
+
+
+def deploy(version):
+    """
+    Deploy to pypi as specified version.
+    """
+    NAME = "hitchbuild"
+    git = Command("git").in_dir(DIR.project)
+    version_file = DIR.project.joinpath("VERSION")
+    old_version = version_file.bytes().decode('utf8')
+    if version_file.bytes().decode("utf8") != version:
+        DIR.project.joinpath("VERSION").write_text(version)
+        git("add", "VERSION").run()
+        git("commit", "-m", "RELEASE: Version {0} -> {1}".format(
+            old_version,
+            version
+        )).run()
+        git("push").run()
+        git("tag", "-a", version, "-m", "Version {0}".format(version)).run()
+        git("push", "origin", version).run()
+    else:
+        git("push").run()
+
+    # Set __version__ variable in __init__.py, build sdist and put it back
+    initpy = DIR.project.joinpath(NAME, "__init__.py")
+    original_initpy_contents = initpy.bytes().decode('utf8')
+    initpy.write_text(
+        original_initpy_contents.replace("DEVELOPMENT_VERSION", version)
+    )
+    python("setup.py", "sdist").in_dir(DIR.project).run()
+    initpy.write_text(original_initpy_contents)
+
+    # Upload to pypi
+    python(
+        "-m", "twine", "upload", "dist/{0}-{1}.tar.gz".format(NAME, version)
+    ).in_dir(DIR.project).run()
---
+++ packages/hitchbuild/hitchbuild-0.1.1.tar.gz/hitchbuild-0.1.1/hitchbuild/build.py
@@ -0,0 +1,93 @@
+from hitchbuild.monitor import Monitor
+from copy import copy
+
+
+class HitchBuild(object):
+    _built_if_exists = False
+    _requirements = {}
+
+    def __init__(self):
+        self._name = None
+        self._manually_triggered = False
+        self._sqlite_filename = None
+
+    def exists(self):
+        raise NotImplemented()
+
+    def build(self):
+        raise NotImplementedError("build method must be implemented")
+
+    def trigger(self):
+        raise NotImplementedError("trigger method must be implemented")
+
+    @property
+    def monitor(self):
+        return Monitor(self._name, self._sqlite_filename)
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def name(self):
+        return self._name
+
+    def in_path(self, path):
+        new_build = copy(self)
+        new_build._path = path
+        return new_build
+
+    def with_db(self, sqlite_filename):
+        new_build = copy(self)
+        new_build._sqlite_filename = sqlite_filename
+        return new_build
+
+    def with_name(self, name):
+        new_build = copy(self)
+        new_build._name = name
+        return new_build
+
+    def manually_triggered(self):
+        new_build = copy(self)
+        new_build._manually_triggered = True
+        return new_build
+
+    def requirement(self, **requirements):
+        new_build = copy(self)
+        new_build._requirements = requirements
+        return new_build
+
+    def ensure_built(self):
+        requirement_triggered = False
+
+        for requirement in self._requirements.values():
+            if requirement.monitor.build_model.was_triggered_on_last_run:
+                requirement_triggered = True
+
+        for requirement in self._requirements.values():
+            if requirement.ensure_built():
+                requirement_triggered = True
+
+        trigger_check = self.trigger().check()
+
+        triggered = (
+            trigger_check or
+            self.monitor.last_run_had_exception or
+            self._manually_triggered or
+            requirement_triggered
+        )
+
+        self._manually_triggered = False
+
+        if triggered:
+            with self.monitor.context_manager():
+                self.build()
+
+        model = self.monitor.build_model
+        model.was_triggered_on_last_run = triggered
+        model.save()
+
+        return triggered
+
+    def __repr__(self):
+        return """HitchBuild("{0}")""".format(self._name)
