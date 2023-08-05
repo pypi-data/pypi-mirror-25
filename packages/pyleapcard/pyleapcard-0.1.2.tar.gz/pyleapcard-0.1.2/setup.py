29672a5c662f01a91b9a84eacde7f1a6bd117a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:03 +0000

    Add package pyleapcard/0.1.2/pyleapcard-0.1.2.tar.gz

---
+++ packages/pyleapcard/pyleapcard-0.1.2.tar.gz/pyleapcard-0.1.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyleapcard',
+      version='0.1.2',
+      description='Python Leap Card API',
+      long_description='See project page with usage examples at https://github.com/skhg/pyleapcard',
+      keywords='leap card public transport smart card api travel ireland',
+      author='Jack Higgins',
+      author_email='pypi@jackhiggins.ie',
+      url='https://github.com/skhg/pyleapcard',
+      packages=['pyleapcard'],
+      install_requires=[
+          'requests',
+          'bs4'
+      ],
+      tests_require = [
+	  	'mock',
+	    'nose',
+	  ],
+      license='MIT'
+      )
