da82cd93508279904e591f899712fe3eddefb4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:45 +0000

    Add package nayesdog/0.1.5/nayesdog-0.1.5.tar.gz

---
+++ packages/nayesdog/nayesdog-0.1.5.tar.gz/nayesdog-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+version = '0.1.5'
+setup(
+    name = 'nayesdog',
+    packages = ['nayesdog'],
+    version = version,
+    description = 'RSS reader with Naive Bayes powered recommendations',
+    author = 'Ilya Prokin, Sergio Peignier',
+    author_email = 'isprokin@gmail.com, sergio.peignier.zapata@gmail.com',
+    url = 'http://github.com/MLdog/nayesdog',
+    download_url = 'https://github.com/MLdog/nayesdog/archive/v{}.tar.gz'.format(version),
+    keywords = ['RSS reader', 'Naive Bayes', 'recommendations'],
+    classifiers = [],
+    #package_data = {'nayesdog':['icons/*','css.css','stopwords.txt']},
+    include_package_data=True,
+    entry_points = """
+      [console_scripts]
+      nayesdog = nayesdog.command_line:main
+    """,
+    install_requires=['feedparser', 'beautifulsoup4'],
+    #scripts=['bin/nayesdog'],
+)
