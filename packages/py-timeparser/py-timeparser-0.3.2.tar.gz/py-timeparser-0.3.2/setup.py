83b510382c46f1b2cc9cb88cfae8ee5f5c302c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:00 +0000

    Add package py-timeparser/0.3.2/py-timeparser-0.3.2.tar.gz

---
+++ packages/py-timeparser/py-timeparser-0.3.2.tar.gz/py-timeparser-0.3.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='py-timeparser',
+      version='0.3.2',
+      description='Parse time string (e.g. 1h5min)',
+      author='utgwkk',
+      author_email='utagawa.kiki@gmail.com',
+      url='https://github.com/utgwkk/py-timeparser',
+      py_modules=['timeparser'],
+      install_requires=[],
+      license='MIT'
+      )
---
+++ packages/py-timeparser/py-timeparser-0.3.2.tar.gz/py-timeparser-0.3.2/timeparser.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+from __future__ import print_function
+import re
+from datetime import timedelta
+
+
+class ParseError(Exception):
+    pass
+
+
+def parse(s, return_type=int):
+    '''
+    Parse time string and return seconds.
+    You can specify the type of return value both int or datetime.timedelta
+    with 'return_type' argument.
+    '''
+    RE_DAY = r'([0-9]+)d(ay)?'
+    RE_HOUR = r'([0-9]+)h(our)?'
+    RE_MINUTE = r'([0-9]+)m(in(ute)?)?'
+    RE_SECOND = r'([0-9]+)(s(ec(ond)?)?)?'
+
+    def _parse_time_with_unit(s):
+        retval = 0
+        md = re.match(RE_DAY, s)
+        mh = re.match(RE_HOUR, s)
+        mm = re.match(RE_MINUTE, s)
+        ms = re.match(RE_SECOND, s)
+        if md:
+            retval = 86400 * int(md.group(1))
+        elif mh:
+            retval = 3600 * int(mh.group(1))
+        elif mm:
+            retval = 60 * int(mm.group(1))
+        elif ms:
+            retval = int(ms.group(1))
+        return retval
+
+    if isinstance(s, (type(None), int, float)):
+        return s
+
+    if s[-1] in '0123456789':
+        s += 's'
+
+    m = re.match(r'^(%s)?(%s)?(%s)?(%s)?$' % (RE_DAY, RE_HOUR,
+                                              RE_MINUTE, RE_SECOND),
+                 s)
+    if not m:
+        raise ParseError('invalid string: "%s"' % s)
+
+    times = [x for x in m.groups() if isinstance(x, str) and
+             re.match(r'[0-9]+[a-z]+', x)]
+    seconds = sum(_parse_time_with_unit(z) for z in times)
+
+    if return_type is int:
+        return seconds
+    elif return_type is timedelta:
+        return timedelta(seconds=seconds)
+    else:
+        raise TypeError('return_type "{}" is not supported.'.format(
+            return_type.__name__))
+
+if __name__ == '__main__':
+    print(parse('8minute10'))
