a5e44dca7c823376a03e3e4a0ca1630bbbf279b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:32 +0000

    Add package helloWorldFredyTrees/1.0.0/helloWorldFredyTrees-1.0.0.tar.gz

---
+++ packages/helloWorldFredyTrees/helloWorldFredyTrees-1.0.0.tar.gz/helloWorldFredyTrees-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'helloWorldFredyTrees',
+version = '1.0.0',
+py_modules = ['helloWorld'],
+author = 'namanyafredy',
+author_email = 'gausah@yahoo.com',
+url = 'http://www.treessolutions.com',
+description = 'Latihan python,padahal consultant oracle',
+)
