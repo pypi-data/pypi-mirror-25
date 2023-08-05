365978ae7d8b64000f0b2d850f0b874337106538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:08 +0000

    Add package antshell/0.4.11/antshell-0.4.11.tar.gz

---
+++ packages/antshell/antshell-0.4.11.tar.gz/antshell-0.4.11/lib/antshell/__init__.py
@@ -0,0 +1,11 @@
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from antshell.release import __prog__, __version__, __author__
+try:
+    from antshell import lang
+    from antshell import base
+    from antshell.AntShell import main
+except:
+    pass
