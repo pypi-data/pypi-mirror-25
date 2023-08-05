edb268df111fab160b0085c5fd90279e87577493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:27 +0000

    Add package py-tools-ds/0.7.0/py_tools_ds-0.7.0.tar.gz

---
+++ packages/py-tools-ds/py_tools_ds-0.7.0.tar.gz/py_tools_ds-0.7.0/py_tools_ds/convenience/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+__author__ = "Daniel Scheffler"
+
+from . import object_oriented
+
+__all__=['object_oriented']
---
+++ packages/py-tools-ds/py_tools_ds-0.7.0.tar.gz/py_tools_ds-0.7.0/py_tools_ds/geo/vector/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+__author__ = "Daniel Scheffler"
+
+from . import geometry
+from . import topology
+from . import conversion
+
+__all__=['geometry',
+         'topology',
+         'conversion']
