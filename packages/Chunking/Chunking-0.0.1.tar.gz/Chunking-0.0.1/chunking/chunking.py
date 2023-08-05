0e9497ab7bf450134679f44b2be418297a949ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:19 +0000

    Add package Chunking/0.0.1/Chunking-0.0.1.tar.gz

---
+++ packages/Chunking/Chunking-0.0.1.tar.gz/Chunking-0.0.1/chunking/chunking.py
@@ -0,0 +1,49 @@
+def _iter(iterable, sentinel):
+    x = iter(iterable)
+    for item in x:
+        yield item
+    yield sentinel
+
+
+def split(iterable, sentinel):
+    """
+    Split any arbitrary iterable into list items separated by a sentinel value.
+    :param iterable: any iterable
+    :param sentinel: value to split on
+    :returns: a list of chunks
+    >>> a_list = ["foo", 'bar', 'SENTINEL', 'bacon', 'eggs']
+    >>> split(a_list, 'SENTINEL')
+    [['foo', 'bar'], ['bacon', 'eggs']]
+    """
+    return list(iter_split(iterable, sentinel))
+
+def iter_split(iterable, sentinel):
+    """
+    Split any arbitrary iterable into list items separated by a sentinel value. Generator used by split
+    :param iterable: any iterable
+    :param sentinel: value to split on
+    :yields: chunks
+    """
+    x = _iter(iterable, sentinel)
+    for item in x:
+        chunk = []
+        while item != sentinel:
+            chunk.append(item)
+            item = next(x)
+        yield chunk
+
+
+def chunk(iterable, chunk_size):
+    """
+    Yield successive n-sized chunks from an iterable.
+    If the iterable does not fit evenly into chunk_size, the last chunk will be smaller than chunk_size.
+    >>> r = range(5)
+    >>> for c in chunk(r, 2):
+    ...     print(c)
+    (0, 1)
+    (2, 3)
+    (4,)
+    """
+    x = iter(iterable)
+    for item in x:
+        yield (item, *(next(x) for _ in range(chunk_size - 1)))
---
+++ packages/Chunking/Chunking-0.0.1.tar.gz/Chunking-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='Chunking',
+    version='0.0.1',
+    url='https://github.com/spyoungtech/chunking/',
+    license='MIT',
+    author='Spencer Young',
+    author_email='spencer.young@spyoung.com',
+    description='Tools for chunking iterables',
+    packages=['chunking'],
+    platforms='any',
+    install_requires=[],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
