353d712840e60b228ee9487b6718ee67438e943f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:31 +0000

    Add package pycreate2/0.7.3/pycreate2-0.7.3-py3-none-any.whl

---
+++ packages/pycreate2/pycreate2-0.7.3-py3-none-any.whl/pycreate2/__init__.py
@@ -0,0 +1,13 @@
+# The MIT License
+#
+# Copyright (c) 2017 Kevin Walchko
+
+from __future__ import print_function
+from __future__ import division
+from .create2api import Create2
+# from .create2api import Fatal, Error, Warning
+
+from .version import __version__
+
+__license__ = 'MIT'
+__author__ = 'Kevin Walchko'
