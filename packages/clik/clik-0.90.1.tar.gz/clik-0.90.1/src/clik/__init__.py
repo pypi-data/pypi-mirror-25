28101d29934f063a3f3a19d5cf2e8d7f424fd108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:59 +0000

    Add package clik/0.90.1/clik-0.90.1.tar.gz

---
+++ packages/clik/clik-0.90.1.tar.gz/clik-0.90.1/src/clik/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+The command line interface kit.
+
+Clik is a tool for writing complex command-line interfaces with minimal
+boilerplate and bookkeeping.
+
+This top-level package pulls together the end user API from the various
+modules within clik.
+
+:author: Joe Joyce <joe@decafjoe.com>
+:copyright: Copyright (c) Joe Joyce and contributors, 2009-2017.
+:license: BSD
+"""
+#: Current version.
+#:
+#: :type: :class:`str`
+__version__ = '0.90.1'
+
+
+# LINT: Ignore unused import violations. This module isn't meant to "do"
+#       anything, just to make clik's API available to the end user.
+from clik.app import app, args, current_app, g, parser, run_children  # noqa
+from clik.command import catch  # noqa: F401
