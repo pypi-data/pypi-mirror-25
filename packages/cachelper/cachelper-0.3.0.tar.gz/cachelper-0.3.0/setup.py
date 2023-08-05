f1be02ef476ede78cd19f0d3f9077f24c9979bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:11 +0000

    Add package cachelper/0.3.0/cachelper-0.3.0.tar.gz

---
+++ packages/cachelper/cachelper-0.3.0.tar.gz/cachelper-0.3.0/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+
+setup(
+    name='cachelper',
+    version='0.3.0',
+    description='A collection of cache helpers',
+    long_description=long_description,
+    url='https://github.com/suzaku/cachelper',
+    license='MIT',
+    author='Satoru Logic',
+    author_email='satorulogic@gmail.com',
+    packages=['cachelper'],
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='cache',
+)
