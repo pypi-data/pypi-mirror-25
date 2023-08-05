5c86111ab15b45e594fb0f9475f121d8b6ed11be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:53 +0000

    Add package ananas-doc/1.0/ananas-doc-1.0.tar.gz

---
+++ packages/ananas-doc/ananas-doc-1.0.tar.gz/ananas-doc-1.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(name='ananas-doc',
+      version=1.0,
+      description=u"docs tools",
+      long_description="",
+      classifiers=[],
+      keywords='docs tools',
+      author="Lonerusn",
+      author_email="yang.guo.sun@gmail.com",
+      url='https://github.com/Lonersun/ananas-doc',
+      license='Apache License 2.0',
+      platforms='any',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'sphinx>=1.6.3',
+          'sphinx-rtd-theme>=0.2.4',
+          'm2r>=0.1.12',
+          'pyaml>=3.12',
+      ],
+)
+
