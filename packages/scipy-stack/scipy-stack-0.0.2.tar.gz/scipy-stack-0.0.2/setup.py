35914221728c6f390805dbc73a6eae4774e74cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:45 +0000

    Add package scipy-stack/0.0.2/scipy-stack-0.0.2.tar.gz

---
+++ packages/scipy-stack/scipy-stack-0.0.2.tar.gz/scipy-stack-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name="scipy-stack",
+    version="0.0.2",
+    packages=find_packages(),
+
+    install_requires=[
+        'numpy>=1.13.1',
+        'scipy>=0.19.1',
+        'matplotlib>=2.0.2',
+        'jupyter>=1.0.0',
+        'pandas>=0.20.3',
+        'sympy>=1.1.1',
+        'nose>=1.3.7',
+    ],
+
+    author="",
+    author_email="",
+    url='',
+    description="Helper package to install the scipy stack",
+    long_description=readme,
+    license="Apache 2.0",
+    keywords="scipy-stack, scipy, numpy",
+)
