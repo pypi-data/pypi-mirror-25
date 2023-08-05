109bb5c62ad42b4edbb61bc08eb05ea1a0e9c75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:25 +0000

    Add package TagScriptEngine/0.3/TagScriptEngine-0.3.tar.gz

---
+++ packages/TagScriptEngine/TagScriptEngine-0.3.tar.gz/TagScriptEngine-0.3/TagScriptEngine/engine.py
@@ -0,0 +1,32 @@
+from .random import RandomFilter
+from .variable import VariableFilter
+from .math import MathEvaluationFilter
+
+
+class Engine():
+    """Engine is the core of this library, it is what you will instantiate to process text script."""
+    def __init__(self):
+        self.variable_bin = {}
+
+        self.random = RandomFilter()
+        self.var = VariableFilter()
+        self.math = MathEvaluationFilter()
+
+    def Set_Variables(self, vars):
+        self.variable_bin = vars
+
+    def Clear_Variables(self):
+        self.variable_bin = {}
+
+    def Add_Variable(self, name, val):
+        self.variable_bin[name] = val
+
+    def Process(self, text):
+        value = text
+
+
+        value = self.random.Process(self, value)
+        value = self.var.Process(self, value)
+        value = self.math.Process(self, value)
+
+        return value
