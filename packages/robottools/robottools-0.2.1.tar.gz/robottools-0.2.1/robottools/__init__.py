9ef9e8277d447fec16af03105d553995938ec3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:32 +0000

    Add package robottools/0.2.1/robottools-0.2.1.tar.gz

---
+++ packages/robottools/robottools-0.2.1.tar.gz/robottools-0.2.1/robottools/__init__.py
@@ -0,0 +1,5 @@
+__title__ = 'robottools'
+__version__ = '0.2.1'
+
+from robottools.modifiers.rename import rename, resetname, rerunrenamedtests, rerunrenamedsuites, unname  # noqa
+from robottools.reporters.testlink import testlink  # noqa
