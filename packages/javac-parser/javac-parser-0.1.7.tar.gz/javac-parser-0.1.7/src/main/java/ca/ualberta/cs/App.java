c796af38f8cdd0175941b23bc15873d160d096ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:50 +0000

    Add package javac-parser/0.1.7/javac-parser-0.1.7.tar.gz

---
+++ packages/javac-parser/javac-parser-0.1.7.tar.gz/javac-parser-0.1.7/src/main/java/ca/ualberta/cs/App.java
@@ -0,0 +1,29 @@
+package ca.ualberta.cs;
+
+import ca.ualberta.cs.ParserWrapper;
+import ca.ualberta.cs.Source;
+
+import py4j.GatewayServer;
+
+public class App 
+{
+    public int getNumParseErrors(String javaSource) {
+        return (new ParserWrapper()).numErrors(javaSource);
+    }
+    
+    public Source lex(String javaSource) {
+        return (new ParserWrapper()).lexIt(javaSource);
+    }
+    
+    public Source checkSyntax(String javaSource) {
+        return (new ParserWrapper()).checkSyntax(javaSource);
+    }
+
+    public static void main( String[] args )
+    {
+        GatewayServer gatewayServer = new GatewayServer(new App());
+        gatewayServer.start();
+//         System.out.println("Java Lexer/Parser Server Started");
+    }
+}
+
