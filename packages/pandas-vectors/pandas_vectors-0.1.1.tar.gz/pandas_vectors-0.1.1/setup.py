f00535cb1b15405d63d525458a9e55c38ef891f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:15 +0000

    Add package pandas-vectors/0.1.1/pandas_vectors-0.1.1.tar.gz

---
+++ packages/pandas-vectors/pandas_vectors-0.1.1.tar.gz/pandas_vectors-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='pandas_vectors',
+    version='0.1.1',
+    description='convenience functions for dealing with vectors in panda dataframes',
+    long_description=readme,
+    author='Richard Joyce',
+    author_email='rjoyce@ucdavis.edu',
+    url='https://github.com/richjoyce/pandas_vectors',
+    license='MIT',
+    py_modules=['pandas_vectors']
+)
