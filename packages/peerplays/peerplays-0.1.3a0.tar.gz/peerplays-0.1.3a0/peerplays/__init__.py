dc3e4ac4112420dc395fe584b058d892aeb8ba40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:47 +0000

    Add package peerplays/0.1.3a0/peerplays-0.1.3a0.tar.gz

---
+++ packages/peerplays/peerplays-0.1.3a0.tar.gz/peerplays-0.1.3a0/peerplays/__init__.py
@@ -0,0 +1,31 @@
+from .peerplays import PeerPlays
+from graphenebase import base58
+
+__all__ = [
+    "account",
+    "aes",
+    "amount",
+    "asset",
+    "bet",
+    "bettingmarket",
+    "bettingmarketgroup",
+    "block",
+    "blockchain",
+    "committee",
+    "event",
+    "eventgroup",
+    "exceptions",
+    "instance",
+    "memo",
+    "peerplays",
+    "proposal",
+    "sport",
+    "storage",
+    "transactionbuilder",
+    "utils",
+    "wallet",
+    "witness",
+    "notify",
+]
+base58.known_prefixes.append("PPY")
+base58.known_prefixes.append("PPY1")
