0ec42f279361cd8c9ec5a2a3b5d804362f744ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:53 +0000

    Add package studioml/0.0.3.post2/studioml-0.0.3.post2.tar.gz

---
+++ packages/studioml/studioml-0.0.3.post2.tar.gz/studioml-0.0.3.post2/studio/tests/http_provider_test.py
@@ -0,0 +1,96 @@
+import unittest
+import subprocess
+import time
+from random import randint
+import os
+
+from studio import model
+from model_test import get_test_experiment
+
+try:
+    import boto3
+except ImportError:
+    boto3 = None
+
+
+@unittest.skipIf(boto3 is None,
+                 "boto3 module is missing, needed for " +
+                 "server to communicate with storage")
+class HTTPProviderTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        if boto3 is None:
+            return
+        print "Starting up the API server"
+        self.port = randint(5000, 9000)
+
+        # self.app.run(port=self.port, debug=True)
+        # self.serverp.start()
+        self.server_config_file = os.path.join(
+            os.path.dirname(
+                os.path.realpath(__file__)),
+            'test_config_http_server.yaml')
+
+        self.client_config_file = os.path.join(
+            os.path.dirname(
+                os.path.realpath(__file__)),
+            'test_config_http_client.yaml')
+
+        self.serverp = subprocess.Popen([
+            'studio-ui',
+            '--port=' + str(self.port),
+            '--verbose=debug',
+            '--config=' + self.server_config_file,
+            '--host=localhost'])
+
+        time.sleep(25)
+
+    @classmethod
+    def tearDownClass(self):
+        if boto3 is None:
+            return
+
+        print "Shutting down the API server"
+        self.serverp.kill()
+
+    def get_db_provider(self):
+        config = model.get_config(self.client_config_file)
+        config['database']['serverUrl'] = 'http://localhost:' + str(self.port)
+        return model.get_db_provider(config)
+
+    def test_add_get_experiment(self):
+        experiment_tuple = get_test_experiment()
+        db = self.get_db_provider()
+        db.add_experiment(experiment_tuple[0])
+
+        experiment = db.get_experiment(experiment_tuple[0].key)
+        self.assertEquals(experiment.key, experiment_tuple[0].key)
+        self.assertEquals(experiment.filename, experiment_tuple[0].filename)
+        self.assertEquals(experiment.args, experiment_tuple[0].args)
+
+        db.delete_experiment(experiment_tuple[1])
+
+    def test_start_experiment(self):
+        db = self.get_db_provider()
+        experiment_tuple = get_test_experiment()
+
+        db.add_experiment(experiment_tuple[0])
+        db.start_experiment(experiment_tuple[0])
+
+        experiment = db.get_experiment(experiment_tuple[1])
+
+        self.assertTrue(experiment.status == 'running')
+        self.assertTrue(experiment.time_added <= time.time())
+        self.assertTrue(experiment.time_started <= time.time())
+
+        self.assertEquals(experiment.key, experiment_tuple[0].key)
+        self.assertEquals(experiment.filename, experiment_tuple[0].filename)
+        self.assertEquals(experiment.args, experiment_tuple[0].args)
+
+        db.finish_experiment(experiment_tuple[0])
+        db.delete_experiment(experiment_tuple[1])
+
+
+if __name__ == '__main__':
+    unittest.main()
