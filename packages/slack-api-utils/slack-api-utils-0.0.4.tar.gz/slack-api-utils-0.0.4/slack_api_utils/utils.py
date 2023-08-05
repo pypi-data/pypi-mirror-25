bc2a02c56456e081cde5ee0a2509d0fbad6c79d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:57 +0000

    Add package slack-api-utils/0.0.4/slack-api-utils-0.0.4.tar.gz

---
+++ packages/slack-api-utils/slack-api-utils-0.0.4.tar.gz/slack-api-utils-0.0.4/slack_api_utils/utils.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import time
+
+
+class TimeRange(object):
+    def __init__(self, start_time=None, end_time=None):
+        def parse_time(t):
+            try:
+                return unicode(time.mktime(time.strptime(t, "%Y%m%d")))
+            except (TypeError, ValueError):
+                return "0"
+
+        self.start_time = parse_time(start_time)
+        self.end_time = parse_time(end_time)
+        if self.end_time == "0":
+            self.end_time = unicode(time.time())
