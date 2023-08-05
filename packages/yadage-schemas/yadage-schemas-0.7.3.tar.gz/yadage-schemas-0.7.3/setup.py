e12537dd88b13b31f88095f1309e8a4473b05050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:35 +0000

    Add package yadage-schemas/0.7.3/yadage-schemas-0.7.3.tar.gz

---
+++ packages/yadage-schemas/yadage-schemas-0.7.3.tar.gz/yadage-schemas-0.7.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    author = 'Lukas Heinrich',
+    author_email = 'lukas.heinrich@gmail.com',
+    name = 'yadage-schemas',
+    version = '0.7.3',
+    description = 'schemas for yadage and packtivity',
+    include_package_data = True,
+    packages = find_packages(),
+    install_requires = [
+        'jsonref',
+        'pyyaml',
+        'requests[security]',
+        'jsonschema'
+    ]
+)
