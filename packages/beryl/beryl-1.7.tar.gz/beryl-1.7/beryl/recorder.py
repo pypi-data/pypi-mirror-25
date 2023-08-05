869a541165ac998392a59f2c6bae4248b017cac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:43 +0000

    Add package beryl/1.7/beryl-1.7.tar.gz

---
+++ packages/beryl/beryl-1.7.tar.gz/beryl-1.7/beryl/recorder.py
@@ -0,0 +1,57 @@
+from multiprocessing import Process
+from random import choice
+from subprocess import call, Popen
+from time import sleep
+
+def convert(input_path, output_path, async=False, silent=True):
+    print "starting convert"
+    _args = ["ffmpeg", "-y", "-i", input_path, output_path]
+
+    if async:
+        Popen(_args)
+    else:
+        call(_args)
+
+def start(path_to_video="/tmp/beryl_recording.ogv", delay=0, silent=True):
+    #return Popen("sleep " + str(delay) + " && recordmydesktop -o " + path_to_video, shell=True)
+    _args = ["recordmydesktop","-o",path_to_video]
+    return Popen(_args)
+
+def record(i, delay=0):
+    print "starting record with", i
+
+    if hasattr(i, "__call__"):
+        def wrapper(*args, **kwargs):
+            print "_wrapper"
+            func_name = i.func_name
+            path_to_ogv = "/tmp/" + func_name + ".ogv"
+            p = start(path_to_ogv, delay=delay)
+            result = i(*args, **kwargs)
+            p.terminate()
+            return result
+        return wrapper
+ 
+    elif isinstance(i, str) or isinstance(i, unicode):
+        _format = i.split(".")[-1]
+        if _format == "ogv":
+            shouldConvert = False
+            path_to_ogv = i
+        else:
+            shouldConvert = True
+            path_to_ogv = "/tmp/" + "".join([choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") for x in range(4)]) + ".ogv"
+ 
+        def outer_wrapper(f):
+            print "outer_wrapper with", f
+            print "i:", i
+            def inner_wrapper(*args, **kwargs):
+                print "inner_wrapper"
+                p = start(path_to_ogv, delay=delay)
+                result = f(*args, **kwargs)
+                p.terminate()
+                if shouldConvert:
+                    sleep(5)
+                    convert(path_to_ogv, i)
+                return result
+            return inner_wrapper
+        return outer_wrapper
+
---
+++ packages/beryl/beryl-1.7.tar.gz/beryl-1.7/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+  name = 'beryl',
+  packages = ['beryl'],
+  package_dir = {'beryl': 'beryl'},
+  package_data = {'beryl': ['__init__.py','recorder.py','cache/text.txt']},
+  version = '1.7',
+  description = 'Makes Writing Tests Super Easy and Quick',
+  author = 'Daniel J. Dufour',
+  author_email = 'daniel.j.dufour@gmail.com',
+  url = 'https://github.com/DanielJDufour/beryl',
+  download_url = 'https://github.com/DanielJDufour/beryl/tarball/download',
+  keywords = ['python', 'testing'],
+  classifiers = [],
+  install_requires=["editdistance","numpy","opencv-python","Pillow","psutil","pytesseract","pyvirtualdisplay","scipy","selenium"]
+)
