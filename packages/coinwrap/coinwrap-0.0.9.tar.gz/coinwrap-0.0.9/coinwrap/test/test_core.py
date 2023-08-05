b6c9547bbc7feaedd7db7ea39713e2ca4cea6efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:59 +0000

    Add package coinwrap/0.0.9/coinwrap-0.0.9.tar.gz

---
+++ packages/coinwrap/coinwrap-0.0.9.tar.gz/coinwrap-0.0.9/coinwrap/test/test_core.py
@@ -0,0 +1,9 @@
+from coinwrap import Market
+
+
+def test_core():
+    market = Market()
+    print '\ncoin()'
+    print market.coin('bitcoin', limit=1)
+    print '\nstats()'
+    print market.stats()
