97b98dd834a61de1c9662e2265de5220337e4a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:36 +0000

    Add package frc3223-azurite/0.0.1/frc3223-azurite-0.0.1.tar.gz

---
+++ packages/frc3223-azurite/frc3223-azurite-0.0.1.tar.gz/frc3223-azurite-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='frc3223-azurite',
+    version='0.0.1',
+    author='Ellery Newcomer',
+    author_email='ellery-newcomer@utulsa.edu',
+    url='https://github.com/Retro3223/azurite',
+    packages=[
+        'frc3223_azurite',
+    ],
+    description='miscellaneous utilities for jupyter notebooks',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
+
