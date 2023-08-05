8a89f50a85d9fa68b2b07b10d771a9fbe2ecc2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:04 +0000

    Add package dcos-shakedown/1.4.8/dcos-shakedown-1.4.8.tar.gz

---
+++ packages/dcos-shakedown/dcos-shakedown-1.4.8.tar.gz/dcos-shakedown-1.4.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+
+setup(name='dcos-shakedown',
+      version='1.4.8',
+      description=u"DC/OS testing harness and library",
+      long_description=u"A tool and library to abstract common DC/OS-related tasks.",
+      classifiers=[],
+      keywords='',
+      author=u"Mesosphere QE",
+      author_email='qe-team@mesosphere.com',
+      url='https://github.com/dcos/shakedown',
+      license='Apache 2',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'click',
+          'dcoscli==0.5.5',
+          'paramiko',
+          'pytest',
+          'pytest-timeout',
+          'retrying',
+          'scp'
+      ],
+      entry_points="""
+      [console_scripts]
+      shakedown=shakedown.cli.main:cli
+      dcos-shakedown=shakedown.cli.main:cli
+      """
+      )
