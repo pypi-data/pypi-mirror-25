1b1935da676ee0b4993703f7304a1e61148263b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:32 +0000

    Add package remoteAPI/1.0.3/remoteAPI-1.0.3.tar.gz

---
+++ packages/remoteAPI/remoteAPI-1.0.3.tar.gz/remoteAPI-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='remoteAPI',
+    packages=['remoteAPI'],
+    version='1.0.3',
+    description='A wrapper for python requests package customized to use in PISHKHAN',
+    author='Mohi Beyki',
+    author_email='mohi@cafebazaar.ir',
+    url='https://github.com/mohibeyki/remoteAPI.git',
+    download_url='https://github.com/mohibeyki/remoteAPI/archive/1.0.3.tar.gz',
+    keywords=['pishkhan', 'cafebazaar', 'remote'],
+    classifiers=[],
+)
