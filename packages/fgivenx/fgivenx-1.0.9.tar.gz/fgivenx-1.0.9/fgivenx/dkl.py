8b6d604129cb38280d584d5abea91740a500f6bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:45 +0000

    Add package fgivenx/1.0.9/fgivenx-1.0.9.tar.gz

---
+++ packages/fgivenx/fgivenx-1.0.9.tar.gz/fgivenx-1.0.9/fgivenx/dkl.py
@@ -0,0 +1,45 @@
+import tqdm
+import numpy
+from scipy.stats import gaussian_kde
+from fgivenx.io import CacheError, Cache
+from fgivenx.parallel import openmp_apply, mpi_apply, rank
+
+def dkl(arrays):
+    """ Compute the Kullback-Liebler divergence from samples from prior and posterior. """
+    samples, prior_samples = arrays
+    return (
+            gaussian_kde(samples).logpdf(samples) 
+            - gaussian_kde(prior_samples).logpdf(samples)
+            ).mean()
+
+
+def compute_dkl(x, fsamps, prior_fsamps, **kwargs):
+
+    parallel = kwargs.pop('parallel', '')
+    nprocs = kwargs.pop('nprocs', None)
+    cache = kwargs.pop('cache', None)
+
+    if cache is not None:
+        cache = Cache(cache + '_dkl')
+        try:
+            return cache.check(x, fsamps, prior_fsamps)  
+        except CacheError as e:
+            print(e.args[0])
+
+    zip_fsamps = list(zip(fsamps, prior_fsamps))
+
+    if parallel is '':
+        dkls = [dkl(arrays) for arrays in tqdm.tqdm(zip_fsamps)]
+    elif parallel is 'openmp':
+        dkls = openmp_apply(dkl, zip_fsamps, nprocs=nprocs)
+    else:
+        raise ValueError("keyword parallel=%s not recognised,"
+                         "options are 'openmp' or 'mpi'" % parallel)
+
+    dkls = numpy.array(dkls)
+
+    if cache is not None:
+        cache.data = x, fsamps, prior_fsamps, dkls
+
+    return dkls
+
