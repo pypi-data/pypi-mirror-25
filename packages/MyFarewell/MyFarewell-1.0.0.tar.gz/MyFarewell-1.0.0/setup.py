44241758516803f8e246608bae636a564946dc96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:13 +0000

    Add package MyFarewell/1.0.0/MyFarewell-1.0.0.tar.gz

---
+++ packages/MyFarewell/MyFarewell-1.0.0.tar.gz/MyFarewell-1.0.0/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='MyFarewell',
+    version="1.0.0",                 
+    description="さよなら2016年",
+    long_description="2016年にお別れを言うだけのパッケージ",
+    author='crea',
+    author_email='masataka0504@gmail.com',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ],
+    keywords='farewell'
+)
