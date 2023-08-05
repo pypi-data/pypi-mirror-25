f279a8e8f4df806cdbe2221f2e0daf23aa4cc7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:02 +0000

    Add package giaola-xml-delta/0.1.0/giaola_xml_delta-0.1.0-py2.py3-none-any.whl

---
+++ packages/giaola-xml-delta/giaola_xml_delta-0.1.0-py2.py3-none-any.whl/xml_delta/network/ping.py
@@ -0,0 +1,11 @@
+import requests
+
+def ping_url(url, timeout=10, proxy=None):
+    try :
+        requests.get(url,
+                     timeout=timeout,
+                     proxies=[proxy] if proxy else None)
+        return True
+    except Exception as e:
+        print(e.message)
+        return False
