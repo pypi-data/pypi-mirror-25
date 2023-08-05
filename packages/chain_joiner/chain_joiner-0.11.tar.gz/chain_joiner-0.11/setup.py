fc28b979a35ed1cea4b68fe5773d1a443d8336e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:09 +0000

    Add package chain_joiner/0.11/chain_joiner-0.11.tar.gz

---
+++ packages/chain_joiner/chain_joiner-0.11.tar.gz/chain_joiner-0.11/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'chain_joiner',
+  packages = ['chain_joiner'], # this must be the same as the name above
+  version = '0.11',
+  description = 'A package to automatically fix chain breaks in PDB files using MODELLER',
+  author = 'Matthew C. Robinson',
+  author_email = 'matthew.robinson@yale.edu',
+  url = 'https://github.com/mc-robinson/chain_joiner', # use the URL to the github repo,
+  keywords = [], # arbitrary keywords
+  classifiers = [
+	'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+	],
+  install_requires=['biopandas'],
+  entry_points={
+        'console_scripts': [
+            'join_chains=chain_joiner.chain_joiner:main',
+	    'make_seq=chain_joiner.make_seq:main',
+	    'make_alignment=chain_joiner.make_alignment:main',
+            'make_model=chain_joiner.make_model:main',
+            'chain_joiner_online=chain_joiner.run_chain_joiner_online:main'
+        ],
+    },
+
+)
