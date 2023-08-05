9edd99727a7d6c5db41b52c236e904999121aa1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:28 +0000

    Add package online-judge-tools/0.1.20/online-judge-tools-0.1.20.tar.gz

---
+++ packages/online-judge-tools/online-judge-tools-0.1.20.tar.gz/online-judge-tools-0.1.20/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+from onlinejudge.implementation.version import __author__, __email__, __license__, __url__, __version__
+
+with open('readme.md') as fh:
+    readme = fh.read()
+
+setup(
+    name='online-judge-tools',
+    version=__version__,
+    description='Tools for online-judge services',
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'colorama',
+        'sympy',
+    ],
+    long_description=readme,
+    author=__author__,
+    author_email=__email__,
+    url=__url__,
+    license=__license__,
+    packages=find_packages(exclude=( 'tests', 'docs' )),
+    scripts=[ 'oj' ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Utilities',
+    ],
+)
