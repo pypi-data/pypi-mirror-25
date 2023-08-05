3ecb964bab70b49dec0baed417b6d371ad752d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:56 +0000

    Add package generators/2017.9.9.2/generators-2017.9.9.2.tar.gz

---
+++ packages/generators/generators-2017.9.9.2.tar.gz/generators-2017.9.9.2/generators/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os.path
+
+sys.path.append(os.path.dirname(__file__))
+
+from all_substrings import all_substrings
+from average import average
+from chunks import chunks
+from counter import counter
+from fork import fork
+from itemgetter import itemgetter
+from iter_kv import iter_kv
+from multi_ops import multi_ops
+from read import read
+from read_file import read_file
+from started import started
+from tee import tee
+from timer import timer
+from total import total
+from unfork import unfork
+from window import window
+
+__all__ = ['all_substrings', 'average', 'chunks', 'counter', 'fork', 'itemgetter', 'iter_kv', 'multi_ops', 'read', 'read_file', 'started', 'tee', 'timer', 'total', 'unfork', 'window']
