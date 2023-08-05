6cb347be80ff8eb8e65e8e697b7e502f70236241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:25 +0000

    Add package papertrade/0.1.0/papertrade-0.1.0.tar.gz

---
+++ packages/papertrade/papertrade-0.1.0.tar.gz/papertrade-0.1.0/papertrade/commands.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+#    This file is part of 'papertrade'.
+#
+#    papertrade is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    papertrade is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with papertrade.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import papertrade as p
+import papertrade.database as db
+
+def main():
+    info = (
+        "    papertrade  Copyright (C) 2017  Svein-Kåre Bjørnsen\n"
+        "    This program comes with ABSOLUTELY NO WARRANTY.\n"
+        "    This is free software, and you are welcome to redistribute it\n"
+        "    under certain conditions; see\n"
+        "    https://github.com/morngrar/papertrade/blob/master/LICENSE.md\n"
+        "    for details.\n\n"
+    )
+    print("\n\n" + info)
+
+    if not os.path.exists(p.DBFILE):
+        db.generate(p.DBFILE, new=True)
+
+    p.main_menu()
