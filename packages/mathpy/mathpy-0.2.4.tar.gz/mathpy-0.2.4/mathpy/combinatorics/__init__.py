54ef22de37b8e62c3792cf0aa4968214f17e9013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:39 +0000

    Add package mathpy/0.2.4/mathpy-0.2.4.tar.gz

---
+++ packages/mathpy/mathpy-0.2.4.tar.gz/mathpy-0.2.4/mathpy/combinatorics/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+
+from mathpy.combinatorics.binomial import binom_coeff
+from mathpy.combinatorics.factorial import factorial, stirling, stirlingln, risingfactorial, fallingfactorial
