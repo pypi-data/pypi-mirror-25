0a3bfd316d6665dac5614273930cd1be17016315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:31 +0000

    Add package starforge/0.3.4/starforge-0.3.4.tar.gz

---
+++ packages/starforge/starforge-0.3.4.tar.gz/starforge-0.3.4/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from os.path import join
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+long_description += '\n\n'
+with open('HISTORY.rst') as file:
+    long_description += file.read()
+
+# Strip RTD :doc: links for PyPI
+long_description = long_description.replace(':doc:', '')
+
+init = join('starforge', '__init__.py')
+with open(init) as f:
+    exec(compile(f.read(), "somefile.py", 'exec'))
+
+
+setup(
+    name = 'starforge',
+    version = __version__,
+    packages = find_packages(),
+    description = 'Build Galaxy things in virtualized environments',
+    long_description = long_description,
+    url = 'https://github.com/galaxyproject/starforge',
+    author = 'The Galaxy Community',
+    author_email = 'galaxy-dev@lists.galaxyproject.org',
+    license = 'MIT',
+    keywords = 'starforge galaxy docker qemu',
+    package_data = {'': [
+        'config/default.yml',
+    ]},
+    install_requires = [
+        'pyyaml',
+        'click',
+        'requests',
+        'six',
+        'wheel'
+    ],
+    extras_require = {
+        ':python_version=="2.6"': ["ordereddict"],
+        'platform_specific': ["lionshead"],
+    },
+    entry_points = {
+        'console_scripts': [
+            'starforge = starforge.cli:starforge'
+        ]
+    },
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+    zip_safe = False
+)
