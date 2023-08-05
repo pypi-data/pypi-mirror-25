da692b73e46a62c4f434019e1cb73ca71425b449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:09 +0000

    Add package doct/1.0.5/doct-1.0.5.tar.gz

---
+++ packages/doct/doct-1.0.5.tar.gz/doct-1.0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='doct',
+    version='1.0.5',
+    author='Brookhaven National Laboratory',
+    license='BSD 3-Clause',
+    py_modules=['doct'],
+    description='A read-only dottable dictionary',
+    url='http://github.com/NSLS-II/doct',
+    install_requires=['six', 'humanize', 'prettytable']
+)
