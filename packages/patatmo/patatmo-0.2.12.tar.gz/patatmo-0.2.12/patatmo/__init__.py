0a3d215817df03629b58906d5aa4ee573cdf4168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:15 +0000

    Add package patatmo/0.2.12/patatmo-0.2.12.tar.gz

---
+++ packages/patatmo/patatmo-0.2.12.tar.gz/patatmo-0.2.12/patatmo/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# System modules
+
+# External modules
+
+# Internal modules
+from . import utils
+from . import api
+from . import cli
+
+__version__ = "0.2.12"
+
+__all__ = []
+
+
---
+++ packages/patatmo/patatmo-0.2.12.tar.gz/patatmo-0.2.12/patatmo/api/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# System modules
+
+# External modules
+
+# Internal modules
+
+from . import authentication
+from . import client
+from . import errors
+from . import variables
+from . import requests
+from . import responsetypes
+
+__all__ = ['authentication']
+
