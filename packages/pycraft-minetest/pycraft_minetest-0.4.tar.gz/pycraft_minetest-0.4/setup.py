7c3a48938ec7592578019c0d7f7da37735980fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:08 +0000

    Add package pycraft-minetest/0.4/pycraft_minetest-0.4.tar.gz

---
+++ packages/pycraft-minetest/pycraft_minetest-0.4.tar.gz/pycraft_minetest-0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='pycraft_minetest',
+    packages=['pycraft_minetest'],
+    version='0.4',
+    description='Modified, simplified and improved libraries to code in Python via Minetest.',
+    author='alenorfo & gmenegoz',
+    author_email='ale.norfo@gmail.com',
+    url='https://github.com/sprintingkiwi/pycraft_lib',
+    download_url='https://github.com/sprintingkiwi/pycraft_lib/tarball/0.4',
+    keywords=['game', 'development', 'learning', 'education', 'turtle'],
+    classifiers=[],
+)
