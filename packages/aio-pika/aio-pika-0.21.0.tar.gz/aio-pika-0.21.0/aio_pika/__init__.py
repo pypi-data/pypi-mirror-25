1565f95f36cbd31126cacb21d838ca3454fcda25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:05 +0000

    Add package aio-pika/0.21.0/aio-pika-0.21.0.tar.gz

---
+++ packages/aio-pika/aio-pika-0.21.0.tar.gz/aio-pika-0.21.0/aio_pika/__init__.py
@@ -0,0 +1,14 @@
+from .connection import Connection, connect
+from .channel import Channel
+from .exchange import Exchange, ExchangeType
+from .message import Message, IncomingMessage, DeliveryMode
+from .queue import Queue
+from .exceptions import AMQPException, MessageProcessError
+from .version import __author__, __version__, author_info, package_info, package_license, version_info
+
+
+__all__ = (
+    '__author__', '__version__', 'AMQPException', 'author_info', 'Channel', 'connect',
+    'Connection', 'DeliveryMode', 'Exchange', 'ExchangeType', 'IncomingMessage',
+    'Message', 'MessageProcessError', 'package_info', 'package_license', 'Queue', 'version_info',
+)
