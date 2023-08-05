8bc836be493099a0383e99596a0fb5a9ba6f97ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:43 +0000

    Add package codetransformer/0.7.2/codetransformer-0.7.2.tar.gz

---
+++ packages/codetransformer/codetransformer-0.7.2.tar.gz/codetransformer-0.7.2/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys
+
+import versioneer
+
+long_description = ''
+
+if 'upload' in sys.argv:
+    with open('README.rst') as f:
+        long_description = f.read()
+
+
+setup(
+    name='codetransformer',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Python code object transformers',
+    author='Joe Jevnik and Scott Sanderson',
+    author_email='joejev@gmail.com',
+    packages=find_packages(),
+    long_description=long_description,
+    license='GPL-2',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Pre-processors',
+    ],
+    url='https://github.com/llllllllll/codetransformer',
+    install_requires=['toolz'],
+    extras_require={
+        'dev': [
+            'flake8==3.3.0',
+            'pytest==2.8.4',
+            'pytest-cov==2.2.1',
+        ],
+    },
+)
