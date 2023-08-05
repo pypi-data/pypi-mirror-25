e1acc073f661d2d777a0bb4cea91c344a3ee62fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:46 +0000

    Add package PythonGoogleSpreadsheet/0.4.1/PythonGoogleSpreadsheet-0.4.1.tar.gz

---
+++ packages/PythonGoogleSpreadsheet/PythonGoogleSpreadsheet-0.4.1.tar.gz/PythonGoogleSpreadsheet-0.4.1/setup.py
@@ -0,0 +1,44 @@
+"""Unofficial wrapper for Google Sheets API, based on python 3.5+
+See:
+https://github.com/HolmesInc/PythonGoogleSpreadsheet
+"""
+try:
+    from setuptools import setup, find_packages
+except:
+    from distutils.core import setup
+
+setup(
+    name='PythonGoogleSpreadsheet',
+    version='0.4.1',
+    install_requires=[
+        'apiclient',
+        'argparse',
+        'google',
+        'google-api-python-client',
+        'httplib2',
+        'oauth2client',
+        'pyasn1',
+        'pyasn1-modules',
+        'requests',
+        'rsa',
+        'simplejson',
+        'six',
+        'uritemplate',
+        'urllib3',
+    ],
+    author="Andrew Babenko",
+    author_email="andruonline11@gmail.com",
+    description='Simple wrapper to create Google Spreadsheet, using Python 3+"',
+    license="LICENSE",
+    url='https://github.com/HolmesInc/PythonGoogleSpreadsheet',
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Office/Business :: Financial :: Spreadsheet",
+    ],
+)
