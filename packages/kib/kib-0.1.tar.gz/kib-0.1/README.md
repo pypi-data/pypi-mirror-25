38bc0ea1129759116192fd0adbdd247398d23647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:58 +0000

    Add package kib/0.1/kib-0.1.tar.gz

---
+++ packages/kib/kib-0.1.tar.gz/kib-0.1/README.md
@@ -0,0 +1,25 @@
+# kib
+Kubernetes image builder is simple service capable to build custom images and push them to registry. It is using [Custom resources](https://kubernetes.io/docs/concepts/api-extension/custom-resources/) and image controller.
+
+Image controller is listening for changes in Kubernetes API and build missing images.
+
+## Instalation
+
+1. Create custom resources
+
+```
+kubectl apply -f resources/
+```
+
+2. Run controller (running in container/pod will be available in next version)
+
+```
+python3 -m kib
+```
+
+3. Add Image resources and wait for them to get builded. Image examples can be found in `examples/`
+
+
+## Demo
+
+[![asciicast](https://asciinema.org/a/137445.png)](https://asciinema.org/a/137445)
---
+++ packages/kib/kib-0.1.tar.gz/kib-0.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+with open('README.md') as f:
+    long_description = ''.join(f.readlines())
+
+setup(
+    name='kib',
+    version=version,
+    description='Kubernetes image builder',
+    long_description=long_description,
+    author='Tomáš Kukrál',
+    author_email='tomas.kukral@6shore.net',
+    license='MIT',
+    url='https://github.com/tomkukral/kib/',
+    download_url='https://github.com/tomkukral/kib/archive/v{}.tar.gz'.format(version),
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'kubernetes',
+        'pytest',
+        'docker-registry-client',
+        'requests',
+        'docker',
+    ],
+    classifiers=[
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points={
+        'console_scripts': [
+            'kib = kib:main',
+        ],
+    },
+)
