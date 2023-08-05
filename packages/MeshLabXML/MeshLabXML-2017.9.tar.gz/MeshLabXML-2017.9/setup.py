ba51371192c2a7e7b6b29e70c900d9f2016f6699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:34 +0000

    Add package MeshLabXML/2017.9/MeshLabXML-2017.9.tar.gz

---
+++ packages/MeshLabXML/MeshLabXML-2017.9.tar.gz/MeshLabXML-2017.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='MeshLabXML',
+      version='2017.9',
+      description='Create and run MeshLab XML scripts',
+      url='https://github.com/3DLIRIOUS/MeshLabXML',
+      author='3DLirious',
+      author_email='3DLirious@gmail.com',
+      license='LGPL-2.1',
+      packages=['meshlabxml'],
+      include_package_data=True)
