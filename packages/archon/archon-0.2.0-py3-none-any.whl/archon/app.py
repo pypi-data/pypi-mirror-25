c1abc1e3e15ce2c45227ba6910ca88d3858dd94d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:08 +0000

    Add package archon/0.2.0/archon-0.2.0-py3-none-any.whl

---
+++ packages/archon/archon-0.2.0-py3-none-any.whl/archon/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+import logging
+from flask_sockets import Sockets
+
+
+def create_app(settings=None):
+    app = Flask(__name__)
+
+    if settings:
+        app.config.from_object(settings)
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    sockets = Sockets(app)
+
+    from archon.models import db
+    db.init_app(app)
+
+    from archon.hub import Hub
+    app.hub = Hub()
+
+    @sockets.route('/api/')
+    def open_socket(ws):
+        socket = app.hub.add(ws)
+        while not socket.ws.closed:
+            message = socket.ws.receive()
+            if message:
+                try:
+                    socket.handle(db, message)
+                except Exception as e:
+                    logging.error(e, exc_info=True)
+
+    return app, db
