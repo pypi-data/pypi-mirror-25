a031feba13f3914dce20d6496c0928a93af112f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:28 +0000

    Add package platron/2.0.0/platron-2.0.0.tar.gz

---
+++ packages/platron/platron-2.0.0.tar.gz/platron-2.0.0/tests/request/data_objects/long_record_test.py
@@ -0,0 +1,29 @@
+import unittest
+from platron.request.data_objects.long_record import LongRecord
+from platron.request.data_objects.tripleg import TripLeg
+from unittest.mock import Mock
+from unittest.mock import MagicMock
+
+class LongRecordTest(unittest.TestCase):
+    '''
+    LongRecord data object test
+    '''
+
+    def test_get_parameters(self):      
+        data_object = LongRecord('alexey lashnev', 'FFF666', '1')
+        data_object.set_agency_code('F')
+        data_object.set_ticket_system('GAT')
+      
+        triplegStub = Mock(spec=TripLeg)
+        triplegStub.get_params = MagicMock(return_value={'long_record_test' : 'long_record_test'})
+        
+        data_object.add_tripleg(triplegStub)
+      
+        params = data_object.get_params()
+        
+        self.assertEqual('alexey lashnev', params.get('pg_ticket_passenger_name'))
+        self.assertEqual('FFF666', params.get('pg_ticket_number'))
+        self.assertEqual('1', params.get('pg_ticket_restricted'))
+        self.assertEqual('F', params.get('pg_ticket_agency_code'))
+        self.assertEqual('GAT', params.get('pg_ticket_system'));
+        self.assertEqual('long_record_test', params.get('long_record_test'));
