110b8599f8a70d2d12a49970e279dd9afe575444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package lee-nest/1.3.0/lee_nest-1.3.0.tar.gz

---
+++ packages/lee-nest/lee_nest-1.3.0.tar.gz/lee_nest-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'lee_nest',
+    version = '1.3.0',
+    py_modules = ['lee_nest'],
+    author = 'mumu_lee',
+    author_email = "1076614217@qq.com",
+    url = 'http://www.headfirstlabs.com',
+    description = 'A Simple printer of nested lists',
+    )
