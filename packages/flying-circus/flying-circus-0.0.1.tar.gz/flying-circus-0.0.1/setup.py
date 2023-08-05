f2ab974d2efbd4ee2c0c569be325d9f13e84aa78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:08 +0000

    Add package flying-circus/0.0.1/flying-circus-0.0.1.tar.gz

---
+++ packages/flying-circus/flying-circus-0.0.1.tar.gz/flying-circus-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup(
+    name="flying-circus",
+    version="0.0.1",
+    packages=find_packages(),
+
+    # metadata for upload to PyPI
+    author="Gary Donovan",
+    author_email="gazza@gazza.id.au",
+    description="A tool for managing AWS infrastructure.",
+    license="LGPL",
+    keywords="cloud formation",
+    url="https://github.com/garyd203/flying-circus",
+    classifiers=[
+        # TODO add more
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
