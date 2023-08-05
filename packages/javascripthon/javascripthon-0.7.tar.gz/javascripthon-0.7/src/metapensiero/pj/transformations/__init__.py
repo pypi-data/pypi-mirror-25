4ec645d8da8ccef3c042fc3e8054e695d2b3360d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:45 +0000

    Add package javascripthon/0.7/javascripthon-0.7.tar.gz

---
+++ packages/javascripthon/javascripthon-0.7.tar.gz/javascripthon-0.7/src/metapensiero/pj/transformations/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# :Project:  metapensiero.pj
+# :Created:  gio 09 mar 2017 19:43:11 CET
+# :Author:   Alberto Berti <alberto@metapensiero.it>
+# :License:  GNU General Public License version 3 or later
+#
+
+import ast
+import logging
+
+import macropy.activate
+
+from ..js_ast import JSKeySubscript
+
+logger = logging.getLogger(__name__)
+
+
+def _normalize_name(n):
+    if n.startswith('d_'):
+        n = n.replace('d_', '$')
+    elif n.startswith('dd_'):
+        n = n.replace('dd_', '$$')
+    elif not n.startswith('_') and n.endswith('_'):
+        n = n[:-1]
+    return n
+
+def _normalize_dict_keys(transformer, keys):
+    res = []
+    for key in keys:
+        if isinstance(key, str):
+            key = ast.Str(key)
+        elif not isinstance(key, ast.Str):
+            if transformer.enable_es6:
+                key = JSKeySubscript(key)
+            else:
+                logger.warning('Ambiguous dict key %r and es6 disabled', key)
+        res.append(key)
+    return res
---
+++ packages/javascripthon/javascripthon-0.7.tar.gz/javascripthon-0.7/src/metapensiero/pj/transformations/exceptions.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+# :Project:  metapensiero.pj -- exceptions transformations
+# :Created:  ven 26 feb 2016 15:17:49 CET
+# :Authors:  Andrew Schaaf <andrew@andrewschaaf.com>,
+#            Alberto Berti <alberto@metapensiero.it>
+# :License:  GNU General Public License version 3 or later
+#
+
+import ast
+
+from ..js_ast import (
+    JSBinOp,
+    JSIfStatement,
+    JSName,
+    JSNewCall,
+    JSOpInstanceof,
+    JSStatements,
+    JSThrowStatement,
+    JSTryCatchFinallyStatement,
+)
+
+
+def Try(t, x):
+    t.unsupported(x, x.orelse, "'else' block of 'try' statement isn't supported")
+    ename = None
+    if x.handlers:
+        for h in x.handlers:
+            t.unsupported(x, not (h.type is None or isinstance(h.type, ast.Name)),
+                          "'except' expressions must be type names")
+            t.unsupported(x, h.name and ename and h.name != ename,
+                          "Different per 'except' block exception names aren't"
+                          " supported")
+            ename = h.name
+        ename = ename or 'e'
+
+        if t.has_child(x.handlers, ast.Raise) and t.has_child(x.finalbody, ast.Return):
+            t.warn(x, node, "The re-raise in 'except' body may be masked by the "
+                   "return in 'final' body.")
+            # see
+            # https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling#The_finally_block
+
+        rhandlers = x.handlers.copy()
+        rhandlers.reverse()
+        prev_except = stmt =  None
+        for ix, h in enumerate(rhandlers):
+            # if it's  the last except and it's a catchall
+            # threat 'except Exception:' as a catchall
+            if (ix == 0 and h.type is None or (isinstance(h.type, ast.Name) and
+                                   h.type.id == 'Exception')):
+                prev_except = JSStatements(*h.body)
+                continue
+            else:
+                if ix == 0:
+                    prev_except = JSThrowStatement(JSName(ename))
+                # then h.type is an ast.Name != 'Exception'
+                stmt = JSIfStatement(
+                    JSBinOp(JSName(ename), JSOpInstanceof(), JSName(h.type.id)),
+                    h.body,
+                    prev_except
+                )
+            prev_except = stmt
+        t.ctx['ename'] = ename
+        result = JSTryCatchFinallyStatement(x.body, ename, prev_except, x.finalbody)
+    else:
+        result = JSTryCatchFinallyStatement(x.body, None, None, x.finalbody)
+    return result
+
+
+def Raise(t, x):
+    if x.exc is None:
+        ename = t.ctx.get('ename')
+        t.unsupported(x, not ename, "'raise' has no argument but failed obtaining"
+                      " implicit exception")
+        res = JSThrowStatement(JSName(ename))
+    elif isinstance(x.exc, ast.Call) and isinstance(x.exc.func, ast.Name) and \
+         len(x.exc.args) == 1 and x.exc.func.id == 'Exception':
+        res = JSThrowStatement(JSNewCall(JSName('Error'), x.exc.args))
+    else:
+        res = JSThrowStatement(x.exc)
+
+    return res
