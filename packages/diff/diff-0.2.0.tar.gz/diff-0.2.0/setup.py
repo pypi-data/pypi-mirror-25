b667315ca42713c8ff91d3183ee04c415d5648b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:47 +0000

    Add package diff/0.2.0/diff-0.2.0.tar.gz

---
+++ packages/diff/diff-0.2.0.tar.gz/diff-0.2.0/setup.py
@@ -0,0 +1,38 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
+    long_description = readme.read()
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy"
+]
+
+setup(
+    name="diff",
+    url="https://github.com/Julian/diff",
+
+    description="You diff objects, you find out why they ain't the same.",
+    long_description=long_description,
+
+    author="Julian Berman",
+    author_email="Julian@GrayVines.com",
+
+    packages=find_packages(),
+
+    setup_requires=["vcversioner>=2.16.0.0"],
+    vcversioner={"version_module_paths": ["diff/_version.py"]},
+
+    classifiers=classifiers,
+)
