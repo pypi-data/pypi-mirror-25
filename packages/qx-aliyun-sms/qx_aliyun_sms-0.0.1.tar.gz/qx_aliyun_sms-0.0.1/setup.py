92862f74695392292258beb2c2fefb669a5adbf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package qx-aliyun-sms/0.0.1/qx_aliyun_sms-0.0.1.tar.gz

---
+++ packages/qx-aliyun-sms/qx_aliyun_sms-0.0.1.tar.gz/qx_aliyun_sms-0.0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='qx_aliyun_sms',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/tyan4g/qx_aliyun_sms.git',
+    license='Apache 2.0',
+    author='deng',
+    author_email='tyan4g@sina.com',
+    description=u'阿里云短信发送服务'
+)
