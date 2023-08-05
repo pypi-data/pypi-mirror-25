1dd3a3dcecefb5c1844cfb6cf70b4f11a4ac7cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:43 +0000

    Add package Leno_1_nester/1.0.0/Leno_1_nester-1.0.0.tar.gz

---
+++ packages/Leno_1_nester/Leno_1_nester-1.0.0.tar.gz/Leno_1_nester-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+	name           = 'Leno_1_nester',
+	version        = '1.0.0',
+	py_modules     = ['Leno_1_nester'],
+	author         = 'Leno',
+	author_email   = '0131123761@mail.imu.edu.cn',
+	url            = 'http://cliugeek.cn',
+	description    = 'A simple printer of nested lists')
