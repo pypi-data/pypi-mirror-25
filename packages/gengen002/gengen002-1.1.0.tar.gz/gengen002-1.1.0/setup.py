8fdef0a5640b53a695602e6111c10217161f893b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:36 +0000

    Add package gengen002/1.1.0/gengen002-1.1.0.tar.gz

---
+++ packages/gengen002/gengen002-1.1.0.tar.gz/gengen002-1.1.0/setup.py
@@ -0,0 +1,13 @@
+
+
+from distutils.core import setup
+
+setup(
+    name = 'gengen002',
+    version ='1.1.0',
+    py_modules=['nester'],
+    author = 'gengen001',
+    author_email = 'm13433870706@163.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'a simple printer',
+)
