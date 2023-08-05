9d2b2d9c308d317eb9704d37ec5244287cbd85f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:02 +0000

    Add package so/0.4/so-0.4.tar.gz

---
+++ packages/so/so-0.4.tar.gz/so-0.4/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="so",
+    version=0.04,
+    description=(
+        '<项目的简单描述>'
+    ),
+    long_description="empty",
+    author='alexanderLuo',
+    author_email='496952252@qq.com',
+    maintainer='alexanderLuo',
+    maintainer_email='496952252@qq.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'click'
+    ]
+)
