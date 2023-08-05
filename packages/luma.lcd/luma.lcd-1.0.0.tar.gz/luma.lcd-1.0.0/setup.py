12e7979589e14256ccaf0475b64245d6bf0ba8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:27 +0000

    Add package luma.lcd/1.0.0/luma.lcd-1.0.0.tar.gz

---
+++ packages/luma.lcd/luma.lcd-1.0.0.tar.gz/luma.lcd-1.0.0/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from setuptools import setup
+
+
+def read_file(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as r:
+        return r.read()
+
+
+README = read_file("README.rst")
+CONTRIB = read_file("CONTRIBUTING.rst")
+CHANGES = read_file("CHANGES.rst")
+version = read_file("VERSION.txt").strip()
+
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+test_deps = [
+    "mock",
+    "pytest>=3.1",
+    "pytest-cov"
+]
+
+setup(
+    name="luma.lcd",
+    version=version,
+    author="Richard Hull",
+    author_email="richard.hull@destructuring-bind.org",
+    description=("A small library to drive PCD8544 and ST7735 LCDs"),
+    long_description="\n\n".join([README, CONTRIB, CHANGES]),
+    license="MIT",
+    keywords="raspberry pi rpi lcd nokia 5110 display screen pcd8544 st7735 spi 84x48 160x128",
+    url="https://github.com/rm-hull/luma.lcd",
+    download_url="https://github.com/rm-hull/luma.lcd/tarball/" + version,
+    namespace_packages=["luma"],
+    packages=["luma.lcd"],
+    zip_safe=False,
+    install_requires=["luma.core>=0.9.0"],
+    setup_requires=pytest_runner,
+    tests_require=test_deps,
+    extras_require={
+        'docs': [
+            'sphinx >= 1.5.3'
+        ],
+        'qa': [
+            'rstcheck',
+            'flake8'
+        ],
+        'test': test_deps
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "Topic :: Education",
+        "Topic :: System :: Hardware",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
+    ]
+)
