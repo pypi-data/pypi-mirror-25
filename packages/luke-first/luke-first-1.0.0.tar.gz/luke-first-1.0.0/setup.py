4b5e1c8c35d0c8936b2d853232518cfab3f34cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:23 +0000

    Add package luke-first/1.0.0/luke-first-1.0.0.tar.gz

---
+++ packages/luke-first/luke-first-1.0.0.tar.gz/luke-first-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'luke-first', 
+        version      = '1.0.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['nester'],
+        author       = 'LukeDream',
+        author_email = 'luke.lufh@gmail.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+     )
