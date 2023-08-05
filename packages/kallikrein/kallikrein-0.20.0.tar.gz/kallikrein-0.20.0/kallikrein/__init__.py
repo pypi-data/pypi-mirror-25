aad8d5c659b9891418d9cfef3690132c0db7595d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:03 +0000

    Add package kallikrein/0.20.0/kallikrein-0.20.0.tar.gz

---
+++ packages/kallikrein/kallikrein-0.20.0.tar.gz/kallikrein-0.20.0/kallikrein/__init__.py
@@ -0,0 +1,4 @@
+from kallikrein.expectable import k, unsafe_k, kf
+from kallikrein.expectation import pending, Expectation
+
+__all__ = ('k', 'unsafe_k', 'pending', 'Expectation', 'kf')
---
+++ packages/kallikrein/kallikrein-0.20.0.tar.gz/kallikrein-0.20.0/kallikrein/matchers/typed.py
@@ -0,0 +1,43 @@
+from typing import TypeVar, Type, Union
+
+from kallikrein.matcher import (Predicate, matcher, NestingUnavailable,
+                                BoundMatcher, StrictMatcher, ChainMatcher)
+
+from amino import Boolean
+from amino.tc.base import TypeClass
+
+A = TypeVar('A')
+
+
+class Typed:
+    pass
+
+
+class ChainTyped(TypeClass):
+
+    def chain(self, tpe: Type) -> BoundMatcher:
+        ...
+
+
+class ChainMatcherTyped(ChainMatcher, tpe=Typed):
+
+    def chain(self, matcher: StrictMatcher, other: Union[A, BoundMatcher]) -> BoundMatcher:
+        return ChainTyped.fatal(matcher.target).chain(matcher, other)
+
+
+class PredTyped(Predicate):
+    pass
+
+
+class PredTypedAny(PredTyped, pred=lambda a: True):
+
+    def check(self, exp: A, tpe: type) -> Boolean:
+        return Boolean(isinstance(exp, tpe))
+
+
+success = '`{}` is a `{}`'
+failure = '`{}` is not a `{}`'
+typed = matcher(Typed, success, failure, PredTyped, NestingUnavailable)
+have_type = typed
+
+__all__ = ('typed', 'have_type')
