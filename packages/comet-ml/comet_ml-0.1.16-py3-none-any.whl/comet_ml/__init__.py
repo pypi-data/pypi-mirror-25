3706404992fa5947978bcb95cae770b0448d7027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:13 +0000

    Add package comet-ml/0.1.16/comet_ml-0.1.16-py3-none-any.whl

---
+++ packages/comet-ml/comet_ml-0.1.16-py3-none-any.whl/comet_ml/__init__.py
@@ -0,0 +1,7 @@
+"""comet - An opinionated, minimal cookiecutter template for Python packages"""
+
+from .comet import Experiment
+
+__version__ = '0.1.0'
+__author__ = 'Gideon <Gideon@semantica-labs.com>'
+__all__ = ['Experiment']
