76feedfcc1f5caff9379f8f6a3c2cb8e6295528b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:00 +0000

    Add package service-buddy/0.1.44/service-buddy-0.1.44.tar.gz

---
+++ packages/service-buddy/service-buddy-0.1.44.tar.gz/service-buddy-0.1.44/service_buddy/commands/initialize/command.py
@@ -0,0 +1,23 @@
+import click
+
+from service_buddy.commandline import cli
+from service_buddy.context.service_context import ServiceContext
+from service_buddy.service.initializer import Initializer
+
+
+@cli.command(name='init',short_help="Analyze service definitions and initialize any new services.")
+@click.option('--code-template-definitions',
+              envvar='CODE_TEMPLATE_DIR',
+              type=click.Path(exists=True),
+              required=True,
+              default="./code-templates",
+              help='File containing references to custom service templates.  '
+                   ' JSON dictionary {service-type: {type: file|git, location: relative path | git URL}}')
+@click.pass_obj
+def list_service(service_ctx,code_template_definitions):
+    # type: (ServiceContext, str,str) -> None
+    vcs =service_ctx.vcs
+    application_map = service_ctx.application_map
+    vcs.validate_repositories(application_map)
+    init = Initializer(vcs, service_ctx.destination_directory, service_ctx.dry_run,code_template_definitions)
+    init.initialize_services(application_map)
---
+++ packages/service-buddy/service-buddy-0.1.44.tar.gz/service-buddy-0.1.44/service_buddy/service/initializer.py
@@ -0,0 +1,43 @@
+import logging
+
+from service_buddy.ci.ci import BuildCreator
+from service_buddy.code.code_creator import CodeCreator
+from service_buddy.service.loader import safe_mkdir, walk_service_map, \
+    ensure_app_directory_exists
+from service_buddy.service.service import Service
+from service_buddy.service.service_template_generator import ServiceTemplateGenerator
+from service_buddy.util.pretty_printer import pretty_print_service
+
+
+
+
+class Initializer(object):
+    def __init__(self, vcs, destination_directory, dry_run,service_template_definitions):
+        super(Initializer, self).__init__()
+        self.code_generator = CodeCreator(service_template_definitions, dry_run)
+        self.build_creator = BuildCreator(service_template_definitions, dry_run)
+        self.dry_run = dry_run
+        safe_mkdir(directory=destination_directory)
+        self.destination_directory = destination_directory
+        self.vcs = vcs
+
+    def init_app(self,app):
+        pass
+    
+    def init_service(self, definition):
+        # type: (Service ) -> None
+        if definition.repo_exists():
+            logging.info("Service exists - {}".format(definition.get_fully_qualified_service_name()))
+            if definition.force_recreate_build():
+                self.build_creator.create_project(service_definition=definition)
+            return
+        logging.info("Creating Service -{}".format(definition.get_fully_qualified_service_name()))
+        app_dir = ensure_app_directory_exists(self.destination_directory, service_defintion=definition)
+        pretty_print_service(definition)
+        self.code_generator.create_project(definition, app_dir)
+        self.vcs.create_project(definition,app_dir)
+        self.build_creator.create_project(definition)
+
+    def initialize_services(self, application_map):
+        walk_service_map(application_map=application_map, application_callback=self.init_app,
+                         service_callback=self.init_service)
