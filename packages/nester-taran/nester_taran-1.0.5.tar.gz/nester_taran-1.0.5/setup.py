3dc6c8c5c99d9beaa745a11619ce75afee7417f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:35 +0000

    Add package nester-taran/1.0.5/nester_taran-1.0.5.tar.gz

---
+++ packages/nester-taran/nester_taran-1.0.5.tar.gz/nester_taran-1.0.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_taran',
+    version = '1.0.5',
+    py_modules = ['nester'],
+    author = 'Taran316',
+    author_email = 'taran.singh316@gmail.com',
+    url = 'http://taran.github.io',
+    description='A simple printer of nested lists, this version add the testnester.py',
+)
