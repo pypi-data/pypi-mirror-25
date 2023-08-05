5e4ac3501c7742b230fbbc8b6ac67291f7b2370a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:53 +0000

    Add package file-import/0.1/file_import-0.1.tar.gz

---
+++ packages/file-import/file_import-0.1.tar.gz/file_import-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='file_import',
+      version='0.1',
+      description='Directly import Python files',
+      url='https://github.com/rbnvrw/file_import',
+      author='Ruben Verweij',
+      author_email='rbnvrw@gmail.com',
+      license='MIT',
+      packages=['file_import'])
