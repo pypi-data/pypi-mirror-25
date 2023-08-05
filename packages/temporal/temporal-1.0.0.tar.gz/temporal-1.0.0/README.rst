a0f8c98fc49024c3a026747b54b04e381c3a6ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:45 +0000

    Add package temporal/1.0.0/temporal-1.0.0.tar.gz

---
+++ packages/temporal/temporal-1.0.0.tar.gz/temporal-1.0.0/README.rst
@@ -0,0 +1,13 @@
+Temporal
+--------
+Track Django temporal models.
+
+:Version: 1.0.0
+:Source: http://github.com/robinhoodmarkets/temporal
+
+Installation
+============
+
+::
+
+    $ pip install temporal
---
+++ packages/temporal/temporal-1.0.0.tar.gz/temporal-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='temporal',
+      version='1.0.0',
+      description='Track Django temporal models.',
+      author='JamshedVesuna',
+      author_email='jvesuna314@gmail.com',
+      packages=['temporal'],
+)
