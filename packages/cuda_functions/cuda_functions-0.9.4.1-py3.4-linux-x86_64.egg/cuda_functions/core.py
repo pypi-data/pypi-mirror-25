8426f1ff390bb2991fde1801707d27d2d17ac3e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:38 +0000

    Add package cuda_functions/0.9.4.1/cuda_functions-0.9.4.1-py3.4-linux-x86_64.egg

---
+++ packages/cuda_functions/cuda_functions-0.9.4.1-py3.4-linux-x86_64.egg/cuda_functions/core.py
@@ -0,0 +1,48 @@
+from cuda_functions.bin import gpu_correlate_dp, gpu_correlate_sp, gpu_correlate_spc, gpu_correlate_dpc
+from cuda_functions.bin import gpu_fft_dpc, gpu_fft_spc
+
+
+import numpy as np
+import warnings
+
+def cuda_acorrelate(array, mode='valid', safe_mode=True):
+    if safe_mode:
+        array = np.ascontiguousarray(array)
+
+    if array.dtype == 'complex64':
+        return gpu_correlate_spc.acorrelate(array, mode=mode)
+    elif array.dtype == 'complex128':
+        return gpu_correlate_dpc.acorrelate(array, mode=mode)
+    elif array.dtype == 'float32':
+        return gpu_correlate_sp.acorrelate(array, mode=mode)
+    elif array.dtype == 'float64':
+        return gpu_correlate_dp.acorrelate(array, mode=mode)
+    else:
+        warnings.warn('{} type not supported, it will be converted to complex128'.format(array.dtype))
+        return gpu_correlate_dpc.acorrelate(np.array(array, dtype='complex128'), mode=mode)
+
+
+def cuda_fft(array, safe_mode=True):
+    if safe_mode:
+        array = np.ascontiguousarray(array)
+
+    if array.dtype == 'complex64':
+        return gpu_fft_spc.fft(array)
+    elif array.dtype == 'complex128':
+        return gpu_fft_dpc.fft(array)
+    else:
+        warnings.warn('{} type not supported, it will be converted to complex128'.format(array.dtype))
+        return gpu_fft_dpc.fft(np.array(array, dtype='complex128'))
+
+
+def cuda_ifft(array, safe_mode=True):
+    if safe_mode:
+        array = np.ascontiguousarray(array)
+
+    if array.dtype == 'complex64':
+        return gpu_fft_spc.ifft(array)
+    elif array.dtype == 'complex128':
+        return gpu_fft_dpc.ifft(array)
+    else:
+        warnings.warn('{} type not supported, it will be converted to complex128'.format(array.dtype))
+        return gpu_fft_dpc.ifft(np.array(array, dtype='complex128'))
