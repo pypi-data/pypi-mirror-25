9f77120517e0ef444699568a689104758f18f99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:09 +0000

    Add package dbrhino-agent/0.1.0a6/dbrhino_agent-0.1.0a6-py3-none-any.whl

---
+++ packages/dbrhino-agent/dbrhino_agent-0.1.0a6-py3-none-any.whl/dbrhino_agent/db/__init__.py
@@ -0,0 +1,11 @@
+from urllib.parse import urlparse
+
+
+def create(*, connect_to, **kwargs):
+    parsed = urlparse(connect_to)
+    if parsed.scheme == "postgresql":
+        from .postgresql import Postgresql
+        cls = Postgresql
+    else:
+        raise Exception("Unknown scheme: " + str(parsed.scheme))
+    return cls(connect_to=connect_to, parsed_url=parsed, **kwargs)
