1ccb2e486133abcdcaa8873f2b301defcc268aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:58 +0000

    Add package pydetector-bblfsh/0.14.1/pydetector-bblfsh-0.14.1.tar.gz

---
+++ packages/pydetector-bblfsh/pydetector-bblfsh-0.14.1.tar.gz/pydetector-bblfsh-0.14.1/pydetector/borrame.py
@@ -0,0 +1,19 @@
+{'body': [
+    {'body': [
+        {'body': [
+            {'col_offset': 20,
+                'value': {'col_offset': 27,
+                          '_attributes': ('lineno', 'col_offset'),
+                          'n': 0L,
+                          'pok': 1234l,
+                          'ast_type': 'Num',
+                          'lineno': 986,
+                          '_fields': ('n',)},
+                '_attributes': ('lineno', 'col_offset'), 'ast_type': 'Return', 'lineno': 986, '_fields': ('value',)}
+            ],
+        },
+        ]
+        },
+    ],
+    '_fields': ('body',), 'ast_type': 'Module', '_attributes': ()
+}
---
+++ packages/pydetector-bblfsh/pydetector-bblfsh-0.14.1.tar.gz/pydetector-bblfsh-0.14.1/pydetector/eval.py
@@ -0,0 +1,4 @@
+from ast import literal_eval
+
+f = open('borrame.py').read()
+literal_eval(f)
