d415c626f62e2aeabb2970b0098cdd25e755717a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:27 +0000

    Add package awareness/25.1/awareness-25.1-py3-none-any.whl

---
+++ packages/awareness/awareness-25.1-py3-none-any.whl/awareness/exception.py
@@ -0,0 +1,34 @@
+#
+# Copyright (c) 2016-2017 Aedan S. Cullen.
+#
+# This file is part of the Awareness Python implementation.
+#
+# Awareness is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Awareness is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Awareness.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+class ProtocolException(Exception):
+    pass
+
+
+class UnitError(ProtocolException):
+    pass
+
+class DataError(ProtocolException):
+    pass
+
+
+
+class ConnectionException(Exception):
+    pass
