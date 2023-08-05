4b8c5a03b4c94835bf81bb782e537e3403c94cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:09 +0000

    Add package persian/0.4.0/Persian-0.4.0.tar.gz

---
+++ packages/persian/Persian-0.4.0.tar.gz/persian-0.4.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="persian",
+    version="0.4.0",
+    author="Mohammad Reza Kamalifard",
+    author_email="mrkamalifard@gmail.com",
+    description=("Simple Python library for Persian language localization"),
+    license="Apache License 2.0",
+    keywords="example documentation tutorial",
+    url="https://github.com/itmard/Persian",
+    packages=['persian'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python"
+    ],
+)
