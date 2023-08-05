e65ac14072114bcce47f74ee1481f469fd35759b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package balderdash/1.18.0/balderdash-1.18.0.tar.gz

---
+++ packages/balderdash/balderdash-1.18.0.tar.gz/balderdash-1.18.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='balderdash',
+    version='1.18.0',
+    description='Builders for monitoring dashboards',
+    url='http://github.com/daviddenton/balderdash/',
+    license='Apache 2.0',
+    author='daviddenton',
+    author_email='denton.david+pypy@gmail.com',
+    packages=find_packages(exclude=['tests*']),
+    test_suite="tests",
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
