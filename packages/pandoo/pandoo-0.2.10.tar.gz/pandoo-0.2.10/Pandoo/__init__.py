6fa6f01876e23e8fb47f9c6a8af83d7934d1d26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:09 +0000

    Add package pandoo/0.2.10/pandoo-0.2.10.tar.gz

---
+++ packages/pandoo/pandoo-0.2.10.tar.gz/pandoo-0.2.10/Pandoo/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+__name__            = 'paando'
+__version__         = '0.2.10'
+__version_date__    = 'Fri 8th September, 2017'
+__description__     = ('Pandoo is a bioinformatics tool for for ' +\
+                       'exploring and characterising bacterial genome ' +\
+                       'data.  Input is paired-end reads and assemblies.  ' +\
+                       'Output is an NJ tree/dendrogram inferred from the ' +\
+                       'assemblies (using mashtree) and a table of results ' +\
+                       '(species ID, mlst, antimicrobial resistance genes, ' +\
+                       'plasmid replicon types, virulence genes, etc., ' +\
+                       'plus read and assembly ' +\
+                       'QC metrics).  Github repo and issue tracker: ' +\
+                       'https://github.com/schultzm/pandoo/. ' +\
+                       'Pypi repository: ' +\
+                       'https://pypi.python.org/pypi/paando')
+__author__          = 'Mark B. Schultz, ' +\
+                      'Anders Gonçalves da Silva, ' +\
+                      'Jason Kwong, ' +\
+                      'Torsten Seemann'
+__author_email__    = 'dr.mark.schultz@gmail.com'
+__github_username__ = 'schultzm'
+__download_url__    = 'https://github.com/schultzm/pandoo/zipball/master'
+__license__         = 'AGPL-3.0'
+__all__             = ['paando']
