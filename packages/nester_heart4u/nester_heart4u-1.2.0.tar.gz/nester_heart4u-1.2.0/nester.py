aeba619e6a75ad02f45f3d4d1c3e9680f5013edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:13 +0000

    Add package nester_heart4u/1.2.0/nester_heart4u-1.2.0.tar.gz

---
+++ packages/nester_heart4u/nester_heart4u-1.2.0.tar.gz/nester_heart4u-1.2.0/nester.py
@@ -0,0 +1,21 @@
+'''
+This is the "nester.py" module and it provides one function called print_lol()
+which prints lists that may or may not include nested lists.
+'''
+
+
+def print_lol(the_list, level=0):
+    '''This function takes one positional argument called "the_list",witch
+    is any Python list (of - possibly - nested lists). Each data item in the
+    provided list is (recursively) printed to the screen on it's own line.'''
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print('\t', end='')
+            print(each_item)
+
+if __name__ == '__main__':
+    k = [1, 2, [3, 4, [5, 6], 7, 8], 9]
+    print_lol(k, 0)
