244f4adbea1b2f2d2a0846a3259923e335d9d437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:26 +0000

    Add package wikiquotes/1.1/wikiquotes-1.1.tar.gz

---
+++ packages/wikiquotes/wikiquotes-1.1.tar.gz/wikiquotes-1.1/wikiquotes/managers/html_manager.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup, Tag
+
+class HTMLManager:
+
+    def __init__(self, html, language):
+        self.language = language
+        self.soup = BeautifulSoup(html, "lxml")
+
+    # Although this doesn't need self, it mutates state so it's better to have it here and not as a free function.
+    def remove_sublists(self, html_list):
+            list_items = html_list.find_all('li')
+
+            for li in list_items:
+                if not li is -1 and li:
+                    if li.ul:
+                        li.ul.decompose()
+
+    # Although this doesn't need self, it mutates state so it's better to have it here and not as a free function.
+    def remove(self, element):
+        for child in element.findChildren():
+            child.decompose()
+
+    def pretty_print(self):
+        print(self.soup.prettify())
+
+    def find_all_lists(self):
+        return self.soup.find_all("ul")
+
+def extract_text_from_list(html_list):
+    return map(lambda li_quote: li_quote.text.strip(), html_list.find_all("li"))
---
+++ packages/wikiquotes/wikiquotes-1.1.tar.gz/wikiquotes-1.1/wikiquotes/managers/logging_manager.py
@@ -0,0 +1,21 @@
+import logging
+import time
+import sys
+import inspect
+from functools import wraps
+
+logging.basicConfig(filename=directory.log_path, level = logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+logger = logging
+
+def log_method_call(task):
+    @wraps(task)
+    def callable(*args, **kwargs):
+        frame = inspect.stack()[1]
+        module = inspect.getmodule(frame[0])
+        function_called = "{}".format(task.__name__)
+        logging.info("{} started: ".format(function_called))
+        start_detection = time.time()
+        task(*args, **kwargs)
+        logging.info("{} finished: took {} seconds".format(function_called, time.time() - start_detection))
+        return task(*args, **kwargs)
+    return callable
