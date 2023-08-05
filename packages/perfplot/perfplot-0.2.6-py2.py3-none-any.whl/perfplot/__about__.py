43339219e3cb15e40ac04e036c0e03a3fac697bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:58 +0000

    Add package perfplot/0.2.6/perfplot-0.2.6-py2.py3-none-any.whl

---
+++ packages/perfplot/perfplot-0.2.6-py2.py3-none-any.whl/perfplot/__about__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+__author__ = u'Nico Schlömer'
+__author_email__ = 'nico.schloemer@gmail.com'
+__copyright__ = 'Copyright (c) 2017, %s <%s>' % (__author__, __author_email__)
+__license__ = 'License :: OSI Approved :: MIT License'
+__version__ = '0.2.6'
+__status__ = 'Development Status :: 4 - Beta'
