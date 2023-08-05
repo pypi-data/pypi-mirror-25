b31fc02429f482bcb7aa6acbdbb2c1b9d27754c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:08 +0000

    Add package metricz/0.1.5.2/metricz-0.1.5.2.tar.gz

---
+++ packages/metricz/metricz-0.1.5.2.tar.gz/metricz-0.1.5.2/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'requests',
+    'stups-tokens',
+    'click',
+    'clickclick',
+    'environmental>=1.0',
+]
+
+test_requirements = [
+    'pytest',
+    'mock'
+]
+
+setup(
+    name='metricz',
+    version='0.1.5.2',
+    description="Metricz makes it easy to write metrics to a Kairosdb instance running with OAuth2 security.",
+    long_description=readme + '\n\n' + history,
+    author="João Santos",
+    author_email='jmcs@jsantos.eu',
+    url='https://github.com/jmcs/metricz',
+    packages=[
+        'metricz',
+    ],
+    package_dir={'metricz':
+                 'metricz'},
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+    keywords='metricz',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=test_requirements,
+    entry_points={'console_scripts': ['metricz = metricz.cli:main']}
+)
