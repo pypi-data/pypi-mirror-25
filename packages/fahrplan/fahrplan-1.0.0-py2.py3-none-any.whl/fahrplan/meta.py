456560c3f56e2319cd86c45f104d1dfc2b90e1bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:44 +0000

    Add package fahrplan/1.0.0/fahrplan-1.0.0-py2.py3-none-any.whl

---
+++ packages/fahrplan/fahrplan-1.0.0-py2.py3-none-any.whl/fahrplan/meta.py
@@ -0,0 +1,9 @@
+"""This module contains metadata about the project."""
+title = 'fahrplan'
+version = '1.0.0'
+description = 'A SBB/CFF/FFS commandline based timetable client.'
+author = 'Danilo Bargen and contributors'
+author_email = 'gezuru@gmail.com'
+url = 'https://github.com/dbrgn/fahrplan'
+keywords = 'fahrplan timetable sbb cff ffs public transport'
+license = 'GPLv3'
