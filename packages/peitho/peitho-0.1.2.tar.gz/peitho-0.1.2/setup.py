fc1a26770cbc3cb22fe64dddf9c285b54c2b88f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:37 +0000

    Add package peitho/0.1.2/peitho-0.1.2.tar.gz

---
+++ packages/peitho/peitho-0.1.2.tar.gz/peitho-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(name='peitho',
+	version='0.1.2',
+	description='Perfecting Experiments with Information Theory',
+	url='https://github.com/MichaelPHStumpf/Peitho',
+	download_url='https://github.com/MichaelPHStumpf/Peitho/archive/0.1.2.tar.gz',
+	author='Leander Dony, Scott Ward, Jonas Mackerodt, Juliane Liepe, Michael PH Stumpf',
+	author_email='m.stumpf@imperial.ac.uk',
+	license='MIT',
+	packages=find_packages(),
+	include_package_data=True,
+	install_requires = [
+		'pycuda',
+		'numpy',
+		'matplotlib',
+		'python-libsbml'
+	],
+	entry_points = {
+		'console_scripts': ['peitho=peitho.main.main:main']
+		},
+	keywords = ['information theory','entropy','experimental design'],
+	classifiers = ['Development Status :: 3 - Alpha',
+	'Environment :: Console',
+	'Intended Audience :: Science/Research',
+	'Topic :: Scientific/Engineering :: Bio-Informatics',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 2.7'],
+	zip_safe=False)
