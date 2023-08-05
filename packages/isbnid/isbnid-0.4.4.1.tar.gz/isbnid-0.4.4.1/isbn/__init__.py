9b97f00bc7bc964d5216edb4a48e1f94cfc54445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:40 +0000

    Add package isbnid/0.4.4.1/isbnid-0.4.4.1.tar.gz

---
+++ packages/isbnid/isbnid-0.4.4.1.tar.gz/isbnid-0.4.4.1/isbn/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Hypatia: Module ISBN [__init__]
+#
+
+__author__ = "Neko"
+__license__ = 'LGPL http://www.gnu.org/licenses/lgpl.txt'
+__version__ = '0.4.4.1'
+
+from . isbn import ISBN, ISBNError, ISBNRangeError
