fa8c60e38121659cf1fbddad52856df5535d79e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:24 +0000

    Add package cve_search/1.0.6/cve_search-1.0.6.tar.gz

---
+++ packages/cve_search/cve_search-1.0.6.tar.gz/cve_search-1.0.6/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="cve_search",
+
+    # Version number (initial):
+    version="1.0.6",
+
+    # Application author details:
+    author="cve_search",
+    author_email="cve-search@cve-search.com",
+
+    # Packages
+    packages=["cve_search"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="http://pypi.python.org/pypi/cve-search/",
+
+    #
+    # license="LICENSE.txt",
+    description="Useful towel-related stuff.",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "click",
+        "flask",
+        "flask-login<=0.2.11",
+        "flask-pymongo",
+        "irc",
+        "itsdangerous",
+        "lxml",
+        "passlib",
+        "python-dateutil",
+        "pytz",
+        "redis",
+        "requests",
+        "six>=1.9.0",
+        "sleekxmpp",
+        "tornado",
+        "whoosh",
+        "xlrd",
+        "Jinja2",
+        "PyMongo>=2.7",
+        "Werkzeug"
+    ],
+)
