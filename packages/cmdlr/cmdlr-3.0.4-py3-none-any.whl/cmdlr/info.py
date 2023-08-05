92a17c529f3967dbbed47951d3bfc49a429d74d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:56 +0000

    Add package cmdlr/3.0.4/cmdlr-3.0.4-py3-none-any.whl

---
+++ packages/cmdlr/cmdlr-3.0.4-py3-none-any.whl/cmdlr/info.py
@@ -0,0 +1,9 @@
+"""Cmdlr infomation files."""
+
+VERSION = '3.0.4'
+DESCRIPTION = ('An extensible comic subscriber.')
+LICENSE = 'MIT'
+AUTHOR = 'Civalin'
+AUTHOR_EMAIL = 'larinawf@gmail.com'
+PROJECT_URL = 'https://github.com/civalin/cmdlr'
+PROJECT_NAME = 'cmdlr'
