e01547756f7ae17e52f3dbdddf4ac08254d04d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:10 +0000

    Add package aqicn-api-client/0.0.1/aqicn_api_client-0.0.1.tar.gz

---
+++ packages/aqicn-api-client/aqicn_api_client-0.0.1.tar.gz/aqicn_api_client-0.0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='aqicn_api_client',
+    version='0.0.1',
+    scripts=['client']                  # The name of your scipt, and also the command you'll be using for calling it
+)
