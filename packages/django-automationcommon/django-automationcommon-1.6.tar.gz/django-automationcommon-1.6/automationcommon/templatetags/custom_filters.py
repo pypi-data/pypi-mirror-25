7234c34ae59d301b1c7601853a7e7d2034bd9b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package django-automationcommon/1.6/django-automationcommon-1.6.tar.gz

---
+++ packages/django-automationcommon/django-automationcommon-1.6.tar.gz/django-automationcommon-1.6/automationcommon/templatetags/custom_filters.py
@@ -0,0 +1,50 @@
+import dateutil
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def username_list(users):
+    """
+    :param users: list of django User entities
+    :return: a comma seperated list of crsid's
+    """
+    return ",".join([user.username for user in users])
+
+
+@register.filter
+def display_user(user):
+    """
+    :param user: django User
+    :return: a rendering of the user's name sand crsid
+    """
+    return "%s (%s)" % (user.get_full_name(), user.username)
+
+
+@register.filter
+def unique_entity_id(entity):
+    """
+    :param entity: django model
+    :return: unique token combining the model type and id for use in HTML
+    """
+    return "%s-%s" % (type(entity).__name__, entity.id)
+
+
+@register.filter
+def get_item(dictionary, key):
+    """
+    :param dictionary: the dictionary where you want to access the data from
+    :param key: the key of the dictionary where you want to access the data from
+    :return: the content of the dictionary corresponding to the key entry or None if the key does not exists
+    """
+    return dictionary.get(key, None)
+
+
+@register.filter
+def parse_date(date_str):
+    """
+    :param date_str: a string representation of a date
+    :return: the parsed date
+    """
+    return dateutil.parser.parse(date_str) if date_str else None
