91396c30e3cbfe479ebbc4c432d4fdbcf4266ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:26 +0000

    Add package dbrhino-agent/0.1.0a18/dbrhino_agent-0.1.0a18-py3-none-any.whl

---
+++ packages/dbrhino-agent/dbrhino_agent-0.1.0a18-py3-none-any.whl/dbrhino_agent/db/utils.py
@@ -0,0 +1,26 @@
+class Database(object):
+    def __init__(self, *, name, connect_to, parsed_url, **kwargs):
+        self.name = name
+        self.connect_to = connect_to
+        self.parsed_url = parsed_url
+        self.type = parsed_url.scheme
+
+
+class GrantPermission(object):
+    NONE = "none"
+    SELECT = "select"
+
+
+def first_column(cur):
+    return [x for x, *_ in cur.fetchall()]
+
+
+def scalar_result(cur):
+    col = first_column(cur)
+    return col[0] if col else None
+
+
+class NoPasswordException(Exception):
+    def __init__(self, username):
+        msg = "User {} has to set a password".format(username)
+        super().__init__(msg)
