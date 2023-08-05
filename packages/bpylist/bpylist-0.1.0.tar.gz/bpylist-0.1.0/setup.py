1b9b6b48732f0f30d276a20fdf6a61e9d41e6d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package bpylist/0.1.0/bpylist-0.1.0.tar.gz

---
+++ packages/bpylist/bpylist-0.1.0.tar.gz/bpylist-0.1.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from distutils.core import Extension
+
+bplist = Extension('bpylist.bplist', sources=['src/bplist.c'])
+
+setup(
+    name='bpylist',
+    version='0.1.0',
+    author='Marketcircle Inc.',
+    author_email='cloud@marketcircle.com',
+    url='https://github.com/marketcircle/bpylist',
+    ext_modules=[
+        bplist,
+    ],
+    packages=[
+        'bpylist',
+    ],
+    include_package_data=True,
+    test_suite='tests',
+    classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python :: 3.6',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries'
+    ]
+
+)
