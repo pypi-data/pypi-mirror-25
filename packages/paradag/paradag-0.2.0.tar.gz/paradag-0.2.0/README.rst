08d064529c6a897320fca39ab0dc2bbbba4b1770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:23 +0000

    Add package paradag/0.2.0/paradag-0.2.0.tar.gz

---
+++ packages/paradag/paradag-0.2.0.tar.gz/paradag-0.2.0/README.rst
@@ -0,0 +1,22 @@
+paradag
+=======
+
+.. image:: https://img.shields.io/pypi/v/paradag.svg
+   :target: https://pypi.python.org/pypi/paradag
+   :alt: PyPI
+
+.. image:: https://travis-ci.org/xianghuzhao/paradag.svg?branch=master
+   :target: https://travis-ci.org/xianghuzhao/paradag
+   :alt: Travis CI Status
+
+.. image:: https://codeclimate.com/github/xianghuzhao/paradag/badges/gpa.svg
+   :target: https://codeclimate.com/github/xianghuzhao/paradag
+   :alt: Code Climate
+
+A robust DAG package for easy parallel execution.
+
+paradag is implemented in pure python and totally independent of any
+other packages.
+
+Try to make execute as simple as possible. Extra operation could be put
+in `report_*` functions, like logging.
---
+++ packages/paradag/paradag-0.2.0.tar.gz/paradag-0.2.0/setup.py
@@ -0,0 +1,52 @@
+import sys
+import os
+
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'paradag', 'VERSION')) as version_file:
+    version = version_file.read().strip()
+
+with open(os.path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name = 'paradag',
+    version = version,
+    description = 'A robust DAG implementation for parallel programming',
+    long_description = long_description,
+    url = 'https://github.com/xianghuzhao/paradag',
+    author = 'Xianghu Zhao',
+    author_email = 'xianghuzhao@gmail.com',
+    license = 'MIT',
+
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    keywords = ['DAG', 'parallel programming'],
+    packages = find_packages(exclude=[]),
+    include_package_data = True,
+    tests_require = [
+        'pytest',
+    ],
+)
