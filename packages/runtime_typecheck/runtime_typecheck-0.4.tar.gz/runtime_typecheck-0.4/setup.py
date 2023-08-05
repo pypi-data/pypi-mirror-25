c14a73c79943b83b233b86e4fdb05f63acefcc95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:17 +0000

    Add package runtime_typecheck/0.4/runtime_typecheck-0.4.tar.gz

---
+++ packages/runtime_typecheck/runtime_typecheck-0.4.tar.gz/runtime_typecheck-0.4/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='runtime_typecheck',
+    long_description='Runtime type checking with Python 3.6 typing functions',
+    version='0.4',
+    description='Runtime type checking with Python 3.6 typing',
+    author='Jacopo Farina',
+    author_email='jacopo1.farina@gmail.com',
+    license='MIT',
+    packages=['runtime_typecheck'],
+    url='https://github.com/jacopofar/runtime_typecheck',
+    download_url='https://github.com/jacopofar/runtime_typecheck/archive/0.2.tar.gz'
+)
