0571c5322aedb0d8e8ae712e5b14956e39776e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:50 +0000

    Add package pypartial/1.0.1/pypartial-1.0.1.tar.gz

---
+++ packages/pypartial/pypartial-1.0.1.tar.gz/pypartial-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+	name='pypartial',
+	version='1.0.1',
+
+	description='Use partial function application in Python',
+	long_description=long_description,
+	url='https://pypi.python.org/pypi/pypartial',
+	author='Akif Patel',
+	author_email='akifpatel@supernovaapps.com',
+	license='Apache 2.0',
+	
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: Apache Software License',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 3',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	],
+	
+	keywords=['python', 'functional programming', 'library', 'notation'],
+	packages=['pypartial']
+)
