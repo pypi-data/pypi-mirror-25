490f6805ce2fc54a97890329da263644951f83f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:40 +0000

    Add package kisscache/0.36/kisscache-0.36.tar.gz

---
+++ packages/kisscache/kisscache-0.36.tar.gz/kisscache-0.36/kisscache/__init__.py
@@ -0,0 +1,12 @@
+#cache
+"""kiscache module.
+
+It is a cache for python.
+It include Sqlalchemy cache, Redis Cache and simple python cache.
+
+"""
+
+from kisscache.kiss_cache import MainCacheClass 
+from kisscache.appid import appid
+
+__version__ = 0.29
---
+++ packages/kisscache/kisscache-0.36.tar.gz/kisscache-0.36/kisscache/appid.py
@@ -0,0 +1,2 @@
+
+appid = 'Untitled'
