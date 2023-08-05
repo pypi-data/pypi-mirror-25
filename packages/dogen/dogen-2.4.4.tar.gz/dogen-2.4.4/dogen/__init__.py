a57df4a65d30d2d2f5e9a9d65af6ad152fa5129b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:00 +0000

    Add package dogen/2.4.4/dogen-2.4.4.tar.gz

---
+++ packages/dogen/dogen-2.4.4.tar.gz/dogen-2.4.4/dogen/__init__.py
@@ -0,0 +1,9 @@
+from dogen.version import version
+
+__version__ = version
+
+# Script that is executed in a script package if it exists and if
+# no "exec" is explicitly defined
+DEFAULT_SCRIPT_EXEC = "run"
+# User used for executing scripts if no "user" is explicitly defined
+DEFAULT_SCRIPT_USER = "root"
