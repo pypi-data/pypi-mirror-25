e100446d28af42d845c6c518c3d598a785dbdabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:11 +0000

    Add package bpylist/0.1.4/bpylist-0.1.4.tar.gz

---
+++ packages/bpylist/bpylist-0.1.4.tar.gz/bpylist-0.1.4/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+from setuptools import setup
+from distutils.core import Extension
+
+bplist = Extension('bpylist.bplist', sources=['src/bplist.c'])
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(
+    name='bpylist',
+    version='0.1.4',
+    description = "parse and generate binary plists and NSKeyedArchiver archives",
+    long_description = long_description,
+    author='Marketcircle Inc.',
+    author_email='cloud@marketcircle.com',
+    url='https://github.com/marketcircle/bpylist',
+    ext_modules=[
+        bplist,
+    ],
+    packages=[
+        'bpylist',
+    ],
+    include_package_data=True,
+    test_suite='tests',
+    classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python :: 3.6',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries'
+    ]
+
+)
