2e956c3959e8b9abf9730a2100723f8871e98940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:19 +0000

    Add package snipshue/0.1.6.5/snipshue-0.1.6.5.tar.gz

---
+++ packages/snipshue/snipshue-0.1.6.5.tar.gz/snipshue-0.1.6.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='snipshue',
+    version='0.1.6.5',
+    description='Philips Hue skill for Snips',
+    author='Michael Fester',
+    author_email='michael.fester@gmail.com',
+    url='https://github.com/snipsco/snips-skill-hue',
+    download_url='',
+    license='MIT',
+    install_requires=['requests==2.6.0'],
+    test_suite="tests",
+    keywords=['snips'],
+    package_data = {
+        '': ['Snipsspec'],
+    },
+    data_files = ['Snipsspec'],
+    # include_package_data=True,
+    packages=[
+        'snipshue'
+    ]
+)
