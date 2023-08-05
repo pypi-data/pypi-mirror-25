47232df7db8ce8bf9a727d6ebf401a11bdefa862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:36 +0000

    Add package flatapi/5.3.0/flatapi-5.3.0-py3-none-any.whl

---
+++ packages/flatapi/flatapi-5.3.0-py3-none-any.whl/flatapi/__main__.py
@@ -0,0 +1,27 @@
+
+from flask import Flask
+from flatapi import *
+import argparse
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-P", "--port", type=int,
+                    help="Customize the port")
+    parser.add_argument("-G", "--cfgfile", type=string,
+                    help="Customize the config file") 
+    parser.add_argument("-S", "--storage", type=string,
+                    help="Customize the storage. Option: FILE | MOMERY ")  
+    parser.add_argument("-X", "--prefix", type=string,
+                    help="Customize the prefix, e.g. /api")
+
+    args = parser.parse_args()               
+    _port = args.port or 5000
+    _cfgfile = args.cfgfile 
+    _prefix = args.prefix
+    _storage = upper(args.storage) if args.storage else args.storage
+
+    app = Flask(__name__)
+    restApi = FlatApi(app, config=_cfgfile, prefix = _prefix , storage = _storage)
+    app.run(debug=False, port = _port )
