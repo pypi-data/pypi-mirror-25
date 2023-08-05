befb52579bc6b5f1c03f1e2af4071f973d8d5399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:44 +0000

    Add package ldap2pg/3.0a1/ldap2pg-3.0a1-py2.py3-none-any.whl

---
+++ packages/ldap2pg/ldap2pg-3.0a1-py2.py3-none-any.whl/ldap2pg/utils.py
@@ -0,0 +1,54 @@
+from __future__ import unicode_literals
+
+import sys
+from fnmatch import fnmatch
+
+
+PY2 = sys.version_info < (3,)
+
+if PY2:
+    string_types = (str, unicode)  # noqa
+else:
+    string_types = (str,)
+
+
+class AllDatabases(object):
+    # Simple object to represent dbname wildcard.
+    def __repr__(self):
+        return '__ALL_DATABASES__'
+
+
+def lower1(string):
+    return string[0].lower() + string[1:]
+
+
+def match(string, patterns):
+    for pattern in patterns:
+        if fnmatch(string, pattern):
+            return pattern
+    return False
+
+
+class UserError(Exception):
+    def __init__(self, message, exit_code=1):
+        super(UserError, self).__init__(message)
+        self.exit_code = exit_code
+
+
+def deepget(mapping, path):
+    """Access deep dict entry."""
+    if ':' not in path:
+        return mapping[path]
+    else:
+        key, sub = path.split(':', 1)
+        return deepget(mapping[key], sub)
+
+
+def deepset(mapping, path, value):
+    """Define deep entry in dict."""
+    if ':' not in path:
+        mapping[path] = value
+    else:
+        key, sub = path.split(':', 1)
+        submapping = mapping.setdefault(key, {})
+        deepset(submapping, sub, value)
