4695dfcb36cc9d9b2b8d4bc162b906fcc6aad526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:53 +0000

    Add package python-nambaone-bot/0.1/python-nambaone-bot-0.1.tar.gz

---
+++ packages/python-nambaone-bot/python-nambaone-bot-0.1.tar.gz/python-nambaone-bot-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'python-nambaone-bot',
+    packages = ['python-nambaone-bot'],
+    version = '0.1',
+    description = 'python-nambaone-bot',
+    author = 'erjanmx',
+    author_email = 'erjanmx@gmail.com',
+    url = 'https://github.com/erjanmx/python-nambaone-bot',
+    download_url = 'https://github.com/erjanmx/python-nambaone-bot/archive/0.1.tar.gz',
+    keywords = ['nambaone', 'bot'],
+    classifiers = [],
+)
