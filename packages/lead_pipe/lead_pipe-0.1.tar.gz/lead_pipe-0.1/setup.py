f3b41d8798edac39b53fe88ea9b867bad7fc340a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:19 +0000

    Add package lead_pipe/0.1/lead_pipe-0.1.tar.gz

---
+++ packages/lead_pipe/lead_pipe-0.1.tar.gz/lead_pipe-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name = 'lead_pipe',
+  packages = ['lead_pipe'],
+  version = '0.1',
+  description = 'A package for piping between functions in Python',
+  author = 'Brett Beatty',
+  author_email = 'brettbeatty@gmail.com',
+  url = 'https://github.com/brettbeatty/lead_pipe',
+  download_url = 'https://github.com/brettbeatty/lead_pipe/archive/0.1.tar.gz',
+  keywords = ['piping']
+)
