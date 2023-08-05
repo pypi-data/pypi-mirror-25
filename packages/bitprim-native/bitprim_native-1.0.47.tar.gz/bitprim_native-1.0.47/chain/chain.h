c22aff0798102d4b65de3fd10f679c5ed6021d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:34 +0000

    Add package bitprim-native/1.0.47/bitprim_native-1.0.47.tar.gz

---
+++ packages/bitprim-native/bitprim_native-1.0.47.tar.gz/bitprim_native-1.0.47/chain/chain.h
@@ -0,0 +1,36 @@
+#ifndef BITPRIM_PY_CHAIN_H_
+#define BITPRIM_PY_CHAIN_H_
+
+#include <Python.h>
+
+#ifdef __cplusplus
+extern "C" {  
+#endif  
+
+PyObject* bitprim_native_chain_fetch_block_by_height(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_block_by_hash(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_merkle_block_by_height(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_merkle_block_by_hash(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_block_header_by_height(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_block_header_by_hash(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_last_height(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_history(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_block_height(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_stealth(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_transaction(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_output(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_transaction_position(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_organize_block(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_organize_transaction(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_validate_tx(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_compact_block_by_hash(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_compact_block_by_height(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_fetch_spend(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_subscribe_reorganize(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_subscribe_transaction(PyObject* self, PyObject* args);
+
+#ifdef __cplusplus
+} //extern "C"
+#endif  
+
+#endif
