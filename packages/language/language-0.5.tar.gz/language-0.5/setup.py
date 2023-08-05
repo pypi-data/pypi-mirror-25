3d52ace70def921686e0ab8363e71d385520a216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:10 +0000

    Add package language/0.5/language-0.5.tar.gz

---
+++ packages/language/language-0.5.tar.gz/language-0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+  name = 'language',
+  packages = ['language'],
+  version = '0.5',
+  description = 'Basic tools for working with natural language text data',
+  author = 'Signal N',
+  author_email = 'language@signaln.com',
+  url = 'https://github.com/signaln/language',
+  download_url = 'https://github.com/signaln/language/archive/0.5.tar.gz',
+  keywords = ['natural language processing', 'text', 'language', 'ngrams'],
+  license='MIT',
+  classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
