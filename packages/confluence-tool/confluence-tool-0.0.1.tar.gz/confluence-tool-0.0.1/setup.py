81389f6c6d5dda98ac2d680f9db041f12d7ebd33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:08 +0000

    Add package confluence-tool/0.0.1/confluence-tool-0.0.1.tar.gz

---
+++ packages/confluence-tool/confluence-tool-0.0.1.tar.gz/confluence-tool-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='confluence-tool',
+    version='0.0.1',
+    description='Confluence API and CLI',
+    author='Kay-Uwe (Kiwi) Lorenz',
+    author_email='kiwi@franka.dyndns.org',
+    #url='https://www.python.org/sigs/distutils-sig/',
+    packages=['confluence_tool', ],
+    license="MIT",
+    )
