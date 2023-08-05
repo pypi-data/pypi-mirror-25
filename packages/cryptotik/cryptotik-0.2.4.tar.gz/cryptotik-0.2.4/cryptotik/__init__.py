2e3db996e8822f00f4b98666744e0feac733ceca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:45 +0000

    Add package cryptotik/0.2.4/cryptotik-0.2.4.tar.gz

---
+++ packages/cryptotik/cryptotik-0.2.4.tar.gz/cryptotik-0.2.4/cryptotik/__init__.py
@@ -0,0 +1,8 @@
+
+from cryptotik.poloniex import Poloniex
+from cryptotik.bittrex import Bittrex
+from cryptotik.wex import Wex
+from cryptotik.therock import TheRock
+from cryptotik.livecoin import Livecoin
+from cryptotik.okcoin import OKcoin
+from cryptotik.hitbtc import Hitbtc
---
+++ packages/cryptotik/cryptotik-0.2.4.tar.gz/cryptotik-0.2.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='cryptotik',
+      version='0.2.4',
+      description='Standardized common API for several cryptocurrency exchanges.',
+      url='https://github.com/peerchemist/cryptotik',
+      author='Peerchemist',
+      author_email='peerchemist@protonmail.ch',
+      license='GLP',
+      packages=['cryptotik'],
+      install_requires=['requests'],
+      tests_require=['pytest'],
+      zip_safe=False)
