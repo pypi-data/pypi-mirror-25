117fadb1a07a3bca897b626d00fccde0566cca94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:34 +0000

    Add package tox/2.8.2/tox-2.8.2-py2.py3-none-any.whl

---
+++ packages/tox/tox-2.8.2-py2.py3-none-any.whl/tox/__init__.py
@@ -0,0 +1,45 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+from .hookspecs import hookspec, hookimpl  # noqa
+
+try:
+    _full_version = get_distribution(__name__).version
+    __version__ = _full_version.split('+', 1)[0]
+except DistributionNotFound:
+    __version__ = '0.0.0.dev0'
+
+
+class exception:
+    class Error(Exception):
+        def __str__(self):
+            return "%s: %s" % (self.__class__.__name__, self.args[0])
+
+    class MissingSubstitution(Exception):
+        FLAG = 'TOX_MISSING_SUBSTITUTION'
+        """placeholder for debugging configurations"""
+        def __init__(self, name):
+            self.name = name
+
+    class ConfigError(Error):
+        """ error in tox configuration. """
+    class UnsupportedInterpreter(Error):
+        """signals an unsupported Interpreter"""
+    class InterpreterNotFound(Error):
+        """signals that an interpreter could not be found"""
+    class InvocationError(Error):
+        """ an error while invoking a script. """
+    class MissingFile(Error):
+        """ an error while invoking a script. """
+    class MissingDirectory(Error):
+        """ a directory did not exist. """
+    class MissingDependency(Error):
+        """ a dependency could not be found or determined. """
+    class MinVersionError(Error):
+        """ the installed tox version is lower than requested minversion. """
+
+        def __init__(self, message):
+            self.message = message
+            super(exception.MinVersionError, self).__init__(message)
+
+
+from tox.session import main as cmdline  # noqa
