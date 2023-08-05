f7a72b57c9f297fd6ade95cbee542eac56a5dded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:24 +0000

    Add package trackml/0.1.12/trackml-0.1.12.tar.gz

---
+++ packages/trackml/trackml-0.1.12.tar.gz/trackml-0.1.12/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name="trackml",
+    version="0.1.12",
+    url="https://github.com/track-ml/python-client/",
+
+    author="Gideon",
+    author_email="Gideon@semantica-labs.com",
+
+    description="An opinionated, minimal cookiecutter template for Python packages",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+
+    install_requires=["websocket-client>=0.44.0","requests>=2.18.4"],
+    test_requires=['websocket-server'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
