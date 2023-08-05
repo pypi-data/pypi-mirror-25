61a573aa99dd64eb40398a7bba88d5c0c82fb39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:24 +0000

    Add package generators/2017.9.23.1/generators-2017.9.23.1.tar.gz

---
+++ packages/generators/generators-2017.9.23.1.tar.gz/generators-2017.9.23.1/generators/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import sys
+import os.path
+
+sys.path.append(os.path.dirname(__file__))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'inline_tools'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'performance_tools'))
+
+import inline_tools
+import performance_tools
+
+from all_substrings import all_substrings
+from average import average
+from chain import chain
+from chunks import chunks
+from counter import counter
+from early_warning import early_warning
+from fork import fork
+from itemgetter import itemgetter
+from iter_kv import iter_kv
+from map import map
+from multi_ops import multi_ops
+from read import read
+from read_file import read_file
+from started import started
+from tee import tee
+from timer import timer
+from total import total
+from unfork import unfork
+from window import window
+
+__all__ = ['inline_tools', 'performance_tools', 'all_substrings', 'average', 'chain', 'chunks', 'counter', 'early_warning', 'fork', 'itemgetter', 'iter_kv', 'map', 'multi_ops', 'read', 'read_file', 'started', 'tee', 'timer', 'total', 'unfork', 'window']
