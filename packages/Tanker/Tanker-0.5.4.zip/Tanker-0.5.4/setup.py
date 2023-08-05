0d583a59379628a7caefa60024da19d0926a8c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:25 +0000

    Add package Tanker/0.5.4/Tanker-0.5.4.zip

---
+++ packages/Tanker/Tanker-0.5.4.zip/Tanker-0.5.4/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+import os
+
+import tanker
+
+description = '''
+Tanker goal is to allow easy batch operations without compromising
+database modeling. For pandas users, it's like DataFrame.to_sql on
+steroids.
+'''
+
+setup(name='Tanker',
+      version=tanker.__version__,
+      description=description,
+      author='Bertrand Chenal',
+      author_email='bertrand@adimian.com',
+      url='https://bitbucket.org/bertrandchenal/tanker',
+      license='MIT',
+      py_modules=['tanker'],
+      entry_points={
+          'console_scripts': [
+              'tk = tanker:cli',
+          ],
+      },
+  )
