daa74b2e423e9fc00a9ca2d9639f6b838027cb26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:52 +0000

    Add package tomato_commander/0.1.2/tomato_commander-0.1.2-py2.py3-none-any.whl

---
+++ packages/tomato_commander/tomato_commander-0.1.2-py2.py3-none-any.whl/tomato_commander/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Tomato Commander."""
+
+__author__ = """James Vogel"""
+__email__ = 'jim.m.vogel@gmail.com'
+__version__ = '0.1.2'
+
+from tomato_commander.libs.main import Router
