217924c0df99a1f460cf9bf0739584e02b7264dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:24 +0000

    Add package denv/1.0.5/denv-1.0.5.tar.gz

---
+++ packages/denv/denv-1.0.5.tar.gz/denv-1.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from os import path
+
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+setup(
+    name='denv',
+    version='1.0.5',
+    author='sloev',
+    author_email='jgv@trustpilot.com',
+    url='https://github.com/trustpilot/python-denv',
+    description='runs commands with env stored in dotfile',
+    long_description=read('README.rst'),
+    packages=['denv'],
+    entry_points={
+        'console_scripts': [
+            'denv=denv:main',
+        ],
+    },
+    license='MIT'
+)
