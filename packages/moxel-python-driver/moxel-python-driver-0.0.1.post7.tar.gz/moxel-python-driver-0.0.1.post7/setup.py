9a7ef971533ccdadfd6898341d3e06ba8032873c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:10 +0000

    Add package moxel-python-driver/0.0.1.post7/moxel-python-driver-0.0.1.post7.tar.gz

---
+++ packages/moxel-python-driver/moxel-python-driver-0.0.1.post7.tar.gz/moxel-python-driver-0.0.1.post7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open('moxel_python_driver/VERSION', 'r') as f:
+    version = f.read()
+    version = version.replace('\n', '')
+
+import pdb; pdb.set_trace();
+setup(name='moxel-python-driver',
+      version=version,
+      description='python driver for moxel models',
+      # url='www.moxel.ai',
+      author='moxel',
+      author_email='support@moxel.ai',
+      license='MIT',
+      zip_safe=False,
+      packages=['moxel_python_driver'],
+      include_package_data=True,
+      package_data={'moxel_python_driver': ['moxel_python_driver/VERSION'] },
+      entry_points={
+          "console_scripts": [
+              "moxel-python-driver = moxel_python_driver.driver:main",
+          ]
+      }
+)
