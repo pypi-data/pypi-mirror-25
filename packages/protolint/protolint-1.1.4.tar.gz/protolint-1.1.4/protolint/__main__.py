ec083bf7252976067c8032a623a1fe009df3130c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:03 +0000

    Add package protolint/1.1.4/protolint-1.1.4.tar.gz

---
+++ packages/protolint/protolint-1.1.4.tar.gz/protolint-1.1.4/protolint/__main__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+  protolint: entrypoint
+  ~~~~~~~~~~~~~~~~~~~~~
+
+"""
+
+import sys
+import pprint
+
+from . import cli
+from . import config
+from . import linter
+from . import output
+
+
+# declare globals
+linter_config = None
+
+
+def run_tool():
+
+  """ Run the CLI tool. """
+
+  global linter_config
+
+  args = cli.parser.parse_args()
+
+  if not args.config:  # pragma: no cover
+    output.error("Must provide argument 'config'. See --help for more.")
+    sys.exit(1)
+  if not args.workspace:  # pragma: no cover
+    output.error("Must provide argument 'workspace'. See --help for more.")
+    sys.exit(1)
+
+  filepath, workspace = (args.config, args.workspace)
+
+  output.info('Preparing to scan workspace "%s"...' % workspace)
+  linter_config = config.LinterConfig(filepath, workspace)
+
+  protolint = linter.Linter(linter_config, args)
+
+  for issue in protolint():
+    issue.write()
+
+  output.info('All done.')
+  sys.exit(0)
+
+if __name__ == "__main__": run_tool()
