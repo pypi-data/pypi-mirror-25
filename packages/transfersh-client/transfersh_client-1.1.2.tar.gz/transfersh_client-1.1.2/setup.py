5d4140dcc59f24385babf77b5ca64d44c42dba2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:46 +0000

    Add package transfersh-client/1.1.2/transfersh_client-1.1.2.tar.gz

---
+++ packages/transfersh-client/transfersh_client-1.1.2.tar.gz/transfersh_client-1.1.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+        name="transfersh_client",
+        version="1.1.2",
+        author="Arsen Losenko",
+        author_email="arsenlosenko@gmail.com",
+        description="transfer.sh command-line client",
+        url="https://github.com/arsenlosenko/transfer.sh_client.git",
+        keywords="transfer.sh command-line client tool utility cli CLI",
+        license="MIT",
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Utilities',
+            'Environment :: Console',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.5'
+        ],
+        packages=find_packages(),
+        long_description=open(join(dirname(__file__), "README.rst")).read(),
+        install_requires=[
+            'requests==2.16.0',
+            'pyperclip==1.5.27',
+        ],
+        entry_points={
+                "console_scripts":
+                ['transfer_files = transfersh_client.app:main'],
+            },
+    )
+
+
