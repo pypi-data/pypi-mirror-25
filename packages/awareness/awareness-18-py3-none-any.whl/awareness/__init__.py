9d6773aa7b5691b72f7d42000345a2471a349034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:06 +0000

    Add package awareness/18/awareness-18-py3-none-any.whl

---
+++ packages/awareness/awareness-18-py3-none-any.whl/awareness/__init__.py
@@ -0,0 +1,85 @@
+# 
+#                                                    ,▄
+#                                                  ╓█▀    ,,
+#                                                  █⌐   ▄█▀╙▀█
+#                                                  ╙▀▓▓▀└   ,█         ╔
+#                                               ╓▄∩      ,▄▄▀         é╫▌
+#                                             ╓█╙    ╓▄, ╙`          ╬╫╫╫╬,
+#                                             █▄  ,▄█▀`▀█          ,╣╫╫╫╫╫╫¼
+#                                              ▀▀▀▀    ,█         ╔╫╫╫╫╫╫╫╫╫N
+#                                           ╓▄;     ▄▄▓▀         ╬╫╫╫╫╫M╣╫╫╫╫▌
+#                                        ╓█▀'`▐█                ╬╫╫╫╫╫┘  ╬╫╫╫╫╫╕
+#                                        █   ▄█   ▐█          ╓╣╫╫╫╫▌`    ╟╫╫╫╫╫N
+#                                        █▄ ▄▀    ▐█         ╔╫╫╫╫╫M       ╙╫╫╫╫╫Φ
+#                                         ▀██µ,,▄▓▀         ╬╫╫╫╫╫M         `╣╫╫╫╫╬,
+#                                   ,▄▓▄▄µ   "╙╙└         ,╣╫╫╫╫╣`            ╬╫╫╫╫╫µ
+#                                  ▄█    ▀▀▓▄,           ╔╣╫╫╫╫▌               ╚╫╫╫╫╫N
+#                                  █        `▀▀         á╫╫╫╫╫M                 ╙╫╫╫╫╫▌
+#                                 ▀██▄                 ╬╫╫╫╫╫╛                    ╣╫╫╫╫╬,
+#                                    "▀█▄ç           ,╣╫╫╫╫╣`                      ╟╫╫╫╫╫¼
+#                             ▄▓▀▀▀█∩    ╙▀         ╔╫╫╫╫╫Å                         ╚╫╫╫╫╫N
+#                            █▀   █▀   ▄           ╬╫╫╫╫╫M                           '╣╫╫╫╫╬
+#                            █  ,█^    █          ╬╫╫╫╫╫┘                              ╬╫╫╫╫╬╕
+#                         ▄w ╙█▄█    ╓█▀        ╔╣╫╫╫╫╣`                                ╟╫╫╫╫╫╗
+#                        ▐▌    `▀▀▀▀▀▀         ╔╫╫╫╫╫M                                   ╙╫╫╫╫╫Φ
+#                       ▄▄█                   ╬╫╫╫╫╫M                                     `╣╫╫╫╫╬,
+#                        ╙▀█▄▄              ,╣╫╫╫╫╣`                                        ╬╫╫╫╫╫µ
+#                     ▓▄,    ╙▀█▄          ╓╣╫╫╫╫▌                                           ╚╫╫╫╫╫N
+#                   ▄▓▀▀▀▀▓▄,             ╔╫╫╫╫╫M                                             ╙╫╫╫╫╫▌
+#                  █▀      ╙██▓          ╬╫╫╫╫╫╛                                               `╣╫╫╫╫╬,
+#                  █        ▐▌         ,╣╫╫╫╫╣`                                                  ╟╫╫╫╫╫¼
+#                  ╙█▄     ╓█╛        ╔╫╫╫╫╫▌                                                     ╙╫╫╫╫╫N
+#               ▀█   `▀▀▀▀▀▀         é╫╫╫╫╫M                                                       "╣╫╫╫╫▌
+#                 ▀█                ╬╫╫╫╫╫┘                                                          ╣╫╫╫╫╬╕
+#                  `▀▌            ╓╣╫╫╫╫╣`                                                            ╟╫╫╫╫╫╗
+#             ╙██▀▀▀▀██▌         ╔╫╫╫╫╫Å                                                               ╙╫╫╫╫╫N
+#               ▀▓              ╬╫╫╫╫╫M                                                                 `╣╫╫╫╫╣
+#        ▀▀▀▓▓▄▄▄▄█▄          ,╬╫╫╫╫╫^                                                                    ╬╫╫╫╫╫╕
+#       ▄▄       `"▀`        ╓╣╫╫╫╫▌                                                                       ╚╫╫╫╫╫N
+#     ▄▄▀▀▀█▄µ              ╔╫╫╫╫╫M                                                                         ╙╫╫╫╫╫╬
+#    █▀      ▀██▄          ╬╫╫╫╫╫M                                                                           `╣╫╫╫╫╬,
+#   ▐█        ║▌         ,╣╫╫╫╫╣`                                                                              ╢╫╫╫╫╫¼
+#    ▀█µ     ,█⌐        ╔╫╫╫╫╫▌                                                                                 ╚╫╫╫╫╫N
+#      ▀▀▓▓▓▀▀         é╫╫╫╫╫╫╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╗╬╫╫╫╫╫▌
+#                     ╬╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╬,
+#                   ,╣╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╫╗
+#
+#
+#
+#
+# Copyright (c) 2016-2017 Aedan S. Cullen.
+#
+# This file is part of the Awareness Python implementation.
+#
+# Awareness is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Awareness is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Awareness.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+from . import component
+from . import algorithm
+from . import backend
+from . import data
+from . import operator
+from . import protocol
+from . import misc
+from . import factory
+
+from .operator import LocalOperator as LocalOperator
+from .operator import RemoteOperator as RemoteOperator
+from .component import LocalComponent as LocalComponent
+from .component import RemoteComponent as RemoteComponent
+
+from .data import Stream as Stream
+from .data import Set as Set
+from .data import Assembly as Assembly
