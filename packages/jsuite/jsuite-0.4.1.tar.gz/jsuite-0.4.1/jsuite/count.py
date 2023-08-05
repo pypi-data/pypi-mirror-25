995e80afb3ab74160856c9302904c661b791e4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:04 +0000

    Add package jsuite/0.4.1/jsuite-0.4.1.tar.gz

---
+++ packages/jsuite/jsuite-0.4.1.tar.gz/jsuite-0.4.1/jsuite/count.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger('jsuite')
+
+def of_type_in_body(etree, type):
+  xpath_query = './body//%s' %(type)
+  return len(etree.findall(xpath_query))
+
+def of_type(etree, type):
+  xpath_query = './/%s' %(type)
+  return len(etree.findall(xpath_query))
