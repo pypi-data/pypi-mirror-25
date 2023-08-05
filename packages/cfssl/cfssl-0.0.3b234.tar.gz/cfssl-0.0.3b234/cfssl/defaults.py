e586f68b1c802726ae7228068744b88e33b60577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:54 +0000

    Add package cfssl/0.0.3b234/cfssl-0.0.3b234.tar.gz

---
+++ packages/cfssl/cfssl-0.0.3b234.tar.gz/cfssl-0.0.3b234/cfssl/defaults.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License MIT (https://opensource.org/licenses/MIT).
+
+from datetime import timedelta
+
+
+DEFAULT_ALGORITHM = 'rsa'
+DEFAULT_STRENGTH = 4096
+DEFAULT_EXPIRE_DELTA = timedelta(days=365)
---
+++ packages/cfssl/cfssl-0.0.3b234.tar.gz/cfssl-0.0.3b234/cfssl/models/config_client.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License MIT (https://opensource.org/licenses/MIT).
+
+from .config_mixer import ConfigMixer
+from ..utils import to_api
+
+
+class ConfigClient(ConfigMixer):
+    """ It provides a Client Config compatible with CFSSL. """
+
+    def __init__(self, sign_policy_default,
+                 sign_policies_add, auth_policies, remotes):
+        """ Initialize a new Client Configuration.
+
+        Args:
+            sign_policy_default (PolicySign): Default signing
+                policy for client to use.
+            sign_policies_add (tuple of PolicySign):
+                Additional signing policies to use for the client.
+            auth_policies (tuple of PolicyAuth): Auth
+                policies for the client.
+            remotes (tuple of Host): Remote hosts that
+                client trusts.
+        """
+        super(ConfigClient, self).__init__(
+            sign_policy_default, auth_policies, remotes,
+        )
+        self.remotes = remotes
+
+    def to_api(self):
+        """ It returns an object compatible with the API. """
+        res = super(ConfigClient, self).to_api()
+        res['remotes'] = {
+            r.name: to_api(r) for r in self.remotes
+        }
+        return res
