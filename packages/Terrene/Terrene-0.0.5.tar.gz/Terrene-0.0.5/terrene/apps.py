9bc2b36da176eb1bb9c5b1207498bc9853d5ab6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:23 +0000

    Add package Terrene/0.0.5/Terrene-0.0.5.tar.gz

---
+++ packages/Terrene/Terrene-0.0.5.tar.gz/Terrene-0.0.5/terrene/apps.py
@@ -0,0 +1,58 @@
+from .api import BaseModelManager, BaseModel
+
+
+class BaseApp(BaseModel):
+    @property
+    def credentials(self):
+        return self.act(['credentials', 'read'], {'object_id': self.object_id})
+
+
+class BaseAppManager(BaseModelManager):
+    model = BaseApp
+    namespace = ['apps']
+
+    def __init__(self, *args, **kwargs):
+        super(BaseAppManager, self).__init__(*args, **kwargs)
+        self.workspace = kwargs.pop('workspace', None)
+
+        if self.workspace is None:
+            raise AssertionError("Missing workspace parameter on initialization")
+
+    def pre_create(self, **params):
+        params['workspace'] = self.workspace.object_id
+        return params
+
+    def pre_save(self, **params):
+        if not isinstance(self.workspace, str):
+            params['workspace'] = params['workspace'].object_id
+
+
+class Workspace(BaseModel):
+    def add_owner(self, email):
+        return self.act(['owners', 'create'], {
+            'object_id': self.object_id, 'user': email})
+
+    def remove_owner(self, email):
+        return self.act(['owners', 'partial_update'], {
+            'object_id': self.object_id, 'user': email})
+
+    def add_contributor(self, email):
+        return self.act(['contributors', 'create'], {
+            'object_id': self.object_id, 'user': email})
+
+    def remove_contributor(self, email):
+        return self.act(['contributors', 'partial_update'], {
+            'object_id': self.object_id, 'user': email})
+
+    def set_payment_method(self, payment_method_object_id):
+        return self.act(['set_payment_method'], {
+            'object_id': self.object_id, 'method': payment_method_object_id})
+
+    def notify(self, subject, content):
+        return self.act(['notify'], {
+            'object_id': self.object_id, 'subject': subject, 'content': content})
+
+
+class WorkspaceManager(BaseModelManager):
+    model = Workspace
+    namespace = ['workspaces']
