12e5402a7d1728dfcbce877e28a7f70ba15c1655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package parallelio/0.9/parallelio-0.9.tar.gz

---
+++ packages/parallelio/parallelio-0.9.tar.gz/parallelio-0.9/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+  name = 'parallelio',
+  packages = ['parallelio'],
+  version = '0.9',
+  description = 'Basic tools for working with natural language text data',
+  author = 'Signal N',
+  author_email = 'parallelio@signaln.com',
+  url = 'https://github.com/signaln/parallelio',
+  download_url = 'https://github.com/signaln/parallelio/archive/0.9.tar.gz',
+  keywords = ['text', 'data', 'files', 'io', 'natural language processing'],
+  license='MIT',
+  classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
