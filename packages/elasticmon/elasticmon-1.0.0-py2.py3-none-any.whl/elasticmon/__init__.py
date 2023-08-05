974a97694a40f2e782755397ee30cd72c9e1cc90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:11 +0000

    Add package elasticmon/1.0.0/elasticmon-1.0.0-py2.py3-none-any.whl

---
+++ packages/elasticmon/elasticmon-1.0.0-py2.py3-none-any.whl/elasticmon/__init__.py
@@ -0,0 +1,37 @@
+from elasticmon.client import ElasticmonClient
+from argparse import ArgumentParser
+
+
+def main():
+    parser = ArgumentParser(description='Elasticsearch monitoring client')
+    parser.add_argument(
+        '--verbose',
+        '-v',
+        action='count',
+        dest='verbosity',
+        help=
+        'verbose mode (-v for warnings, -vv for informational, -vvv for debug')
+    parser.add_argument(
+        '--es-hosts',
+        '-e',
+        dest='es_hosts',
+        nargs='+',
+        help='elasticsearch host(s) to retrieve information from')
+    parser.add_argument(
+        '--environment',
+        '-n',
+        dest='environment',
+        nargs='?',
+        help='the environment in which this tool is running')
+
+    args = parser.parse_args()
+
+    mon_client = ElasticmonClient(
+        environment=args.environment,
+        verbosity=args.verbosity,
+        hosts=args.es_hosts)
+    chealth = mon_client.cluster_health()
+    mon_client.print_cluster_health_flattened(j=chealth)
+
+    nstats = mon_client.node_stats()
+    mon_client.print_node_stats_flattened(j=nstats)
