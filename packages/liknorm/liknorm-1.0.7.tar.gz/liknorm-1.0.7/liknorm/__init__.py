227b366518d41ed8740dd86dd9aa6e94d3511dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:09 +0000

    Add package liknorm/1.0.7/liknorm-1.0.7.tar.gz

---
+++ packages/liknorm/liknorm-1.0.7.tar.gz/liknorm-1.0.7/liknorm/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import as _
+
+import _cffi_backend as _
+
+from ._test import test
+from .machine import LikNormMachine
+
+__name__ = "liknorm"
+__version__ = "1.0.7"
+__author__ = "Danilo Horta"
+__author_email__ = "horta@ebi.ac.uk"
+
+__all__ = [
+    "__name__", "__version__", "__author__", "__author_email__", "test",
+    "LikNormMachine"
+]
