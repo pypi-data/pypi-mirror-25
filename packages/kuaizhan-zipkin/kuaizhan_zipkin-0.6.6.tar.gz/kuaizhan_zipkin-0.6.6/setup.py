0e41b83cd6ed816ec729ac54c9c08591eb9526f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:10 +0000

    Add package kuaizhan-zipkin/0.6.6/kuaizhan_zipkin-0.6.6.tar.gz

---
+++ packages/kuaizhan-zipkin/kuaizhan_zipkin-0.6.6.tar.gz/kuaizhan_zipkin-0.6.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kuaizhan_zipkin',
+    version='0.6.6',
+    keywords='kuaizhan zipkin',
+    description='kuaizhan zipkin sender',
+    license='MIT License',
+    url='',
+    author='xuchen',
+    author_email='chenxu211618@sohu-inc.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=['thriftpy', 'py_zipkin', 'kafka', 'bottle'],
+)
