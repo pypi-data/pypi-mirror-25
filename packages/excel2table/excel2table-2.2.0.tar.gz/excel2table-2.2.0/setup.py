d14a32ffecc7b5a5619078c9def0ff66edf7634f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:54 +0000

    Add package excel2table/2.2.0/excel2table-2.2.0.tar.gz

---
+++ packages/excel2table/excel2table-2.2.0.tar.gz/excel2table-2.2.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+_version = "2.2.0"
+
+setup(
+    name="excel2table",
+    version=_version,
+    description="Simple commandline utility to convert excel files"
+    "to searchable and sortable HTML table.",
+    author="Vivek R",
+    author_email="vividvilla@gmail.com",
+    maintainer="C. W.",
+    maintainer_email="wangc_2011@hotmail.com",
+    url="https://github.com/pyexcel/exceltotable",
+    packages=["excel2table"],
+    include_package_data=True,
+    download_url="https://github.com/pyexcel/exceltotable/archive/{}.tar.gz"
+        .format(_version),
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries"
+    ],
+    install_requires=["click >= 6.7", "jinja2 >= 2.9.6",
+                      "pyexcel >= 0.5.0", "six >= 1.10.0",
+                      "pyexcel-xls >= 0.4.0", "pyexcel-odsr >= 0.4.0"],
+    entry_points={
+        "console_scripts": [
+            "excel2table = excel2table.cli:cli",
+            ]
+    }
+)
