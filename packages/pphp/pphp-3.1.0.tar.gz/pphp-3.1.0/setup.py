4317372b83c996964e582d93ddd472f6b5c9ef72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:41 +0000

    Add package pphp/3.1.0/pphp-3.1.0.tar.gz

---
+++ packages/pphp/pphp-3.1.0.tar.gz/pphp-3.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+f = open('README.rst', 'r')
+long_description = f.read()
+f.close()
+
+setup(
+	name='pphp',
+	version='3.1.0',
+	description='A spinoff of PHP in Python',
+	long_description=long_description,
+	url='https://kenny2github.github.io/pphp',
+	author='Ken Hilton',
+	author_email='kenny2minecraft@gmail.com',
+        py_modules=['pphp'],
+	license='GNU GPLv3',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Programming Language :: Python :: 2.7',
+	],
+	keywords='http php server',
+	python_requires='<3'
+)
