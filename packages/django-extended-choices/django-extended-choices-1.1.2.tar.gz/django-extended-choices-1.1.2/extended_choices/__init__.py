81b49ccb646e3e46769925f2b251d38444b9c3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:02 +0000

    Add package django-extended-choices/1.1.2/django-extended-choices-1.1.2.tar.gz

---
+++ packages/django-extended-choices/django-extended-choices-1.1.2.tar.gz/django-extended-choices-1.1.2/extended_choices/__init__.py
@@ -0,0 +1,11 @@
+"""Little helper application to improve django choices (for fields)"""
+
+from __future__ import unicode_literals
+
+from .choices import Choices, OrderedChoices
+
+__all__ = ['Choices', 'OrderedChoices']
+__author__ = 'Stephane "Twidi" Ange;'
+__contact__ = "s.angel@twidi.com"
+__homepage__ = "https://pypi.python.org/pypi/django-extended-choices"
+__version__ = "1.1.2"
