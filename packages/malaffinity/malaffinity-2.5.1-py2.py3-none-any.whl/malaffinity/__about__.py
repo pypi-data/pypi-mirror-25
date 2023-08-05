778b3f7d7df1c599d8a86d4cc79a1d4eabc1a4e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:49 +0000

    Add package malaffinity/2.5.1/malaffinity-2.5.1-py2.py3-none-any.whl

---
+++ packages/malaffinity/malaffinity-2.5.1-py2.py3-none-any.whl/malaffinity/__about__.py
@@ -0,0 +1,20 @@
+"""malaffinity config."""
+
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__",
+    "__author__", "__email__", "__license__", "__copyright__"
+]
+
+
+__title__ = "malaffinity"
+__summary__ = "Calculate affinity between MyAnimeList users"
+__uri__ = "https://github.com/erkghlerngm44/malaffinity"
+
+__version__ = "2.5.1"
+
+__author__ = "erkghlerngm44"
+__email__ = "erkghlerngm44@protonmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (C) 2017, erkghlerngm44"
