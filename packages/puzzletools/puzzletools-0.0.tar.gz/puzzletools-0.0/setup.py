656b81ff9e8030ccac2a55d71413d31d137112ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:26 +0000

    Add package puzzletools/0.0/puzzletools-0.0.tar.gz

---
+++ packages/puzzletools/puzzletools-0.0.tar.gz/puzzletools-0.0/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(name = 'puzzletools',
+                 packages = ['puzzletools'],
+                 url = 'https://github.com/dhashe/puzzletools',
+                 author = 'David Hashe',
+                 author_email = '6146@dhashe.com',
+                 version = '0.0')
