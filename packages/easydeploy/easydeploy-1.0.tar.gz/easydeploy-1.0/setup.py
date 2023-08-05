09cfb44375b453717527492dc691b1704aeb187d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:15 +0000

    Add package easydeploy/1.0/easydeploy-1.0.tar.gz

---
+++ packages/easydeploy/easydeploy-1.0.tar.gz/easydeploy-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easydeploy',
+    version='1.0',
+    packages=['easydeploy'],
+    url='https://github.com/rodrigorodriguescosta/easydeploy.git',
+    license='',
+    author='Rodrigo Rodrigues',
+    author_email='rodrigorodriguescosta@gmail.com',
+    description=''
+)
