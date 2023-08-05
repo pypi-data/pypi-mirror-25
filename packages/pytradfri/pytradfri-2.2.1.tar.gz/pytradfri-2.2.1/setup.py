e215c5fc8bcd2fecb915fe6541a9a4159e2af5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:54 +0000

    Add package pytradfri/2.2.1/pytradfri-2.2.1.tar.gz

---
+++ packages/pytradfri/pytradfri-2.2.1.tar.gz/pytradfri-2.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = "2.2.1"
+DOWNLOAD_URL = \
+    'https://github.com/ggravlingen/pytradfri/archive/{}.zip'.format(VERSION)
+
+EXTRAS_REQUIRE = {
+    'async': ['aiocoap >= 0.3']
+}
+
+DEP_LINKS = [
+    "http://github.com/chrysn/aiocoap/tarball/master#egg=aiocoap-0.3"
+]
+
+PACKAGES = find_packages(exclude=['tests', 'tests.*'])
+
+setup(
+  name='pytradfri',
+  packages=PACKAGES,
+  python_requires='>=3.4',
+  version=VERSION,
+  description='A python library for communicating with the Tradfri Gateway',
+  author='balloob, ggravlingen',
+  author_email='no@email.com',
+  url='https://github.com/ggravlingen/pytradfri',
+  download_url=DOWNLOAD_URL,
+  extras_require=EXTRAS_REQUIRE,
+  dependency_links=DEP_LINKS,
+)
