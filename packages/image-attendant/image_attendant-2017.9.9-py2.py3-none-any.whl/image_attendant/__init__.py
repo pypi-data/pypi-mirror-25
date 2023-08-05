19099307978816c33033f211bc6c8766eccf1f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:22 +0000

    Add package image-attendant/2017.9.9/image_attendant-2017.9.9-py2.py3-none-any.whl

---
+++ packages/image-attendant/image_attendant-2017.9.9-py2.py3-none-any.whl/image_attendant/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import division, print_function, unicode_literals, absolute_import
+
+from .image_io import read, write, download
+from .compression import compress, decompress
+from .mpl import figure_to_image, figure_to_file
+from .utility import image_data_mode, setup_uint8, data_url, collapse_alpha
