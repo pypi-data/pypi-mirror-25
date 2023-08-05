38678f8ea2dad812585e80941735731e38d059ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:30 +0000

    Add package OrgModeIO/0.1.2/OrgModeIO-0.1.2.tar.gz

---
+++ packages/OrgModeIO/OrgModeIO-0.1.2.tar.gz/OrgModeIO-0.1.2/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='OrgModeIO',
+    version='0.1.2',
+    description='This is a python library for parsing Emacs org-mode files and write to html or others',
+    author='auzn',
+    author_email='auzn.cn@gmail.com',
+    url='https://github.com/auzn/OrgModeIO',
+    keywords='org mode',
+    license='MIT'
+)
