ff313d64ecb56c1ec49056c527088da743785ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:12 +0000

    Add package utipy/0.2.5/utipy-0.2.5.tar.gz

---
+++ packages/utipy/utipy-0.2.5.tar.gz/utipy-0.2.5/utipy/__init__.py
@@ -0,0 +1,16 @@
+# outer __init__.py
+
+from .pandas.drop import drop
+from .pandas.makes_up import makes_up
+from .pandas.resemble import resemble
+from .pandas.distort import distort
+from .pandas.subset_by_levels import subset_by_levels
+from .pandas.polynomializer import polynomializer
+
+from .groups.fold import fold
+from .groups.group_uniques import group_uniques
+from .groups.group import group
+from .groups.partition import partition
+
+from .array.blend import blend
+from .array.windowed_reverse import windowed_reverse
