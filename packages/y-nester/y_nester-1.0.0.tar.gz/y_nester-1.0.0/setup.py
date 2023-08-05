87d2dd179e02d3aba806932868a9f818f12bc9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:11 +0000

    Add package y-nester/1.0.0/y_nester-1.0.0.tar.gz

---
+++ packages/y-nester/y_nester-1.0.0.tar.gz/y_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'y_nester',
+    version     = '1.0.0',
+    py_modules  = ['y_nester'],
+    author      = 'cosmic',
+    author_email= 'yangguanyu_2006@126.com',
+    url         = '',
+    description = 'A simple printer of nested lists',
+)
