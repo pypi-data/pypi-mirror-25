03fb13611cd36eba84be95428ab87403906c3797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:16 +0000

    Add package pathdict/0.0.2/pathdict-0.0.2.tar.gz

---
+++ packages/pathdict/pathdict-0.0.2.tar.gz/pathdict-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.2'
+
+
+setup(name='pathdict',
+      version=VERSION,
+      packages=find_packages(exclude=['*test*']),
+      url='https://github.com/diogommartins/pathdict',
+      author='Diogo Magalhães Martins',
+      author_email='magalhaesmartins@icloud.com',
+      keywords='collection dictionary dict path dotted',
+      long_description_markdown_filename='README.md')
