21d3fdccdae7bba6ee7d406f450e8a27297768ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:27 +0000

    Add package declaration/0.1.2/declaration-0.1.2.tar.gz

---
+++ packages/declaration/declaration-0.1.2.tar.gz/declaration-0.1.2/README.md
@@ -0,0 +1,42 @@
+# Declaration
+Declarative models and fields for Python objects, similar to how many Python ORMs work. Includes automatic support for JSON encoding with no custom encoder required.
+
+[![Build Status](https://travis-ci.org/boomletsgo/declaration.svg?branch=master)](https://travis-ci.org/boomletsgo/declaration)
+[![PyPI](https://img.shields.io/pypi/v/declaration.svg)](https://pypi.python.org/pypi/declaration/)
+[![Python Versions](https://img.shields.io/pypi/pyversions/declaration.svg)](https://pypi.python.org/pypi/declaration/)
+[![Coverage Status](https://coveralls.io/repos/boomletsgo/declaration/badge.svg?branch=master)](https://coveralls.io/r/boomletsgo/declaration?branch=master)
+
+
+
+
+## Installation
+
+`$ pip install declaration`
+
+## Usage
+
+Inside your application file:
+
+```
+import datetime
+import json
+
+from declaration import fields, models
+
+class Person(models.DeclarativeBase):
+	first_name = fields.StringField()
+	last_name = fields.StringField()
+	join_date = fields.DateTimeField()
+	# See fields.py for a complete list of fields
+	
+me = Person()
+me.first_name = "Joe"
+me.last_name = "Somebody"
+me.join_date = datetime.datetime.now()
+
+json.dumps(me)
+```
+
+The normal use case for declarative models and fields is for ORMs and business objects. You could use it for APIs and SDKs, or to pass data between systems via JSON. All JSON encoding is done automatically based on the field type you use.
+
+If you need a new field or have a suggestion, please create an issue or PR! 
---
+++ packages/declaration/declaration-0.1.2.tar.gz/declaration-0.1.2/declaration/fields.py
@@ -0,0 +1,85 @@
+import datetime
+import uuid
+
+from dateutil import parser
+import six
+
+
+class DeclarativeField(object):
+
+    def __init__(self):
+        self.value = None
+
+    def parse(self, value):
+        raise NotImplementedError
+
+    def encode(self, value):
+        raise NotImplementedError
+
+
+class GenericField(DeclarativeField):
+
+    def parse(self, value):
+        return value
+
+    def encode(self, value):
+        return value
+
+
+class UUIDField(GenericField):
+
+    def parse(self, value):
+        try:
+            value = uuid.UUID(value, version=4)
+        except ValueError:
+            pass
+
+        return value
+
+    def encode(self, value):
+        if isinstance(value, uuid.UUID):
+            return str(value)
+
+        return value
+
+
+class StringField(GenericField):
+
+    def parse(self, value):
+        return str(value)
+
+    def encode(self, value):
+        return str(value)
+
+
+class DateTimeField(GenericField):
+
+    def parse(self, value):
+        if not isinstance(value, datetime.datetime):
+            value = parser.parse(value)
+
+        return value
+
+    def encode(self, value):
+        return value.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+class DateField(DateTimeField):
+
+    def parse(self, value):
+        if isinstance(value, six.string_types):
+            value = parser.parse(value)
+
+        if isinstance(value, datetime.datetime):
+            value = value.date()
+
+        return value
+
+
+class TimeField(DateTimeField):
+
+    def parse(self, value):
+        if isinstance(value, datetime.datetime):
+            value = value.time()
+
+        return value
