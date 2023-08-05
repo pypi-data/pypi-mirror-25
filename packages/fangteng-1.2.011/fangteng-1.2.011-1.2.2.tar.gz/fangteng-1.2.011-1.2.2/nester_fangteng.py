db62634f4d993643af8501a675a5f3380483f7a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:36 +0000

    Add package fangteng-1.2.011/1.2.2/fangteng-1.2.011-1.2.2.tar.gz

---
+++ packages/fangteng-1.2.011/fangteng-1.2.011-1.2.2.tar.gz/fangteng-1.2.011-1.2.2/nester_fangteng.py
@@ -0,0 +1,8 @@
+﻿def print_lol(the_list,level=0):
+	for i in the_list:
+		if isinstance(i,list):
+			print_lol(i,level)
+		else:
+			for tab_stop in range(level):
+				print("\t",end='')
+			print(i)
---
+++ packages/fangteng-1.2.011/fangteng-1.2.011-1.2.2.tar.gz/fangteng-1.2.011-1.2.2/setup_fangteng.py
@@ -0,0 +1,10 @@
+﻿from distutils.core import setup
+setup(
+	name='fangteng-1.2.011',
+	version='1.2.2',
+	py_modules=['nester_fangteng'],
+	author='hfpython',
+	author_email='10583848@qq.com',
+	url='http://www.headfirstlabs.com',
+	description='A simpe printer of nested lists',
+	)
