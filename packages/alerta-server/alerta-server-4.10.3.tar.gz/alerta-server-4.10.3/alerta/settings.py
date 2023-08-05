b7c3bab1d826a2afd5d01eb6fbe6f39c32823f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:17 +0000

    Add package alerta-server/4.10.3/alerta-server-4.10.3.tar.gz

---
+++ packages/alerta-server/alerta-server-4.10.3.tar.gz/alerta-server-4.10.3/alerta/settings.py
@@ -0,0 +1,91 @@
+#
+# ***** ALERTA SERVER DEFAULT SETTINGS -- DO NOT MODIFY THIS FILE *****
+#
+# To override these settings use /etc/alertad.conf or the contents of the
+# configuration file set by the environment variable ALERTA_SVR_CONF_FILE.
+#
+# Further information on settings can be found at http://docs.alerta.io
+
+DEBUG = False
+
+BASE_URL = ''
+LOGGER_NAME = 'alerta'
+LOG_FILE = None
+
+SECRET_KEY = 'changeme'
+
+QUERY_LIMIT = 10000  # maximum number of alerts returned by a single query
+HISTORY_LIMIT = 100  # cap the number of alert history entries
+
+# MongoDB
+DATABASE_ENGINE = 'mongo'
+MONGO_URI = 'mongodb://localhost:27017/monitoring'
+MONGO_DATABASE = None  # can be used to override default database, above
+
+AUTH_REQUIRED = False
+ADMIN_USERS = []
+USER_DEFAULT_SCOPES = ['read', 'write']  # Note: 'write' scope implicitly includes 'read'
+CUSTOMER_VIEWS = False
+
+OAUTH2_CLIENT_ID = None  # Google or GitHub OAuth2 client ID and secret
+OAUTH2_CLIENT_SECRET = None
+ALLOWED_EMAIL_DOMAINS = ['*']
+
+GITHUB_URL = None
+ALLOWED_GITHUB_ORGS = ['*']
+
+GITLAB_URL = None
+ALLOWED_GITLAB_GROUPS = ['*']
+
+KEYCLOAK_URL = None
+KEYCLOAK_REALM = None
+ALLOWED_KEYCLOAK_ROLES = ['*']
+
+# TODO: Add SAML default config
+
+TOKEN_EXPIRE_DAYS = 14
+API_KEY_EXPIRE_DAYS = 365  # 1 year
+
+# switches
+AUTO_REFRESH_ALLOW = 'ON'  # set to 'OFF' to reduce load on API server by forcing clients to manually refresh
+SENDER_API_ALLOW = 'ON'    # set to 'OFF' to block clients from sending new alerts to API server
+
+CORS_ALLOW_HEADERS = ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin']
+CORS_ORIGINS = [
+    'http://try.alerta.io',
+    'http://explorer.alerta.io',
+    'http://localhost'
+]
+CORS_SUPPORTS_CREDENTIALS = AUTH_REQUIRED
+
+SEVERITY_MAP = {
+    'security': 0,
+    'critical': 1,
+    'major': 2,
+    'minor': 3,
+    'warning': 4,
+    'indeterminate': 5,
+    'cleared': 5,
+    'normal': 5,
+    'ok': 5,
+    'informational': 6,
+    'debug': 7,
+    'trace': 8,
+    'unknown': 9
+}
+DEFAULT_SEVERITY = 'indeterminate'
+
+BLACKOUT_DURATION = 3600  # default period = 1 hour
+
+EMAIL_VERIFICATION = False
+SMTP_HOST = 'smtp.gmail.com'
+SMTP_PORT = 587
+MAIL_FROM = 'your@gmail.com'  # replace with valid sender address
+SMTP_PASSWORD = ''  # password for MAIL_FROM account, Gmail uses application-specific passwords
+
+# Plug-ins
+PLUGINS = ['reject']
+
+ORIGIN_BLACKLIST = []
+#ORIGIN_BLACKLIST = ['foo/bar$', '.*/qux']  # reject all foo alerts from bar, and everything from qux
+ALLOWED_ENVIRONMENTS = ['Production', 'Development']  # reject alerts without allowed environments
