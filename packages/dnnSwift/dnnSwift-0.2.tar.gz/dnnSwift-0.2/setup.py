ede2a358029f048c81b4f38e9c463d0e0dcce4e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:20 +0000

    Add package dnnSwift/0.2/dnnSwift-0.2.tar.gz

---
+++ packages/dnnSwift/dnnSwift-0.2.tar.gz/dnnSwift-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='dnnSwift',
+    version='0.2',
+    packages=['dnnSwift'],
+    description="Quick Convolutional Neural Network Implementation",
+    url="https://github.com/DragonDuck/DNNSwift",
+    license='GNU General Public License Version 3',
+    install_requires=["numpy", "tensorflow", "h5py", "pygraphviz"],
+    keywords=["Deep Learning", "Convolution", "Neural Network", 
+              "DNN", "CNN", "Classification", "Classifier"]
+)
