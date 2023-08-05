a22af8ac2f0f5b5485b03b569b50431e29020c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:02 +0000

    Add package abi2fastq/1.0.2/abi2fastq-1.0.2.tar.gz

---
+++ packages/abi2fastq/abi2fastq-1.0.2.tar.gz/abi2fastq-1.0.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+version = '1.0.2'
+
+required = open('requirements.txt').read().split('\n')
+
+setup(
+    name='abi2fastq',
+    version=version,
+    description='abi2fastq is a small utility to convert Sanger sequencing '
+                'reads in .abi (applied biosystems) format to FASTQ Edit',
+    author='olgabot',
+    author_email='olga.botvinnik@gmail.com',
+    url='https://github.com/olgabot/abi2fastq',
+    packages=['abi2fastq'],
+    install_requires=required,
+    long_description='See ' + 'https://github.com/olgabot/abi2fastq',
+    license='MIT',
+    entry_points={'console_scripts': [
+        'abi2fastq = abi2fastq.cli:cli'
+    ]},
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
