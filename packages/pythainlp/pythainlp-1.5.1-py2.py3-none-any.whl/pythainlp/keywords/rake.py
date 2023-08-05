e48150fa0af411c93b2568beedcf2fb46705a3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:48 +0000

    Add package pythainlp/1.5.1/pythainlp-1.5.1-py2.py3-none-any.whl

---
+++ packages/pythainlp/pythainlp-1.5.1-py2.py3-none-any.whl/pythainlp/keywords/rake.py
@@ -0,0 +1,83 @@
+from __future__ import division,print_function
+import operator
+import nltk
+import string
+from pythainlp.tokenize import word_tokenize
+from pythainlp.corpus import stopwords
+def isPunct(word):
+      return len(word) == 1 and word in string.punctuation
+
+def isNumeric(word):
+  try:
+    float(word) if '.' in word else int(word)
+    return True
+  except ValueError:
+    return False
+
+class RakeKeywordExtractor:
+
+  def __init__(self):
+    self.stopwords = set(stopwords.words('thai'))
+    self.top_fraction = 1 # consider top third candidate keywords by score
+
+  def _generate_candidate_keywords(self, sentences):
+    phrase_list = []
+    for sentence in sentences:
+      words = map(lambda x: "|" if x in self.stopwords else x,word_tokenize(sentence))
+      phrase = []
+      for word in words:
+        if word == "|" or isPunct(word):
+          if len(phrase) > 0:
+            phrase_list.append(phrase)
+            phrase = []
+        else:
+          phrase.append(word)
+    return phrase_list
+
+  def _calculate_word_scores(self, phrase_list):
+    word_freq = nltk.FreqDist()
+    word_degree = nltk.FreqDist()
+    for phrase in phrase_list:
+      degree = len(filter(lambda x: not isNumeric(x), phrase)) - 1
+      for word in phrase:
+        word_freq.inc(word)
+        word_degree.inc(word, degree) # other words
+    for word in word_freq.keys():
+      word_degree[word] += word_freq[word] # itself
+    # word score = deg(w) / freq(w)
+    word_scores = {}
+    for word in word_freq.keys():
+      word_scores[word] = word_degree[word] / word_freq[word]
+    return word_scores
+
+  def _calculate_phrase_scores(self, phrase_list, word_scores):
+    phrase_scores = {}
+    for phrase in phrase_list:
+      phrase_score = 0
+      for word in phrase:
+        phrase_score += word_scores[word]
+      phrase_scores[" ".join(phrase)] = phrase_score
+    return phrase_scores
+    
+  def extract(self, text, incl_scores=False):
+    sentences = [text]
+    phrase_list = self._generate_candidate_keywords(sentences)
+    word_scores = self._calculate_word_scores(phrase_list)
+    phrase_scores = self._calculate_phrase_scores(phrase_list, word_scores)
+    sorted_phrase_scores = sorted(phrase_scores.items(),key=operator.itemgetter(1), reverse=True)
+    n_phrases = len(sorted_phrase_scores)
+    if incl_scores:
+      return sorted_phrase_scores[0:int(n_phrases/self.top_fraction)]
+    else:
+      return map(lambda x: x[0],
+        sorted_phrase_scores[0:int(n_phrases/self.top_fraction)])
+
+def test():
+  rake = RakeKeywordExtractor()
+  keywords = rake.extract("""
+แมวชอบกินปลาแมวคือแมวแมวเล่นเกมแมวก็แมวแมวจะยึดโลกโลกของแมวมีแต่กินแล้วนอนแล้วน่ารักฉันกลายเป็นทาสแมวแมวไปแล้วแมว
+  """, incl_scores=True)
+  print(keywords)
+  
+if __name__ == "__main__":
+  test()
