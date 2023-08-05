95441aa1560e0d63eac65fbe0c2caaa14ba182e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:16 +0000

    Add package nodel/0.3.3/nodel-0.3.3.tar.gz

---
+++ packages/nodel/nodel-0.3.3.tar.gz/nodel-0.3.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+	name='nodel',
+	version='0.3.3',
+	packages=find_packages(),
+	url='https://github.com/ary4n/nodel',
+	license='MIT',
+	author='aryan',
+	author_email='alikhaniaryan@live.com',
+	description='django project manager',
+	keywords='minimal django project manager',
+	download_url="https://github.com/ary4n/nodel/archive/0.3.3.tar.gz",
+	install_requires=[
+		'python-dotenv',
+	],
+)
