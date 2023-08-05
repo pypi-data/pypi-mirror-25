aba225fd9fdfdec227a90add73bfa22eabcb2a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:44 +0000

    Add package nsfw-dl/0.4.1/nsfw_dl-0.4.1-py3-none-any.whl

---
+++ packages/nsfw-dl/nsfw_dl-0.4.1-py3-none-any.whl/nsfw_dl/__init__.py
@@ -0,0 +1,13 @@
+"""
+Read the license at:
+https://github.com/IzunaDevs/nsfw_dl/blob/master/LICENSE
+"""
+
+from .dl import NSFWDL, LOADERS as SOURCES
+
+__title__ = 'nsfw_dl'
+__author__ = 'IzunaDevs'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 IzunaDevs'
+__version__ = '0.4.1'
+__all__ = ["NSFWDL", "SOURCES"]
