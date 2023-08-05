cb0edec2015f8658d94cc06e79c859d0c06cf883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:16 +0000

    Add package django-framework/0.1.26/django_framework-0.1.26.tar.gz

---
+++ packages/django-framework/django_framework-0.1.26.tar.gz/django_framework-0.1.26/django_framework/example_widget/basic_test_run/workers.py
@@ -0,0 +1,47 @@
+from django_framework.django_helpers.worker_helpers import register_worker
+from django_framework.django_helpers.worker_helpers import BaseWorker, BaseWorkerJob, BaseWorkerJobRegistry
+
+
+BASIC_TEST_RUN_WORKER_JOB_REGISTRY = BaseWorkerJobRegistry()
+
+BASIC_TEST_RUN_WORKER_JOB_REGISTRY.add_job(job = BaseWorkerJob(command = 'update', action='self', allowed_job_types=['synchronous', 'local', 'asynchronous'], allowed_priorities=[], timeout_ttl=3600))
+BASIC_TEST_RUN_WORKER_JOB_REGISTRY.add_job(job = BaseWorkerJob(command = 'update', action='test', allowed_job_types=['asynchronous'], allowed_priorities=[], timeout_ttl=3600))
+BASIC_TEST_RUN_WORKER_JOB_REGISTRY.add_job(job = BaseWorkerJob(command = 'update', action='delete', allowed_job_types=['local'], allowed_priorities=[], timeout_ttl=3600))
+
+
+
+class BasicTestRunWorker(BaseWorker):
+    ALLOWED_JOBS = BASIC_TEST_RUN_WORKER_JOB_REGISTRY
+    
+    def generate_initial_payload(self, command, action, **kwargs):
+        
+        if command == 'update':
+            if action == 'self':
+                return {"command" : "blah", "action" : "blah", "job_timeout" : 1, "scheduled_at" :None}
+            elif action == 'test':
+                return {"command" : "sdf", "action" : "blah", "job_timeout" : 1, "scheduled_at" :None}
+            
+        raise ValueError('This job/command is not allowed!')
+    
+    def process_response(self, worker_response):
+        # note that worker_response is always going to a BaseWorkerResponse object
+        
+        # do everything you need to do in here!!
+        if worker_response.command == 'update':
+            worker_response = self.process_response_update(worker_response=worker_response)
+
+        else:
+            raise ValueError('The command/action for BasicTestRun was not recognized!')
+        
+        
+        return worker_response
+    
+    def process_response_update(self, worker_response):
+        response = {"status" : 0, "error_notes" : None}
+        if worker_response.action == 'self':
+            response['response'] = 'Ran checks on self.  Everything seems ok!'
+        
+        
+        return response
+        
+register_worker(BasicTestRunWorker)
