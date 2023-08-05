9d0bd18f96dd1f64019e5cee20c5329500c65fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:39 +0000

    Add package paipy/0.1/paipy-0.1.tar.gz

---
+++ packages/paipy/paipy-0.1.tar.gz/paipy-0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='paipy',
+      version='0.1',
+      description='Tools and Algorithms used for Predictive Analytics and Machine Learning',
+      url='http://github.com/bawdiest/predictiveanalytics',
+      author='bawdiest',
+      author_email='com@mikmak.cc',
+      license='MIT',
+      packages=['paipy'],
+      zip_safe=False)
