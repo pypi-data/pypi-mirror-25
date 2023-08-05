7b15d2f5f0c0b9e759d57082fedf26f19aac0034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:15 +0000

    Add package collective.fingerpointing/1.4b2/collective.fingerpointing-1.4b2.tar.gz

---
+++ packages/collective.fingerpointing/collective.fingerpointing-1.4b2.tar.gz/collective.fingerpointing-1.4b2/src/collective/fingerpointing/subscribers/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from .iterate_logger import iterate_logger  # noqa
+from .lifecycle_logger import lifecycle_logger  # noqa
+from .pas_logger import pas_logger  # noqa
+from .registry_logger import registry_logger  # noqa
+from .workflow_logger import workflow_logger  # noqa: F401
