04685c97d84c7cab7d5a8682b44a06dcc1639ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:35 +0000

    Add package fgivenx/1.0.10/fgivenx-1.0.10.tar.gz

---
+++ packages/fgivenx/fgivenx-1.0.10.tar.gz/fgivenx-1.0.10/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='fgivenx',
+      version='1.0.10',
+      author='Will Handley',
+      author_email='wh260@cam.ac.uk',
+      url='https://github.com/williamjameshandley/fgivenx',
+      packages=['fgivenx'],
+      install_requires=['numpy','matplotlib','scipy','joblib','mpi4py','tqdm'],
+      license='MIT',
+      classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Natural Language :: English',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3.6',
+      'Topic :: Scientific/Engineering :: Astronomy',
+      'Topic :: Scientific/Engineering :: Physics',
+      'Topic :: Scientific/Engineering :: Visualization',
+      'Topic :: Scientific/Engineering :: Information Analysis',
+      ],
+      description='Functional Posterior Plotter',
+      long_description=readme(),
+      )
