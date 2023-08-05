e4ef16c4a7fcfb64a7fff94f33a1c3a418233b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:19 +0000

    Add package asphalt-wamp/2.1.0/asphalt-wamp-2.1.0.tar.gz

---
+++ packages/asphalt-wamp/asphalt-wamp-2.1.0.tar.gz/asphalt-wamp-2.1.0/tests/conftest.py
@@ -0,0 +1,35 @@
+import asyncio
+import os
+
+import pytest
+import txaio
+from asphalt.core import Context
+
+from asphalt.wamp.client import WAMPClient
+
+
+@pytest.fixture
+def event_loop():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    txaio.use_asyncio()
+    txaio.config.loop = loop
+    yield loop
+    loop.close()
+
+
+@pytest.fixture
+def context(event_loop):
+    with Context() as ctx:
+        yield ctx
+
+
+@pytest.fixture
+def wampclient(request, event_loop, context):
+    kwargs = getattr(request, 'param', {})
+    kwargs.setdefault('host', os.getenv('CROSSBAR_HOST', '127.0.0.1'))
+    kwargs.setdefault('max_reconnection_attempts', 0)
+    client = WAMPClient(**kwargs)
+    event_loop.run_until_complete(client.start(context))
+    yield client
+    event_loop.run_until_complete(client.close())
