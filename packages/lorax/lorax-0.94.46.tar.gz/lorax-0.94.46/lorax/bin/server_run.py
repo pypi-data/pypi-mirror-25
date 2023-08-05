d4149ac833546a90b6a4e0bcb3cae676034f035d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:00 +0000

    Add package lorax/0.94.46/lorax-0.94.46.tar.gz

---
+++ packages/lorax/lorax-0.94.46.tar.gz/lorax-0.94.46/lorax/bin/server_run.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import importlib
+import os
+import sys
+
+package_name = os.path.basename(__file__).split('_')[0]
+
+try:
+    app = importlib.import_module(package_name).app
+    configure_logging = importlib.import_module(
+        package_name + '.logging').configure_logging
+    init_filesystem = importlib.import_module(
+        package_name + '.filesystem').init_filesystem
+except ModuleNotFoundError:
+    print('ERROR--the needed modules for the "%s" package were not found.'
+          % package_name)
+    sys.exit(1)
+
+init_filesystem(app)
+configure_logging(app)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ packages/lorax/lorax-0.94.46.tar.gz/lorax-0.94.46/lorax/filesystem.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""Create and manage essential filesystem locations."""
+
+import os
+from pathlib import Path  # python 3.4 or later
+from flask import current_app
+from .config_file import create_config_file
+
+DIRS = [('TMP', 'nginx'),
+        ('LOG', 'nginx'),
+        ('VAR', 'redis'),
+        ('VAR', 'run/nginx'),
+        ('DATA', ''),
+        ('USERDATA', '')]
+SERVICE_NAME = os.getenv('FLASK_APP', __name__.split('.')[0])
+
+
+def create_dir(config_path, subdir, app):
+    """Creates runtime directories, if they don't exist."""
+    dir_path = Path(app.config[config_path]) / subdir
+    if not dir_path.is_dir():  # create logs/ dir
+        print('Creating directory %s/%s at %s.'
+              % (config_path, subdir, str(dir_path)))
+        try:
+            dir_path.mkdir(mode=int(app.config['DIR_MODE'], 8),
+                           parents=True)
+        except OSError:
+            print('Unable to create directory "%s"'
+                  % (str(dir_path)))
+            raise OSError
+
+
+def init_filesystem(app):
+    """Initialize the filesystem."""
+    for dir_tuple in DIRS:
+        create_dir(*dir_tuple, app=app)
+    #
+    # Config file may not exist if SETTINGS value was changed.
+    #
+    create_config_file(Path(app.config['ROOT']) / 'etc' /
+                       app.config['SETTINGS'])
