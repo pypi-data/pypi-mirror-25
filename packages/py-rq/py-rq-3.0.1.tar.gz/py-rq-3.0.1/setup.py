c29a8e3bf648c477ab8a21e893d039e77f38c845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:49 +0000

    Add package py-rq/3.0.1/py-rq-3.0.1.tar.gz

---
+++ packages/py-rq/py-rq-3.0.1.tar.gz/py-rq-3.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='py-rq',
+    version='3.0.1',
+    packages=['pyrq'],
+    url='https://github.com/heureka/py-rq',
+    license='MIT',
+    author='Heureka.cz',
+    author_email='podpora@heureka.cz',
+    description='Redis queue for Python',
+    install_requires=[
+        "redis>2.0.0"
+    ]
+)
