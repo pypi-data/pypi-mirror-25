17125834b1a7683ee73286dd936f3bbf83defcfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:09 +0000

    Add package json_stable_stringify_python/0.2/json_stable_stringify_python-0.2.tar.gz

---
+++ packages/json_stable_stringify_python/json_stable_stringify_python-0.2.tar.gz/json_stable_stringify_python-0.2/json_stable_stringify_python/stringify.py
@@ -0,0 +1,23 @@
+from collections import OrderedDict, Mapping, Sequence
+import json
+
+COMPACT_SEPARATORS = (',', ':')
+
+def order_by_key(kv):
+  key, val = kv
+  return key
+
+def recursive_order(node):
+  if isinstance(node, Mapping):
+    ordered_mapping = OrderedDict(sorted(node.items(), key=order_by_key))
+    for key, value in ordered_mapping.items():
+      ordered_mapping[key] = recursive_order(value)
+    return ordered_mapping
+  elif isinstance(node, Sequence) and not isinstance(node, (str, bytes)):
+    return [recursive_order(item) for item in node]
+  return node
+
+def stringify(node):
+  return json.dumps(recursive_order(node), separators=COMPACT_SEPARATORS)
+
+__all__ = ['stringify']
