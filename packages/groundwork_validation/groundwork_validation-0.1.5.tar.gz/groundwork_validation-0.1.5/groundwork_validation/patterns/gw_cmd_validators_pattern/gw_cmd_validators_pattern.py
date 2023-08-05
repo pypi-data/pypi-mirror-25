a3c3d682ccd363fe68c5dc0a1fbdb46add325813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:14 +0000

    Add package groundwork_validation/0.1.5/groundwork_validation-0.1.5.tar.gz

---
+++ packages/groundwork_validation/groundwork_validation-0.1.5.tar.gz/groundwork_validation-0.1.5/groundwork_validation/patterns/gw_cmd_validators_pattern/gw_cmd_validators_pattern.py
@@ -0,0 +1,108 @@
+import sys
+from re import finditer
+
+from groundwork_validation.patterns import GwValidatorsPattern
+
+# This uses a backport of subrprocess for python < 3
+# This is needed to use the timeout functionality.
+if sys.version_info[0] < 3:
+    import subprocess32 as subprocess
+else:
+    import subprocess
+
+
+class GwCmdValidatorsPattern(GwValidatorsPattern):
+    """
+    Allows the validation of output, return code and execution time of a given command.
+
+    Usage::
+
+        class My_Plugin(GwCmdValidatorsPattern):
+            def __init__(self, app, **kwargs):
+                self.name = "My_Plugin"
+                super(My_Plugin, self).__init__(app, **kwargs)
+
+            def activate(self):
+                if self.validators.cmd.validate("dir", search="my_folder"):
+                    print("Command 'dir' works a expected.")
+                else:
+                    print("Command 'dir' seems not to work correctly. We stop here")
+                    sys.exit(1)
+
+            def deactivate(self):
+                pass
+    """
+
+    def __init__(self, app, **kwargs):
+        super(GwCmdValidatorsPattern, self).__init__(app, **kwargs)
+        self.app = app
+        self.validators.cmd = CmdValidatorsPlugin(self)
+
+
+class CmdValidatorsPlugin:
+
+    def __init__(self, plugin):
+        self.plugin = plugin
+
+    def validate(self, command, search=None, regex=None, timeout=2, allowed_return_codes=None, decode="utf-8"):
+        """
+        Validates the output of a given command.
+
+        The validation can be based on a simple string search or on a complex regular expression.
+        Also the return_code can be validated. As well as the execution duration by setting a timeout.
+
+        :param command: string, which is used as command for a new subprocess. E.g. 'git -v'.
+        :param search: string, which shall be contained in the output of the command. Default is None
+        :param regex:  regular expression, which is tested against the command output.
+                       Default is None
+        :param timeout: Time ins seconds, after which the execution is stopped and the validation fails.
+                        Default is 2 seconds
+        :param allowed_return_codes: List of allowed return values. Default is []
+        :param decode: Format of the console encoding, which shall be used. Default is 'utf-8'
+        :return: True, if validation succeeded. Else False.
+        """
+        if search is None and regex is None:
+            raise ValueError("Parameter search or regex must be set.")
+        if search is not None and regex is not None:
+            raise ValueError("Only search OR regex is allowed to be used. Not both!")
+
+        if allowed_return_codes is None:
+            allowed_return_codes = []
+        if isinstance(allowed_return_codes, int):
+            allowed_return_codes = [allowed_return_codes]
+        if not isinstance(allowed_return_codes, list):
+            raise TypeError("allowed_return_code must be a list of integers")
+
+        try:
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True, timeout=timeout)
+            return_code = 0
+        except subprocess.CalledProcessError as e:
+            output = e.output
+            return_code = e.returncode
+        except subprocess.TimeoutExpired as e:
+            raise CommandTimeoutExpired(e)
+
+        if len(allowed_return_codes) > 0 and return_code not in allowed_return_codes:
+            raise NotAllowedReturnCode("For command %s got return code '%s', which is not in %s"
+                                       % (command, return_code, allowed_return_codes))
+
+        self.plugin.log.debug("Executed '%s' with return code: %s" % (command, return_code))
+
+        output = output.decode(decode)
+        found = False
+        if search is not None:
+            if search in output:
+                found = True
+        elif regex is not None:
+            for m in finditer(regex, output):
+                self.plugin.log.debug("Found cmd validation '%s' at %02d-%02d" % (m.group(0), m.start(), m.end()))
+                found = True
+        return found
+
+
+class NotAllowedReturnCode(BaseException):
+    pass
+
+
+class CommandTimeoutExpired(BaseException):
+    pass
---
+++ packages/groundwork_validation/groundwork_validation-0.1.5.tar.gz/groundwork_validation-0.1.5/groundwork_validation/patterns/gw_validators_pattern/gw_validators_pattern.py
@@ -0,0 +1,179 @@
+import hashlib
+import pickle
+
+from groundwork.patterns import GwBasePattern
+from groundwork.util import gw_get
+
+
+class GwValidatorsPattern(GwBasePattern):
+    """
+    Allows the creation of hashes for python objects (and its validation).
+    """
+
+    def __init__(self, app, **kwargs):
+        super(GwValidatorsPattern, self).__init__(app, **kwargs)
+        self.app = app
+
+        self.validators = ValidatorsPlugin(self)
+        if not hasattr(self.app, "validators"):
+            self.app.validators = ValidatorsApplication(self)
+
+
+class ValidatorsPlugin:
+    """
+    Cares about the Validator handling on plugin level.
+    """
+
+    def __init__(self, plugin):
+        self.plugin = plugin
+        self.app = plugin.app
+
+    def register(self, name, description, algorithm=None, attributes=None):
+        """
+        Registers a new validator on plugin level.
+
+        :param name: Unique name of the validator
+        :param description: Helpful description of the validator
+        :param algorithm: A hashlib compliant function. If None, hashlib.sha256 is taken.
+        :param attributes: List of attributes, for which the hash must be created. If None, all contained
+                           attributes are used.
+        :return: Validator instance
+        """
+        if algorithm is None:
+            algorithm = hashlib.sha256
+        return self.app.validators.register(name, description, self.plugin, algorithm=algorithm, attributes=attributes)
+
+    def unregister(self, name):
+        self.app.validators.unregister(name)
+
+    def get(self, name):
+        """
+        Returns a single or a list of validator instance, which were registered by the current plugin.
+
+        :param name: Name of the validator. If None, all validators of the current plugin are returned.
+        :return: Single or list of Validator instances
+        """
+        self.app.validators.get(name, self.plugin)
+
+
+class ValidatorsApplication:
+    """
+    Cares about the Validator handling on application level.
+    """
+    def __init__(self, app):
+        self.app = app
+        self._validators = {}
+
+    def register(self, name, description, plugin, algorithm=None, attributes=None):
+        """
+        Registers a new validator on application level.
+
+        :param name: Unique name of the validator
+        :param description: Helpful description of the validator
+        :param algorithm: A hashlib compliant function. If None, hashlib.sha256 is taken.
+        :param attributes: List of attributes, for which the hash must be created. If None, all contained
+                           attributes are used.
+        :param plugin: Plugin instance, for which the validator gets registered.
+        :return: Validator instance
+        """
+        if name in self._validators.keys():
+            raise KeyError("Validator %s already registered" % name)
+
+        if algorithm is None:
+            algorithm = hashlib.sha256
+
+        self._validators[name] = Validator(name, description,
+                                           algorithm=algorithm,
+                                           attributes=attributes,
+                                           plugin=plugin)
+
+        return self._validators[name]
+
+    def unregister(self, name):
+        if name in self._validators.keys():
+            del(self._validators[name])
+        else:
+            raise KeyError("Validator %s does not exist" % name)
+
+    def get(self, name, plugin):
+        """
+        Returns a single or a list of validator instance
+
+        :param name: Name of the validator. If None, all validators are returned.
+        :param plugin: Plugin instance, which has registered the requested validator.
+                       If None, all validators are returned.
+        :return: Single or list of Validator instances
+        """
+        gw_get(self._validators, name, plugin)
+
+
+class Validator:
+    """
+    Represent the final validator, which provides functions to hash a given python object and to validate a
+    python object against a given hash.
+    """
+    def __init__(self, name, description, algorithm=None, attributes=None, plugin=None):
+        self.name = name
+        self.description = description
+        self.plugin = plugin
+        if algorithm is None:
+            algorithm = hashlib.sha256
+        self.algorithm = algorithm
+        self.attributes = attributes
+
+    def validate(self, data, hash_string, no_pickle=False):
+        """
+        Validates a python object against a given hash
+
+        :param data: Python object
+        :param hash_string: hash as string, which must be compliant to the configured hash algorithm of
+                            the used validator.
+        :param no_pickle: If True data is not pickled before hash is calculated.
+                          Helpful, if data is already serialised (like file inputs)
+        :return: True, if object got validated by hash. Else False
+        """
+        if self.hash(data, no_pickle=no_pickle) == hash_string:
+            return True
+        return False
+
+    def hash(self, data, hash_object=None, return_hash_object=False, strict=False, no_pickle=False):
+        """
+        Generates a hash of a given Python object.
+
+        :param data: Python object
+        :param return_hash_object: If true, the complete hashlib object is returned
+                                   instead of a hexdigest representation as string.
+        :param hash_object: An existing  hash object, which will be updated. Instead of creating a new one.
+        :param strict: If True, all configured attributes **must** exist in the given data, otherwise an exception
+                       is thrown.
+        :param no_pickle: If True data is not pickled before hash is calculated.
+                          Helpful, if data is already serialised (like file inputs)
+        :return: hash as string
+        """
+        if hash_object is None:
+            current_hash = self.get_hash_object()
+        else:
+            current_hash = hash_object
+
+        if self.attributes is None:
+            if not no_pickle:
+                current_hash.update(pickle.dumps(data))
+            else:
+                current_hash.update(data)
+        else:
+            for attribute in self.attributes:
+                if strict and hasattr(data, attribute) is False:
+                    raise AttributeError("Data has no attribute called %s" % attribute)
+                current_hash.update(pickle.dumps(getattr(data, attribute, None)))
+
+        if return_hash_object:
+            return current_hash
+        return current_hash.hexdigest()
+
+    def get_hash_object(self):
+        """
+        Returns a hash object, which can be used as input for validate functions.
+
+        :return: An unused hash object
+        """
+        return self.algorithm()
