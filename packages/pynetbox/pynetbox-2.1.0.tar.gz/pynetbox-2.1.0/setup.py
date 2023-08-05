51be02ec11df3037d5935856d2a00b0a9c1b8ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:18 +0000

    Add package pynetbox/2.1.0/pynetbox-2.1.0.tar.gz

---
+++ packages/pynetbox/pynetbox-2.1.0.tar.gz/pynetbox-2.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='pynetbox',
+    version='2.1.0',
+    description='NetBox API client library',
+    url='https://github.com/digitalocean/pynetbox',
+    author='Zach Moody',
+    author_email='zmoody@do.co',
+    license='Apache2',
+    packages=[
+        'pynetbox',
+        'pynetbox.lib'
+    ],
+    install_requires=[
+        'netaddr',
+        'requests'
+    ],
+    zip_safe=False,
+    keywords=['netbox'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+)
