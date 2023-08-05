0b1de60f759fe9bfe61817ff4f1cccf5c6051ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:36 +0000

    Add package slack-webhook-cli/0.2.4/slack-webhook-cli-0.2.4.tar.gz

---
+++ packages/slack-webhook-cli/slack-webhook-cli-0.2.4.tar.gz/slack-webhook-cli-0.2.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+version = __import__('slack').get_version()
+
+setup(
+    name='slack-webhook-cli',
+    description='Send messages to Slack from the command line',
+    version=version,
+    packages=find_packages(),
+    install_requires=['requests'],
+    author='Philipp Bosch',
+    author_email='hello@pb.io',
+    license='MIT',
+    url='https://github.com/philippbosch/slack-webhook-cli',
+    scripts=['slack/cli.py'],
+    entry_points={
+        'console_scripts': [
+            'slack = slack.cli:main'
+        ]
+    },
+)
