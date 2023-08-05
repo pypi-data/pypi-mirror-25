003bb137acbc9ba19eff1571dd90e24ccc01188c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:54 +0000

    Add package cardbin/0.1.0/cardbin-0.1.0-py2.py3-none-any.whl

---
+++ packages/cardbin/cardbin-0.1.0-py2.py3-none-any.whl/cardbin/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for cardbin."""
+
+__author__ = """cardbin"""
+__email__ = 'ibopo@126.com'
+__version__ = '0.1.0'
+
+from cardbin.cardbin import cardbin
---
+++ packages/cardbin/cardbin-0.1.0-py2.py3-none-any.whl/cardbin/cardbin.py
@@ -0,0 +1,24 @@
+import json,os
+import pickle
+
+from stdnum import luhn
+
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
+BANKBIN = pickle.load(open(os.path.join(BASEDIR,'cardbin.pkl'),'rb'))
+
+def cardbin(card):
+    if not luhn.is_valid(card):
+        print ('卡号不合法')
+        return False
+    key = BANKBIN.get(card[:7]) if BANKBIN.get(card[:7]) else BANKBIN.get(card[:6], None)
+
+    if key:
+        key = key.split('|')
+        print({'bank':key[0],'type':key[1]})
+        return {'bank':key[0],'type':key[1]}
+    else:
+        print('未知银行卡')
+        return False
+
+# if __name__ == '__main__':
+#     cardbin('6228480402564890018')
