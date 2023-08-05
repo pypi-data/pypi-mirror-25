29fdd78af24575f0c8588dc9b9fddfb13a9b3f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:34 +0000

    Add package amaasinfra/0.2.13/amaasinfra-0.2.13.tar.gz

---
+++ packages/amaasinfra/amaasinfra-0.2.13.tar.gz/amaasinfra-0.2.13/tests/argomi_user.py
@@ -0,0 +1,42 @@
+"""
+Crude tests.  Need a better way to mock out the DynamoDB calls
+"""
+import unittest
+from mock import patch
+from amaasinfra.authorization.argomi_user import ArgomiUser
+from amaasinfra.authorization.token_utils import TokenAttribute
+
+class ArgomiUserTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+    
+    @patch('amaasinfra.authorization.argomi_user.unpack_token')
+    @patch('amaasinfra.authorization.argomi_user.AssetManagerRelationship')
+    def testCheckAmidPermissions(self, mock_relationship, mock_unpack_token):
+        mock_unpack_token.return_value = {TokenAttribute.asset_manager_id.value: 1}
+        user = ArgomiUser('')
+        user.asset_manager_permissions = [1, 10, 11, 12, 101]
+
+        errors = user.check_asset_manager_permissions([1, 10, 11, 12])
+        self.assertTrue(len(errors) == 0)
+
+        errors = user.check_asset_manager_permissions([101])
+        self.assertTrue(len(errors) == 0)
+
+        errors = user.check_asset_manager_permissions([102])
+        self.assertTrue(102 in errors)
+    
+    def testLoadRelationships(self):
+        pass
+    
+    def testLoadDataSources(self):
+        pass
+    
+    def testLoadAssetManagerIdPermissions(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
