3d3ce4a3088def6a8d97a57257d18d7f536c5470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:59 +0000

    Add package dftparse/0.2.0/dftparse-0.2.0.tar.gz

---
+++ packages/dftparse/dftparse-0.2.0.tar.gz/dftparse-0.2.0/dftparse/core.py
@@ -0,0 +1,22 @@
+
+class BlockParser(object):
+    """Parser built on rules that parse blocks of input"""
+
+    def __init__(self, rules=[]):
+        """Create a BlockParser, pre-loading a set of rules"""
+        self.rules = rules
+
+    def add_rule(self, rule):
+        """Add a rule to this parser"""
+        self.rules.append(rule)
+
+    def parse(self, generator):
+        """Parse an iterable source of strings into a generator"""
+        gen = iter(generator)
+        for line in gen:
+            block = {}
+            for rule in self.rules:
+                if rule[0](line):
+                    block = rule[1](line, gen)
+                    break
+            yield block
