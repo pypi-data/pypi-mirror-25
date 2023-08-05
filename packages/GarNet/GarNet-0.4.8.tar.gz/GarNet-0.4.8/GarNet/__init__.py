72c58ec4d6b20de7d1848603e81a3dcfd1af6f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:58 +0000

    Add package GarNet/0.4.8/GarNet-0.4.8.tar.gz

---
+++ packages/GarNet/GarNet-0.4.8.tar.gz/GarNet-0.4.8/GarNet/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+from .garnet import map_peaks, TF_regression
+
---
+++ packages/GarNet/GarNet-0.4.8.tar.gz/GarNet-0.4.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='GarNet',
+    packages=['GarNet'],
+    package_data={'GarNet': ['summary.jinja']},
+    version='0.4.8',
+    url='https://github.com/fraenkel-lab/GarNet',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'],
+    license='MIT',
+    author='zfrenchee',
+    author_email='alex@lenail.org',
+    description='',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'statsmodels',
+        'matplotlib',
+        'intervaltree',
+        'jinja2'
+    ],
+)
+
