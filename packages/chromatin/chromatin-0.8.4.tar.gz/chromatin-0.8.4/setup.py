b5e6fb5478bfbf04786784633a59e185cd035fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:07 +0000

    Add package chromatin/0.8.4/chromatin-0.8.4.tar.gz

---
+++ packages/chromatin/chromatin-0.8.4.tar.gz/chromatin-0.8.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version_parts = (0, 8, 4)
+version = '.'.join(map(str, version_parts))
+
+setup(
+    name='chromatin',
+    description='neovim python plugin manager',
+    version=version,
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    url='https://github.com/tek/chromatin',
+    include_package_data=True,
+    packages=find_packages(exclude=['unit', 'unit.*', 'integration', 'integration.*']),
+    install_requires=[
+        'ribosome~=10.18.3',
+        'pyuv',
+    ],
+    tests_require=[
+        'kallikrein',
+    ],
+    entry_points={
+        'console_scripts': [
+            'crm_run = chromatin.cli:run',
+        ],
+    },
+)
