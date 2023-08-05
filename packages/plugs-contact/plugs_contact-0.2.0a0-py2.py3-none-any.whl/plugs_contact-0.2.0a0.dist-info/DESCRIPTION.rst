aebdc5d6896661ee282d717fcae89333637884f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:40 +0000

    Add package plugs-contact/0.2.0a0/plugs_contact-0.2.0a0-py2.py3-none-any.whl

---
+++ packages/plugs-contact/plugs_contact-0.2.0a0-py2.py3-none-any.whl/plugs_contact-0.2.0a0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,24 @@
+=====
+Plugs Contact
+=====
+
+.. image:: https://badge.fury.io/py/plugs-contact.svg
+    :target: https://badge.fury.io/py/plugs-contact
+
+Quick start
+-----------
+
+Soon...
+
+
+
+
+History
+-------
+
+0.1.1 (2017-01-13)
+++++++++++++++++++
+
+* First release on PyPI.
+
+
---
+++ packages/plugs-contact/plugs_contact-0.2.0a0-py2.py3-none-any.whl/plugs_contact/views.py
@@ -0,0 +1,41 @@
+"""
+Plugs Contact Views
+"""
+from django.conf import settings
+
+from rest_framework import permissions
+
+from plugs_core.viewsets import CreateViewSet
+
+from plugs_mail import utils
+
+from plugs_contact import serializers, models, emails
+
+from plugs_auth.utils import language_from_header
+
+# pylint: disable=R0901
+class ContactViewSet(CreateViewSet):
+    """
+    Contact Viewset
+    """
+    queryset = models.Contact.objects.all()
+    serializer_class = serializers.ContactSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_language(self):
+        try:
+            accept_language_header = self.request.META['HTTP_ACCEPT_LANGUAGE']
+            return language_from_header(accept_language_header)
+        except KeyError:
+            return settings.LANGUAGE_CODE
+
+    def perform_create(self, serializer):
+        try:
+            language = self.request.user.language
+        except AttributeError:
+            language = self.get_language()
+        contact = serializer.save()
+        data = {'contact': contact}
+        payload = {'head': serializer.data.get('email'), 'body': serializer.data.get('message')}
+        utils.to_staff(emails.ContactCreated, **data)
+        utils.to_email(emails.ContactReceived, contact.email, language, **data)
