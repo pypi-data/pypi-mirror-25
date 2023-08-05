10b461dd699db90c6bbf0c9324990277149bafb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:00 +0000

    Add package civis/1.6.2/civis-1.6.2.tar.gz

---
+++ packages/civis/civis-1.6.2.tar.gz/civis-1.6.2/civis/resources/__init__.py
@@ -0,0 +1,7 @@
+from ._resources import (generate_classes,
+                         get_api_spec,
+                         generate_classes_maybe_cached)
+
+__all__ = ["generate_classes",
+           "get_api_spec",
+           "generate_classes_maybe_cached"]
