a933aa2612438d9e4288ea439135141dcca36fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:51 +0000

    Add package public/0.0.39.dev10/public-0.0.39.dev10.tar.gz

---
+++ packages/public/public-0.0.39.dev10.tar.gz/public-0.0.39.dev10/.Tests/clone.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+{ set +x; } 2>/dev/null
+
+path=~/.Tests
+[[ -z "$TESTS_URL" ]] && {
+	echo "DEBUG: \$TESTS_URL NOT DEFINED"
+	txt="${BASH_SOURCE[0]%/*}/url.txt"
+	TESTS_URL="$(set -x; cat "$txt")" || exit
+}
+
+# known-issues: ssh permissions with urls like 'git@github.com:owner/repo.git'
+# fix: https url - 'https://github.com/owner/repo.git' 
+( set -x; git clone -q --depth 1 "$TESTS_URL" "$path" ) || exit
