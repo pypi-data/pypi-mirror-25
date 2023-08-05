5480055f73cd5818a32aceb1e28cfa4bbc1169c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:25 +0000

    Add package pyvcloud/18.0.13.dev18/pyvcloud-18.0.13.dev18.tar.gz

---
+++ packages/pyvcloud/pyvcloud-18.0.13.dev18.tar.gz/pyvcloud-18.0.13.dev18/tests/vcd_vapp.py
@@ -0,0 +1,15 @@
+import os
+import unittest
+import yaml
+from pyvcloud.vcd.client import BasicLoginCredentials
+from pyvcloud.vcd.client import Client
+from pyvcloud.vcd.org import Org
+from pyvcloud.vcd.test import TestCase
+
+class TestVApp(TestCase):
+
+    def test_instantiate_vapp(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
