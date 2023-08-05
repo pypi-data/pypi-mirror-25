5f99bbb4ffd9fdc983e86e2c97efcc2c79c41433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:13 +0000

    Add package freeipmi/0.0.3/freeipmi-0.0.3.tar.gz

---
+++ packages/freeipmi/freeipmi-0.0.3.tar.gz/freeipmi-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+version = '0.0.3'
+
+setup(
+    name='freeipmi',
+    packages=['freeipmi'],
+    version=version,
+    description='Python library for FreeIPMI',
+    author='Matteo Cerutti',
+    author_email='matteo.cerutti@hotmail.co.uk',
+    url='https://github.com/m4ce/freeipmi-python',
+    download_url='https://github.com/m4ce/freeipmi-python/tarball/%s' % (
+        version,),
+    keywords=['freeipmi', 'ipmi'],
+    classifiers=[]
+)
