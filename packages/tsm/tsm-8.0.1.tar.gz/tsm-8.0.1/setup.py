19183f410386587f14b30514d85d239c7ac22363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:55 +0000

    Add package tsm/8.0.1/tsm-8.0.1.tar.gz

---
+++ packages/tsm/tsm-8.0.1.tar.gz/tsm-8.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'tsm',
+  packages = ['tsm'], # this must be the same as the name above
+  version = '8.0.1',
+  description = 'Twitter Subgraph Manipulator (TSM)',
+  author = 'Deen Freelon',
+  author_email = 'dfreelon@gmail.com',
+  url = 'https://github.com/dfreelon/tsm/', # use the URL to the github repo
+  download_url = 'https://github.com/dfreelon/tsm/tarball/8.0', # I'll explain this in a second
+  keywords = ['twitter', 'network analysis', 'data science','big data'], # arbitrary keywords
+  classifiers = [],
+)
