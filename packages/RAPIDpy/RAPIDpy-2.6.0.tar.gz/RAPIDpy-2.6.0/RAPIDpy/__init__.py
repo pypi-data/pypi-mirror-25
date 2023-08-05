f5e6b9a8b017ddc67dbae8a9466f68d251aa4cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:18 +0000

    Add package RAPIDpy/2.6.0/RAPIDpy-2.6.0.tar.gz

---
+++ packages/RAPIDpy/RAPIDpy-2.6.0.tar.gz/RAPIDpy-2.6.0/RAPIDpy/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    RAPIDpy
+
+    Created by Alan D Snow, 2015.
+    License: BSD 3-Clause
+"""
+from .rapid import RAPID  # noqa
+from .dataset import RAPIDDataset  # noqa
