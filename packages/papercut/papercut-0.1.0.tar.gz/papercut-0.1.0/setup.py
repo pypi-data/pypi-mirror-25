c5e76b0c9a07d91d1930d4225fa3654ec41380b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:14 +0000

    Add package papercut/0.1.0/papercut-0.1.0.tar.gz

---
+++ packages/papercut/papercut-0.1.0.tar.gz/papercut-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='papercut',
+    version='0.1.0',
+
+    description='Papercut API',
+    long_description=long_description,
+
+    url='https://github.com/kulinacs/papercut',
+
+    author='Nicklaus McClendon',
+    author_email='nicklaus@kulinacs.com',
+
+    license='ISC',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='papercut printing',
+
+    packages=find_packages(),
+
+    install_requires=[],
+)
