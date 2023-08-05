55e950b87d57cfffcb1b1d9ff501887eb9440299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:28 +0000

    Add package auquan-toolbox/1.1.7/auquan_toolbox-1.1.7-py2-none-any.whl

---
+++ packages/auquan-toolbox/auquan_toolbox-1.1.7-py2-none-any.whl/backtester/features/ma_ribbon_feature.py
@@ -0,0 +1,20 @@
+from backtester.features.feature import Feature
+from backtester.financial_fn import ma
+import numpy as np
+
+
+class MARibbonHammingDistanceFeature(Feature):
+
+    @classmethod
+    def computeForLookbackData(cls, featureParams, featureKey, currentFeatures, lookbackDataDf):
+        data = lookbackDataDf[featureParams['featureName']]
+        rolling_means = np.zeros(featureParams['numRibbons'])
+        if len(data) < 1:
+        	return rolling_means
+        space = int((featureParams['endPeriod'] - featureParams['startPeriod'])/(featureParams['numRibbons']-1))
+        for idx in np.linspace(featureParams['startPeriod'], featureParams['endPeriod'], featureParams['numRibbons']):
+            i = int(idx)
+            rolling_means[i/space - 1] = data[-i:].mean()
+        ranking = stats.rankdata(rolling_means)
+        d = distance.hamming(ranking, range(1, 1+len(rolling_means)))
+        return d
