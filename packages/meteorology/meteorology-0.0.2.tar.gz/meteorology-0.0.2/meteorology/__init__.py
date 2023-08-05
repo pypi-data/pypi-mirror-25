94c99e5d3c9deca0296f738a39d0cca8b98b78ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:21 +0000

    Add package meteorology/0.0.2/meteorology-0.0.2.tar.gz

---
+++ packages/meteorology/meteorology-0.0.2.tar.gz/meteorology-0.0.2/meteorology/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# internal modules
+from . import temperature
+
+__version__ = "0.0.2"
---
+++ packages/meteorology/meteorology-0.0.2.tar.gz/meteorology-0.0.2/meteorology/constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# system modules
+
+# internal modules
+
+# external modules
+
+
+# zero degrees celsius in kelvin
+zero_celsius_in_kelvin = 273.15
