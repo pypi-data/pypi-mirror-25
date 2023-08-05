7054f092765b69ec024fba89e53d0bf160c6b321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:54 +0000

    Add package marshmallow-configparser/0.3.3/marshmallow_configparser-0.3.3-py2.py3-none-any.whl

---
+++ packages/marshmallow-configparser/marshmallow_configparser-0.3.3-py2.py3-none-any.whl/marshmallow_configparser/validators.py
@@ -0,0 +1,27 @@
+from marshmallow.validate import ValidationError, Validator
+
+from .helpers import is_blank
+
+
+class IsNotBlank(Validator):
+    """Validator which succeeds if the value passed is not blank string."""
+
+    error = None
+
+    def __call__(self, value):
+        if is_blank(value):
+            raise ValidationError("Can't be blank!")
+
+        return value
+
+
+class IsNotNone(Validator):
+    """Validator which succeeds if the value passed is not blank string."""
+
+    error = None
+
+    def __call__(self, value):
+        if value is None:
+            raise ValidationError("Can't be null!")
+
+        return value
