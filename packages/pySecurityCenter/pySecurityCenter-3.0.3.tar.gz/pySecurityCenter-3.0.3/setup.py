09577ff0698999d5643010fca732d981969b2881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:55 +0000

    Add package pySecurityCenter/3.0.3/pySecurityCenter-3.0.3.tar.gz

---
+++ packages/pySecurityCenter/pySecurityCenter-3.0.3.tar.gz/pySecurityCenter-3.0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import os
+
+long_description = ''
+try:
+    from pypandoc import convert
+    if os.path.exists('README.md'):
+        long_description = convert('README.md', 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert README.md to RST")
+
+
+setup(
+    name="pySecurityCenter",
+    version='3.0.3',
+    description="Security Center API Library",
+    long_description='Python Interface into Tenable\'s SecurityCenter',
+    author='Steven McGrath <steve@chigeek.com>',
+    author_email="steve@chigeek.com",
+    url="https://github.com/SteveMcGrath/pySecurityCenter",
+    packages=["securitycenter",],
+    install_requires=["requests",],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"
+    ]
+)
