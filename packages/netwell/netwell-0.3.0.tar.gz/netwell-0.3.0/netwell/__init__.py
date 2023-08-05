df116e5ef70cbce1c009b779d97fd3898d69bb3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:42 +0000

    Add package netwell/0.3.0/netwell-0.3.0.tar.gz

---
+++ packages/netwell/netwell-0.3.0.tar.gz/netwell-0.3.0/netwell/__init__.py
@@ -0,0 +1,17 @@
+"""
+    _   __     __                ____
+   / | / /__  / /__      _____  / / /
+  /  |/ / _ \/ __/ | /| / / _ \/ / /
+ / /|  /  __/ /_ | |/ |/ /  __/ / /
+/_/ |_/\___/\__/ |__/|__/\___/_/_/
+"""
+
+VERSION = (0, 3, 0, 'final', 0)
+
+__title__ = 'netwell'
+__version_info__ = VERSION
+__version__ = '.'.join(map(str, VERSION[:3])) + ('-{}{}'.format(
+    VERSION[3], VERSION[4] or '') if VERSION[3] != 'final' else '')
+__author__ = 'Raymond Penners'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Raymond Penners'
---
+++ packages/netwell/netwell-0.3.0.tar.gz/netwell-0.3.0/netwell/command.py
@@ -0,0 +1,25 @@
+from netwell import checkers
+import argparse
+
+from importlib.machinery import SourceFileLoader
+
+
+def handle():
+    parser = argparse.ArgumentParser(
+        description='Run checkups for your net infrastructure.')
+    parser.add_argument(
+        'specfile',
+        metavar='specfile',
+        type=str,
+        nargs=1,
+        help='The netwell spec file')
+    parser.add_argument(
+        '-q',
+        '--quiet',
+        action='store_true',
+        help='Quiet, only report errors')
+
+    args = parser.parse_args()
+    checkers.output.quiet = args.quiet
+    SourceFileLoader("specfile", args.specfile[0]).load_module()
+    exit(1 if checkers.result.failures else 0)
