243a1b69864be5fc68935a493a36d2a26c1f081f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:43 +0000

    Add package backslash/2.31.0/backslash-2.31.0.tar.gz

---
+++ packages/backslash/backslash-2.31.0.tar.gz/backslash-2.31.0/backslash/test.py
@@ -0,0 +1,41 @@
+from sentinels import NOTHING
+
+from .api_object import APIObject
+from .commentable import Commentable
+from .error_container import ErrorContainer
+from .related_entity_container import RelatedEntityContainer
+from .warning_container import WarningContainer
+from .lazy_query import LazyQuery
+from .metadata_holder import MetadataHolder
+
+
+class Test(APIObject, MetadataHolder, ErrorContainer, WarningContainer, Commentable, RelatedEntityContainer):
+
+    @property
+    def ui_url(self):
+        return self.client.url + '#/sessions/{}/tests/{}'.format(self.session_display_id, self.logical_id or self.id)
+
+    def report_end(self, duration=NOTHING):
+        self.client.api.call_function('report_test_end', {'id': self.id, 'duration': duration})
+
+    def mark_skipped(self, reason=None):
+        self.client.api.call_function('report_test_skipped', {'id': self.id, 'reason': reason})
+
+    def report_interrupted(self):
+        self.client.api.call_function('report_test_interrupted', {'id': self.id})
+
+    def edit_status(self, status):
+        return self.client.api.call_function('edit_test_status', {'id': self.id, 'status': status})
+
+    def query_errors(self):
+        """Queries tests of the current session
+
+        :rtype: A lazy query object
+        """
+        return LazyQuery(self.client, '/rest/errors', query_params={'test_id': self.id})
+
+    def get_session(self):
+        return self.client.api.get('/rest/sessions/{0}'.format(self.session_id))
+
+    def get_parent(self):
+        return self.get_session()
---
+++ packages/backslash/backslash-2.31.0.tar.gz/backslash-2.31.0/docs/changelog.rst
@@ -0,0 +1,18 @@
+Changelog
+=========
+
+* :release:`2.31.0 <10-9-2017>`
+* :feature:`52` Support reporting sessions with a specific TTL, marking them for future deletion on the server
+* :feature:`51` Report local and remote SCM branches if supported
+* :release:`2.30.0 <8-8-2017>`
+* :feature:`50` Added session_webapp_url property to the Slash plugin
+* :release:`2.29.0 <6-8-2017>`
+* :feature:`49` Add hook for intercepting keepalive thread exceptions
+* :feature:`-` Move to PBR
+* :bug:`31 major` Be more resilient to I/O errors when compressing tracebacks
+* :feature:`39` Added ``webapp_url`` and ``rest_url`` to the official Slash plugin
+* :feature:`37` Support reporting fatal exceptions
+* :feature:`36` Added ``get_parent`` to test objects to retrieve the parent session
+* :release:`2.28.0 <15-05-2017>`
+* :feature:`-` Added Slash plugin option to propagate exceptions (useful for debugging)
+* :feature:`23` Enable injecting metadata via environment variables
