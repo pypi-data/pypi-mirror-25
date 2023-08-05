2da1e47b949eae6c63b802a5aba88f4fc9d137b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:20 +0000

    Add package our_groceries_client/0.1/our_groceries_client-0.1.tar.gz

---
+++ packages/our_groceries_client/our_groceries_client-0.1.tar.gz/our_groceries_client-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+      with open('README.rst') as f:
+            return f.read()
+
+setup(name='our_groceries_client',
+      version='0.1',
+      description='An interface to manipulating items on an OurGroceries.com list',
+      long_description=readme(),
+      keywords='grocery list',
+      url='https://github.com/smpickett/our_groceries_client',
+      author='smpickett',
+      author_email='smpickett@gmail.com',
+      license='MIT',
+      packages=['our_groceries_client'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      install_requires=[
+          'requests',
+          're',
+          'json',
+      ],
+      zip_safe=False)
