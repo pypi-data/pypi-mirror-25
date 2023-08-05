3707a8164967c25e33b15874ada28640296634f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:20 +0000

    Add package flask-notifyAll/1.0.2/flask_notifyAll-1.0.2.tar.gz

---
+++ packages/flask-notifyAll/flask_notifyAll-1.0.2.tar.gz/flask_notifyAll-1.0.2/flask_notifyAll/__init__.py
@@ -0,0 +1,9 @@
+"""
+:copyright: Mike Yusko
+:license: MIT, see LICENSE for more details.
+"""
+
+from .helpers import generate_verification_code  # noqa
+from .core import FlaskNotify
+
+__version__ = '1.0.2'
