dfe354566c317b5da910467a8646640fc2b6e678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:58 +0000

    Add package bwatch/1.0.1/bwatch-1.0.1.tar.gz

---
+++ packages/bwatch/bwatch-1.0.1.tar.gz/bwatch-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from watch import __version__, __author__, __url__
+
+setup(
+    name='bwatch',
+    version=__version__,
+    author=__author__,
+    py_modules=['watch'],
+    author_email="hellflamedly@gmail.com",
+    keywords=('binary watch', ),
+    license='MIT',
+    url=__url__,
+    entry_points={
+        'console_scripts': [
+            'bwatch=watch:terminal'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Terminals',
+        'Topic :: Text Processing'
+    ],
+    description="display a binary watch view in the terminal"
+)
+
