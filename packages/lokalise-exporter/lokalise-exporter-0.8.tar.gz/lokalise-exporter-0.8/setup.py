beb09b918b9c266161ba8e5d44bb6ea532e51600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:17 +0000

    Add package lokalise-exporter/0.8/lokalise-exporter-0.8.tar.gz

---
+++ packages/lokalise-exporter/lokalise-exporter-0.8.tar.gz/lokalise-exporter-0.8/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+setup(name='lokalise-exporter',
+      version='0.8',
+      description='Export strings from lokalise.co',
+      long_description='Utility tool to export localization strings from lokalise.co for iOS/Android/Frontend/Backend',
+      url='https://github.com/gotev/lokalise-exporter',
+      author='Aleksandar Gotev',
+      author_email='alexgotev@gmail.com',
+      license='Apache-2.0',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.0',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Software Development :: Internationalization',
+          'Topic :: Software Development :: Localization'
+      ],
+      keywords='localization l10n internationalization i18n lokalise localise export exporter tools development',
+      packages=['lokalise_exporter'],
+      entry_points={
+          # This has to be like this because of begins package for command line parsing
+          'console_scripts': ['lokalise-exporter=lokalise_exporter.main:main.start'],
+      },
+      install_requires=['requests', 'begins', 'colorlog', 'xmltodict', 'future',
+                        'backports.tempfile;python_version<"3.2"',
+                        'scandir;python_version<"3.5"'],
+      zip_safe=False)
