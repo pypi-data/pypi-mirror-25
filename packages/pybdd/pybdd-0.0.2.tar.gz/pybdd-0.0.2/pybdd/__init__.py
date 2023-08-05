4d8398dbc46bb316645235257f37b471f4651be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:53 +0000

    Add package pybdd/0.0.2/pybdd-0.0.2.tar.gz

---
+++ packages/pybdd/pybdd-0.0.2.tar.gz/pybdd-0.0.2/pybdd/__init__.py
@@ -0,0 +1,3 @@
+def joke():
+    return (u'This is test framework for BDD'
+            u'\nHope it will work..:)')
---
+++ packages/pybdd/pybdd-0.0.2.tar.gz/pybdd-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='pybdd',
+      version='0.0.2',
+      description='This is test framework for BDD',
+      url='https://github.com/HarshadKavathiya/pybdd',
+      download_url='https://github.com/HarshadKavathiya/pybdd/archive/master.tar.gz',
+      author='Harshad Kavathiya',
+      author_email='harshad3011@gmail.com',
+      license='MIT',
+      packages=['pybdd'],
+      keywords=['bdd', 'testing', 'logging', 'bdd', 'cucumber'],
+      classifiers=[],
+      zip_safe=False
+      )
