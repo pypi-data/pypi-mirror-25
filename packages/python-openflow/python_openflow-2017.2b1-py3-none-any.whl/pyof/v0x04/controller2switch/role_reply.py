92c18025127a7315bd40cdce53ea3981ab297779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:02 +0000

    Add package python-openflow/2017.2b1/python_openflow-2017.2b1-py3-none-any.whl

---
+++ packages/python-openflow/python_openflow-2017.2b1-py3-none-any.whl/pyof/v0x04/controller2switch/role_reply.py
@@ -0,0 +1,29 @@
+"""Request a change of the role of the controller."""
+
+# System imports
+
+# Third-party imports
+
+# Local source tree imports
+from pyof.v0x04.common.header import Type
+from pyof.v0x04.controller2switch.common import RoleBaseMessage
+
+__all__ = ('RoleReply',)
+
+# Classes
+
+
+class RoleReply(RoleBaseMessage):
+    """RoleReply Message."""
+
+    def __init__(self, xid=None, role=None, generation_id=None):
+        """Create a RoleReply with the optional parameters below.
+
+        Args:
+            xid (int): OpenFlow xid to the header.
+            role (:class:`~.controller2switch.common.ControllerRole`):
+                Is the new role that the controller wants to assume.
+            generation_id (int): Master Election Generation Id.
+        """
+        super().__init__(xid, role, generation_id)
+        self.header.message_type = Type.OFPT_ROLE_REPLY
