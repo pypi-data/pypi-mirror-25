7d417aba348cd1333ee422c014fbf8a44b4bb9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:33 +0000

    Add package nosql-versioning/0.1.3/nosql_versioning-0.1.3-py3-none-any.whl

---
+++ packages/nosql-versioning/nosql_versioning-0.1.3-py3-none-any.whl/nosql_versioning/__init__.py
@@ -0,0 +1,40 @@
+import json
+
+__version__ = '0.1.3'
+
+
+class VersionConflictionException(Exception):
+    def __init__(self, n):
+        super().__init__('Version {} has already been specified'.format(n))
+
+
+def database(version=None, decode=json.loads):
+    if version is None:
+        def version(o): return o['_ver']
+
+    def instance():
+        versions = {}
+        latest = -1
+
+        def version_descriptor(n=0):
+            def wrapper(cls):
+                if n in versions:
+                    raise VersionConflictionException(n)
+                versions[n] = cls
+                nonlocal latest
+                latest = max(latest, n)
+                return cls
+            return wrapper
+
+        def record(jstr):
+            jobj = decode(jstr)
+            n = version(jobj)
+            while n < latest:
+                cls = versions[n + 1]
+                cls.migrate(jobj)
+                n += 1
+            return versions[latest](jobj)
+
+        return version_descriptor, record
+
+    return instance()
