f34e46daa317f7fe3ed9e355aae0bfe7321ce44c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:58 +0000

    Add package hg-docgraph/0.0.1.dev0/hg-docgraph-0.0.1.dev0.tar.gz

---
+++ packages/hg-docgraph/hg-docgraph-0.0.1.dev0.tar.gz/hg-docgraph-0.0.1.dev0/setup.py
@@ -0,0 +1,16 @@
+#coding: utf8
+from setuptools import setup
+
+setup(
+    name='hg-docgraph',
+    version='0.0.1.dev',
+    author='Boris Feld',
+    author_email='boris.feld@octobus.next',
+    maintainer='Octobus',
+    maintainer_email='contact@octobus.net',
+    url='https://bitbucket.org/octobus/mercurial_docgraph',
+    description=('Mercurial extension to general graphivz output from mercurial repositories'),
+    keywords='hg mercurial',
+    license='GPLv2+',
+    packages=['hgext3rd'],
+)
