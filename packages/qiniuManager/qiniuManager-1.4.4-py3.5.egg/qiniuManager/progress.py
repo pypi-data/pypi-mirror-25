f89519c9d301555e7e54d8b51c30378625db2911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:29 +0000

    Add package qiniuManager/1.4.4/qiniuManager-1.4.4-py3.5.egg

---
+++ packages/qiniuManager/qiniuManager-1.4.4-py3.5.egg/qiniuManager/progress.py
@@ -0,0 +1,61 @@
+"""
+    Progress Bar decorator for classes
+"""
+import sys
+import os
+import math
+
+__all__ = ['bar']
+
+
+def bar(width=0, fill='#'):
+    """
+    进度条处理
+    :param width: 手动设置进度条宽度
+    :param fill: 进度填充字符
+    """
+    def function_wrapper(func):
+        def arguments(self, *args, **kwargs):
+            if not hasattr(self, 'progressed') or not hasattr(self, 'total'):
+                print("progressed, total attribute is needed!")
+                return
+            while self.progressed <= self.total:
+                func(self, *args, **kwargs)
+                if not hasattr(self, 'disable_progress') or not self.disable_progress:
+                    if self.total <= 0:
+                        print("Total Length Invalid !")
+                        self.progressed = self.total = 1
+                        break
+                    if not width:
+                        try:
+                            w = int(os.popen("stty size 2>/dev/null").read().split(" ")[1])
+                        except:
+                            w = 50
+                    else:
+                        w = width
+                    percent = self.progressed / float(self.total)
+                    # marks count
+                    percent_show = "{}%".format(int(percent * 100))
+                    # marks width
+                    if hasattr(self, 'title'):
+                        title = self.title
+                    else:
+                        title = ''
+                    mark_width = w - len(percent_show) - 5 - len(title) - 2
+                    mark_count = int(math.floor(mark_width * percent))
+                    sys.stdout.write(
+                        ' ' + title + ' ' +
+                        '[' + fill * mark_count + ' ' * (mark_width - mark_count) + ']  ' + percent_show + '\r')
+                    sys.stdout.flush()
+                    if self.progressed == self.total:
+                        sys.stdout.write(" " * w + '\r')
+                        sys.stdout.flush()
+                        break
+                else:
+                    if self.progressed == self.total:
+                        break
+        return arguments
+    return function_wrapper
+
+
+
