b22a24889f6b41173cd0b4cbe0a298af35fb748b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:50 +0000

    Add package eventify/0.4.4/eventify-0.4.4.tar.gz

---
+++ packages/eventify/eventify-0.4.4.tar.gz/eventify-0.4.4/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='eventify',
+    packages=find_packages(),
+    version='0.4.4',
+    description='Event Driven Asynchronous Framework',
+    author='Matthew Harris',
+    author_email='matt@x-qa.com',
+    url='https://github.com/eventifyio/eventify',
+    download_url='https://github.com/eventifyio/eventify/raw/master/dist/eventify-0.4.4.tar.gz',
+    keywords=['event', 'event-driven', 'async',
+              'framework', 'producer', 'consumer'],
+    classifiers=[],
+    install_requires=[
+        'asyncio',
+        'aioredis',
+        'asyncpg',
+        'pyOpenSSL',
+        'autobahn',
+        'service_identity',
+        'txaio'
+    ]
+)
