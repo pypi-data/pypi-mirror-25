f420419ef403ab794dc8fc80d92c782d1440366e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:46 +0000

    Add package UCCA/1.0.10/UCCA-1.0.10.tar.gz

---
+++ packages/UCCA/UCCA-1.0.10.tar.gz/UCCA-1.0.10/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert("README.md", "rst")
+except (IOError, ImportError, RuntimeError):
+    long_description = ""
+
+
+setup(name="UCCA",
+      version="1.0.10",
+      install_requires=["spacy", "requests"],
+      extras_require={"visualize": ["matplotlib", "networkx"]},
+      description="Universal Conceptual Cognitive Annotation",
+      long_description=long_description,
+      author="Daniel Hershcovich",
+      author_email="danielh@cs.huji.ac.il",
+      url="https://github.com/huji-nlp/ucca",
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Science/Research",
+          "Programming Language :: Python :: 3.6",
+          "Topic :: Text Processing :: Linguistic",
+          "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+      ],
+      packages=find_packages(),
+      )
