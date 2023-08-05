5640bf2aa83c524fac2308b11eae2eb5308933bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:46 +0000

    Add package vlcplaylister/17.9.1/vlcplaylister-17.9.1.tar.gz

---
+++ packages/vlcplaylister/vlcplaylister-17.9.1.tar.gz/vlcplaylister-17.9.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name = 'vlcplaylister',
+	version = '17.09.1',
+	scripts = ['playlister', 'README'],
+	description = 'VLC Playlist generater',
+	long_description = open('README').read(),
+	url = 'https://github.com/compmonk/playlister',
+	author = 'compmonk',
+	author_email = 'zsescape@gmail.com',
+	license='MIT',
+	keywords='vlc videos videos-series tv-series tv-shows playlist vlc-playlist',
+)
