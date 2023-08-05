832a70d91041cfd513824351281c5ad2d23d9d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:40 +0000

    Add package django-pgjsonb/0.0.29/django-pgjsonb-0.0.29.tar.gz

---
+++ packages/django-pgjsonb/django-pgjsonb-0.0.29.tar.gz/django-pgjsonb-0.0.29/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+README = open('README.md').read()
+VERSION = "0.0.29"
+
+setup(
+    name='django-pgjsonb',
+    version=VERSION,
+    description='Django Postgres JSONB Fields support with lookups',
+    url="https://github.com/yjmade/django-pgjsonb",
+    long_description=README,
+    author='Jay Young(yjmade)',
+    author_email='dev@yjmade.net',
+    packages=find_packages(),
+    install_requires=['Django>=1.7', 'psycopg2>=2.5.4'],
+)
