c96f0521f9838322cfbdffda831713b6b5999db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:17 +0000

    Add package bothub/0.1.24/bothub-0.1.24-py2.py3-none-any.whl

---
+++ packages/bothub/bothub-0.1.24-py2.py3-none-any.whl/tests/test_client.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+import requests_mock
+
+from bothub_client.bot import BaseBot
+from bothub_client.clients import handle_message
+
+
+class Bot(BaseBot):
+    def handle_message(self, event, context):
+        content = event.get('content')
+        self.send_message('hello, you said: {}'.format(content))
+
+
+def test_handle_message_should_add_channel_cmd_buff():
+    event = {
+        'content': 'hi!',
+        'channel': 'mychannel',
+        'sender': {
+            'id': 'abcd1234',
+            'name': 'myname'
+        }
+    }
+
+    context = {
+        'channel': {
+            'endpoint': 'http://localhost'
+        }
+    }
+
+    with requests_mock.mock() as m:
+        m.post('http://localhost/messages')
+        response = handle_message(event, context, Bot)
+        assert response['response'] is None
+        assert m.called
+        assert m.request_history[0].json() == {
+            'channel': None,
+            'receiver': None,
+            'message': 'hello, you said: hi!',
+            'event': event,
+            'extra': None,
+            'context': {'api_key': '', 'project_id': None}
+        }
