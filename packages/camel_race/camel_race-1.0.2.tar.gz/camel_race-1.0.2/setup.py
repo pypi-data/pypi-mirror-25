db53d96d0fdd5221e58c2900d2d0fef931d1e606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:12 +0000

    Add package camel_race/1.0.2/camel_race-1.0.2.tar.gz

---
+++ packages/camel_race/camel_race-1.0.2.tar.gz/camel_race-1.0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='camel_race',
+      version='1.0.2',
+      description='Camel Race ascii game',
+      author='Ibrahim Menem',
+      author_email='ibrahimmenem@gmail.com',
+      url='https://dummystack.com',
+      packages=find_packages(),
+      install_requires=['enum34'],
+      data_files = [('./games/camels_race', ['config.json'])],
+      entry_points={
+        'console_scripts': [
+            'camel_race=games.play:main',
+        ],
+       }
+
+     )
