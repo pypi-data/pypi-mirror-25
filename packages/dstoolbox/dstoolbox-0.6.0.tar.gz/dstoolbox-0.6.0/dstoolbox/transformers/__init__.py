ca24f7ab0ab5fd169226d241f7bccb76c00ff1b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:46 +0000

    Add package dstoolbox/0.6.0/dstoolbox-0.6.0.tar.gz

---
+++ packages/dstoolbox/dstoolbox-0.6.0.tar.gz/dstoolbox-0.6.0/dstoolbox/transformers/__init__.py
@@ -0,0 +1,29 @@
+"""Use this to allow imports, e.g.
+
+```
+from dstoolbox.transformers import ItemSelector
+```
+
+instead of
+
+```
+from dstoolbox.transformers.slicing import ItemSelector
+```
+
+"""
+from dstoolbox.pipeline import DataFrameFeatureUnion
+from dstoolbox.pipeline import PipelineY
+from dstoolbox.transformers.casting import ToDataFrame
+from dstoolbox.transformers.preprocessing import ParallelFunctionTransformer
+from dstoolbox.transformers.preprocessing import XLabelEncoder
+from dstoolbox.transformers.slicing import ItemSelector
+
+
+__all__ = [
+    'DataFrameFeatureUnion',
+    'ItemSelector',
+    'ParallelFunctionTransformer',
+    'PipelineY',
+    'ToDataFrame',
+    'XLabelEncoder',
+]
