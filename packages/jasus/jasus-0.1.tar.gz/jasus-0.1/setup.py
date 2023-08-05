31b330209fa40a7aec35915a47486abcd14a6d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:39 +0000

    Add package jasus/0.1/jasus-0.1.tar.gz

---
+++ packages/jasus/jasus-0.1.tar.gz/jasus-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jasus',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/Lokesh523s/funniest',
+      author='Flying Circus',
+      author_email='',
+      license='MIT',
+      packages=['jasus'],
+      zip_safe=False)
+
