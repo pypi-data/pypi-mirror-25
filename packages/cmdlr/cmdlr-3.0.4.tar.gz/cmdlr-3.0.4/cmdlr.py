e3c9b3413b5754e045f862d95bc053274fcd01b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:56 +0000

    Add package cmdlr/3.0.4/cmdlr-3.0.4.tar.gz

---
+++ packages/cmdlr/cmdlr-3.0.4.tar.gz/cmdlr-3.0.4/cmdlr.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Cmdlr command ui starting script."""
+
+import sys
+import os
+import importlib.abc
+
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+
+
+import cmdlr.cui as cui  # NOQA
+
+
+cui.main()
