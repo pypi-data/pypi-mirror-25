84789db3acf4b710218a1c36402faf98f100631a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:26 +0000

    Add package py-quiz/0.1/py-quiz-0.1.tar.gz

---
+++ packages/py-quiz/py-quiz-0.1.tar.gz/py-quiz-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='py-quiz',
+    version='0.1',
+    author='Abhijit Nathwani',
+    author_email='abhijit.nathwani@gmail.com',
+    url='https://github.com/abhijitnathwani/PyQuiz',
+    scripts=['main']
+)
