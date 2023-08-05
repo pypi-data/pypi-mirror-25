cbd4415595ee657b47ab389d852c6f32b6f018b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:23 +0000

    Add package YoClient/0.0.2/YoClient-0.0.2-py2-none-any.whl

---
+++ packages/YoClient/YoClient-0.0.2-py2-none-any.whl/yo_client/__init__.py
@@ -0,0 +1,4 @@
+from yo_client.yo_client import YoClient
+from yo_client.models import Coordinate
+import yo_client.urls
+import yo_client.request_parameters
---
+++ packages/YoClient/YoClient-0.0.2-py2-none-any.whl/yo_client/urls.py
@@ -0,0 +1,6 @@
+BASE_URL = "https://api.justyo.co"
+SEND_YO_URL = "{BASE_URL}/yo/".format(BASE_URL=BASE_URL)
+SEND_YOS_TO_ALL_SUBSCRIBERS_URL = "{BASE_URL}/yoall/".format(BASE_URL=BASE_URL)
+CREATE_ACCOUNT_URL = "{BASE_URL}/accounts/".format(BASE_URL=BASE_URL)
+USERNAME_EXISTS_URL = "{BASE_URL}/check_username/".format(BASE_URL=BASE_URL)
+SUBSCRIBERS_COUNT_URL = "{BASE_URL}/subscribers_count/".format(BASE_URL=BASE_URL)
