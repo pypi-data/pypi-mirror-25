315fb182684dea4a8d3a7203c51408ee82453cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:17 +0000

    Add package Telemetry_Pattern_Analyzer/0.24/Telemetry_Pattern_Analyzer-0.24.tar.gz

---
+++ packages/Telemetry_Pattern_Analyzer/Telemetry_Pattern_Analyzer-0.24.tar.gz/Telemetry_Pattern_Analyzer-0.24/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+     	name='Telemetry_Pattern_Analyzer',   
+     	version='0.24',                         
+     	scripts=['telem_pattern_analyzer.py']                  
+     )
