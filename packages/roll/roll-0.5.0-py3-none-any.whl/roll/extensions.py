1fb68a468a83939fa0faaac6fa56866db9cbab8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:10 +0000

    Add package roll/0.5.0/roll-0.5.0-py3-none-any.whl

---
+++ packages/roll/roll-0.5.0-py3-none-any.whl/roll/extensions.py
@@ -0,0 +1,83 @@
+import asyncio
+import logging
+from http import HTTPStatus
+from traceback import print_exc
+
+
+def cors(app, origin='*', methods=None):
+
+    @app.listen('response')
+    async def add_cors_headers(request, response):
+        response.headers['Access-Control-Allow-Origin'] = origin
+        if methods is not None:
+            allow_methods = ','.join(methods)
+            response.headers['Access-Control-Allow-Methods'] = allow_methods
+
+
+def logger(app, level=logging.DEBUG, handler=None):
+
+    logger = logging.getLogger('roll')
+    logger.setLevel(level)
+    handler = handler or logging.StreamHandler()
+
+    @app.listen('request')
+    async def log_request(request, response):
+        logger.info("{} {}".format(request.method, request.url.decode()))
+
+    @app.listen('startup')
+    async def startup():
+        logger.addHandler(handler)
+
+    @app.listen('shutdown')
+    async def shutdown():
+        logger.removeHandler(handler)
+
+
+def options(app):
+
+    @app.listen('request')
+    async def handle_options(request, response):
+        # Shortcut the request handling for OPTIONS requests.
+        return request.method == 'OPTIONS'
+
+
+def traceback(app):
+
+    @app.listen('error')
+    async def on_error(request, response, error):
+        if error.status == HTTPStatus.INTERNAL_SERVER_ERROR:
+            print_exc()
+
+
+def igniter(app):
+
+    @app.listen('startup')
+    async def make_it_roll_like_it_never_rolled_before():
+        logger = logging.getLogger('roll')
+        logger.debug('''
+         _          _                        _ _
+        | |        | |  ()                  | | |
+        | |     ___| |_  / ___     ____ ___ | | |
+        | |    / _ \ __|  / __|   |  __/ _ \| | |
+        | |___|  __/ |_   \__ \   | | | (_) | | |
+        |______\___|\__|  |___/   |_|  \___/|_|_| ()
+
+        ''')
+
+
+def simple_server(app, port=3579, host='127.0.0.1', quiet=False):
+    app.loop = asyncio.get_event_loop()
+    app.loop.run_until_complete(app.startup())
+    if not quiet:
+        print('Rolling on http://%s:%d' % (host, port))
+    server = app.loop.create_server(app.factory, host, port)
+    app.loop.create_task(server)
+    try:
+        app.loop.run_forever()
+    except KeyboardInterrupt:
+        if not quiet:
+            print('Bye.')
+    finally:
+        app.loop.run_until_complete(app.shutdown())
+        server.close()
+        app.loop.close()
