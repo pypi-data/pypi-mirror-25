009505f28317f4664c0ad62850d0ca2600bbe4a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:48 +0000

    Add package django-bootstrap4/0.0.3/django-bootstrap4-0.0.3.tar.gz

---
+++ packages/django-bootstrap4/django-bootstrap4-0.0.3.tar.gz/django-bootstrap4-0.0.3/README.rst
@@ -0,0 +1,95 @@
+======================
+Bootstrap 4 for Django
+======================
+
+Write Django as usual, and let ``django-bootstrap4`` make template output into Bootstrap 4 code.
+
+
+.. image:: https://travis-ci.org/zostera/django-bootstrap4.svg?branch=develop
+    :target: https://travis-ci.org/zostera/django-bootstrap4
+
+.. image:: https://img.shields.io/coveralls/dzostera/django-bootstrap4/master.svg
+  :target: https://coveralls.io/r/zostera/django-bootstrap4?branch=master
+
+.. image:: https://img.shields.io/pypi/v/django-bootstrap4.svg
+    :target: https://pypi.python.org/pypi/django-bootstrap4
+    :alt: Latest PyPI version
+
+.. image:: https://img.shields.io/pypi/dm/django-bootstrap4.svg
+    :target: https://pypi.python.org/pypi/django-bootstrap4
+    :alt: Number of PyPI downloads per month
+
+
+Requirements
+------------
+
+- Django >= 1.11 (and compatible Python version)
+
+
+Installation
+------------
+
+1. Install using pip:
+
+   ``pip install django-bootstrap4``
+
+   Alternatively, you can install download or clone this repo and call ``pip install -e .``.
+
+2. Add to INSTALLED_APPS in your ``settings.py``:
+
+   ``'bootstrap4',``
+
+3. In your templates, load the ``bootstrap4`` library and use the ``bootstrap_*`` tags:
+
+This app will soon require Django 1.8+, python 2.7+. Thanks for understanding.
+
+
+Example template
+----------------
+
+   .. code:: Django
+
+    {% load bootstrap4 %}
+
+    {# Display a form #}
+
+    <form action="/url/to/submit/" method="post" class="form">
+        {% csrf_token %}
+        {% bootstrap_form form %}
+        {% buttons %}
+            <button type="submit" class="btn btn-primary">Submit</button>
+        {% endbuttons %}
+    </form>
+
+
+Documentation
+-------------
+
+The full documentation is at https://django-bootstrap.readthedocs.org/.
+
+
+Bugs and suggestions
+--------------------
+
+If you have found a bug or if you have a request for additional functionality, please use the issue tracker on GitHub.
+
+https://github.com/zostera/django-bootstrap4/issues
+
+
+License
+-------
+
+You can use this under BSD-3-Clause. See `LICENSE
+<LICENSE>`_ file for details.
+
+
+Author
+------
+
+Developed and maintained by `Zostera <https://zostera.nl/>`_.
+
+Original author & Development lead: `Dylan Verheul <https://github.com/dyve>`_.
+
+Thanks to everybody that has contributed pull requests, ideas, issues, comments and kind words.
+
+Please see AUTHORS.rst for a list of contributors.
---
+++ packages/django-bootstrap4/django-bootstrap4-0.0.3.tar.gz/django-bootstrap4-0.0.3/bootstrap4/bootstrap.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from importlib import import_module
+
+from django.conf import settings
+
+# Default settings
+
+BOOTSTRAP4_DEFAULTS = {
+    'base_url': None,  # '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/'
+    'css_url': {
+        'href': "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css",
+        'integrity': "sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M",
+        'crossorigin': "anonymous",
+    },
+    'theme_url': None,
+    'jquery_url': {
+        'url': 'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+        'crossorigin': 'anonymous',
+    },
+    'tether_url': {
+        'url': "https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js",
+        'integrity': "sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb",
+        'crossorigin': "anonymous",
+
+    },
+    'popper_url': {
+        'url': "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.5/umd/popper.min.js",
+        'integrity': "sha256-jpW4gXAhFvqGDD5B7366rIPD7PDbAmqq4CO0ZnHbdM4=",
+        'crossorigin': "anonymous",
+
+    },
+    'javascript_url': {
+        'url': "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js",
+        'integrity': "sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1",
+        'crossorigin': "anonymous",
+    },
+    'javascript_in_head': False,
+    'include_jquery': False,
+    'horizontal_label_class': 'col-md-3',
+    'horizontal_field_class': 'col-md-9',
+
+    'set_placeholder': True,
+    'required_css_class': '',
+    'error_css_class': 'has-error',
+    'success_css_class': 'has-success',
+    'formset_renderers': {
+        'default': 'bootstrap4.renderers.FormsetRenderer',
+    },
+    'form_renderers': {
+        'default': 'bootstrap4.renderers.FormRenderer',
+    },
+    'field_renderers': {
+        'default': 'bootstrap4.renderers.FieldRenderer',
+        'inline': 'bootstrap4.renderers.InlineFieldRenderer',
+    },
+}
+
+
+def get_bootstrap_setting(name, default=None):
+    """
+    Read a setting
+    """
+    # Start with a copy of default settings
+    BOOTSTRAP4 = BOOTSTRAP4_DEFAULTS.copy()
+
+    # Override with user settings from settings.py
+    BOOTSTRAP4.update(getattr(settings, 'BOOTSTRAP4', {}))
+
+    return BOOTSTRAP4.get(name, default)
+
+
+def bootstrap_url(postfix):
+    """
+    Prefix a relative url with the bootstrap base url
+    """
+    return get_bootstrap_setting('base_url') + postfix
+
+
+def jquery_url():
+    """
+    Return the full url to jQuery file to use
+    """
+    return get_bootstrap_setting('jquery_url')
+
+
+def tether_url():
+    """
+    Return the full url to the Bootstrap JavaScript file
+    """
+    return get_bootstrap_setting('tether_url')
+
+
+def popper_url():
+    """
+    Return the full url to Popper file
+    """
+    return get_bootstrap_setting('popper_url')
+
+
+def javascript_url():
+    """
+    Return the full url to the Bootstrap JavaScript file
+    """
+    url = get_bootstrap_setting('javascript_url')
+    return url if url else bootstrap_url('js/bootstrap.min.js')
+
+
+def css_url():
+    """
+    Return the full url to the Bootstrap CSS file
+    """
+    url = get_bootstrap_setting('css_url')
+    return url if url else bootstrap_url('css/bootstrap.min.css')
+
+
+def theme_url():
+    """
+    Return the full url to the theme CSS file
+    """
+    return get_bootstrap_setting('theme_url')
+
+
+def get_renderer(renderers, **kwargs):
+    layout = kwargs.get('layout', '')
+    path = renderers.get(layout, renderers['default'])
+    mod, cls = path.rsplit(".", 1)
+    return getattr(import_module(mod), cls)
+
+
+def get_formset_renderer(**kwargs):
+    renderers = get_bootstrap_setting('formset_renderers')
+    return get_renderer(renderers, **kwargs)
+
+
+def get_form_renderer(**kwargs):
+    renderers = get_bootstrap_setting('form_renderers')
+    return get_renderer(renderers, **kwargs)
+
+
+def get_field_renderer(**kwargs):
+    renderers = get_bootstrap_setting('field_renderers')
+    return get_renderer(renderers, **kwargs)
