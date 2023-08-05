c6185c190bb1303f8cfabdccf7d139582d06db27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:06 +0000

    Add package pyunsplash/1.0.0b3/pyunsplash-1.0.0b3.zip

---
+++ packages/pyunsplash/pyunsplash-1.0.0b3.zip/pyunsplash-1.0.0b3/pyunsplash/__init__.py
@@ -0,0 +1,17 @@
+###############################################################################
+#
+#      File: pyunsplash.py
+#
+#    Author: Salvatore Ventura <salvoventura@gmail.com>
+#      Date: 08 Dec 2016
+#   Purpose: simplify import statements
+#
+#   Comment:
+#
+###############################################################################
+from pyunsplash import PyUnsplash
+# TRAVIS_BUILD_NUMBER
+# TRAVIS_COMMIT
+# TRAVIS_COMMIT_RANGE
+# TRAVIS_TAG
+__version__ = '1.0.0b3'
