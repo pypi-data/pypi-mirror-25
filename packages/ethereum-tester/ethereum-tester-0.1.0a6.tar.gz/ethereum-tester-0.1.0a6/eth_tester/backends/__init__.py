af50bb9332ad07412dfa6c26a72ebd30aa667632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:48 +0000

    Add package ethereum-tester/0.1.0a6/ethereum-tester-0.1.0a6.tar.gz

---
+++ packages/ethereum-tester/ethereum-tester-0.1.0a6.tar.gz/ethereum-tester-0.1.0a6/eth_tester/backends/__init__.py
@@ -0,0 +1,34 @@
+import os
+
+from eth_tester.utils.module_loading import (
+    get_import_path,
+    import_string,
+)
+
+from .mock import (  # noqa: F401
+    MockBackend,
+)
+from .pyethereum.v16 import (
+    PyEthereum16Backend,
+)
+from .pyethereum.v20 import (  # noqa: F401
+    PyEthereum20Backend,
+)
+
+
+DEFAULT_CHAIN_BACKEND_CLASS = get_import_path(PyEthereum16Backend)
+
+
+def get_chain_backend_class(backend_import_path=None):
+    if backend_import_path is None:
+        backend_import_path = os.environ.get(
+            'ETHEREUM_TESTER_CHAIN_BACKEND',
+            DEFAULT_CHAIN_BACKEND_CLASS,
+        )
+    return import_string(backend_import_path)
+
+
+def get_chain_backend(backend_class=None):
+    if backend_class is None:
+        backend_class = get_chain_backend_class()
+    return backend_class()
