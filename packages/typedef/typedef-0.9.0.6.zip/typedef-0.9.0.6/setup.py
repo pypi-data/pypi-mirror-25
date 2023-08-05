27ede111d36e89779218a08a88ef0ec632e6a37a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:05 +0000

    Add package typedef/0.9.0.6/typedef-0.9.0.6.zip

---
+++ packages/typedef/typedef-0.9.0.6.zip/typedef-0.9.0.6/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import typedef
+
+setup(
+    name='typedef',
+    packages=['typedef'],
+    version=typedef.__version__,
+    description=typedef.__description__,
+    author='bdr00',
+    author_email='bad.32@outlook.com',
+    url='https://github.com/bdr00/typedef',
+    download_url='https://github.com/bdr00/typedef/archive/v{}.tar.gz'.format(typedef.__version__),
+    keywords=['typedef', 'struct', 'union', ' pack', 'unpack', 'binary'],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent'
+    ],
+    license='MIT'
+)
