b1476f88f18224cb918496912656d7d7967f942b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:30 +0000

    Add package mpf-mc/0.33.15/mpf_mc-0.33.15-cp34-none-win_amd64.whl

---
+++ packages/mpf-mc/mpf_mc-0.33.15-cp34-none-win_amd64.whl/mpfmc/widgets/line.py
@@ -0,0 +1,24 @@
+from kivy.graphics import Line as KivyLine
+from kivy.graphics.context_instructions import Color
+from kivy.uix.widget import Widget
+from mpfmc.uix.widget import MpfWidget
+
+
+class Line(MpfWidget, Widget):
+
+    widget_type_name = 'Line'
+
+    def on_pos(self, *args):
+        del args
+
+        with self.canvas:
+            Color(*self.config['color'])
+            KivyLine(points=self.config['points'],
+                     width=self.config['thickness'],
+                     cap=self.config['cap'],
+                     joint=self.config['joint'],
+                     cap_precision=self.config['cap_precision'],
+                     joint_precision=self.config['joint_precision'],
+                     close=self.config['close'])
+
+widget_classes = [Line]
---
+++ packages/mpf-mc/mpf_mc-0.33.15-cp34-none-win_amd64.whl/mpfmc/widgets/triangle.py
@@ -0,0 +1,18 @@
+from kivy.graphics import Triangle as KivyTriangle
+from kivy.graphics.context_instructions import Color
+from kivy.uix.widget import Widget
+from mpfmc.uix.widget import MpfWidget
+
+
+class Triangle(MpfWidget, Widget):
+
+    widget_type_name = 'Triangle'
+
+    def on_pos(self, *args):
+        del args
+
+        with self.canvas:
+            Color(*self.config['color'])
+            KivyTriangle(points=self.config['points'])
+
+widget_classes = [Triangle]
