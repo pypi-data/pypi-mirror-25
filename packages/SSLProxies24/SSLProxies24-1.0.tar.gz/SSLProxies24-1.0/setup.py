2b9ca2ab97bc6c122f306faa92c56aa32df0634b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:11 +0000

    Add package SSLProxies24/1.0/SSLProxies24-1.0.tar.gz

---
+++ packages/SSLProxies24/SSLProxies24-1.0.tar.gz/SSLProxies24-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+package_name = "SSLProxies24"
+package_version = "1.0"
+
+setup(
+    name=package_name,
+    version=package_version,
+    packages=[package_name],
+    author="Henrique da Silva Santos (rique_dev)",
+    author_email="rique_dev@hotmail.com",
+    url="https://github.com/riquedev/SSLProxies24Feed",
+    download_url='https://github.com/riquedev/SSLProxies24Feed/archive/master.zip',
+    keywords=['proxy', 'ssl', '24', 'feed', 'blog', 'anonymous']
+)
