564c78611981c17fd617fab9918bffced95a8d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:11 +0000

    Add package json_ptr/0.0.0/json_ptr-0.0.0.tar.gz

---
+++ packages/json_ptr/json_ptr-0.0.0.tar.gz/json_ptr-0.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='json_ptr',
+    author='Chris Ermel',
+    author_email='ermel272@gmail.com',
+    version='0.0.0',
+    description='An implementation of IETF RFC 6901',
+    license='MIT',
+    url='https://github.com/ermel272/json-pointer.git',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords='JSON pointer IETF RFC 6901',
+    install_requires=[],
+    extras_require={
+        'dev': [
+            'setuptools==36.2.7',
+            'sphinx==1.6.3'
+        ],
+        'test': [
+            'nose==1.3.7',
+            'coverage==4.4.1',
+            'coveralls==1.2.0'
+        ]
+    },
+    packages=find_packages(),
+    include_package_data=True
+)
