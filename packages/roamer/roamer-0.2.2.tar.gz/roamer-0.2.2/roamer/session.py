dac95a3f0a4ec13870b9469db076aa0fa723ace5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:08 +0000

    Add package roamer/0.2.2/roamer-0.2.2.tar.gz

---
+++ packages/roamer/roamer-0.2.2.tar.gz/roamer-0.2.2/roamer/session.py
@@ -0,0 +1,37 @@
+"""
+Represents a single session of roamer.
+Preps the state, gathers records from disk, opens user's file editor
+and passes into the Engine for processing
+"""
+
+from __future__ import print_function
+import os
+from roamer.file_edit import file_editor
+from roamer.directory import Directory
+from roamer.edit_directory import EditDirectory
+from roamer.engine import Engine
+from roamer import record
+from roamer.database import db_init
+
+class Session(object):
+    def __init__(self, cwd=None):
+        self.cwd = cwd
+        if cwd is None:
+            self.cwd = os.getcwd()
+        db_init()
+        raw_entries = os.listdir(self.cwd)
+        self.directory = Directory(self.cwd, raw_entries)
+        self.edit_directory = None
+        record.add_dir(self.directory)
+
+    def run(self):
+        output = file_editor(self.directory.text())
+        self.process(output)
+
+    def process(self, output):
+        self.edit_directory = EditDirectory(self.cwd, output)
+        engine = Engine(self.directory, self.edit_directory)
+        engine.compile_commands()
+        print(engine.commands_to_str())
+        engine.run_commands()
+        record.add_dir(Directory(self.cwd, os.listdir(self.cwd)))
---
+++ packages/roamer/roamer-0.2.2.tar.gz/roamer-0.2.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+version = '0.2.2'
+setup(
+  name = 'roamer',
+  packages = ['roamer'],
+  version = version,
+  description = 'Plain Text File Explorer',
+  author = 'Alex Baldwin',
+  author_email = 'abaldwintech+hub@gmail.com',
+  url = 'https://github.com/abaldwin88/roamer',
+  download_url = 'https://github.com/abaldwin88/roamer/archive/v%s.zip' % version,
+  keywords = ['explorer', 'plain text', 'directory'], # arbitrary keywords
+  classifiers = [],
+  scripts=['bin/roamer'],
+)
