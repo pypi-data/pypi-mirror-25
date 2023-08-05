3d5c802bb51abfac737b4c43ba185775cf2982b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package tomodachi/0.4.5/tomodachi-0.4.5.tar.gz

---
+++ packages/tomodachi/tomodachi-0.4.5.tar.gz/tomodachi-0.4.5/tomodachi/protocol/json_base.py
@@ -0,0 +1,53 @@
+import ujson
+import uuid
+import time
+from typing import Any, Dict, Tuple, Union
+
+PROTOCOL_VERSION = 'json_base-wip'
+COMPATIBLE_PROTOCOL_VERSIONS = ['json_base-wip']
+
+
+class JsonBase(object):
+    @classmethod
+    async def build_message(cls, service: Any, topic: str, data: Any) -> str:
+        _uuid = str(uuid.uuid4())
+        message = {
+            'service': {
+                'name': getattr(service, 'name', None),
+                'uuid': getattr(service, 'uuid', None)
+            },
+            'metadata': {
+                'message_uuid': '{}.{}'.format(getattr(service, 'uuid', ''), _uuid),
+                'protocol_version': PROTOCOL_VERSION,
+                'compatible_protocol_versions': COMPATIBLE_PROTOCOL_VERSIONS,
+                'timestamp': time.time(),
+                'topic': topic
+            },
+            'data': data
+        }
+        return ujson.dumps(message)
+
+    @classmethod
+    async def parse_message(cls, payload: str) -> Union[Dict, Tuple]:
+        message = ujson.loads(payload)
+
+        compatible_protocol_versions = message.get('metadata', {}).get('compatible_protocol_versions')
+        message_uuid = message.get('metadata', {}).get('message_uuid')
+        timestamp = message.get('metadata', {}).get('timestamp')
+        if PROTOCOL_VERSION not in compatible_protocol_versions:
+            return False, message_uuid, timestamp
+
+        return {
+            'service': {
+                'name': message.get('service', {}).get('name'),
+                'uuid': message.get('service', {}).get('uuid')
+            },
+            'metadata': {
+                'message_uuid': message.get('metadata', {}).get('message_uuid'),
+                'protocol_version': message.get('metadata', {}).get('protocol_version'),
+                'compatible_protocol_versions': message.get('metadata', {}).get('compatible_protocol_versions'),
+                'timestamp': message.get('metadata', {}).get('timestamp'),
+                'topic': message.get('metadata', {}).get('topic')
+            },
+            'data': message.get('data')
+        }, message_uuid, timestamp
