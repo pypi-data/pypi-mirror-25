10e8f7980ec9bfe7cfbcfec0e0cda98e4238f08c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:57 +0000

    Add package dbrhino-agent/0.1.0a8/dbrhino_agent-0.1.0a8-py3-none-any.whl

---
+++ packages/dbrhino-agent/dbrhino_agent-0.1.0a8-py3-none-any.whl/dbrhino_agent/db/utils.py
@@ -0,0 +1,21 @@
+from collections import namedtuple
+import re
+import logging
+from functools import wraps
+
+
+class Database(object):
+    def __init__(self, *, name, connect_to, parsed_url):
+        self.name = name
+        self.connect_to = connect_to
+        self.parsed_url = parsed_url
+        self.type = parsed_url.scheme
+
+
+class GrantPermission(object):
+    NONE = "none"
+    SELECT = "select"
+
+
+def first_column(cur):
+    return [x for x, in cur.fetchall()]
---
+++ packages/dbrhino-agent/dbrhino_agent-0.1.0a8-py3-none-any.whl/dbrhino_agent/grants.py
@@ -0,0 +1,36 @@
+import re
+
+IDENTIFIER_PATTERN = re.compile(r"^\w+$")
+
+
+class Statement(object):
+    def __init__(self, *, permissions, type=None, match=None, on=None,
+                 **kwargs):
+        self.permissions = permissions \
+            if isinstance(permissions, list) else [permissions]
+        self.type = type
+        self.match = match
+        self.on = on
+        self.extra = kwargs
+
+    def matches_schema(self, schema):
+        return self.match[0] == "*" or self.match[0] == schema
+
+
+class Policy(object):
+    def __init__(self, *, statements):
+        stmts = []
+        for stmt_def in statements:
+            stmts.append(Statement(**stmt_def))
+        self.statements = stmts
+
+
+class Grant(object):
+    def __init__(self, *, id, database, username, policy, version,
+                 password=None):
+        self.id = id
+        self.database = database
+        self.username = username
+        self.policy = Policy(**policy)
+        self.version = version
+        self.password = password
