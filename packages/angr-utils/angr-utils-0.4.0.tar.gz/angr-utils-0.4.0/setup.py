0dab0986a6ebdac1887dee74c61b6b0350d587ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:35 +0000

    Add package angr-utils/0.4.0/angr-utils-0.4.0.tar.gz

---
+++ packages/angr-utils/angr-utils-0.4.0.tar.gz/angr-utils-0.4.0/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='angr-utils',
+    version='0.4.0',
+    author='Attila Axt',
+    author_email='axt@load.hu',
+    license='BSD',
+    platforms=['Linux'],
+    packages=['angrutils'],
+    install_requires=[
+        'pydot',
+        'networkx',
+        'angr',
+        'claripy',
+        'simuvex',
+        'bingraphvis >= 0.2.0'
+    ],
+    description='Various utilities for angr binary analysis framework',
+    long_description='Various utilities for angr binary analysis framework',
+    url='https://github.com/axt/angr-utils',
+)
