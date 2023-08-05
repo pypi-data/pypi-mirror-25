0a7ea843cf48cee694adae9593e8ba479a292d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:06 +0000

    Add package httpie-f5-auth/0.0.4/httpie-f5-auth-0.0.4.tar.gz

---
+++ packages/httpie-f5-auth/httpie-f5-auth-0.0.4.tar.gz/httpie-f5-auth-0.0.4/httpie_f5_auth.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+F5 BIG-IQ auth plugin for HTTPie.
+"""
+import requests
+from requests_f5auth import XF5Auth
+from httpie.plugins import AuthPlugin
+
+__version__ = '0.0.4'
+__author__ = 'ivan mecimore'
+__license__ = 'MIT'
+
+class F5AuthPlugin(AuthPlugin):
+    """Plugin registration"""
+
+    name = 'X-F5-Auth-Token auth'
+    auth_type = 'xf5'
+    description = 'Authenticate using an X-F5-Auth-Token'
+
+    def get_auth(self, username, password):
+        return XF5Auth(username, password)
