cb2b90934dee67d1b27c9e15268ebfa6c1b470e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:25 +0000

    Add package telegramPublishBot/0.1.5/telegramPublishBot-0.1.5.tar.gz

---
+++ packages/telegramPublishBot/telegramPublishBot-0.1.5.tar.gz/telegramPublishBot-0.1.5/telegramPublishBot/publisher.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+from .token import *
+from .network import postAsync
+
+
+URL = lambda token, method:\
+    "https://api.telegram.org/bot%s/%s" % (token, method)
+
+def getMe(token):
+    url = URL(token, "getMe")
+    r = requests.get(url)
+    try:
+        j = r.json()
+        return j["ok"] == True
+    except:
+        return False
+    
+
+def publishToTelegram(token, channel, data, silent=False):
+    try:
+        assert type(data) in [str, bytes]
+        if type(data) == bytes:
+            data = data.decode('utf-8')
+        data = json.loads(data)
+    except:
+        raise Exception("Data sent to server must be stringified JSON.")
+
+    action = data["action"]
+    legitKeys = {
+        "sendMessage": ["text", "parse_mode"],
+        "sendPhoto": ["photo", "caption"],
+        "sendLocation": ["latitude", "longitude"],
+    }
+
+    assert action in legitKeys
+
+    url = URL(token, action)
+    print(url)
+    
+    payload = {
+        "chat_id": channel,
+        "disable_notification": silent,
+    }
+
+    for key in data:
+        if key in legitKeys[action]:
+            payload[key] = data[key]
+
+    postAsync(url, payload)
+    return
+
+    r = requests.post(url, data = payload)
+    print(r.status_code)
+    print(r.text)
+    return r.status_code
---
+++ packages/telegramPublishBot/telegramPublishBot-0.1.5.tar.gz/telegramPublishBot-0.1.5/telegramPublishBot/server.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import json
+import base64
+
+from bottle import *
+
+from .publisher import *
+from .token import *
+
+
+__CONFIG = None
+
+
+@get("/")
+def hello():
+    return "Publish to telegram channels."
+
+@get("/<token:re:pub:[0-9a-z]+>/test")
+def test(token):
+    global __CONFIG
+    if token in __CONFIG["authorized"]:
+        return "%s is okay." % token
+    return abort(404, "This token is not registered.")
+
+@post("/<token:re:pub:[0-9a-z]+>/publish")
+def publish(token):
+    global __CONFIG
+    if token not in __CONFIG["authorized"]:
+        return abort(401, "Unauthorized access with this token.")
+    else:
+        channelConfig = __CONFIG["authorized"][token]
+    try:
+        data = verify(token, base64.b64decode(request.body.read()))
+        assert data != None
+        silent = False
+        if "silent" in channelConfig:
+            silent = channelConfig["silent"]
+        publishToTelegram(\
+            __CONFIG["token"],          # access token for telegram
+            channelConfig["channel"], # access token given by user
+            data,
+            silent=silent
+        )
+    except:
+        return abort(400, "No valid data received for this token.")
+    return data
+
+
+
+
+
+def startServer(config):
+    global __CONFIG
+    __CONFIG = config
+    run(host="127.0.0.1", port=3113)
