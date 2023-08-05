1180f33248983244d3699796a7331458d263f24a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:21 +0000

    Add package glow-navigator/1.15.3/glow_navigator-1.15.3-py2-none-any.whl

---
+++ packages/glow-navigator/glow_navigator-1.15.3-py2-none-any.whl/glow_navigator/glow_templates.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=redefined-builtin
+# pylint: disable=invalid-name
+
+"""Glow Navigator Template Lookup
+
+Create lookup for old style Page -> PK in formflows
+"""
+
+# python2 and python3 portability
+from __future__ import print_function
+
+import glob
+import os.path
+import click
+
+from . glow_config import settings
+from . glow_utils import load_yaml_file
+
+TEMPLATE_LOOKUP = {}
+abs_path = os.path.abspath(settings["template"]["path"])
+label_text = "{0:25}".format("Loading template lookup")
+
+with click.progressbar(glob.glob(abs_path), label=label_text, show_eta=False) as bar:
+    for file_name in bar:
+        values = load_yaml_file(file_name)
+        TEMPLATE_LOOKUP[values["VZ_FormID"]] = values["VZ_PK"]
