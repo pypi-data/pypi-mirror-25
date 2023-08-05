a6ef89ce8f7740dda2d1760dd8f21fd48680a302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:05 +0000

    Add package hb-config/0.1.0/hb-config-0.1.0.tar.gz

---
+++ packages/hb-config/hb-config-0.1.0.tar.gz/hb-config-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+HB_CONFIG_VERSION = '0.1.0'
+
+setup (
+    name='hb-config',
+    version=HB_CONFIG_VERSION,
+    author='Dongjun Lee',
+    packages=find_packages(),
+    author_email='humanbrain.djlee@gmail.com',
+    description='easy to configure your python packge',
+    keywords = ['config', 'util'],
+    license='MIT license'
+)
