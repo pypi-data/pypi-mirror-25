aaf926328fd382c282fab4f22b194ed01a8aba1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:07 +0000

    Add package goodtables/1.4.0/goodtables-1.4.0.tar.gz

---
+++ packages/goodtables/goodtables-1.4.0.tar.gz/goodtables-1.4.0/goodtables/checks/unique_constraint.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from ..spec import spec
+from ..registry import check
+
+
+# Module API
+
+@check('unique-constraint', type='schema', context='body')
+class UniqueConstraint(object):
+
+    # Public
+
+    def __init__(self, **options):
+        self.__row_indexes = {}
+
+    def check_row(self, errors, cells, row_number):
+        for cell in cells:
+
+            # Skip if cell is incomplete
+            if not set(cell).issuperset(['number', 'header', 'field', 'value']):
+                continue
+
+            # Skip if not constraint
+            constraint = cell['field'].constraints.get('unique')
+            if not constraint:
+                continue
+
+            # Skip if value is null
+            if cell['value'] is None:
+                continue
+
+            # Get references
+            rindex = self.__row_indexes.setdefault(cell['number'], {})
+            references = rindex.setdefault(cell['value'], [])
+
+            # Add error
+            if references:
+                message = spec['errors']['unique-constraint']['message']
+                message = message.format(
+                    row_numbers=', '.join(map(str, references + [row_number])),
+                    column_number=cell['number'])
+                errors.append({
+                    'code': 'unique-constraint',
+                    'message': message,
+                    'row-number': row_number,
+                    'column-number': cell['number'],
+                })
+
+            # Update references
+            references.append(row_number)
