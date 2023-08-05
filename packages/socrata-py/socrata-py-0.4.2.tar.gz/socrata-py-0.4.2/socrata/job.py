732f6e2dd7ed4232a8dce85cfc7e6b56d99da8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:11 +0000

    Add package socrata-py/0.4.2/socrata-py-0.4.2.tar.gz

---
+++ packages/socrata-py/socrata-py-0.4.2.tar.gz/socrata-py-0.4.2/socrata/job.py
@@ -0,0 +1,25 @@
+import time
+from socrata.http import noop
+from socrata.resource import Resource
+
+class Job(Resource):
+    def is_complete(self):
+        """
+        Has this job finished or failed
+        """
+        status = self.attributes['status']
+        return (status == 'failure' or status == 'successful')
+
+    def wait_for_finish(self, progress = noop):
+        """
+        Wait for this job to finish applying to the underlying
+        dataset
+        """
+        while not self.is_complete():
+            (ok, job) = self.show()
+            progress(self)
+            if not ok:
+                return (ok, job)
+            time.sleep(1)
+        return (True, self)
+
