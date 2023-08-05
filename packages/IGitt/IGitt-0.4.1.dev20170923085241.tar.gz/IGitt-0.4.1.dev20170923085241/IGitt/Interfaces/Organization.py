0d2beae0699d2b155d170256219229c2ef3221f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:02 +0000

    Add package IGitt/0.4.1.dev20170923085241/IGitt-0.4.1.dev20170923085241.tar.gz

---
+++ packages/IGitt/IGitt-0.4.1.dev20170923085241.tar.gz/IGitt-0.4.1.dev20170923085241/IGitt/Interfaces/Organization.py
@@ -0,0 +1,39 @@
+"""
+This module contains the Issue abstraction class which provides properties and
+actions related to issues and bug reports.
+"""
+from IGitt.Interfaces import IGittObject
+
+
+class Organization(IGittObject):
+    """
+    Represents an organization on GitHub or GitLab.
+    """
+
+    @property
+    def billable_users(self) -> int:
+        """
+        Number of paying/registered users on the organization.
+        """
+        raise NotImplementedError
+
+    @property
+    def owners(self) -> {str}:
+        """
+        Returns the user handles of all admin users, usually the owner role.
+        """
+        raise NotImplementedError
+
+    @property
+    def name(self) -> str:
+        """
+        The name of the organization.
+        """
+        raise NotImplementedError
+
+    @property
+    def url(self) -> str:
+        """
+        Retrieves the url of the issue.
+        """
+        raise NotImplementedError
