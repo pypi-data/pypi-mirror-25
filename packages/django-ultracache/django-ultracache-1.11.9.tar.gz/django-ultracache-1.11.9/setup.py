a007f3d4b4dac05fff6ecc712cb97d7437b04a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:57 +0000

    Add package django-ultracache/1.11.9/django-ultracache-1.11.9.tar.gz

---
+++ packages/django-ultracache/django-ultracache-1.11.9.tar.gz/django-ultracache-1.11.9/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-ultracache",
+    description="Drop-in replacement for Django's template fragment caching. Provides automatic cache invalidation.",
+    long_description = open("README.rst", "r").read() + open("AUTHORS.rst", "r").read() + open("CHANGELOG.rst", "r").read(),
+    version="1.11.9",
+    author="Praekelt Consulting",
+    author_email="dev@praekelt.com",
+    license="BSD",
+    url="http://github.com/praekelt/django-ultracache",
+    packages = find_packages(),
+    dependency_links = [
+    ],
+    install_requires = [
+        "django",
+        "requests",
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
