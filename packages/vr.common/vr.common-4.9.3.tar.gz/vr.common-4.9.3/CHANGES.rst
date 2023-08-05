cb121c36c990cd010bd58f3dead63d415cd408d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:48 +0000

    Add package vr.common/4.9.3/vr.common-4.9.3.tar.gz

---
+++ packages/vr.common/vr.common-4.9.3.tar.gz/vr.common-4.9.3/CHANGES.rst
@@ -0,0 +1,50 @@
+4.9.2
+=====
+
+Clean empty pools in StingRay balancer
+
+4.9.1
+=====
+
+Remove dependency to SetuptoolsVersion
+
+4.9
+===
+
+Make overlayfs configuration dependent on LXC
+
+4.8.2
+=====
+
+Fixed ImportError in 'vr.common.balancer.varnish'.
+
+4.8
+===
+
+Moved project to Github.
+
+Incorporated `project
+skeleton from jaraco <https://github.com/jaraco/skeleton>`_.
+Enabled automatic releases of tagged commits.
+
+4.6.1
+=====
+
+Fix QueryResult iterability on Python 3.
+
+4.6
+===
+
+In models, allow parameters to be supplied when loading
+entities.
+
+4.5
+===
+
+Nicer error reporting in Velociraptor client when credentials are
+invalid.
+
+4.0
+===
+
+Installation now requires pip 6.x or later.
---
+++ packages/vr.common/vr.common-4.9.3.tar.gz/vr.common-4.9.3/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+    long_description = readme.read()
+
+name = 'vr.common'
+description = 'Libraries and for deploying with Velociraptor'
+
+params = dict(
+    name=name,
+    use_scm_version=True,
+    author="Brent Tubbs",
+    author_email="brent.tubbs@gmail.com",
+    description=description or name,
+    long_description=long_description,
+    url="https://github.com/yougov/" + name,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    namespace_packages=name.split('.')[:-1],
+    python_requires='>=2.7',
+    install_requires=[
+        'isodate>=0.4.4',
+        'six>=1.4.1',
+        'utc',
+        'requests',
+        'PyYAML>=3.10',
+        'sseclient==0.0.11',
+    ],
+    extras_require={
+        ':python_version=="2.7"': [
+            'suds==0.4',
+        ],
+    },
+    setup_requires=[
+        'setuptools_scm>=1.15.0',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points={
+    },
+)
+if __name__ == '__main__':
+	setuptools.setup(**params)
