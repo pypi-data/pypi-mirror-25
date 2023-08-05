e554190156a3d7c8e943da992da07770df3fca65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:51 +0000

    Add package pipenv/7.0.6/pipenv-7.0.6-py3-none-any.whl

---
+++ packages/pipenv/pipenv-7.0.6-py3-none-any.whl/pipenv/__init__.py
@@ -0,0 +1,16 @@
+# |~~\'    |~~
+# |__/||~~\|--|/~\\  /
+# |   ||__/|__|   |\/
+#      |
+
+import os
+import sys
+
+# Inject vendored directory into system path.
+v_path = os.path.sep.join([os.path.dirname(os.path.realpath(__file__)), 'vendor'])
+sys.path.insert(1, v_path)
+
+from .cli import cli
+
+if __name__ == '__main__':
+    cli()
