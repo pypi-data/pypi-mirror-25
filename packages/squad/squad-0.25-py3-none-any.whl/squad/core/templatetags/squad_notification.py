6a7e816fc869be0eb5d63b98e7c5975ed82559b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:33 +0000

    Add package squad/0.25/squad-0.25-py3-none-any.whl

---
+++ packages/squad/squad-0.25-py3-none-any.whl/squad/core/templatetags/squad_notification.py
@@ -0,0 +1,37 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def tabulate_test_comparison(comparison, test_results=None):
+    if test_results is None:
+        test_results = comparison.results
+    if not test_results:
+        return '(none)'
+
+    text = []
+    header = []
+    indent = 0
+    header_sep = ''
+    row_format = ''
+    for env in comparison.all_environments:
+        for build in comparison.builds:
+            header.append((build, env))
+            text.append("%s+--- %s, %s" % ('|    ' * indent, build.version, env))
+            indent += 1
+            row_format += '%-4s '
+            header_sep += '|    '
+    row_format += '%s'  # for the test name
+
+    text.append(header_sep)
+
+    for test, results in test_results.items():
+        row = []
+        for build, env in header:
+            row.append(results.get((build, env), 'n/a'))
+        row.append(test)
+        text.append(row_format % tuple(row))
+
+    return "\n".join(text)
