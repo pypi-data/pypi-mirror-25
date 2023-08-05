aa2dc6b60f0b52acd5d53ad7df1292413c42a84a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:24 +0000

    Add package jabstract/0.1.1/jabstract-0.1.1.tar.gz

---
+++ packages/jabstract/jabstract-0.1.1.tar.gz/jabstract-0.1.1/jabstract/__init__.py
@@ -0,0 +1,16 @@
+import copy
+
+
+def apply_kwargs(kwargs, default_kwargs):
+    for k, v in kwargs.items():
+        if isinstance(v, dict):
+            default_kwargs[k] = apply_kwargs(v, default_kwargs[k])
+        else:
+            default_kwargs[k] = v
+    return default_kwargs
+
+
+def jabstract(payload):
+    def p(**kwargs):
+        return apply_kwargs(kwargs, copy.deepcopy(payload))
+    return p
