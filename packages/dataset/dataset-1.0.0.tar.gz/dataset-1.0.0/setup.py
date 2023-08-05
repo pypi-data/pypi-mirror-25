61d94a72b75bf93c561616e3735230c8b9e20688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:56 +0000

    Add package dataset/1.0.0/dataset-1.0.0.tar.gz

---
+++ packages/dataset/dataset-1.0.0.tar.gz/dataset-1.0.0/setup.py
@@ -0,0 +1,42 @@
+import sys
+from setuptools import setup, find_packages
+
+
+py26_dependency = []
+if sys.version_info[:2] <= (2, 6):
+    py26_dependency = ["argparse >= 1.1", "ordereddict >= 1.1"]
+
+setup(
+    name='dataset',
+    version='1.0.0',
+    description="Toolkit for Python-based data processing.",
+    long_description="",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords='sql sqlalchemy etl loading utility',
+    author='Friedrich Lindenberg, Gregor Aisch, Stefan Wehrmeyer',
+    author_email='friedrich@pudo.org',
+    url='http://github.com/pudo/dataset',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+        'sqlalchemy >= 1.1.0',
+        'alembic >= 0.6.2',
+        'normality >= 0.3.9',
+        "six >= 1.7.3"
+    ] + py26_dependency,
+    tests_require=[],
+    test_suite='test',
+    entry_points={}
+)
