de382a69ffb46941e01c994babc1c96962ac27d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:02 +0000

    Add package webfriend/0.2.9/webfriend-0.2.9-py2-none-any.whl

---
+++ packages/webfriend/webfriend-0.2.9-py2-none-any.whl/webfriend/exceptions.py
@@ -0,0 +1,30 @@
+class WebfriendError(Exception):
+    pass
+
+
+class ProtocolError(WebfriendError):
+    pass
+
+
+class TimeoutError(WebfriendError):
+    pass
+
+
+class NotFound(WebfriendError):
+    pass
+
+
+class HttpError(WebfriendError):
+    pass
+
+
+class EmptyResult(WebfriendError):
+    pass
+
+
+class TooManyResults(WebfriendError):
+    pass
+
+
+class NotImplemented(WebfriendError):
+    pass
