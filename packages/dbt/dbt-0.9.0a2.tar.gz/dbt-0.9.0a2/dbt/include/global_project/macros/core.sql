afd8b875976791d148de27ce47cdf32e62414392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:46 +0000

    Add package dbt/0.9.0a2/dbt-0.9.0a2.tar.gz

---
+++ packages/dbt/dbt-0.9.0a2.tar.gz/dbt-0.9.0a2/dbt/include/global_project/macros/core.sql
@@ -0,0 +1,16 @@
+{% macro statement(name=None, fetch_result=False, auto_begin=True) -%}
+  {%- if execute: -%}
+    {%- set sql = render(caller()) -%}
+
+    {%- if name == 'main' -%}
+      {{ log('Writing runtime SQL for node "{}"'.format(model['unique_id'])) }}
+      {{ write(sql) }}
+    {%- endif -%}
+
+    {%- set status, res = adapter.execute(sql, auto_begin=auto_begin, fetch=fetch_result) -%}
+    {%- if name is not none -%}
+      {{ store_result(name, status=status, data=res) }}
+    {%- endif -%}
+
+  {%- endif -%}
+{%- endmacro %}
---
+++ packages/dbt/dbt-0.9.0a2.tar.gz/dbt-0.9.0a2/dbt/include/global_project/macros/materializations/view.sql
@@ -0,0 +1,39 @@
+{%- materialization view, default -%}
+
+  {%- set identifier = model['name'] -%}
+  {%- set tmp_identifier = identifier + '__dbt_tmp' -%}
+  {%- set non_destructive_mode = (flags.NON_DESTRUCTIVE == True) -%}
+  {%- set existing = adapter.query_for_existing(schema) -%}
+  {%- set existing_type = existing.get(identifier) -%}
+
+  {{ run_hooks(pre_hooks, inside_transaction=False) }}
+  {{ drop_if_exists(existing, schema, tmp_identifier) }}
+
+  -- `BEGIN` happens here:
+  {{ run_hooks(pre_hooks, inside_transaction=True) }}
+
+  -- build model
+  {% if non_destructive_mode and existing_type == 'view' -%}
+    -- noop
+  {%- else -%}
+    {% call statement('main') -%}
+      {{ create_view_as(tmp_identifier, sql) }}
+    {%- endcall %}
+  {%- endif %}
+
+  {{ run_hooks(post_hooks, inside_transaction=True) }}
+
+  -- cleanup
+  {% if non_destructive_mode and existing_type == 'view' -%}
+    -- noop
+  {%- else -%}
+    {{ drop_if_exists(existing, schema, identifier) }}
+    {{ adapter.rename(schema, tmp_identifier, identifier) }}
+  {%- endif %}
+
+  -- `COMMIT` happens here
+  {{ adapter.commit() }}
+
+  {{ run_hooks(post_hooks, inside_transaction=False) }}
+
+{%- endmaterialization -%}
