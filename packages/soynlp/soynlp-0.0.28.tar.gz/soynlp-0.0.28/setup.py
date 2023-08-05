b2844cea86b35a28ed9ad14d63f9047399e1e76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:31 +0000

    Add package soynlp/0.0.28/soynlp-0.0.28.tar.gz

---
+++ packages/soynlp/soynlp-0.0.28.tar.gz/soynlp-0.0.28/setup.py
@@ -0,0 +1,39 @@
+from description import __version__, __author__
+from setuptools import setup, find_packages
+
+setup(
+   name="soynlp",
+   version=__version__,
+   author=__author__,
+   author_email='soy.lovit@gmail.com',
+   url='https://github.com/lovit/soynlp',
+   description="Unsupervised Korean Natural Language Processing Toolkits",
+   long_description="""It contains unsupervised word extraction, tokenizers and noun extractors. 
+   These algorithms are not depending training corpus but extract patterns from data by theirselves.
+
+   Current version has follows
+   - Word extraction
+     - Cohesion score
+     - Branching Entropy
+     - Accessor Variety
+   - Tokenizers
+     - RegexTokenizer
+     - LTokenizer
+     - MaxScoreTokenizer
+   - Noun extractor
+     - LRNounExtractor
+     
+
+   Following packages are helpful
+   - krwordrank: Unsupervised Korean word/keyword extractor
+     - https://github.com/lovit/KR-WordRank
+     - pip install krwordrank
+   - soyspacing: Korean spacing error corrector
+     - https://github.com/lovit/soyspacing
+     - pip install soyspacing
+   """,
+   install_requires=["numpy>=1.12.1", "psutil>=5.0.1"],
+   keywords = ['korean natural language processing'],
+   packages=find_packages(),
+   package_data={'soynlp':['trained_models/*', 'pos/dictionary/*.txt', 'pos/dictionary/*/*.txt']}
+)
