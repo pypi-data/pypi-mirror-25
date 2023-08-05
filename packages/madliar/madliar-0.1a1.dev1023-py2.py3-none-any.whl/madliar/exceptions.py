c8fec3658796815cf9fe2a4818a99761efeca007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:07 +0000

    Add package madliar/0.1a1.dev1023/madliar-0.1a1.dev1023-py2.py3-none-any.whl

---
+++ packages/madliar/madliar-0.1a1.dev1023-py2.py3-none-any.whl/madliar/exceptions.py
@@ -0,0 +1,11 @@
+"""
+Global Django exception and warning classes.
+
+"""
+
+
+class HTTPResponseWriteError(Exception):
+    """
+    The exception used for syntax errors during parsing or rendering.
+    """
+    pass
