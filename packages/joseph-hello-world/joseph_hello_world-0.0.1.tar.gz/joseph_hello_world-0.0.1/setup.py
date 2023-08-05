886a7f56d395ddb84740d2a7d87308bd0bfb48f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:42 +0000

    Add package joseph-hello-world/0.0.1/joseph_hello_world-0.0.1.tar.gz

---
+++ packages/joseph-hello-world/joseph_hello_world-0.0.1.tar.gz/joseph_hello_world-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+__VERSION__ = "0.0.1"
+
+setup(
+    version=__VERSION__,
+    name="joseph_hello_world",
+    description="Minimal Hello world plugin for Joseph",
+    author="Niek Keijzer",
+    author_email="info@niekkeijzer.com",
+    url="https://github.com/NiekKeijzer/HelloWorld",
+    download_url="https://github.com/NiekKeijzer/HelloWorld/archive/"
+                 "{}.tar.gaz".format(__VERSION__),
+    entry_points={
+        'joseph.plugins': [
+            'hello_world = src.hello'
+        ]
+    }
+)
