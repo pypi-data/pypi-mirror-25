3a70822a8192182dc80c34a1a964bce4e8383c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:13 +0000

    Add package mahjong/1.0.1/mahjong-1.0.1.tar.gz

---
+++ packages/mahjong/mahjong-1.0.1.tar.gz/mahjong-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='mahjong',
+    packages=['mahjong'],
+    version='1.0.1',
+    description='Mahjong hands calculation',
+    long_description="""A python module for calculate mahjong hands. You can find usage examples here https://github.com/MahjongRepository/mahjong""",
+    author='Alexey Lisikhin',
+    author_email='lisikhin@gmail.com',
+    url='https://github.com/MahjongRepository/mahjong',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
