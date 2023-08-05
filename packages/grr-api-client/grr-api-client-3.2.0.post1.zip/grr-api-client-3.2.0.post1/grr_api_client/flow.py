cde6c295032728a4aafe2c0ee78f768a919db4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:55 +0000

    Add package grr-api-client/3.2.0.post1/grr-api-client-3.2.0.post1.zip

---
+++ packages/grr-api-client/grr-api-client-3.2.0.post1.zip/grr-api-client-3.2.0.post1/grr_api_client/flow.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+"""Flows-related part of GRR API client library."""
+
+from grr_api_client import utils
+from grr.proto.api import flow_pb2
+
+
+class FlowResult(object):
+  """Wrapper class for flow results."""
+
+  def __init__(self, data=None):
+    super(FlowResult, self).__init__()
+    self.data = data
+
+    self.timestamp = data.timestamp
+
+  @property
+  def payload(self):
+    return utils.UnpackAny(self.data.payload)
+
+
+class FlowBase(object):
+  """Base class for FlowRef and Flow."""
+
+  def __init__(self, client_id=None, flow_id=None, context=None):
+    super(FlowBase, self).__init__()
+
+    if not client_id:
+      raise ValueError("client_id can't be empty.")
+
+    if not flow_id:
+      raise ValueError("flow_id can't be empty.")
+
+    if not context:
+      raise ValueError("context can't be empty")
+
+    self.client_id = client_id
+    self.flow_id = flow_id
+    self._context = context
+
+  def Cancel(self):
+    args = flow_pb2.ApiCancelFlowArgs(
+        client_id=self.client_id, flow_id=self.flow_id)
+    self._context.SendRequest("CancelFlow", args)
+
+  def ListResults(self):
+    args = flow_pb2.ApiListFlowResultsArgs(
+        client_id=self.client_id, flow_id=self.flow_id)
+    items = self._context.SendIteratorRequest("ListFlowResults", args)
+    return utils.MapItemsIterator(lambda data: FlowResult(data=data), items)
+
+  def GetFilesArchive(self):
+    args = flow_pb2.ApiGetFlowFilesArchiveArgs(
+        client_id=self.client_id, flow_id=self.flow_id)
+    return self._context.SendStreamingRequest("GetFlowFilesArchive", args)
+
+
+class FlowRef(FlowBase):
+  """Ref to a flow."""
+
+  def Get(self):
+    """Fetch flow's data and return proper Flow object."""
+
+    args = flow_pb2.ApiGetFlowArgs(
+        client_id=self.client_id, flow_id=self.flow_id)
+    data = self._context.SendRequest("GetFlow", args)
+    return Flow(data=data, context=self._context)
+
+
+class Flow(FlowBase):
+  """Flow object with fetched data."""
+
+  def __init__(self, data=None, context=None):
+    if data is None:
+      raise ValueError("data can't be None")
+
+    client_id = utils.UrnStringToClientId(data.urn)
+    flow_id = data.flow_id
+
+    super(Flow, self).__init__(
+        client_id=client_id, flow_id=flow_id, context=context)
+
+    self.data = data
