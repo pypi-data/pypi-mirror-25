c640abbe00e1789c795e36aaa6a3897c59479162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:06 +0000

    Add package MyAirthLib/0.1.2/MyAirthLib-0.1.2.zip

---
+++ packages/MyAirthLib/MyAirthLib-0.1.2.zip/MyAirthLib-0.1.2/README.txt
@@ -0,0 +1,6 @@
+As we have finalized creation of 
+our application followed by 
+making necessary amendments to the 
+file structure to prepare it for a flawless 
+distribution build, we can begin 
+with going through the packaging operations.
---
+++ packages/MyAirthLib/MyAirthLib-0.1.2.zip/MyAirthLib-0.1.2/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="MyAirthLib",
+
+    # Version number (initial):
+    version="0.1.2",
+
+    # Application author details:
+    author="Shrawan Shinde",
+    author_email="syntaxerror1972@gmail.com",
+
+    # Packages
+    packages=["appairth"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="https://upload.pypi.org/legacy/MyAirthLib_v010/",
+
+    #
+    license='MIT',
+    description="Useful arithmetic functions.",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "flask",
+    ],
+)
