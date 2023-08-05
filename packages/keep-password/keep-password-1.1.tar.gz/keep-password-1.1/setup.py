779664b60c221a3e845b26edb4f15b9909c91bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:28 +0000

    Add package keep-password/1.1/keep-password-1.1.tar.gz

---
+++ packages/keep-password/keep-password-1.1.tar.gz/keep-password-1.1/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+requirements = [pkg.split('=')[0] for pkg in open('requirements.txt').readlines()]
+
+
+classifiers = ['Environment :: Console',
+               'Programming Language :: Python :: 3'
+               ]
+
+
+setup(
+    name='keep-password',
+
+    version='1.1',  # Ideally should be same as your GitHub release tag varsion
+    description='Save password for username',
+    author='chandra khadka',
+    author_email='chandra2khadka4@gmail.com',
+    url='https://github.com/chandrabrt/keep',
+    scripts=['src/keep-password'],
+    install_requires=requirements,
+    package=['keep_password'],
+    package_dir={'keep_password': 'src/keep_password'},
+    classifiers=classifiers
+)
+
