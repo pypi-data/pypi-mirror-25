5a015f5311f986bf5e35f027e8971b6c3f315909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:59 +0000

    Add package paramspace/0.9/ParamSpace-0.9-py3.6.egg

---
+++ packages/paramspace/ParamSpace-0.9-py3.6.egg/pspace/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .paramspace import ParamSpace, ParamSpan, CoupledParamSpan
+
+# to not pollute the namespace
+del(paramspace) # FIXME need more elegant solution!
