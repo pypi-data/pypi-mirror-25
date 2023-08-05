814ebc5a14e62ffc60356404aa695754d1d7bbe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:55 +0000

    Add package weblablib/0.4/weblablib-0.4.tar.gz

---
+++ packages/weblablib/weblablib-0.4.tar.gz/weblablib-0.4/setup.py
@@ -0,0 +1,36 @@
+#-*-*- encoding: utf-8 -*-*-
+from setuptools import setup
+
+classifiers=[
+    "Development Status :: 4 - Beta",
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU Affero General Public License v3",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Education",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Software Development :: Libraries :: Application Frameworks",
+]
+
+cp_license="GNU AGPL v3"
+
+setup(name='weblablib',
+      version='0.4',
+      description="WebLab-Deusto library for creating unmanaged laboratories",
+      classifiers=classifiers,
+      zip_safe=False,
+      author='LabsLand',
+      author_email='dev@labsland.com',
+      url='https://docs.labsland.com/weblablib/',
+      license=cp_license,
+      py_modules=['weblablib'],
+      install_requires=['redis', 'flask>=0.12', 'six', 'requests'],
+     )
