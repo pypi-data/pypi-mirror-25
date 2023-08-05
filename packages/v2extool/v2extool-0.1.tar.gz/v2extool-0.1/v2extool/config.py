a97568f18bbbb3c6515819503487834fdd19007b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:46 +0000

    Add package v2extool/0.1/v2extool-0.1.tar.gz

---
+++ packages/v2extool/v2extool-0.1.tar.gz/v2extool-0.1/v2extool/config.py
@@ -0,0 +1,21 @@
+"""
+this is v2ex config
+including headers, urls
+"""
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    "Referer": "http://www.v2ex.com/signin",
+    "Origin": "http://www.v2ex.com",
+    "Host": "www.v2ex.com"
+}
+
+main_url = "https://www.v2ex.com"
+login_url = "https://www.v2ex.com/signin"
+check_in_url = "https://www.v2ex.com/mission/daily"
+user_info_url = "https://www.v2ex.com/api/members/show.json"
+balance_url = "https://www.v2ex.com/balance"
+
+node_name = {
+
+}
