2b04bb416fd1062ab890f5bfb949e8b96a9f6429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:49 +0000

    Add package colorednoise/1.0rc1/colorednoise-1.0rc1.tar.gz

---
+++ packages/colorednoise/colorednoise-1.0rc1.tar.gz/colorednoise-1.0rc1/CHANGELOG.rst
@@ -0,0 +1,14 @@
+Changelog
+=========
+
+
+:Version: 1.0-rc.2 of 2017/09/23
+
+Refactoring: moved powerlaw_psd_gaussian into the present pure python module to 
+release it to the public in pip-installable form.
+
+
+:Version: [unreleased] of 2014-04-07
+
+powerlaw_psd_gaussian was part of a personal collection of random number
+generation algorithms for research written in python and c.
---
+++ packages/colorednoise/colorednoise-1.0rc1.tar.gz/colorednoise-1.0rc1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='colorednoise',
+    version='1.0-rc.1',
+    description='Generate Gaussian (1/f)**beta noise (e.g. pink noise)',
+    long_description="""
+        Generate Gaussian distributed noise with a power law spectrum.
+        Based on the algorithm in 
+            Timmer, J. and Koenig, M.:
+            On generating power law noise. 
+            Astron. Astrophys. 300, 707-710 (1995)
+    """,
+    classifiers=[
+      'Development Status :: 5 - Production/Stable',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 2.7',
+      'Topic :: Scientific/Engineering'
+    ],
+    keywords='1/f flicker power-law correlated colored noise generator',
+    url='http://github.com/felixpatzelt/colorednoise',
+    download_url=(
+      'https://github.com/felixpatzelt/colorednoise/archive/1.0.0-rc.1.tar.gz'
+    ),
+    author='Felix Patzelt',
+    author_email='felix@neuro.uni-bremen.de',
+    license='MIT',
+    py_modules=['colorednoise'],
+    install_requires=[
+        'numpy',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='tests',
+)
