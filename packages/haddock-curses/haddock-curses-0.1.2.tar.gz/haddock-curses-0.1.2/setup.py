a07bdd876a9af9121a6215aad7374e4fda2f1593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:40 +0000

    Add package haddock-curses/0.1.2/haddock-curses-0.1.2.tar.gz

---
+++ packages/haddock-curses/haddock-curses-0.1.2.tar.gz/haddock-curses-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='haddock-curses',
+	  version='0.1.2',
+	  description='Random curses from Captain Haddock.',
+	  long_description=readme(),
+	  classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+      ],
+	  url='https://github.com/asmaier/haddock',
+	  author='asmaier',
+	  author_email='asmaier@web.de',
+	  license='MIT',
+	  packages=find_packages(),
+	  include_package_data=True,
+	  zip_safe=False)
