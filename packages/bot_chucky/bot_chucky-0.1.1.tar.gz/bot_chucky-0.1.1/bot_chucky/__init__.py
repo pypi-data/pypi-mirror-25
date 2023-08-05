5f6ac748d6de8dceac4a0f9c14d6622e65afe0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:15 +0000

    Add package bot_chucky/0.1.1/bot_chucky-0.1.1.tar.gz

---
+++ packages/bot_chucky/bot_chucky-0.1.1.tar.gz/bot_chucky-0.1.1/bot_chucky/__init__.py
@@ -0,0 +1,6 @@
+"""
+:copyright: Mike Yusko a.k.a Frozen Monkey
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.1.1'
---
+++ packages/bot_chucky/bot_chucky-0.1.1.tar.gz/bot_chucky-0.1.1/setup.py
@@ -0,0 +1,35 @@
+import bot_chucky
+from setuptools import setup
+
+version = bot_chucky.__version__
+
+setup_kwargs = {
+    'name': 'bot_chucky',
+    'version': version,
+    'url': 'https://github.com/MichaelYusko/Bot-Chucky',
+    'license': 'MIT',
+    'author': 'Frozen Monkey',
+    'author_email': 'freshjelly12@yahoo.com',
+    'description': 'Python bot which able to work with messenger of facebook',
+    'packages': ['bot_chucky'],
+    'classifiers': [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License'
+    ],
+ }
+
+requirements = [
+    'requests==2.17.3',
+    'facebook-sdk==2.0.0',
+    'soundcloud==0.5.0',
+    'python-twitter==3.3',
+]
+
+setup_kwargs['install_requires'] = requirements
+
+setup(**setup_kwargs)
+
+print(u"\n\n\t\t    "
+      "BotChucky version {} installation succeeded.\n".format(version))
