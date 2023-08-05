d0d1ee4dc601a3fe65b278d1f42b52b3f535ad2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:11 +0000

    Add package list-dict-DB/20170911.3/list_dict_DB-20170911.3.tar.gz

---
+++ packages/list-dict-DB/list_dict_DB-20170911.3.tar.gz/list_dict_DB-20170911.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals, absolute_import
+
+from setuptools import setup
+
+setup(
+    name='list_dict_DB',
+    py_modules=['list_dict_DB'],
+    long_description=open('README.rst').read(),
+    version='20170911.3',
+    description='in memory database like object with O(1) queries',
+    url='https://github.com/Jwink3101/list_dict_db',
+    author='Justin Winokur',
+    author_email='Jwink3101@gmail.com',
+)
