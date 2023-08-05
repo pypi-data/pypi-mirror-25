13e59830fd2352d72576876d5f28e3e4c58660d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:43 +0000

    Add package gino/0.5.2/gino-0.5.2-py2.py3-none-any.whl

---
+++ packages/gino/gino-0.5.2-py2.py3-none-any.whl/gino/__init__.py
@@ -0,0 +1,7 @@
+from .api import Gino
+from .pool import GinoPool
+from .connection import GinoConnection
+from .local import get_local, enable_task_local, disable_task_local
+from .exceptions import *
+
+__version__ = '0.5.2'
