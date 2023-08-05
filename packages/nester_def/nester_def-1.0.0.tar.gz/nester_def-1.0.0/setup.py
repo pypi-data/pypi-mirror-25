1aa357d941dee817c11edeb70e60b1ae378813f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:38 +0000

    Add package nester_def/1.0.0/nester_def-1.0.0.tar.gz

---
+++ packages/nester_def/nester_def-1.0.0.tar.gz/nester_def-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "nester_def",
+    version = "1.0.0",
+    py_modules = ["nester_def"],
+    author = "WyhEric",
+    author_email = "wuyuanhengeric@gmail.com",
+    url = "http://www.headfirstlabs.com",
+    description = "A simple printer of function_def list",
+)
