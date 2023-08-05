3bab12f0c42e63a4799e056363a75cc27f947479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:35 +0000

    Add package amaasinfra/0.2.9/amaasinfra-0.2.9.tar.gz

---
+++ packages/amaasinfra/amaasinfra-0.2.9.tar.gz/amaasinfra-0.2.9/amaasinfra/infra/dynamodb/asset.py
@@ -0,0 +1,13 @@
+from amaasinfra.infra.dynamodb.amaas_model import AMaaSModel
+from pynamodb.attributes import UnicodeAttribute, BooleanAttribute
+
+class Asset(AMaaSModel):
+    asset_id = UnicodeAttribute(range_key=True)
+    asset_class = UnicodeAttribute(null=False)
+    asset_type = UnicodeAttribute(null=False)
+    asset_status = UnicodeAttribute(null=False)
+    fungible = BooleanAttribute()
+    hashcode = UnicodeAttribute(null=False)
+    updated_time = UnicodeAttribute(null=False)
+
+    
