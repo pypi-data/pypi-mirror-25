74a8a6bbafb9779c941ecd3fd7b07e0264bff511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:08 +0000

    Add package issue-pub/0.1.0/issue_pub-0.1.0-py2.py3-none-any.whl

---
+++ packages/issue-pub/issue_pub-0.1.0-py2.py3-none-any.whl/issue_pub/cli.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for issue_pub."""
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for issue_pub."""
+    click.echo("Replace this message by putting your code into "
+               "issue_pub.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+
+
+if __name__ == "__main__":
+    main()
