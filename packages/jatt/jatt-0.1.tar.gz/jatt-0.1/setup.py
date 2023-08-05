3b096867ce825dd8b52987672ed4adb45d5703f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:10 +0000

    Add package jatt/0.1/jatt-0.1.tar.gz

---
+++ packages/jatt/jatt-0.1.tar.gz/jatt-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jatt',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/Lokesh523s/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['jatt'],
+      zip_safe=False)
+
