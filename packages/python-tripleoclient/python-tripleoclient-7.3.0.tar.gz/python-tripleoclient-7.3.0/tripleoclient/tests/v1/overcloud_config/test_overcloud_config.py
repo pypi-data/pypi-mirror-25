b98f8d0ee3d34360b105fd17d90897eaf34bada1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:42 +0000

    Add package python-tripleoclient/7.3.0/python-tripleoclient-7.3.0.tar.gz

---
+++ packages/python-tripleoclient/python-tripleoclient-7.3.0.tar.gz/python-tripleoclient-7.3.0/tripleoclient/tests/v1/overcloud_config/test_overcloud_config.py
@@ -0,0 +1,171 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+import mock
+
+from mock import call
+from mock import patch
+from osc_lib.tests import utils
+
+from tripleoclient.tests.v1.overcloud_config import fakes
+from tripleoclient.v1 import overcloud_config
+
+
+class TestOvercloudConfig(utils.TestCommand):
+
+    def setUp(self):
+        super(TestOvercloudConfig, self).setUp()
+
+        self.cmd = overcloud_config.DownloadConfig(self.app, None)
+        self.app.client_manager.workflow_engine = mock.Mock()
+        self.app.client_manager.orchestration = mock.Mock()
+        self.workflow = self.app.client_manager.workflow_engine
+
+    @patch.object(overcloud_config.DownloadConfig, '_mkdir')
+    @patch.object(overcloud_config.DownloadConfig, '_open_file')
+    @mock.patch('tempfile.mkdtemp', autospec=True)
+    def test_overcloud_config_generate_config(self,
+                                              mock_tmpdir,
+                                              mock_open,
+                                              mock_mkdir):
+        arglist = ['--name', 'overcloud', '--config-dir', '/tmp']
+        verifylist = [
+            ('name', 'overcloud'),
+            ('config_dir', '/tmp')
+        ]
+        config_type_list = ['config_settings', 'global_config_settings',
+                            'logging_sources', 'monitoring_subscriptions',
+                            'service_config_settings',
+                            'service_metadata_settings',
+                            'service_names',
+                            'upgrade_batch_tasks', 'upgrade_tasks']
+        fake_role = [role for role in
+                     fakes.FAKE_STACK['outputs'][1]['output_value']]
+
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+        clients = self.app.client_manager
+        orchestration_client = clients.orchestration
+        orchestration_client.stacks.get.return_value = fakes.create_tht_stack()
+        mock_tmpdir.return_value = "/tmp/tht"
+        self.cmd.take_action(parsed_args)
+        expected_mkdir_calls = [call('/tmp/tht/%s' % r) for r in fake_role]
+        mock_mkdir.assert_has_calls(expected_mkdir_calls, any_order=True)
+        expected_calls = []
+        for config in config_type_list:
+            for role in fake_role:
+                if config == 'step_config':
+                    expected_calls += [call('/tmp/tht/%s/%s.pp' %
+                                            (role, config))]
+                else:
+                    expected_calls += [call('/tmp/tht/%s/%s.yaml' %
+                                            (role, config))]
+        mock_open.assert_has_calls(expected_calls, any_order=True)
+
+    @patch.object(overcloud_config.DownloadConfig, '_mkdir')
+    @patch.object(overcloud_config.DownloadConfig, '_open_file')
+    @mock.patch('tempfile.mkdtemp', autospec=True)
+    def test_overcloud_config_one_config_type(self,
+                                              mock_tmpdir,
+                                              mock_open,
+                                              mock_mkdir):
+
+        arglist = ['--name', 'overcloud', '--config-dir', '/tmp',
+                   '--config-type', ['config_settings']]
+        verifylist = [
+            ('name', 'overcloud'),
+            ('config_dir', '/tmp'),
+            ('config_type', ['config_settings'])
+        ]
+        expected_config_type = 'config_settings'
+        fake_role = [role for role in
+                     fakes.FAKE_STACK['outputs'][1]['output_value']]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        clients = self.app.client_manager
+        orchestration_client = clients.orchestration
+        orchestration_client.stacks.get.return_value = fakes.create_tht_stack()
+        mock_tmpdir.return_value = "/tmp/tht"
+        self.cmd.take_action(parsed_args)
+        expected_mkdir_calls = [call('/tmp/tht/%s' % r) for r in fake_role]
+        expected_calls = [call('/tmp/tht/%s/%s.yaml'
+                          % (r, expected_config_type))
+                          for r in fake_role]
+        mock_mkdir.assert_has_calls(expected_mkdir_calls, any_order=True)
+        mock_open.assert_has_calls(expected_calls, any_order=True)
+
+    @mock.patch('os.mkdir')
+    @mock.patch('six.moves.builtins.open')
+    @mock.patch('tempfile.mkdtemp', autospec=True)
+    def test_overcloud_config_wrong_config_type(self, mock_tmpdir,
+                                                mock_open, mock_mkdir):
+
+        arglist = [
+            '--name', 'overcloud',
+            '--config-dir',
+            '/tmp', '--config-type', ['bad_config']]
+        verifylist = [
+            ('name', 'overcloud'),
+            ('config_dir', '/tmp'),
+            ('config_type', ['bad_config'])
+        ]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+        clients = self.app.client_manager
+        mock_tmpdir.return_value = "/tmp/tht"
+        orchestration_client = clients.orchestration
+        orchestration_client.stacks.get.return_value = fakes.create_tht_stack()
+        self.assertRaises(
+            KeyError,
+            self.cmd.take_action, parsed_args)
+
+    @mock.patch('tripleoclient.utils.get_role_data', autospec=True)
+    @mock.patch('os.mkdir')
+    @mock.patch('six.moves.builtins.open')
+    @mock.patch('tempfile.mkdtemp', autospec=True)
+    def test_overcloud_config_upgrade_tasks(self, mock_tmpdir,
+                                            mock_open,
+                                            mock_mkdir,
+                                            mock_get_role_data):
+
+        clients = self.app.client_manager
+        orchestration_client = clients.orchestration
+        orchestration_client.stacks.get.return_value = fakes.create_tht_stack()
+        mock_tmpdir.return_value = "/tmp/tht"
+        fake_role = [role for role in
+                     fakes.FAKE_STACK['outputs'][1]['output_value']]
+        fake_tasks = {'FakeController': [{'name': 'Stop fake service',
+                                          'service': 'name=fake '
+                                          'state=stopped',
+                                          'tags': 'step1',
+                                          'when': 'step|int == 1'}],
+                      'FakeCompute': [{'name': 'Stop fake service',
+                                       'service': 'name=fake state=stopped',
+                                       'tags': 'step1',
+                                       'when': 'step|int == 1'},
+                                      {'name': 'Stop nova-'
+                                       'compute service',
+                                       'service':
+                                       'name=openstack-nova-'
+                                       'compute state=stopped',
+                                       'tags': 'step1',
+                                       'when': 'step|int == 1'}]
+                      }
+        mock_get_role_data.return_value = fake_role
+
+        for role in fake_role:
+            filepath = "/tmp/tht/%s/upgrade_tasks_playbook" % role
+            with mock.patch('os.fdopen'), \
+                    mock.patch('os.mkdir'), mock.patch('os.open') as open:
+                playbook_tasks = self.cmd._write_playbook_get_tasks(
+                    fakes.FAKE_STACK['outputs'][1]['output_value'][role]
+                    ['upgrade_tasks'], role, filepath)
+                self.assertEqual(fake_tasks[role], playbook_tasks)
+                open.assert_called()
---
+++ packages/python-tripleoclient/python-tripleoclient-7.3.0.tar.gz/python-tripleoclient-7.3.0/tripleoclient/v1/overcloud_config.py
@@ -0,0 +1,147 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+import logging
+import os
+import re
+import six
+import tempfile
+import yaml
+
+from osc_lib.command import command
+from osc_lib.i18n import _
+
+from tripleoclient import utils
+
+
+class DownloadConfig(command.Command):
+    """Download Overcloud Config"""
+
+    log = logging.getLogger(__name__ + ".DownloadConfig")
+
+    def get_parser(self, prog_name):
+        parser = super(DownloadConfig, self).get_parser(prog_name)
+        parser.add_argument(
+            '--name',
+            dest='name',
+            default='overcloud',
+            help=_('The name of the plan, which is used for the object '
+                   'storage container, workflow environment and orchestration '
+                   'stack names.'),
+        )
+        parser.add_argument(
+            '--config-dir',
+            dest='config_dir',
+            default=os.path.expanduser("~"),
+            help=_('The directory where the configuration files will be '
+                   'pushed'),
+        )
+        parser.add_argument(
+            '--config-type',
+            dest='config_type',
+            type=list,
+            help=_('Type of object config to be extract from the deployment, '
+                   'defaults to all keys available'),
+        )
+        return parser
+
+    @staticmethod
+    def _open_file(path):
+        return os.fdopen(os.open(path,
+                                 os.O_WRONLY | os.O_CREAT, 0o600),
+                         'w')
+
+    def _step_tags_to_when(self, sorted_tasks):
+        for task in sorted_tasks:
+            tag = task.get('tags', '')
+            match = re.search('step([0-9]+)', tag)
+            if match:
+                step = match.group(1)
+                whenline = task.get('when', None)
+                if whenline:  # how about list of when conditionals
+                    when_exists = re.search('step.* == [0-9]', whenline)
+                    if when_exists:  # skip if there is an existing 'step == N'
+                        continue
+                    task['when'] = "(%s) and (step|int == %s)" % (whenline,
+                                                                  step)
+                else:
+                    task.update({"when": "step|int == %s" % step})
+
+    def _write_playbook_get_tasks(self, tasks, role, filepath):
+        playbook = []
+        sorted_tasks = sorted(tasks, key=lambda x: x.get('tags', None))
+        self._step_tags_to_when(sorted_tasks)
+        playbook.append({'name': '%s playbook' % role,
+                         'hosts': role,
+                         'tasks': sorted_tasks})
+        with self._open_file(filepath) as conf_file:
+            yaml.safe_dump(playbook, conf_file, default_flow_style=False)
+        return sorted_tasks
+
+    def _mkdir(self, dirname):
+        if not os.path.exists(dirname):
+            try:
+                os.mkdir(dirname, 0o700)
+            except OSError as e:
+                message = 'Failed to create: %s, error: %s' % (dirname,
+                                                               str(e))
+                raise OSError(message)
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)" % parsed_args)
+        clients = self.app.client_manager
+
+        name = parsed_args.name
+        config_dir = parsed_args.config_dir
+        self._mkdir(config_dir)
+        stack = utils.get_stack(clients.orchestration, name)
+        tmp_path = tempfile.mkdtemp(prefix='tripleo-',
+                                    suffix='-config',
+                                    dir=config_dir)
+        self.log.info("Generating configuration under the directory: "
+                      "%s" % tmp_path)
+        role_data = utils.get_role_data(stack)
+        for role_name, role in six.iteritems(role_data):
+            role_path = os.path.join(tmp_path, role_name)
+            self._mkdir(role_path)
+            for config in parsed_args.config_type or role.keys():
+                if config == 'step_config':
+                    filepath = os.path.join(role_path, 'step_config.pp')
+                    with self._open_file(filepath) as step_config:
+                        step_config.write('\n'.join(step for step in
+                                                    role[config]
+                                                    if step is not None))
+                else:
+                    if 'upgrade_tasks' in config:
+                        filepath = os.path.join(role_path, '%s_playbook.yaml' %
+                                                config)
+                        data = self._write_playbook_get_tasks(
+                            role[config], role_name, filepath)
+                    else:
+                        try:
+                            data = role[config]
+                        except KeyError as e:
+                            message = 'Invalid key: %s, error: %s' % (config,
+                                                                      str(e))
+                            raise KeyError(message)
+                    filepath = os.path.join(role_path, '%s.yaml' % config)
+                    with self._open_file(filepath) as conf_file:
+                        yaml.safe_dump(data,
+                                       conf_file,
+                                       default_flow_style=False)
+        role_config = utils.get_role_config(stack)
+        for config_name, config in six.iteritems(role_config):
+            conf_path = os.path.join(tmp_path, config_name + ".yaml")
+            with self._open_file(conf_path) as conf_file:
+                conf_file.write(config)
+        print("The TripleO configuration has been successfully generated "
+              "into: {0}".format(tmp_path))
