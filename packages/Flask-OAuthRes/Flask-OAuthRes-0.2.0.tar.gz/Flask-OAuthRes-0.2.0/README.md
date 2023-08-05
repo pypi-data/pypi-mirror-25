540d3756e8b3cfed937f7252f0f8325414bd7f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:05 +0000

    Add package Flask-OAuthRes/0.2.0/Flask-OAuthRes-0.2.0.tar.gz

---
+++ packages/Flask-OAuthRes/Flask-OAuthRes-0.2.0.tar.gz/Flask-OAuthRes-0.2.0/README.md
@@ -0,0 +1,6 @@
+[![Build Status](https://travis-ci.org/socialwifi/flask-oauthres.svg?branch=master)](https://travis-ci.org/socialwifi/flask-oauthres)
+# Flask OAuth2 Token Validation
+
+This project exposes methods for a protected resource to query an OAuth 2.0 authorization server to determine the active state of an OAuth 2.0 token and to determine additional information about this token.
+
+ref: https://tools.ietf.org/html/rfc7662
---
+++ packages/Flask-OAuthRes/Flask-OAuthRes-0.2.0.tar.gz/Flask-OAuthRes-0.2.0/setup.py
@@ -0,0 +1,34 @@
+from pip.req import parse_requirements
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='Flask-OAuthRes',
+    version='0.2.0',
+    description="OAuth Resource for Flask",
+    author='Lukasz Marcin Podkalicki',
+    author_email='lukasz.podkalicki@socialwifi.com',
+    url='https://github.com/socialwifi/flask-oauthres',
+    packages=find_packages(exclude=['tests', 'example']),
+    install_requires=[str(ir.req) for ir in parse_requirements('base_requirements.txt', session=False)],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
