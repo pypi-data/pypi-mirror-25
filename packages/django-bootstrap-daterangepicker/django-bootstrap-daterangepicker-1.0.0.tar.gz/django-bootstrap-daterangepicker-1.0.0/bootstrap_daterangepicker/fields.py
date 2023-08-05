1bf6c4d106c2d676f6202f31ae25632ba1f972d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:19 +0000

    Add package django-bootstrap-daterangepicker/1.0.0/django-bootstrap-daterangepicker-1.0.0.tar.gz

---
+++ packages/django-bootstrap-daterangepicker/django-bootstrap-daterangepicker-1.0.0.tar.gz/django-bootstrap-daterangepicker-1.0.0/bootstrap_daterangepicker/fields.py
@@ -0,0 +1,72 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils import six
+from django.utils.encoding import force_text
+from django.utils.translation import string_concat, gettext_lazy as _
+
+from .widgets import DateRangeWidget, DateTimeRangeWidget, DatePickerWidget
+
+
+def to_python(self, value):
+    # Try to coerce the value to unicode.
+    unicode_value = force_text(value, strings_only=True)
+    if isinstance(unicode_value, six.text_type):
+        value = unicode_value.strip()
+    else:
+        raise ValidationError(
+            _("Date range value: " + str(value) + " was not able to be converted to unicode.")
+        )
+
+    if self.widget.clearable:
+        if value.strip() == '':
+            return None, None
+
+    if self.widget.separator in value:
+        str_dates = value.split(self.widget.separator, 2)
+
+        try:
+            beginning = super(type(self), self).to_python(str_dates[0])
+        except ValidationError as e:
+            raise ValidationError(string_concat('Error in period beginning: ', e.message), e.code)
+
+        try:
+            end = super(type(self), self).to_python(str_dates[1])
+        except ValidationError as e:
+            raise ValidationError(string_concat('Error in period end: ', e.message), e.code)
+
+        return beginning, end
+    else:
+        raise ValidationError(
+            _("Invalid date range format."),
+            code='invalid'
+        )
+
+
+class DateRangeField(forms.DateField):
+    widget = DateRangeWidget
+
+    def __init__(self, clearable=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.widget.clearable = clearable
+
+    def to_python(self, value):
+        return to_python(self, value)
+
+
+class DateTimeRangeField(forms.DateTimeField):
+    widget = DateTimeRangeWidget
+
+    def __init__(self, clearable=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.widget.clearable = clearable
+
+    def to_python(self, value):
+        return to_python(self, value)
+
+
+class DateField(forms.DateField):
+    widget = DatePickerWidget
+
+    def __init__(self, clearable=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.widget.clearable = clearable
