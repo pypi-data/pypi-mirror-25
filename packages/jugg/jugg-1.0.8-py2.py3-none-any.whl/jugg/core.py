982a1bd7b1bb9d78f49c4fe9f44ca5c02edb9932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:50 +0000

    Add package jugg/1.0.8/jugg-1.0.8-py2.py3-none-any.whl

---
+++ packages/jugg/jugg-1.0.8-py2.py3-none-any.whl/jugg/core.py
@@ -0,0 +1,228 @@
+import asyncio
+import json
+import pyarchy
+import socket
+import struct
+
+from concurrent.futures import TimeoutError
+
+from . import constants, security
+
+
+class Datagram(object):
+
+    @classmethod
+    def from_string(cls, str_ : str):
+        return cls(**json.loads(str_))
+
+    def __init__(self,
+                 command : int = None,
+                 sender : str = None, recipient : str = None,
+                 data : str = None, hmac : str = None):
+        object.__init__(self)
+
+        self.__command = command
+        self.__sender = sender
+        self.__recipient = recipient
+        self.__data = data
+        self.__hmac = hmac
+
+    def __str__(self):
+        return json.dumps({
+            'command': self.command,
+            'sender': self.sender,
+            'recipient': self.recipient,
+            'data': self.data,
+            'hmac': self.hmac,
+        })
+
+    @property
+    def command(self):
+        return self.__command
+
+    @command.setter
+    def command(self, command):
+        self.__command = command
+
+    @property
+    def sender(self) -> str:
+        return self.__sender
+
+    @property
+    def recipient(self) -> str:
+        return self.__recipient
+
+    @recipient.setter
+    def recipient(self, recipient : str):
+        self.__recipient = str(recipient)
+
+    @property
+    def route(self):
+        return (self.sender, self.recipient)
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, data):
+        if isinstance(data, bytes):
+            self.__data = data.decode()
+        else:
+            self.__data = data
+
+    @property
+    def hmac(self):
+        return self.__hmac
+
+
+class Node(security.KeyHandler):
+
+    def __init__(self, stream_reader, stream_writer):
+        security.KeyHandler.__init__(self)
+
+        self._stream_reader = stream_reader
+        self._stream_writer = stream_writer
+
+        self._commands = {
+            constants.CMD_SHAKE: self.handle_handshake,
+            constants.CMD_ERR: self.handle_error,
+        }
+
+    async def recv(self, n_bytes : int = None):
+        try:
+            data = await asyncio.wait_for(
+                # Read size if not provided
+                self._stream_reader.read(n_bytes if n_bytes else socket.ntohl(
+                    struct.unpack(
+                        'I',
+                        # Size indicator
+                        await asyncio.wait_for(
+                            self._stream_reader.read(4),
+                            timeout = 4)
+                        )[0]
+                    )),
+                timeout = 1)
+
+            if data:
+                return self.decrypt(data)
+            else:
+                return None
+        except TimeoutError:
+            return ''
+        except struct.error:
+            return None
+
+    async def send(self, dg : Datagram):
+        data = self.encrypt(dg)
+        self._stream_writer.write(
+            struct.pack('I', socket.htonl(len(data))) + data)
+        await self._stream_writer.drain()
+
+    async def start(self):
+        await self.send_handshake()
+
+        # Maintain the connection
+        flag = False
+        while not flag:
+            data = await self.recv()
+            flag = not await self.run(data)
+
+    async def run(self, dg):
+        if dg is None:
+            #  Connection broke
+            return False
+        elif dg:
+            await self.handle_datagram(dg)
+            return True
+        else:
+            # Still connected; nothing to do
+            return True
+
+    async def stop(self):
+        self._stream_writer.close()
+
+    async def send_handshake(self):
+        await self.send(
+            Datagram(
+                command = constants.CMD_SHAKE,
+                sender = self.id,
+                recipient = self.id,
+                data = self.key))
+
+    async def handle_handshake(self, dg):
+        self.counter_key = int(dg.data) if dg.data else 0
+
+    async def send_error(self, errno : int):
+        await self.send(
+            Datagram(
+                command = constants.CMD_ERR,
+                sender = self.id,
+                recipient = self.id,
+                data = errno))
+
+    # Add functionality in subclass
+    async def do_error(self, errno):
+        print('err({0}):'.format(errno), constants.ERROR_INFO_MAP.get(errno))
+
+    async def handle_error(self, dg : Datagram):
+        await self.do_error(dg.data)
+
+    async def handle_datagram(self, dg: Datagram):
+        func = self._commands.get(dg.command)
+        if func:
+            await func(dg)
+        else:
+            await self.send_error(constants.ERR_DISCONNECT)
+
+
+class ClientBase(Node, pyarchy.common.ClassicObject):
+
+    def __init__(self,
+                 stream_reader, stream_writer,
+                 hmac_key, challenge_key):
+        Node.__init__(self, stream_reader, stream_writer)
+        pyarchy.common.ClassicObject.__init__(self, '', rand_id = False)
+
+        self._hmac_key = hmac_key or  b''
+        self._challenge_key = challenge_key or b''
+
+        self._name = None
+
+    def __lt__(self, obj):
+        if isinstance(obj, pyarchy.core.NamedObject):
+            return self.name < obj.name
+        else:
+            return NotImplemented
+
+    def __gt__(self):
+        if isinstance(obj, pyarchy.core.NamedObject):
+            return self.name > obj.name
+        else:
+            return NotImplemented
+
+    @property
+    def name(self) -> str:
+        return str(self._name)
+
+    @name.setter
+    def name(self, name : str):
+        if self._name is None:
+            self._name = str(name)
+        else:
+            raise AttributeError('name can only be set once')
+
+    async def send_response(self, data):
+        await self.send(
+            Datagram(
+                command = constants.CMD_RESP,
+                sender = self.id,
+                recipient = self.id,
+                data = data))
+
+
+__all__ = [
+    Datagram,
+    Node,
+    ClientBase,
+]
---
+++ packages/jugg/jugg-1.0.8-py2.py3-none-any.whl/jugg/server.py
@@ -0,0 +1,165 @@
+import asyncio
+import pyarchy
+import socket
+import srp
+
+from . import constants, utils
+from .core import ClientBase, Datagram
+from .security import KeyHandler
+
+
+class ClientAI(ClientBase):
+
+    def __init__(self,
+                 stream_reader, stream_writer,
+                 hmac_key : bytes, challenge_key : bytes):
+        ClientBase.__init__(
+            self,
+            stream_reader, stream_writer,
+            hmac_key, challenge_key)
+
+        self._commands[constants.CMD_LOGIN] = self.handle_login
+        
+    def verify_credentials(self, data):
+        return utils.validate_name(data)
+
+    async def handle_login(self, dg : Datagram):
+        # Credentials
+        if not self.verify_credentials(dg.data):
+            await self.send_error(constants.ERR_CREDENTIALS)
+            return
+        else:
+            name = dg.data
+            await self.send(
+                Datagram(
+                    command = constants.CMD_LOGIN,
+                    recipient = name))
+
+        # HMAC
+        response = await self.recv()
+
+        if not response or \
+           not response.data or \
+           not self.verify_HMAC(
+               response.data.encode(),
+               name.encode(),
+               self._hmac_key):
+            await self.send_error(constants.ERR_HMAC)
+            return
+        else:
+            await self.send_response(True)
+
+        # Challenge
+        response = await self.recv()
+
+        if response and response.data:
+            svr = srp.Verifier(
+                name.encode(),
+                *srp.create_salted_verification_key(
+                    name.encode(), self._challenge_key),
+                bytes.fromhex(response.data))
+        else:
+            return
+
+        s, B = svr.get_challenge()
+        if (s is None) or (B is None):
+            await self.send_error(constants.ERR_CHALLENGE)
+            return
+        else:
+            await self.send_response([s.hex(), B.hex()])
+
+        # Verification
+        response = await self.recv()
+
+        if response and response.data:
+            HAMK = svr.verify_session(bytes.fromhex(response.data))
+            if HAMK and svr.authenticated():
+                await self.send_response(HAMK.hex())
+                self.counter_cipher = svr.get_session_key()
+
+                self.name = name
+                self.id = pyarchy.core.Identity()
+            else:
+                await self.send_error(constants.ERR_VERIFICATION)
+
+
+class Server(object):
+
+    client_handler = ClientAI
+
+    def __init__(self,
+                 host : str = None, port : int = None,
+                 socket_ : socket.socket = None,
+                 hmac_key : bytes = None, challenge_key : bytes = None):
+        KeyHandler.__init__(self)
+
+        if host and port:
+            self._address = (host, port)
+            self._socket = None
+        elif socket_:
+            self._address = socket_.getsockname()
+            self._socket = socket_
+        else:
+            raise TypeError('must supply either address or socket')
+
+        self._hmac_key = hmac_key or  b''
+        self._challenge_key = challenge_key or b''
+
+    async def new_connection(self, stream_reader, stream_writer, **kwargs):
+        try:
+            # Create the client on the server
+            client = self.client_handler(
+                stream_reader, stream_writer,
+                self._hmac_key, self._challenge_key,
+                **kwargs)
+            self.clients.add(client)
+            # Maintain the connection
+            await client.start()
+            # Connection broke; remove the client from the server
+            self.clients.remove(client)
+        except asyncio.CancelledError:
+            return
+
+    def start(self):
+        if self._socket:
+            pass
+        elif self._address:
+            # Make the socket
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self._socket.bind(self._address)
+        else:
+            raise AttributeError('no socket or address specified')
+
+        # Establish the conncetion
+        loop = asyncio.get_event_loop()
+        server_coro = loop.create_server(
+            # Stream factory
+            lambda: asyncio.StreamReaderProtocol(
+                asyncio.StreamReader(loop = loop),
+                self.new_connection,
+                loop),
+            sock = self._socket)
+
+        # Make the client pool
+        self.clients = pyarchy.data.ItemPool()
+        self.clients.object_type = ClientBase
+
+        self.run(loop, server_coro)
+
+    def run(self, event_loop, start_coro):
+        # Maintain the connection
+        utils.reactive_event_loop(
+            event_loop,
+            start_coro, self.stop(),
+            run_forever = True)
+
+    async def stop(self):
+        # Cleanup
+        pass
+
+
+__all__ = [
+    ClientAI,
+    Server,
+]
