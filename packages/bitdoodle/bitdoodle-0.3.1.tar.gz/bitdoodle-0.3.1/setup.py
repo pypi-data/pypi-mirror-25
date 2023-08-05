9ccf784b031ecf13c5d712c49a0ac12ec77b1e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:02 +0000

    Add package bitdoodle/0.3.1/bitdoodle-0.3.1.tar.gz

---
+++ packages/bitdoodle/bitdoodle-0.3.1.tar.gz/bitdoodle-0.3.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='bitdoodle',
+    version='0.3.1',
+    packages=['bitdoodle',],
+    author='Jared Newell',
+    author_email='jared.newell@gmail.com',
+    license='MIT',
+    long_description='This package is used to doodle with the bits',
+    url='https://github.com/Jaredn/bitdoodle',
+    download_url='https://github.com/Jaredn/bitdoodle/archive/0.3.1.tar.gz',
+    keywords="bit doodle bitmath ip ipaddress",
+    install_requires=[
+        'ipaddress',
+    ]
+)
