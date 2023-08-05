b196f18fac2c1f1d1fab570fe04ae9fdcedcda4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:48 +0000

    Add package django-alipay2/0.9.0/django-alipay2-0.9.0.tar.gz

---
+++ packages/django-alipay2/django-alipay2-0.9.0.tar.gz/django-alipay2-0.9.0/alipay/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+
+class AlipayPaymentAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'out_no', 'no', 'reference_id', 'subject',
+                    'amount_total', 'buyer_email', 'status', 'is_succeed')
+    search_fields = ('out_no', 'no', 'buyer_email')
+    list_filter = ('status',)
