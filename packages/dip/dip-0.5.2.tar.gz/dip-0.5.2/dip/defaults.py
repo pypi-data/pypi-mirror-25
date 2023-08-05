621a081588252fe9f697a59263628a010e6a0583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:17 +0000

    Add package dip/0.5.2/dip-0.5.2.tar.gz

---
+++ packages/dip/dip-0.5.2.tar.gz/dip-0.5.2/dip/defaults.py
@@ -0,0 +1,9 @@
+"""
+Default values.
+"""
+from dip import utils
+
+PATH = u'/usr/local/bin'
+HOME = utils.abspath('config.json')
+TEMPLATE = utils.abspath('template.sh')
+SLEEP = 10
---
+++ packages/dip/dip-0.5.2.tar.gz/dip-0.5.2/dip/errors.py
@@ -0,0 +1,38 @@
+"""
+dip exceptions
+"""
+import click
+
+
+class DipError(click.ClickException):
+    """ Generic dip error. """
+    pass
+
+
+class SettingsError(DipError):
+    """ Error accessing dip config. """
+    def __init__(self, path):
+        super(SettingsError, self).__init__(
+            "Unable to access dip configuration at '{path}'".format(path=path))
+
+
+class NotInstalledError(DipError):
+    """ CLI not installed. """
+    def __init__(self, name):
+        super(NotInstalledError, self).__init__(
+            "'{name}' command is not installed".format(name=name))
+
+
+class ComposeFileNotFound(DipError):
+    """ CLI not installed. """
+    def __init__(self, name):
+        super(ComposeFileNotFound, self).__init__(
+            "No compose file found for '{name}' command".format(name=name))
+
+
+class NoSuchService(DipError):
+    """ CLI not installed. """
+    def __init__(self, name):
+        super(NoSuchService, self).__init__(
+            "No service named '{name}' found in compose file"
+            .format(name=name))
