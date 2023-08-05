9d3d0aedc32c8f4cdc89d297c6700f9c35cce919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:04 +0000

    Add package derivcheck/1.1.4/derivcheck-1.1.4.tar.gz

---
+++ packages/derivcheck/derivcheck-1.1.4.tar.gz/derivcheck-1.1.4/setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Derivcheck is robust and very sensitive tester for analytic derivatives.
+# Copyright (C) 2017 Toon Verstraelen <Toon.Verstraelen@UGent.be>.
+#
+# This file is part of Derivcheck.
+#
+# Derivcheck is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# Derivcheck is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+# --
+"""Derivcheck setup script.
+
+If you are not familiar with setup.py, just use pip instead:
+
+    pip install derivcheck --user --upgrade
+
+Alternatively, you can install from source with
+
+    ./setup.py install --user
+"""
+
+from __future__ import print_function
+
+from setuptools import setup
+
+def get_version():
+    """Load the version from version.py, without importing it.
+
+    This function assumes that the last line in the file contains a variable defining the
+    version string with single quotes.
+
+    """
+    with open('derivcheck/version.py', 'r') as f:
+        return f.read().split('=')[-1].replace('\'', '').strip()
+
+
+def readme():
+    """Load README.rst for display on PyPI."""
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='derivcheck',
+    version=get_version(),
+    description='A robust and very sensitive tester for analytic derivatives.',
+    long_description=readme(),
+    author='Toon Verstraelen',
+    author_email='Toon.Verstraelen@UGent.be',
+    url='https://github.com/theochem/derivcheck',
+    packages=['derivcheck'],
+    install_requires=['numpy', 'nose'],
+    python_requires='>=2.7',
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+)
