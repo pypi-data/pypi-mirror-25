52a984e41a6ab714ff779a4fd05b02298a95b858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:50 +0000

    Add package GADDAG/0.1/GADDAG-0.1.tar.gz

---
+++ packages/GADDAG/GADDAG-0.1.tar.gz/GADDAG-0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, Extension
+from distutils.command.build_ext import build_ext
+
+
+class build_ext(build_ext):
+
+    def build_extension(self, ext):
+        self._ctypes = isinstance(ext, CTypes)
+        return super().build_extension(ext)
+
+    def get_export_symbols(self, ext):
+        if self._ctypes:
+            return ext.export_symbols
+        return super().get_export_symbols(ext)
+
+    def get_ext_filename(self, ext_name):
+        if self._ctypes:
+            return ext_name + '.so'
+        return super().get_ext_filename(ext_name)
+
+
+class CTypes(Extension):
+    pass
+
+
+cgaddag = CTypes("cgaddag", sources=["src/cgaddag.c", "src/gdg.c"])
+
+setup(name="GADDAG",
+      version="0.1",
+      description="Python wrapper of a C GADDAG implementation",
+      author="Jordan Bass",
+      author_email="jordan.bass@mykolab.com",
+      url="https://github.com/jorbas/GADDAG",
+      download_url="https://github.com/jorbas/GADDAG/archive/0.1.tar.gz",
+      keywords=["gaddag", "trie"],
+      package_dir={"": "src"},
+      py_modules=["gaddag"],
+      ext_modules=[cgaddag],
+      cmdclass={"build_ext": build_ext},
+      classifiers=[])
