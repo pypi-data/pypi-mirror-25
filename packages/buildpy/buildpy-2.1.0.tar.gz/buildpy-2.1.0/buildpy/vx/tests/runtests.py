6f45a11b72d3cd3f8d9a85940e27cab96ed353d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:32 +0000

    Add package buildpy/2.1.0/buildpy-2.1.0.tar.gz

---
+++ packages/buildpy/buildpy-2.1.0.tar.gz/buildpy-2.1.0/buildpy/vx/tests/runtests.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import doctest
+import sys
+
+import buildpy.vx
+
+
+def main(argv):
+    doctest.testmod(buildpy.vx, raise_on_error=True)
+    # doctest.testmod(buildpy.vx)
+
+    @buildpy.vx.DSL.let
+    def _():
+        s = buildpy.vx._TSet()
+        s.add(s.add(s.add(1)))
+        assert len(s) == 2
+        s.remove(s.remove(1))
+        assert len(s) == 0
+
+
+if __name__ == '__main__':
+    main(sys.argv)
