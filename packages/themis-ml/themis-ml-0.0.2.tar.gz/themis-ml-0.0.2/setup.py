1f250ccc50adef9d7cdd5668e5aca805930e75c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:21 +0000

    Add package themis-ml/0.0.2/themis-ml-0.0.2.tar.gz

---
+++ packages/themis-ml/themis-ml-0.0.2.tar.gz/themis-ml-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name="themis-ml",
+    version="0.0.2",
+    description="Fairness-aware Machine Learning",
+    author="Niels Bantilan",
+    author_email="niels.bantilan@gmail.com",
+    url="https://github.com/cosmicBboy/themis-ml",
+    download_url="https://github.com/cosmicBboy/themis-ml/archive/0.0.2.tar.gz",
+    keywords=["machine-learning", "fairness-aware", "social-bias"],
+    license="MIT",
+    packages=[
+        "themis_ml",
+        "themis_ml.datasets",
+        "themis_ml.preprocessing",
+        "themis_ml.postprocessing",
+        "themis_ml.linear_model"
+        ],
+    )
