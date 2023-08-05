21e6c43bb9a330ae4e60e08151075894c95c537e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:07 +0000

    Add package gplaycli/0.2.13/GPlayCli-0.2.13.tar.gz

---
+++ packages/gplaycli/GPlayCli-0.2.13.tar.gz/GPlayCli-0.2.13/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, Command
+import os
+
+setup(name='GPlayCli',
+        version='0.2.13',
+        description='GPlayCli, a Google play downloader command line interface',
+        author="Matlink",
+        author_email="matlink@matlink.fr",
+        url="https://github.com/matlink/gplaycli",
+        license="AGPLv3",
+        scripts=['gplaycli/gplaycli'],
+        packages=[
+            'ext_libs/googleplay_api/',
+            'ext_libs/',
+            'gplaycli/',
+        ],
+        data_files=[
+            [os.path.expanduser('~')+'/.config/gplaycli/', ['gplaycli.conf','cron/cronjob']],
+        ],
+        install_requires=[
+                'requests >= 2.0.0',
+                'protobuf',
+                'ndg-httpsclient',
+                'androguard',
+                'clint',
+		'pyasn1',
+                'pycrypto',
+        ],
+    )
