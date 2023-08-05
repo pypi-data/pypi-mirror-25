860e49b7f8f6454481ad3ae19661f5ee57234db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:55 +0000

    Add package dyz/0.4.6/dyz-0.4.6.tar.gz

---
+++ packages/dyz/dyz-0.4.6.tar.gz/dyz-0.4.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+version = '0.4.6'
+
+setup(
+    name='dyz',
+    version=version,
+    description="dyz",
+    long_description=README,
+    classifiers=[
+    ],
+    keywords='dyz',
+    author='me',
+    author_email='me@example.org',
+    url='https://example.org',
+    license='LGPL v3',
+    py_modules=['dyz'],
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'odoorpc',
+        'prettytable',
+        'ConfigParser',
+        'XlsxWriter',
+        'lxml',
+        'pyyaml',
+    ],
+    entry_points='''
+        [console_scripts]
+        dyz=dyz:main
+    ''',
+)
