9cf159c41425fc9799c500c29c27d140a3901594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:23 +0000

    Add package changeOffice/0.52/changeOffice-0.52.tar.gz

---
+++ packages/changeOffice/changeOffice-0.52.tar.gz/changeOffice-0.52/changeOffice/__init__.py
@@ -0,0 +1,1 @@
+from .changeOffice import Change
---
+++ packages/changeOffice/changeOffice-0.52.tar.gz/changeOffice-0.52/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+  name = 'changeOffice',
+  packages = ['changeOffice'],
+  version = '0.52',
+  description = 'change msOffice format from xls to xlsx, doc to docx, ppt to pptx',
+  author = 'Wang Qi',
+  author_email = 'wangmarkqi@gmail.com',
+  url = 'https://github.com/wangmarkqi/changeOffice',
+  download_url = 'https://github.com/wangmarkqi/changeOffice.git',
+    install_requires=[ 'pywin32>=214'],
+  keywords = ['excel', 'word', 'ppt','xls to xlsx','doc to docx','ppt to pptx'],
+  classifiers = [],
+)
+
+'''
+# 上传source 包
+python setup.py sdist build
+python setup.py sdist upload
+# 上传pre-compiled包
+python setup.py bdist_wheel --universal
+python setup.py bdist_wheel upload
+'''
