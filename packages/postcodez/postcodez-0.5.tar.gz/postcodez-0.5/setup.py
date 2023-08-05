4c721ff2682c8247c0031328f2f099ade173166b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:30 +0000

    Add package postcodez/0.5/postcodez-0.5.tar.gz

---
+++ packages/postcodez/postcodez-0.5.tar.gz/postcodez-0.5/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(
+  name = 'postcodez',
+  packages = ['postcodez'],
+  version = '0.5',
+  description = 'Python module for managing badly formatted UK postcode data.',
+  author = 'Toby Petty',
+  author_email = 'hello@tobypetty.com',
+  url = 'https://github.com/woblers/postcodez',
+  keywords = ['postcode', 'UK', 'london', 'data', 'clean']
+)
