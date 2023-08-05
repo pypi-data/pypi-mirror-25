be1ae070cb29f35ffc9416447640ec50c151adcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:08 +0000

    Add package akagi/0.2.1/akagi-0.2.1-py2.py3-none-any.whl

---
+++ packages/akagi/akagi-0.2.1-py2.py3-none-any.whl/akagi/data_sources/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from akagi.data_sources import redshift_data_source
+from akagi.data_sources import s3_data_source
+from akagi.data_sources import local_data_source
+from akagi.data_sources import spreadsheet_data_source
+
+
+from akagi.data_sources.redshift_data_source import RedshiftDataSource
+from akagi.data_sources.s3_data_source import S3DataSource
+from akagi.data_sources.local_data_source import LocalDataSource
+from akagi.data_sources.spreadsheet_data_source import SpreadsheetDataSource
