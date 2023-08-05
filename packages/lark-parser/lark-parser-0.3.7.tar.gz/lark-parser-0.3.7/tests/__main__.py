ebcea8496fde3cb648e391a46c0171534386ce22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:06 +0000

    Add package lark-parser/0.3.7/lark-parser-0.3.7.tar.gz

---
+++ packages/lark-parser/lark-parser-0.3.7.tar.gz/lark-parser-0.3.7/tests/__main__.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import, print_function
+
+import unittest
+import logging
+
+from .test_trees import TestTrees
+
+# from .test_selectors import TestSelectors
+# from .test_grammars import TestPythonG, TestConfigG
+
+from .test_parser import (
+        TestLalrStandard,
+        TestEarleyStandard,
+        TestLalrContextual,
+        TestEarleyScanless,
+        TestEarleyDynamic,
+
+        TestFullEarleyScanless,
+        TestFullEarleyDynamic,
+
+        TestParsers,
+        )
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == '__main__':
+    unittest.main()
