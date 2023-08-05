a7dcddc34d6b2cab3d90ba1fb5a5fea32a9e3656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:06 +0000

    Add package apple-search-ads/0.2.7/apple_search_ads-0.2.7.tar.gz

---
+++ packages/apple-search-ads/apple_search_ads-0.2.7.tar.gz/apple_search_ads-0.2.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='apple_search_ads',
+      version='0.2.7',
+      description='Python wrapper for Apple Search Ads APIs',
+      url='https://github.com/aovlexus/apple-searchads-api',
+      author='Luca Giacomel forked by Aleksnadr Usov',
+      author_email='lg@bendingspoons.com, aovlexus@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          "python-decouple",
+          "pandas",
+          "requests",
+          "tqdm",
+      ],
+      zip_safe=False)
