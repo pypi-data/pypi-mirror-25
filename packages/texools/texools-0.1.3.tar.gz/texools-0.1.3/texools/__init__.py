319a300b7199a08d8fef67c418ae48f4fe72df21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:50 +0000

    Add package texools/0.1.3/texools-0.1.3.tar.gz

---
+++ packages/texools/texools-0.1.3.tar.gz/texools-0.1.3/texools/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for texools"""
+
+from .core import asciify  # noqa
+
+__author__ = """CW Andrews"""
+__email__ = 'cwandrews.oh@gmail.com'
+__version__ = '0.1.3'
