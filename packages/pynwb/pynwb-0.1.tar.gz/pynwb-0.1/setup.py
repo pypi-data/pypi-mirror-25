1b3d35ab3f3eb27dc7e8977fe83750afed854ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:14 +0000

    Add package pynwb/0.1/pynwb-0.1.tar.gz

---
+++ packages/pynwb/pynwb-0.1.tar.gz/pynwb-0.1/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+
+import json
+import pickle
+import os
+
+
+schema_dir = '%s/src/pynwb/data' % os.path.abspath(os.path.dirname(__file__))
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('license.txt') as f:
+    license = f.read()
+
+
+pkgs = find_packages('src', exclude=['data'])
+print('found these packages:', pkgs)
+
+schema_dir = 'data'
+setup_args = {
+    'name': 'pynwb',
+    'version': '0.1',
+    'description': 'Package for working with Neurodata stored in the NWB format',
+    'long_description': readme,
+    'author': 'Andrew Tritt',
+    'author_email': 'ajtritt@lbl.gov',
+    'url': 'https://github.com/NeurodataWithoutBorders/pynwb',
+    'license': license,
+    'packages': pkgs,
+    'package_dir': {'': 'src'},
+    'package_data': {'pynwb':["%s/*.yaml" % schema_dir, "%s/*.json" % schema_dir]},
+    #'package_data': {'pynwb':["data/*.yaml"]},
+    #'cmdclass':{
+    #    'build_py': CustomBuild,
+    #}
+}
+
+if __name__ == '__main__':
+    '''
+    Do some stuff here to retrieve (download from centralized location, or parse
+    from a location in the repo directory) the schema. Once retrieved write to
+    to Python code somewhere. This could be pickling it, or writing it to a
+    Python file as an ecoded string. This way, modifying the schema will require
+    rebuilding the package, and the schema will be hardcoded.
+    '''
+
+    #get_schema()
+    setup(**setup_args)
---
+++ packages/pynwb/pynwb-0.1.tar.gz/pynwb-0.1/src/form/build/__init__.py
@@ -0,0 +1,7 @@
+from .builders import GroupBuilder
+from .builders import DatasetBuilder
+from .builders import LinkBuilder
+
+from .map import ObjectMapper
+from .map import BuildManager
+from .map import TypeMap
