1aca68e3c097cfdc706345eebe9bbfb2598b85ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:48 +0000

    Add package edrn.labcas.ui/1.0.14/edrn.labcas.ui-1.0.14.zip

---
+++ packages/edrn.labcas.ui/edrn.labcas.ui-1.0.14.zip/edrn.labcas.ui-1.0.14/src/edrn/labcas/ui/views/__init__.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+from ._accept import AcceptView
+from ._authentication import AuthenticationView
+from ._collection import CollectionView
+from ._collections import CollectionsView
+from ._completions import PeopleCompletionsView, ProtocolCompletionsView, LDAPGroupsCompletionsView
+from ._dataset import DatasetView
+from ._favicon import favicon_view
+from ._file import FileView
+from ._metadata import MetadataView
+from ._search import SearchView
+from ._upload import UploadView
+from ._wfmetadata import WFMetadataView
+
+
+__all__ = (
+    AcceptView,
+    AuthenticationView,
+    CollectionsView,
+    CollectionView,
+    DatasetView,
+    favicon_view,
+    FileView,
+    LDAPGroupsCompletionsView,
+    MetadataView,
+    PeopleCompletionsView,
+    ProtocolCompletionsView,
+    SearchView,
+    UploadView,
+    WFMetadataView
+)
---
+++ packages/edrn.labcas.ui/edrn.labcas.ui-1.0.14.zip/edrn.labcas.ui-1.0.14/src/edrn/labcas/ui/views/_collection.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+from pyramid.view import view_config, view_defaults
+from edrn.labcas.ui import PACKAGE_NAME
+from edrn.labcas.ui.utils import LabCASCollection
+
+
+@view_defaults(renderer=PACKAGE_NAME + ':templates/collection.pt')
+class CollectionView(object):
+    def __init__(self, request):
+        self.request = request
+    @view_config(route_name='collection', permission='view')
+    def __call__(self):
+        collectionID = self.request.matchdict['collectionID']
+        principals = frozenset(self.request.effective_principals)
+        collection = LabCASCollection.get(collectionID, principals)
+        return {'collection': collection, 'pageTitle': collection.name}
