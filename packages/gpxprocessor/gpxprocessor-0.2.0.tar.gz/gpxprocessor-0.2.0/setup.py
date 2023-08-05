c53e1d566bf69b56471cebc673e3f70140457543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:12 +0000

    Add package gpxprocessor/0.2.0/gpxprocessor-0.2.0.tar.gz

---
+++ packages/gpxprocessor/gpxprocessor-0.2.0.tar.gz/gpxprocessor-0.2.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""A setuptools to install the script"""
+
+from setuptools import setup
+
+setup(
+    # Name of the script as it will appear in /usr/bin.
+    name='gpxprocessor',
+
+    # Current version of the script.
+    version='0.2.0',
+
+    # Author details.
+    author='Konrad Dryja',
+    author_email='k.dryja.15@aberdeen.ac.uk',
+
+    # Packages that will be installed.
+    packages=['gpxprocessor'],
+
+    # Install all required packages.
+    install_requires=['argparse', 'lxml', 'python-dateutil'],
+
+    # Required Python3 or above.
+    python_requires='>=3',
+
+    # Repository URL.
+    url='https://github.com/devon96/GPXProcessor',  # use the URL to the github repo
+
+    # Main entrypoint to the script.
+    entry_points={
+        'console_scripts': ['gpxprocessor = gpxprocessor.processor:main']})
