1445c70278734735fc5a6619a613bcbe4d74fe52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:28 +0000

    Add package db-migrator/1.0.1/db-migrator-1.0.1.tar.gz

---
+++ packages/db-migrator/db-migrator-1.0.1.tar.gz/db-migrator-1.0.1/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2015, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+
+import sys
+from setuptools import setup, find_packages
+
+install_requires = (
+    'psycopg2>=2.7',
+    )
+
+tests_require = [
+    ]
+
+if sys.version_info.major < 3:
+    tests_require.append('mock')
+
+LONG_DESC = '\n\n~~~~\n\n'.join([open('README.rst').read(),
+                                 open('CHANGELOG.rst').read()])
+
+setup(
+    name='db-migrator',
+    version='1.0.1',
+    author='Connexions',
+    author_email='info@cnx.org',
+    url='https://github.com/karenc/db-migrator',
+    license='AGPL, see also LICENSE.txt',
+    description='Python package to migrate postgresql database',
+    long_description=LONG_DESC,
+    packages=find_packages(),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite='dbmigrator.tests',
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'dbmigrator = dbmigrator.cli:main',
+            ],
+        },
+    )
