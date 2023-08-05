15e425df7afa5ab9eda0b59d05f109535d5a86f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:11 +0000

    Add package eSSP6/1.0.8/eSSP6-1.0.8.tar.gz

---
+++ packages/eSSP6/eSSP6-1.0.8.tar.gz/eSSP6-1.0.8/_eSSP/init.c
@@ -0,0 +1,42 @@
+#include "inc/SSPComs.h"
+#include "port_linux.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+SSP_COMMAND *ssp_init(char *port_c, char *addr_c, int debug)
+{
+    SSP_COMMAND* sspC = malloc(sizeof(SSP_COMMAND));
+    
+    sspC->SSPAddress = (int)(strtod(addr_c, NULL));
+    // Linux does not need to do any initialisation for the SSP library
+    sspC->Timeout = 1000;
+    sspC->EncryptionStatus = NO_ENCRYPTION; // But there is an encryption
+    sspC->RetryLevel = 3;
+    sspC->BaudRate = 9600;
+    sspC->Key.EncryptKey = 1;   // Just to make sure 
+    //Open the COM port
+    if ( debug == 1 ){ 
+	    printf("PORT: %s\n", port_c); // Printing the connection informations
+
+	    printf ("##%s##\n", port_c);
+    }
+
+    if (open_ssp_port(port_c) == 0)
+    {
+	printf("Port Error\n");
+	return NULL;
+    }
+     
+
+    //run_validator(&sspC);
+    // close the com port
+    //close_ssp_port();
+    
+    return sspC;
+}
+
+unsigned char* ssp_get_response_data(SSP_COMMAND* sspc)
+{
+    return sspc->ResponseData;
+}
