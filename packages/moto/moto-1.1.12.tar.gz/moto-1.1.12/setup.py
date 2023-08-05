26cbb9337b869b3c0ba9fc0b4b4f3a77319f996f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:10 +0000

    Add package moto/1.1.12/moto-1.1.12.tar.gz

---
+++ packages/moto/moto-1.1.12.tar.gz/moto-1.1.12/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+from setuptools import setup, find_packages
+
+install_requires = [
+    "Jinja2>=2.8",
+    "boto>=2.36.0",
+    "boto3>=1.2.1",
+    "cookies",
+    "requests>=2.5",
+    "xmltodict",
+    "dicttoxml",
+    "six>1.9",
+    "werkzeug",
+    "pyaml",
+    "pytz",
+    "python-dateutil<3.0.0,>=2.1",
+    "mock",
+]
+
+extras_require = {
+    'server': ['flask'],
+}
+
+setup(
+    name='moto',
+    version='1.1.12',
+    description='A library that allows your python tests to easily'
+                ' mock out the boto library',
+    author='Steve Pulec',
+    author_email='spulec@gmail.com',
+    url='https://github.com/spulec/moto',
+    entry_points={
+        'console_scripts': [
+            'moto_server = moto.server:main',
+        ],
+    },
+    packages=find_packages(exclude=("tests", "tests.*")),
+    install_requires=install_requires,
+    extras_require=extras_require,
+    license="Apache",
+    test_suite="tests",
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Testing",
+    ],
+)
