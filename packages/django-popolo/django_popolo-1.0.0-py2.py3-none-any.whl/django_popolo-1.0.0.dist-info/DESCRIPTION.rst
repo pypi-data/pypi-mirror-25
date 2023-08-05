ab29a1257dd61b4930d5bf25a19bc75f4cb9d7b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:01 +0000

    Add package django-popolo/1.0.0/django_popolo-1.0.0-py2.py3-none-any.whl

---
+++ packages/django-popolo/django_popolo-1.0.0-py2.py3-none-any.whl/django_popolo-1.0.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,61 @@
+popolo
+========================
+
+
+.. image:: https://travis-ci.org/openpolis/django-popolo.svg?branch=master
+  :target: https://travis-ci.org/openpolis/django-popolo
+
+.. image:: https://coveralls.io/repos/github/openpolis/django-popolo/badge.svg?branch=master
+  :target: https://coveralls.io/github/openpolis/django-popolo?branch=master
+
+
+Welcome to the documentation for django-popolo!
+
+
+**django-popolo** is a django-based implementation of the
+`Popolo's open government data specifications <http://popoloproject.com/>`_.
+
+It is developed as a django application to be deployed directly within django projects.
+
+It will allow web developers using it to manage and store data according to Popolo's specifications.
+
+The standard sql-oriented django ORM will be used.
+
+Project is under way and any help is welcome.
+
+
+Installation
+------------
+To install ``django-popolo`` as a third party app within a django project,
+you need to add it to the django project's requirements.txt.
+You can do this from GitHub in the usual way, or using the
+``django-popolo`` package on PyPI.
+
+
+Compatibility
+-------------
+
+
+Running the Tests
+-----------------
+
+Set up the test environment with::
+
+    pip install -r tests_requirements.txt
+    python setup.py install
+
+You can run the tests with::
+
+    python setup.py test
+
+or::
+
+    python runtests.py
+
+Notes on mysociety's fork
+-------------------------
+mysociety/django-popolo is a fork of this project where integer ID's are used
+instead of slugs. Otherwise the two projects are functionally equivalent.
+
+
+
---
+++ packages/django-popolo/django_popolo-1.0.0-py2.py3-none-any.whl/popolo/behaviors/models.py
@@ -0,0 +1,115 @@
+try:
+    from django.contrib.contenttypes.fields import GenericForeignKey
+except ImportError:
+    # This fallback import is the version that was deprecated in
+    # Django 1.7 and is removed in 1.9:
+    from django.contrib.contenttypes.generic import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from model_utils.fields import AutoCreatedField, AutoLastModifiedField
+from autoslug import AutoSlugField
+from datetime import datetime
+
+__author__ = 'guglielmo'
+
+
+def get_slug_source(instance):
+    """For use in AutoSlugField's populate_from"""
+    return instance.slug_source
+
+
+class GenericRelatable(models.Model):
+    """
+    An abstract class that provides the possibility of generic relations
+    """
+    content_type = models.ForeignKey(ContentType, blank=True, null=True)
+    object_id = models.CharField(blank=True, null=True, max_length=256)
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    class Meta:
+        abstract = True
+
+
+def validate_partial_date(value):
+    """
+    Validate a partial date, it can be partial, but it must yet be a valid date.
+    Accepted formats are: YYYY-MM-DD, YYYY-MM, YYYY.
+    2013-22 must rais a ValidationError, as 2013-13-12, or 2013-11-55.
+    """
+    try:
+        datetime.strptime(value, '%Y-%m-%d')
+    except ValueError:
+        try:
+            datetime.strptime(value, '%Y-%m')
+        except ValueError:
+            try:
+                datetime.strptime(value, '%Y')
+            except ValueError:
+                raise ValidationError(
+                    u'date seems not to be correct %s' % value)
+
+
+class Dateframeable(models.Model):
+    """
+    An abstract base class model that provides a start and an end dates to
+    the class.
+    Uncomplete dates can be used. The validation pattern is: "^[0-9]{4}(-[
+    0-9]{2}){0,2}$"
+    """
+    partial_date_validator = RegexValidator(regex="^[0-9]{4}(-[0-9]{2}){0,2}$",
+                                            message="Date has wrong format")
+
+    start_date = models.CharField(
+        _("start date"), max_length=10, blank=True, null=True,
+        validators=[partial_date_validator, validate_partial_date],
+        help_text=_("The date when the validity of the item starts"),
+    )
+    end_date = models.CharField(
+        _("end date"), max_length=10, blank=True, null=True,
+        validators=[partial_date_validator, validate_partial_date],
+        help_text=_("The date when the validity of the item ends")
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Timestampable(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    ``created`` and ``modified`` fields.
+    """
+    created_at = AutoCreatedField(_('creation time'))
+    updated_at = AutoLastModifiedField(_('last modification time'))
+
+    class Meta:
+        abstract = True
+
+
+class Permalinkable(models.Model):
+    """
+    An abstract base class model that provides a unique slug,
+    and the methods necessary to handle the permalink
+    """
+    from django.utils.text import slugify
+
+    slug = AutoSlugField(
+        populate_from=get_slug_source,
+        unique=True,
+        slugify=slugify
+    )
+
+    class Meta:
+        abstract = True
+
+    def get_url_kwargs(self, **kwargs):
+        kwargs.update(getattr(self, 'url_kwargs', {}))
+        return kwargs
+
+    @models.permalink
+    def get_absolute_url(self):
+        url_kwargs = self.get_url_kwargs(slug=self.slug)
+        return (self.url_name, (), url_kwargs)
