a9b9ec2f056096ff40c4ab14a020d632a771d440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:13 +0000

    Add package backend.ai/1.0.0/backend.ai-1.0.0.tar.gz

---
+++ packages/backend.ai/backend.ai-1.0.0.tar.gz/backend.ai-1.0.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+from pathlib import Path
+
+
+setup(
+    name='backend.ai',
+    version='1.0.0',
+    description='Lablup Backend.AI Meta-package',
+    long_description=Path('README.rst').read_text(),
+    url='https://github.com/lablup/sorna',
+    author='Lablup Inc.',
+    author_email='devops@lablup.com',
+    license='LGPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Environment :: No Input/Output (Daemon)',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+    ],
+
+    packages=['backend'],
+    namespace_packages=['backend'],
+
+    python_requires='>=3.6',
+    install_requires=[
+        'sorna-common~=1.0.0',
+    ],
+    extras_require={
+        'manager': [
+            'sorna-manager~=1.0.0',
+        ],
+        'agent': [
+            'sorna-agent~=1.0.0',
+        ],
+    },
+    data_files=[],
+)
