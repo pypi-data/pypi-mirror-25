1486e74e1ff3796e4ca8ab9a770ed2db243f732a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:34 +0000

    Add package gems/0.2.3/gems-0.2.3-py2.py3-none-any.whl

---
+++ packages/gems/gems-0.2.3-py2.py3-none-any.whl/gems/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Blake Printy'
+__email__ = 'bprinty@gmail.com'
+__version__ = '0.2.3'
+
+
+from .datatypes import composite
+from .datatypes import filetree
+
+from .decorators import require
+from .decorators import exception
+
+from .metaclasses import DocRequire
