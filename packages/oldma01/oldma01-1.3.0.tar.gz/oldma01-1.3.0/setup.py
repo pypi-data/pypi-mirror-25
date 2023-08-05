77e4e9e494123178dc87cf0de390b45a649c99cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:13 +0000

    Add package oldma01/1.3.0/oldma01-1.3.0.tar.gz

---
+++ packages/oldma01/oldma01-1.3.0.tar.gz/oldma01-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='oldma01',
+    version='1.3.0',
+    py_modules=['oldma01'],
+    author='oldma',
+    author_email='oldma89@gmail.com',
+    description='A simple printer of nester lists',
+
+    )
