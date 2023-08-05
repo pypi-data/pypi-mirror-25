3e0c48c26af59a9ffcf368a526211235edaf2788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:38 +0000

    Add package indy-plenum-dev/1.1.126/indy-plenum-dev-1.1.126.tar.gz

---
+++ packages/indy-plenum-dev/indy-plenum-dev-1.1.126.tar.gz/indy-plenum-dev-1.1.126/plenum/test/checkpoints/test_stable_checkpoint.py
@@ -0,0 +1,31 @@
+from plenum.test import waits
+from plenum.test.checkpoints.helper import chkChkpoints, checkRequestCounts
+from plenum.test.helper import sendReqsToNodesAndVerifySuffReplies
+from stp_core.loop.eventually import eventually
+
+
+def testRequestOlderThanStableCheckpointRemoved(chkFreqPatched, looper,
+                                                txnPoolNodeSet, client1,
+                                                wallet1, client1Connected,
+                                                reqs_for_checkpoint):
+    max_batch_size = chkFreqPatched.Max3PCBatchSize
+    chk_freq = chkFreqPatched.CHK_FREQ
+    reqs = sendReqsToNodesAndVerifySuffReplies(
+        looper, wallet1, client1, reqs_for_checkpoint - max_batch_size, 1)
+    timeout = waits.expectedTransactionExecutionTime(len(txnPoolNodeSet))
+    looper.run(eventually(chkChkpoints, txnPoolNodeSet, 1, retryWait=1,
+                          timeout=timeout))
+    checkRequestCounts(txnPoolNodeSet, len(reqs), chk_freq - 1, 1)
+    sendReqsToNodesAndVerifySuffReplies(looper, wallet1, client1,
+                                        max_batch_size, 1)
+
+    looper.run(eventually(chkChkpoints, txnPoolNodeSet, 1, 0, retryWait=1,
+                          timeout=timeout))
+    checkRequestCounts(txnPoolNodeSet, 0, 0, 0)
+
+    sendReqsToNodesAndVerifySuffReplies(looper, wallet1, client1,
+                                        reqs_for_checkpoint + 1, 1)
+
+    looper.run(eventually(chkChkpoints, txnPoolNodeSet, 2, 0, retryWait=1,
+                          timeout=timeout))
+    checkRequestCounts(txnPoolNodeSet, 1, 1, 1)
