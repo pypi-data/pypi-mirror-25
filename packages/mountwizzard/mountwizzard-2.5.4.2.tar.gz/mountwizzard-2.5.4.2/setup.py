e550922f3e0617dc00de77577a9799bf935ac58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:24 +0000

    Add package mountwizzard/2.5.4.2/mountwizzard-2.5.4.2.tar.gz

---
+++ packages/mountwizzard/mountwizzard-2.5.4.2.tar.gz/mountwizzard-2.5.4.2/setup.py
@@ -0,0 +1,35 @@
+# from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='mountwizzard',
+    version='2.5.4.2',
+    packages=[
+        'analyse',
+        'astrometry',
+        'automation',
+        'baseclasses',
+        'camera',
+        'dome',
+        'environment',
+        'gui',
+        'modeling',
+        'mount',
+        'relays',
+        'remote',
+        'widgets'
+    ],
+    python_requires='~=3.5',
+    install_requires=[
+        'PyQt5>=5.5',
+        'matplotlib>=1.5.3',
+        'pypiwin32>=219',
+        'pyfits>=3.4',
+        'wakeonlan>=0.2.2'
+    ],
+    url='https://pypi.python.org/pypi/mountwizzard',
+    license='APL 2.0',
+    author='mw',
+    author_email='michael@wuertenberger.org',
+    description='tooling for a 10micron mount',
+)
