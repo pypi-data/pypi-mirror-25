4318b48bd2b162c161c10de72a1cdff977d7bb11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:44 +0000

    Add package iml/0.2.6/iml-0.2.6.tar.gz

---
+++ packages/iml/iml-0.2.6.tar.gz/iml-0.2.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(name='iml',
+      version='0.2.6',
+      description='Interpretable Machine Learning (iML) package. Explain the predictions of any model.',
+      url='http://github.com/interpretable-ml/iml',
+      author='Scott Lundberg',
+      author_email='slund1@cs.washington.edu',
+      license='MIT',
+      packages=['iml', 'iml.explainers'],
+      package_data={
+        'iml': ['resources/*']
+      },
+      install_requires=['numpy', 'scipy', 'esvalues'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
