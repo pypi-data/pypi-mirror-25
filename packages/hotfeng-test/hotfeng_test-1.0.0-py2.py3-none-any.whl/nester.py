d97837f8753031607534d6feb5271e24c0c26328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:13 +0000

    Add package hotfeng-test/1.0.0/hotfeng_test-1.0.0-py2.py3-none-any.whl

---
+++ packages/hotfeng-test/hotfeng_test-1.0.0-py2.py3-none-any.whl/nester.py
@@ -0,0 +1,8 @@
+"""这是nester.py模块，提供了一个名为print_lol()的函数
+这个函数作用是打印列表，其中有可能包含嵌套列表"""
+def print_lol(the_list):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item)
+		else:
+			print(each_item)
