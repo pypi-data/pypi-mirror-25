934f2136f6d5ce7df5fb29887517dc2f037cb577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:45 +0000

    Add package pyscores/0.2/pyscores-0.2.tar.gz

---
+++ packages/pyscores/pyscores-0.2.tar.gz/pyscores-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='pyscores',
+      version='0.2',
+      description='Football (soccer) scores in your command line',
+      url='https://github.com/conormag94/pyscores',
+      author='Conor Maguire',
+      author_email='conormag94@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'scores=pyscores.cli:main'
+          ]
+      },
+      install_requires=[
+          'click==5.1',
+          'requests==2.8.1',
+          'tabulate==0.7.5',
+          'termcolor==1.1.0'
+      ])
