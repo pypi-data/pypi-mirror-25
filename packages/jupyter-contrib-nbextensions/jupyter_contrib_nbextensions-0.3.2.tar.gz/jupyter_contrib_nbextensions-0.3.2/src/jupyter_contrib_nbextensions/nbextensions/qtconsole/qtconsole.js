e47f33932049a19ce61b441091f29612eae98ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:32 +0000

    Add package jupyter-contrib-nbextensions/0.3.2/jupyter_contrib_nbextensions-0.3.2.tar.gz

---
+++ packages/jupyter-contrib-nbextensions/jupyter_contrib_nbextensions-0.3.2.tar.gz/jupyter_contrib_nbextensions-0.3.2/src/jupyter_contrib_nbextensions/nbextensions/qtconsole/qtconsole.js
@@ -0,0 +1,25 @@
+// Launch QT Console attached to the current kernel
+
+define([
+    'base/js/namespace',
+    'base/js/events'
+    ], function(Jupyter, events) {
+        var load_ipython_extension = function () {
+            Jupyter.toolbar.add_buttons_group([{
+                /**
+                 * Button to launch QTConsole
+                 */
+                id: 'qtconsole',
+                action: Jupyter.keyboard_manager.actions.register ({
+                     'help'   : 'Run QTConsole',
+                     'icon'   : 'fa-terminal',
+                     'handler': function () {
+                         Jupyter.notebook.kernel.execute('%qtconsole')
+                     }
+                }, 'run-qtconsole', 'qtconsole')
+            }]);
+        };
+        return {
+            load_ipython_extension : load_ipython_extension
+        };
+});
