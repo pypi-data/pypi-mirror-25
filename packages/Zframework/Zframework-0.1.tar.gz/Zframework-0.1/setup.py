9b0ea053fb17696808236197ab8fb7fd6d3b2715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:32 +0000

    Add package Zframework/0.1/Zframework-0.1.tar.gz

---
+++ packages/Zframework/Zframework-0.1.tar.gz/Zframework-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'Zframework',
+  packages = ['Z'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Easy Deep Learning Tool with efficient algorithms.',
+  author = 'Abhishek Yanamandra',
+  author_email = 'abhishekasdzxc@gmail.com',
+  url = 'https://github.com/abhishekyana/Zframework', # use the URL to the github repo
+  download_url = 'https://github.com/abhishekyana/Zframework/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['Deep Learning', 'Machine Learning', 'Framework','Numpy','like Tensorflow'], # arbitrary keywords
+  classifiers = [],
+)
