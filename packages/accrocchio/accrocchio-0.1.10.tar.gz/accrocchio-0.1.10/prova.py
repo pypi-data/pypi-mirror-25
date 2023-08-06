697b4b8360045f08fb44c6e70e0d9778016528f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:30 +0000

    Add package accrocchio/0.1.10/accrocchio-0.1.10.tar.gz

---
+++ packages/accrocchio/accrocchio-0.1.10.tar.gz/accrocchio-0.1.10/prova.py
@@ -0,0 +1,7 @@
+from accrocchio.badgeofshame import detonator, epoxy, this_is_a, this_is_an
+
+
+this_is_an(epoxy)
+this_is_a(detonator)
+print(detonator.how_many())  # this will be 1
+print(epoxy.how_many())  # this will be 1
