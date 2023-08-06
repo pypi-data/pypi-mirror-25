a4c6594e00d7d3ec0da767e2d53d6fa8ac96b83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:23 +0000

    Add package dyploy/0.4.1/dyploy-0.4.1.tar.gz

---
+++ packages/dyploy/dyploy-0.4.1.tar.gz/dyploy-0.4.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+version = '0.4.1'
+
+setup(
+    name='dyploy',
+    version=version,
+    description="dyploy",
+    long_description=README,
+    classifiers=[],
+    keywords='dyploy',
+    author='me',
+    author_email='me@example.org',
+    url='https://example.org',
+    license='LGPL v3',
+    zip_safe=True,
+    py_modules=['dyploy'],
+    packages=['dyploy'],
+
+    package_dir={'dyploy': 'templates', },
+
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'prettytable',
+        'configobj',
+        'paramiko',
+        'scp',
+        'voluptuous',
+        'pyyaml',
+        'dpath',
+        'dyools',
+    ],
+    entry_points='''
+        [console_scripts]
+        dyploy=dyploy:main
+    ''',
+)
