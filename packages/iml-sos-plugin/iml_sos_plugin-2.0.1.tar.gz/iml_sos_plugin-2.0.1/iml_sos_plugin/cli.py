c0caf55c04886b6dfcee515b52542f98a4ed0f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:55 +0000

    Add package iml-sos-plugin/2.0.1/iml_sos_plugin-2.0.1.tar.gz

---
+++ packages/iml-sos-plugin/iml_sos_plugin-2.0.1.tar.gz/iml_sos_plugin-2.0.1/iml_sos_plugin/cli.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2017 Intel Corporation. All rights reserved.
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file.
+
+import sys
+from subprocess import call
+from itertools import chain, imap
+
+
+def flat_map(fn, xs):
+    return chain.from_iterable(imap(fn, xs))
+
+
+def main():
+    args = sys.argv[1:]
+
+    plugins = [
+        "iml",
+        "pacemaker",
+        "kernel",
+        "pci",
+        "logs",
+        "processor",
+        "memory",
+        "filesys",
+        "block",
+        "yum",
+    ]
+
+    code = call(["sosreport"] + args + ["--batch", "--log_size=0"] +
+                list(flat_map(lambda x: ["--only", x], plugins)))
+
+    sys.exit(code)
