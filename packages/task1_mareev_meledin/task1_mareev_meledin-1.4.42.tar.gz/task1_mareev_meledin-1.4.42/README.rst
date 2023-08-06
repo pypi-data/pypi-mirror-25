b2c6c63ecc4dedb0c8df0051ab28691cbf284911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:41 +0000

    Add package task1_mareev_meledin/1.4.42/task1_mareev_meledin-1.4.42.tar.gz

---
+++ packages/task1_mareev_meledin/task1_mareev_meledin-1.4.42.tar.gz/task1_mareev_meledin-1.4.42/README.rst
@@ -0,0 +1,16 @@
+task1_mareev_meledin
+--------
+
+This package is here to demonstrate the skill of creating packages.
+
+To use, simply do: ::
+
+    import import solve
+    print(solve.nash_equilibrium([[1, 2], [3, 4]]))
+
+
+Посмотрим на исходный код:
+::
+
+    Пример исходного кода
+
---
+++ packages/task1_mareev_meledin/task1_mareev_meledin-1.4.42.tar.gz/task1_mareev_meledin-1.4.42/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+# Функция для загрузки подробного описания проекта.
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+      # Имя пакета.
+setup(name='task1_mareev_meledin',
+      # Версия.
+      version='1.4.42',
+      # Краткое описание пакета.
+      description='Workshop 3 course. Autumn 17-18. Exercise 1.',
+      # Авторы.
+      author='Mareev Gleb and Meledin Stanislav',
+      # Контактный email.
+      author_email='wowmagic.gm@gmail.com',
+      # Лицензия, под которой распространяется пакет.
+      license='MIT',
+      packages=['task1_mareev_meledin'],
+      # Зависимости от других пакетов.
+      install_requires=[
+          'numpy',
+          'scipy'
+      ],
+      # Подробное описание проекта.
+      long_description=readme(),
+      include_package_data=True,
+      zip_safe=False)
