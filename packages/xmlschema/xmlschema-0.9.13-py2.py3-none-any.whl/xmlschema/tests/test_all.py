b76b9a09ee923632ce707a3754413b24f2afbaaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:38 +0000

    Add package xmlschema/0.9.13/xmlschema-0.9.13-py2.py3-none-any.whl

---
+++ packages/xmlschema/xmlschema-0.9.13-py2.py3-none-any.whl/xmlschema/tests/test_all.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c), 2016-2017, SISSA (International School for Advanced Studies).
+# All rights reserved.
+# This file is distributed under the terms of the MIT License.
+# See the file 'LICENSE' in the root directory of the present
+# distribution, or http://opensource.org/licenses/MIT.
+#
+# @author Davide Brunato <brunato@sissa.it>
+#
+"""
+This module runs all tests of the 'xmlschema' package.
+"""
+from _test_common import *
+
+pkg_folder = os.path.dirname(os.getcwd())
+sys.path.insert(0, pkg_folder)
+
+if __name__ == '__main__':
+    from test_meta import *
+    from test_xpath import *
+    from test_schemas import *
+    from test_decoding import *
+    from test_resources import *
+    from test_validation import *
+
+    pkg_folder = os.path.dirname(os.getcwd())
+    sys.path.insert(0, pkg_folder)
+    pathname = os.path.join(pkg_folder, "tests/*/testfiles")
+    globals().update(create_schema_tests(pathname))
+    globals().update(create_decoding_tests(pathname))
+    globals().update(create_validation_tests(pathname))
+    unittest.main()
