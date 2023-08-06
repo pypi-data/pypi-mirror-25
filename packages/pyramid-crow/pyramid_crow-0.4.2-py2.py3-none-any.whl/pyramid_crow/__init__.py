8ecb0272296bf50599b19f4036c527625ea3fda5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:04 +0000

    Add package pyramid-crow/0.4.2/pyramid_crow-0.4.2-py2.py3-none-any.whl

---
+++ packages/pyramid-crow/pyramid_crow-0.4.2-py2.py3-none-any.whl/pyramid_crow/__init__.py
@@ -0,0 +1,138 @@
+import sys
+
+from pyramid.tweens import EXCVIEW
+from pyramid.settings import aslist
+from pyramid.settings import asbool
+from pyramid.httpexceptions import WSGIHTTPException
+from pyramid.util import DottedNameResolver
+from pyramid.events import (
+    NewRequest,
+    subscriber,
+)
+
+from raven import Client
+
+import logging
+logger = logging.getLogger(__name__)
+
+resolver = DottedNameResolver(None)
+
+PY3 = sys.version_info[0] == 3
+
+if PY3: # pragma: no cover
+    import builtins
+else:
+    import __builtin__ as builtins
+
+
+def as_globals_list(value):
+    L = []
+    value = aslist(value)
+    for dottedname in value:
+        if dottedname in builtins.__dict__:
+            if PY3: # pragma: no cover
+                dottedname = 'builtins.%s' % dottedname
+            else:
+                dottedname = '__builtin__.%s' % dottedname
+        obj = resolver.resolve(dottedname)
+        L.append(obj)
+    return L
+
+
+def crow_tween_factory(handler, registry):
+
+    get = registry.settings.get
+
+    ignored = get('pyramid_crow.ignore', tuple())
+    if WSGIHTTPException not in ignored:
+        ignored = ignored + (WSGIHTTPException,)
+
+    def crow_tween(request):
+        try:
+            return handler(request)
+        except ignored:
+            raise
+        except:
+            request.raven.captureException()
+            raise
+
+    return crow_tween
+
+
+def _filter_request_body(request):
+    """Ensure we don't send an over-long request body to sentry
+
+    65 KB is probably enough for anybody"""
+    if request.content_length is None:
+        # No content_length - pass through whatever we got.
+        return request.body
+    elif request.content_length >= 2 ** 16:
+        return 'Over-long request body of {} bytes omitted'.format(
+            request.content_length
+        )
+    else:
+        return request.body
+
+
+def _request_to_http_context(request):
+    return {
+        'method': request.method,
+        'url': request.path_url,
+        'query_string': request.query_string,
+        'data': _filter_request_body(request),
+        'headers': dict(request.headers),
+        'env': dict(request.environ),
+    }
+
+
+def _raven_clear_context(r):
+    r.raven.context.clear()
+
+
+def _raven(request):
+    client = request.registry["raven.client"]
+    request.add_finished_callback(_raven_clear_context)
+    return client
+
+
+def raven_client(registry):
+    """Configure a raven client from pyramid config file, add request context
+
+    Uses keys from the pyramid config file beginning with raven. and turns each
+    into a kwarg to raven.Client. Also produces http context from request and
+    adds to client's context"""
+    kwargs = {
+        'processors': (
+            'pyramid_crow.processors.PyramidSanitizePasswordsProcessor',
+        ),
+        'timeout': 4,
+    }
+
+    for key in registry.settings:
+        if key.startswith('raven.'):
+            kwarg = key.partition('.')[2]
+            kwargs[kwarg] = registry.settings[key]
+
+    client = Client(**kwargs)
+    return client
+
+
+@subscriber(NewRequest)
+def add_http_context(event):
+    client = event.request.raven
+    client.http_context(_request_to_http_context(event.request))
+
+
+def includeme(config):
+    get = config.registry.settings.get
+    ignored = as_globals_list(get('pyramid_crow.ignore',
+                              'pyramid.httpexceptions.WSGIHTTPException'))
+
+    config.registry.settings['pyramid_crow.ignore'] = tuple(ignored)
+
+    client = raven_client(config.registry)
+    config.registry['raven.client'] = client
+
+    config.add_request_method(_raven, 'raven', reify=True)
+    config.add_tween('pyramid_crow.crow_tween_factory', under=EXCVIEW)
+    config.scan(ignore='pyramid_crow.tests')
---
+++ packages/pyramid-crow/pyramid_crow-0.4.2-py2.py3-none-any.whl/pyramid_crow/tests.py
@@ -0,0 +1,272 @@
+import unittest
+from pyramid import (
+    testing,
+    httpexceptions,
+)
+from raven.utils import json
+from webtest import TestApp
+import mock
+
+import pyramid_crow
+
+
+class ExpectedException(Exception):
+    pass
+
+
+class IgnoredException(Exception):
+    pass
+
+
+class TestIntegration(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp(
+            settings={
+                'raven.dsn': 'https://foo:bar@example.com/notadsn',
+            },
+        )
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def _makeApp(self):
+        self.config.include('pyramid_crow')
+        app = self.config.make_wsgi_app()
+        return TestApp(app)
+
+    def test_noop(self):
+        config = self.config
+
+        def view(request):
+            return 'ok'
+
+        config.add_view(view, name='', renderer='string')
+
+        app = self._makeApp()
+
+        with mock.patch.object(pyramid_crow.Client,
+                               'captureException') as mock_capture:
+            resp = app.get('/')
+
+        mock_capture.assert_not_called()
+        self.assertEqual(resp.body, b'ok')
+
+    def test_ignored_exception(self):
+        config = self.config
+
+        config.registry.settings['pyramid_crow.ignore'] =\
+            'pyramid_crow.tests.IgnoredException'
+
+        def view(request):
+            raise IgnoredException()
+
+        config.add_view(view, name='', renderer='string')
+
+        app = self._makeApp()
+
+        with mock.patch.object(pyramid_crow.Client,
+                               'captureException') as mock_capture:
+            self.assertRaises(IgnoredException, app.get, '/')
+
+        mock_capture.assert_not_called()
+
+    def test_http_exception(self):
+        config = self.config
+
+        config.registry.settings['pyramid_crow.ignore'] =\
+            'pyramid_crow.tests.IgnoredException'
+
+        def view(request):
+            raise httpexceptions.HTTPFound()
+
+        config.add_view(view, name='', renderer='string')
+
+        app = self._makeApp()
+
+        with mock.patch.object(pyramid_crow.Client,
+                               'captureException') as mock_capture:
+            self.assertRaises(httpexceptions.HTTPFound, app.get, '/')
+
+        mock_capture.assert_not_called()
+
+    def test_capture_called(self):
+        config = self.config
+
+        def view(request):
+            self.request = request
+            raise ExpectedException()
+
+        config.add_view(view, name='', renderer='string')
+
+        app = self._makeApp()
+
+        with mock.patch.object(
+            pyramid_crow.Client, 'captureException',
+        ) as mock_capture, mock.patch(
+            'pyramid_crow._raven_clear_context', new=(lambda r: r),
+        ):
+            self.assertRaises(ExpectedException, app.get, '/')
+
+        mock_capture.assert_called_once()
+        self.assertEqual(
+            self.request.raven.context['request']['method'], 'GET',
+        )
+        self.assertEqual(
+            self.request.raven.context['request']['url'], 'http://localhost/',
+        )
+        self.assertEqual(
+            self.request.raven.context['request']['data'], b'',
+        )
+        self.assertEqual(
+            self.request.raven.context['request']['query_string'], '',
+        )
+        self.assertEqual(
+            self.request.raven.context['request']['headers'],
+            {'Host': 'localhost:80'},
+        )
+
+    def test_capture_query_string(self):
+        config = self.config
+
+        def view(request):
+            self.request = request
+            raise ExpectedException()
+
+        config.add_view(view, name='', renderer='string')
+
+        app = self._makeApp()
+
+        with mock.patch.object(
+            pyramid_crow.Client, 'captureException',
+        ) as mock_capture, mock.patch(
+            'pyramid_crow._raven_clear_context', new=(lambda r: r),
+        ):
+            self.assertRaises(
+                ExpectedException, app.get, '/',
+                params=(('foo', 'bar'), ('baz', 'garply')),
+            )
+
+        mock_capture.assert_called_once()
+        self.assertEqual(
+            self.request.raven.context['request']['method'], 'GET',
+        )
+        self.assertEqual(
+            self.request.raven.context['request']['url'], 'http://localhost/',
+        )
+        self.assertEqual(
+            self.request.raven.context['request']['data'], b'',
+        )
+        self.assertEqual(
+            self.request.raven.context['request']['query_string'],
+            'foo=bar&baz=garply',
+        )
+        self.assertEqual(
+            self.request.raven.context['request']['headers'],
+            {'Host': 'localhost:80'},
+        )
+
+    def test_context_empty(self):
+        config = self.config
+
+        def view(request):
+            self.request = request
+            raise ExpectedException()
+
+        config.add_view(view, name='', renderer='string')
+
+        app = self._makeApp()
+
+        with mock.patch.object(pyramid_crow.Client,
+                               'captureException') as mock_capture:
+            self.assertRaises(ExpectedException, app.get, '/',
+                              params=(('foo', 'bar'), ('baz', 'garply')))
+
+        mock_capture.assert_called_once()
+        self.assertFalse('request' in self.request.raven.context)
+
+    def test_capture_body(self):
+        config = self.config
+
+        def view(request):
+            self.request = request
+            raise ExpectedException()
+
+        config.add_view(view, name='', renderer='string')
+
+        app = self._makeApp()
+
+        with mock.patch.object(
+            pyramid_crow.Client, 'captureException',
+        ) as mock_capture, mock.patch(
+            'pyramid_crow._raven_clear_context', new=(lambda r: r),
+        ):
+            self.assertRaises(
+                ExpectedException, app.post, '/',
+                params=(('foo', 'bar'), ('baz', 'garply')),
+            )
+
+        mock_capture.assert_called_once()
+        self.assertEqual(
+            self.request.raven.context['request']['method'], 'POST',
+        )
+        self.assertEqual(
+            self.request.raven.context['request']['url'], 'http://localhost/',
+        )
+        self.assertEqual(
+            self.request.raven.context['request']['data'],
+            b'foo=bar&baz=garply',
+        )
+        self.assertEqual(
+            self.request.raven.context['request']['query_string'], '',
+        )
+        self.assertEqual(
+            self.request.raven.context['request']['headers'],
+            {
+                'Content-Length': '18',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Host': 'localhost:80',
+            },
+        )
+
+    def test_scrub_sensitive_get(self):
+        """Bad form to submit VIA get but let's not leak everywhere"""
+        config = self.config
+
+        def view(request):
+            self.request = request
+            raise ExpectedException()
+
+        config.add_view(view, name='', renderer='string')
+
+        app = self._makeApp()
+
+        with mock.patch.object(pyramid_crow.Client,
+                               'send') as mock_send:
+            self.assertRaises(ExpectedException, app.get, '/',
+                              params=(('password', 'ohno'),))
+
+        sentry_data = mock_send.call_args[1]
+
+        dumped_data = json.dumps(sentry_data)
+        self.assertNotIn('ohno', dumped_data)
+
+    def test_scrub_sensitive_post(self):
+        config = self.config
+
+        def view(request):
+            self.request = request
+            raise ExpectedException()
+
+        config.add_view(view, name='', renderer='string')
+
+        app = self._makeApp()
+
+        with mock.patch.object(pyramid_crow.Client,
+                               'send') as mock_send:
+            self.assertRaises(ExpectedException, app.post, '/',
+                              params=(('password', 'ohno'),))
+
+        sentry_data = mock_send.call_args[1]
+
+        dumped_data = json.dumps(sentry_data)
+        self.assertNotIn('ohno', dumped_data)
