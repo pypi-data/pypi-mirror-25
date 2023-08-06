caa874130a56cd16b0042164b7f591f8abd5a919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:38 +0000

    Add package multidict/3.2.1a0/multidict-3.2.1a0-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/multidict/multidict-3.2.1a0-cp35-cp35m-manylinux1_x86_64.whl/multidict/__init__.py
@@ -0,0 +1,30 @@
+"""Multidict implementation.
+
+HTTP Headers and URL query string require specific data structure:
+multidict. It behaves mostly like a dict but it can have
+several values for the same key.
+"""
+
+from ._compat import USE_CYTHON_EXTENSIONS
+
+
+__all__ = ('MultiDictProxy', 'CIMultiDictProxy',
+           'MultiDict', 'CIMultiDict', 'upstr', 'istr')
+
+__version__ = '3.2.1a0'
+
+
+try:
+    if not USE_CYTHON_EXTENSIONS:
+        raise ImportError
+    from ._multidict import (MultiDictProxy,
+                             CIMultiDictProxy,
+                             MultiDict,
+                             CIMultiDict,
+                             upstr, istr)
+except ImportError:  # pragma: no cover
+    from ._multidict_py import (MultiDictProxy,
+                                CIMultiDictProxy,
+                                MultiDict,
+                                CIMultiDict,
+                                upstr, istr)
