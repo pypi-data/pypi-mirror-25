113eb3e71f4c3a966637a09983fd2dbd777f6ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:09 +0000

    Add package lollygag/1.0.0rc2/lollygag-1.0.0rc2.tar.gz

---
+++ packages/lollygag/lollygag-1.0.0rc2.tar.gz/lollygag-1.0.0rc2/lollygag/services/__init__.py
@@ -0,0 +1,6 @@
+"""
+This module holds the default services available in lollygag.
+Services and register_services are available on the root level.
+"""
+from lollygag.services.services import Services as Services
+from lollygag.services.services import register_services as register_services
