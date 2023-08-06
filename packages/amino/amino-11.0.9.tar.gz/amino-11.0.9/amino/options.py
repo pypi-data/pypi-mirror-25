824d7f9cec4a1a8910b7da523fd2a73c9239f5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:49 +0000

    Add package amino/11.0.9/amino-11.0.9.tar.gz

---
+++ packages/amino/amino-11.0.9.tar.gz/amino-11.0.9/amino/options.py
@@ -0,0 +1,32 @@
+import os
+
+import amino
+
+
+class EnvOption:
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    @property
+    def exists(self) -> bool:
+        return self.name in os.environ
+
+    def __bool__(self) -> bool:
+        return self.exists
+
+    @property
+    def value(self) -> 'amino.Maybe[str]':
+        return amino.env[self.name]
+
+    def __str__(self) -> str:
+        value = self.value.map(lambda a: f'={a}') | ' is unset'
+        return f'{self.name}{value}'
+
+
+development = EnvOption('AMINO_DEVELOPMENT')
+integration_test = EnvOption('AMINO_INTEGRATION')
+anon_debug = EnvOption('AMINO_ANON_DEBUG')
+io_debug = EnvOption('AMINO_IO_DEBUG')
+
+__all__ = ('development', 'integration_test', 'anon_debug', 'io_debug')
