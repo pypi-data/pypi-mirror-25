edc7a9ebdb386b9efaebb122a9af14a1c450111a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:30 +0000

    Add package samslacker-python/0.0.5/samslacker_python-0.0.5-py2.py3-none-any.whl

---
+++ packages/samslacker-python/samslacker_python-0.0.5-py2.py3-none-any.whl/samslacker/__init__.py
@@ -0,0 +1,22 @@
+from samslacker.resources import Events
+
+api_base = 'https://samplify.io/slacker/api'
+api_version = 'v1'
+token = None
+project = None
+
+
+def event(_name, *args, **kwargs):
+
+    if token is None:
+        raise Exception('Please specify samslacker.token')
+
+    data = {
+        'event': _name,
+        'arguments': kwargs
+    }
+
+    if project is not None:
+        data['project_id'] = project
+
+    return Events.create(data)
