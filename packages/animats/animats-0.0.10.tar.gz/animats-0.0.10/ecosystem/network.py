0affdad8667370c45788f35afbd94748e13f2025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:20 +0000

    Add package animats/0.0.10/animats-0.0.10.tar.gz

---
+++ packages/animats/animats-0.0.10.tar.gz/animats-0.0.10/ecosystem/network.py
@@ -0,0 +1,112 @@
+# pylint: disable=missing-docstring, global-statement, invalid-name
+#
+
+# Imports
+# =======
+
+from gzutils.gzutils import Logging
+
+
+# Setup logging
+# =============
+
+DEBUG_MODE = True
+l = Logging('network', DEBUG_MODE)
+
+#
+# func - the function to apply to the percept:[Things], vars:[any type] -> (boolean, [any type])
+# vars - the initial state the function executes in, necessary for SEQ (and other
+#        function that require a state)
+class Node:
+    # pylint: disable=too-few-public-methods
+    def __init__(self, type_, func, vars_, inputs):
+        self.type_ = type_
+        self.vars_ = vars_
+        self.func = func
+        self.inputs = inputs
+
+    def __call__(self, things):
+        res, self.vars_ = self.func(things, self.vars_)
+        return res
+
+    def __repr__(self):
+        return '(' + self.type_ + ' - vars:' + str(self.vars_) + ',inputs:' + str(self.inputs) + ')'
+
+# Create a sensor that recognise Things of type `cls`, radius is ignored
+def SENSOR_factory(cls):
+    return Node('SENSOR:' + cls.__name__,
+                lambda things, _: (any([isinstance(x, cls) for x, _ in things]), []),
+                [], [])
+
+def AND_factory(indexes, state):
+    return Node('AND', lambda _, _2: (all([state[i] for i in indexes]), []),
+                [], indexes)
+
+def SEQ_factory(idx1, idx2, state):
+    return Node('SEQ',
+                lambda things, vars: (bool(vars[0]) and state[idx2], [state[idx1]]),
+                [None], [idx1, idx2])
+
+#
+# state - a list of booleans consisting of the sensors and other nodes that
+#         have been added to the network (AND, SEQ etc.)
+# nodes - a list of Node objects used to calculate the values for the SENSORS and
+#         other types of nodes (AND, SEQ etc.)
+# root_nodes - the root Nodes in the trees that makes up the network
+#
+class Network:
+
+    # sensors = [('sensor name', Thing to recognise)]
+    def __init__(self, sensors=None):
+        self.state = []
+        self.nodes = []
+        self.root_nodes = []
+        if sensors:
+            self.add_sensors(sensors)
+
+    def add_sensors(self, sensors):
+        for _, cls in sensors:
+            self.add_SENSOR_node(cls)
+
+    def __repr__(self):
+        return ('state:' + str(self.state) +
+                ', nodes:' + str(self.nodes) +
+                ', root_nodes:' + str(self.root_nodes))
+
+    # the state of the network is updated using a depth first search starting
+    # in the root nodes. Nodes might be updated several times, but this will do
+    # for now.
+    def update(self, percept):
+        for node in self.root_nodes:
+            self.update_node(node, percept)
+
+    def update_node(self, node, percept):
+        idx = self.nodes.index(node)
+        for i in node.inputs:
+            self.update_node(self.nodes[i], percept)
+        self.state[idx] = node(percept)
+
+    def get(self):
+        return tuple(self.state)
+
+    def add_root_node(self, node):
+        self.state.append(None)
+        self.nodes.append(node)
+        self.root_nodes.append(node)
+
+    # when other nodes than SENSORs are added are the nodes below no longer
+    # root nodes
+    def delete_root_nodes(self, indexes):
+        for i in indexes:
+            self.root_nodes.remove(self.nodes[i])
+
+    def add_SENSOR_node(self, cls):
+        self.add_root_node(SENSOR_factory(cls))
+
+    def add_AND_node(self, indexes):
+        self.add_root_node(AND_factory(indexes, self.state))
+        self.delete_root_nodes(indexes)
+
+    def add_SEQ_node(self, idx1, idx2):
+        self.add_root_node(SEQ_factory(idx1, idx2, self.state))
+        self.delete_root_nodes([idx1, idx2])
---
+++ packages/animats/animats-0.0.10.tar.gz/animats-0.0.10/test/test_network.py
@@ -0,0 +1,81 @@
+# pylint: disable=missing-docstring, global-statement, invalid-name, too-few-public-methods
+#
+# Copyright (C) 2017  Jonas Colmsjö, Claes Strannegård
+#
+
+
+# Imports
+# ======
+
+import unittest
+from gzutils.gzutils import Logging
+from ecosystem.network import Network
+
+
+# Setup logging
+# =============
+
+DEBUG_MODE = True
+l = Logging('test_network', DEBUG_MODE)
+
+
+class Thing1:
+    pass
+
+class Thing2:
+    pass
+
+class TestNetwork(unittest.TestCase):
+
+    def setUp(self):
+        l.info('Testing network...')
+
+    def tearDown(self):
+        l.info('...done with test_network.')
+
+    def test_SENSOR_AND(self):
+        network = Network()
+        network.add_SENSOR_node(Thing1)
+
+        self.assertTrue(network.get() == (None,))
+
+        network.update([(Thing2(), 1.0)])
+        self.assertTrue(network.get() == (False,))
+
+        network.update([(Thing1(), 1.0)])
+        self.assertTrue(network.get() == (True,))
+
+        network.add_SENSOR_node(Thing2)
+        network.update([(Thing2(), 1.0)])
+        self.assertTrue(network.get() == (False, True, ))
+
+        network.update([])
+        self.assertTrue(network.get() == (False, False, ))
+
+        network.update([(Thing2(), 2.0), (Thing1(), 0.5)])
+        self.assertTrue(network.get() == (True, True, ))
+
+        network.add_AND_node([0, 1])
+        network.update([(Thing2(), 1.0), (Thing1(), 0.3)])
+        self.assertTrue(network.get() == (True, True, True))
+
+        network.update([(Thing2(), 2.0)])
+        self.assertTrue(network.get() == (False, True, False))
+
+    def test_SEQ(self):
+        network = Network([('thing1', Thing1), ('thing2', Thing2)])
+        #network.add_SENSOR_node(Thing1)
+        #network.add_SENSOR_node(Thing2)
+        network.add_SEQ_node(0, 1)
+
+        network.update([(Thing1(), 1.0)])
+        self.assertTrue(network.get() == (True, False, False))
+
+        network.update([(Thing2(), 0.5)])
+        self.assertTrue(network.get() == (False, True, True))
+
+        network.update([(Thing2(), 1.0)])
+        self.assertTrue(network.get() == (False, True, False))
+
+        network.update([])
+        self.assertTrue(network.get() == (False, False, False))
