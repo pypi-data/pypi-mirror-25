b9a86243358d333a4e1ca32c7e38018e26ffff55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:17 +0000

    Add package unpdf/0.0.1/unpdf-0.0.1.tar.gz

---
+++ packages/unpdf/unpdf-0.0.1.tar.gz/unpdf-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='unpdf',
+    version='0.0.1',
+    description='Extract text from PDF.',
+    # url='https://bitbucket.org/luismsgomes/unpdf',
+    author='Luís Gomes',
+    author_email='luismsgomes@gmail.com',
+    license='',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Topic :: Text Processing :: General',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=['unpdf'],
+)
