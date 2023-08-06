737bda639fa89f369386dd3602a7bf39ef968e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:24 +0000

    Add package db_refresh/0.3/db_refresh-0.3.tar.gz

---
+++ packages/db_refresh/db_refresh-0.3.tar.gz/db_refresh-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='db_refresh',
+	version='0.3',
+	description='refresh db',
+	author='EranArtzi',
+	author_email='eartzi@gmail.com',
+	url='https://www.test.com',
+	packages=['db_refresh'],
+	install_requires=['pandas', 'lcconnect', 'note_class', 'db_class'])
