1728eba247ae3a9fb4853606e20e239037b3b0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:29 +0000

    Add package auquan-toolbox/2.0.5/auquan_toolbox-2.0.5-py2-none-any.whl

---
+++ packages/auquan-toolbox/auquan_toolbox-2.0.5-py2-none-any.whl/backtester/features/fees_feature.py
@@ -0,0 +1,21 @@
+from backtester.features.feature import Feature
+import numpy as np
+
+
+class FeesFeature(Feature):
+
+    @classmethod
+    def computeForInstrument(cls, updateNum, time, featureParams, featureKey, instrumentManager):
+        instrumentLookbackData = instrumentManager.getLookbackInstrumentFeatures()
+        positionData = instrumentLookbackData.getFeatureDf('position')
+        feesDict = featureParams['feesDict']
+        currentPosition = positionData.iloc[-1]
+        previousPosition = 0 if updateNum < 2 else positionData.iloc[-2]
+        changeInPosition = currentPosition - previousPosition
+        fees = np.abs(changeInPosition) * [feesDict[np.sign(x)] for x in changeInPosition]
+        return fees
+
+    @classmethod
+    def computeForMarket(cls, updateNum, time, featureParams, featureKey, currentMarketFeatures, instrumentManager):
+        raise NotImplementedError
+        return None
