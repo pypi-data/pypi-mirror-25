49ed1d0c7e7b6d0a2e6291961d86b2b777300183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:48 +0000

    Add package astunparse-noparen/1.5.4/astunparse-noparen-1.5.4.tar.gz

---
+++ packages/astunparse-noparen/astunparse-noparen-1.5.4.tar.gz/astunparse-noparen-1.5.4/lib/astunparse_noparen/__init__.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+from __future__ import absolute_import
+
+from six.moves import cStringIO
+
+from .printer import Printer
+from .unparser import Unparser
+
+__version__ = '1.5.4'
+
+
+def unparse(tree):
+    v = cStringIO()
+    Unparser(tree, file=v)
+    return v.getvalue()
+
+
+def dump(tree):
+    v = cStringIO()
+    Printer(file=v).visit(tree)
+    return v.getvalue()
