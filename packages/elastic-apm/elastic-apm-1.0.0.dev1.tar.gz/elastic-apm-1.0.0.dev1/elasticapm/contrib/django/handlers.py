af83ebb341aef2804371f57ffc1d65aa0bad1f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:44 +0000

    Add package elastic-apm/1.0.0.dev1/elastic-apm-1.0.0.dev1.tar.gz

---
+++ packages/elastic-apm/elastic-apm-1.0.0.dev1.tar.gz/elastic-apm-1.0.0.dev1/elasticapm/contrib/django/handlers.py
@@ -0,0 +1,63 @@
+"""
+elasticapm.contrib.django.handlers
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2011-2017 Elasticsearch
+
+Large portions are
+:copyright: (c) 2010 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import
+
+import logging
+import sys
+import warnings
+
+from django.conf import settings as django_settings
+
+from elasticapm.handlers.logging import LoggingHandler as BaseLoggingHandler
+
+
+class LoggingHandler(BaseLoggingHandler):
+    def __init__(self):
+        logging.Handler.__init__(self)
+
+    def _get_client(self):
+        from elasticapm.contrib.django.client import client
+
+        return client
+
+    client = property(_get_client)
+
+    def _emit(self, record, **kwargs):
+        from elasticapm.contrib.django.middleware import LogMiddleware
+
+        # Fetch the request from a threadlocal variable, if available
+        request = getattr(LogMiddleware.thread, 'request', None)
+        request = getattr(record, 'request', request)
+
+        return super(LoggingHandler, self)._emit(record, request=request, **kwargs)
+
+
+def exception_handler(client, request=None, **kwargs):
+    def actually_do_stuff(request=None, **kwargs):
+        exc_info = sys.exc_info()
+        try:
+            if (django_settings.DEBUG and not client.config.debug) or getattr(exc_info[1], 'skip_elasticapm', False):
+                return
+
+            client.capture('Exception', exc_info=exc_info, request=request)
+        except Exception as exc:
+            try:
+                client.error_logger.exception(u'Unable to process log entry: %s' % (exc,))
+            except Exception as exc:
+                warnings.warn(u'Unable to process log entry: %s' % (exc,))
+        finally:
+            try:
+                del exc_info
+            except Exception as e:
+                client.error_logger.exception(e)
+
+    return actually_do_stuff(request, **kwargs)
