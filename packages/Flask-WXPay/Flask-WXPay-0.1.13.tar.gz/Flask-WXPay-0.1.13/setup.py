f09578eaba1b2bb0bfadeac82598e3fde6f49232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:07 +0000

    Add package Flask-WXPay/0.1.13/Flask-WXPay-0.1.13.tar.gz

---
+++ packages/Flask-WXPay/Flask-WXPay-0.1.13.tar.gz/Flask-WXPay-0.1.13/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import re
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+
+with open('flask_wxpay/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name='Flask-WXPay',
+    version=version,
+    description='Flask Extension for WeChat Pay.',
+    long_description=readme,
+    author='codeif',
+    author_email='me@codeif.com',
+    url='https://github.com/codeif/Flask-WXPay',
+    license='MIT',
+    install_requires=['requests'],
+    packages=find_packages(),
+)
