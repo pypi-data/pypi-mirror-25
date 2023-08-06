e0623474c26438ed8952ff4e5c1857b4ec1ae45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:29 +0000

    Add package sickrage/9.1.27/sickrage-9.1.27-py2.py3-none-any.whl

---
+++ packages/sickrage/sickrage-9.1.27-py2.py3-none-any.whl/sickrage/core/webserver/gui/default/views/home/postprocess.mako
@@ -0,0 +1,88 @@
+<%inherit file="../layouts/main.mako"/>
+<%!
+    import sickrage
+%>
+<%block name="content">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+            <h1 class="title">${title}</h1>
+            <form name="processForm" method="post" action="processEpisode" style="line-height: 40px;">
+                <input type="hidden" id="type" name="proc_type" value="manual">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <b>Enter the folder containing the episode</b>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <div class="input-group input350">
+                            <div class="input-group-addon">
+                                <span class="glyphicon glyphicon-folder-open"></span>
+                            </div>
+                            <input name="proc_dir" id="episodeDir" class="form-control" autocapitalize="off"
+                                   title="directory"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <b>Process Method to be used:</b>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <div class="input-group input350">
+                            <div class="input-group-addon">
+                                <span class="glyphicon glyphicon-refresh"></span>
+                            </div>
+                            <select name="process_method" id="process_method" title="Choose post-processing method"
+                                    class="form-control form-control-inline input-sm">
+                                <% process_method_text = {'copy': "Copy", 'move': "Move", 'hardlink': "Hard Link", 'symlink' : "Symbolic Link",'symlink_reversed' : "Symbolic Link Reversed"} %>
+                                % for curAction in process_method_text:
+                                    <option value="${curAction}" ${('', 'selected="selected"')[sickrage.srCore.srConfig.PROCESS_METHOD == curAction]}>${process_method_text[curAction]}</option>
+                                % endfor
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <b>Force already Post Processed Dir/Files:</b>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <input id="force" name="force" type="checkbox" title="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <b>Mark Dir/Files as priority download:</b>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <input id="is_priority" name="is_priority" type="checkbox" title="">
+                        <span style="line-height: 0; font-size: 12px;"><i>&nbsp;(Check it to replace the file even if it exists at higher quality)</i></span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <b>Delete files and folders:</b>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <input id="delete_on" name="delete_on" type="checkbox" title="">
+                        <span style="line-height: 0; font-size: 12px;"><i>&nbsp;(Check it to delete files and folders like auto processing)</i></span>
+                    </div>
+                </div>
+                % if sickrage.srCore.srConfig.USE_FAILED_DOWNLOADS:
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <b>Mark download as failed:</b>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <input id="failed" name="failed" type="checkbox" title="">
+                        </div>
+                    </div>
+                % endif
+                <div class="row">
+                    <div class="col-md-12">
+                        <input id="submit" class="btn" type="submit" value="Process"/>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</%block>
---
+++ packages/sickrage/sickrage-9.1.27-py2.py3-none-any.whl/sickrage/providers/torrent/cpasbien.py
@@ -0,0 +1,126 @@
+# Author: echel0n <echel0n@sickrage.ca>
+# URL: https://sickrage.ca
+#
+# This file is part of SickRage.
+#
+# SickRage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SickRage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import traceback
+
+import sickrage
+from sickrage.core.caches.tv_cache import TVCache
+from sickrage.core.helpers import bs4_parser
+from sickrage.providers import TorrentProvider
+
+
+class CpasbienProvider(TorrentProvider):
+    def __init__(self):
+        super(CpasbienProvider, self).__init__("Cpasbien", "http://www.cpasbien.io", False)
+
+        self.supports_backlog = True
+
+        self.ratio = None
+        self.urls.update({
+            'download': '{base_url}/telechargement/%s'.format(base_url=self.urls['base_url'])
+        })
+
+        self.proper_strings = ['PROPER', 'REPACK']
+
+        self.cache = TVCache(self, min_time=30)
+
+    def search(self, search_params, search_mode='eponly', epcount=0, age=0, epObj=None):
+
+        size = -1
+        seeders = 1
+        leechers = 0
+
+        results = []
+        items = {'Season': [], 'Episode': [], 'RSS': []}
+
+        for mode in search_params.keys():
+            sickrage.srCore.srLogger.debug("Search Mode: %s" % mode)
+            for search_string in search_params[mode]:
+
+                if mode != 'RSS':
+                    sickrage.srCore.srLogger.debug("Search string: %s " % search_string)
+
+                searchURL = self.urls['base_url'] + '/recherche/' + search_string.replace('.', '-') + '.html'
+                sickrage.srCore.srLogger.debug("Search URL: %s" % searchURL)
+
+                try:
+                    data = sickrage.srCore.srWebSession.get(searchURL, cache=False).text
+                except Exception:
+                    sickrage.srCore.srLogger.debug("No data returned from provider")
+                    continue
+
+                try:
+                    with bs4_parser(data) as html:
+                        lin = erlin = 0
+                        resultdiv = []
+                        while erlin == 0:
+                            try:
+                                classlin = 'ligne' + str(lin)
+                                resultlin = html.findAll(attrs={'class': [classlin]})
+                                if resultlin:
+                                    for ele in resultlin:
+                                        resultdiv.append(ele)
+                                    lin += 1
+                                else:
+                                    erlin = 1
+                            except Exception:
+                                erlin = 1
+
+                        for row in resultdiv:
+                            try:
+                                link = row.find("a", title=True)
+                                title = link.text.lower().strip()
+                                pageURL = link['href']
+
+                                # downloadTorrentLink = torrentSoup.find("a", title.startswith('Cliquer'))
+                                tmp = pageURL.split('/')[-1].replace('.html', '.torrent')
+
+                                downloadTorrentLink = (self.urls['download'] % tmp)
+
+                                if downloadTorrentLink:
+                                    download_url = downloadTorrentLink
+                                    size = -1
+                                    seeders = 1
+                                    leechers = 0
+
+                            except (AttributeError, TypeError):
+                                continue
+
+                            if not all([title, download_url]):
+                                continue
+
+                            item = title, download_url, size, seeders, leechers
+                            if mode != 'RSS':
+                                sickrage.srCore.srLogger.debug("Found result: %s " % title)
+
+                            items[mode].append(item)
+
+                except Exception as e:
+                    sickrage.srCore.srLogger.error("Failed parsing provider. Traceback: %s" % traceback.format_exc())
+
+            # For each search mode sort all the items by seeders if available
+            items[mode].sort(key=lambda tup: tup[3], reverse=True)
+
+            results += items[mode]
+
+        return results
+
+    def seed_ratio(self):
+        return self.ratio
