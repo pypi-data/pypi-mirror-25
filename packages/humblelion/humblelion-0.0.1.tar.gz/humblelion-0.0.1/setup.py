9b2ef666851752d4b66279bb838d39a9f7d972b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:43 +0000

    Add package humblelion/0.0.1/humblelion-0.0.1.tar.gz

---
+++ packages/humblelion/humblelion-0.0.1.tar.gz/humblelion-0.0.1/setup.py
@@ -0,0 +1,44 @@
+import sys
+
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('requirements.txt', 'r') as dependencies:
+    install_requires = [line.strip().replace('\n', '') for line in dependencies.readlines()]
+    print(install_requires)
+
+if sys.platform.startswith('linux'):
+    install_requires += [
+        'dbus-python',   # requires libdbus-glib-1-dev
+        'secretstorage',
+    ]
+setup(
+    name='humblelion',
+    version='0.0.1',
+    url='https://github.com/MestreLion/humblebundle',
+    description='An unofficial Humble Bundle API for listing and downloading games and bundles',
+    packages=find_packages(exclude=['tests']),
+    setup_requires=['setuptools-git'],
+    author='Rodrigo Silva',
+    author_email='linux@rodrigosilva.com',
+    license='GPLv3+',
+    keywords='api',
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'humblelion = humblelion:cli',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Games/Entertainment',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
+
