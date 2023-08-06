06ef0f74165ae31aef7a016f702029473d9ce200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:08 +0000

    Add package fibu-slechler/1.1.1/fibu_slechler-1.1.1.tar.gz

---
+++ packages/fibu-slechler/fibu_slechler-1.1.1.tar.gz/fibu_slechler-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name = 'fibu_slechler',
+	version = '1.1.1',
+	py_modules = ['fibu_slechler'],
+	author = 'lecsim',
+	author_email = 'lechler@gmx.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'fibutest',
+	)
