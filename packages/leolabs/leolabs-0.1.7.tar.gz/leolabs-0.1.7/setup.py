edde90761e7e3ce854e2f3a6059f7391f92a89f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:16 +0000

    Add package leolabs/0.1.7/leolabs-0.1.7.tar.gz

---
+++ packages/leolabs/leolabs-0.1.7.tar.gz/leolabs-0.1.7/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'leolabs',
+  packages = ['leolabs'],
+  version = '0.1.7',
+  description = 'LeoLabs Api',
+  author = 'LeoLabs, Inc.',
+  author_email = 'support@leolabs.space',
+  url = 'https://github.com/leolabs-space/leo-api-python',
+  keywords = ['leolabs', 'radar', 'space', 'leo', 'orbit', 'propagation', 'norad'],
+  classifiers = [],
+  install_requires = ['requests'],
+  entry_points = {
+    'console_scripts': [
+      'leolabs=leolabs.bin.cli:main'
+    ]
+  }
+)
