164e5cc3a81307506c57f1eb5b9097e24216dcdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:11 +0000

    Add package rtfd-cli/0.1.2/rtfd-cli-0.1.2.tar.gz

---
+++ packages/rtfd-cli/rtfd-cli-0.1.2.tar.gz/rtfd-cli-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import rtfd
+
+setup(name='rtfd-cli',
+      version='0.1.2',
+      description='CLI to download docs from ReadTheDocs.org',
+      author='Nitanshu Vashistha',
+      author_email='nitanshu.vzard@gmail.com',
+      packages = find_packages(),
+      entry_points={
+            'console_scripts': [
+                  'rtfd-cli = rtfd.rtfd:command_line',
+            ]
+      },
+      url='https://github.com/MUSoC/rtfd-cli',
+      keywords=['readthedocs', 'terminal', 'command-line', 'rtfd', 'python'],
+      license='MIT',
+      downlaod_url='https://github.com/nvzard/rtfd-cli/archive/0.1.2.tar.gz',
+      classifiers=[],
+      install_requires=[
+            'requests',
+            'BeautifulSoup4',
+            'tqdm',
+            'colorama'
+      ]
+     )
