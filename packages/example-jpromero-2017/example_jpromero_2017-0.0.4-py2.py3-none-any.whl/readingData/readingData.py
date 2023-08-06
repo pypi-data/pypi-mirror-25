9bd899708f22d4968d55eb6fdeee35e8089da640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:39 +0000

    Add package example-jpromero-2017/0.0.4/example_jpromero_2017-0.0.4-py2.py3-none-any.whl

---
+++ packages/example-jpromero-2017/example_jpromero_2017-0.0.4-py2.py3-none-any.whl/readingData/readingData.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from os import path
+class ReadCSV(object):
+    def __init__(self):
+        here = path.dirname(path.dirname(path.abspath(__file__)))
+        pathFile = path.join(here,"train.csv")
+        print pathFile
+        self.df = pd.read_csv(pathFile)
+
+    def making_print(self):
+        print self.df
