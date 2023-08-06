5b9fca7ede26a229fa42c00af8bf740347ca771e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:26 +0000

    Add package senaite.api/1.0.1/senaite.api-1.0.1.zip

---
+++ packages/senaite.api/senaite.api-1.0.1.zip/senaite.api-1.0.1/setup.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = '1.0.1'
+
+
+setup(
+    name='senaite.api',
+    version=version,
+    description="SENAITE API",
+    long_description=open("docs/About.rst").read() +
+                     "\n\n" +
+                     open("src/senaite/api/docs/API.rst").read() +
+                     "\n\n" +
+                     "Changelog\n" +
+                     "=========\n" +
+                     open("docs/Changelog.rst").read() +
+                     "\n\n" +
+                     "Authors and maintainers\n" +
+                     "=======================\n\n" +
+                     "- Ramon Bartl (RIDING BYTES) <rb@ridingbytes.com>\n",
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+    ],
+    keywords='',
+    author='SENAITE Foundation',
+    author_email='hello@senaite.com',
+    url='https://github.com/senaite/senaite.api',
+    license='GPLv3',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    namespace_packages=['senaite'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.api',
+        'bika.lims>=3.2.1rc2',
+    ],
+    extras_require={
+        'test': [
+            'Products.PloneTestCase',
+            'Products.SecureMailHost',
+            'plone.app.robotframework',
+            'plone.app.testing',
+            'robotframework-debuglibrary',
+            'robotframework-selenium2library',
+            'robotsuite',
+            'unittest2',
+        ]
+    },
+    entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
