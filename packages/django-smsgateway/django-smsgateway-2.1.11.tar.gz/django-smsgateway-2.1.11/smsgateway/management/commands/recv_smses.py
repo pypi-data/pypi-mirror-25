0f53a22127fdcee2ad5d4a87bf54d00bcb4326dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:37 +0000

    Add package django-smsgateway/2.1.11/django-smsgateway-2.1.11.tar.gz

---
+++ packages/django-smsgateway/django-smsgateway-2.1.11.tar.gz/django-smsgateway-2.1.11/smsgateway/management/commands/recv_smses.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from django.core.management.base import NoArgsCommand
+from optparse import make_option
+from smsgateway.tasks import recv_smses
+
+
+class Command(NoArgsCommand):
+    help = 'Receive SMSes from the Redis queue.'
+    option_list = NoArgsCommand.option_list + (
+            make_option('--backend', help='Slug of the SMS backend to read from'),
+            make_option('--async',
+                        action='store_true',
+                        dest='async',
+                        default=False,
+                        help='Process the SMSes via asynchronously via Celery')
+            )
+
+    def handle_noargs(self, **options):
+        recv_smses(options['backend'], options['async'])
