e5895c6eb202244b5f44e5147f39deffc01eb8a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:15 +0000

    Add package futoin-cid/0.7.9994/futoin-cid-0.7.9994.tar.gz

---
+++ packages/futoin-cid/futoin-cid-0.7.9994.tar.gz/futoin-cid-0.7.9994/futoin/cid/tool/pumatool.py
@@ -0,0 +1,62 @@
+
+from ..runtimetool import RuntimeTool
+from .gemtoolmixin import GemToolMixIn
+
+
+class pumaTool(GemToolMixIn, RuntimeTool):
+    """A ruby web server built for concurrency http://puma.io
+"""
+    __slots__ = ()
+
+    def _sigReload(self):
+        return self._ext.signal.SIGUSR2
+
+    def tuneDefaults(self, env):
+        return {
+            'minMemory': '128M',
+            'connMemory': '8M',
+            'connFD': 8,
+            'socketTypes': ['unix', 'tcp'],
+            'socketType': 'unix',
+            'socketProtocol': 'http',
+            'scalable': True,
+            'reloadable': True,
+            'multiCore': False,  # use workers on service level
+            'maxRequestSize': '1M',
+        }
+
+    def onRun(self, config, svc, args):
+        svc_tune = svc['tune']
+
+        #---
+
+        if svc_tune['socketType'] == 'unix':
+            socket = 'unix://{0}'.format(svc_tune['socketPath'])
+        else:
+            socket = 'tcp://{0}:{1}'.format(
+                svc_tune['socketAddress'], svc_tune['socketPort'])
+
+        #---
+        resource = self._ext.resource
+        heap_limit = self._configutil.parseMemory(svc_tune['maxMemory'])
+        # both limit RAM and HEAP (not the same)
+        resource.setrlimit(resource.RLIMIT_RSS, (heap_limit, heap_limit))
+        resource.setrlimit(resource.RLIMIT_DATA, (heap_limit, heap_limit))
+
+        #---
+        threads = svc_tune['maxConnections']
+
+        puma_args = [
+            '-b', socket,
+            '-e', self._environ['RUBY_ENV'],
+            '-t', '{0}:{0}'.format(threads)
+        ]
+
+        #---
+
+        cmd = [
+            config['env']['pumaBin'],
+            svc['path']
+        ] + puma_args + args
+
+        self._executil.callInteractive(cmd)
