60a1beb8794c19e77748e55d0158860134676d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:40 +0000

    Add package uncompyle6/2.12.0/uncompyle6-2.12.0.tar.gz

---
+++ packages/uncompyle6/uncompyle6-2.12.0.tar.gz/uncompyle6-2.12.0/test/simple-uncompyle-code-test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from uncompyle6 import uncompyle
+import sys, inspect
+
+def uncompyle_test():
+    frame = inspect.currentframe()
+    try:
+        co = frame.f_code
+        uncompyle(2.7, co, sys.stdout, 1, 1)
+        print()
+    finally:
+        del frame
+
+uncompyle_test()
