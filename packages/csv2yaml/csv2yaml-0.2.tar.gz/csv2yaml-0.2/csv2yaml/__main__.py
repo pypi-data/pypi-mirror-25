7026decc8e495dce559c2e4622f64bb9d66b831d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:07 +0000

    Add package csv2yaml/0.2/csv2yaml-0.2.tar.gz

---
+++ packages/csv2yaml/csv2yaml-0.2.tar.gz/csv2yaml-0.2/csv2yaml/__main__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from .csv2yaml import *
+import time
+import sys
+import doctest
+import os
+def run(filename,header=None,error_pass=False):
+    first_time = time.perf_counter()
+    print("Converting ... ")
+    json_size=json_convert(filename,header=header,error_pass=error_pass)
+    if json_size!=None:
+        pickle_size=json_to_pickle(filename,error_pass=error_pass)
+        yaml_size=json_to_yaml(filename,error_pass=error_pass)
+        print(json_size)
+        print(pickle_size)
+        print(yaml_size)
+    second_time = time.perf_counter()
+    elapsed_time = second_time - first_time
+    elapsed_time_format = time_convert(str(elapsed_time))
+    print("Converted In " + elapsed_time_format)
+    print("Where --> " + Source_dir)
+    line()
+if __name__=="__main__":
+    args=sys.argv
+    if len(args)>1:
+        if args[1].upper()=="TEST":
+            doctest.testfile("test.py",verbose=True)
+        elif args[1].upper()=="ALL":
+            file_header=None
+            if len(args)>2:
+                file_header=args[2]
+            for file in os.listdir():
+                if file.find(".csv")!=-1:
+                    run(filename=file,header=file_header,error_pass=True)
+        elif args[1].upper()=="HELP":
+            help_func()
+        elif len(args)>2:
+            run(args[1],header=args[2])
+        else:
+            run(args[1])
+    else:
+        help_func()
+        sys.exit()
---
+++ packages/csv2yaml/csv2yaml-0.2.tar.gz/csv2yaml-0.2/csv2yaml/csv2yaml.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+import os
+import yaml
+import pickle
+import json
+import sys
+from art import *
+Source_dir=os.getcwd()
+version="0.2"
+def help_func():
+    '''
+    Print Help Page
+    :return: None
+    '''
+    tprint("csv")
+    tprint("v"+version)
+    print("Help : \n")
+    print("     - filename.csv header(optional) Example : 'python -m csv2yaml test.csv header_1'\n")
+    print("     - test (run tests)\n")
+    print("     - all  'Example : 'python -m csv2yaml all header_2'\n")
+    print("     - help (help page)'\n")
+
+
+def line(char="*",number=30):
+    print(char*number)
+
+def zero_insert(input_string):
+    '''
+    This function get a string as input if input is one digit add a zero
+    :param input_string: input digit az string
+    :type input_string:str
+    :return: modified output as str
+    >>> zero_insert("22")
+    >>> '22'
+    '''
+    if len(input_string)==1:
+        return "0"+input_string
+    return input_string
+
+
+
+def convert_bytes(num):
+    """
+    convert num to idiomatic byte unit
+    :param num: the input number.
+    :type num:int
+    :return: str
+    """
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+
+def filesize(fileaddr,fileformat):
+    '''
+    This function calculate output file size
+    :param fileaddr: file addresses
+    :type fileaddr:str
+    :return: file size for print as string
+    '''
+    try:
+        file_info=os.stat(fileaddr+"."+fileformat)
+        file_size= file_info.st_size
+        return fileformat.upper()+" File Size : "+convert_bytes(file_size)
+    except Exception:
+        return None
+
+def time_convert(input_string):
+    '''
+    This function convert input_string from sec to DD,HH,MM,SS Format
+    :param input_string: input time string  in sec
+    :type input_string:str
+    :return: converted time as string
+    '''
+    input_sec=float(input_string)
+    input_minute=input_sec//60
+    input_sec=int(input_sec-input_minute*60)
+    input_hour=input_minute//60
+    input_minute=int(input_minute-input_hour*60)
+    input_day=int(input_hour//24)
+    input_hour=int(input_hour-input_day*24)
+    return zero_insert(str(input_day))+" days, "+zero_insert(str(input_hour))+" hour, "+zero_insert(str(input_minute))+" minutes, "+zero_insert(str(input_sec))+" seconds"
+
+def json_convert(file_name,header=None,error_pass=False):
+    '''
+    This function create output file in json format
+    :param file_name: file name
+    :type file_name:str
+    :return: filesize as str
+    '''
+    try:
+        file_name=file_name.split(".")[0]
+        if header==None:
+            header=file_name
+        csv_file=open(file_name+".csv","r")
+        json_file = open(file_name + ".json", "w")
+        csv_lines=csv_file.readlines()
+        data='\t\t\t"data":[\n'
+        first_line=csv_lines[0].split(",")
+        first_line[-1]=first_line[-1][:-1]
+        for index in range(1,len(csv_lines)):
+            data = data + '\t\t\t{'
+            splited_data=csv_lines[index].split(",")
+            data = data + '\n\t\t\t\t' + '"id"' + ':' + '"' + str(index) + '",'
+            for i in range(len(first_line)):
+                line_data=splited_data[i].replace("\n","")
+                line_data=line_data.replace('"','')
+                data=data+'\n\t\t\t\t'+'"'+first_line[i]+'"'+':'+'"'+line_data+'",'
+            data=data[:-1]+"\n\t\t\t},\n"
+        data=data[:-2]+"\n\t\t]\n\t}\n}"
+        json_file.write('{\n\t'+'"'+header+'"'+': {\n')
+        json_file.write(data)
+        json_file.close()
+        csv_file.close()
+        return filesize(file_name,"json")
+    except Exception:
+        print("[Error] Json Conversion Faild!")
+        if error_pass==False:
+            sys.exit()
+
+def json_to_yaml(filename,error_pass=False):
+    '''
+    This function convert json file to yaml file
+    :param filename: filename
+    :type filename: str
+    :return: filesize as str
+    '''
+    try:
+        filename = filename.split(".")[0]
+        file=open(filename+".json","r")
+        json_data=json.loads(file.read())
+        yaml_file = open(filename + ".yaml", "w")
+        yaml.safe_dump(json_data,yaml_file,default_flow_style=False)
+        file.close()
+        yaml_file.close()
+        return filesize(filename, "yaml")
+    except FileNotFoundError:
+        print("[Error] YAML Conversion Faild")
+        if error_pass==False:
+            sys.exit()
+
+
+def json_to_pickle(filename,error_pass=False):
+    '''
+    This function convert json file to yaml file
+    :param filename: filename
+    :type filename: str
+    :return: filesize as str
+    '''
+    try:
+        filename = filename.split(".")[0]
+        file=open(filename+".json","r")
+        pickle_file=open(filename+".p","wb")
+        json_data=json.loads(file.read())
+        pickle.dump(json_data,pickle_file)
+        pickle_file.close()
+        file.close()
+        return filesize(filename, "p")
+    except FileNotFoundError:
+        print("[Error] Pickle Conversion Faild")
+        if error_pass==False:
+            sys.exit()
+
+
+
+
+
+
