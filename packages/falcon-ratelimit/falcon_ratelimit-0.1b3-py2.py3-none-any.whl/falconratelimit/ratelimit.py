2672cc30e9a6348d3da3ffec898e8d94462e0a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:40 +0000

    Add package falcon-ratelimit/0.1b3/falcon_ratelimit-0.1b3-py2.py3-none-any.whl

---
+++ packages/falcon-ratelimit/falcon_ratelimit-0.1b3-py2.py3-none-any.whl/falconratelimit/ratelimit.py
@@ -0,0 +1,46 @@
+# coding=UTF-8
+from __future__ import print_function, absolute_import, division
+import six
+import logging
+import functools
+import collections
+import time
+import falcon
+
+logger = logging.getLogger(__name__)
+
+
+class _RateLimitDB(object):
+    _RATE_LIMIT_DB = collections.defaultdict(
+        lambda: collections.defaultdict(list)
+    )
+
+    @staticmethod
+    def filter(user, resource_name, window_size):
+        p = _RateLimitDB._RATE_LIMIT_DB[user][resource_name]
+        t = time.time()
+        exp_int = t - window_size
+        p = [s for s in p if s >= exp_int]
+        _RateLimitDB._RATE_LIMIT_DB[user][resource_name] = p
+
+    @staticmethod
+    def add_call(user, resource_name):
+        _RateLimitDB._RATE_LIMIT_DB[user][resource_name].append(
+            time.time()
+        )
+
+    @staticmethod
+    def check_for(user, resource_name, window_size):
+        _RateLimitDB.filter(user, resource_name, window_size)
+        _RateLimitDB.add_call(user, resource_name)
+        p = len(_RateLimitDB._RATE_LIMIT_DB[user][resource_name])
+        return p / window_size
+
+
+def rate_limit(per_second=30, resource=u'', window_size=10):
+    def hook(req, resp, resource, params):
+        if _RateLimitDB.check_for(req.forwarded_host,
+                                  resource,
+                                  window_size) > per_second:
+            raise falcon.HTTPTooManyRequests('Rate limited')
+    return hook
