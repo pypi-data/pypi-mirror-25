24a05fbce7b8a8044c9dabbe7d2a650b604a1c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:59 +0000

    Add package pr-nester17/1.3.0/pr-nester17-1.3.0.tar.gz

---
+++ packages/pr-nester17/pr-nester17-1.3.0.tar.gz/pr-nester17-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name          = 'pr-nester17',
+        version       = '1.3.0',
+        py_modules    = ['pr-nester17'],
+        author        = 'Garry Meng',
+        author_email  = 'menggaoling@163.com',
+        url           = 'http://blog.csdn.net/garrymeng',
+        description   = 'A module just for test upload function',
+    )
