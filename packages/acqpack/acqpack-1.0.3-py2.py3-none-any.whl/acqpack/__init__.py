d45f949d24ad54fe412f43f7229dcd8988fc1321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:24 +0000

    Add package acqpack/1.0.3/acqpack-1.0.3-py2.py3-none-any.whl

---
+++ packages/acqpack/acqpack-1.0.3-py2.py3-none-any.whl/acqpack/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for acqpack."""
+
+__author__ = """Scott Longwell"""
+__email__ = 'longwell@stanford.edu'
+__version__ = '1.0.3'
+
+import gui
+import utils
+from asicontroller import AsiController
+from autosampler import Autosampler
+from fractioncollector import FractionCollector
+from manifold import Manifold
+from mfcs import Mfcs
+from motor import Motor
+
+# TODO: packaging https://python-packaging.readthedocs.io/en/latest/non-code-files.html
