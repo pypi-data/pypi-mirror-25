ba979b8283af2abe4b755fd257ac5fe0b388070c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:15 +0000

    Add package networks/0.3/networks-0.3.tar.gz

---
+++ packages/networks/networks-0.3.tar.gz/networks-0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'networks',
+    packages = ['networks'],
+    version = '0.3',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Allows to create NN models',
+    author = 'Shharrnam Chhatpar',
+    author_email = 'sharnam19.nc@gmail.com',
+    url = 'https://github.com/sharnam19/NN-Library',
+    download_url = 'https://github.com/sharnam19/NN-Library/archive/0.3.tar.gz',
+    keywords = ['machine-learning', 'deep-learning','neural-networks','linear regression',
+                'logistic regression'],
+    classifiers = [],
+)
