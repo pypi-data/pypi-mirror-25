6f572457b54f5c0491d0dc31ef9e991d92a0fca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:01 +0000

    Add package ilinq/0.1.3/ilinq-0.1.3.tar.gz

---
+++ packages/ilinq/ilinq-0.1.3.tar.gz/ilinq-0.1.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from ilinq import __VERSION__
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Intended Audience :: Developers",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Utilities",
+]
+
+setup(
+    name='ilinq',
+    version=__VERSION__,
+    description='linq library',
+    long_description=open('Readme.rst').read(),
+    author='yassu',
+    author_email='yasu0320.dev@gmail.com',
+    classifiers=classifiers,
+    url='https://github.com/yassu/ilinq.py',
+  )
