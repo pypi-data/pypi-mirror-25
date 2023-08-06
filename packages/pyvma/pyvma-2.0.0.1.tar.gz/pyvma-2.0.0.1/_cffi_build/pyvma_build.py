f1d0024e2f869580226134a6872e3a770a7dc85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:06 +0000

    Add package pyvma/2.0.0.1/pyvma-2.0.0.1.tar.gz

---
+++ packages/pyvma/pyvma-2.0.0.1.tar.gz/pyvma-2.0.0.1/_cffi_build/pyvma_build.py
@@ -0,0 +1,38 @@
+from cffi import FFI
+from os import path
+import platform
+
+
+HERE = path.dirname(path.realpath(__file__))
+INCLUDE_FOLDER = path.join(HERE, 'include')
+LINUX = platform.system() == 'Linux'
+
+# ----------
+# BUILD WRAPPER
+# ----------
+ffibuilder = FFI()
+
+# prepare cdef
+cdef = open(path.join(HERE, 'cdef', 'vulkan.cdef.h')).read()
+cdef += '\n'
+cdef += open(path.join(HERE, 'cdef', 'vk_mem_alloc.cdef.h')).read()
+ffibuilder.cdef(cdef)
+
+# prepare libraries
+l = ['vk_mem_alloc']
+if LINUX:
+    l += ['stdc++']
+
+# prepare source
+ffibuilder.set_source(
+    '_pyvma',
+    open(path.join(HERE, 'vk_mem_alloc.h')).read(),
+    libraries=l,
+    library_dirs=[HERE],
+    extra_compile_args=["-I"+INCLUDE_FOLDER]
+)
+
+
+if __name__ == '__main__':
+    ffibuilder.compile(verbose=True)
+
