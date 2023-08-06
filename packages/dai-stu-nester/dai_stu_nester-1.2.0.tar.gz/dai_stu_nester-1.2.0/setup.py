b219047591fa63ac743d94424fee6f7ad486f7cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:10 +0000

    Add package dai-stu-nester/1.2.0/dai_stu_nester-1.2.0.tar.gz

---
+++ packages/dai-stu-nester/dai_stu_nester-1.2.0.tar.gz/dai_stu_nester-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name           = 'dai_stu_nester',
+    version        = '1.2.0',
+    py_modules     = ['dai_stu_nester'],
+    author         = 'daixiangdong',
+    author_email   = 'daixiangdong@139.com',
+    url            = 'http://www.headfirstlabs.com',
+    description    = 'A simple printer of nested lists',
+    )
