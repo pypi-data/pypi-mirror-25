e66150c6c20e3d0469e865de1f64f8b284d5a464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:49 +0000

    Add package flumine/0.2.3/flumine-0.2.3.tar.gz

---
+++ packages/flumine/flumine-0.2.3.tar.gz/flumine-0.2.3/flumine/exceptions.py
@@ -0,0 +1,19 @@
+
+
+class FlumineException(Exception):
+    """Base class for Flumine Errors"""
+    pass
+
+
+class RunError(FlumineException):
+    """Error raised if start() called when already running"""
+
+    def __init__(self, message):
+        super(RunError, self).__init__(message)
+
+
+class ListenerError(FlumineException):
+    """Error raised if error in Listener"""
+
+    def __int__(self, message):
+        super(ListenerError, self).__init__(message)
