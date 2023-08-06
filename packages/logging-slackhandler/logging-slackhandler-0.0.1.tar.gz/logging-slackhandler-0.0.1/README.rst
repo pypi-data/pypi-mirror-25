d08564c8bc12b4c789dd1306ecde8261e43c833e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:35 +0000

    Add package logging-slackhandler/0.0.1/logging-slackhandler-0.0.1.tar.gz

---
+++ packages/logging-slackhandler/logging-slackhandler-0.0.1.tar.gz/logging-slackhandler-0.0.1/README.rst
@@ -0,0 +1,10 @@
+logging-slackhandler
+====================
+
+Python logging handler for Slack web hook integration with simple configuration.
+
+Installation
+------------
+.. code-block:: bash
+
+    pip install logging-slackhandler
---
+++ packages/logging-slackhandler/logging-slackhandler-0.0.1.tar.gz/logging-slackhandler-0.0.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='logging-slackhandler',
+
+    version='0.0.1',
+
+    description='A logging handler for Slack',
+    long_description=long_description,
+
+    url='https://github.com/Greums/logging-slackhandler',
+
+    author='Damien Le Bourdonnec',
+    author_email='damien.lebourdonnec@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    keywords='sample setuptools development',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=['requests']
+)
