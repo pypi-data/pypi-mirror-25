f2033b8a875fb5307de840fce7bf2b3bafc19681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:20 +0000

    Add package OGN-lib/0.1.0/OGN%20lib-0.1.0.tar.gz

---
+++ packages/OGN-lib/OGN%20lib-0.1.0.tar.gz/OGN lib-0.1.0/README.md
@@ -0,0 +1,4 @@
+# OGN Lib 
+[![Build Status](https://travis-ci.org/akolar/ogn-lib.svg?branch=master)](https://travis-ci.org/akolar/ogn-lib)
+[![codecov](https://codecov.io/gh/akolar/ogn-lib/branch/master/graph/badge.svg)](https://codecov.io/gh/akolar/ogn-lib)
+
---
+++ packages/OGN-lib/OGN%20lib-0.1.0.tar.gz/OGN lib-0.1.0/ogn_lib/__init__.py
@@ -0,0 +1,10 @@
+from ogn_lib.client import OgnClient  # noqa: F401
+from ogn_lib.parser import Parser  # noqa: F401
+
+__title__ = 'ogn-lib'
+__description__ = 'Beacon processor for the OGN data stream.'
+__version__ = '0.1.0'
+__author__ = 'Anze Kolar'
+__author_email__ = 'me@akolar.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Anze Kolar'
