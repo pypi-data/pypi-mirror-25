9824d5fc59c446510f93e416f09ad059e2f3005c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:01 +0000

    Add package firas/1.0.0/firas-1.0.0.tar.gz

---
+++ packages/firas/firas-1.0.0.tar.gz/firas-1.0.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='firas',
+      version='1.0.0',
+      description='Firas Test PyPI',
+      author='Firas Kassim',
+      author_email='kassim.firas@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+     )
