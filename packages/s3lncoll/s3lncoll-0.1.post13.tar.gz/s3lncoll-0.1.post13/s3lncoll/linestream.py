f2a8dfc3631991bc19abc419e7183d04091042b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:19 +0000

    Add package s3lncoll/0.1.post13/s3lncoll-0.1.post13.tar.gz

---
+++ packages/s3lncoll/s3lncoll-0.1.post13.tar.gz/s3lncoll-0.1.post13/s3lncoll/linestream.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import json, logtool, tempfile
+
+@logtool.log_call
+def linestream (keylist, cb = None, validate = False):
+  keylist = keylist
+  for ndx, key in enumerate (keylist):
+    if cb is not None:
+      cb (ndx)
+    with tempfile.NamedTemporaryFile (prefix = "s3lncoll_in__") as f:
+      f.write (key.get ()["Body"].read ())
+      f.seek (0)
+      for line in file (f.name): # pylint: disable=not-an-iterable
+        try:
+          if validate:
+            json.loads (line)
+          yield line
+        except ValueError:
+          continue
+  raise StopIteration
