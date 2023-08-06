a159b9e9879024a96a01ea640f930edd2a9014e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:07 +0000

    Add package xinjingru/1.2.0/xinjingru-1.2.0.zip

---
+++ packages/xinjingru/xinjingru-1.2.0.zip/xinjingru-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name          ='xinjingru',
+    version       ='1.2.0',
+    py_modules    =['nester'],
+    author        ='xinjingru',
+    author_email  ='1273414264@qq.com',
+    url           ='http://www.34miao.com',
+    description   ='A simple printer of nested lists',
+    )
