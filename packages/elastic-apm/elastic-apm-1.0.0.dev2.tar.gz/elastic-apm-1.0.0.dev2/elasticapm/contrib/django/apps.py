f9015e1dfbd2e875250e5e0864adc4b66b4a9420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:33 +0000

    Add package elastic-apm/1.0.0.dev2/elastic-apm-1.0.0.dev2.tar.gz

---
+++ packages/elastic-apm/elastic-apm-1.0.0.dev2.tar.gz/elastic-apm-1.0.0.dev2/elasticapm/contrib/django/apps.py
@@ -0,0 +1,57 @@
+from functools import partial
+
+from django.apps import AppConfig
+
+from elasticapm.contrib.django.client import get_client
+
+
+class ElasticAPMConfig(AppConfig):
+    name = 'elasticapm.contrib.django'
+    label = 'elasticapm.contrib.django'
+    verbose_name = 'ElasticAPM'
+
+    def ready(self):
+        client = get_client()
+        register_handlers(client)
+        if not client.config.disable_instrumentation:
+            instrument(client)
+        else:
+            client.logger.debug("Skipping instrumentation. DISABLE_INSTRUMENTATION is set.")
+
+
+def register_handlers(client):
+    from django.core.signals import got_request_exception
+    from elasticapm.contrib.django.handlers import exception_handler
+
+    # Connect to Django's internal signal handler
+    dispatch_uid = 'elasticapm-exceptions'
+    got_request_exception.disconnect(dispatch_uid=dispatch_uid)
+    got_request_exception.connect(partial(exception_handler, client), dispatch_uid=dispatch_uid, weak=False)
+
+    # If we can import celery, register ourselves as exception handler
+    try:
+        import celery  # noqa F401
+        from elasticapm.contrib.celery import register_exception_tracking
+
+        try:
+            register_exception_tracking(client)
+        except Exception as e:
+            client.logger.exception('Failed installing django-celery hook: %s' % e)
+    except ImportError:
+        client.logger.debug("Not instrumenting Celery, couldn't import")
+
+
+def instrument(client):
+    """
+    Auto-instruments code to get nice traces
+    """
+    from elasticapm.instrumentation.control import instrument
+
+    instrument()
+    try:
+        import celery  # noqa F401
+        from elasticapm.contrib.celery import register_instrumentation
+
+        register_instrumentation(client)
+    except ImportError:
+        client.logger.debug("Not instrumenting Celery, couldn't import")
---
+++ packages/elastic-apm/elastic-apm-1.0.0.dev2.tar.gz/elastic-apm-1.0.0.dev2/elasticapm/version.py
@@ -0,0 +1,2 @@
+__version__ = (1, 0, 0, 'dev2')
+VERSION = '.'.join(map(str, __version__))
