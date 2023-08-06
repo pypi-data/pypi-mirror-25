dd422d720d83da18ad8bf139e8c6a851a7096d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:57 +0000

    Add package starfish/0.0.0a0/starfish-0.0.0a0.tar.gz

---
+++ packages/starfish/starfish-0.0.0a0.tar.gz/starfish-0.0.0a0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="starfish",
+    version="0.0.0a",
+    description="Pipelines and pipeline components for the analysis of image-based transcriptomics data",
+    author="Deep Ganguli",
+    author_email="dganguli@chanzuckerberg.com",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': "starfish=starfish.starfish:starfish"
+    }
+)
