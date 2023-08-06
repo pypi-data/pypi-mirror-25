824c74d0b5022fa40a9825bc19741172d9b2b69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:35 +0000

    Add package matchstick/0.1/matchstick-0.1.tar.gz

---
+++ packages/matchstick/matchstick-0.1.tar.gz/matchstick-0.1/matchstick/__init__.py
@@ -0,0 +1,1 @@
+from .matchstick import match, NoMatchException
---
+++ packages/matchstick/matchstick-0.1.tar.gz/matchstick-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name = 'matchstick',
+  packages = ['matchstick'],
+  version = '0.1',
+  description = 'A decorator for overloading Python functions',
+  author = 'Brett Beatty',
+  author_email = 'brettbeatty@gmail.com',
+  url = 'https://github.com/brettbeatty/matchstick',
+  download_url = 'https://github.com/brettbeatty/matchstick/archive/0.1.tar.gz',
+  keywords = ['match', 'overload', 'triage']
+)
