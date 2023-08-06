ed0f00c0bbbf4405542460e4b04bd17f27775862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:48 +0000

    Add package pyselog/0.0.3/pyselog-0.0.3.zip

---
+++ packages/pyselog/pyselog-0.0.3.zip/pyselog-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from pyselog.pyselog import __version__
+from setuptools import setup
+
+setup(
+    name='pyselog',
+    version=__version__,
+    description='This is a simple logger for python',
+    long_description='Not so long',
+    author='Jeffrey Zhang',
+    author_email='124303687@qq.com',
+    license='MIT',
+    packages=['pyselog'],
+    classifiers=[],
+)
