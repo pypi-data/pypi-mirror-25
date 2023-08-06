ca492a52d7c8d712d066ab65facd40bfeb29edaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:02 +0000

    Add package nerster/1.4.1/nerster-1.4.1.tar.gz

---
+++ packages/nerster/nerster-1.4.1.tar.gz/nerster-1.4.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='nerster',
+    version = '1.4.1',
+    py_modules=['nerster'],
+    author=['yuebei'],
+    auther_email='yuebei58@gmail.com',
+    url='',
+    description='A simple printer of nested lists',
+    
+)
