20433c5a331da93d95101807dcda80feaa9c2e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:31 +0000

    Add package treecut/0.7.8/treecut-0.7.8.tar.gz

---
+++ packages/treecut/treecut-0.7.8.tar.gz/treecut-0.7.8/treecut/draw.py
@@ -0,0 +1,167 @@
+"""
+Draws vertically presented hierarchical tree, along with the associated values
+"""
+
+import itertools
+
+import numpy as np
+import numpy.ma as ma
+import random
+import matplotlib
+matplotlib.use("Agg")
+from matplotlib.patches import Rectangle
+import matplotlib.pyplot as plt
+
+# latex fonts
+_ = lambda x: r"$\mathsf{%s}$" % (x.replace(" ", r"\ "))
+label_style = dict(rotation=90, ha="center", va="center", color="w", \
+    bbox=dict(boxstyle="round", fc="slategray", ec="slategray"))
+
+def clear_ax(ax):
+    ax.set_xlim(0,1)
+    ax.set_ylim(0,1)
+    ax.set_axis_off()
+
+
+class Dendrogram(object):
+
+    def __init__(self, ext_tree, datatype="continuous", cutoff=.05, **kwargs):
+
+        self.tree = ext_tree
+        self.accessions = []
+        self.values = self.tree.values
+        self.xinterval = 0
+        self.datatype = datatype
+
+        self.figure = fig = plt.figure(1, (8,6))
+        root = fig.add_axes([0,0,1,1])
+        tree_ax = fig.add_axes([0,.5,1,.5])
+        value_ax = fig.add_axes([0,.3,1,.2])
+
+        self.draw_tree(tree_ax)
+        self.draw_values(value_ax)
+        self.draw_modules(value_ax, cutoff=cutoff)
+        self.draw_legend(root)
+
+        for a in (tree_ax, value_ax, root):
+            clear_ax(a)
+
+    def draw_legend(self, root):
+        root.text(.1, .1, _("High-value cluster"),
+                bbox=dict(fc="r", ec="r", alpha=.3))
+        root.text(.3, .1, _("Low-value cluster"),
+                bbox=dict(fc="g", ec="g", alpha=.3))
+        root.text(.5, .1, r"$\mathsf{Missing\ value\ (\ast)}$")
+
+    def draw_tree(self, ax):
+
+        t = self.tree.node
+        farthest, max_dist = t.get_farthest_leaf()
+
+        margin = .1
+        xstart = margin
+        ystart = 1 - margin
+        canvas = 1 - 2 * margin
+        # scale the tree
+        scale = canvas / max_dist
+
+        num_leaves = len(t.get_leaf_names())
+        self.xinterval = xinterval = canvas / (num_leaves - 1)
+
+        coords = {}
+        i = itertools.count()
+        for n in t.traverse("postorder"):
+            dist = n.get_distance(t)
+            yy = ystart - scale * dist
+
+            if n.is_leaf():
+                xx = xstart + i.next() * xinterval
+                self.accessions.append(n.name)
+            else:
+                children = [coords[x] for x in n.get_children()]
+                children_x, children_y = zip(*children)
+                min_x, max_x = min(children_x), max(children_x)
+                # plot the horizontal bar
+                ax.plot((min_x, max_x), (yy, yy), "k-")
+                # plot the vertical bar
+                for cx, cy in children:
+                    ax.plot((cx, cx), (cy, yy), "k-")
+                xx = np.mean(children_x)
+
+            coords[n] = (xx, yy)
+
+        ax.text(xstart*.5, .5, "Phylogeny", label_style)
+
+    def draw_values(self, ax):
+
+        margin = .1
+        xstart = margin
+        ystart = .4
+        xinterval = self.xinterval
+
+        # mask array for missing data
+        num_leaves = len(self.accessions)
+        mask = np.zeros(num_leaves, dtype=int)
+        for i, x in enumerate(self.accessions):
+            if x not in self.values: mask[i] = 1
+
+        accession_values = ma.array([self.values.get(x, -1) \
+                for x in self.accessions], mask=mask, dtype="f")
+        min_val, max_val = accession_values.min(), accession_values.max()
+        # base line
+        ax.plot((xstart, 1-xstart), (ystart, ystart), "-", color="gray", lw=3)
+        ax.text(xstart*.5, .6, "Values", label_style)
+
+        if self.datatype=="discrete": return
+
+        # draw the gauge to the right showing the data range
+        gauge, tip = 1-margin*.7, .005
+        gc = "m"
+        ax.plot((gauge, gauge), (ystart, 1), "-", color=gc, lw=2)
+        ax.plot((gauge-tip, gauge+tip), (ystart, ystart), "-", color=gc, lw=2)
+        ax.plot((gauge-tip, gauge+tip), (1, 1), "-", color=gc, lw=2)
+        ax.text(gauge+tip, ystart, _("%.1f" % min_val), color=gc)
+        ax.text(gauge+tip, 1, _("%.1f" % max_val), va="top", color=gc)
+
+        scale = (1 - ystart) / (max_val - min_val)
+        accession_values -= min_val
+        accession_values *= scale
+
+        for i, a in enumerate(accession_values):
+            xx = xstart + i * xinterval
+            if accession_values[i] is ma.masked:
+                ax.text(xx, ystart, r"$\ast$", ha="center", va="top", color="r")
+            else:
+                ax.plot((xx, xx), (ystart, ystart + a), "-", color="gray", lw=2)
+
+    def draw_modules(self, ax, cutoff=.05):
+
+        margin = .1
+        xstart = margin
+        ystart = .4
+        xinterval = self.xinterval
+
+        modules = self.tree.get_modules(cutoff=cutoff)
+        lmean = np.mean
+
+        if self.datatype=="discrete":
+            mcolors = dict((x.note, random.choice("rgbmcky")) for x in modules)
+
+        for e in modules:
+            if self.datatype=="continuous":
+                mcolor = "g" if lmean(e.a) < lmean(e.b) else "r"
+            else:
+                mcolor = mcolors[e.note]
+
+            accs = e.get_leaf_names()
+            xx = xstart + min(self.accessions.index(x) for x in accs) * xinterval
+            width = (len(accs) - 1) * xinterval
+            ax.add_patch(Rectangle((xx, ystart), width, 1-ystart, fc=mcolor, alpha=.3, lw=0))
+            note = r"$\bar{x}=%s$" % e.note if self.datatype=="continuous" else _(e.note)
+            ax.text(xx+width*.5, ystart-.05, note, color=mcolor, ha="center", va="top")
+            ax.text(xx+width*.5, ystart-.15, r"$(P=%.1g)$" % e.val, color=mcolor, ha="center", va="top")
+
+    def savefig(self, image_name, **kwargs):
+
+        self.figure.savefig(image_name, **kwargs)
+        self.figure.clf()
---
+++ packages/treecut/treecut-0.7.8.tar.gz/treecut-0.7.8/treecut/tree.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+ExtTree class is similar to the ete2.Tree, but each node has an associated P-value
+this allows easy propagation of P-values either ascending or descending the tree.
+"""
+
+import sys
+from stats import stat_test, mean
+
+
+class ExtTree(list):
+    __slots__ = ("node", "values", "values2", "datatype", "a", "b", "val",
+            "hi_min", "lo_min", "note", "desc")
+
+    def __init__(self, node, values, values2, accs, datatype="continuous"):
+
+        self.node = node
+        self.values = values
+        self.values2 = values2
+        self.datatype = datatype
+        self.note = ""
+        self.desc = ""
+
+        for n in node.children:
+            if not n.is_leaf():
+                self.append(ExtTree(n, values, values2, accs, datatype=datatype))
+
+        nset = set(x.name for x in node.iter_leaves())
+        oset = accs - nset
+
+        # values for the direct children
+        self.a = a = self.get_values(nset, values)
+        # values for non-children (sibs)
+        self.b = b = self.get_values(oset, values)
+
+        self.val = self.hi_min = self.lo_min = 1.0
+
+        if a and b:
+            sys.stderr.write(".")
+            self.val, self.note = stat_test(a, b, datatype=datatype)
+
+        # core dynamic programming
+        self.lomin()
+        self.himin()
+
+    def __str__(self):
+        return "%d\t%d\t%s\t%.1g\t%.1g\t%.1g" % (\
+                len(self.a), len(self.b), self.note,
+                self.val, self.hi_min, self.lo_min)
+
+    def __getattr__(self, attr):
+        if attr not in self.__slots__:
+            return getattr(self.node, attr)
+
+    def render(self, image_name, cutoff=.05, **kwargs):
+        from draw import Dendrogram
+        d = Dendrogram(self, datatype=self.datatype, cutoff=cutoff)
+        d.savefig(image_name, **kwargs)
+        print >>sys.stderr, "tree image saved to %s" % image_name
+
+    def get_values(self, leaf_set, values):
+        return [values[x] for x in leaf_set if x in values]
+
+    def get_all_nodes(self):
+        res = []
+        for e in self:
+            res.append(e)
+            res += e.get_all_nodes()
+        return res
+
+    def get_modules(self, cutoff=.05):
+        modules = []
+        for e in self:
+            if e.val < min(e.lo_min, e.hi_min, cutoff):
+                if self.datatype=="continuous":
+                    e.desc = "lo" if mean(e.a) < mean(e.b) else "hi"
+                else:
+                    e.desc = "enriched"
+                modules.append(e)
+            else:
+                modules += e.get_modules(cutoff=cutoff)
+        return modules
+
+    verbose_fields = ("node_id ntaxa_a ntaxa_b member_mean P-value min_ancestor_P-value min_descendant_P-value").split()
+
+    def print_all_nodes(self, filehandle):
+        for i, e in enumerate(self.get_all_nodes()):
+            print >>filehandle, "%d\t%s" % (i, e)
+
+    def print_modules(self, filehandle, cutoff=.05):
+        modules = self.get_modules(cutoff=cutoff)
+        for i, e in enumerate(modules):
+            print >>filehandle, "%s\t%s\t%s\t%.1g" % (",".join(sorted(e.get_leaf_names())), e.desc, e.note, e.val)
+        return modules
+
+    def himin(self):
+        for e in self:
+            e.hi_min = min(self.val, self.hi_min)
+            e.himin()
+
+    def lomin(self):
+        if len(self)!=0:
+            self.lo_min = min([x.lomin() for x in self] +\
+                    [x.val for x in self])
+        return self.lo_min
