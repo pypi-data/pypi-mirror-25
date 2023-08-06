d6bdee3a67fb1bd4d554790bc3b3eb7cac6002d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:02 +0000

    Add package ahqapiclient/0.7.7/ahqapiclient-0.7.7.tar.gz

---
+++ packages/ahqapiclient/ahqapiclient-0.7.7.tar.gz/ahqapiclient-0.7.7/ahqapiclient/resources/reports.py
@@ -0,0 +1,24 @@
+from ahqapiclient.resources import Resource
+from six import text_type
+
+
+class Reports(Resource):
+
+    def __init__(self, http_client):
+        super(Reports, self).__init__('/reports', http_client)
+
+    def create_report(self, report_type, value, s3_data=None):
+
+        if not isinstance(value, text_type):
+            value = value.encode('utf-8')
+
+        data = {'value': value}
+
+        if s3_data:
+            data['s3_data'] = s3_data
+
+        return self.post(
+            path=self.rurl(report_type),
+            data=data
+
+        )
