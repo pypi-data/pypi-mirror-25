dff085750aa24a48f1b1acc9f836bb6a900ebd78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:45 +0000

    Add package Flask-WAT/0.1.1/Flask-WAT-0.1.1.tar.gz

---
+++ packages/Flask-WAT/Flask-WAT-0.1.1.tar.gz/Flask-WAT-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Flask-WAT',
+    version='0.1.1',
+    packages=['flask_wat'],
+    url='',
+    license='',
+    author='ll1l11',
+    author_email='',
+    description='',
+    install_requires=['requests']
+)
