71452b2edeb0bb7c99ee464fd86131ea17d7da72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:39 +0000

    Add package mahjong/1.0.5/mahjong-1.0.5.tar.gz

---
+++ packages/mahjong/mahjong-1.0.5.tar.gz/mahjong-1.0.5/setup.py
@@ -0,0 +1,37 @@
+import io
+from distutils.core import setup
+
+
+def get_long_description():
+    """Generate a long description from the README file."""
+    descr = []
+    for fname in ('README.rst',):
+        with io.open(fname, encoding='utf-8') as f:
+            descr.append(f.read())
+    return '\n\n'.join(descr)
+
+
+setup(
+    name='mahjong',
+    packages=[
+        'mahjong',
+        'mahjong.hand_calculating',
+        'mahjong.hand_calculating.yaku_list',
+        'mahjong.hand_calculating.yaku_list.yakuman',
+    ],
+    version='1.0.5',
+    description='Mahjong hands calculation',
+    long_description=get_long_description(),
+    author='Alexey Lisikhin',
+    author_email='lisikhin@gmail.com',
+    url='https://github.com/MahjongRepository/mahjong',
+    data_files=[('', ['README.rst'])],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
