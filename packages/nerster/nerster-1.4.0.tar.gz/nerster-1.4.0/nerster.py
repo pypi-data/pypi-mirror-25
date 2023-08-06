34b083dbc602a3ef48a95d8a35bf81029c0afd38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:02 +0000

    Add package nerster/1.4.0/nerster-1.4.0.tar.gz

---
+++ packages/nerster/nerster-1.4.0.tar.gz/nerster-1.4.0/nerster.py
@@ -0,0 +1,13 @@
+"""This is the "nerster.py" module and it provides one function called print_lol() which prints lists that may or may not incluede nested lists."""
+def print_lol(a_list, indent=False, level=0, fn= sys.stdout):
+    """Prints each item in a list, recursively descending
+       into nested lists (if necessary)."""
+
+    for each_item in a_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1, fn)
+        else:
+            if indent:
+                for l in range(level):
+                    print("\t", end='', file=fn)
+            print(each_item, file=fn)
