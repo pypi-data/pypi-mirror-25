9d24a7dce0020a8093d05207519525b74cee47ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:16 +0000

    Add package regulations/8.4.1/regulations-8.4.1.tar.gz

---
+++ packages/regulations/regulations-8.4.1.tar.gz/regulations-8.4.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="regulations",
+    version="8.4.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'cached-property',
+        'django>=1.8,<1.12',
+        'enum34',
+        'futures',
+        'requests',
+        'six',
+    ],
+    extras_require={
+        'notice_comment': ['boto3', 'celery', 'requests-toolbelt'],
+    },
+    classifiers=[
+        'License :: Public Domain',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'
+    ]
+)
