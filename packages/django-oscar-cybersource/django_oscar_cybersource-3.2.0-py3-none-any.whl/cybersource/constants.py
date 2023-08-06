88df507271a6a25b3f4338a8192c534caa649dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:27 +0000

    Add package django-oscar-cybersource/3.2.0/django_oscar_cybersource-3.2.0-py3-none-any.whl

---
+++ packages/django-oscar-cybersource/django_oscar_cybersource-3.2.0-py3-none-any.whl/cybersource/constants.py
@@ -0,0 +1,6 @@
+CHECKOUT_ORDER_ID = 'checkout_order_id'
+CHECKOUT_FINGERPRINT_SESSION_ID = 'cybersource_fingerprint_session_id'
+
+# Default code for the email to send after successful checkout
+DECISION_ACCEPT = 'ACCEPT'
+DECISION_REVIEW = 'REVIEW'
