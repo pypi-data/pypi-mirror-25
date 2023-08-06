3f9c1a048e20f3c129a232364195ee43cd4b86ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:07 +0000

    Add package tbzuploader/2017.5.0/tbzuploader-2017.5.0.tar.gz

---
+++ packages/tbzuploader/tbzuploader-2017.5.0.tar.gz/tbzuploader-2017.5.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='tbzuploader',
+    version='2017.5.0',
+    license='Apache Software License 2.0',
+    long_description=open('README.rst').read(),
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'requests',
+    ],
+
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': [
+            'tbzuploader=tbzuploader.console_scripts:main',
+        ],
+    }
+)
