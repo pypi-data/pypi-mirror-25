0496c3be659146363a990708767cb84a3bb490c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:22 +0000

    Add package django-unused-media/0.1.9/django-unused-media-0.1.9.tar.gz

---
+++ packages/django-unused-media/django-unused-media-0.1.9.tar.gz/django-unused-media-0.1.9/django_unused_media/management/commands/cleanup_unused_media.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+import six.moves
+
+from django_unused_media.cleanup import get_unused_media, remove_empty_dirs
+
+import os
+
+
+class Command(BaseCommand):
+
+    help = "Clean unused media files which have no reference in models"
+
+    def add_arguments(self, parser):
+
+        parser.add_argument('--noinput', '--no-input',
+                            dest='interactive',
+                            action='store_false',
+                            default=True,
+                            help='Do not ask confirmation')
+
+        parser.add_argument('-e', '--exclude',
+                            dest='exclude',
+                            action='append',
+                            default=[],
+                            help='Exclude files by mask (only * is supported), can use multiple --exclude')
+        
+        parser.add_argument('--remove-empty-dirs',
+                            dest='remove_empty_dirs',
+                            action='store_false',
+                            default=False,
+                            help='Remove empty dirs after files cleanup')
+
+    def handle(self, *args, **options):
+
+        unused_media = get_unused_media(options.get('exclude') or [])
+
+        if not unused_media:
+            self.stdout.write('Nothing to delete. Exit')
+            return
+
+        if options.get('interactive'):
+
+            self.stdout.write('These files will be deleted:')
+
+            for f in unused_media:
+                self.stdout.write(f)
+
+            # ask user
+
+            if six.moves.input('Are you sure you want to remove %s unused files? (y/N)' % len(unused_media)).upper() != 'Y':
+                self.stdout.write('Interrupted by user. Exit.')
+                return
+
+        for f in unused_media:
+            self.stdout.write('Remove %s' % f)
+            os.remove(os.path.join(settings.MEDIA_ROOT, f))
+
+        if options.get('remove_empty_dirs'):
+            remove_empty_dirs()
+
+        self.stdout.write('Done. %s unused files have been removed' % len(unused_media))
---
+++ packages/django-unused-media/django-unused-media-0.1.9.tar.gz/django-unused-media-0.1.9/tests/test_management.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+import mock
+import six
+
+from preggy import expect
+from django.core.management import call_command
+
+from django_unused_media.management.commands.cleanup_unused_media import Command
+from .base import BaseTestCase
+
+
+class TestManagementCommand(BaseTestCase):
+    def test_command_call(self):
+        expect(call_command('cleanup_unused_media', interactive=False)).Not.to_be_an_error()
+
+    def test_command_nothing_to_delete(self):
+        cmd = Command()
+        cmd.handle(interactive=False)
+        expect(cmd.stdout.getvalue().split('\n'))\
+            .to_include(u'Nothing to delete. Exit')
+
+    def test_command_not_interactive(self):
+        self._media_create('file.txt')
+
+        cmd = Command()
+        cmd.handle(interactive=False)
+        expect(cmd.stdout.getvalue().split('\n'))\
+            .to_include(u'Remove {}'.format(self._media_abs_path(u'file.txt')))\
+            .to_include(u'Done. 1 unused files have been removed')
+
+        expect(self._media_exists('file.txt')).to_be_false()
+
+    @mock.patch('six.moves.input', return_value='n')
+    def test_command_interactive_n(self, mock_input):
+        self._media_create(u'file.txt')
+
+        cmd = Command()
+        cmd.handle(interactive=True)
+        expect(cmd.stdout.getvalue().split('\n'))\
+            .to_include(u'Interrupted by user. Exit.')
+
+        expect(self._media_exists(u'file.txt')).to_be_true()
+
+    @mock.patch('six.moves.input', return_value='Y')
+    def test_command_interactive_y(self, mock_input):
+        self._media_create(u'file.txt')
+
+        cmd = Command()
+        cmd.handle(interactive=True)
+        expect(cmd.stdout.getvalue().split('\n')) \
+            .to_include(u'Remove {}'.format(self._media_abs_path(u'file.txt'))) \
+            .to_include(u'Done. 1 unused files have been removed')
+
+        expect(self._media_exists(u'file.txt')).to_be_false()
+
+    @mock.patch('six.moves.input', return_value='Y')
+    def test_command_interactive_y_with_ascii(self, mock_input):
+        self._media_create(u'Тест.txt')
+
+        expected_string = u'Remove {}'.format(self._media_abs_path(u'Тест.txt'))
+        if six.PY2:
+            expected_string = expected_string.encode('utf-8')
+
+        cmd = Command()
+        cmd.handle(interactive=True)
+        expect(cmd.stdout.getvalue().split('\n')) \
+            .to_include(expected_string) \
+            .to_include(u'Done. 1 unused files have been removed')
+
+        expect(self._media_exists(u'Тест.txt')).to_be_false()
+
+    @mock.patch('django_unused_media.management.commands.cleanup_unused_media.remove_empty_dirs')
+    def test_command_do_not_remove_dirs(self, mock_remove_empty_dirs):
+        self._media_create(u'sub1/sub2/sub3/notused.txt')
+
+        cmd = Command()
+        cmd.handle(interactive=False)
+
+        mock_remove_empty_dirs.assert_not_called()
+
+    @mock.patch('django_unused_media.management.commands.cleanup_unused_media.remove_empty_dirs')
+    def test_command_remove_dirs(self, mock_remove_empty_dirs):
+        self._media_create(u'sub1/sub2/sub3/notused.txt')
+
+        cmd = Command()
+        cmd.handle(interactive=False, remove_empty_dirs=True)
+
+        mock_remove_empty_dirs.assert_called_once()
