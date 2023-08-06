9afa3f1ed26d8e03f7456e7cbf293a899cdacc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:13 +0000

    Add package modbus/0.1/modbus-0.1.tar.gz

---
+++ packages/modbus/modbus-0.1.tar.gz/modbus-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='modbus',
+      version='0.1',
+      description='Modbus TCP Server and Client Programs',
+      url='https://github.com/ipal0/modbus',
+      author='Pal',
+      author_email='ipal0can@gmail.com',
+      license='GPL',
+      packages=['modbus'],
+      zip_safe=False)
