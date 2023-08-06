bfafc1cd8e219a125a3ee4b6529a4c343252e614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:14 +0000

    Add package upgradegit/0.0.5/upgradegit-0.0.5.tar.gz

---
+++ packages/upgradegit/upgradegit-0.0.5.tar.gz/upgradegit-0.0.5/README.md
@@ -0,0 +1,17 @@
+# gitupgrade
+Upgrades pip git dependancies with a git hash
+
+# Installation
+```
+pip install gitupgrade
+```
+
+# Usage
+```
+gitupgrade
+```
+
+| Option         | Default          | Description                      |
+| ---------------|------------------|----------------------------------|
+| --file         | requirements.txt | file to upgrade                  |
+| --branch       | master           | git branch to get hash from      |
---
+++ packages/upgradegit/upgradegit-0.0.5.tar.gz/upgradegit-0.0.5/src/upgradegit/cli.py
@@ -0,0 +1,40 @@
+import click
+import requirements
+import os
+import re
+
+
+@click.command()
+@click.option('--file', default='requirements.txt', help='File to upgrade')
+@click.option('--branch', default='master', help='Branch to upgrade from')
+def upgrade(file, branch):
+    lines = []
+    with open(file, 'r') as f:
+        for req in requirements.parse(f):
+            line = ''
+            if (req.uri):
+                reg = r'([0-9a-z]*)(?=(\s+refs\/heads\/'+branch+'))'
+                uri = req.uri.replace('git+ssh://', 'ssh://git@')
+                cmd = 'git ls-remote {} {} HEAD'.format(uri, branch)
+                result = os.popen(cmd).read()
+                result = result.strip()
+                results = re.findall(reg, result)
+                result = results[0][0]
+                line = re.sub(r'\@([0-9a-f]*)(?=(#|$))', '@'+result, req.line)
+
+            else:
+                name = req.name
+                spec_op = req.specs[0][0]
+                spec_ver = req.specs[0][1]
+                line = '{name}{spec_op}{spec_ver}'.format(
+                    name=name, spec_op=spec_op, spec_ver=spec_ver)
+
+            lines.append(line)
+
+    with open(file, 'w') as f:
+        for line in lines:
+            f.write(line+'\n')
+
+
+if __name__ == '__main__':
+    upgrade()
