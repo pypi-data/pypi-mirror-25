89917e2b4595173fe9b7fe1650eebd5716c9a89c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:34 +0000

    Add package aprs2influxdb/0.1.1/aprs2influxdb-0.1.1.zip

---
+++ packages/aprs2influxdb/aprs2influxdb-0.1.1.zip/aprs2influxdb-0.1.1/aprs2influxdb/__main__.py
@@ -0,0 +1,236 @@
+import aprslib
+import influxdb
+from influxdb import InfluxDBClient
+import logging
+import argparse
+import sys
+import threading
+import time
+
+# Globals
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("aprs2influxdb")
+
+# Command line input
+parser = argparse.ArgumentParser(description='Connects to APRS-IS and saves stream to local InfluxDB')
+parser.add_argument('--dbhost', help='Set InfluxDB host', default="localhost")
+parser.add_argument('--dbport', help='Set InfluxDB port', default="8086")
+parser.add_argument('--dbuser', help='Set InfluxDB user', default="root")
+parser.add_argument('--dbpassword', help='Set InfluxDB password', default="root")
+parser.add_argument('--dbname', help='Set InfluxDB database name', default="mydb")
+parser.add_argument('--callsign', help='Set APRS-IS login callsign', default="nocall")
+parser.add_argument('--port', help='Set APRS-IS port', default="10152")
+parser.add_argument('--interval', help='Set APRS-IS heartbeat interval in minutes', default="15")
+
+# Parse the arguments
+args = parser.parse_args()
+
+
+def jsonToLineProtocol(jsonData):
+    """Converts JSON APRS-IS packet to influxdb line protocol
+
+    Takes in a JSON packet from aprslib (raw=false) and parses it into an
+    influxdb line protocol compliant string to insert into database. Returns
+    a valid line protocol string ready to be inserted into the database.
+
+    keyword arguments:
+    jsonData -- aprslib parsed JSON packet
+    """
+    # Converts aprslib JSON to influxdb line protocol
+    # Schema
+    # measurement = packet
+    # tag = from
+    # tag = to
+    # tag = symbolTable
+    # tag = symbol
+    # tag = format
+    # tag = comment
+    # field = latitude
+    # field = longitude
+    # field = posAmbiguity
+    # field = altitude
+    # field = speed
+    # field = sequenceNumber
+    # field = analog1
+    # field = analog2
+    # field = analog3
+    # field = analog4
+    # field = analog5
+    # field = digital
+
+    # Parse uncompressed format packets
+    if jsonData["format"] == "uncompressed":
+        # initialize variables
+        tags = []
+        fields = []
+
+        # Set measurement to "packet"
+        measurement = "packet"
+
+        try:
+            tags.append("from={0}".format(jsonData.get("from")))
+            tags.append("to={0}".format(jsonData.get("to")))
+            tags.append("format={0}".format(jsonData.get("format")))
+
+        except KeyError as e:
+            logger.error(e)
+            logger.error(jsonData)
+
+        tagStr = ",".join(tags)
+
+        try:
+            fields.append("latitude={0}".format(jsonData.get("latitude", 0)))
+            fields.append("longitude={0}".format(jsonData.get("longitude", 0)))
+            fields.append("posAmbiguity={0}".format(jsonData.get("posambiguity", 0)))
+            fields.append("altitude={0}".format(jsonData.get("altitude", 0)))
+            fields.append("speed={0}".format(jsonData.get("speed", 0)))
+        except KeyError as e:
+            logger.error(e)
+            logger.error(jsonData)
+
+        try:
+            if jsonData["telemetry"]["seq"]:
+                fields.append("sequenceNumber={0}".format(jsonData["telemetry"]["seq"]))
+                fields.append("analog1={0}".format(jsonData["telemetry"]["vals"][0]))
+                fields.append("analog2={0}".format(jsonData["telemetry"]["vals"][1]))
+                fields.append("analog3={0}".format(jsonData["telemetry"]["vals"][2]))
+                fields.append("analog4={0}".format(jsonData["telemetry"]["vals"][3]))
+                fields.append("analog5={0}".format(jsonData["telemetry"]["vals"][4]))
+                fields.append("digital={0}".format(jsonData["telemetry"]["bits"]))
+
+        except KeyError as e:
+            # Expect many KeyErrors for stations not sending telemetry
+            logger.debug(e)
+            logger.debug(jsonData)
+
+        try:
+            comment = jsonData.get("comment").encode('ascii', 'ignore')
+
+            if comment:
+                logger.debug(comment)
+                fields.append("comment=\"{0}\"".format(comment.replace("\"", "")))
+
+        except UnicodeError as e:
+            logger.error(e)
+
+        except TypeError as e:
+            logger.error(e)
+
+        fieldsStr = ",".join(fields)
+
+        return measurement + "," + tagStr + " " + fieldsStr
+
+
+def callback(packet):
+    """aprslib callback for every packet received from APRS-IS connection
+
+    keyword arguments:
+    packet -- APRS-IS packet from aprslib connection
+    """
+    logger.debug(packet)
+
+    # Open a new connection every time, probably SLOWWWW
+    influxConn = connectInfluxDB()
+    line = jsonToLineProtocol(packet)
+
+    if line:
+        logger.debug(line)
+        try:
+            influxConn.write_points([line], protocol='line')
+
+        except StandardError as e:
+            logger.error(e)
+            logger.error(packet)
+
+        except influxdb.exceptions.InfluxDBClientError as e:
+            logger.error(e)
+            logger.error(packet)
+
+
+def connectInfluxDB():
+    """Connect to influxdb database with configuration values"""
+
+    return InfluxDBClient(args.dbhost,
+                          args.dbport,
+                          args.dbuser,
+                          args.dbpassword,
+                          args.dbname)
+
+
+def consumer(conn):
+    """Start consumer function for thread
+
+    keyword arguments:
+    conn -- APRS-IS connection from aprslib
+    """
+    logger.debug("starting consumer thread")
+    # Obtain raw APRS-IS packets and sent to callback when received
+    conn.consumer(callback, immortal=True, raw=False)
+
+
+def heartbeat(conn, callsign, interval):
+    """Send out an APRS status message to keep connection alive
+
+    keyword arguments:
+    conn -- APRS-IS connction from aprslib
+    callsign -- Callsign of status message
+    interval -- Minutes betwee status messages
+    """
+    logger.debug("Starting heartbeat thread")
+    while True:
+        # Create timestamp
+        timestamp = int(time.time())
+
+        # Create APRS status message
+        status = "{0}>APRS,TCPIP*:>aprs2influxdb heartbeat {1}"
+        conn.sendall(status.format(callsign, timestamp))
+        logger.debug("Sent heartbeat")
+
+        # Sleep for specified time
+        time.sleep(float(interval) * 60)  # Sent every interval minutes
+
+
+def main():
+    """Main function of aprs2influxdb
+
+    Reads in configuration values and starts connection to APRS-IS with aprslib.
+    Then two threads are started, one to monitor for APRS-IS packets and
+    another to periodically send status packets to APRS-IS in order to keep
+    the connection alive.
+    """
+
+    # Start login for APRS-IS
+    logger.info("Logging into APRS-IS as {0} on port {1}".format(args.callsign, args.port))
+    if args.callsign == "nocall":
+        logger.warning("APRS-IS ignores the callsign \"nocall\"!")
+
+    # Open APRS-IS connection
+    passcode = aprslib.passcode(args.callsign)
+    AIS = aprslib.IS(args.callsign,
+                     passwd=passcode,
+                     port=args.port)
+    try:
+        AIS.connect()
+
+    except aprslib.exceptions.LoginError as e:
+        logger.error(e)
+        logger.info("APRS Login Callsign: {0} Port: {1}".format(args.callsign, args.port))
+        sys.exit(1)
+
+    except aprslib.exceptions.ConnectionError as e:
+        logger.error(e)
+        sys.exit(1)
+
+    # Create heartbeat
+    t1 = threading.Thread(target=heartbeat, args=(AIS, args.callsign, args.interval))
+
+    # Create consumer
+    t2 = threading.Thread(target=consumer, args=(AIS,))
+
+    # Start threads
+    t1.start()
+    t2.start()
+
+
+if __name__ == "__main__":
+    main()
