a8c7c6119be5313314b67db6dbf9ae0248ba0b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:29 +0000

    Add package asn1tools/0.11.0/asn1tools-0.11.0.tar.gz

---
+++ packages/asn1tools/asn1tools-0.11.0.tar.gz/asn1tools-0.11.0/tests/test_parse.py
@@ -0,0 +1,41 @@
+import sys
+import unittest
+
+import asn1tools
+
+sys.path.append('tests/files')
+
+from foo import FOO
+from rrc_8_6_0 import RRC_8_6_0
+from rfc5280 import RFC5280
+from zforce import ZFORCE
+from bar import BAR
+
+
+class Asn1ToolsParseTest(unittest.TestCase):
+
+    maxDiff = None
+
+    def test_parse_foo(self):
+        foo = asn1tools.parse_file('tests/files/foo.asn')
+        self.assertEqual(foo, FOO)
+
+    def test_parse_rrc_8_6_0(self):
+        rrc_8_6_0 = asn1tools.parse_file('tests/files/rrc_8_6_0.asn')
+        self.assertEqual(rrc_8_6_0, RRC_8_6_0)
+
+    def test_parse_rfc5280(self):
+        rfc5280 = asn1tools.parse_file('tests/files/rfc5280.asn')
+        self.assertEqual(rfc5280, RFC5280)
+
+    def test_parse_zforce(self):
+        zforce = asn1tools.parse_file('tests/files/zforce.asn')
+        self.assertEqual(zforce, ZFORCE)
+
+    def test_parse_bar(self):
+        bar = asn1tools.parse_file('tests/files/bar.asn')
+        self.assertEqual(bar, BAR)
+
+
+if __name__ == '__main__':
+    unittest.main()
