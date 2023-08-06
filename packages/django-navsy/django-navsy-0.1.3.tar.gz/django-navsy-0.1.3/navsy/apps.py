23996908281fe8356b394c994fca483718e8ee93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:13 +0000

    Add package django-navsy/0.1.3/django-navsy-0.1.3.tar.gz

---
+++ packages/django-navsy/django-navsy-0.1.3.tar.gz/django-navsy-0.1.3/navsy/apps.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+
+class NavsyConfig(AppConfig):
+
+    name = 'navsy'
+    verbose_name = 'Navigation'
+
+    def ready(self):
+
+        from navsy import settings, signals
+
+        settings.check_settings()
