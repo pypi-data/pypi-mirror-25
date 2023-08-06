a4e1ee9a0c43d98620d75cad4796abcf9178c0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:19 +0000

    Add package osquery/3.0.0/osquery-3.0.0.tar.gz

---
+++ packages/osquery/osquery-3.0.0.tar.gz/osquery-3.0.0/osquery/extension_client.py
@@ -0,0 +1,65 @@
+"""This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. An additional grant
+of patent rights can be found in the PATENTS file in the same directory.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import time
+
+from thrift.protocol import TBinaryProtocol
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+
+from osquery.extensions.ExtensionManager import Client
+
+DEFAULT_SOCKET_PATH = "/var/osquery/osquery.em"
+"""The default path for osqueryd sockets"""
+
+class ExtensionClient(object):
+    """A client for connecting to an existing extension manager socket"""
+
+    _protocol = None
+    _transport = None
+
+    def __init__(self, path=DEFAULT_SOCKET_PATH, uuid=None):
+        """
+        Keyword arguments:
+        path -- the path of the extension socket to connect to
+        uuid -- the additional UUID to use when constructing the socket path
+        """
+        self.path = path
+        if uuid:
+            self.path += ".%s" % str(uuid)
+        sock = TSocket.TSocket(unix_socket=self.path)
+        self._transport = TTransport.TBufferedTransport(sock)
+        self._protocol = TBinaryProtocol.TBinaryProtocol(self._transport)
+
+    def close(self):
+        """Close the extension client connection"""
+        if self._transport:
+            self._transport.close()
+
+    def open(self, timeout=1, interval=0.2):
+        """Attempt to open the UNIX domain socket"""
+        delay = 0
+        while delay < timeout:
+            try:
+                self._transport.open()
+                return True
+            except TTransport.TTransportException:
+                pass
+            delay += interval
+            time.sleep(interval)
+        return False
+
+    def extension_manager_client(self):
+        """Return an extension manager (osquery core) client."""
+        return Client(self._protocol)
+
+    def extension_client(self):
+        """Return an extension (osquery extension) client."""
+        return Client(self._protocol)
