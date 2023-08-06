4f4c3868c2c309a9f2a0feace6e914e4fe5e2697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:19 +0000

    Add package nodeconductor-digitalocean/0.9.0/nodeconductor-digitalocean-0.9.0.tar.gz

---
+++ packages/nodeconductor-digitalocean/nodeconductor-digitalocean-0.9.0.tar.gz/nodeconductor-digitalocean-0.9.0/src/nodeconductor_digitalocean/extension.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from nodeconductor.core import NodeConductorExtension
+
+
+class DigitalOceanExtension(NodeConductorExtension):
+
+    @staticmethod
+    def django_app():
+        return 'nodeconductor_digitalocean'
+
+    @staticmethod
+    def rest_urls():
+        from .urls import register_in
+        return register_in
