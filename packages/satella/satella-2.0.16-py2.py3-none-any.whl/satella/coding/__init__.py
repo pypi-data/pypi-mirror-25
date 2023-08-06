c48eed5e2c8af46f11e830ca6e5d1b4cab713b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:03 +0000

    Add package satella/2.0.16/satella-2.0.16-py2.py3-none-any.whl

---
+++ packages/satella/satella-2.0.16-py2.py3-none-any.whl/satella/coding/__init__.py
@@ -0,0 +1,23 @@
+# coding=UTF-8
+"""
+Just useful objects to make your coding nicer every day
+"""
+from __future__ import print_function, absolute_import, division
+
+from .concurrent import Monitor, RMonitor, CallableGroup
+from .algos import merge_dicts
+from .recast_exceptions import rethrow_as, silence_excs
+from .typecheck import typed, List, Tuple, Dict, Callable, Sequence, \
+    TypeVar, Mapping, Iterable, Union, Any, Optional, CallSignature, \
+    Number, coerce
+from .structures import TimeBasedHeap, Heap, typednamedtuple, OmniHashableMixin
+
+__all__ = [
+    'typednamedtuple', 'OmniHashableMixin'
+    'TimeBasedHeap', 'Heap', 'CallableGroup',
+    'Monitor', 'RMonitor', 'merge_dicts',
+    'typed', 'List', 'Tuple', 'Dict', 'NewType', 'Callable', 'Sequence', 'coerce'
+    'TypeVar', 'Generic', 'Mapping', 'Iterable', 'Union', 'Any', 'Optional', 'CallSignature', 'Number',
+    'rethrow_as', 'silence_excs'
+]
+
