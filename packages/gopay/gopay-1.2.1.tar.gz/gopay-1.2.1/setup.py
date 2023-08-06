89f6640609b57e1a927b1dd8c7c6a9d6185220d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:39 +0000

    Add package gopay/1.2.1/gopay-1.2.1.tar.gz

---
+++ packages/gopay/gopay-1.2.1.tar.gz/gopay-1.2.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from pypandoc import convert
+
+setup(
+    name='gopay',
+    version='1.2.1',
+    description='GoPay\'s Python SDK for Payments REST API',
+    long_description=convert('README.md', 'rst'),
+    url='https://github.com/gopaycommunity/gopay-python-sdk',
+    author='GoPay',
+    author_email='integrace@gopay.cz',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    keywords='gopay payments sdk rest api',
+    packages=['gopay'],
+    install_requires=['requests'],
+)
