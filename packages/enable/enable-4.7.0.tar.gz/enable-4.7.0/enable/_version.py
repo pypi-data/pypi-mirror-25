692c4db02e5f348d0f9ce15bd370dfcd6039db2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:48 +0000

    Add package enable/4.7.0/enable-4.7.0.tar.gz

---
+++ packages/enable/enable-4.7.0.tar.gz/enable-4.7.0/enable/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM ENABLE SETUP.PY
+version = '4.7.0'
+full_version = '4.7.0'
+git_revision = 'e0199dd1'
+is_released = True
+
+if not is_released:
+    version = full_version
