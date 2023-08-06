7d0bc09b03d81d8d2b1a203f01c662bc0b9cee49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:06 +0000

    Add package bloop/1.3.0/bloop-1.3.0.tar.gz

---
+++ packages/bloop/bloop-1.3.0.tar.gz/bloop-1.3.0/bloop/__init__.py
@@ -0,0 +1,60 @@
+import warnings
+
+from .conditions import Condition
+from .engine import Engine
+from .exceptions import (
+    BloopException,
+    ConstraintViolation,
+    MissingObjects,
+    RecordsExpired,
+    ShardIteratorExpired,
+    TableMismatch,
+)
+from .models import BaseModel, Column, GlobalSecondaryIndex, LocalSecondaryIndex
+from .search import QueryIterator, ScanIterator
+from .signals import (
+    before_create_table,
+    model_bound,
+    model_created,
+    model_validated,
+    object_deleted,
+    object_loaded,
+    object_modified,
+    object_saved,
+)
+from .stream import Stream
+from .types import (
+    UUID,
+    Binary,
+    Boolean,
+    DateTime,
+    Integer,
+    List,
+    Map,
+    Number,
+    Set,
+    String,
+)
+
+
+__all__ = [
+    # Models
+    "BaseModel", "Boolean", "Binary", "Column", "DateTime", "Engine", "GlobalSecondaryIndex", "Integer",
+    "List", "LocalSecondaryIndex", "Map", "Number", "Set", "String", "UUID",
+
+    # Exceptions
+    "BloopException", "ConstraintViolation", "MissingObjects",
+    "RecordsExpired", "ShardIteratorExpired", "TableMismatch",
+
+    # Signals
+    "before_create_table", "model_bound", "model_created", "model_validated",
+    "object_deleted", "object_loaded", "object_modified", "object_saved",
+
+    # Types
+    "UUID", "Binary", "Boolean", "DateTime", "Integer", "List", "Map", "Number", "Set", "String",
+
+    # Misc
+    "Condition", "QueryIterator", "ScanIterator", "Stream"
+]
+__version__ = "1.3.0"
+warnings.simplefilter('always', DeprecationWarning)
