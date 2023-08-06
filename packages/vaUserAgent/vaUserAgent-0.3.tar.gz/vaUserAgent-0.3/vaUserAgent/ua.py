83f140d5289646e9f354415fb18ef07d41d04149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:40 +0000

    Add package vaUserAgent/0.3/vaUserAgent-0.3.tar.gz

---
+++ packages/vaUserAgent/vaUserAgent-0.3.tar.gz/vaUserAgent-0.3/vaUserAgent/ua.py
@@ -0,0 +1,10 @@
+import random
+
+def rr():
+    return random.choice(open('vaUserAgent/ua.txt').readlines()).strip()
+
+def out():
+    print(random.choice(open('vaUserAgent/ua.txt').readlines()).strip()
+
+# print(rr())
+# out()
