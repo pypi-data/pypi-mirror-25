86c95b9d0b7e13ec3963eed19bdd18841c1fc27b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:13 +0000

    Add package nuimo/0.3.6/nuimo-0.3.6.tar.gz

---
+++ packages/nuimo/nuimo-0.3.6.tar.gz/nuimo-0.3.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='nuimo',
+    packages=['nuimo'],
+    install_requires=['gatt>=0.2.4'],
+    version='0.3.6',
+    description='Nuimo SDK for Python on Linux',
+    keywords='nuimo',
+    url='https://github.com/getsenic/nuimo-linux-python',
+    download_url='https://github.com/getsenic/nuimo-linux-python/archive/0.3.6.tar.gz',
+    author='Senic GmbH',
+    author_email='developers@senic.com',
+    license='MIT',
+    py_modules=['nuimoctl'],
+    entry_points={
+        'console_scripts': ['nuimoctl = nuimoctl:main']
+    }
+)
