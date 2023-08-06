c0ad766218fd16d898b8d9db2c61a7243d86d7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:19 +0000

    Add package quinn/0.2.0/quinn-0.2.0-py3-none-any.whl

---
+++ packages/quinn/quinn-0.2.0-py3-none-any.whl/quinn/functions.py
@@ -0,0 +1,23 @@
+from quinn.spark import *
+
+import pyspark.sql.functions as F
+
+from pyspark.sql.types import BooleanType
+
+
+def exists(f):
+    def temp_udf(l):
+        return any(map(f, l))
+    return F.udf(temp_udf, BooleanType())
+
+
+def forall(f):
+    def temp_udf(l):
+        return all(map(f, l))
+    return F.udf(temp_udf, BooleanType())
+
+
+def multi_equals(value):
+    def temp_udf(*cols):
+        return all(map(lambda col: col == value, cols))
+    return F.udf(temp_udf, BooleanType())
