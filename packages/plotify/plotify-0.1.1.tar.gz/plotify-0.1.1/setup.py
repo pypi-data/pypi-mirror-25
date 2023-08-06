a5e537026ab9b31707b75fb590cc25e008b8d93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:59 +0000

    Add package plotify/0.1.1/plotify-0.1.1.tar.gz

---
+++ packages/plotify/plotify-0.1.1.tar.gz/plotify-0.1.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import (
+    setup as install,
+    find_packages,
+)
+
+VERSION = '0.1.1'
+
+install(
+    name='plotify',
+    version=VERSION,
+    description="Make beautiful plots, fast.",
+    long_description=open('README.md').read(),
+    author='Seb Arnold',
+    author_email='smr.arnold@gmail.com',
+    url='http://www.seba1511.com',
+    download_url='https://github.com/seba-1511/plotify/archive/0.1.1.zip',
+    license='License :: OSI Approved :: Apache Software License',
+    packages=find_packages(exclude=["tests"]),
+    classifiers=[]
+)
