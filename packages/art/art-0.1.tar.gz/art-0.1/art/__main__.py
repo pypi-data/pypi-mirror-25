ba8b81ca238f190c36bf023ed910dbe8a10d2571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:06 +0000

    Add package art/0.1/art-0.1.tar.gz

---
+++ packages/art/art-0.1.tar.gz/art-0.1/art/__main__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from .art import *
+from .art_dic import *
+import sys
+import doctest
+
+if __name__=="__main__":
+    args=sys.argv
+    if len(args)>1:
+        if args[1].upper()=="TEST":
+            doctest.testfile("test.py", verbose=True)
+        elif args[1].upper()=="LIST":
+            aprint_test()
+        elif len(args)>2:
+            if args[1].upper()=="TEXT":
+                tprint(args[2])
+            elif args[1].upper()=="SHAPE":
+                aprint(args[2])
+            else:
+                help()
+        else:
+            help()
+    else:
+        help()
+
+
+
