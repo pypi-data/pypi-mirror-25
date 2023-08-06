e9e64476e1a299c815b23728bcccc7b56554e0fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:04 +0000

    Add package pyrtg/0.1.dev2/pyrtg-0.1.dev2.tar.gz

---
+++ packages/pyrtg/pyrtg-0.1.dev2.tar.gz/pyrtg-0.1.dev2/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/jonpurdy/pyrtg
+
+from setuptools import setup, find_packages
+
+# with open('README.rst') as f:
+#     readme = f.read()
+
+# with open('LICENSE') as f:
+#     license = f.read()
+
+setup(
+    name='pyrtg',
+    version='0.1dev2',
+    description='Classes for easily generating PRTG XML output.',
+    long_description='Classes for easily generating PRTG XML output.',
+    author='Jon Purdy',
+    author_email='jon+pyrtg@jonpurdy.com',
+    url='https://github.com/jonpurdy/pyrtg',
+    license='GNU General Public License v3 (GPLv3)',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
