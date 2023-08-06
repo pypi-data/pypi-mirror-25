aa06ff5d5b2c0c5b6790e973be49b48c190c3bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:12 +0000

    Add package spookyaction/0.0.5/spookyaction-0.0.5.tar.gz

---
+++ packages/spookyaction/spookyaction-0.0.5.tar.gz/spookyaction-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='spookyaction',
+    version='0.0.5',
+    author='Adam Short',
+    author_email='adam010101@icloud.com',
+    packages=['spookyaction'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description='A framework for pyautogui automation.',
+    long_description=open('README.md').read(),
+)
