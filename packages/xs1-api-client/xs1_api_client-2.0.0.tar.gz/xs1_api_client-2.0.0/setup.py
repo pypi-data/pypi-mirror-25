7ba7247d622c7ad0634d80976cd6aa5535c50eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:55 +0000

    Add package xs1-api-client/2.0.0/xs1_api_client-2.0.0.tar.gz

---
+++ packages/xs1-api-client/xs1_api_client-2.0.0.tar.gz/xs1_api_client-2.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xs1_api_client',
+    version='2.0.0',
+    description='A library to get and set values of the EZcontrol XS1 Gateway',
+    license='GPLv3+',
+    author='Markus Ressel',
+    author_email='mail@markusressel.de',
+    url='https://www.markusressel.de',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'requests',
+        # 'logging'
+    ]
+)
