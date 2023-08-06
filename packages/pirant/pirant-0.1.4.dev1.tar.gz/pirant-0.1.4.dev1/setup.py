00acdbb3e2cf84ce1c982495fd9fb29fc33d4e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:27 +0000

    Add package pirant/0.1.4.dev1/pirant-0.1.4.dev1.tar.gz

---
+++ packages/pirant/pirant-0.1.4.dev1.tar.gz/pirant-0.1.4.dev1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import find_packages
+setup(
+    name='pirant',
+    version='0.1.4dev1',
+    author = "aayush",
+    author_email='aayushsingh.mail@gmail.com',
+    url="https://github.com/aayush26/pirant",
+    license='MIT',
+    description = "DevRant wrapper in Python",
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords="devrant api wrapper",
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=[
+        "colander",
+        "requests",
+    ]
+)
