698a21ae1bd876fa34b57b1c6dea58bfc52e2be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:13 +0000

    Add package django-db-locking/2.0.3/django_db_locking-2.0.3-py2.py3-none-any.whl

---
+++ packages/django-db-locking/django_db_locking-2.0.3-py2.py3-none-any.whl/locking/models.py
@@ -0,0 +1,266 @@
+from __future__ import absolute_import
+import uuid
+
+from datetime import timedelta
+
+from django.utils import timezone
+from django.db import models, IntegrityError, transaction
+from django.db.models import Q
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+from .exceptions import NotLocked, AlreadyLocked, NonexistentLock, Expired, RenewalError
+
+
+#: The default lock age.
+MAX_AGE_FOREVER = 0
+DEFAULT_MAX_AGE = getattr(settings, 'LOCK_MAX_AGE', MAX_AGE_FOREVER)
+
+
+def _get_lock_name(obj):
+    '''
+    Gets a lock name for the object.
+
+    :param django.db.models.Model obj: the object for which we want the lock
+
+    :returns: a name for this object
+    :rtype: :class:`str`
+    '''
+    return '%s.%s__%d' % (obj.__module__, obj.__class__.__name__, obj.id)
+
+
+class LockManager(models.Manager):
+    '''
+    The manager for :class:`Lock`
+    '''
+    def acquire_lock(self, obj=None, max_age=None, lock_name=''):
+        '''
+        Acquires a lock
+
+        :param obj: the object we want to lock, this will override
+            ``lock_name``.
+        :type: :class:`django.db.models.Model` or ``None``
+        :param int max_age: the maximum age of the lock
+        :param str lock_name: the name for the lock
+        '''
+        if max_age is None:
+            max_age = getattr(settings, 'LOCK_MAX_AGE', DEFAULT_MAX_AGE)
+
+        if obj is not None:
+            lock_name = _get_lock_name(obj)
+
+        with transaction.atomic():
+            try:
+                now = timezone.now()
+
+                defaults = {'max_age': max_age,
+                            'created_on': now,
+                            'renewed_on': now,
+                            'expires_on': now + timedelta(seconds=max_age)}
+
+                lock, created = self.get_or_create(locked_object=lock_name,
+                                                   defaults=defaults)
+                if not created:
+                    # check whether lock is expired
+                    if lock.is_expired:
+                        # Create a new lock to provide a new id for renewal.
+                        # This ensures the owner of the previous lock doesn't
+                        # remain in possession of the active lock id.
+                        lock.release()
+                        lock = self.create(locked_object=lock_name, max_age=max_age)
+                    else:
+                        raise AlreadyLocked()
+
+            except IntegrityError:
+                raise AlreadyLocked()
+
+        return lock
+
+    def renew_lock(self, pk):
+        '''
+        Renews a lock
+
+        :param int pk: the primary key for the lock to renew
+        '''
+
+        try:
+            lock = self.get(pk=pk)
+        except self.model.DoesNotExist:
+            raise NonexistentLock()
+
+        lock.renew()
+
+        return lock
+
+    def release_lock(self, pk):
+        '''
+        Releases a lock
+        :param int pk: the primary key for the lock to release
+        '''
+
+        try:
+            lock = self.get(pk=pk)
+        except self.model.DoesNotExist:
+            raise NotLocked()
+
+        lock.release()
+
+        return lock
+
+    def filter_lock_for_obj(self, obj):
+        return self.filter(locked_object=_get_lock_name(obj))
+
+    def filter_active_lock_for_obj(self, obj):
+        return self.filter_lock_for_obj(obj).filter(self.not_expired_lookup)
+
+    def is_locked(self, obj):
+        '''
+        Check whether a lock exists on a certain object
+
+        :param django.db.models.Model obj: the object which we want to check
+
+        :returns: ``True`` if one exists
+        '''
+        return self.filter_active_lock_for_obj(obj).exists()
+
+    def get_expired_locks(self):
+        '''
+        Gets all expired locks
+
+        :returns: a :class:`~django.db.models.query.QuerySet` containing all
+            expired locks
+        '''
+        return self.filter(self.expired_lookup)
+
+    @property
+    def not_expired_lookup(self):
+        '''
+        locks are not expired if max_age is forever or expires_on is in the future
+
+        :returns: :class:`~from django.db.models.Q` matching all locks that are NOT expired
+        '''
+        return Q(max_age=MAX_AGE_FOREVER) | Q(expires_on__gt=timezone.now())
+
+    @property
+    def expired_lookup(self):
+        '''
+        negate the "not expired lookup"
+        :returns: :class:`~from django.db.models.Q` matching all locks that ARE expired
+        '''
+        return ~self.not_expired_lookup
+
+
+class NonBlockingLock(models.Model):
+    """A non-blocking MySQL lock
+
+    This is a workaround for the fact the MySQL does not support
+    non-blocking locks. It uses `get_or_create` with a unique index
+    for the lock name.
+
+    `select_for_update()` should be used for blocking locks.
+
+    Non-blocking locks are supported natively with
+    `select_for_update(nowait=True)` when using alternative backends
+    such as PostgreSQL.
+
+    """
+    # UUIDField is more appropriate for the lock id than AutoField
+    # and allows exposing the id client-side without leaking data
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    #: The lock name
+    locked_object = models.CharField(
+        max_length=150, verbose_name=_('locked object'), unique=True
+    )
+    #: The creation time of the lock
+    created_on = models.DateTimeField(
+        verbose_name=_('created on'), db_index=True
+    )
+    #: The renewal time of the lock
+    renewed_on = models.DateTimeField(
+        verbose_name=_('renewed on'), db_index=True
+    )
+    #: The expiration time of the lock
+    expires_on = models.DateTimeField(
+        verbose_name=_('expires on'), db_index=True
+    )
+    #: The age of a lock before it can be overwritten. If it's ``MAX_AGE_FOREVER``, it will
+    #: never expire.
+    max_age = models.PositiveIntegerField(
+        default=MAX_AGE_FOREVER, verbose_name=_('Maximum lock age'),
+        help_text=_('The age of a lock before it can be overwritten. '
+                    '%s means indefinitely.' % MAX_AGE_FOREVER)
+    )
+
+    objects = LockManager()
+
+    class Meta:
+        verbose_name = _('NonBlockingLock')
+        verbose_name_plural = _('NonBlockingLocks')
+        ordering = ['created_on']
+
+    def __unicode__(self):
+        values = {'object': self.locked_object,
+                  'creation_date': self.created_on}
+        return _('Lock exists on %(object)s since %(creation_date)s') % values
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.release(silent=True)
+
+        # Do not suppress exceptions
+        return None
+
+    def release(self, silent=True):
+        '''
+        Releases the lock
+
+        :param bool silent: if it's ``False`` it will raise an
+            :class:`~locking.exceptions.NotLocked` error.
+        '''
+        if not getattr(self, 'unlocked', False):
+            self.delete()
+            self.unlocked = True
+            return True
+        if not silent:
+            raise NotLocked()
+
+    def renew(self):
+        if self.is_expired:
+            raise Expired()
+
+        self.renewed_on = timezone.now()
+
+        try:
+            self.save()
+        except IntegrityError:
+            raise RenewalError()
+
+    @property
+    def is_expired(self):
+        '''
+        Is the lock expired?
+
+        :returns: ``True`` or ``False``
+        '''
+        if self.max_age == MAX_AGE_FOREVER:
+            return False
+        else:
+            return self.expires_on < timezone.now()
+
+
+@receiver(pre_save, sender=NonBlockingLock)
+def lock_pre_save(sender, instance, raw, **kwargs):
+    if not raw:
+        now = timezone.now()
+
+        if instance.created_on is None:
+            instance.created_on = now
+
+        if instance.renewed_on is None:
+            instance.renewed_on = now
+
+        instance.expires_on = instance.renewed_on + timedelta(seconds=instance.max_age)
---
+++ packages/django-db-locking/django_db_locking-2.0.3-py2.py3-none-any.whl/locking/tests.py
@@ -0,0 +1,181 @@
+"""
+Tests for the locking application
+"""
+from __future__ import absolute_import
+import uuid
+
+from datetime import datetime, timedelta
+from django.conf import settings
+
+from freezegun import freeze_time
+
+from django.contrib.auth.models import User
+from django.test import TestCase, override_settings
+
+from .exceptions import AlreadyLocked, RenewalError, NonexistentLock, NotLocked, Expired
+from .models import NonBlockingLock, _get_lock_name
+from .tasks import clean_expired_locks
+
+
+class NonBlockingLockTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='NonBlockingLock test')
+
+    def test_acquire_and_release(self):
+        ''' Tests an aquire/release cycle '''
+        l = NonBlockingLock.objects.acquire_lock(self.user)
+        self.assertTrue(NonBlockingLock.objects.is_locked(self.user))
+        l.release()
+        self.assertTrue(not NonBlockingLock.objects.is_locked(self.user))
+        self.assertRaises(NotLocked, l.release, silent=False)
+        l2 = NonBlockingLock.objects.acquire_lock(self.user)
+        self.assertTrue(NonBlockingLock.objects.is_locked(self.user))
+        NonBlockingLock.objects.release_lock(l2.pk)
+        self.assertTrue(not NonBlockingLock.objects.is_locked(self.user))
+
+    def test_renew_integrity_error(self):
+        l = NonBlockingLock.objects.acquire_lock(self.user)
+        self.assertTrue(NonBlockingLock.objects.is_locked(self.user))
+        NonBlockingLock.objects.release_lock(l.pk)
+        self.assertFalse(NonBlockingLock.objects.is_locked(self.user))
+        NonBlockingLock.objects.acquire_lock(self.user)
+        self.assertTrue(NonBlockingLock.objects.is_locked(self.user))
+        self.assertRaises(RenewalError, l.renew)
+
+    def test_obj_with_expired_lock_is_not_locked(self):
+        ''' Tests that manager.is_locked returns False if locks are expired '''
+        with freeze_time("2015-01-01 10:00"):
+            NonBlockingLock.objects.acquire_lock(self.user, max_age=1)
+        with freeze_time("2015-01-01 11:00"):
+            self.assertFalse(NonBlockingLock.objects.is_locked(self.user))
+
+    def test_acquire_and_renew(self):
+        ''' Tests an aquire/renew cycle '''
+        with freeze_time("2015-01-01 10:00"):
+            l = NonBlockingLock.objects.acquire_lock(self.user)
+            expires = l.expires_on
+        with freeze_time("2015-01-01 11:00"):
+            l.renew()
+            self.assertLess(expires, l.expires_on)
+
+            l2 = NonBlockingLock.objects.renew_lock(l.pk)
+            self.assertEqual(l.pk, l2.pk)
+            self.assertEqual(l.expires_on, l2.expires_on)
+
+    def test_renew_expired(self):
+        ''' Tests renew an expired lock '''
+        with freeze_time("2015-01-01 10:00"):
+            l = NonBlockingLock.objects.acquire_lock(self.user, 1)
+        self.assertRaises(Expired, l.renew)
+
+    def test_renew_nonexistinglock(self):
+        ''' Tests renewing a non existent lock '''
+        self.assertRaises(NonexistentLock, NonBlockingLock.objects.renew_lock, uuid.uuid4())
+
+    def test_release_nonexistinglock(self):
+        ''' Tests release a non existent lock '''
+        self.assertRaises(NotLocked, NonBlockingLock.objects.release_lock, uuid.uuid4())
+
+    def test_lock_twice(self):
+        ''' Tests a double locking (lock and try to lock again) '''
+        l = NonBlockingLock.objects.acquire_lock(self.user)
+        self.assertTrue(NonBlockingLock.objects.is_locked(self.user))
+        self.assertRaises(AlreadyLocked, NonBlockingLock.objects.acquire_lock, self.user)
+        self.assertTrue(NonBlockingLock.objects.is_locked(self.user))
+        l.release()
+
+    def test_unlock_twice(self):
+        ''' Tests a double unlocking (unlock and try to unlock again) '''
+        l = NonBlockingLock.objects.acquire_lock(self.user)
+        self.assertTrue(NonBlockingLock.objects.is_locked(self.user))
+        l.release()
+        self.assertTrue(not NonBlockingLock.objects.is_locked(self.user))
+        l.release()
+
+    def test_model(self):
+        '''Test the model'''
+        l = NonBlockingLock.objects.acquire_lock(self.user, max_age=10)
+        self.assertTrue(l.locked_object.endswith('%d' % self.user.id))
+        self.assertEquals(l.locked_object, _get_lock_name(self.user))
+        self.assertTrue(l.created_on)
+        self.assertEquals(l.max_age, 10)
+        l.release()
+        l = NonBlockingLock.objects.acquire_lock(lock_name='test_lock', max_age=10)
+        self.assertEquals(l.locked_object, 'test_lock')
+        self.assertIsInstance(l.id, uuid.UUID)
+
+    def test_relock(self):
+        '''Test to allow lock if lock is expired'''
+        with freeze_time("2015-01-01 10:00"):
+            l = NonBlockingLock.objects.acquire_lock(self.user, max_age=10)
+        with freeze_time("2015-01-01 11:00"):
+            self.assertTrue(l.is_expired)
+            # try to lock again
+            l2 = NonBlockingLock.objects.acquire_lock(self.user, max_age=1)
+            self.assertNotEquals(l.created_on, l2.created_on)
+
+    def test_expired(self):
+        '''Test the expired locks'''
+        with freeze_time("2015-01-01 10:00"):
+            l = NonBlockingLock.objects.acquire_lock(self.user, max_age=0)
+            l2 = NonBlockingLock.objects.acquire_lock(l, max_age=1)
+        with freeze_time("2015-01-01 11:00"):
+            self.assertTrue(not l.is_expired)
+            self.assertTrue(l2.is_expired)
+            expired_locks = NonBlockingLock.objects.get_expired_locks()
+            self.assertEquals(len(expired_locks), 1)
+
+    def test_context_manager(self):
+        """A lock can be used as a context manager"""
+        with NonBlockingLock.objects.acquire_lock(self.user):
+            self.assertTrue(NonBlockingLock.objects.is_locked(self.user))
+        self.assertFalse(NonBlockingLock.objects.is_locked(self.user))
+
+
+class CleanExpiredLocksTest(TestCase):
+    """Tests correct functioning of the task that cleans expired locks."""
+    def setUp(self):
+        self.user = User.objects.create(username='hellofoo')
+
+    def test_clean(self):
+        """Make expired lock, ensure the management command cleans it up."""
+        with freeze_time("2015-01-01 10:00"):
+            lock_to_be_released = NonBlockingLock.objects.acquire_lock(self.user, max_age=0)
+            NonBlockingLock.objects.acquire_lock(lock_to_be_released, max_age=1)
+        with freeze_time("2015-01-01 11:00"):
+            # Only the non-expired lock should remain
+            clean_expired_locks()
+            self.assertEqual(NonBlockingLock.objects.get(), lock_to_be_released)
+
+    def test_implicit_cleaning_disabled(self):
+        """If no max_age parameter is given and locks aren't configured to autoexpire, don't clean them up."""
+        assert hasattr(settings, 'LOCK_MAX_AGE') is False
+        initial_timestamp = datetime(2017, 1, 1)
+
+        with freeze_time(initial_timestamp):
+            lock_to_remain = NonBlockingLock.objects.acquire_lock(self.user)
+        with freeze_time(initial_timestamp + timedelta(days=9600)):
+            clean_expired_locks()
+            assert NonBlockingLock.objects.get() == lock_to_remain
+
+    @override_settings(LOCK_MAX_AGE=0)
+    def test_implicit_cleaning_set_to_zero(self):
+        """If the LOCK_MAX_AGE setting is set to 0, no locks should autoexpire."""
+        initial_timestamp = datetime(2017, 1, 1)
+
+        with freeze_time(initial_timestamp):
+            lock_to_remain = NonBlockingLock.objects.acquire_lock(self.user)
+        with freeze_time(initial_timestamp + timedelta(days=9600)):
+            clean_expired_locks()
+            assert NonBlockingLock.objects.get() == lock_to_remain
+
+    @override_settings(LOCK_MAX_AGE=1)
+    def test_implicit_cleaning_set_to_nonzero(self):
+        """If the LOCK_MAX_AGE setting is bigger than 0, locks should autoexpire."""
+        initial_timestamp = datetime(2017, 1, 1)
+
+        with freeze_time(initial_timestamp):
+            NonBlockingLock.objects.acquire_lock(self.user)
+        with freeze_time(initial_timestamp + timedelta(seconds=1)):
+            clean_expired_locks()
+            assert NonBlockingLock.objects.count() == 0
