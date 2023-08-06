27a97dd24ad2c998504cc7f44449e7f69c630819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:49 +0000

    Add package django-eximagination/0.8.3/django-eximagination-0.8.3.tar.gz

---
+++ packages/django-eximagination/django-eximagination-0.8.3.tar.gz/django-eximagination-0.8.3/setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+try:
+    readme = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+except:
+    readme = ''
+
+version = '0.8.3'
+
+setup(
+    name='django-eximagination',
+    version=version,
+    description="A Django template tag library which allows to download "
+                "external images, store them locally and return the local "
+                "path to locally stored image to a desired context variable",
+    long_description=readme,
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+    ],
+    keywords='eximagination, django, external images, app, python',
+    author='Artur Barseghyan',
+    author_email='artur.barseghyan@gmail.com',
+    url='https://github.com/barseghyanartur/django-eximagination',
+    package_dir={'': 'src'},
+    packages=find_packages(where='./src'),
+    license='GPL 2.0/LGPL 2.1',
+    install_requires=[
+        'six',
+        'Pillow'
+    ]
+)
---
+++ packages/django-eximagination/django-eximagination-0.8.3.tar.gz/django-eximagination-0.8.3/src/eximagination/__init__.py
@@ -0,0 +1,25 @@
+"""
+A Django template tag library which allows to download external images, store
+them locally and return the local path to locally stored image to a desired
+context variable, along with `width` and `height` of the image fetched. Caches
+the fetched images locally for the given time (set in settings). You could,
+for example, use this app to solve the problems with displaying of a mixed
+content (assets loaded from HTTP and HTTPS sources).
+
+- default_app_config: Default Django app config.
+"""
+
+from .utils import obtain_image
+
+__title__ = 'eximagination'
+__version__ = '0.8.3'
+__build__ = 0x00000b
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2013-2017 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = (
+    'obtain_image',
+    'default_app_config',
+)
+
+default_app_config = 'eximagination.apps.Config'
