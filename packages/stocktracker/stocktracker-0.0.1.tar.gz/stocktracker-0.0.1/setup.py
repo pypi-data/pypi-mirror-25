835d0c10ba664df50b67c757153259a91f5cebbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:41 +0000

    Add package stocktracker/0.0.1/stocktracker-0.0.1.tar.gz

---
+++ packages/stocktracker/stocktracker-0.0.1.tar.gz/stocktracker-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+
+from setuptools import setup
+
+setup(
+    name='stocktracker',
+    version='0.0.1',
+    description='stocktracker',
+    author='Michael Giuffrida',
+    author_email='michaelg@michaelg.us',
+    url='https://github.com/mgiuffrida/stocktracker',
+    license='MIT',
+    packages=['stocktracker']
+)
