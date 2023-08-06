51d5d34c143ff3f54fbc1d0b3555e553f0ab7902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:13 +0000

    Add package libcp2k/0.1/libcp2k-0.1.tar.gz

---
+++ packages/libcp2k/libcp2k-0.1.tar.gz/libcp2k-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+  name = 'libcp2k',
+  packages = ['libcp2k'],
+  version = '0.1',
+  author = 'Ole Schuett',
+  author_email = 'ole@cp2k.org',
+  url = 'https://www.cp2k.org',
+)
