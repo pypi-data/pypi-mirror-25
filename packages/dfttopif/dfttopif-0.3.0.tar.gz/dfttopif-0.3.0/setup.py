9e52b091bbf4e881be9e50343c3582110261ec1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:25 +0000

    Add package dfttopif/0.3.0/dfttopif-0.3.0.tar.gz

---
+++ packages/dfttopif/dfttopif-0.3.0.tar.gz/dfttopif-0.3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dfttopif',
+    version='0.3.0',
+    description='Library for parsing Density Functional Theory calculations',
+    url='https://github.com/CitrineInformatics/pif-dft',
+    install_requires=[
+        'ase',
+        'pypif==1.1.6',
+        'dftparse>=0.2.1'
+    ],
+    extras_require={
+        'report': ["requests"],
+    },
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points={
+        'citrine.dice.converter': [
+            'dft = dfttopif'
+        ]
+    }
+)
