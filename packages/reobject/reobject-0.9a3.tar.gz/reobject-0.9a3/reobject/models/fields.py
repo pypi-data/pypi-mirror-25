32c7056e96dd1ea2d2eb09c33625b65a4316bdc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:56 +0000

    Add package reobject/0.9a3/reobject-0.9a3.tar.gz

---
+++ packages/reobject/reobject-0.9a3.tar.gz/reobject-0.9a3/reobject/models/fields.py
@@ -0,0 +1,19 @@
+import attr
+
+
+def Field(*args, default=attr.NOTHING, **kwargs):
+    if callable(default):
+        default = attr.Factory(default)
+
+    return attr.ib(*args, default=default, **kwargs)
+
+
+def ManyToManyField(cls, *args, **kwargs):
+    metadata = {
+        'related': {
+            'target': cls,
+            'type': 'ManyToMany',
+        }
+    }
+
+    return attr.ib(*args, metadata=metadata, **kwargs)
