9686875f4a1516c304842d7bc4807deaa7673b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:36 +0000

    Add package five9/0.0.2b87/five9-0.0.2b87-py3-none-any.whl

---
+++ packages/five9/five9-0.0.2b87-py3-none-any.whl/five9/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-TODAY LasLabs Inc.
+# License MIT (https://opensource.org/licenses/MIT).
+
+from .key_value_pair import KeyValuePair
+from .web_connector import WebConnector
+
+__all__ = [
+    'KeyValuePair',
+    'WebConnector',
+]
