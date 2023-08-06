86d8f275790d56b0f2551a7f648587780d542429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:52 +0000

    Add package rpi-featureSelection-python-tools/1.0.8/rpi_featureSelection_python_tools-1.0.8.tar.gz

---
+++ packages/rpi-featureSelection-python-tools/rpi_featureSelection_python_tools-1.0.8.tar.gz/rpi_featureSelection_python_tools-1.0.8/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+	name='rpi_featureSelection_python_tools',  # This is the name of your PyPI-package. 
+	version= '1.0.8',  # Update the version number for new releases
+	author= 'Keyi Liu',
+	packages=['rpi_featureSelection_python_tools']
+)
