d5116b6b3288545b5b271d1bd71a737b05ad5f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:43 +0000

    Add package wagtailapproval/0.2.7/wagtailapproval-0.2.7-py3-none-any.whl

---
+++ packages/wagtailapproval/wagtailapproval-0.2.7-py3-none-any.whl/wagtailapproval/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+default_app_config = 'wagtailapproval.apps.WagtailApprovalConfig'
+
+__author__ = 'Taylor C. Richberger <tcr@absolute-performance.com>'
+__description__ = 'Wagtail Approval pipeline'
+__email__ = 'tcr@absolute-performance.com'
+__license__ = 'BSD 2-Clause'
+__modulename__ = 'wagtailapproval'
+__version__ = '0.2.7'
+__website__ = 'https://github.com/absperf/wagtailapproval'
