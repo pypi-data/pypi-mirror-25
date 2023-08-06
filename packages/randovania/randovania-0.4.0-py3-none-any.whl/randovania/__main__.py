0d594c0d88b59565f825016fd1e6919dd3041a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:10 +0000

    Add package randovania/0.4.0/randovania-0.4.0-py3-none-any.whl

---
+++ packages/randovania/randovania-0.4.0-py3-none-any.whl/randovania/__main__.py
@@ -0,0 +1,21 @@
+import argparse
+import os
+
+from randovania import cli
+from randovania.resolver import resolver, data_reader
+from randovania.games.prime import log_parser
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    cli.create_subparsers(parser.add_subparsers(dest="game"))
+    args = parser.parse_args()
+    if args.game is None:
+        parser.print_help()
+        raise SystemExit(1)
+
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
