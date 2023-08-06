f64dcf64d9890ead43d9a520cf82433752eda206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:48 +0000

    Add package rwkt-nester/1.0.0/rwkt_nester-1.0.0.zip

---
+++ packages/rwkt-nester/rwkt_nester-1.0.0.zip/rwkt_nester-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name		= 'rwkt_nester',
+	version		= '1.0.0',
+	py_modules	= ['rwkt_nester'],
+	author		= 'rwktpython',
+	description	= 'A simple printer of nester lists',
+)
