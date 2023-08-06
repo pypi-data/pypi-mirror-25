59f0ef067d5dec2559197845bccc9a21464101e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:25 +0000

    Add package ivy/0.9.0/ivy-0.9.0.tar.gz

---
+++ packages/ivy/ivy-0.9.0.tar.gz/ivy-0.9.0/ivy/ext/ivy_syntex.py
@@ -0,0 +1,19 @@
+# --------------------------------------------------------------------------
+# This extension adds support for source files in Syntex format.
+# --------------------------------------------------------------------------
+
+import ivy
+
+try:
+    import syntex
+except ImportError:
+    syntex = None
+
+
+# The syntex package is an optional dependency.
+if syntex:
+
+    # Register a callback to render files with a .stx extension.
+    @ivy.renderers.register('stx')
+    def render(text):
+        return syntex.render(text, pygmentize=True)
