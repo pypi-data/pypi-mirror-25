59c8df022d4279c75601966a3d46ce9c84bdab91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:56 +0000

    Add package martian-chatbot/0.2.6/martian_chatbot-0.2.6.tar.gz

---
+++ packages/martian-chatbot/martian_chatbot-0.2.6.tar.gz/martian_chatbot-0.2.6/chatbot/models/facebook_message.py
@@ -0,0 +1,8 @@
+# from chatbot.models.user_input_message import UserInputMessage
+
+from models.user_input_message import UserInputMessage
+
+class FacebookMessage(UserInputMessage):
+
+    def message_service(self):
+        return 'facebook'
