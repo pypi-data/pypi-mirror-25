488b08f10444d09b4ebb0915e23b8a1a6091badc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:34 +0000

    Add package MPInterfaces_Latest/1.9.2/MPInterfaces_Latest-1.9.2.tar.gz

---
+++ packages/MPInterfaces_Latest/MPInterfaces_Latest-1.9.2.tar.gz/MPInterfaces_Latest-1.9.2/mpinterfaces/mat2d/__init__.py
@@ -0,0 +1,8 @@
+from mpinterfaces import set_config 
+
+MPINT_CONFIG, MP_API, VASP_PSP, VASP_STD_BIN, VASP_TWOD_BIN,\
+USERNAME, VDW_KERNEL, QUEUE_SYSTEM, MPR = set_config()
+
+__author__ = "Michael Ashton, Joshua J. Gabriel, Joshua T. Paul, " \
+             "Seve G. Monahan"
+__version__ = "1.7.0"
