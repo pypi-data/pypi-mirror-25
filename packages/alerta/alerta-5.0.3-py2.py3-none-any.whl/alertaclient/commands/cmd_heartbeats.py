9f663039c17519970231c669ee650505809c3270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:35 +0000

    Add package alerta/5.0.3/alerta-5.0.3-py2.py3-none-any.whl

---
+++ packages/alerta/alerta-5.0.3-py2.py3-none-any.whl/alertaclient/commands/cmd_heartbeats.py
@@ -0,0 +1,25 @@
+
+import click
+
+from tabulate import tabulate
+
+
+@click.command('heartbeats', short_help='List heartbeats')
+@click.option('--purge', is_flag=True, help='Delete stale heartbeats')
+@click.pass_obj
+def cli(obj, purge):
+    """List heartbeats."""
+    client = obj['client']
+    timezone = obj['timezone']
+    headers = {
+        'id': 'ID', 'origin': 'ORIGIN', 'customer': 'CUSTOMER', 'tags': 'TAGS', 'createTime': 'CREATED',
+        'receiveTime': 'RECEIVED', 'latency': 'LATENCY', 'timeout': 'TIMEOUT', 'since': 'SINCE', 'status': 'STATUS'
+    }
+    heartbeats = client.get_heartbeats()
+    click.echo(tabulate([h.tabular(timezone) for h in heartbeats], headers=headers, tablefmt=obj['output']))
+
+    expired = [hb for hb in heartbeats if hb.status == 'expired']
+    if purge:
+        with click.progressbar(expired, label='Purging {} heartbeats'.format(len(expired))) as bar:
+            for b in bar:
+                client.delete_heartbeat(b.id)
