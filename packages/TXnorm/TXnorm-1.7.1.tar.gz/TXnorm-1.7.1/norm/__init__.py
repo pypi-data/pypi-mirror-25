d6ef42f41039d68827a03d1634ca85db88fbbdf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:11 +0000

    Add package TXnorm/1.7.1/TXnorm-1.7.1.tar.gz

---
+++ packages/TXnorm/TXnorm-1.7.1.tar.gz/TXnorm-1.7.1/norm/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Matt Haggard.
+# See LICENSE for details.
+
+__all__ = ['__version__', 'makePool', 'insert', 'ormHandle']
+
+__version__ = '1.7.1'
+from norm.porcelain import makePool, insert, ormHandle
