f17705722604e5d4e50854ccce635de0b07d1d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:39 +0000

    Add package nailpack-flask/0.1.0/nailpack-flask-0.1.0.tar.gz

---
+++ packages/nailpack-flask/nailpack-flask-0.1.0.tar.gz/nailpack-flask-0.1.0/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+install_requires = list()
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    for line in f.readlines():
+        install_requires.append(line)
+
+setup(
+    name='nailpack-flask',
+
+    version='0.1.0',
+
+    description='Flask support for nails',
+
+    long_description=long_description,
+
+    url='https://github.com/jamrizzi/nailpack-flask',
+
+    author='Jam Risser',
+
+    author_email='jam@jamrizzi.com',
+
+    license='MIT',
+
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Framework :: Flask',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='mvc api rest framework flask rails trails sails development',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=install_requires
+)
