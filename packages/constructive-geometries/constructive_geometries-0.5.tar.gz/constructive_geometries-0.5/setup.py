70a761661e44750aa9d923212c2b7c81ffd8eb56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:23 +0000

    Add package constructive-geometries/0.5/constructive_geometries-0.5.tar.gz

---
+++ packages/constructive-geometries/constructive_geometries-0.5.tar.gz/constructive_geometries-0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import os
+
+setup(
+    name='constructive_geometries',
+    version="0.5",
+    packages=["constructive_geometries"],
+    package_data={'constructive_geometries': [
+        "data/faces.gpkg",
+        "data/faces.json"
+    ]},
+    author="Chris Mutel",
+    author_email="cmutel@gmail.com",
+    license=open('LICENSE.txt', encoding='utf-8').read(),
+    install_requires=["wrapt",],
+    url="https://bitbucket.org/cmutel/py-constructive-geometries",
+    long_description=open('README.rst', encoding='utf-8').read(),
+    description='Tool to build new geometrical definitions from the ecoinvent topology',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
+)
