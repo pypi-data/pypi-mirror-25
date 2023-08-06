d109131e9328897c171efe8ff22135c29f55d78f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:14 +0000

    Add package mali/0.573/mali-0.573-py2.py3-none-any.whl

---
+++ packages/mali/mali-0.573-py2.py3-none-any.whl/mali_commands/legit/ref_container/__init__.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+from .mysql_ref_container import MySqlRefsContainer
+from .spanner_ref_container import SpannerRefContainer
+from .datastore_ref_container import DatastoreRefContainer
