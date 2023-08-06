9b6baae0fab0a22c79629afa6f436409718c3273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:02 +0000

    Add package lifepo4weredPy/0.1.1/lifepo4weredPy-0.1.1.tar.gz

---
+++ packages/lifepo4weredPy/lifepo4weredPy-0.1.1.tar.gz/lifepo4weredPy-0.1.1/setup.py
@@ -0,0 +1,56 @@
+"""
+Publish a new version:
+Change version in package __init__ file
+Change version in this setup file
+Execute:
+$ git tag X.Y.Z -m "Release X.Y.Z"
+$ git push --tags
+$ pip install --upgrade twine wheel
+$ python setup.py sdist bdist_wheel --universal
+$ twine upload dist/*
+"""
+import codecs
+import os
+import sys
+from setuptools import setup, find_packages
+
+
+NAME = 'lifepo4weredPy'
+GITHUB_NAME = 'Lifepo4weredPy'
+VERSION = '0.1.1'
+DOWNLOAD_URL = (
+    'https://github.com/fredericklussier/' + GITHUB_NAME + '/' + VERSION
+)
+
+
+def read_file(filename):
+    """
+    Read a utf8 encoded text file and return its contents.
+    """
+    with codecs.open(filename, 'r', 'utf8') as f:
+        return f.read()
+
+
+setup(
+    name=NAME,
+    packages=[NAME],
+    version=VERSION,
+    description='A wrapper to enable lifepo4wered SO library to Python.',
+    long_description=read_file('ReadMe.rst'),
+    license='MIT',
+    author='Frederick Lussier',
+    author_email='frederick.lussier@hotmail.com',
+    url='https://github.com/fredericklussier/' + GITHUB_NAME,
+    download_url=DOWNLOAD_URL,
+    keywords=[
+        'wrapper', 'raspberry-pi', 'raspberrypi', 'lifepo4wered', 'battery'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Natural Language :: English',
+    ],
+)
