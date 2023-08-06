8daa2bf626919f40005260c2aabfe81e9fabdfde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:13 +0000

    Add package gpapi/0.1.2/gpapi-0.1.2.tar.gz

---
+++ packages/gpapi/gpapi-0.1.2.tar.gz/gpapi-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='gpapi',
+    version='0.1.2',
+    description='Unofficial python api for google play',
+    url='https://github.com/NoMore201/googleplay-api',
+    author='NoMore201',
+    author_email='domenico.iezzi.201@gmail.com',
+    license='MIT',
+    packages=['gpapi'],
+    package_data={
+        'gpapi': ['device.properties'],
+    },
+    install_requires=['pycrypto',
+                     'protobuf',
+                     'requests'],
+)
