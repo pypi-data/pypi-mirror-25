78c246706216b0a569771e7f64e7aee8ffb6547b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:01 +0000

    Add package command-lifecycle/1.0.0/command_lifecycle-1.0.0-py3-none-any.whl

---
+++ packages/command-lifecycle/command_lifecycle-1.0.0-py3-none-any.whl/command_lifecycle/timeout.py
@@ -0,0 +1,31 @@
+import abc
+
+
+class BaseTimeoutManager(abc.ABC):
+    allowed_silent_frames = abc.abstractproperty()
+    elapsed_silent_frames = 0
+
+    def increment(self):
+        self.elapsed_silent_frames += 1
+
+    def reset(self):
+        self.elapsed_silent_frames = 0
+
+    def has_timedout(self) -> bool:
+        return self.elapsed_silent_frames >= self.allowed_silent_frames
+
+    @property
+    def remaining_silent_frames(self):
+        return self.allowed_silent_frames - self.elapsed_silent_frames
+
+
+class ShortTimeoutManager(BaseTimeoutManager):
+    allowed_silent_frames = 10
+
+
+class MediumTimeoutManager(BaseTimeoutManager):
+    allowed_silent_frames = 15
+
+
+class LongTimeoutManager(BaseTimeoutManager):
+    allowed_silent_frames = 20
