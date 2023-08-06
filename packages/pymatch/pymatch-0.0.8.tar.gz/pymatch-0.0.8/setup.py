b2646bfc9fca7c57a4ad4291421f3edd92172bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:57 +0000

    Add package pymatch/0.0.8/pymatch-0.0.8.tar.gz

---
+++ packages/pymatch/pymatch-0.0.8.tar.gz/pymatch-0.0.8/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+dependencies =[
+  'seaborn',
+  'statsmodels',
+  'scipy',
+  'patsy',
+  'matplotlib',
+  'pandas', 
+  'numpy'
+  ]
+
+setup(
+  name = 'pymatch',
+  packages = ['pymatch'],
+  version = '0.0.8',
+  description = 'Matching techniques for Observational Studies',
+  author = 'Ben Miroglio',
+  author_email = 'benmiroglio@gmail.com',
+  url = 'https://github.com/benmiroglio/pymatch', 
+  download_url = 'https://github.com/benmiroglio/pymatch/archive/0.0.8.tar.gz', 
+  keywords = ['logistic', 'regression', 'matching', 'observational', 'study', 'causal', 'inference'],
+  classifiers = [],
+  requires=dependencies,
+  provides=['utils', 'sys']
+)
