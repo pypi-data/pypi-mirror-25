9e17e50c8c333bd27fb70c85f7c7f055c5fffece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:34 +0000

    Add package django-applepodcast/0.3.2/django-applepodcast-0.3.2.tar.gz

---
+++ packages/django-applepodcast/django-applepodcast-0.3.2.tar.gz/django-applepodcast-0.3.2/docs/_build/html/_sources/resources.rst.txt
@@ -0,0 +1,42 @@
+.. _resources:
+
+Resources
+*********
+
+Some of the helpful resources used to create the app.
+
+Documentation
+=============
+
+* `Django's syndication feed framework <https://docs.djangoproject.com/en/1.11/ref/contrib/syndication/>`_
+
+Specifications
+==============
+
+* `RSS 2.0 specification <https://cyber.harvard.edu/rss/rss.html>`_
+* `Podcasts Connect help (iTunes-specific tags) <https://help.apple.com/itc/podcasts_connect/#/itcb54353390>`_
+* `iOS 11 specification update <http://podcasts.apple.com/resources/spec/ApplePodcastsSpecUpdatesiOS11.pdf>`_
+
+Validators
+==========
+
+* `W3C Feed Validation Service <https://validator.w3.org/feed/>`_
+* `Podba.se Podcast Validator <http://podba.se/validate/>`_
+* `Cast Feed Validator <http://castfeedvalidator.com/>`_
+* `Feed Validator <http://www.feedvalidator.org/>`_
+
+Resources
+=========
+
+* `iTunes Connect resources <https://itunespartner.apple.com/en/podcasts/overview>`_
+* `Podcasts Connect help <https://help.apple.com/itc/podcasts_connect/>`_
+
+Marketing
+=========
+
+* `Apple Podcasts Identity Guidelines <https://www.apple.com/itunes/marketing-on-podcasts/identity-guidelines.html>`_
+
+Submission
+==========
+
+* `iTunes Connect <https://itunesconnect.apple.com/login>`_
---
+++ packages/django-applepodcast/django-applepodcast-0.3.2.tar.gz/django-applepodcast-0.3.2/podcast/admin.py
@@ -0,0 +1,76 @@
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from .models import Speaker, Category, Show, Episode, Enclosure
+
+
+class EnclosureInline(admin.TabularInline):
+    model = Enclosure
+    fields = ('file', 'type', 'poster', 'cc', 'get_duration',)
+    readonly_fields = ('get_duration',)
+
+
+class SpeakerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email',)
+
+
+class CateogryAdmin(admin.ModelAdmin):
+    list_display = ('full', 'parent',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_latest_duration', 'get_latest_pub_date',)
+    prepopulated_fields = {'slug': ('title',)}
+    filter_horizontal = ('categories', 'hosts',)
+    fieldsets = (
+        (None, {
+            'fields': ('type', 'title', 'slug', 'image', 'description',)
+        }),
+        ('iTunes', {
+            'fields': ('subtitle', 'summary', 'author_name', 'author_email', 'owner_name', 'owner_email', 'copyright', 'categories', 'explicit', 'block', 'complete', 'itunes',),
+        }),
+        ('Arrivals', {
+            'fields': ('coming',),
+        }),
+        ('Departures', {
+            'fields': ('going',),
+        }),
+        ('Speakers', {
+            'fields': ('hosts',),
+        }),
+    )
+
+
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_duration', 'pub_date', 'show', 'explicit',)
+    list_filter = ('show',)
+    prepopulated_fields = {'slug': ('title',)}
+    fieldsets = (
+        (None, {
+            'fields': ('show', 'title', 'slug', 'description', 'pub_date',)
+        }),
+        ('iTunes', {
+            'fields': ('type', 'season', 'number', 'itunes_title', 'summary', 'notes', 'author_name', 'author_email', 'image', 'explicit', 'block',),
+        }),
+        ('Speakers', {
+            'fields': ('hosts', 'guests',),
+        }),
+    )
+    inlines = [
+        EnclosureInline,
+    ]
+
+
+class EnclosureAdmin(admin.ModelAdmin):
+    list_display = ('episode', 'get_duration', 'type', 'file',)
+    fields = ('episode', 'type', 'file', 'poster', 'cc', 'get_duration',)
+    readonly_fields = ('get_duration',)
+
+
+admin.site.register(Speaker, SpeakerAdmin)
+admin.site.register(Category, CateogryAdmin)
+admin.site.register(Show, ShowAdmin)
+admin.site.register(Episode, EpisodeAdmin)
+admin.site.register(Enclosure, EnclosureAdmin)
