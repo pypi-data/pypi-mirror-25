67967e334c1e91ea0be6bc619aa630e8d57bd72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:41 +0000

    Add package pressenter2exit/0.0.3/pressenter2exit-0.0.3.tar.gz

---
+++ packages/pressenter2exit/pressenter2exit-0.0.3.tar.gz/pressenter2exit-0.0.3/setup.py
@@ -0,0 +1,26 @@
+
+import os
+from setuptools import setup
+
+import pressenter2exit
+
+application_name = 'pressenter2exit'
+
+with open(os.path.join('docs', 'source', 'index.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(
+    name=application_name,
+    description='Facilitates exit of a Python CLI program in a controlled way',
+    long_description=long_description,
+    version=pressenter2exit.__version__,
+    author='James Abel',
+    author_email='j@abel.co',
+    license='MIT License',
+    url='https://github.com/jamesabel/pressenter2exit',
+    download_url='https://github.com/jamesabel/pressenter2exit/archive/master.zip',
+    keywords=['cli', 'exit', 'control'],
+    packages=[application_name],
+    install_requires=[],
+    classifiers=[]
+)
