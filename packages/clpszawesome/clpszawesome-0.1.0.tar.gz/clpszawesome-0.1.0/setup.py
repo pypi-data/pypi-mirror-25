cdd569e345bda4281bd8636e1bc11bc317a95760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:40 +0000

    Add package clpszawesome/0.1.0/clpszawesome-0.1.0.tar.gz

---
+++ packages/clpszawesome/clpszawesome-0.1.0.tar.gz/clpszawesome-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='clpszawesome',
+    version='0.1.0',
+    author='Peter Zuo',
+    author_email='pengclpsz@gmail.com',
+    packages=['clpszawesome'],
+    url='https://github.com/clpsz',
+    license='LICENSE.txt',
+    description='first pypi'
+)
