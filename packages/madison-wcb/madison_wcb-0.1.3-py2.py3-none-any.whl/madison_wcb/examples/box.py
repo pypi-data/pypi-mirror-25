84e927f431e065cd40dc3856cc00799c0d60943a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:18 +0000

    Add package madison-wcb/0.1.3/madison_wcb-0.1.3-py2.py3-none-any.whl

---
+++ packages/madison-wcb/madison_wcb-0.1.3-py2.py3-none-any.whl/madison_wcb/examples/box.py
@@ -0,0 +1,22 @@
+from madison_wcb.wcb import *
+
+
+def box():
+    initialize()
+
+    get_color(2)
+
+    move_to(-10, 10)
+    point_in_direction(0)
+    brush_down()
+
+    for i in range(4):
+        move_forward(20)
+        turn_right(90)
+
+    cleanup()
+    input("Done! Press Enter to close the program.")
+
+
+if __name__ == "__main__":
+    box()
