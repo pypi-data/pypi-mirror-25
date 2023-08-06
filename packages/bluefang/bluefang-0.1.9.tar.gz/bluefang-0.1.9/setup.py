8d63f1003cab1b9353dde64c0c7676698c0ed000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:53 +0000

    Add package bluefang/0.1.9/bluefang-0.1.9.tar.gz

---
+++ packages/bluefang/bluefang-0.1.9.tar.gz/bluefang-0.1.9/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages # type: ignore
+import sys
+
+setup(
+    name='bluefang',
+    version='0.1.9',
+    description='Bluetooth and HID utilities for Python 3',
+    author='Todd McNeal',
+    author_email='todd.mcneal@gmail.com',
+    keywords = ['bluetooth', 'hid', 'l2cap', 'bluez', 'pybluez', 'dbus'],
+    url='https://www.github.com/tmcneal/bluefang',
+    packages=find_packages(exclude=['scripts', 'tests']),
+    python_requires='>=3.4',
+    install_requires=[
+        'dbus-python==1.2.4',
+        'PyBluez==0.22',
+        'vext.gi==0.5.20'
+    ]
+)
