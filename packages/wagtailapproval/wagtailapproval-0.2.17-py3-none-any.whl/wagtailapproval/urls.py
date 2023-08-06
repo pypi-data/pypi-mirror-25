76a87c4659de2ebca7d572d59cb716621252fecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:19 +0000

    Add package wagtailapproval/0.2.17/wagtailapproval-0.2.17-py3-none-any.whl

---
+++ packages/wagtailapproval/wagtailapproval-0.2.17-py3-none-any.whl/wagtailapproval/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+
+from .views import approve, index, reject
+
+UUID_REGEX = (
+    '{hex}{{8}}' +
+    ('-?{hex}{{4}}' * 3) +
+    '-?{hex}{{12}}').format(hex='[0-9a-fA-F]')
+
+app_name = 'wagtailapproval'
+urlpatterns = [
+    url(r'^$', index, name='index'),
+    url(r'^(?P<pk>{uuid})/approve/$'.format(uuid=UUID_REGEX),
+        approve,
+        name='approve'),
+    url(r'^(?P<pk>{uuid})/reject/$'.format(uuid=UUID_REGEX),
+        reject,
+        name='reject'),
+]
