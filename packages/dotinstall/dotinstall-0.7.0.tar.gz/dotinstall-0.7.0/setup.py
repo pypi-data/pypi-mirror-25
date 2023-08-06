5a18a2e66b13ae1fc37c8e0bd026e5c852635201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:31 +0000

    Add package dotinstall/0.7.0/dotinstall-0.7.0.tar.gz

---
+++ packages/dotinstall/dotinstall-0.7.0.tar.gz/dotinstall-0.7.0/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='dotinstall',
+    packages=[
+        'dotinstall',
+        'dotinstall.plugins',
+        'dotinstall.installer',
+        'dotinstall.util',
+    ],
+    install_requires=[
+        'PyYAML',
+    ],
+    version='0.7.0',
+    description='Command-line tool to help install and setup your dotfiles.',
+    author='Jeffrey Xiao',
+    author_email='jeffrey.xiao1998@gmail.com',
+    url='https://github.com/jeffrey-xiao/dotinstall',
+    download_url='https://github.com/jeffrey-xiao/dotinstall/archive/0.7.0.tar.gz',
+    keywords=['dotfiles', 'commandline'],
+    classifiers=[],
+    entry_points={
+        'console_scripts': [
+            'dotinstall=dotinstall.dotinstall:main',
+        ],
+    },
+)
