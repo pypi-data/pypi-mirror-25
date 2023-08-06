c2e6990f020f317a153cebd8e429b80fec30f129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:48 +0000

    Add package mountwizzard/2.5.6.1/mountwizzard-2.5.6.1.tar.gz

---
+++ packages/mountwizzard/mountwizzard-2.5.6.1.tar.gz/mountwizzard-2.5.6.1/setup.py
@@ -0,0 +1,23 @@
+# from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='mountwizzard',
+    version='2.5.6.1',
+    packages=[
+        # '',
+    ],
+    python_requires='~=3.5',
+    install_requires=[
+        # 'PyQt5>=5.6',                 # problem on ubuntu, can't be installed via pip, should be done with apt-get install
+        'matplotlib>=1.5.3',            # sudo apt-get install libfreetype6-dev might be needed
+        # 'pypiwin32>=219',             # not useful for linux
+        'pyfits>=3.4',
+        'wakeonlan>=0.2.2'
+    ],
+    url='https://pypi.python.org/pypi/mountwizzard',
+    license='APL 2.0',
+    author='mw',
+    author_email='michael@wuertenberger.org',
+    description='tooling for a 10micron mount',
+)
