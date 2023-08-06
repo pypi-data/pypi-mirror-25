ff393bc3d12c8f7ee873931a2b1af0c73e034d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:55 +0000

    Add package PyMUSE/0.1.8/PyMUSE-0.1.8.tar.gz

---
+++ packages/PyMUSE/PyMUSE-0.1.8.tar.gz/PyMUSE-0.1.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup 
+#from Cython.Build import cythonize
+
+setup(
+    name="PyMUSE",
+    version="0.1.8",
+    description="Python software for handling VLT/MUSE data.",
+    author="I. Pessa",
+    license="MIT",
+    author_email="ismael.pessa@gmail.com",
+    url="https://github.com/ismaelpessa/Muse_Cube",
+    packages=['PyMUSE'],
+    install_requires=[
+        'astropy',
+        'scipy',
+        'matplotlib',
+        'aplpy',
+        'h5py',
+        'pyregion',
+        'numpy',
+        'linetools'
+        ],
+    dependency_links=[
+        'https://github.com/linetools/linetools/tarball/master#egg=linetools'])
