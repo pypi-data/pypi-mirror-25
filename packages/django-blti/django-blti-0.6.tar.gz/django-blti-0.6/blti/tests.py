ebd546ea2088051ec997e7aca2d306d819c54e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:49 +0000

    Add package django-blti/0.6/django-blti-0.6.tar.gz

---
+++ packages/django-blti/django-blti-0.6.tar.gz/django-blti-0.6/blti/tests.py
@@ -0,0 +1,67 @@
+from django.test import TestCase
+from django.core.exceptions import ImproperlyConfigured
+from blti.validators import BLTIOauth, BLTIRoles
+from blti.crypto import aes128cbc
+from blti import BLTI, BLTIException
+
+
+class BLTIOAuthTest(TestCase):
+    def test_no_config(self):
+        self.assertRaises(ImproperlyConfigured, BLTIOauth)
+
+    def test_no_consumer(self):
+        with self.settings(LTI_CONSUMERS={}):
+            self.assertRaises(BLTIException, BLTIOauth().get_consumer, 'XYZ')
+
+        with self.settings(LTI_CONSUMERS={'ABC': '12345'}):
+            self.assertRaises(BLTIException, BLTIOauth().get_consumer, 'XYZ')
+
+    def test_get_consumer(self):
+        with self.settings(LTI_CONSUMERS={'ABC': '12345'}):
+            self.assertEquals(BLTIOauth().get_consumer('ABC').secret, '12345')
+
+
+class BLTIRolesTest(TestCase):
+    def test_has_admin_role(self):
+        self.assertEquals(
+            True, BLTIRoles().has_admin_role(['Faculty', 'Alumni']))
+        self.assertEquals(False, BLTIRoles().has_admin_role(['Guest']))
+
+    def test_has_instructor_role(self):
+        self.assertEquals(
+            True, BLTIRoles().has_instructor_role(['TeachingAssistant']))
+        self.assertEquals(
+            False, BLTIRoles().has_instructor_role(['Faculty', 'Alumni']))
+
+    def test_has_learner_role(self):
+        self.assertEquals(
+            True, BLTIRoles().has_learner_role(['Member']))
+        self.assertEquals(
+            False, BLTIRoles().has_learner_role(['Faculty', 'Staff']))
+
+    def test_validate(self):
+        blti = {'roles': 'Member'}
+        self.assertEquals(None, BLTIRoles().validate(blti, 'member'))
+        self.assertRaises(
+            BLTIException, BLTIRoles().validate, blti, 'admin')
+
+        blti = {'roles': 'Faculty,Staff'}
+        self.assertEquals(None, BLTIRoles().validate(blti, 'member'))
+        self.assertEquals(None, BLTIRoles().validate(blti, 'admin'))
+
+
+class CryptoTest(TestCase):
+    test_key = 'DUMMY_KEY_FOR_TESTING_1234567890'
+    test_iv = 'DUMMY_IV_TESTING'
+    msgs = [
+        ('LTI provides a framework through which an LMS can send some '
+         'verifiable information about a user to a third party.'),
+        "'abc': {'key': value}"
+    ]
+
+    def test_encrypt_decrypt(self):
+        aes = aes128cbc(self.test_key, self.test_iv)
+
+        for msg in self.msgs:
+            enc = aes.encrypt(msg)
+            self.assertEquals(aes.decrypt(enc), msg)
