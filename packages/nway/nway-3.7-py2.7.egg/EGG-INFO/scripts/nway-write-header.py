aefdbe40ddfba14ef312f31c821094b2bd19e039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:25 +0000

    Add package nway/3.7/nway-3.7-py2.7.egg

---
+++ packages/nway/nway-3.7-py2.7.egg/EGG-INFO/scripts/nway-write-header.py
@@ -0,0 +1,22 @@
+import astropy.io.fits as pyfits
+import sys
+
+if len(sys.argv) != 4:
+	sys.stderr.write("""SYNOPSIS: %s <catalogue.fits> <tablename> <skyarea>
+
+tablename: name of the catalogue
+skyarea: catalogue area in square degrees
+
+Author: Johannes Buchner (C) 2014-2017
+""" % sys.argv[0])
+	sys.exit(1)
+
+f = pyfits.open(sys.argv[1])
+print 'current', f[1].name, 'SKYAREA:', f[1].header.get('SKYAREA', None)
+f[1].name = sys.argv[2]
+f[1].header['SKYAREA'] = float(sys.argv[3])
+print 'new    ', f[1].name, 'SKYAREA:', f[1].header.get('SKYAREA', None)
+f.writeto(sys.argv[1], clobber=True)
+
+
+
