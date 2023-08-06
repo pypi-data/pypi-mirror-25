7324d4be1c2d0500f2d613204377314f40389a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:20 +0000

    Add package kcem/2.1/kcem-2.1.tar.gz

---
+++ packages/kcem/kcem-2.1.tar.gz/kcem-2.1/kcem/views.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from django.http import JsonResponse
+from djangoApiDec.djangoApiDec import queryString_required
+from kcem import KCEM
+from udic_nlp_API.settings_database import uri
+from django.shortcuts import render
+from kcem.utils.utils import criteria
+
+# Create your views here.
+@queryString_required(['lang', 'keyword', 'kcm', 'kem'])
+def kcem(request):
+    import json, requests
+    """Generate list of term data source files
+    Returns:
+        if contains invalid queryString key, it will raise exception.
+    """
+    keyword = request.GET['keyword']
+    lang = request.GET['lang']
+    kcm = request.GET['kcm']
+    kem = request.GET['kem']
+    k = KCEM(uri)
+    return JsonResponse(k.get(keyword, lang, num = int(request.GET['num']) if 'num' in request.GET else 10, kem_topn_num=kem, kcm_topn_num=kcm), safe=False)
+
+# Create your views here.
+@queryString_required(['keyword'])
+def kcem_new(request):
+    """Generate list of term data source files
+    Returns:
+        if contains invalid queryString key, it will raise exception.
+    """
+    keyword = request.GET['keyword']
+    kcm = request.GET['kcm'] if 'kcm' in request.GET else 22
+    kem = request.GET['kem'] if 'kem' in request.GET else 12
+
+    k = KCEM(uri)
+    kcemAns = k.get(keyword, 'cht', num = 10, kem_topn_num=kem, kcm_topn_num=kcm)
+
+    result = criteria('hybrid', kcemAns, keyword)
+    return JsonResponse(result, safe=False)
