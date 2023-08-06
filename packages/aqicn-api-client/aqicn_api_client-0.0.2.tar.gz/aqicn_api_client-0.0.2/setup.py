ce173ca3d455d10b6c73131b084024c629a7d592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:28 +0000

    Add package aqicn-api-client/0.0.2/aqicn_api_client-0.0.2.tar.gz

---
+++ packages/aqicn-api-client/aqicn_api_client-0.0.2.tar.gz/aqicn_api_client-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='aqicn_api_client',
+    author="Krzysztof Bober",
+    author_email="krizzb@gmail.com",
+    version='0.0.2',
+    packages = ["aqicn_api_client"]
+)
