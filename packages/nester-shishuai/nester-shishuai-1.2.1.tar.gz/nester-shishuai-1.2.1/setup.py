4a77447506a8dc222159d8c6f3e0562146760ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:01 +0000

    Add package nester-shishuai/1.2.1/nester-shishuai-1.2.1.tar.gz

---
+++ packages/nester-shishuai/nester-shishuai-1.2.1.tar.gz/nester-shishuai-1.2.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester-shishuai',
+    version='1.2.1',
+    py_modules= ['nester-ss'],
+    author='wss',
+    author_email='shishuaiwang@icloud.com',
+    url='http://www.shishuai.tech',
+    description='A simple printer of nested lists',
+)
