6c9be932ee8943aff7043175ebe6a922ddeea490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:19 +0000

    Add package CMax/5.3/CMax-5.3.tar.gz

---
+++ packages/CMax/CMax-5.3.tar.gz/CMax-5.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='CMax',
+      version='5.3',
+      description='A simple circuit simulator',
+      long_description='A program for designing and simulating circuits on a breadboard',
+      author='6.01 Staff',
+      author_email='6.01-core@mit.edu',
+      url='https://sixohone.mit.edu',
+      packages=['cmax', 'cmax.sims'],
+      package_data={'cmax.sims': ['sims/*.csim']},
+      entry_points={'console_scripts': ['cmax = cmax.__main__:main', 'CMax = cmax.__main__:main']},
+      install_requires=['matplotlib>=2.0'],
+      python_requires='>=3',
+      license='GPLv3+',
+      classifiers=['License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'],
+      )
