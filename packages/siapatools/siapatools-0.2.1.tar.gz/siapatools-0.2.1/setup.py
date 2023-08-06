b8d1a2772be33388b2d165f4ab2240e1cc4018cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:53 +0000

    Add package siapatools/0.2.1/siapatools-0.2.1.tar.gz

---
+++ packages/siapatools/siapatools-0.2.1.tar.gz/siapatools-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="siapatools",
+    version="0.2.1",
+    author="Rafael Alves Ribeiro",
+    author_email="rafael.alves.ribeiro@gmail.com",
+    packages=["siapatools"],
+    install_requires=[
+        "pandas",
+        "psycopg2",
+    ],
+)
