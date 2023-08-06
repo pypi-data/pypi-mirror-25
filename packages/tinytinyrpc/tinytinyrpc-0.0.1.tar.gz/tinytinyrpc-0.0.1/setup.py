942d44d7d93e7b83ad1d1807eb5c28c9cd73e90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:37 +0000

    Add package tinytinyrpc/0.0.1/tinytinyrpc-0.0.1.tar.gz

---
+++ packages/tinytinyrpc/tinytinyrpc-0.0.1.tar.gz/tinytinyrpc-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='tinytinyrpc',
+    version='0.0.1',
+    py_modules=['tinyrpc'],
+    url='http://github.com/rokups/tinyrpc',
+    description='A tiny transport-agnostic RPC implementation',
+    keywords=['rpc'],
+    license='MIT',
+)
