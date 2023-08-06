8d01c05dab7508a0b4a825bb1cf65172c3b739c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:35 +0000

    Add package sonic182_my_logger/0.0.1/sonic182_my_logger-0.0.1.tar.gz

---
+++ packages/sonic182_my_logger/sonic182_my_logger-0.0.1.tar.gz/sonic182_my_logger-0.0.1/setup.py
@@ -0,0 +1,29 @@
+"""Setup module."""
+
+from setuptools import setup
+from pip.req import parse_requirements
+
+REQS = [str(ir.req) for ir in parse_requirements(
+    'requirements.txt', session='hack')]
+REQS2 = [str(ir.req) for ir in parse_requirements(
+    'dev-requirements.txt', session='hack')]
+
+setup(
+    name='sonic182_my_logger',
+    version='0.0.1',
+    description='Logger utilities for aiohttp',
+    author='Johanderson Mogollon',
+    author_email='johanderson@mogollon.com.ve',
+    license='MIT',
+    setup_requires=['pytest-runner'],
+    test_requires=['pytest'],
+    install_requires=REQS,
+    extras_require={
+        'dev': REQS2,
+        'test': [
+            'pytest',
+            'coverage',
+            'aiohttp'
+        ]
+    }
+)
