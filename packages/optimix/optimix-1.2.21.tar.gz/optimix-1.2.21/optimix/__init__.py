bae6e3e8abd9ff7abb64a3c120da87006a7dbff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:55 +0000

    Add package optimix/1.2.21/optimix-1.2.21.tar.gz

---
+++ packages/optimix/optimix-1.2.21.tar.gz/optimix-1.2.21/optimix/__init__.py
@@ -0,0 +1,27 @@
+r"""
+***************
+optimix package
+***************
+
+Abstract function optimisation.
+
+"""
+from . import testing
+from .testit import test
+from .check_grad import approx_fprime, check_grad
+from .exception import OptimixError
+from .function import Function, FunctionReduce
+from .optimize import maximize, maximize_scalar, minimize, minimize_scalar
+from .types import Matrix, Scalar, Vector
+
+__name__ = "optimix"
+__version__ = "1.2.21"
+__author__ = "Danilo Horta"
+__author_email__ = "horta@ebi.ac.uk"
+
+__all__ = [
+    "__name__", "__version__", "__author__", "__author_email__", "test",
+    'testing', 'approx_fprime', 'check_grad', 'OptimixError', 'Function',
+    'FunctionReduce', 'maximize', 'maximize_scalar', 'minimize',
+    'minimize_scalar', 'Matrix', 'Scalar', 'Vector'
+]
