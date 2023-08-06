63f59ec1e15461b7cbfa6da4fcffa98f9f610a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:00 +0000

    Add package limp/0.5.0/limp-0.5.0.tar.gz

---
+++ packages/limp/limp-0.5.0.tar.gz/limp-0.5.0/limp/environment.py
@@ -0,0 +1,128 @@
+import limp.errors as Errors
+import operator
+import math
+from functional import seq
+
+
+class _Environment:
+
+    def __init__(self, parent = None):
+        self.__parent = parent
+        self.__symbols = {}
+    
+    def define(self, name, value):
+        if name in self.__symbols:
+            raise Errors.RedefinedSymbol(name)
+        else:
+            self.__symbols[name] = value
+
+    def resolve(self, name):
+        if name in self.__symbols:
+            return self.__symbols[name]
+        else:
+            if self.__parent is not None:
+                return self.__parent.resolve(name)
+            else:
+                raise Errors.UndefinedSymbol(name)
+
+    def define_batch_of(self, symbols):
+        self.__symbols.update(symbols)
+            
+    def new_child(self):
+        return _Environment(self)
+        
+    def __str__(self):
+        BORDER_WIDTH = 32
+        BORDER_CHARACTER = '-'
+        NEW_LINE = '\n'
+        BORDER = (BORDER_CHARACTER * BORDER_WIDTH) + NEW_LINE
+        s = BORDER
+        for name, value in self.__symbols.items():
+            s += f'{name}: {value}{NEW_LINE}'
+        if self.__parent is not None:
+            s += str(self.__parent)
+        s += BORDER
+        return s
+
+
+def create_empty():
+    return _Environment()
+
+
+def create_standard():
+    environment = _Environment()
+    environment.define_batch_of(_mathematical_functions())
+    environment.define_batch_of(_comparison_functions())
+    environment.define_batch_of(_boolean_functions())
+    environment.define_batch_of(_string_functions())
+    environment.define_batch_of(_easter_egg_symbols())
+    environment.define_batch_of(_looping_functions())
+    return environment
+
+
+def _mathematical_functions():
+    return {
+        '+':    operator.add,
+        '-':    operator.sub,
+        '*':    operator.mul,
+        '/':    operator.truediv,
+        '//':   operator.floordiv,
+        '**':   operator.pow,
+        '%':    operator.mod,
+        'sqrt': math.sqrt,
+        '!':    math.factorial,
+    }
+
+
+def _comparison_functions():
+    return {
+        '=':  operator.eq,
+        '>':  operator.gt,
+        '<':  operator.lt,
+        '<=': operator.le,
+        '>=': operator.ge,
+    }
+
+
+def _boolean_functions():
+    return {
+        'not': lambda x: not x,
+        'and': lambda a, b: a and b,
+        'or':  lambda a, b: a or b,
+        'xor': lambda a, b: (a and not b) or (b and not a)
+    }
+
+
+def _string_functions():
+    return {
+        'string':      str,
+        'concatenate': lambda *args: seq(args).map(str).reduce(operator.add),
+        'strip':       lambda s: s.strip(),
+        'length':      lambda s: len(s),
+        'in':          lambda a, b: a in b,
+        'repeat':      lambda s, amount: s * amount,
+        'reverse':     lambda s: s[::-1],
+        'lowercase':   lambda s: s.lower(),
+        'uppercase':   lambda s: s.upper()
+    }
+
+
+def _looping_functions():
+    return {
+        'times': _times,
+        'iterate': _iterate,
+    }
+
+
+def _times(iterations, callback):
+    for _ in range(iterations): callback()
+
+
+def _iterate(iterations, callback):
+    for i in range(iterations): callback(i)
+    
+
+def _easter_egg_symbols():
+    return {
+        'bizkit': "Keep ROLLIN ROLLIN ROLLIN ROLLIN whaaat!",
+    }
---
+++ packages/limp/limp-0.5.0.tar.gz/limp-0.5.0/limp/internal_types/behavioural.py
@@ -0,0 +1,166 @@
+import limp.errors as Errors
+from functional import seq
+
+
+class NullOperation:
+    def evaluate(self):
+        return None
+
+
+class Function:
+
+    KEYWORD = 'function'
+    
+    def __init__(self, contents, environment):
+        self.__contents = contents
+        self.__child_environment = environment.new_child()
+
+    def is_valid(self):
+        return self.__contents[0] == Function.KEYWORD
+
+    def evaluate(self):
+        from limp.types import Form
+        argument_names = self.__contents[1]
+        body_contents = self.__contents[2]
+        
+        def __internal_function(*called_with):
+            execution_environment = self.__child_environment.new_child()
+            self.__bind_parameters(execution_environment, argument_names, called_with)
+            executable_form = Form.infer_from(body_contents, execution_environment)
+            function_output = executable_form.evaluate()
+            return function_output
+        
+        return __internal_function
+
+    def __bind_parameters(self, execution_environment, names, values):
+        for name, value in zip(names, values):
+            execution_environment.define(name, value)
+
+
+class Invocation:
+
+    def __init__(self, contents, environment):
+        self.__contents = contents
+        self.__environment = environment
+
+    def is_valid(self):
+        return type(self.__contents) == list
+        
+    def evaluate(self):
+        function_node = self.__contents[0]
+        function = self.__to_form(function_node).evaluate()
+        argument_nodes = self.__contents[1:]
+        arguments = (seq(argument_nodes)
+                     .map(self.__to_form)
+                     .map(self.__evaluated))
+        return function(*arguments)
+
+    def __to_form(self, node):
+        from limp.types import Form
+        return Form.infer_from(node, self.__environment)
+
+    def __evaluated(self, form):
+        return form.evaluate()
+
+    
+
+class SimpleConditional:
+
+    KEYWORD = 'if'
+
+    def __init__(self, contents, environment):
+        self.__contents = contents
+        self.__environment = environment
+
+    def is_valid(self):
+        return self.__contents[0] == SimpleConditional.KEYWORD
+
+    def evaluate(self):
+        condition = self.__new_form(self.__contents[1])
+        outcome = condition.evaluate()
+        result_form = self.__get_result_form_for(outcome)
+        return result_form.evaluate()
+
+    def __new_form(self, contents):
+        from limp.types import Form
+        return Form.infer_from(contents, self.__environment)
+
+    def __get_result_form_for(self, outcome):
+        form = NullOperation()
+        if outcome == True:
+            form = self.__new_form(self.__contents[2])
+        elif outcome == False:
+            if self.__else_form_supplied():
+                form = self.__new_form(self.__contents[3])
+        return form
+    
+    def __else_form_supplied(self):
+        return len(self.__contents) >= 4
+
+
+class ComplexConditional:
+
+    KEYWORD = 'condition'
+    
+    def __init__(self, contents, environment):
+        self.__contents = contents
+        self.__environment = environment
+
+    def is_valid(self):
+        return self.__contents[0] == ComplexConditional.KEYWORD
+
+    def evaluate(self):
+        result_form = NullOperation()
+        pairs = self.__contents[1:]
+        for pair in pairs:
+            condition = self.__new_form(pair[0])
+            outcome = condition.evaluate()
+            if outcome:
+                result_form = self.__new_form(pair[1])
+                break
+        return result_form.evaluate()
+
+    def __new_form(self, contents):
+        from limp.types import Form
+        return Form.infer_from(contents, self.__environment)
+
+
+class SequentialEvaluator:
+
+    KEYWORD = 'do'
+
+    def __init__(self, contents, environment):
+        self.__contents = contents
+        self.__environment = environment
+
+    def is_valid(self):
+        return self.__contents[0] == SequentialEvaluator.KEYWORD
+        
+    def evaluate(self):
+        from limp.types import Form
+        forms = self.__contents[1:]
+        if len(forms) <= 1:
+            raise Errors.UnnecessarySequentialEvaluator()
+        for node in forms:
+            form = Form.infer_from(node, self.__environment)
+            result = form.evaluate()
+        return result
+
+
+class Definition:
+
+    KEYWORD = 'define'
+
+    def __init__(self, contents, environment):
+        self.__contents = contents
+        self.__environment = environment
+
+    def is_valid(self):
+        return self.__contents[0] == Definition.KEYWORD
+        
+    def evaluate(self):
+        from limp.types import Form
+        name = self.__contents[1]
+        value = Form.infer_from(self.__contents[2], self.__environment).evaluate()
+        self.__environment.define(name, value)
+        
