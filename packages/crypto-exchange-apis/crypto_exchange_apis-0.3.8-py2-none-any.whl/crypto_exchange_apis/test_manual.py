6cfb3dca037cecf387bfb9ce45e6b0968291a34d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:23 +0000

    Add package crypto-exchange-apis/0.3.8/crypto_exchange_apis-0.3.8-py2-none-any.whl

---
+++ packages/crypto-exchange-apis/crypto_exchange_apis-0.3.8-py2-none-any.whl/crypto_exchange_apis/test_manual.py
@@ -0,0 +1,35 @@
+from crypto_exchange_apis import itBit
+from crypto_exchange_apis import Bitfinex
+from crypto_exchange_apis import Bittrex
+from crypto_exchange_apis import Poloniex
+from crypto_exchange_apis import Tuxexchange
+
+
+itbit = itBit()
+
+print('itBit tests:')
+print(str(itbit.get_ticker())[0:77]+'...') # Conversion to string and [0:77] are just to keep output on one terminal line -- use itbit.get_ticker() in practice
+print(str(itbit.get_order_book())[0:77]+'...')
+
+bitfinex = Bitfinex()
+
+print('Bitfinex tests:')
+print(str(bitfinex.ticker())[0:77]+'...')
+
+bittrex = Bittrex()
+
+print('Bittrex tests:')
+print(str(bittrex.get_currencies())[0:77]+'...')
+print(str(bittrex.get_markets())[0:77]+'...')
+
+poloniex = Poloniex()
+
+print('Poloniex tests:')
+print(str(poloniex.returnTicker()['BTC_XMR'])[0:77]+'...')
+print(str(poloniex.returnCurrencies())[0:77]+'...')
+
+tuxexchange = Tuxexchange()
+
+print('Tux Exchange tests:')
+print(str(tuxexchange.api_query('getcoins'))[0:77]+'...')
+print(str(tuxexchange.api_query('getticker'))[0:77]+'...')
