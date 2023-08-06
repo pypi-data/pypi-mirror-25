ae872e2361000f6ab24b16049ce15e8b98827f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:45 +0000

    Add package pyspark_postgresql_utils/0.0.1/pyspark_postgresql_utils-0.0.1.tar.gz

---
+++ packages/pyspark_postgresql_utils/pyspark_postgresql_utils-0.0.1.tar.gz/pyspark_postgresql_utils-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pyspark_postgresql_utils',
+      version='0.0.1',
+      description='Usefull functions for working with PostgreSQL in PySpark',
+      url='https://github.com/osahp/pyspark_postgresql_utils',
+      author='Smelov Vladimir',
+      author_email='vladimirfol@gmail.com',
+      packages=['pyspark_postgresql_utils'],
+      zip_safe=False)
