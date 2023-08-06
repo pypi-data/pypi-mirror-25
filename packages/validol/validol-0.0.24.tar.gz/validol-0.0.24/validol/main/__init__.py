427cc96197c83e6f09d9c8d16e3967ad1b7c1b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:58 +0000

    Add package validol/0.0.24/validol-0.0.24.tar.gz

---
+++ packages/validol/validol-0.0.24.tar.gz/validol-0.0.24/validol/main/__init__.py
@@ -0,0 +1,21 @@
+import sys
+import traceback
+
+from validol.controller.telegram_notifier import TelegramNotifier
+from validol.controller.launcher import ControllerLauncher
+
+
+def exchook(*args):
+    message = ''.join(traceback.format_exception(*args))
+    TelegramNotifier().notify(message)
+    sys.stderr.write(message)
+
+    sys.exit(1)
+
+def main():
+    sys.excepthook = exchook
+
+    ControllerLauncher()
+
+if __name__ == '__main__':
+    main()
