b2af923d80f518a90230aeec1bced7029b579bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:25 +0000

    Add package onegov.people/0.3.0/onegov.people-0.3.0-py3-none-any.whl

---
+++ packages/onegov.people/onegov.people-0.3.0-py3-none-any.whl/onegov/people/tests/test_collections.py
@@ -0,0 +1,18 @@
+from onegov.people import PersonCollection
+
+
+def test_full_name(session):
+    people = PersonCollection(session)
+
+    tom = people.add(
+        first_name='Tom',
+        last_name='Chandler'
+    )
+    rachel = people.add(
+        first_name='Rachel',
+        last_name='Scott',
+        salutation='Dr.'
+    )
+
+    assert tom.title == 'Chandler Tom'
+    assert rachel.title == 'Scott Rachel'
---
+++ packages/onegov.people/onegov.people-0.3.0-py3-none-any.whl/onegov/people/upgrade.py
@@ -0,0 +1,25 @@
+""" Contains upgrade tasks that are executed when the application is being
+upgraded on the server. See :class:`onegov.core.upgrade.upgrade_task`.
+
+"""
+from onegov.core.upgrade import upgrade_task
+from sqlalchemy import Column, Text
+
+
+@upgrade_task('Rename academic_title to salutation')
+def rename_academic_title_to_salutation(context):
+
+    context.operations.alter_column(
+        'people', 'academic_title', new_column_name='salutation')
+
+
+@upgrade_task('Add function column')
+def add_function_column(context):
+    context.operations.add_column(
+        'people', Column('function', Text, nullable=True))
+
+
+@upgrade_task('Add notes column')
+def add_notes_column(context):
+    context.operations.add_column(
+        'people', Column('notes', Text, nullable=True))
