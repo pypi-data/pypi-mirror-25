62d16537c9e89d4a182bfb6623f8a485a3974e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:32 +0000

    Add package helpscout/0.0.6b164/helpscout-0.0.6b164-py3-none-any.whl

---
+++ packages/helpscout/helpscout-0.0.6b164-py3-none-any.whl/helpscout/web_hook/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-TODAY LasLabs Inc.
+# License MIT (https://opensource.org/licenses/MIT).
+
+from .web_hook import HelpScoutWebHook
+from .web_hook_event import WebHookEvent
+
+__all__ = [
+    'HelpScoutWebHook',
+    'WebHookEvent',
+]
