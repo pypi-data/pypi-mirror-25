1c4ae74a7bb6a08c61dff6afd6719ff974a7f00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:13 +0000

    Add package boolrule/0.2.1/boolrule-0.2.1.tar.gz

---
+++ packages/boolrule/boolrule-0.2.1.tar.gz/boolrule-0.2.1/boolrule/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Steve Webster'
+__email__ = 'spjwebster@gmail.com'
+__version__ = '0.2.1'
+
+from .boolrule import BoolRule, MissingVariableException, UnknownOperatorException  # noqa
