88a607b5ba5200a889a3043671a2f26687dfc858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:15 +0000

    Add package aliyun-ros-cli/1.0.2/aliyun-ros-cli-1.0.2.tar.gz

---
+++ packages/aliyun-ros-cli/aliyun-ros-cli-1.0.2.tar.gz/aliyun-ros-cli-1.0.2/setup.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup (
+    name = 'aliyun-ros-cli',
+    version = '1.0.2',
+    keywords = ('aliyun', 'ros', 'template', 'orchestration'),
+    install_requires = ['aliyun-python-sdk-ros'],
+    python_requires='>=2.6, <3',
+    author = 'shenggong.wang, quming.ly',
+    author_email = 'shenggong.wang@alibaba-inc.com',
+    scripts=['bin/ros'],
+    packages = find_packages(),
+    include_package_data=True,
+    exclude_package_date={'':['.gitignore']},
+    platforms = 'any',
+    description = 'Aliyun ros command line tools.',
+    long_description=readme()
+)
