a58b7bd5ed322381076e16bafd74a54f2d90b57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:25 +0000

    Add package gandi.cli/1.0/gandi.cli-1.0.tar.gz

---
+++ packages/gandi.cli/gandi.cli-1.0.tar.gz/gandi.cli-1.0/gandi/cli/tests/compat.py
@@ -0,0 +1,23 @@
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+
+try:
+    import configparser as ConfigParser
+except ImportError:
+    import ConfigParser
+
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
