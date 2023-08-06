fef570934b0fff05a991af219c22391d3f053dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:58 +0000

    Add package pyextractor/0.8b0/pyextractor-0.8b0.tar.gz

---
+++ packages/pyextractor/pyextractor-0.8b0.tar.gz/pyextractor-0.8b0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from setuptools import setup
+import os,sys
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md','rst')
+except (IOError,ImportError):
+    long_description = open('README.md').read()
+
+setup(name='pyextractor',
+            version='0.8-beta',
+            description='Command Line Tool to extract data from files',
+            long_description=long_description,
+            url='https://github.com/petryx/extractor',
+            author='Marlon Petry',
+            author_email='marlonpetry@gmail.com',
+            license='BSD',
+            scripts=['extractor/extractor.py'],
+            install_requires = [ 'pygrok>=1.0.0',
+                                 'textract==1.6.1',
+                                 'termcolor',
+                               ],
+            zip_safe=False)
