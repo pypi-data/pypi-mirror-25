ab1d98a69a93168519f7840ce512c37562652552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:26 +0000

    Add package liknorm/1.0.12/liknorm-1.0.12.tar.gz

---
+++ packages/liknorm/liknorm-1.0.12.tar.gz/liknorm-1.0.12/liknorm/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import as _
+
+import _cffi_backend as _
+
+from .machine import LikNormMachine
+from .testit import test
+
+__name__ = "liknorm"
+__version__ = "1.0.12"
+__author__ = "Danilo Horta"
+__author_email__ = "horta@ebi.ac.uk"
+
+__all__ = [
+    "__name__", "__version__", "__author__", "__author_email__", "test",
+    "LikNormMachine"
+]
