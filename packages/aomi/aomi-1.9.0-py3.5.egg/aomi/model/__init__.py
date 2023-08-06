71c01b455746aa8f4a10aba6b96d3d7bff98e266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:20 +0000

    Add package aomi/1.9.0/aomi-1.9.0-py3.5.egg

---
+++ packages/aomi/aomi-1.9.0-py3.5.egg/aomi/model/__init__.py
@@ -0,0 +1,10 @@
+"""A Model definition of Vault resources"""
+
+#  Ensure every defined model is loaded I guess
+import aomi.model.generic
+import aomi.model.auth
+import aomi.model.aws
+import aomi.model.resource
+import aomi.model.backend
+
+from aomi.model.context import Context
