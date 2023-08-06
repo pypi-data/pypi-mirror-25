d5c5d521142eb6b82e25f9850bbc8a4661da2a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:07 +0000

    Add package dynaconf/0.5.0/dynaconf-0.5.0.tar.gz

---
+++ packages/dynaconf/dynaconf-0.5.0.tar.gz/dynaconf-0.5.0/dynaconf/loaders/yaml_loader.py
@@ -0,0 +1,74 @@
+# coding: utf-8
+try:
+    import yaml
+except ImportError as e:  # pragma: no cover
+    yaml = None
+
+IDENTIFIER = 'yaml_loader'
+
+
+def load(obj, namespace=None, silent=True, key=None, filename=None):
+    """
+    Reads and loads in to "settings" a single key or all keys from yaml file
+    :param obj: the settings instance
+    :param namespace: settings namespace default='DYNACONF'
+    :param silent: if errors should raise
+    :param key: if defined load a single key, else load all in namespace
+    :return: None
+    """
+    if yaml is None:  # pragma: no cover
+        raise RuntimeError(
+            "PyYAML package is not installed in your environment.\n"
+            "To use this loader you have to install it with\n"
+            "pip install PyYAML\n"
+            "or\n"
+            "pip install dynaconf[yaml]"
+        )
+
+    filename = filename or obj.get('YAML')
+    if not filename:
+        return
+
+    namespace = namespace or obj.get('DYNACONF_NAMESPACE')
+
+    # clean(obj, namespace, identifier=filename)
+
+    if filename.endswith(('.yaml', '.yml')):  # pragma: no cover
+        yaml_data = yaml.load(open(filename))
+    else:
+        # for tests it is possible to pass YAML string
+        yaml_data = yaml.load(filename)
+
+    yaml_data = {key.lower(): value for key, value in yaml_data.items()}
+
+    # ---->
+    # Load from namescape_settings.yaml
+
+    data = {}
+    try:
+        data = yaml_data[namespace.lower()]
+    except KeyError as e:
+        if silent:
+            if hasattr(obj, 'logger'):
+                obj.logger.error(str(e))
+        else:
+            raise KeyError(
+                '%s namespace not defined in %s' % (namespace, filename)
+            )
+
+    if namespace and namespace != obj.get('DYNACONF_NAMESPACE'):
+        identifier = "{0}_{1}".format(IDENTIFIER, namespace.lower())
+    else:
+        identifier = IDENTIFIER
+
+    if not key:
+        obj.update(data, loader_identifier=identifier)
+    else:
+        obj.set(key, data.get(key), loader_identifier=identifier)
+
+
+def clean(obj, namespace, silent=True):  # noqa
+    for identifier, data in obj.loaded_by_loaders.items():
+        if identifier.startswith('yam_loader_'):
+            for key in data:
+                obj.unset(key)
