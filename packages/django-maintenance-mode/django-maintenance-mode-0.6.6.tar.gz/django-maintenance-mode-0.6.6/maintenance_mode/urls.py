e67bbfcd9a33d2d2688c0bfb844ad663f019e20a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:26 +0000

    Add package django-maintenance-mode/0.6.6/django-maintenance-mode-0.6.6.tar.gz

---
+++ packages/django-maintenance-mode/django-maintenance-mode-0.6.6.tar.gz/django-maintenance-mode-0.6.6/maintenance_mode/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+
+from maintenance_mode import views
+
+
+urlpatterns = [
+    url(r'^off/$', views.maintenance_mode_off, name='maintenance_mode_off'),
+    url(r'^on/$', views.maintenance_mode_on, name='maintenance_mode_on'),
+]
---
+++ packages/django-maintenance-mode/django-maintenance-mode-0.6.6.tar.gz/django-maintenance-mode-0.6.6/maintenance_mode/utils.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+def get_client_ip_address(request):
+    return request.META['REMOTE_ADDR']
