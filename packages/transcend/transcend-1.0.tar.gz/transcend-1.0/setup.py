50e0effdb938f71a4766729205e334c0cf11b556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:18 +0000

    Add package transcend/1.0/transcend-1.0.tar.gz

---
+++ packages/transcend/transcend-1.0.tar.gz/transcend-1.0/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+setup(
+	 name='transcend',    # This is the name of your PyPI-package.
+	 version='1.0'                          # Update the version number for new releases
+)
