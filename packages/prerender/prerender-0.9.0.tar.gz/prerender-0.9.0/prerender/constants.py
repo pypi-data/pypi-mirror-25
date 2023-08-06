4aad3bc93d2616b60e8d2248396ac90d7be5724c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:13 +0000

    Add package prerender/0.9.0/prerender-0.9.0.tar.gz

---
+++ packages/prerender/prerender-0.9.0.tar.gz/prerender-0.9.0/prerender/constants.py
@@ -0,0 +1,35 @@
+import os
+
+BLOCKED_URLS = [
+    "google-analytics.com",
+    "api.mixpanel.com",
+    "stats.g.doubleclick.net",
+    "mc.yandex.ru",
+    "use.typekit.net",
+    "beacon.tapfiliate.com",
+    "js-agent.newrelic.com",
+    "api.segment.io",
+    "woopra.com",
+    "static.olark.com",
+    "static.getclicky.com",
+    "fast.fonts.com",
+    "youtube.com\/embed",
+    "cdn.heapanalytics.com",
+    "googleads.g.doubleclick.net",
+    "pagead2.googlesyndication.com",
+    "fullstory.com/rec",
+    "navilytics.com/nls_ajax.php",
+    "log.optimizely.com/event",
+    "hn.inspectlet.com",
+    "tpc.googlesyndication.com",
+    "partner.googleadservices.com",
+    "hm.baidu.com",
+]
+
+if os.getenv('BLOCK_FONTS', '1').lower() in ('1', 'y', 'yes', 'on', 't', 'true'):
+    BLOCKED_URLS.extend((
+        ".ttf",
+        ".eot",
+        ".otf",
+        ".woff",
+    ))
