c7e54bdf7970d0ac42d69fa65f448560e8dfe1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:04 +0000

    Add package hudai/2.6.5/hudai-2.6.5.tar.gz

---
+++ packages/hudai/hudai-2.6.5.tar.gz/hudai-2.6.5/hudai/resources/domain.py
@@ -0,0 +1,31 @@
+"""
+hudai.resources.domain
+"""
+from ..helpers.resource import Resource
+
+
+class DomainResource(Resource):
+    def __init__(self, client):
+        Resource.__init__(
+            self, client, base_path='/companies/domains')
+        self.resource_name = 'Domain'
+
+    def list(self, company_id, page=None):
+        query_params = self._set_limit_offset({'page': page})
+        query_params['company_id'] = company_id
+
+        return self.http_get('/', query_params=query_params)
+
+    def create(self, company_id, hostname):
+        return self.http_post('/',
+                              data={'hostname': hostname,'company_id': company_id})
+
+    def fetch(self, company_id, entity_id):
+        return self.http_get('/{id}',
+                             query_params={'company_id': company_id},
+                             params={'id': entity_id})
+
+    def delete(self, company_id, entity_id):
+        return self.http_delete('/{id}',
+                                query_params={'company_id': company_id},
+                                params={'id': entity_id})
