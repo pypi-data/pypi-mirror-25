10dc86de5050bcfb5ce46be70dca2cced6c17b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:18 +0000

    Add package blo/0.7/blo-0.7.tar.gz

---
+++ packages/blo/blo-0.7.tar.gz/blo-0.7/blo/blog.py
@@ -0,0 +1,124 @@
+import os
+import sys
+import json
+import argparse
+import shutil
+from datetime import datetime
+import logging
+
+try:
+    # import for python2.7
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+    from BaseHTTPServer import HTTPServer
+except ImportError:
+    # import for python3
+    from http.server import SimpleHTTPRequestHandler
+    from http.server import HTTPServer
+
+from .page_generator import PageGenerator
+    
+
+
+log = logging.getLogger(__name__)
+
+
+class BlogAction:
+    def __init__(self, config):
+        self.config = config
+
+    def serve(self):
+        """ Run web-server to test static site """
+        os.chdir(self.config["render_dir"])
+        server_addr = ('localhost', 8000)
+        request_handler = SimpleHTTPRequestHandler
+        httpd = HTTPServer(server_addr, request_handler)
+        log.info("Serving at http://{0}:{1}".format(*server_addr))
+        httpd.serve_forever()
+
+    def build(self):
+        """ Generate finalized html. Also fill metadata for new drafts """
+        page_generator = PageGenerator(self.config)
+        page_generator.generate_all()
+
+    def post(self, slug, draft_type):
+        """ Make a new page draft with given slug """
+        now = datetime.now()
+        draft_path = os.path.join(self.config['pages_dir'], 
+                                  now.strftime("%Y-%m-%d__") + slug + '.md')
+        date_time = now.strftime(self.config['date_format'])
+        with open(draft_path, 'w') as f:
+            f.write(self.config['draft_templates'][draft_type]['content']
+                    .format(slug=slug, date_time=date_time))
+        log.info(draft_path)
+
+
+def create_blog(blog_dir):
+    blog_path = os.path.join(os.getcwd(), blog_dir, '_blog')
+    pages_path = os.path.join(blog_path, 'pages')
+    os.makedirs(pages_path)
+
+    engine_path = os.path.dirname(os.path.realpath(__file__))
+    shutil.copytree(os.path.join(engine_path, 'templates'), os.path.join(blog_path, 'templates'))
+    shutil.copyfile(
+        os.path.join(engine_path, 'default.json'),
+         os.path.join(blog_path, 'default.json'))
+    shutil.copyfile(
+        os.path.join(engine_path, 'draft_templates.json'),
+        os.path.join(blog_path, 'draft_templates.json'))
+    log.info("Blog created. \ncd {0} && blo post hello-worlds".format(blog_dir))
+
+
+def parse_args():
+    """
+    How to use blog:
+     - build  --  generate htmls in 'render_dir'
+     - serve  --  run webserver for testing in 'render_dir'
+     - post "slug"  --  greate new page with given title
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action', nargs='+')
+    parser.add_argument('--config', default='default.json')
+    parser.add_argument('--type', default='post')
+    return parser.parse_args()
+
+
+def main():
+    opts = parse_args()
+    if opts.action[0] == 'create':
+        if len(opts.action) == 1:
+            log.info("Add blog folder name")
+            return 1
+        #
+        try:
+            # for python2.7
+            blog_dir = unicode(opts.action[1], 'utf-8')
+        except NameError:
+            # for python3
+            blog_dir = opts.action[1]
+        create_blog(blog_dir)
+        return 0
+
+    config_path = os.path.join(os.getcwd(), '_blog', opts.config)
+    config = json.load(open(config_path))
+    draft_templates_path = os.path.join(os.getcwd(), '_blog', 'draft_templates.json')
+    config['draft_templates'] = json.load(open(draft_templates_path))
+
+    blog_action = BlogAction(config)
+    if opts.action[0] == 'build':
+        blog_action.build()
+    if opts.action[0] == 'serve':
+        blog_action.serve()
+    elif opts.action[0] == 'post':
+        try:
+            # for python2.7
+            slug = unicode(opts.action[1], 'utf-8')
+        except NameError:
+            # for python3
+            slug = opts.action[1]
+        blog_action.post(slug, opts.type)
+
+
+if __name__ == "__main__":
+    log.addHandler(logging.StreamHandler(sys.stdout))
+    log.setLevel(logging.DEBUG)
+    sys.exit(main())
---
+++ packages/blo/blo-0.7.tar.gz/blo-0.7/blo/content_aggregator.py
@@ -0,0 +1,39 @@
+"""
+Module for content aggregation to give data
+"""
+import collections
+
+
+class ContentAggregator:
+    def __init__(self, config):
+        self.tags = collections.defaultdict(list)
+        self.content = collections.defaultdict(dict)
+        self.config = config
+
+    def get_content_items(self, tag=None):
+        pages_list = []
+        for page_url, page_vars in iter(self.content.items()):
+            if tag and tag not in page_vars['page_tags']:
+                continue
+            page_vars['page_url'] = page_url
+            pages_list.append(page_vars)
+        return sorted(pages_list,
+                      key=lambda x: x['page_date_time'],
+                      reverse=True)
+
+    def get_tags_list(self):
+        return sorted([{"page_url": "/tag/{0}.html".format(tag),
+                        "page_title": tag,
+                        "page_brefing": len(items)}
+                       for tag, items in iter(self.tags.items())],
+                      key=lambda x: x['page_brefing'],
+                      reverse=True)
+
+    def get_latest_posts(self, number=None):
+        if not number and self.config.get('items_per_page'):
+            number = self.config.get('items_per_page')
+        posts = sorted(self.content.items(),
+                       key=lambda x: x[1]['page_date_time'],
+                       reverse=True)[:number]
+        return collections.OrderedDict(posts)
+
