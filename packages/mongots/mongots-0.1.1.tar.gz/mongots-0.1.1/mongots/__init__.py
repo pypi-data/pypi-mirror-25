3d66225771229fc5345caeb1219a69d12ee70a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:14 +0000

    Add package mongots/0.1.1/mongots-0.1.1.tar.gz

---
+++ packages/mongots/mongots-0.1.1.tar.gz/mongots-0.1.1/mongots/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.1.1'
+
+from mongots.client import MongoTSClient
+from mongots.database import MongoTSDatabase
+from mongots.collection import MongoTSCollection
---
+++ packages/mongots/mongots-0.1.1.tar.gz/mongots-0.1.1/mongots/constants.py
@@ -0,0 +1,13 @@
+AGGREGATION_MONTH_KEY = 'months'
+AGGREGATION_DAY_KEY = 'days'
+AGGREGATION_HOUR_KEY = 'hours'
+
+DATETIME_KEY = 'datetime'
+
+COUNT_KEY = 'count'
+SUM_KEY = 'sum'
+SUM2_KEY = 'sum2'
+MIN_KEY = 'min'
+MAX_KEY = 'max'
+MEAN_KEY = 'mean'
+STD_KEY = 'std'
