a984270d44b08a6a83693ff251fb29714bb94d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:09 +0000

    Add package django-tool/1.0.1/django-tool-1.0.1.tar.gz

---
+++ packages/django-tool/django-tool-1.0.1.tar.gz/django-tool-1.0.1/README.md
@@ -0,0 +1,10 @@
+# django-public
+django通用方法
+### 使用方法： ###
+安装：
+
+`pip3 install django-public`
+
+升级：
+
+`pip3 install --upgrade django-public`
---
+++ packages/django-tool/django-tool-1.0.1.tar.gz/django-tool-1.0.1/djtool/__init__.py
@@ -0,0 +1,3 @@
+from djcommon.common import Common
+from djcommon.oauth import ClientOauth
+from djcommon.review import TemplateView, ListView, ActionView
