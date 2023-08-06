4da3c4d680cb13a4cce546dbdfba839554de2785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:56 +0000

    Add package culqipy1_2/0.0.4/culqipy1_2-0.0.4.tar.gz

---
+++ packages/culqipy1_2/culqipy1_2-0.0.4.tar.gz/culqipy1_2-0.0.4/culqipy1_2/__init__.py
@@ -0,0 +1,12 @@
+from culqipy1_2.resource import (
+    Cargo,
+    Plan,
+    Suscripcion
+)
+from culqipy1_2.utils import Util
+
+# Configuration variables.
+API_URL_INTEG = "https://integ-pago.culqi.com/api/v1"
+API_URL_PROD = "https://pago.culqi.com/api/v1"
+llave_publica = None
+llave_privada = None
