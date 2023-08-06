19be67166df8b11e7db79ae14fa3ea703ade71e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:29 +0000

    Add package indexed-gzip/0.6.1/indexed_gzip-0.6.1-cp35-cp35m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/indexed-gzip/indexed_gzip-0.6.1-cp35-cp35m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl/indexed_gzip/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#
+# __init__.py - The indexed_gzip namespace.
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+"""The indexed_gzip namespace. """
+
+
+from .indexed_gzip import (IndexedGzipFile,
+                           SafeIndexedGzipFile,
+                           NotCoveredError,
+                           ZranError)
+
+
+__version__ = '0.6.1'
