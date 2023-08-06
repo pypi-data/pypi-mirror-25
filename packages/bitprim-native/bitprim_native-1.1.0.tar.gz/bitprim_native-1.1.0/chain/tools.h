df4aa476e524fe1ad390e8671f2b07def7ef7a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:18 +0000

    Add package bitprim-native/1.1.0/bitprim_native-1.1.0.tar.gz

---
+++ packages/bitprim-native/bitprim_native-1.1.0.tar.gz/bitprim_native-1.1.0/chain/tools.h
@@ -0,0 +1,15 @@
+#ifndef BITPRIM_PY_TOOLS_H_
+#define BITPRIM_PY_TOOLS_H_
+
+
+#ifdef __cplusplus
+extern "C" {  
+#endif  
+
+void print_thread_id();
+
+#ifdef __cplusplus
+} //extern "C"
+#endif  
+
+#endif // BITPRIM_PY_TOOLS_H_
