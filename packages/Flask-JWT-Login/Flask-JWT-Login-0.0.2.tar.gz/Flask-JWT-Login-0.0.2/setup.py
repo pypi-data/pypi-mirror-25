ef9e03c20ea8c2f768839f51c6f1e3b1dc8318bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:48 +0000

    Add package Flask-JWT-Login/0.0.2/Flask-JWT-Login-0.0.2.tar.gz

---
+++ packages/Flask-JWT-Login/Flask-JWT-Login-0.0.2.tar.gz/Flask-JWT-Login-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask-JWT-Login',
+    version='0.0.2',
+    url='https://github.com/JeongUkJae/Flask-JWT-Login',
+    license='MIT',
+    author='Jeong Ukjae',
+    author_email='jeongukjae@gmail.com',
+    description='Flask extension that helps authentication using JWT',
+    packages=find_packages(exclude=['tests']),
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    test_suite='nose.collector',
+    include_package_data=True,
+    install_requires=[
+        'click==6.7',
+        'Flask==0.12.2',
+        'itsdangerous==0.24',
+        'Jinja2==2.9.6',
+        'MarkupSafe==1.0',
+        'PyJWT==1.5.3',
+        'Werkzeug==0.12.2'
+    ],
+    tests_require=['nose'],
+)
