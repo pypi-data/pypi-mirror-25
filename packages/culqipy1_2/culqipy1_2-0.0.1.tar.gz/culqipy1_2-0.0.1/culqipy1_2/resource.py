f667557c5fa0b0be05fa9b26638415a0e9da87a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:25 +0000

    Add package culqipy1_2/0.0.1/culqipy1_2-0.0.1.tar.gz

---
+++ packages/culqipy1_2/culqipy1_2-0.0.1.tar.gz/culqipy1_2-0.0.1/culqipy1_2/resource.py
@@ -0,0 +1,58 @@
+from culqipy1_2.utils import RequestMethodError, Util
+
+
+class Operation():
+
+    @staticmethod
+    def list(url, params=None, key=None):
+        return Util(
+            url=url,
+            data=params,
+            method="GET",
+            key=key,
+        ).json_result()
+
+    @staticmethod
+    def create(url, body, key=None):
+        return Util(
+            url=url,
+            data=body,
+            method="POST",
+            key=key,
+        ).json_result()
+
+    @staticmethod
+    def get_delete(url, id, method, key=None):
+        """
+        Get or delete, just change the method: "GET" or "DELETE".
+        """
+        return Util(
+            url=url + id + "/",
+            method=method,
+            key=key,
+        ).json_result()
+
+
+class BaseResource:
+    """Parent class for all the resources."""
+
+    URL = None
+
+    @classmethod
+    def create(cls, body):
+        return Operation.create(cls.URL, body)
+
+
+class Cargos(BaseResource):
+
+    URL = "/cargos"
+
+
+class Plan(BaseResource):
+
+    URL = "/planes"
+
+
+class Suscripciones(BaseResource):
+
+    URL = "/suscripciones"
---
+++ packages/culqipy1_2/culqipy1_2-0.0.1.tar.gz/culqipy1_2-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='culqipy1_2',
+  packages=['culqipy1_2'],
+  version='0.0.1',
+  description='Biblioteca de Culqi 1.2 en Python',
+  author='Willy Aguirre',
+  author_email='marti1125@gmail.com',
+  url='https://github.com/marti1125/culqipy1_2',
+  download_url='https://github.com/marti1125/culqipy1_2/tarball/0.0.1',
+  keywords=['api', 'json', 'culqi', 'apiv1.2'],
+  classifiers=[],
+)
