0dfc5020a6286dd9fef9a8460fb383cfed1fde07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:34 +0000

    Add package aireamhan/1.4/aireamhan-1.4.tar.gz

---
+++ packages/aireamhan/aireamhan-1.4.tar.gz/aireamhan-1.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'aireamhan',
+  packages = ['aireamhan'], # this must be the same as the name above
+  version = '1.4',
+  description = 'Teanga Ríomhchlárúchain as Gaeilge',
+  author = 'Neal Ó Riain',
+  author_email = 'neal@n-o-r.xyz',
+  url = 'https://github.com/neal-o-r/aireamhan', # use the URL to the github repo
+  download_url = 'https://github.com/neal-o-r/aireamhan/archive/1.4.tar.gz', 
+  keywords = ['Irish', 'language', 'programming language'], 
+  classifiers = [],
+  scripts=['bin/áireamhán']
+)
