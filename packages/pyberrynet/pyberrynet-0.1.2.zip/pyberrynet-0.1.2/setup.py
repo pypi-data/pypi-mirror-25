f01653cec8df8566b61a7ef5d817e059f7b85f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:09 +0000

    Add package pyberrynet/0.1.2/pyberrynet-0.1.2.zip

---
+++ packages/pyberrynet/pyberrynet-0.1.2.zip/pyberrynet-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pyberrynet',
+    version='0.1.2',
+    packages=['pyberrynet'],
+    url='https://github.com/wliu2016/PyBerryNet',
+    download_url='https://github.com/wliu2016/PyBerryNet/archive/V0.1.2.tar.gz',
+    keywords=['RPi3', 'Python','YOLO','tensorflow'],
+    license='GUN GPLv3',
+    author='Wenlong Liu',
+    author_email='wliu14@ncsu.edu',
+    description='A Python API for BerryNet: A Gateway for Deep Learning On Raspberry Pi 3'
+)
