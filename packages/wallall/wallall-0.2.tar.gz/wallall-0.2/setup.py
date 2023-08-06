987fe778921915f6c6fb0842230387fcb38485c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:33 +0000

    Add package wallall/0.2/wallall-0.2.tar.gz

---
+++ packages/wallall/wallall-0.2.tar.gz/wallall-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'wallall',
+  packages = ['wallall'], 
+  version = '0.2',
+  description = 'A wallall lib',
+  author = 'Yves Vanlerberghe',
+  author_email = 'yves@infothek.be',
+  url = 'https://github.com/drbytes/wallall',
+  download_url = 'https://github.com/drbytes/wallall/archive/0.2.tar.gz', 
+  keywords = ['wallall', 'control', 'tts'], 
+  classifiers = [],
+)
