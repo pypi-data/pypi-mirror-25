bee232f57d2f1c7cf3514de6ea54f418430935f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:26 +0000

    Add package aioethereum/0.1.0/aioethereum-0.1.0.tar.gz

---
+++ packages/aioethereum/aioethereum-0.1.0.tar.gz/aioethereum-0.1.0/aioethereum/__init__.py
@@ -0,0 +1,10 @@
+from .client import AsyncIOHTTPClient, AsyncIOIPCClient, create_ethereum_client
+
+
+__version__ = '0.1.0'
+
+__all__ = [
+    'AsyncIOHTTPClient',
+    'AsyncIOIPCClient',
+    'create_ethereum_client',
+]
---
+++ packages/aioethereum/aioethereum-0.1.0.tar.gz/aioethereum-0.1.0/aioethereum/utils.py
@@ -0,0 +1,20 @@
+from .constants import BLOCK_TAGS
+
+
+def add_0x(string):
+    return '0x{0}'.format(string)
+
+
+def hex_to_dec(x):
+    return int(x, 16)
+
+
+def validate_block(block):
+    if isinstance(block, str):
+        if block not in BLOCK_TAGS:
+            raise ValueError('Invalid block tag.')
+    elif isinstance(block, int):
+        block = hex(block)
+    else:
+        raise ValueError('Invalid block type.')
+    return block
