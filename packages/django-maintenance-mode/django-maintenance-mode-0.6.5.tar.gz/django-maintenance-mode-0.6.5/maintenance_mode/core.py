ea9f470f2979c069a3973f0ffbc614e5f70866e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:17 +0000

    Add package django-maintenance-mode/0.6.5/django-maintenance-mode-0.6.5.tar.gz

---
+++ packages/django-maintenance-mode/django-maintenance-mode-0.6.5.tar.gz/django-maintenance-mode-0.6.5/maintenance_mode/core.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from maintenance_mode import io, settings
+
+
+def get_maintenance_mode():
+
+    value = io.read_file(settings.MAINTENANCE_MODE_STATE_FILE_PATH, '0')
+
+    if value not in ['0', '1']:
+        raise ValueError('state file content value is not 0|1')
+
+    value = bool(int(value))
+    return value
+
+
+def set_maintenance_mode(value):
+
+    if not isinstance(value, bool):
+        raise TypeError('value argument type is not boolean')
+
+    value = str(int(value))
+    io.write_file(settings.MAINTENANCE_MODE_STATE_FILE_PATH, value)
