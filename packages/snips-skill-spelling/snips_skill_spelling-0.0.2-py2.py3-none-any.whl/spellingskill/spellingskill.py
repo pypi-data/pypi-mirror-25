6f5174115e0b06df611398b43eeff9526c8ec8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:39 +0000

    Add package snips-skill-spelling/0.0.2/snips_skill_spelling-0.0.2-py2.py3-none-any.whl

---
+++ packages/snips-skill-spelling/snips_skill_spelling-0.0.2-py2.py3-none-any.whl/spellingskill/spellingskill.py
@@ -0,0 +1,20 @@
+class SpellingSkill:
+
+    def __init__(self,tts_service=None):
+        """
+        Initialisation.
+        :param db: the json database.
+        :param tts_service: A TTS service, i.e. an object which has a
+                `speak(text)` method for speaking the result.
+        """
+        self.tts_service = tts_service
+
+    def spell(self,wordToSpell):
+        if wordToSpell is not None:
+            return " ".join(list(wordToSpell))
+
+    def spell_and_say(self,wordToSpell):
+        if wordToSpell is not None:
+            result = self.spell(wordToSpell)
+            if self.tts_service:
+                self.tts_service.speak("{} is spelled {}".format(wordToSpell,result))
