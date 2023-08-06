0d2d6dc0fe14f5bb03f64fd2d355ae975697956b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:34 +0000

    Add package stimulus/0.2.0/stimulus-0.2.0.tar.gz

---
+++ packages/stimulus/stimulus-0.2.0.tar.gz/stimulus-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'stimulus',
+  packages = ['stimulus'],
+  version = '0.2.0',
+  description = 'a Python-based general-purpose engine for queue simulation',
+  author = 'Mike Deck',
+  author_email = 'michaelanthonydeck@gmail.com',
+  url = 'https://github.com/m-deck/stimulus',
+  download_url = 'https://github.com/m-deck/stimulus/archive/0.2.0.tar.gz',
+  keywords = ['queueing', 'simulation'],
+  classifiers = [],
+)
+
