6c856aabdd015f0c6569e40631406963a0382680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:36 +0000

    Add package cpexcel/0.0.0/cpexcel-0.0.0.tar.gz

---
+++ packages/cpexcel/cpexcel-0.0.0.tar.gz/cpexcel-0.0.0/setup.py
@@ -0,0 +1,20 @@
+from Cython.Build import cythonize
+import sys
+import subprocess
+
+from distutils.core import setup
+from distutils.extension import Extension
+
+
+def lib():
+    command = 'cd lib && make && make install'
+    subprocess.call(command, shell=True)
+    subprocess.call('python setup.py build_ext --inplace', shell=True)
+
+
+if 'build_ext' not in sys.argv:
+    lib()
+ext_modules = cythonize(
+    [Extension("cpexcel", ["src/cpexcel.pyx"], libraries=["xlsxwriter"])])
+
+setup(name="cpexcel", ext_modules=cythonize(ext_modules))
