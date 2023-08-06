7a6234fa55dd6a49b6b29cdecd4f198aa60b86ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:21 +0000

    Add package textblockomator/0.2/textblockomator-0.2.tar.gz

---
+++ packages/textblockomator/textblockomator-0.2.tar.gz/textblockomator-0.2/README.txt
@@ -0,0 +1,6 @@
+Text block automator
+Written by: Joseph Jones
+
+Notes:
+    The keywords must be proceeded by an = and ended with an enter
+    in order to paste out given text
---
+++ packages/textblockomator/textblockomator-0.2.tar.gz/textblockomator-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="textblockomator",
+      version="0.2",
+      description="Listens for keywords and prints out blocks of text",
+      author="Joseph Jones",
+      author_email="jjones15470@gmail.com",
+      url="",
+      py_modules=["textblockomator"],
+      install_requires=["pynput"])
