e064c2db7d6c5390b87c114ffdac026aa1cd43c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:20 +0000

    Add package ttnmqtt/0.3/ttnmqtt-0.3-py2-none-any.whl

---
+++ packages/ttnmqtt/ttnmqtt-0.3-py2-none-any.whl/ttnmqtt/ttnmqtt.py
@@ -0,0 +1,38 @@
+import paho.mqtt.client as mqtt
+
+class MQTTClient:
+
+    def __init__(self, APPID, DEVID, PSW):
+        self.client = mqtt.Client()
+        self.APPID = APPID
+        self.PSW = PSW
+        self.DEVID = DEVID
+
+    def connect(self):
+        self.client.username_pw_set(self.APPID, self.PSW)
+        self.client.connect('eu.thethings.network', 1883, 60)
+
+    def setBehavior(self, connect, message, publish):
+        self.client.on_connect = connect
+        self.client.on_publish = publish
+        self.client.on_message = message
+
+    def start(self):
+        print('LOOP STARTED')
+        self.client.loop_forever()
+
+    def startBackground(self):
+        print('LOOP STARTED BACKGROUND')
+        self.client.loop_start()
+
+    def stopBackground(self):
+        print('LOOP STOPPED BACKGROUND')
+        self.client.loop_stop()
+        self.client.disconnect()
+
+    def stop(self):
+        print('LOOP STOPPED')
+        self.client.disconnect()
+
+    def publish(self, msg):
+        self.client.publish(self.APPID+'/devices/'+self.DEVID+'/down', msg)
