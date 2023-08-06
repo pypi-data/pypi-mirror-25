7850873aa0188519beb9f83b3920071b7f14099a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:15 +0000

    Add package pyobjc-framework-iTunesLibrary/4.0/pyobjc-framework-iTunesLibrary-4.0.tar.gz

---
+++ packages/pyobjc-framework-iTunesLibrary/pyobjc-framework-iTunesLibrary-4.0.tar.gz/pyobjc-framework-iTunesLibrary-4.0/setup.py
@@ -0,0 +1,27 @@
+'''
+Wrappers for the "iTunesLibrary" framework on macOS.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+
+Note that using the library requires a signed application bundle.
+'''
+
+from pyobjc_setup import setup
+import os
+
+VERSION="4.0"
+
+setup(
+    name='pyobjc-framework-iTunesLibrary',
+    description = "Wrappers for the framework iTunesLibrary on macOS",
+    packages = [ "iTunesLibrary" ],
+    version=VERSION,
+    install_requires = [
+        'pyobjc-core>='+VERSION,
+        'pyobjc-framework-Cocoa>='+VERSION,
+    ],
+    long_description=__doc__,
+)
