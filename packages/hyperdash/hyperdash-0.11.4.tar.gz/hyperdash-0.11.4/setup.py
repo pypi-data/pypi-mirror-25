88389584791b066ff9c55a132ef05e5219c06a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:06 +0000

    Add package hyperdash/0.11.4/hyperdash-0.11.4.tar.gz

---
+++ packages/hyperdash/hyperdash-0.11.4.tar.gz/hyperdash-0.11.4/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+
+with open(os.path.join("hyperdash", "VERSION"), "r") as f:
+    VERSION = f.read().strip()
+
+setup(
+    name='hyperdash',
+    packages=['hyperdash', 'hyperdash_cli'],
+    install_requires=[
+        'requests',
+        'six>=1.10.0',
+        'python-slugify',
+    ],
+    entry_points={
+        'console_scripts': ['hyperdash = hyperdash_cli.cli:main']
+    },
+    version=VERSION,
+    description='Hyperdash.io CLI and SDK',
+    author='Hyperdash',
+    author_email='support@hyperdash.io',
+    url='https://hyperdash.io',
+)
