d7f16c24fb6730e400d33134bf47e17f205b4437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:34 +0000

    Add package typecraft_python/0.5.3/typecraft_python-0.5.3-py2.py3-none-any.whl

---
+++ packages/typecraft_python/typecraft_python-0.5.3-py2.py3-none-any.whl/typecraft_python/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Tormod Haugland'
+__email__ = 'tormod.haugland@gmail.com'
+__version__ = '0.5.3'
+
+
+__all__ = ['globals', 'models', 'parsing', 'exceptions']
