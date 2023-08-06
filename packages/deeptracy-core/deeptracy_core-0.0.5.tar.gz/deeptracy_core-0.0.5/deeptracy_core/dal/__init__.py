d5598064821330c270872bb2adac522454274e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:22 +0000

    Add package deeptracy-core/0.0.5/deeptracy_core-0.0.5.tar.gz

---
+++ packages/deeptracy-core/deeptracy_core-0.0.5.tar.gz/deeptracy_core-0.0.5/deeptracy_core/dal/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from .project.model import Project
+from .scan.model import Scan
+from .models import Plugin, ScanAnalysis, ScanAnalysisVulnerability, ScanVulnerability
+from .plugin_results import *
