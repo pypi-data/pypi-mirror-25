ed816053dd92cfbf311df437c1752e600b910a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:22 +0000

    Add package django-devutils/0.11.1/django-devutils-0.11.1.tar.gz

---
+++ packages/django-devutils/django-devutils-0.11.1.tar.gz/django-devutils-0.11.1/devutils/tests.py
@@ -0,0 +1,34 @@
+import logging
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+
+class Test(TestCase):
+
+    assert_http_status = [
+    ]
+
+    def setUp(self):
+        self.logger = logging.getLogger(__name__)
+        self.user = User.objects.create_superuser('testrunner', 'test@test.com', 'testrunner')
+        self.client.login(username='testrunner', password='testrunner')
+
+    def tearDown(self):
+        self.client.logout()
+        self.user.delete()
+
+    def test_url(self):
+        for row in self.assert_http_status:
+            url = row['url']
+            status = row['status']
+            response = self.client.get(url)
+
+            if row.get('skip'):
+                logging.warning(f'SKIPPED: {url}')
+                continue
+
+            if response.status_code == status:
+                self.logger.info(f'OK {status} {url}')
+            else:
+                self.logger.error(f'{response.status_code} {url}')
+                raise AssertionError(f'HTTP {response.status_code} for "{url}"')
