16f0a37f7498b03e1a7aa3607eded3f79ea8bbe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:44 +0000

    Add package gems/0.2.6/gems-0.2.6.tar.gz

---
+++ packages/gems/gems-0.2.6.tar.gz/gems-0.2.6/gems/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Blake Printy'
+__email__ = 'bprinty@gmail.com'
+__version__ = '0.2.6'
+
+
+from .datatypes import composite
+from .datatypes import filetree
+
+from .decorators import require
+from .decorators import exception
+from .decorators import keywords
+
+from .metaclasses import DocRequire
