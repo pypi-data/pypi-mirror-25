e06760acb99801c118c0d630525f7a5e12baaf55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:24 +0000

    Add package labugr/1.0.0/labugr-1.0.0-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/labugr/labugr-1.0.0-cp35-cp35m-manylinux1_x86_64.whl/labugr-1.0.0.data/scripts/remove_build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#Eliminar los archivos creados con pip3 install -e o setup.py build
+#Utilizar cuando se desarrolla en python 3.5
+
+#Archivos y carpetas de la build
+sudo rm -r labugr.egg-info
+
+sudo rm -r build
+
+sudo rm -r dist
+
+sudo rm labugr/__config__.py 
+
+#Archivos C generados dinamicamente
+sudo rm labugr/fftpack/src/dct.c 
+
+sudo rm labugr/fftpack/src/dst.c 
+
+sudo rm labugr/fftpack/convolvemodule.c 
+
+sudo rm labugr/fftpack/_fftpackmodule.c
+
+sudo rm labugr/integrate/_lib/_ccallbac_c.c
+
+sudo rm labugr/integrate/_lib/_ccallbac_c.c
+
+sudo rm labugr/signal/src/lfilter.c
+
+sudo rm labugr/signal/src/correlate_nd.c
+
+#Archivo generado por distutils
+sudo rm MANIFEST
+
+#Archivo generado por cythonyze
+sudo rm cythonize.dat
+
+#Archivo generado por cythonyze
+sudo rm site.cfg
+
+#Eliminado los links en la distribucion de python
+directorio=$(pwd)
+
+cd -P /usr/local/lib/python3.5/dist-packages
+
+sudo sed -i '/proyecto/d' ./easy-install.pth
+
+sudo rm labugr.egg-link
+
+cd -P $directorio
+
+echo Archivos de instalacion eliminados
