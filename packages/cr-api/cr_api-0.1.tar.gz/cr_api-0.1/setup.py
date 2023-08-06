a741d6fb02608abd678732655ad4153aed092772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:44 +0000

    Add package cr-api/0.1/cr_api-0.1.tar.gz

---
+++ packages/cr-api/cr_api-0.1.tar.gz/cr_api-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cr_api',
+  packages = ['cr_api'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A wrapper for cr-api.com, a Clash Royale API.',
+  author = 'SML',
+  author_email = 'smlbiobot@gmail.com',
+  url = 'https://github.com/smlbiobot/cr-api-py', # use the URL to the github repo
+  download_url = 'https://github.com/smlbiobot/cr-api-py/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['Clash Royale', 'ClashRoyale', 'Clash', 'Royale', 'API', 'CR', 'CR-API', 'wrapper'], # arbitrary keywords
+  classifiers = [],
+)
