1dc5f810610455037f1cfc6774440d453fdff84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:19 +0000

    Add package pobo/1.0.5/pobo-1.0.5.tar.gz

---
+++ packages/pobo/pobo-1.0.5.tar.gz/pobo-1.0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='pobo',
+      version='1.0.5',
+      description='Python3 parser and network representation of Open Biological and Biomedical Ontologies (OBO)',
+      author='XiaoGerGer',
+      author_email='is.me.xiao@gmail.com',
+      url='https://github.com/mschubert/python-obo',
+      packages=[],
+      keywords='parse obo',
+      license="MIT",
+      long_description=open("README.rst").read()
+      )
