af24894010fa8e7882d1baeac73261e0592b93df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:29 +0000

    Add package rocketchat-API/0.6.3/rocketchat_API-0.6.3.tar.gz

---
+++ packages/rocketchat-API/rocketchat_API-0.6.3.tar.gz/rocketchat_API-0.6.3/setup.py
@@ -0,0 +1,18 @@
+# -*-coding:utf-8-*-
+
+from setuptools import setup
+
+setup(
+    name='rocketchat_API',
+    version='0.6.3',
+    packages=['rocketchat_API', 'rocketchat_API.APIExceptions'],
+    url='https://github.com/jadolg/rocketchat_API',
+    license='MIT',
+    author='Jorge Alberto Díaz Orozco',
+    author_email='diazorozcoj@gmail.com',
+    description='Python API wrapper for Rocket.Chat',
+    long_description=open("README.md", "r").read(),
+    install_requires=(
+        'requests',
+    )
+)
