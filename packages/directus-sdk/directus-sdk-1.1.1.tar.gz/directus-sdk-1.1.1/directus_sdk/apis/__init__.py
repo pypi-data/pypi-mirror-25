97abf9e4673db8ec6946557f4f049d8de0e4d7db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:43 +0000

    Add package directus-sdk/1.1.1/directus-sdk-1.1.1.tar.gz

---
+++ packages/directus-sdk/directus-sdk-1.1.1.tar.gz/directus-sdk-1.1.1/directus_sdk/apis/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .activity_api import ActivityApi
+from .bookmarks_api import BookmarksApi
+from .files_api import FilesApi
+from .groups_api import GroupsApi
+from .messages_api import MessagesApi
+from .preferences_api import PreferencesApi
+from .settings_api import SettingsApi
+from .tables_api import TablesApi
+from .utils_api import UtilsApi
