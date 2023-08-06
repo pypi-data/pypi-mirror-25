b4e56fd727234ea9bc608f4086303391a8fe0e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:09 +0000

    Add package jwt-generator/0.3.2/jwt-generator-0.3.2.tar.gz

---
+++ packages/jwt-generator/jwt-generator-0.3.2.tar.gz/jwt-generator-0.3.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+"""setup.py: setuptools control."""
+
+
+import re
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), 'r') as f:
+    long_description = f.read()
+
+version="0.3.2"
+
+setup(
+    name = "jwt-generator",
+    packages = ["generator"],
+    entry_points = {
+        "console_scripts": ['jwt-generator = generator.jwt_generator:main']
+        },
+    version = version,
+    description = "unix command line JWT generator.",
+    long_description=long_description,
+    include_package_data=True,
+    zip_safe=True,
+    platforms='any',
+    install_requires=['jwt', 'click'],
+    author = "Mark van Holsteijn",
+    author_email = "mvanholsteijn@xebia.com",
+    url = "https://github.com/mvanholsteijn/jwt-generator",
+    )
