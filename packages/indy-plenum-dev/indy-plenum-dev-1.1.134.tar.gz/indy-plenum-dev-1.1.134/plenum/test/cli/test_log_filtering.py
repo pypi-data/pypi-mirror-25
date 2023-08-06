6738718aa84814b2d4895356add547662f68a110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:13 +0000

    Add package indy-plenum-dev/1.1.134/indy-plenum-dev-1.1.134.tar.gz

---
+++ packages/indy-plenum-dev/indy-plenum-dev-1.1.134.tar.gz/indy-plenum-dev-1.1.134/plenum/test/cli/test_log_filtering.py
@@ -0,0 +1,25 @@
+import types
+
+from plenum.test.cli.helper import checkRequest
+from plenum.test.helper import waitForSufficientRepliesForRequests
+
+
+def testLogFiltering(cli, validNodeNames, createAllNodes):
+    msg = '{"Hello": "There", "type": "greeting"}'
+    client, wallet = checkRequest(cli, msg)
+
+    x = client.handleOneNodeMsg
+
+    def handleOneNodeMsg(self, wrappedMsg, excludeFromCli=None):
+        return x(wrappedMsg, excludeFromCli=True)
+
+    client.handleOneNodeMsg = types.MethodType(handleOneNodeMsg, client)
+    client.nodestack.msgHandler = client.handleOneNodeMsg
+    msg = '{"Hello": "Where", "type": "greeting"}'
+    cli.enterCmd('client {} send {}'.format(client.name, msg))
+
+    lastRequestId = wallet._getIdData().lastReqId
+    waitForSufficientRepliesForRequests(cli.looper, client,
+                                        requestIds=[lastRequestId])
+
+    assert "got msg from node" not in cli.lastCmdOutput
