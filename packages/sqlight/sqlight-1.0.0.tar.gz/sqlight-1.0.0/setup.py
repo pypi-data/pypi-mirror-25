7ca635f882150f45e2000a4ba594173cf7a69a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:36 +0000

    Add package sqlight/1.0.0/sqlight-1.0.0.tar.gz

---
+++ packages/sqlight/sqlight-1.0.0.tar.gz/sqlight-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from sqlight import version
+
+setup(
+    name="sqlight",
+    version=version,
+    description="A lightweight wrapper around SQLite.",
+    long_description='A lightweight wrapper around SQLite. just on Python3',
+    keywords='sqlite sqlite3 lightweight wrapper',
+    author='laoma',
+    author_email='laomafeima@gmail.com',
+    url='https://github.com/laomafeima/sqlight',
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    py_modules=["sqlight"],
+    )
