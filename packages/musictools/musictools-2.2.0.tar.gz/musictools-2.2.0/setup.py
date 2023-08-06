041a56e2e40b064446a843b35c9f2b10155874f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:25 +0000

    Add package musictools/2.2.0/musictools-2.2.0.tar.gz

---
+++ packages/musictools/musictools-2.2.0.tar.gz/musictools-2.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='musictools',
+    version='2.2.0',
+    description='Lets you repair your music files by adding metadata and album art',
+    url='https://github.com/lakshaykalbhor/musictools',
+    author='Lakshay Kalbhor',
+    author_email='lakshaykalbhor@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'bs4',
+        'youtube-dl',
+        'mutagen',
+        'spotipy',
+        'requests',
+    ],
+)
