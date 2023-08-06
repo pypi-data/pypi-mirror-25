61ef27bc53c59b37a2075983015a672da5305bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:03 +0000

    Add package netpyne/0.7.4/netpyne-0.7.4-py2-none-any.whl

---
+++ packages/netpyne/netpyne-0.7.4-py2-none-any.whl/netpyne/sim.py
@@ -0,0 +1,27 @@
+"""
+sim.py
+
+Contains all the model shared variables and modules.
+It is imported as "sim" from all other file,  so that any variable or module can be referenced from any module using sim.varName
+
+Contributors: salvadordura@gmail.com
+"""
+
+# check for -nogui option
+import sys
+if '-nogui' in sys.argv:
+    import netpyne
+    netpyne.__gui__ = False
+
+# import all required modules
+from simFuncs import *
+from neuromlFuncs import *
+from wrappers import *
+import analysis
+from network import Network
+from cell import CompartCell, PointCell, NML2Cell, NML2SpikeSource
+from pop import Pop
+import utils
+from neuron import h
+import tests
+from tests.tests import SimTestObj
