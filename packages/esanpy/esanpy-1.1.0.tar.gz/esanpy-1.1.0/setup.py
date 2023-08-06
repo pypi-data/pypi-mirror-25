245b67994eed2b19e1f213f4a1083f43478dd7e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:25 +0000

    Add package esanpy/1.1.0/esanpy-1.1.0.tar.gz

---
+++ packages/esanpy/esanpy-1.1.0.tar.gz/esanpy-1.1.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION = "1.1.0"
+
+setup(
+    name="esanpy",
+    version=VERSION,
+    packages=['esanpy'],
+    author="CodeLibs",
+    author_email="dev@codelibs.org",
+    license="Apache Software License",
+    description=("Elasticsearch based Text Analyzer."),
+    keywords="text analyzer",
+    url="https://github.com/codelibs/esanpy",
+    download_url='https://github.com/codelibs/esanpy/tarball/' + VERSION,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    entry_points={
+        "console_scripts": [
+            "esanpy=esanpy:main"
+        ],
+    },
+)
