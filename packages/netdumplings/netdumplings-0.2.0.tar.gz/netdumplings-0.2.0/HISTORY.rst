b82139ad4aa43ddbb2bcf4688e7bf23473e77516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:50 +0000

    Add package netdumplings/0.2.0/netdumplings-0.2.0.tar.gz

---
+++ packages/netdumplings/netdumplings-0.2.0.tar.gz/netdumplings-0.2.0/HISTORY.rst
@@ -0,0 +1,23 @@
+.. :changelog:
+
+Release History
+---------------
+
+0.2.0 (2017-10-15)
+++++++++++++++++++
+
+* snifty now maintains more information on the chefs it's sending packets to
+* Added time to ARPChef dumpling payload
+* Added time of last lookup to DNSLookupChef
+* Changed time format to float milliseconds
+* Minor code style and documentation tweaks
+
+0.1.0 (2016-11-05)
+++++++++++++++++++
+
+* First actual github release (0.0.1 was really just the first git commit)
+
+0.0.1 (2016-04-22)
+++++++++++++++++++
+
+* Initial release
---
+++ packages/netdumplings/netdumplings-0.2.0.tar.gz/netdumplings-0.2.0/netdumplings/dumplingchefs/dnslookupchef.py
@@ -0,0 +1,98 @@
+import time
+
+from netdumplings import DumplingChef, DumplingDriver
+
+
+class DNSLookupChef(DumplingChef):
+    """
+    Makes dumplings which describe DNS activity.  Sends per-packet dumplings
+    for individual DNS (Domain Name System) lookups; and poke-interval
+    dumplings which describe the hosts lookups seen so far with per-host lookup
+    counts and timestamp of last lookup.
+
+    Dumpling payload examples:
+
+    Per DNS lookup: ::
+
+        {
+            "lookup": {
+                "hostname": "srirachamadness.com",
+                "when": 1499040017.811247
+            }
+        }
+
+    Per poke-interval: ::
+
+        {
+            "lookups_seen": {
+                "srirachamadness.com": {
+                    "count": 28,
+                    "latest": 1499040017.811247
+                },
+                "www.fleegle.com": {
+                    "count": 1,
+                    "latest": 1499045642.57563
+                },
+                "floople.com": {
+                    "count": 7,
+                    "latest": 1499043343.104648
+                }
+            }
+        }
+    """
+    def __init__(self, kitchen=None, dumpling_queue=None, receive_pokes=True):
+        """
+        """
+        super().__init__(kitchen=kitchen, dumpling_queue=dumpling_queue,
+                         receive_pokes=receive_pokes)
+        self.lookups_seen = {}
+
+    def packet_handler(self, packet):
+        """
+        Processes a packet from nd-snifty.  Makes a dumpling summarizing the
+        contents of each each valid DNS lookup.
+
+        :param packet: Packet from nd-snifty.
+        """
+        if not packet.haslayer('DNS'):
+            return
+
+        dns_query = packet.getlayer('DNS')
+        query = dns_query.fields['qd']
+        hostname = query.qname.decode('utf-8')
+
+        if hostname.endswith('.'):
+            hostname = hostname[:-1]
+
+        now_millis = time.time() * 1000
+
+        try:
+            self.lookups_seen[hostname]['count'] += 1
+            self.lookups_seen[hostname]['latest'] = now_millis
+        except KeyError:
+            self.lookups_seen[hostname] = {
+                'count': 1,
+                'latest': now_millis,
+            }
+
+        payload = {
+            'lookup': {
+                'hostname': hostname,
+                'when': now_millis
+            }
+        }
+
+        self.send_dumpling(payload=payload, driver=DumplingDriver.packet)
+
+    def interval_handler(self, interval=None):
+        """
+        Makes a dumpling at regular intervals which summarizes all the host
+        lookups seen so far along with the count and latest lookup time for
+        each host.
+        """
+        payload = {
+            'lookups_seen': self.lookups_seen
+        }
+
+        self.send_dumpling(payload=payload, driver=DumplingDriver.interval)
+
