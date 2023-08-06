a6d3f2b7f3cb263066966975d37b1a3edb957a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:54 +0000

    Add package toastedmarshmallow/0.2.0/toastedmarshmallow-0.2.0-py2.py3-none-any.whl

---
+++ packages/toastedmarshmallow/toastedmarshmallow-0.2.0-py2.py3-none-any.whl/toastedmarshmallow/__init__.py
@@ -0,0 +1,32 @@
+from marshmallow import SchemaJit
+
+from .jit import (
+    generate_marshall_method,
+    generate_unmarshall_method,
+    JitContext
+)
+
+__version__ = '0.2.0'
+
+
+class Jit(SchemaJit):
+    def __init__(self, schema, use_cython=False):
+        super(Jit, self).__init__(schema)
+        self.schema = schema
+        self.marshal_method = generate_marshall_method(
+            schema, context=JitContext(use_cython=use_cython))
+        self.unmarshal_method = generate_unmarshall_method(
+            schema, context=JitContext(use_cython=use_cython))
+
+    @property
+    def jitted_marshal_method(self):
+        return self.marshal_method
+
+    @property
+    def jitted_unmarshal_method(self):
+        return self.unmarshal_method
+
+
+class CythonJit(Jit):
+    def __init__(self, schema):
+        super(CythonJit, self).__init__(schema, use_cython=True)
