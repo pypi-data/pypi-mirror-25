8cc1c74c715f43a3e3c6415445c38a046c461c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:21 +0000

    Add package trol/0.2.2/trol-0.2.2.tar.gz

---
+++ packages/trol/trol-0.2.2.tar.gz/trol-0.2.2/trol/database.py
@@ -0,0 +1,41 @@
+from trol import ModelType, Collection, Property
+
+
+class DatabaseType(type):
+    def __init__(cls, *args, **kwargs):
+        """
+        TODO: Write a docstring
+        """
+        cls._trol_properties = dict()
+        cls._trol_collections = dict()
+        cls._trol_models = dict()
+
+        for attrname, attr in cls.__dict__.items():
+            if isinstance(attr, Property):
+                cls._trol_properties[attrname] = attr
+
+                if attr._name is None:
+                    attr._name = attrname
+
+            if isinstance(attr, Collection):
+                cls._trol_collections[attrname] = attr
+
+                if attr._name is None:
+                    attr._name = attrname
+
+                attr._trol_database = cls
+
+            if isinstance(attr, ModelType):
+                cls._trol_models[attrname] = attr
+                attr._trol_database = cls
+
+        super().__init__(*args, **kwargs)
+
+
+class Database(metaclass=DatabaseType):
+    # Wait a minute! This class is empty
+    # Ya, but it gives you the nice syntactic sugar of:
+    #     class MyDB(Database):
+    #
+    # and avoids the question "What's a metaclass?"
+    redis = None
