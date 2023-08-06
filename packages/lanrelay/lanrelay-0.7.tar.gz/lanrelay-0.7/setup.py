e2a21f75b93d2eb90f7ef33854d4bee5f6b3d62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:23 +0000

    Add package lanrelay/0.7/lanrelay-0.7.tar.gz

---
+++ packages/lanrelay/lanrelay-0.7.tar.gz/lanrelay-0.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'lanrelay',
+  packages = ['lanrelay'], # this must be the same as the name above
+  version = '0.7',
+  description = 'Eight channel lan relay control',
+  author = 'Max van Rooijen',
+  author_email = 'max@triangle1.net',
+  url = 'https://github.com/elmexdechileen/eightchanrelay', # use the URL to the github repo
+  download_url = 'https://github.com/elmexdechileen/eightchanrelay/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['relay', 'home automation', 'hass'], # arbitrary keywords
+  classifiers = [],
+    
+)
