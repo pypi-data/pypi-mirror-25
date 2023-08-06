0675c80c62e819579978d8ef9f074185a71b3b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:51 +0000

    Add package dsbox.overfitdetector/0.0.2.5/dsbox.overfitdetector-0.0.2.5.tar.gz

---
+++ packages/dsbox.overfitdetector/dsbox.overfitdetector-0.0.2.5.tar.gz/dsbox.overfitdetector-0.0.2.5/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'dsbox.overfitdetector',
+  packages = ['dsbox.overfitdetector'],
+  version = '0.0.2.5',
+  description = 'A library for determining if a machine learning model is overfit. Used for regression and classification tasks.',
+  author = 'Matt Michelson',
+  author_email = 'mmichelson@inferlink.com',
+  url = 'https://github.com/usc-isi-i2/dsbox-overfit-detector',
+  download_url = 'https://github.com/usc-isi-i2/dsbox-overfit-detector/archive/0.0.4.tar.gz',
+  keywords = ['machine learning', 'generalization', 'overfitting'],
+  install_requires=["nose == 1.3.7", "numpy == 1.13.1", "pandas == 0.20.3","scikit-learn == 0.19.0",
+                    "scipy == 0.19.1", "six == 1.10.0", "sklearn == 0.0"],
+    python_requires='>=3',
+    entry_points={
+        'd3m.primitives': [
+            'dsbox.overfitdetector = dsbox.overfitdetector.detector:Detector',
+        ],
+    }
+)
