e3161a31eb72111c1a69a6fdb773768b3df255a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:16 +0000

    Add package omnic/0.1.15/omnic-0.1.15.tar.gz

---
+++ packages/omnic/omnic-0.1.15.tar.gz/omnic-0.1.15/omnic/builtin/services/viewer/converters.py
@@ -0,0 +1,52 @@
+import json
+
+import omnic
+from omnic.builtin.types.nodejs import NODE_PACKAGE
+from omnic.conversion import converter
+from omnic.web.viewer import VIEWER_EXT
+
+PACKAGE_JSON_TEMPLATE = {
+    'name': 'omnicviewer',
+    'version': omnic.__version__,
+    'description': 'Auto-generated viewer package',
+    'main': 'index.js',
+    'dependencies': None,
+    'author': '',
+    'license': 'GPL-3.0'
+}
+
+
+LOADED_JS = '''
+window._OMNIC_VIEWER_BUNDLE_IS_LOADED = true;
+'''
+
+
+def generate_index_js(viewer_names):
+    requires = '\n'.join('require("%s");' % name for name in viewer_names)
+    return '%s\n%s' % (requires, LOADED_JS)
+
+
+class ViewerNodePackageBuilder(converter.Converter):
+    inputs = [
+        VIEWER_EXT.upper(),
+    ]
+    outputs = [
+        str(NODE_PACKAGE),
+    ]
+
+    async def convert(self, in_resource, out_resource):
+
+        # Read in dependencies object from the in resource
+        with in_resource.cache_open('r') as fd:
+            dependencies = json.load(fd)
+
+        # Assemble the package json and write to file
+        package_json_dict = dict(PACKAGE_JSON_TEMPLATE)
+        package_json_dict['dependencies'] = dependencies
+        with out_resource.cache_open_as_dir('package.json', 'w') as fd:
+            json.dump(package_json_dict, fd, indent=4)
+
+        # Write the index JavaScript file that serves as the entry point for
+        # all viewers
+        with out_resource.cache_open_as_dir('index.js', 'w') as fd:
+            fd.write(generate_index_js(dependencies.keys()))
---
+++ packages/omnic/omnic-0.1.15.tar.gz/omnic-0.1.15/omnic/web/security.py
@@ -0,0 +1,60 @@
+import hmac
+
+from omnic import singletons
+from omnic.types.resource import ForeignResource
+from omnic.utils.security import get_hmac_sha1_digest
+
+
+class SecurityException(Exception):
+    pass
+
+
+class InvalidDigestException(SecurityException):
+    pass
+
+
+class InvalidQueryDataException(SecurityException):
+    pass
+
+
+class SecurityChecker:
+    pass
+
+
+class DummySecurity(SecurityChecker):
+    async def check(self, typestring, querydata):
+        pass
+
+# Simple built-in hmac request checker
+
+
+class HmacSha1(SecurityChecker):
+    async def check(self, typestring, querydata):
+        if 'url' not in querydata:
+            raise InvalidQueryDataException('URL missing')
+        if 'digest' not in querydata:
+            raise InvalidQueryDataException('digest missing')
+
+        url_string = querydata['url'][0]
+        supplied_digest = querydata['digest'][0]
+        correct_digest = get_hmac_sha1_digest(
+            singletons.settings.HMAC_SECRET,
+            url_string,
+            typestring,
+        )
+
+        if not hmac.compare_digest(supplied_digest.lower(), correct_digest):
+            raise InvalidDigestException(
+                'Incorrect digest supplied: ' + correct_digest)
+
+
+async def check(typestring, querydata):
+    if singletons.settings.SECURITY is not None:
+        checker_class = singletons.settings.load('SECURITY')
+    else:
+        checker_class = DummySecurity
+    checker = checker_class()
+    await checker.check(typestring, querydata)
+    url_string = querydata['url'][0]
+    foreign_res = ForeignResource(url_string)
+    foreign_res.validate()
