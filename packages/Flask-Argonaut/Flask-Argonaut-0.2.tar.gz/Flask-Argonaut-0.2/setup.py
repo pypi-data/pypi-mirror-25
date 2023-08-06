0ef4cee64988e85c5b4622ed8cf0272bee49db01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:43 +0000

    Add package Flask-Argonaut/0.2/Flask-Argonaut-0.2.tar.gz

---
+++ packages/Flask-Argonaut/Flask-Argonaut-0.2.tar.gz/Flask-Argonaut-0.2/setup.py
@@ -0,0 +1,36 @@
+"""
+    Flask-Argonaut
+    ------------
+    Argon2 hashing for your Flask.
+
+"""
+from setuptools import setup
+import re, io
+
+__version__ = re.search(
+    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  # It excludes inline comment too
+    io.open('flask_argonaut/__init__.py', encoding='utf_8_sig').read()
+    ).group(1)
+
+setup(name='Flask-Argonaut',
+      version=__version__,
+      description='Flask extension use hashing data with Argon2',
+      author='Anton Oleynik',
+      author_email='levantado@me.com',
+      license='BSD',
+      urls='https://github.com/Levantado/Flask-Argonaut',
+      download_url = 'https://github.com/Levantado/Flask-Argonaut/tarball/v0.18',
+      packages=['flask_argonaut'],
+      platforms='any',
+      install_requires=['Flask', 'argon2'],
+      classifiers=[
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      zip_safe=False,
+      test_suite='test')
