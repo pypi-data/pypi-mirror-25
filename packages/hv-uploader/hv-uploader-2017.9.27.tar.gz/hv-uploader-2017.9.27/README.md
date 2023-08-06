c649078b978c676db427165771d32ccff8b7516c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:16 +0000

    Add package hv-uploader/2017.9.27/hv-uploader-2017.9.27.tar.gz

---
+++ packages/hv-uploader/hv-uploader-2017.9.27.tar.gz/hv-uploader-2017.9.27/README.md
@@ -0,0 +1,27 @@
+# Heavy Uploader
+Python script for uploading and managing content on the Heavy Cloud Service (https://enzienaudio.com)
+
+### Installing
+
+`$ pip install hv-uploader`
+
+### Upgrading
+
+`$ pip install hv-uploader -U`
+
+(Note: this will also update any dependencies)
+
+---
+## Development
+
+### Creating and uploading new package version
+
+`$ pip install twine`
+
+Update the `version` string in [setup.py](https://github.com/enzienaudio/hv-uploader/blob/master/setup.py#L8) to the date of version upload.
+
+`$ rm -rf ./dist/`
+
+`$ python setup.py sdist bdist_wheel`
+
+`$ twine upload dist/* `
---
+++ packages/hv-uploader/hv-uploader-2017.9.27.tar.gz/hv-uploader-2017.9.27/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(name="hv-uploader",
+      version="2017.09.27",
+      description="Uploads content to the Heavy Cloud Service (https://enzienaudio.com).",
+      long_description=readme(),
+      classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities"
+      ],
+      keywords="procedural audio uploader heavy enzien audio interactive games",
+      url="http://github.com/enzienaudio/hv-uploader",
+      author="Enzien Audio Ltd",
+      author_email="info@enzienaudio.com",
+      license="MIT",
+      packages=["hv_uploader"],
+      install_requires=[
+          "requests",
+      ],
+      entry_points={
+          "console_scripts": ["hv-uploader=hv_uploader:main"],
+      },
+      include_package_data=True,
+      zip_safe=True)
