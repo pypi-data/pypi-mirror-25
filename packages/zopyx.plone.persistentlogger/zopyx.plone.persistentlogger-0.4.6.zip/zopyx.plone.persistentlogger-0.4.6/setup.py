c9184fe359b1bcd3794f8e45872bd232c1df892b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:05 +0000

    Add package zopyx.plone.persistentlogger/0.4.6/zopyx.plone.persistentlogger-0.4.6.zip

---
+++ packages/zopyx.plone.persistentlogger/zopyx.plone.persistentlogger-0.4.6.zip/zopyx.plone.persistentlogger-0.4.6/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.4.6'
+
+setup(name='zopyx.plone.persistentlogger',
+      version=version,
+      description="Persistent logging for Plone objects",
+      long_description=open(os.path.join("docs", "source", "README.rst")).read() + "\n" +
+      open(os.path.join("docs", "source", "HISTORY.rst")).read(),
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.3",
+          "Framework :: Plone :: 5.0",
+          "Framework :: Zope2",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='Plone Logging',
+      author='Andreas Jung',
+      author_email='info@zopyx.com',
+      url='http://pypi.python.org/pypi/zopyx.plone.persistentlogger',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['zopyx', 'zopyx.plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.api',
+          # -*- Extra requirements: -*-
+      ],
+      tests_require=['zope.testing'],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
