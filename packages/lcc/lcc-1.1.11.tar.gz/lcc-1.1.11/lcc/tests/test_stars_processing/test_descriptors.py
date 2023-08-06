3f8902e8fd7556c52ef3ba7a0193cf5cf7c06e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:59 +0000

    Add package lcc/1.1.11/lcc-1.1.11.tar.gz

---
+++ packages/lcc/lcc-1.1.11.tar.gz/lcc-1.1.11/lcc/tests/test_stars_processing/test_descriptors.py
@@ -0,0 +1,39 @@
+import logging
+
+from lcc.data_manager.package_reader import PackageReader
+from lcc.entities.star import Star
+import unittest
+import numpy as np
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class TestDescriptors(unittest.TestCase):
+
+    n_stars = 10
+
+    def setUp(self):
+        self.descriptors = PackageReader.getClassesDict("descriptors")
+        self.stars = []
+        for i in range(self.n_stars):
+            star = Star()
+            x = np.linspace(0, 100, 10)
+            y = np.sin(x**i)
+            star.putLightCurve([x, y])
+            self.stars.append(star)
+
+    def testAbbe(self):
+        abbe_filter = self.descriptors["AbbeValueDescr"]
+        abbe_values = abbe_filter(bins=10).getSpaceCoords(self.stars)
+        self.failUnless(len(abbe_values) == self.n_stars)
+
+        # TODO
+        # assert False not in [abbe_values[i] < abbe_values[i + 1] for i in range(len(abbe_values) - 1)]
+
+    def testComparative(self):
+        logging.debug("Starting comparative test")
+        descr = self.descriptors["HistShapeDescr"](self.stars[:4], 10, 10)
+
+
+if __name__ == "__main__":
+    unittest.main()
