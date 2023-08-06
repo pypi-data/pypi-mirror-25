08158b81b00cea0cbc52c53a468a9d65a4577ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:06 +0000

    Add package InSilicoSeq/0.5.0/InSilicoSeq-0.5.0.tar.gz

---
+++ packages/InSilicoSeq/InSilicoSeq-0.5.0.tar.gz/InSilicoSeq-0.5.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='InSilicoSeq',
+    version='0.5.0',
+
+    description='a sequencing simulator',
+
+    url='https://github.com/HadrienG/InSilicoSeq',
+    download_url='https://github.com/HadrienG/InSilicoSeq/tarball/0.5.0',
+    author='Hadrien Gourlé',
+    author_email='hadrien.gourle@slu.se',
+
+    license='MIT',
+    packages=find_packages(),
+
+    tests_require=['nose'],
+    install_requires=['numpy', 'scipy', 'biopython', 'pysam', 'future'],
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': ['iss = iss.app:main'],
+    }
+)
