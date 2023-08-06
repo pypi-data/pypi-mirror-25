a47ae403c62f87141146d0c550c8b0d634e188cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:17 +0000

    Add package bilibiliSpiderSet/0.1.17/bilibiliSpiderSet-0.1.17.tar.gz

---
+++ packages/bilibiliSpiderSet/bilibiliSpiderSet-0.1.17.tar.gz/bilibiliSpiderSet-0.1.17/bilibiliSpiderSet/info.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: Haut-Stone
+# @Date:   2017-09-30 09:56:23
+# @Last Modified by:   Haut-Stone
+# @Last Modified time: 2017-09-30 09:56:40
+
+
+cookies = {
+	'DedeUserID': '221013145',
+	'DedeUserID__ckMd5': '0ada37d8e37bee1f',
+	'SESSDATA': 'ddff3d5b%2C1508937653%2C5dc59211'
+}
