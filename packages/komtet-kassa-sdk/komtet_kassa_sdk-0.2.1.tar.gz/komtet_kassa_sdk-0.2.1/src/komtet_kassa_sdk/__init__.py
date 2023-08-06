3130ba0a3f8a762ae1cc7c8fcdb8ab12919aee51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:21 +0000

    Add package komtet-kassa-sdk/0.2.1/komtet_kassa_sdk-0.2.1.tar.gz

---
+++ packages/komtet-kassa-sdk/komtet_kassa_sdk-0.2.1.tar.gz/komtet_kassa_sdk-0.2.1/src/komtet_kassa_sdk/__init__.py
@@ -0,0 +1,5 @@
+from .check import Check, CorrectionCheck, Intent, TaxSystem, VatRate, CorrectionType
+from .client import Client, Task
+
+
+__all__ = ['Check', 'Client', 'Intent', 'Task', 'TaxSystem', 'VatRate']
