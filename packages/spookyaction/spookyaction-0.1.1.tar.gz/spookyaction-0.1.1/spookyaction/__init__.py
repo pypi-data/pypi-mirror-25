6a07049841cf5ac6fed45ef3ee14e314b0c7837f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:12 +0000

    Add package spookyaction/0.1.1/spookyaction-0.1.1.tar.gz

---
+++ packages/spookyaction/spookyaction-0.1.1.tar.gz/spookyaction-0.1.1/spookyaction/__init__.py
@@ -0,0 +1,37 @@
+import pyautogui
+import os
+import item_object
+from item_object import Item, Items
+from functools import wraps
+from error_handling import Error
+
+METHODS = { }
+currentresults = []
+
+class GUI_INTERFACE():
+    def __init__(self):
+        pass
+
+    def keywrite(self, *args):
+        pyautogui.keywrite(list(args))
+
+ui = GUI_INTERFACE()
+
+def new_method(name):
+    def new_method_wrapper(fn):
+        METHODS[name] = fn
+        @wraps(fn)
+        def wrapped_method(*args, **kwargs):
+            fn(*args, **kwargs)
+        return wrapped_method
+    return new_method_wrapper
+
+
+def run(obj=None, cmd=None, cmd_args=None, cmd_kwargs=None):
+    METHODS["START"](obj, cmd)
+    try:
+        currentresults = METHODS[cmd](*cmd_args, **cmd_kwargs)
+    except Error as e:
+        METHODS["FAILED"](e, obj, cmd)
+    else:
+        METHODS["COMPLETED"](obj, cmd)
