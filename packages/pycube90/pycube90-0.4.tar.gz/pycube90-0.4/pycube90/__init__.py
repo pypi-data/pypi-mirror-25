5dee092cee7b7543772d11b8711bed994d8fbe93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:15 +0000

    Add package pycube90/0.4/pycube90-0.4.tar.gz

---
+++ packages/pycube90/pycube90-0.4.tar.gz/pycube90-0.4/pycube90/__init__.py
@@ -0,0 +1,2 @@
+from pycube90 import Cube
+from pycube90 import CubePassword
---
+++ packages/pycube90/pycube90-0.4.tar.gz/pycube90-0.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'pycube90',
+  packages = ['pycube90'],
+  version = '0.4',
+  description = 'Python implementation of the Cube90 cipher',
+  author = 'Karl Zander',
+  author_email = 'pvial@kryptomagik.com',
+  url = 'https://github.com/pvial00/pycube90',
+  keywords = ['cryptography', 'encryption', 'security'],
+  classifiers = [],
+)
