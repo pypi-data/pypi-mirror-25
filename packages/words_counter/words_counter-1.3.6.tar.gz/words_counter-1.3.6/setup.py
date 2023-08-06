14f3a91133ca880ca1c8d312d678a32b71d1534e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:41 +0000

    Add package words_counter/1.3.6/words_counter-1.3.6.tar.gz

---
+++ packages/words_counter/words_counter-1.3.6.tar.gz/words_counter-1.3.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='words_counter',
+    version='1.3.6',
+    packages=find_packages(),
+    author='Vladimir Aleshin',
+    author_email='rancvova@gmail.com',
+    url='https://github.com/Ranc58/verbs_counter',
+    license='MIT License',
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    entry_points={
+        'console_scripts':
+            ['words_counter = words_counter.words_counter:main']
+    },
+    install_requires=[
+        'six==1.11.0',
+        'nltk==3.2.4',
+        'GitPython==2.1.7',
+    ],
+    test_suite='words_counter.tests',
+)
