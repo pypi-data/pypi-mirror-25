d0706064cb06d10d7f94b734201478a519ea312b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:12 +0000

    Add package pobo/1.0.2/pobo-1.0.2.tar.gz

---
+++ packages/pobo/pobo-1.0.2.tar.gz/pobo-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pobo',
+      version='1.0.2',
+      description='Parse .obo file',
+      author='XiaoGerGer',
+      author_email='is.me.xiao@gmail.com',
+      url='https://github.com/mschubert/python-obo',
+      packages=["pobo"],
+      keywords='parse obo'
+      )
