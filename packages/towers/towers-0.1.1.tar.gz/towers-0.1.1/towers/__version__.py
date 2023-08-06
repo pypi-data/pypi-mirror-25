49318fa4a409c7a3e0fd90f5bf36b07b2a24f482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:17 +0000

    Add package towers/0.1.1/towers-0.1.1.tar.gz

---
+++ packages/towers/towers-0.1.1.tar.gz/towers-0.1.1/towers/__version__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+# pragma: no-cover
+
+__title__ = 'towers'
+__description__ = 'Towers of Hanoi for Python'
+__url__ = 'http://towers.readthedocs.io/en/latest'
+__version__ = '0.1.1'
+__author__ = 'Francis Horsman'
+__author_email__ = 'francis.horsman@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Francis Horsman'
