fe55b67f2921dceab5fdef01cbfdd6b96cab6a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:10 +0000

    Add package mrrdt_vision/1.0.0/mrrdt_vision-1.0.0.tar.gz

---
+++ packages/mrrdt_vision/mrrdt_vision-1.0.0.tar.gz/mrrdt_vision-1.0.0/mrrdt_vision/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.5
+# -------------------------------------------------------------------------
+# Multi-Rotor Robot Design Team
+# Missouri University of Science and Technology
+# Fall 2017
+# Christopher O'Toole
+# -------------------------------------------------------------------------
+
+__version__ = '1.0.0'
