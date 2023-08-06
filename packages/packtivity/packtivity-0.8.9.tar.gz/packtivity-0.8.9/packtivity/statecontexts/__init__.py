922378c4d5a6021b9916c20edc48ecf7a4633085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:51 +0000

    Add package packtivity/0.8.9/packtivity-0.8.9.tar.gz

---
+++ packages/packtivity/packtivity-0.8.9.tar.gz/packtivity-0.8.9/packtivity/statecontexts/__init__.py
@@ -0,0 +1,21 @@
+import os
+import importlib
+from .posixfs_context import LocalFSState,LocalFSProvider
+
+def load_state(jsondata):
+    if jsondata['state_type'] == 'localfs':
+        return LocalFSState.fromJSON(jsondata)
+    if 'PACKTIVITY_STATEPROVIDER' in os.environ:
+        module = importlib.import_module(os.environ['PACKTIVITY_STATEPROVIDER'])
+        return module.load_state(jsondata)
+    raise TypeError('unknown state type {}'.format(jsondata['state_type']))
+
+def load_provider(jsondata):
+    if jsondata == None:
+        return None
+    if jsondata['state_provider_type'] == 'localfs_provider':
+        return LocalFSProvider.fromJSON(jsondata)
+    if 'PACKTIVITY_STATEPROVIDER' in os.environ:
+        module = importlib.import_module(os.environ['PACKTIVITY_STATEPROVIDER'])
+        return module.load_provider(jsondata)
+    raise TypeError('unknown provider type {}'.format(jsondata['state_provider_type']))
