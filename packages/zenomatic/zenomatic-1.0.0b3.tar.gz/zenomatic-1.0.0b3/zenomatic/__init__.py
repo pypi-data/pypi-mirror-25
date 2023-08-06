ac2b8014a0fe05f0222b4d0cf67b59e2265cae24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:41 +0000

    Add package zenomatic/1.0.0b3/zenomatic-1.0.0b3.tar.gz

---
+++ packages/zenomatic/zenomatic-1.0.0b3.tar.gz/zenomatic-1.0.0b3/zenomatic/__init__.py
@@ -0,0 +1,10 @@
+"""Module Initialization.
+
+Start colorama library and puts quotes on namespace.
+"""
+from colorama import init
+from zenomatic.quotes import get_quote, get_quotes, get_random_quote
+
+init()
+
+__version__ = "1.0.0b3"
