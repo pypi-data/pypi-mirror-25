a881621936f123a6ba2219fc52ca3b99bc2bcca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:18 +0000

    Add package molo.profiles/5.4.1/molo.profiles-5.4.1.tar.gz

---
+++ packages/molo.profiles/molo.profiles-5.4.1.tar.gz/molo.profiles-5.4.1/molo/profiles/wagtail_hooks.py
@@ -0,0 +1,36 @@
+from molo.profiles.admin import FrontendUsersModelAdmin, UserProfileModelAdmin
+from molo.profiles.models import UserProfilesSettings, UserProfile
+from wagtail.contrib.modeladmin.options import modeladmin_register
+from wagtail.wagtailadmin.site_summary import SummaryItem
+from wagtail.wagtailcore import hooks
+
+
+class ProfileWarningMessagee(SummaryItem):
+    order = 100
+    template = 'admin/profile_warning_message.html'
+
+
+@hooks.register('construct_homepage_panels')
+def profile_warning_message(request, panels):
+    profile_settings = UserProfilesSettings.for_site(request.site)
+    if not profile_settings.country_code and \
+            profile_settings.show_mobile_number_field:
+        panels[:] = [ProfileWarningMessagee(request)]
+
+
+modeladmin_register(FrontendUsersModelAdmin)
+modeladmin_register(UserProfileModelAdmin)
+
+
+class AccessErrorMessage(SummaryItem):
+    order = 100
+    template = 'wagtail/access_error_message.html'
+
+
+@hooks.register('construct_homepage_panels')
+def add_access_error_message_panel(request, panels):
+    if UserProfile.objects.filter(user=request.user).exists() and \
+            not request.user.is_superuser:
+        if not request.user.profile.admin_sites.filter(
+                pk=request.site.pk).exists():
+            panels[:] = [AccessErrorMessage(request)]
