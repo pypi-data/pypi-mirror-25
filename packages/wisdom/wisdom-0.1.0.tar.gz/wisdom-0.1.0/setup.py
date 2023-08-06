098c938987f42c5daad00e6cde79f67a1b842b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:13 +0000

    Add package wisdom/0.1.0/wisdom-0.1.0.tar.gz

---
+++ packages/wisdom/wisdom-0.1.0.tar.gz/wisdom-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name='wisdom',
+  packages=['wisdom'],
+  version='0.1.0',
+  description='wisdom',
+  author='Jordan Suchow',
+  author_email='jwsuchow@gmail.com',
+  url='https://suchow.com/',
+)
