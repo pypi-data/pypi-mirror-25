7d756734392a8aa5139adbf63fcf0cda8bb93a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:15 +0000

    Add package hotfeng-test/1.1.0/hotfeng_test-1.1.0.tar.gz

---
+++ packages/hotfeng-test/hotfeng_test-1.1.0.tar.gz/hotfeng_test-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name		= 'hotfeng_test',
+	version		= '1.1.0',
+	py_modules	= ['hotfeng_test'],
+	author		= 'hotfeng',
+	author_email= 'gfaimxw@163.com',
+	url			= 'http://www.hotfeng.club',
+	packages	= find_packages(),
+	description = 'A simple printer of nested lists',
+	)
