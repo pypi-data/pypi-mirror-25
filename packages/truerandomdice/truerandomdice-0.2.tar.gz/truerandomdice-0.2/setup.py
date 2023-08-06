8a0839f04bbcf6481938441d714cf7e0dfc0d181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:25 +0000

    Add package truerandomdice/0.2/truerandomdice-0.2.tar.gz

---
+++ packages/truerandomdice/truerandomdice-0.2.tar.gz/truerandomdice-0.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='truerandomdice',
+    version='0.2',
+    packages=['truerandomdice'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    url='https://github.com/EarthModule/TrulyRandomDiceThrower',
+    download_url='https://github.com/EarthModule/TrulyRandomDiceThrower/releases/tag/0.2',
+    license='',
+    author='toni nurmi',
+    author_email='toni.nurmi@hotmail.com',
+    description=''
+)
