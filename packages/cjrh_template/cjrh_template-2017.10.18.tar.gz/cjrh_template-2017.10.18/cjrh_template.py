63cbdef4598821ca27239f238c92bb9e2cec7fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:22 +0000

    Add package cjrh_template/2017.10.18/cjrh_template-2017.10.18.tar.gz

---
+++ packages/cjrh_template/cjrh_template-2017.10.18.tar.gz/cjrh_template-2017.10.18/cjrh_template.py
@@ -0,0 +1,38 @@
+""" Subclass for string.Template: adds placeholders() method. """
+import string
+
+
+__version__ = '2017.10.18'
+
+
+class Template(string.Template):
+    """
+    Very thin subclass of ``string.Template``.
+    """
+    def placeholders(self, allow_repeats=False):
+        """
+        Return the names of the template variables in the template. Note that
+        this returns a generator.
+
+        The ``allow_repeats`` argument will cause the iteration to skip any
+        repeated variable names.
+
+        :type allow_repeats: bool
+        :return: A generator of all the named vars
+        :rtype: generator
+        """
+        seen = set()
+        for match in self.pattern.finditer(self.template):
+            named = match.group('named') or match.group('braced')
+            if not allow_repeats:
+                if named in seen:
+                    continue
+                else:
+                    # Structured like this so that we don't incur the memory
+                    # cost of storing previously-seen items when repeats are
+                    # allowed.
+                    seen.add(named)
+            if named is None:
+                # Seems to be necessary for things like "$(blah)".
+                continue
+            yield named
