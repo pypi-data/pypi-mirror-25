7be48618c44d06eaffab118ae54bc4f10ce483d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:55 +0000

    Add package gendobot/4.1.2/gendobot-4.1.2.tar.gz

---
+++ packages/gendobot/gendobot-4.1.2.tar.gz/gendobot-4.1.2/gendo/exceptions.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+gendo.exceptions
+----------------
+
+All exceptions used in the Gendo code base are defined here.
+"""
---
+++ packages/gendobot/gendobot-4.1.2.tar.gz/gendobot-4.1.2/gendo/utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+gendo.utils
+------------------
+
+Helper functions used throughout Gendo.
+"""
