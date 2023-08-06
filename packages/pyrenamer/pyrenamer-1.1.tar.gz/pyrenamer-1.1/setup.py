0a7dd95c7f2073d24f54d39baca4de6e307afb2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:41 +0000

    Add package pyrenamer/1.1/pyrenamer-1.1.tar.gz

---
+++ packages/pyrenamer/pyrenamer-1.1.tar.gz/pyrenamer-1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+ name='pyrenamer',
+ version='1.1',
+ py_modules=['pyrenamer'],
+ author='Lucas Barzan',
+ author_email='codenific.com@gmail.com',
+ url='https://github.com/lucasbarzan/py-renamer',
+ description='Easily Rename Your Files in Order (1, 2, 3, 4, 5...)',
+ download_url='https://github.com/lucasbarzan/py-renamer/archive/1.1.tar.gz',
+ keywords=['renamer', 'rename', 'files', 'directories'],
+ classifiers=[],
+ )
