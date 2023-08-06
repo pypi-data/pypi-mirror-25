203c073c5aec7501ea7128275fb6c162f01fa8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:03 +0000

    Add package mimicbot/0.0.12/mimicbot-0.0.12.tar.gz

---
+++ packages/mimicbot/mimicbot-0.0.12.tar.gz/mimicbot-0.0.12/src/mimicbot/__init__.py
@@ -0,0 +1,42 @@
+import os
+
+from mimicbot import filter, generate, process
+
+__version__ = "0.0.12"
+
+class Bot:
+
+    dir = None
+
+    generator = None
+
+    filter = None
+
+    processor = None
+
+    def __init__(self, name):
+        home = os.path.expanduser("~")
+        self.dir = os.path.join(home, ".mimicbot", name)
+        if not os.path.isdir(self.dir):
+            raise Exception("bot dir does not exist")
+        self.generator = generate.Generator(self.dir)
+        self.filter = filter.Filter()
+        self.processor = process.Processor()
+
+    def _handle(self, text):
+        self.filter.run(text)
+        # print("filter passed")
+        return self.processor.run(text)
+
+    def get_text(self, use_context, manual_context):
+        text = None
+        for i in range(100000):
+            text = self.generator.run(use_context, manual_context)
+            # print("\ngenerated: %s" % text)
+            try:
+                return self._handle(text)
+            except Exception as error:
+                print("error: %s" % error)
+                continue
+        raise Exception("too many failed attempts to filter text")
+
