ea7d5cb7622c98f3994d40040d4d82da5e162183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:15 +0000

    Add package elem/0.0.14/elem-0.0.14.tar.gz

---
+++ packages/elem/elem-0.0.14.tar.gz/elem-0.0.14/elem/log.py
@@ -0,0 +1,26 @@
+import logging
+
+
+def setup_custom_logger(name):
+    format_string = '%(asctime)s - %(levelname)s - %(module)s - %(message)s'
+    formatter = logging.Formatter(fmt=format_string)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    return logger
+
+
+def setup_console_logger(name):
+    format_string = '\n%(message)s'
+    formatter = logging.Formatter(fmt=format_string)
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    return logger
