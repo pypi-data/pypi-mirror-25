447ce4f389000ebdec8a508d06dbfdc6a73ad7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:51 +0000

    Add package Pypitest123321/1.0/Pypitest123321-1.0.tar.gz

---
+++ packages/Pypitest123321/Pypitest123321-1.0.tar.gz/Pypitest123321-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup   
+setup(name='Pypitest123321',  
+      version='1.0',  
+     description='description',     
+      author=' ',     
+      author_email='lilisiacup@gmail.com ',     
+      url=' ',     
+      py_modules=['test'], 
+)   
