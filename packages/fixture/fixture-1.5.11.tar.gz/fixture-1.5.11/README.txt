93ee10cb4b938cadcaa4f5fdc441d5e149ce6520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:19 +0000

    Add package fixture/1.5.11/fixture-1.5.11.tar.gz

---
+++ packages/fixture/fixture-1.5.11.tar.gz/fixture-1.5.11/README.txt
@@ -0,0 +1,7 @@
+For docs see docs/index.rst or http://farmdev.com/projects/fixture/
+
+To build docs run  ::
+
+    $ make -C docs html
+
+To run tests, just use `nosetests`.
---
+++ packages/fixture/fixture-1.5.11.tar.gz/fixture-1.5.11/setup.py
@@ -0,0 +1,59 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+_CURRENT_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
+_VERSION = \
+    open(os.path.join(_CURRENT_DIR_PATH, 'VERSION.txt')).readline().rstrip()
+
+_LONG_DESCRIPTION = """
+It provides several utilities for achieving a *fixed state* when testing
+Python programs.  Specifically, these utilities setup / teardown databases and
+work with temporary file systems.
+
+You may want to start by reading the `End User Documentation`_.
+
+.. _End User Documentation: http://farmdev.com/projects/fixture/docs/
+"""
+
+
+setup(
+    name='fixture',
+    version=_VERSION,
+    author='Kumar McMillan',
+    author_email='kumar.mcmillan@gmail.com',
+    description='fixture is a package for loading and referencing test data',
+    classifiers=['Environment :: Other Environment',
+                 'Intended Audience :: Developers',
+                 ('License :: OSI Approved :: GNU Library or Lesser '
+                  'General Public License (LGPL)'),
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Testing',
+                 'Topic :: Software Development :: Quality Assurance',
+                 'Topic :: Utilities'],
+    long_description=_LONG_DESCRIPTION,
+    license='GNU Lesser General Public License (LGPL)',
+    keywords=('test testing tools unittest fixtures setup teardown '
+              'database stubs IO tempfile'),
+    url='http://farmdev.com/projects/fixture/',
+
+    packages=find_packages(),
+
+    test_suite="fixture.setup_test_not_supported",
+    entry_points={
+        'console_scripts': ['fixture = fixture.command.generate:main']
+    },
+    install_requires=[
+        'six >= 1.10.0',
+    ],
+    # the following allows e.g. easy_install fixture[django]
+    extras_require={
+        'decorators': ['nose>=0.9.2'],
+        'sqlalchemy': ['SQLAlchemy>=0.4'],
+        'sqlobject': ['SQLObject==0.8'],
+        'django': ['django>=1.8'],
+    },
+)
