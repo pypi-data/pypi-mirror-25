15397df54f926ab80a5d98412f71101fd9476ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:59 +0000

    Add package SkitterCrawler/0.0.3/SkitterCrawler-0.0.3.tar.gz

---
+++ packages/SkitterCrawler/SkitterCrawler-0.0.3.tar.gz/SkitterCrawler-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='SkitterCrawler',
+      version='0.0.3',
+      description='Generic packages with Crawlers for OSNs',
+      url='https://github.com/manoelhortaribeiro/SkitterCrawler',
+      download_url='https://github.com/manoelhortaribeiro/SkitterCrawler',
+      author='Manoel Horta Ribeiro',
+      author_email='manoelhortaribeiro@gmail.com',
+      license='MIT',
+      packages=['SkitterCrawler'],
+      keywords='Online social network crawler utilities',
+      install_requires=['sqlalchemy', 'SkitterSchema', 'tweepy', 'numpy'],
+      entry_points={
+          'console_scripts': ['rw_crawler=SkitterCrawler.random_walker_crawler:_random_walk_crawler']
+      },
+      include_package_data=True,
+      zip_safe=False)
