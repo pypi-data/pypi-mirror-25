9a2ac1888c67c3055de17924c54f6878321105d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:42 +0000

    Add package rekall-efilter/1.5.2/rekall-efilter-1.5.2.tar.gz

---
+++ packages/rekall-efilter/rekall-efilter-1.5.2.tar.gz/rekall-efilter-1.5.2/efilter/ext/lazy_repetition.py
@@ -0,0 +1,151 @@
+# EFILTER Forensic Query Language
+#
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Implements IRepeated using a restartable generator.
+"""
+
+__author__ = "Adam Sindelar <adamsh@google.com>"
+
+from efilter.protocols import counted
+from efilter.protocols import ordered
+from efilter.protocols import repeated
+
+
+class LazyRepetition(object):
+    """Repeated variable backed by a restartable generator.
+
+    Arguments:
+        generator_func: A stable function that returns a generator. Stable
+            means that the generator must be the same every time the function
+            is called (for the express purpose of reseting iteration).
+    """
+
+    _generator_func = None
+    _value_type = None  # Just a cache for value_type.
+    _watermark = 0  # Highest idx reached so far.
+
+    # The count of values. After first complete iteration this will be one
+    # higher than watermark.
+    _count = None
+
+    def __init__(self, generator_func):
+        if not callable(generator_func):
+            raise TypeError("Generator function must be callable.")
+
+        self._generator_func = generator_func
+
+    def __eq__(self, other):
+        if not isinstance(other, repeated.IRepeated):
+            return False
+
+        return self.value_eq(other)
+
+    def __iter__(self):
+        return self._generator_func()
+
+    def __repr__(self):
+        return "LazyRepetition(generator_func=%r, value_type=%r)" % (
+            self._generator_func, self.value_type())
+
+    # IRepeated protocol implementation (see IRepeated for behavior docs).
+
+    def getvalues(self):
+        """Yields all the values from 'generator_func' and type-checks.
+
+        Yields:
+            Whatever 'generator_func' yields.
+
+        Raises:
+            TypeError: if subsequent values are of a different type than first
+                value.
+
+            ValueError: if subsequent iteration returns a different number of
+                values than the first iteration over the generator. (This would
+                mean 'generator_func' is not stable.)
+        """
+        idx = 0
+        generator = self._generator_func()
+        first_value = next(generator)
+        self._value_type = type(first_value)
+        yield first_value
+
+        for idx, value in enumerate(generator):
+            if not isinstance(value, self._value_type):
+                raise TypeError(
+                    "All values of a repeated var must be of the same type."
+                    " First argument was of type %r, but argument %r is of"
+                    " type %r." %
+                    (self._value_type, value, repeated.value_type(value)))
+
+            self._watermark = max(self._watermark, idx + 1)
+            yield value
+
+        # Iteration stopped - check if we're at the previous watermark and raise
+        # if not.
+        if idx + 1 < self._watermark:
+            # for rekall, this check is too strict, since in live-mode
+            # repeated analysis can yield different results
+            pass
+
+
+        # Watermark is higher than previous count! Generator function returned
+        # more values this time than last time.
+        if self._count is not None and self._watermark >= self._count:
+            # for rekall, this check is too strict, since in live-mode
+            # repeated analysis can yield different results
+            pass
+
+
+        # We've finished iteration - cache count. After this the count will be
+        # watermark + 1 forever.
+        self._count = self._watermark + 1
+
+    def value_type(self):
+        if self._value_type is None:
+            for value in self.getvalues():
+                self._value_type = type(value)
+                break
+
+        return self._value_type
+
+    def value_eq(self, other):
+        """Sorted comparison of values."""
+        self_sorted = ordered.ordered(self.getvalues())
+        other_sorted = ordered.ordered(repeated.getvalues(other))
+        return self_sorted == other_sorted
+
+    def value_apply(self, f):
+        def _generator():
+            for value in self.getvalues():
+                yield f(value)
+
+        return LazyRepetition(_generator)
+
+    # ICounted implementation:
+
+    def count(self):
+        if not self._count:
+            # Do a complete pass over the generator to cause _count to be set.
+            for _ in self.getvalues():
+                pass
+
+        return self._count
+
+
+repeated.IRepeated.implicit_static(LazyRepetition)
+repeated.lazy.implement(for_type=object, implementation=LazyRepetition)
+counted.ICounted.implicit_static(LazyRepetition)
---
+++ packages/rekall-efilter/rekall-efilter-1.5.2.tar.gz/rekall-efilter-1.5.2/setup.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+import sys
+
+try:
+    from setuptools import find_packages, setup
+except ImportError:
+    from distutils.core import find_packages, setup
+
+try:
+    from setuptools.commands.bdist_rpm import bdist_rpm
+except ImportError:
+    from distutils.command.bdist_rpm import bdist_rpm
+
+try:
+    from setuptools.command.sdist import sdist
+except ImportError:
+    from distutils.command.sdist import sdist
+
+# Change PYTHONPATH to include efilter so that we can get the version.
+sys.path.insert(0, ".")
+
+try:
+    from efilter.version import get_txt_version, get_version
+except ImportError:
+    # If we can't import EFILTER then we can't generate version from git, but
+    # can still just read the version file.
+    def get_version(_=None):
+        return get_txt_version()
+
+    def get_txt_version():
+        try:
+            with open("version.txt", "r") as fp:
+                return fp.read().strip()
+        except IOError:
+            return None
+
+
+__version__ = get_txt_version()
+
+
+class BdistRPMCommand(bdist_rpm):
+    """Custom handler for the bdist_rpm command."""
+
+    def _make_spec_file(self):
+        """Generates the text of an RPM spec file.
+
+        Returns:
+          A list of strings containing the lines of text.
+        """
+        # Note that bdist_rpm can be an old style class.
+        if issubclass(BdistRPMCommand, object):
+            spec_file = super(BdistRPMCommand, self)._make_spec_file()
+        else:
+            spec_file = bdist_rpm._make_spec_file(self)
+
+        if sys.version_info[0] < 3:
+            python_package = "python"
+        else:
+            python_package = "python3"
+
+        description = []
+        summary = ""
+        in_description = False
+
+        python_spec_file = []
+        for line in spec_file:
+            if line.startswith("Summary: "):
+                summary = line
+
+            elif line.startswith("BuildRequires: "):
+                line = "BuildRequires: {0:s}-setuptools".format(python_package)
+
+            elif line.startswith("Requires: "):
+                if python_package == "python3":
+                    line = line.replace("python", "python3")
+
+            elif line.startswith("%description"):
+                in_description = True
+
+            elif line.startswith("%files"):
+                line = "%files -f INSTALLED_FILES -n {0:s}-%{{name}}".format(
+                    python_package)
+
+            elif line.startswith("%prep"):
+                in_description = False
+
+                python_spec_file.append(
+                    "%package -n {0:s}-%{{name}}".format(python_package))
+                python_spec_file.append("{0:s}".format(summary))
+                python_spec_file.append("")
+                python_spec_file.append(
+                    "%description -n {0:s}-%{{name}}".format(python_package))
+                python_spec_file.extend(description)
+
+            elif in_description:
+                # Ignore leading white lines in the description.
+                if not description and not line:
+                    continue
+
+                description.append(line)
+
+            python_spec_file.append(line)
+
+        return python_spec_file
+
+
+class SDistCommand(sdist):
+    """Custom handler for the sdist command."""
+
+    def run(self):
+        global __version__
+        __version__ = get_version(False)
+        with open("version.txt", "w") as fd:
+            fd.write(__version__)
+
+        # Need to use old style super class invocation here for
+        # backwards compatibility.
+        sdist.run(self)
+
+
+setup(name="rekall-efilter",
+      version=__version__,
+      description="EFILTER query language",
+      long_description=(
+          "EFILTER is a general-purpose destructuring and search language "
+          "implemented in Python, and suitable for integration with any "
+          "Python project that requires a search function for some of its "
+          "data."),
+      license="Apache 2.0",
+      author="Adam Sindelar",
+      author_email="adam.sindelar@gmail.com",
+      url="https://github.com/google/dotty/",
+      packages=find_packages(exclude=["efilter_tests*"]),
+      package_dir={"efilter": "efilter"},
+      cmdclass={
+          "bdist_rpm": BdistRPMCommand,
+          "sdist": SDistCommand},
+      install_requires=[
+          "python-dateutil > 2",
+          "pytz >= 2011k",
+          "six >= 1.4.0"])
