93bead1da74e42f9cbe0eb1bfe3c3ac3e35a3fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:02 +0000

    Add package licant/0.1/licant-0.1.tar.gz

---
+++ packages/licant/licant-0.1.tar.gz/licant-0.1/licant/libs.py
@@ -0,0 +1,30 @@
+from licant.scripter import scriptq
+
+import os
+import sys
+import json
+
+gpath = "/etc/licant"
+lpath = os.path.expanduser("~/.config/licant")
+
+libs = None
+
+def include(lib):
+	global libs
+	if libs == None:
+		glibs = {}
+		llibs = {}
+
+		if os.path.exists(gpath):
+			glibs = json.load(open(gpath))
+
+		if os.path.exists(lpath):
+			llibs = json.load(open(lpath))
+
+		libs = {**glibs, **llibs}
+
+	if not lib in libs:
+		print("Unregistred library")
+		exit(-1)
+
+	scriptq.execute(libs[lib])	
---
+++ packages/licant/licant-0.1.tar.gz/licant-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'licant',
+  packages = ['licant'],
+  version = '0.1',
+  license='MIT',
+  description = 'licant make system',
+  author = 'Sorokin Nikolay',
+  author_email = 'mirmikns@yandex.ru',
+  url = 'https://github.com/mirmik/licant',
+  keywords = ['testing', 'make'],
+  classifiers = [],
+)
