6fb9f83f3c840b2e9726fbf815ced80f7fbb9339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:50 +0000

    Add package ipcmb-thinclient-primitive/1.0.0/ipcmb_thinclient_primitive-1.0.0.tar.gz

---
+++ packages/ipcmb-thinclient-primitive/ipcmb_thinclient_primitive-1.0.0.tar.gz/ipcmb_thinclient_primitive-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+	name='ipcmb_thinclient_primitive',  # This is the name of your PyPI-package. 
+	version= '1.0.0',  # Update the version number for new releases
+	author= 'Keyi Liu',
+	packages=['ipcmb_thinclient_primitive']
+)
