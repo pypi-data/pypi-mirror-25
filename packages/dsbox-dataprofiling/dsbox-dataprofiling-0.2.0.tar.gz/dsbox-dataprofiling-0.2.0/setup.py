1868c365fd7b7432c48a82ddef977b6fcbad49ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:33 +0000

    Add package dsbox-dataprofiling/0.2.0/dsbox-dataprofiling-0.2.0.tar.gz

---
+++ packages/dsbox-dataprofiling/dsbox-dataprofiling-0.2.0.tar.gz/dsbox-dataprofiling-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='dsbox-dataprofiling',
+      version='0.2.0',
+      url='https://github.com/usc-isi-i2/dsbox-cleaning.git',
+      maintainer_email='kyao@isi.edu',
+      maintainer='Ke-Thia Yao',
+      description='DSBox data preprocssing tools',
+      license='MIT',
+      packages=['dsbox', 'dsbox.datapreprocessing', 'dsbox.datapreprocessing.profiler'],
+      zip_safe=False,
+      python_requires='>=3.5',
+      install_requires=[
+          'numpy>=1.11.1', 'pandas>=0.20.1', 'langdetect>=1.0.7'
+      ]
+)
