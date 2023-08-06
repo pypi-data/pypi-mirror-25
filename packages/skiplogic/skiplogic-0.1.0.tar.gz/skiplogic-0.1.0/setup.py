5e93b3ff153353cc1fe05f680ad8b9d43235860d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:43 +0000

    Add package skiplogic/0.1.0/skiplogic-0.1.0.tar.gz

---
+++ packages/skiplogic/skiplogic-0.1.0.tar.gz/skiplogic-0.1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='skiplogic',
+      version='0.1.0',
+      description='Skip Logic API wrapper',
+      author='Scott Cruwys',
+      author_email='scott@skiplogic.io',
+      url='https://www.skiplogic.io',
+      packages=['skiplogic'],
+     )
