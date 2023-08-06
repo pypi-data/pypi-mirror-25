14f119685ebf6e49bf087b851b56209a727e8550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:42 +0000

    Add package test-stmb-primitive/1.0/test_stmb_primitive-1.0.tar.gz

---
+++ packages/test-stmb-primitive/test_stmb_primitive-1.0.tar.gz/test_stmb_primitive-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+	name='test_stmb_primitive',  # This is the name of your PyPI-package. 
+	version= '1.0',  # Update the version number for new releases
+	author= 'Keyi Liu',
+	packages=['test_stmb_primitive']
+)
