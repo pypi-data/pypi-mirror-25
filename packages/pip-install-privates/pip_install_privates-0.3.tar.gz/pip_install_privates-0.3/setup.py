a58a784daf23d40ad514e3746e4cf16dd95fc860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:35 +0000

    Add package pip-install-privates/0.3/pip_install_privates-0.3.tar.gz

---
+++ packages/pip-install-privates/pip_install_privates-0.3.tar.gz/pip_install_privates-0.3/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='pip_install_privates',
+    version='0.3',
+    description='Install pip packages from private repositories without an ssh agent',
+    author='Byte Internet',
+    author_email='rickvandeloo@gmail.com',
+    license='MIT',
+    url='https://github.com/ByteInternet/pip_install_privates',
+    packages=['pip_install_privates'],
+    install_requires=['pip'],
+    entry_points={
+        'console_scripts': [
+            'pip_install_privates = pip_install_privates.install:install'
+        ]
+    }
+)
