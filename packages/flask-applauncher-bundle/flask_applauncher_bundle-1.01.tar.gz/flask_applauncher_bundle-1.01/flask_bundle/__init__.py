e1c065af128b2a5eef9ea020725147fbe7524d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:05 +0000

    Add package flask-applauncher-bundle/1.01/flask_applauncher_bundle-1.01.tar.gz

---
+++ packages/flask-applauncher-bundle/flask_applauncher_bundle-1.01.tar.gz/flask_applauncher_bundle-1.01/flask_bundle/__init__.py
@@ -0,0 +1,55 @@
+from flask.json import JSONEncoder
+import zope.event
+import inject
+from flask import Flask
+import datetime
+import threading
+import applauncher.kernel
+
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+        return JSONEncoder.default(self, obj)
+
+
+# Just for inject an array as ApiBlueprints
+class ApiBlueprints(object):
+    pass
+
+
+class FlaskBundle(object):
+    def __init__(self):
+        self.config_mapping = {
+            "flask": {
+                "use_debugger": False,
+                "port": 3003,
+                "host": "0.0.0.0"
+            }
+        }
+
+        self.blueprints = []
+        self.injection_bindings = {
+            ApiBlueprints: self.blueprints
+        }
+
+        zope.event.subscribers.append(self.kernel_ready)
+
+    @inject.params(config=applauncher.kernel.Configuration)
+    def start_sever(self, config):
+        app = Flask("FlaskServer")
+        app.json_encoder = CustomJSONEncoder
+
+        for blueprint in self.blueprints:
+            app.register_blueprint(blueprint)
+
+        c = config.flask
+        app.run(use_debugger=c.use_debugger, port=c.port, host=c.host)
+
+    def kernel_ready(self, event):
+        if isinstance(event, applauncher.kernel.KernelReadyEvent):
+            t = threading.Thread(target=self.start_sever)
+            t.start()
+
+
