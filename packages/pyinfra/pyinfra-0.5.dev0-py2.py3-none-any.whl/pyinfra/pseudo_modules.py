5eb3b462ea2ac41b664d89bb9efb80cde6643847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:32 +0000

    Add package pyinfra/0.5.dev0/pyinfra-0.5.dev0-py2.py3-none-any.whl

---
+++ packages/pyinfra/pyinfra-0.5.dev0-py2.py3-none-any.whl/pyinfra/pseudo_modules.py
@@ -0,0 +1,56 @@
+# pyinfra
+# File: pyinfra/pseudo_modules.py
+# Desc: essentially a hack that provides dynamic imports for the current deploy (CLI only)
+
+import sys
+import pyinfra
+
+
+class PseudoModule(object):
+    _module = None
+
+    def __getattr__(self, key):
+        return getattr(self._module, key)
+
+    def __setattr__(self, key, value):
+        if key == '_module':
+            return object.__setattr__(self, key, value)
+
+        setattr(self._module, key, value)
+
+    def __getitem__(self, key):
+        return self._module[key]
+
+    def __iter__(self):
+        return iter(self._module)
+
+    def __len__(self):
+        return len(self._module)
+
+    def __eq__(self):
+        pass
+
+    def set(self, module):
+        self._module = module
+
+    def reset(self):
+        self._module = None
+
+    def isset(self):
+        return self._module is not None
+
+
+# The current deploy state
+sys.modules['pyinfra.pseudo_state'] = sys.modules['pyinfra.state'] = \
+    pyinfra.pseudo_state = pyinfra.state = \
+    PseudoModule()
+
+# The current deploy inventory
+sys.modules['pyinfra.pseudo_inventory'] = sys.modules['pyinfra.inventory'] = \
+    pyinfra.pseudo_inventory = pyinfra.inventory = \
+    PseudoModule()
+
+# The current target host
+sys.modules['pyinfra.pseudo_host'] = sys.modules['pyinfra.host'] = \
+    pyinfra.pseudo_host = pyinfra.host = \
+    PseudoModule()
