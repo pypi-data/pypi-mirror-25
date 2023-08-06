eb2b812fabf39cce6725f89757fa6ba86bc1bfc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:02 +0000

    Add package mistool/1.2.1b0/mistool-1.2.1b0.tar.gz

---
+++ packages/mistool/mistool-1.2.1b0.tar.gz/mistool-1.2.1b0/mistool/config/frame.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Note: the following ugly dictionary was automatically built.
+
+ALL_FRAMES = {}
+ALL_FRAMES['ascii_star'] = {'down': '*', 'upright': '**', 'right': ' *', 'downleft': '**', 'left': '* ', 'downright': '**', 'up': '*', 'upleft': '**'}
+ALL_FRAMES['c_basic'] = {'down': '*', 'upright': '**', 'right': ' *', 'downleft': ' **', 'left': ' * ', 'downright': '**/', 'up': '*', 'upleft': '/**'}
+ALL_FRAMES['c_pretty'] = {'down': '-', 'upright': '--- *', 'right': ' -- *', 'downleft': ' * ---', 'left': ' * -- ', 'downright': '--- */', 'up': '-', 'upleft': '/* ---'}
+ALL_FRAMES['pyba_title_1'] = {'down': '=', 'upright': '', 'right': '', 'downleft': '', 'left': '', 'downright': '', 'up': '=', 'upleft': ''}
+ALL_FRAMES['pyba_title_2'] = {'down': '-', 'upright': '', 'right': '', 'downleft': '', 'left': '', 'downright': '', 'up': '-', 'upleft': ''}
+ALL_FRAMES['pyba_title_3'] = {'down': '*', 'upright': '', 'right': '', 'downleft': '', 'left': '', 'downright': '', 'up': '*', 'upleft': ''}
+ALL_FRAMES['pyba_title_4'] = {'down': ':', 'upright': '', 'right': '', 'downleft': '', 'left': '', 'downright': '', 'up': ':', 'upleft': ''}
+ALL_FRAMES['python_basic'] = {'down': '#', 'upright': '##', 'right': ' #', 'downleft': '##', 'left': '# ', 'downright': '##', 'up': '#', 'upleft': '##'}
+ALL_FRAMES['python_pretty'] = {'down': '-', 'upright': '--- #', 'right': ' -- #', 'downleft': '# ---', 'left': '# -- ', 'downright': '--- #', 'up': '-', 'upleft': '# ---'}
---
+++ packages/mistool/mistool-1.2.1b0.tar.gz/mistool-1.2.1b0/setup.py
@@ -0,0 +1,70 @@
+# ------------------------------------------------ #
+# -- LINE COMMANDS FOR TWINE (which uses HTTPS) -- #
+# ------------------------------------------------ #
+
+# Source: https://pypi.python.org/pypi/twine
+
+#     1) Create some distributions in the normal way:
+#         > python setup.py sdist bdist_wheel |
+#
+#     2) Upload with twine:
+#         > twine upload dist/* --skip-existing
+
+
+# -------------------- #
+# -- STANDARD TOOLS -- #
+# -------------------- #
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+import pypandoc
+
+# ----------------- #
+# -- README FILE -- #
+# ----------------- #
+
+readme = Path(__file__).parent / 'README.md'
+
+longdesc = pypandoc.convert(str(readme), 'rst')
+
+
+# ----------------- #
+# -- OUR SETTNGS -- #
+# ----------------- #
+
+setup(
+# General
+    name         = "mistool",
+    version      = "1.2.1-beta",
+    url          = 'https://github.com/bc-python-tools/mistool',
+    license      = 'GPLv3',
+    author       = "Christophe BAL",
+    author_email = "projetmbc@gmail.com",
+
+# Descritions
+    long_description = longdesc,
+    description      = "Miscellaneous missing tools that can help the py-developper.",
+
+# What to add ?
+    packages = find_packages(),
+
+# Uggly classifiers
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Desktop Environment :: File Managers',
+        'Topic :: System :: Logging',
+        'Topic :: Text Processing :: Markup :: LaTeX'
+    ],
+
+# What does your project relate to?
+    keywords = 'python latex os path string terminal tool',
+)
