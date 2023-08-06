39f1d2cd24bb639fbbeed085d79938a9e2dec6ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:35 +0000

    Add package ols-client/0.0.6/ols_client-0.0.6.tar.gz

---
+++ packages/ols-client/ols_client-0.0.6.tar.gz/ols_client-0.0.6/src/ols_client/cli.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+import click
+
+from .api import get_labels
+from .constants import BASE_URL, write_config, get_config
+
+
+@click.group()
+def main():
+    """OLS Client Command Line Interface"""
+
+
+@main.command()
+@click.argument('ontology')
+@click.option('-o', '--output', type=click.File('r'))
+@click.option('-b', '--ols-base', help="OLS base url. Defaults to {}".format(BASE_URL))
+def labels(ontology, output, ols_base):
+    """Output the names to the given file"""
+    for label in get_labels(ontology=ontology, ols_base=ols_base):
+        click.echo(label, file=output)
+
+
+@main.command()
+@click.argument('base')
+def set_base(base):
+    """Sets the default OLS base url"""
+    config = get_config()
+    config['BASE'] = base
+    write_config(config)
+
+
+if __name__ == '__main__':
+    main()
