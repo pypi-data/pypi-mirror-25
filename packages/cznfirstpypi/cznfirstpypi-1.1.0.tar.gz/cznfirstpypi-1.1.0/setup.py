0435f753a3f2f3fc9d758076b7b45fff71bc0d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:15 +0000

    Add package cznfirstpypi/1.1.0/cznfirstpypi-1.1.0.tar.gz

---
+++ packages/cznfirstpypi/cznfirstpypi-1.1.0.tar.gz/cznfirstpypi-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name         = 'cznfirstpypi',
+    version       = '1.1.0',
+    py_modules   = ['nester'],
+    author       = 'Betterczn',
+    author_email = '997926504@qq.com',
+    url          = 'http://www.headfirstlabs.com',
+    description  = 'A simple printer of nester lists',
+    )
