e79d0f8fee6bfbc27e0e9f75a19c732e1c91e384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:17 +0000

    Add package pyranda/0.0.18/pyranda-0.0.18.tar.gz

---
+++ packages/pyranda/pyranda-0.0.18.tar.gz/pyranda-0.0.18/pyranda/__init__.py
@@ -0,0 +1,11 @@
+from pyranda.okres import Okres
+from pyranda.okress import Okress
+from pyranda.kraj import Kraj
+from pyranda.krajs import Krajs, getRandomKraj, list_kraj
+from pyranda.jmeno import Jmeno
+from pyranda.jmenos import Jmenos
+from pyranda.osoba import Osoba
+from pyranda.prijmeni import Prijmeni
+from pyranda.prijmenis import Prijmenis
+from pyranda.zeme  import Zeme
+from pyranda.mesto import Mesto
