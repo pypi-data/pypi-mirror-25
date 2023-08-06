2054fd8e9beaf51221245e7763e74ba70baab0ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:06 +0000

    Add package kcem/2.2/kcem-2.2.tar.gz

---
+++ packages/kcem/kcem-2.2.tar.gz/kcem-2.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name = 'kcem',
+    packages = ['kcem'],
+    package_data={'kcem':['utils/*']},
+    version = '2.2',
+    description = 'kcem class file',
+    author = 'davidtnfsh',
+    author_email = 'davidtnfsh@gmail.com',
+    url = 'https://github.com/udicatnchu/kcem',
+    download_url = 'https://github.com/udicatnchu/kcem/archive/v2.2.tar.gz',
+    keywords = ['kcem'],
+    classifiers = [],
+    license='GPL3.0',
+    install_requires=[
+        'pymongo',
+        'simplejson',
+        'requests',
+        'keras',
+        'click',
+        'jieba',
+        'tensorflow',
+        'h5py'
+    ],
+    zip_safe=True
+)
