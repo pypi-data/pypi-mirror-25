fe1fee31289e9b40025654595e5399dc0b8e82fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:01 +0000

    Add package restfulpy/0.32.1/restfulpy-0.32.1.tar.gz

---
+++ packages/restfulpy/restfulpy-0.32.1.tar.gz/restfulpy-0.32.1/restfulpy/__init__.py
@@ -0,0 +1,15 @@
+
+
+from restfulpy.application import Application
+
+
+import warnings
+warnings.filterwarnings('ignore', message='Unknown REQUEST_METHOD')
+
+
+__version__ = '0.32.1'
+
+
+__all__ = [
+    'Application'
+]
