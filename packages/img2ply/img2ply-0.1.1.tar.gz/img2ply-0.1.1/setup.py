ef9000a435a7c9fbc447b8243947ec44a7445aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:58 +0000

    Add package img2ply/0.1.1/img2ply-0.1.1.tar.gz

---
+++ packages/img2ply/img2ply-0.1.1.tar.gz/img2ply-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1.1"
+REQUIRES = ["Pillow"]
+
+setup(
+    name="img2ply",
+    version=VERSION,
+    author="Robert Joosten",
+    author_email="rwm.joosten@gmail.com",
+    package_dir={"": "src"},
+    py_modules=["img2ply"],
+    install_requires=REQUIRES,
+    url="https://github.com/robertjoosten/img2ply",
+    download_url="https://github.com/robertjoosten/img2ply/archive/{0}.tar.gz".format(VERSION),
+    license="GNU General Public License v3.0",
+    description="Convert an image sequence to a PLY point cloud.",
+    keywords="image ply converter",
+    entry_points={
+        "console_scripts": [
+            "img2ply=img2ply:main",
+        ],
+    },
+)
