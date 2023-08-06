d22f59621ee8711341ab1218cfd81c91ea6aa38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:24 +0000

    Add package fdjangodog/0.5.0/fdjangodog-0.5.0.tar.gz

---
+++ packages/fdjangodog/fdjangodog-0.5.0.tar.gz/fdjangodog-0.5.0/setup.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from setuptools import setup
+
+
+setup(
+    name='fdjangodog',
+    version='0.5.0',
+    packages = [
+        'fdjangodog',
+    ],
+    include_package_data = True,
+    license = 'BSD',
+    description = 'Simple Django middleware for submitting timings and exceptions to Datadog.',
+    url='https://github.com/tom-dalton-fanduel/fdjangodog',
+    author='Tom Dalton',
+    author_email='tom.dalton@fanduel.com',
+    install_requires=[
+        'datadog==0.16.0',
+    ]
+)
