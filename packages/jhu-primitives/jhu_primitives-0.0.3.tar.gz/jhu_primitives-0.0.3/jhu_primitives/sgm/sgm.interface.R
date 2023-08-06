826d17d4b23b9997697e856642dae6f177b7012d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:13 +0000

    Add package jhu-primitives/0.0.3/jhu_primitives-0.0.3.tar.gz

---
+++ packages/jhu-primitives/jhu_primitives-0.0.3.tar.gz/jhu_primitives-0.0.3/jhu_primitives/sgm/sgm.interface.R
@@ -0,0 +1,27 @@
+if(!require(VN)) {
+    install.packages("http://www.cis.jhu.edu/~parky/D3M/VN_0.3.1.tar.gz",type="source")
+    suppressMessages(library(VN))
+}
+if(!require(igraph)) {
+    install.packages("igraph")
+    suppressMessages(library(igraph))
+}
+
+sgm.interface <- function(g1, g2, S)
+{
+    A1 <- as.matrix(g1[]); n <- nrow(A1)
+    A2 <- as.matrix(g2[]); m <- nrow(A2)
+
+    gamma <- 1
+    niter <- 30
+    s <- nrow(S)
+    if(is.null(s)){
+        s <- 0
+        S <- NULL
+    }else{
+        S <- S[,c(2,1)]
+    }
+
+    out <- sgm(A2, A1, hard=FALSE, S, iteration=niter)$P
+    return(out)
+}
