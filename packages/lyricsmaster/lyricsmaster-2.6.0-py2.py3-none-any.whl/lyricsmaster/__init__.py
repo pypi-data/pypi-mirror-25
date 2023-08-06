323b00335b9356ecd137d6b615a74f10c72235a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:46 +0000

    Add package lyricsmaster/2.6.0/lyricsmaster-2.6.0-py2.py3-none-any.whl

---
+++ packages/lyricsmaster/lyricsmaster-2.6.0-py2.py3-none-any.whl/lyricsmaster/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for LyricsMaster."""
+
+__author__ = """SekouD"""
+__email__ = 'sekoud.python@gmail.com'
+__version__ = '2.6.0'
+
+from .lyricsmaster import LyricWiki, AzLyrics
+from .utils import TorController
