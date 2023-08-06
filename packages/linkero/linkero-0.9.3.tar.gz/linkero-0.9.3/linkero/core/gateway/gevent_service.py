cb7d080ed412689caf03180e96622336d6de7a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:03 +0000

    Add package linkero/0.9.3/linkero-0.9.3.tar.gz

---
+++ packages/linkero/linkero-0.9.3.tar.gz/linkero-0.9.3/linkero/core/gateway/gevent_service.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import linkero.core.linkero as linkero
+from gevent.wsgi import WSGIServer
+from gevent.pool import Pool
+
+def run(app):
+    linkero.printWellcome()
+    linkero.createDB()
+    if linkero.config["SSL"]["activate"]:
+        gevert_server = WSGIServer((linkero.config["host"]["ip"], linkero.config["host"]["port"]), app,
+                                   spawn=Pool(linkero.config["gevent"]["spawn"]),
+                                   keyfile=linkero.config["SSL"]["key"], certfile=linkero.config["SSL"]["certificate"])
+    else:
+        gevert_server = WSGIServer((linkero.config["host"]["ip"], linkero.config["host"]["port"]), app,
+                                   spawn=Pool(linkero.config["gevent"]["spawn"]))
+    gevert_server.serve_forever()
