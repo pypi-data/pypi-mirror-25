83138b5251c79f38b8694e59d2da3578ba7b7f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:34 +0000

    Add package numismatic/0.1.3/numismatic-0.1.3.tar.gz

---
+++ packages/numismatic/numismatic-0.1.3.tar.gz/numismatic-0.1.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+from setuptools import setup
+
+README = Path('README.rst').open('rt').read()
+REQUIREMENTS = Path('requirements.txt').open('rt').read().strip().split('\n')
+print(REQUIREMENTS)
+VERSION = open(Path('VERSION')).read().strip()
+print(VERSION)
+
+setup(name='numismatic',
+      version=VERSION,
+      description='Coin Collector for digital assets ',
+      url='http://github.com/snth/numismatic/',
+      maintainer='Tobias Brandt',
+      maintainer_email='Tobias.Brandt@gmail.com',
+      license='MIT',
+      keywords='bitcoin,ethereum,cryptocurrencies',
+      packages=['numismatic'],
+      long_description=README,
+      install_requires=REQUIREMENTS,
+      python_requires='>=3.6',
+      zip_safe=False,
+      entry_points='''
+          [console_scripts]
+          coin=numismatic.cli:coin
+          '''
+      )
