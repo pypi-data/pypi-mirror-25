ee86ce63b23a52658e19718fce4926e6a5450920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:50 +0000

    Add package awswrapper/0.2.1/awswrapper-0.2.1.tar.gz

---
+++ packages/awswrapper/awswrapper-0.2.1.tar.gz/awswrapper-0.2.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+try:
+    with open('README.md') as f:
+        long_description = f.read()
+except IOError:
+    long_description = ""
+
+requirements = ['boto3']
+
+setuptools.setup(
+    name='awswrapper',
+    license='MIT',
+    author='Hector Reyes Aleman',
+    author_email='birkoffh@gmail.com',
+    install_requires=requirements,
+    version='0.2.1',
+    packages=['awswrapper'],
+    description='Wrapper of AWS API to use with Lambda',
+    long_description=long_description,
+    url='https://github.com/birkoff/PythonAwsWrapper'
+)
