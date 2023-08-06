7112bf16031f72083e2a3e4be94b58cf682c8a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:01 +0000

    Add package iomed/0.0.11/iomed-0.0.11.tar.gz

---
+++ packages/iomed/iomed-0.0.11.tar.gz/iomed-0.0.11/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import iomed
+from setuptools import setup
+
+setup(name='iomed',
+      version='0.0.11',
+      description='cli to IOMED Medical Language API',
+      author='IOMED Medical Solutions SL',
+      author_email='dev@iomed.es',
+      url='https://iomed.es/es',
+      packages=['iomed'],
+      entry_points = {
+        'console_scripts': [
+            'iomed = iomed.__main__:main',
+        ],
+      },
+      install_requires=['requests', 'argparse']
+     )
