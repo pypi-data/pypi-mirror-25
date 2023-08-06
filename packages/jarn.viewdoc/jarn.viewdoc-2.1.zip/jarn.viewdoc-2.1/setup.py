e319f58bdb5e94baa9d40f686187f62b33009106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:16 +0000

    Add package jarn.viewdoc/2.1/jarn.viewdoc-2.1.zip

---
+++ packages/jarn.viewdoc/jarn.viewdoc-2.1.zip/jarn.viewdoc-2.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+version = '2.1'
+
+setup(name='jarn.viewdoc',
+      version=version,
+      description='Python documentation viewer',
+      long_description=open('README.rst').read() + '\n' +
+                       open('CHANGES.rst').read(),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+      keywords='view rest rst package docs rst2html long-description',
+      author='Stefan H. Holek',
+      author_email='stefan@epy.co.at',
+      url='https://github.com/Jarn/jarn.viewdoc',
+      license='BSD-2-Clause',
+      packages=find_packages(),
+      namespace_packages=['jarn'],
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='jarn.viewdoc.tests',
+      install_requires=[
+          'setuptools',
+          'docutils >= 0.13.1',
+          'pygments >= 2.2.0',
+      ],
+      entry_points={
+          'console_scripts': 'viewdoc=jarn.viewdoc.viewdoc:main',
+      },
+)
