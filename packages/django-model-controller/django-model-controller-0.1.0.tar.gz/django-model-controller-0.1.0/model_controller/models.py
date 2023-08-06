58d604ff126c759d414daf2b1ecb8040c7a03074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:00 +0000

    Add package django-model-controller/0.1.0/django-model-controller-0.1.0.tar.gz

---
+++ packages/django-model-controller/django-model-controller-0.1.0.tar.gz/django-model-controller-0.1.0/model_controller/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class AbstractTimeStampMarker(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created at"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Updated at"))
+
+    class Meta:
+        abstract = True
+
+
+class AbstractModelController(AbstractTimeStampMarker):
+    created_user = models.ForeignKey(User, related_name="%(class)s_created_user", verbose_name=_("Created User"),
+                                     on_delete=models.DO_NOTHING)
+    updated_user = models.ForeignKey(User, related_name="%(class)s_updated_user", verbose_name=_("Updated User"),
+                                     on_delete=models.DO_NOTHING)
+
+    class Meta:
+        abstract = True
