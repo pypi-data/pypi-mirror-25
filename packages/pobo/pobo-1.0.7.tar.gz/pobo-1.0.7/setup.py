0e922e60b61a46543e0456092c579a9d0ebc88bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:20 +0000

    Add package pobo/1.0.7/pobo-1.0.7.tar.gz

---
+++ packages/pobo/pobo-1.0.7.tar.gz/pobo-1.0.7/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import os
+
+README_rst = ''
+fndoc = os.path.join(os.path.dirname(__file__), 'README.rst')
+with open(fndoc, mode='r', encoding='utf-8') as fd:
+    README_rst = fd.read()
+
+setup(name='pobo',
+      version='1.0.7',
+      description='Python3 parser and network representation of Open Biological and Biomedical Ontologies (OBO)',
+      author='XiaoGerGer',
+      author_email='is.me.xiao@gmail.com',
+      url='https://github.com/mschubert/python-obo',
+      packages=[],
+      keywords='parse obo',
+      license="MIT",
+      long_description=README_rst
+      )
