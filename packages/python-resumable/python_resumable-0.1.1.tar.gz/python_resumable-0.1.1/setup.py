5b93ca1499cd296de01b76798108e1ed87f766ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:50 +0000

    Add package python-resumable/0.1.1/python_resumable-0.1.1.tar.gz

---
+++ packages/python-resumable/python_resumable-0.1.1.tar.gz/python_resumable-0.1.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='python_resumable',
+      version='0.1.1',
+      description='Hook for working with resumable.js',
+      author='Igor Velme <ivelmey@gmail.com>, Andrey Kasatov',
+      author_email='ivelmey@gmail.com',
+      license='MIT',
+      keywords='resumablejs',
+      install_requires=['natsort'],
+      python_requires='>=3',
+      long_description=long_description,
+      url='https://github.com/Reriiru/python-resumable',
+
+      package_data={
+        '': ['*.txt', '*.rst', '*.md'],
+      },
+
+      packages=find_packages(),
+)
