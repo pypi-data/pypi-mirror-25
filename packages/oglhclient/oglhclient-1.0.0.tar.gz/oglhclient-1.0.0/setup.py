b2758a8d7653b5451c63f23212c6c5bface65a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:15 +0000

    Add package oglhclient/1.0.0/oglhclient-1.0.0.tar.gz

---
+++ packages/oglhclient/oglhclient-1.0.0.tar.gz/oglhclient-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import io
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'oglhclient',
+  version = '1.0.0',
+  description = 'An API client library for Opengear Lighthouse',
+  long_description=long_description,
+  author = 'Lighthouse Team',
+  author_email = 'engineering@opengear.com',
+  url = 'https://github.com/opengear/oglhclient',
+  download_url = 'https://github.com/opengear/oglhclient/archive/v1.0.0.tar.gz',
+  keywords = ['api', 'opengear', 'lighthouse'],
+  classifiers = ['Programming Language :: Python :: 3', 'Programming Language :: Python :: 2'],
+  install_requires = ['requests','pyyaml','future'],
+  packages=find_packages(),
+  package_data={'': ['*.raml', '*.html']},
+)
