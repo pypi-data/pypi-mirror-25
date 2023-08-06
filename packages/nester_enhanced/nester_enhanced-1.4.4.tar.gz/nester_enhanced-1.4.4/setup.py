86a2a036a908be61a04486bdc02077889c35f4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:10 +0000

    Add package nester_enhanced/1.4.4/nester_enhanced-1.4.4.tar.gz

---
+++ packages/nester_enhanced/nester_enhanced-1.4.4.tar.gz/nester_enhanced-1.4.4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name          = 'nester_enhanced',
+        version       = '1.4.4',
+        py_modules    = ['nester_enhanced'],
+        author        = 'Garry Meng',
+        author_email  = 'menggaoling@163.com',
+        url           = 'http://blog.csdn.net/garrymeng',
+        description   = 'Enhanced printer for nested list printing',
+    )
