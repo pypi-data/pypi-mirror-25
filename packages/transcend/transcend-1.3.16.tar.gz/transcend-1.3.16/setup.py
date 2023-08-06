70fe2f5d894f029de63f64def33a396d55ce374c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:17 +0000

    Add package transcend/1.3.16/transcend-1.3.16.tar.gz

---
+++ packages/transcend/transcend-1.3.16.tar.gz/transcend-1.3.16/setup.py
@@ -0,0 +1,10 @@
+#python setup.py sdist upload
+from setuptools import setup
+setup(
+	name='transcend',    # This is the name of your PyPI-package.
+	version='1.3.16',                          # Update the version number for new releases
+	author='Michael Farrell',
+	author_email='mike@transcend.io',
+	url='https://github.com/transcend-inc/transcend-sdk-python',
+	packages=['transcend', 'transcend/formats'] 
+)
