958a5e552f51d72301ca0a40b9a4cf4643d89e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:13 +0000

    Add package KWsearch/0.0.1/KWsearch-0.0.1.zip

---
+++ packages/KWsearch/KWsearch-0.0.1.zip/KWsearch-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='KWsearch',
+    version='0.0.1',
+    description='file cataloging tool',
+    long_description=long_description,
+    url='https://github.com/vkopey/KWsearch',
+    author='Volodymyr Kopei',
+    author_email='vkopey@gmail.com',
+    license='MIT',
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: Ukranian',
+    'Operating System :: Microsoft :: Windows',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7'
+    ],
+    keywords=['search','cataloging'],
+    py_modules=["KWsearch"],
+
+)
