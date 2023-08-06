36ad35a855b459b91a7839cb1c76df2639073ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:17 +0000

    Add package q-stdev/0.1/q-stdev-0.1.tar.gz

---
+++ packages/q-stdev/q-stdev-0.1.tar.gz/q-stdev-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='q-stdev',
+    version='0.1',
+    url='https://github.com/coddingtonbear/q-stdev',
+    description=(
+        'Adds a new `stdev` aggregate to Q (textasdata).'
+    ),
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[],
+    packages=find_packages(),
+    entry_points={
+        'q_aggregates': {
+            'stdev = q_stdev.StDev'
+        }
+    }
+)
