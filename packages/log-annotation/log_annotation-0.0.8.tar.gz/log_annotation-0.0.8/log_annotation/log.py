c9833dc3fd6245fd64b295531bb813e1070637ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:30 +0000

    Add package log-annotation/0.0.8/log_annotation-0.0.8.tar.gz

---
+++ packages/log-annotation/log_annotation-0.0.8.tar.gz/log_annotation-0.0.8/log_annotation/log.py
@@ -0,0 +1,31 @@
+import logging
+import graypy
+import requests
+def log(name='', level=logging.INFO, **graylog):
+    import time
+    import logging
+    def decorator(function):
+        def wrapper(*args, **kwds):
+            try:
+                # Init logger
+                logger = logging.getLogger(name)
+                logger.setLevel(level)
+                # Log Init
+                print(graylog)
+                print(graylog.get("host",None))
+                if (graylog.get("host",None) != None and graylog.get("port",None) != None):
+                    handler = graypy.GELFHandler(graylog["host"], graylog["port"])
+                    logger.addHandler(handler)
+                else:
+                    logger.addHandler(logging.StreamHandler())
+                start_time = time.time()
+                res = function(*args, **kwds)
+                elapsed_time = str(round(time.time() - start_time,2))
+                logger.info({"function":function.__name__, "elapsed_time":elapsed_time})
+                return res
+            except requests.exceptions.HTTPError as err:
+                logger.error({'error': {'function':function.__name__ , 'description': str(err), 'response': err.response.json()}})
+            except Exception as err:
+                logger.error({'error': {'function':function.__name__ , 'description': str(err)}})
+        return wrapper
+    return decorator
