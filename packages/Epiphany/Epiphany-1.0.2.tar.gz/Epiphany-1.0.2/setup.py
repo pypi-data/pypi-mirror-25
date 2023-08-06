92b64965d66c9160668d6efd29b3d17eed836a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:24 +0000

    Add package Epiphany/1.0.2/Epiphany-1.0.2.tar.gz

---
+++ packages/Epiphany/Epiphany-1.0.2.tar.gz/Epiphany-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name='Epiphany',
+    version='1.0.2',
+    packages=['epiphany', 'epiphany.types'],
+    url='http://br0kenb1nary.github.io/epiphany',
+    license='MIT',
+    author='Damien Rose',
+    author_email='br0kenb1nary@users.noreply.github.com',
+    description='Epiphany'
+)
