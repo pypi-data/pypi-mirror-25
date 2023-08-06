e4d678d56947f2e960640b3133738ea675b77897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:55 +0000

    Add package pycook/0.1.3/pycook-0.1.3.tar.gz

---
+++ packages/pycook/pycook-0.1.3.tar.gz/pycook-0.1.3/pycook/__main__.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import pycook as pc
+import elisp as el
+
+def script_describe(book):
+    return "usage: cook <recipe>\n\nAvailable recipes:\n" + \
+        pc.cook.recipe_names(book)
+
+def script_get_book():
+    if el.file_exists_p("Cookbook.py"):
+        return os.path.realpath("Cookbook.py")
+    elif el.file_exists_p("cook/Cookbook.py"):
+        return os.path.realpath("cook/Cookbook.py")
+    else:
+        raise RuntimeError("No Cookbook.py or cook/Cookbook.py found")
+
+def main(argv = None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        book = script_get_book()
+        pc.main(argv, book)
+    except RuntimeError as e:
+        print(e)
