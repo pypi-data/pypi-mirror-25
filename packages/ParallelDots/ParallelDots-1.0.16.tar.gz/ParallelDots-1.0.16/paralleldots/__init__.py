9c9d778de95dd3284549be3d5e59496b60fa78bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:50 +0000

    Add package ParallelDots/1.0.16/ParallelDots-1.0.16.tar.gz

---
+++ packages/ParallelDots/ParallelDots-1.0.16.tar.gz/ParallelDots-1.0.16/paralleldots/__init__.py
@@ -0,0 +1,17 @@
+__all__= [ "taxonomy", "config", "ner", "similarity", "sentiment", "keywords", "emotion", "intent", "multilang", "sentiment_social", "get_usage" ]
+
+from paralleldots.taxonomy            import get_taxonomy            as taxonomy
+from paralleldots.ner                 import get_ner                 as ner
+from paralleldots.similarity          import get_similarity          as similarity
+from paralleldots.sentiment           import get_sentiment           as sentiment
+from paralleldots.sentiment_social    import get_sentiment_social    as sentiment_social
+from paralleldots.keywords            import get_keywords            as keywords
+from paralleldots.emotion             import get_emotion             as emotion
+from paralleldots.intent              import get_intent              as intent
+from paralleldots.multilang           import get_multilang           as multilang
+from paralleldots.abuse               import get_abuse               as abuse
+
+from paralleldots.usage               import get_usage               as usage
+
+from paralleldots.config              import set_api_key
+from paralleldots.config              import get_api_key
