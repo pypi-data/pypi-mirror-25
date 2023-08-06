7d355631332600cd172dda296992cad6490615af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:10 +0000

    Add package canopen/0.5.0/canopen-0.5.0.tar.gz

---
+++ packages/canopen/canopen-0.5.0.tar.gz/canopen-0.5.0/canopen/sync.py
@@ -0,0 +1,55 @@
+import time
+import threading
+
+from .network import CanError
+
+
+class SyncProducer(object):
+    """Transmits a SYNC message periodically."""
+
+    #: COB-ID of the SYNC message
+    cob_id = 0x80
+
+    def __init__(self, network):
+        self.network = network
+        self.period = None
+        self.transmit_thread = None
+        self.stop_event = threading.Event()
+
+    def transmit(self):
+        """Send out a SYNC message once."""
+        self.network.send_message(self.cob_id, [])
+
+    def start(self, period=None):
+        """Start periodic transmission of SYNC message in a background thread.
+        
+        :param float period:
+            Period of SYNC message in seconds.
+        """
+        if period is not None:
+            self.period = period
+
+        if not self.period:
+            raise ValueError("A valid transmission period has not been given")
+
+        if not self.transmit_thread or not self.transmit_thread.is_alive():
+            self.stop_event.clear()
+            self.transmit_thread = threading.Thread(
+                target=self._periodic_transmit)
+            self.transmit_thread.daemon = True
+            self.transmit_thread.start()
+
+    def stop(self):
+        """Stop periodic transmission of SYNC message."""
+        self.stop_event.set()
+        self.transmit_thread = None
+
+    def _periodic_transmit(self):
+        while not self.stop_event.is_set():
+            start = time.time()
+            try:
+                self.transmit()
+            except CanError as error:
+                print(str(error))
+            time_left = self.period - (time.time() - start)
+            time.sleep(max(time_left, 0.0))
---
+++ packages/canopen/canopen-0.5.0.tar.gz/canopen-0.5.0/test/test_network.py
@@ -0,0 +1,42 @@
+import os
+import unittest
+import canopen
+
+import can
+
+
+EDS_PATH = os.path.join(os.path.dirname(__file__), 'sample.eds')
+
+
+class TestNetwork(unittest.TestCase):
+
+    def setUp(self):
+        network = canopen.Network()
+        network.add_node(2, EDS_PATH)
+        network.add_node(3, network[2].object_dictionary)
+        self.network = network
+
+    def test_add_node(self):
+        node = self.network[2]
+        self.assertIsInstance(node, canopen.Node)
+        self.assertEqual(node.id, 2)
+        self.assertEqual(self.network[2], node)
+        self.assertEqual(len(self.network), 2)
+
+    def test_notify(self):
+        node = self.network[2]
+        self.network.notify(0x82, b'\x01\x20\x02\x00\x01\x02\x03\x04', 1473418396.0)
+        self.assertEqual(len(node.emcy.active), 1)
+        self.network.notify(0x702, b'\x05', 1473418396.0)
+        self.assertEqual(node.nmt.state, 'OPERATIONAL')
+        self.assertListEqual(self.network.scanner.nodes, [2])
+
+
+class TestScanner(unittest.TestCase):
+
+    def test_passive_scanning(self):
+        scanner = canopen.network.NodeScanner()
+        scanner.on_message_received(0x586)
+        scanner.on_message_received(0x587)
+        scanner.on_message_received(0x586)
+        self.assertListEqual(scanner.nodes, [6, 7])
