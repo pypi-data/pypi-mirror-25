ff1cf1d76b182b6175726453be190d358b0207e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:52 +0000

    Add package django-sortable-listview/0.42/django-sortable-listview-0.42.tar.gz

---
+++ packages/django-sortable-listview/django-sortable-listview-0.42.tar.gz/django-sortable-listview-0.42/setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup
+
+install_requires = [
+    'django>=1.8',
+]
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    # add backport to list of required modules
+    install_requires.append('ordereddict')
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+CHANGELOG = open(os.path.join(os.path.dirname(__file__), 'CHANGELOG.md')).read()
+LICENSE = open(os.path.join(os.path.dirname(__file__), 'LICENSE.txt')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-sortable-listview',
+    version='0.42',
+    packages=['sortable_listview'],
+    include_package_data=True,
+    license=LICENSE,
+    description='An extension of django\'s ListView that provides sorting',
+    long_description=README + CHANGELOG,
+    url='https://github.com/aptivate/django-sortable-listview',
+    author='Aptivate',
+    author_email='info@aptivate.org',
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    test_suite='tests.runtests.runtests'
+)
