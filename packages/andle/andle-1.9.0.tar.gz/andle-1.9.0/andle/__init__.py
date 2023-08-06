ab883b81202fd97d5ff57cb093d6b56642023b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:51 +0000

    Add package andle/1.9.0/andle-1.9.0.tar.gz

---
+++ packages/andle/andle-1.9.0.tar.gz/andle-1.9.0/andle/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import andle.android
+import andle.sdk
+
+__version__ = "1.9.0"
+
+
+def update(path, dryrun, remote, gradle, interact):
+    data = sdk.load()
+    android.update(path, data, dryrun, remote, gradle, interact)
+
+
+def setsdk(path):
+    sdk.setpath(path)
---
+++ packages/andle/andle-1.9.0.tar.gz/andle-1.9.0/andle/remote.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import andle.http
+from xml.dom import minidom
+
+JCENTER_URL = "https://jcenter.bintray.com/"
+# MAVEN_URL = "https://repo1.maven.org/maven2/"
+
+
+def load(name, url=JCENTER_URL):
+    request = andle.http.request(
+        url + name.replace(".", "/").replace(":", "/") + "/maven-metadata.xml")
+    try:
+        DOMTree = minidom.parse(request)
+
+        collection = DOMTree.documentElement
+        versioning = collection.getElementsByTagName("versioning")[0]
+        latest = versioning.getElementsByTagName("release")[0]
+        version = latest.childNodes[0].data
+        return version
+    except BaseException:
+        print("fail to get version : " + name)
+        return None
