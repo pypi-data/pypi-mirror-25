f113005b16d3ea2bcb9afa872089cdaaf4aa7bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:19 +0000

    Add package AWSScout2/3.0.5/AWSScout2-3.0.5.tar.gz

---
+++ packages/AWSScout2/AWSScout2-3.0.5.tar.gz/AWSScout2-3.0.5/AWSScout2/rules/exceptions.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Exceptions handling
+"""
+
+import json
+
+
+def process_exceptions(aws_config, exceptions_filename = None):
+    """
+    DDDD
+
+    :param aws_config:
+    :param exceptions_filename:
+    :return:
+    """
+
+    # Load exceptions
+    if not exceptions_filename:
+        return
+    with open(exceptions_filename, 'rt') as f:
+        exceptions = json.load(f)
+
+    # Process exceptions
+        for service in exceptions['services']:
+            for rule in exceptions['services'][service]['exceptions']:
+                filtered_items = []
+                for item in aws_config['services'][service]['findings'][rule]['items']:
+                    if item not in exceptions['services'][service]['exceptions'][rule]:
+                        filtered_items.append(item)
+                aws_config['services'][service]['findings'][rule]['items'] = filtered_items
+                aws_config['services'][service]['findings'][rule]['flagged_items'] = len(aws_config['services'][service]['findings'][rule]['items'])
