2eec78cc3fc66031260693c52acf9e91e112d291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:22 +0000

    Add package modoboa-imap-migration/1.1.2/modoboa-imap-migration-1.1.2.tar.gz

---
+++ packages/modoboa-imap-migration/modoboa-imap-migration-1.1.2.tar.gz/modoboa-imap-migration-1.1.2/modoboa_imap_migration/models.py
@@ -0,0 +1,29 @@
+"""
+IMAP migration models.
+"""
+
+from django.db import models
+
+from modoboa.admin.models import Mailbox
+from modoboa.lib.cryptutils import encrypt, decrypt
+
+
+class Migration(models.Model):
+
+    """Represent mailboxes to migrate."""
+
+    mailbox = models.ForeignKey(Mailbox)
+    _password = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.mailbox.full_address
+
+    @property
+    def password(self):
+        """Password getter."""
+        return decrypt(self._password)
+
+    @password.setter
+    def password(self, value):
+        """Password setter."""
+        self._password = encrypt(value)
