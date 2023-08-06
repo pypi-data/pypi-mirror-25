e67358d84a36cfaa248da061a52f439523de21cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:01 +0000

    Add package xcirculardichro/0.6/xcirculardichro-0.6.tar.gz

---
+++ packages/xcirculardichro/xcirculardichro-0.6.tar.gz/xcirculardichro-0.6/xcirculardichro/gui/view/hdfdatafileitem.py
@@ -0,0 +1,13 @@
+'''
+ Copyright (c) 2017, UChicago Argonne, LLC
+ See LICENSE file.
+'''
+import logging
+logger = logging.getLogger(__name__)
+from xcirculardichro.gui.view.dataitem import DataItem
+
+class HDFDataFileItem(DataItem):
+    
+    def __init__(self):
+        print("Hello")
+        
---
+++ packages/xcirculardichro/xcirculardichro-0.6.tar.gz/xcirculardichro-0.6/xcirculardichro/gui/view/textdatafileitem.py
@@ -0,0 +1,14 @@
+'''
+ Copyright (c) 2017, UChicago Argonne, LLC
+ See LICENSE file.
+'''
+import logging
+logger = logging.getLogger(__name__)
+from xcirculardichro.gui.view.dataitem import DataItem
+
+class TextDataFileItem(DataItem):
+    
+    def __init__(self):
+        print("Hello")
+            
+    
