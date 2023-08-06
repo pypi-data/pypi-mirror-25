0f4dd162dbae6859784352ea3b19f98f99fdbd2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:31 +0000

    Add package pyfbi/0.1.2/pyfbi-0.1.2.tar.gz

---
+++ packages/pyfbi/pyfbi-0.1.2.tar.gz/pyfbi-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+requires = ["tornado>=4.5.2"]
+
+
+setup(
+    name="pyfbi",
+    version="0.1.2",
+    description="pyFBI enables 'as much as needed' profiling by decorator",
+    url="https://github.com/icoxfog417/pyfbi",
+    author="icoxfog417",
+    author_email="icoxfog417@yahoo.co.jp",
+    license="MIT",
+    keywords="performance-analysis pstat profile",
+    packages=[
+        "pyfbi",
+        "pyfbi.visualize",
+    ],
+    entry_points={
+        "console_scripts": ["pyfbi_viz=pyfbi.pyfbi_viz:main"],
+    },
+    install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+    ],
+)
