3b9e040788f399cbd81e443615e6cd10e9669355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:13 +0000

    Add package wattzon-link/0.1.dev1/wattzon-link-0.1.dev1.tar.gz

---
+++ packages/wattzon-link/wattzon-link-0.1.dev1.tar.gz/wattzon-link-0.1.dev1/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='wattzon-link',
+    version='0.1.dev1',
+    description='WattzOn Link API client',
+    url='https://github.com/WattzOn/link-python-client',
+    keywords='wattzon link',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=['requests'],
+    python_requires='>=3'
+)
+
