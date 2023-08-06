10a91aa8716b3b48a3d230f3c9a84cb37774b177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:00 +0000

    Add package spirit.plone.theming/0.6/spirit.plone.theming-0.6.tar.gz

---
+++ packages/spirit.plone.theming/spirit.plone.theming-0.6.tar.gz/spirit.plone.theming-0.6/CHANGES.rst
@@ -0,0 +1,41 @@
+Changelog
+=========
+
+
+0.6 (2017-10-13)
+----------------
+
+- Add snippets to show availability of theming addons.
+
+
+0.5 (2017-05-18)
+----------------
+
+- Instead of providing a diazo snippet for the logo, provide a custom logo viewlet.
+
+
+0.4 (2017-05-18)
+----------------
+
+- Load collective.monkeypatcher, if available, to prevent ConfigurationError.
+- Add ``site_logo`` option to upload a custom site logo (Plone 4 only).
+
+
+0.3 (2017-01-24)
+----------------
+
+- Add option to hide footer and colophon texts completely.
+- Update installation docs and documentation images.
+
+
+0.2 (2017-01-12)
+----------------
+
+- Add ``site_favicon`` option to upload a custom favicon.
+- Update documentation.
+
+
+0.1 (2016-12-20)
+----------------
+
+- Initial release.
---
+++ packages/spirit.plone.theming/spirit.plone.theming-0.6.tar.gz/spirit.plone.theming-0.6/src/spirit/plone/theming/browser/controlpanel.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""Plone Theming Settings Control Panel."""
+
+# zope imports
+from plone.app.registry.browser import controlpanel
+from plone.formwidget.namedfile.widget import NamedImageFieldWidget
+from plone.registry.interfaces import IRegistry
+from z3c.form import field
+from zope.component import getUtility
+from zope.interface import implementer
+
+# local imports
+from spirit.plone.theming import (
+    _,
+    PLONE_4,
+)
+from spirit.plone.theming.interfaces import (
+    IPloneThemeSettings,
+    IPloneThemeSettingsEditForm,
+)
+
+
+class SelfHealingRegistryEditForm(controlpanel.RegistryEditForm):
+    """Registers the schema if an error occured."""
+
+    def getContent(self):
+        registry = getUtility(IRegistry)
+        try:
+            return registry.forInterface(  # noqa
+                self.schema,
+                prefix=self.schema_prefix,
+            )
+        except KeyError:
+            self.ignoreContext = True
+            self.fields = field.Fields()
+            registry.registerInterface(self.schema)
+            self.status = _(
+                u'Registry has been updated. Please reload this page.'
+            )
+            return None
+
+
+@implementer(IPloneThemeSettingsEditForm)
+class PloneThemeSettingsEditForm(SelfHealingRegistryEditForm):
+    """Plone Theming Settings Form."""
+
+    schema = IPloneThemeSettings
+    label = _(u'Plone Theming Settings')
+    description = _(
+        u'Settings that can be used by Diazo themes. Please note that not '
+        u'every theme supports all possible options.'
+    )
+
+    def updateFields(self):
+        super(PloneThemeSettingsEditForm, self).updateFields()
+        if PLONE_4:
+            self.fields['site_logo'].widgetFactory = NamedImageFieldWidget
+        self.fields['site_favicon'].widgetFactory = NamedImageFieldWidget
+
+    def updateWidgets(self):
+        super(PloneThemeSettingsEditForm, self).updateWidgets()
+
+
+class PloneThemeSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
+    """Plone Theming Settings Control Panel."""
+
+    form = PloneThemeSettingsEditForm
