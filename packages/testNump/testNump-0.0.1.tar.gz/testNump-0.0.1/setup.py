3fdc02a512cb5dc50155f20e5ed0a535cfb9848a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:06 +0000

    Add package testNump/0.0.1/testNump-0.0.1.tar.gz

---
+++ packages/testNump/testNump-0.0.1.tar.gz/testNump-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='testNump',
+    version='0.0.1',
+    description='A test project',
+    url='https://github.com/alvinhui/',
+    author='Alvin Hui',
+    author_email='alvinxwt@gmail.com',
+    license='MIT',
+    install_requires=[
+        "numpy>=1.13",
+    ],
+    packages=['testNump']
+)
