f3e4992ae1d7285e7309e13f35fed70f49362ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:19 +0000

    Add package tablesnap/1.2.0/tablesnap-1.2.0.tar.gz

---
+++ packages/tablesnap/tablesnap-1.2.0.tar.gz/tablesnap-1.2.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='tablesnap',
+    description='Uses inotify to monitor Cassandra SSTables and upload them to S3',
+    long_description='Tablesnap is a script that uses inotify to monitor a directory for IN_MOVED_TO events and reacts to them by spawning a new thread to upload that file to Amazon S3, along with a JSON-formatted list of what other files were in the directory at the time of the copy.',
+    version='1.2.0',
+    license='BSD',
+    author='Jeremy Grosser',
+    author_email='jeremy@synack.me',
+    url='https://github.com/JeremyGrosser/tablesnap',
+    scripts=[
+        'tablesnap',
+        'tableslurp',
+        'tablechop'
+    ],
+    install_requires=[
+        'pyinotify',
+        'boto>=2.6.0',
+        'argparse',
+        'python-dateutil',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: System :: Archiving :: Backup',
+        'Topic :: Utilities',
+    ]
+)
