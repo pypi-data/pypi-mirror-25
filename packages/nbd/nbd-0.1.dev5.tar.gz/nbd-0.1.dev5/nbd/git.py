7e9b4c3c5c78b8730afc3ca3c09a04fd258f988e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:43 +0000

    Add package nbd/0.1.dev5/nbd-0.1.dev5.tar.gz

---
+++ packages/nbd/nbd-0.1.dev5.tar.gz/nbd-0.1.dev5/nbd/git.py
@@ -0,0 +1,43 @@
+from subprocess import (call, check_output)
+
+from .command import (ANSI_LIGHT_RED, echo)
+
+HEAD = 'HEAD'
+
+class Git(object):
+  """
+  Namespace for git functions.
+  """
+
+  @staticmethod
+  def rev_parse_show_toplevel():
+    args = ['git', 'rev-parse', '--show-toplevel']
+    echo('git', ANSI_LIGHT_RED, " ".join(args))
+    return check_output(args).strip('\n')
+
+  @staticmethod
+  def show(filepath, commit=HEAD):
+    args = ['git', 'show', '{}:{}'.format(commit, filepath)]
+    echo('git', ANSI_LIGHT_RED, " ".join(args))
+    return check_output(args)
+
+  @staticmethod
+  def diff_no_index(file_a, file_b, options=None):
+    args = [
+      'git',
+      '--no-pager',
+      'diff',
+      '--exit-code',
+      '--no-index',
+      '--color=always']
+    if options is not None:
+      args.extend(options)
+    args.extend([file_a, file_b])
+    echo('git', ANSI_LIGHT_RED, " ".join(args))
+    call(args)
+
+  @staticmethod
+  def diff_name_status(commit):
+    args = ['git', 'diff', commit, '--name-status']
+    echo('git', ANSI_LIGHT_RED, " ".join(args))
+    return check_output(args)
