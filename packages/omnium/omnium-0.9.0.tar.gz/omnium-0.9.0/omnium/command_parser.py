f59c241f67c9e543281629a75a999672acb77b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:25 +0000

    Add package omnium/0.9.0/omnium-0.9.0.tar.gz

---
+++ packages/omnium/omnium-0.9.0.tar.gz/omnium-0.9.0/omnium/command_parser.py
@@ -0,0 +1,30 @@
+import argparse
+
+try:
+    import argcomplete
+    use_argparse = True
+except ImportError:
+    use_argparse = False
+
+
+def parse_commands(name, args, module, cmdline_args):
+    # create the top-level parser.
+    parser = argparse.ArgumentParser(prog=name)
+    subparsers = parser.add_subparsers(dest='cmd_name')
+
+    for args, kwargs in args:
+        parser.add_argument(*args, **kwargs)
+
+    cmds = module.modules
+    for cmd_name, cmd_module in cmds.items():
+        # create the subparser for each command.
+        subparser = subparsers.add_parser(cmd_name,
+                                          help=cmd_module.__doc__)
+        for args, kwargs in cmd_module.ARGS:
+            subparser.add_argument(*args, **kwargs)
+
+    if use_argparse:
+        argcomplete.autocomplete(parser)
+    args = parser.parse_args(cmdline_args)
+
+    return cmds, args
