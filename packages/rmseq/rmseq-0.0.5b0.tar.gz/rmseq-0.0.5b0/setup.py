82ae72e7594753c6ecf5f8fbedf894484f3e7518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:09 +0000

    Add package rmseq/0.0.5b0/rmseq-0.0.5b0.tar.gz

---
+++ packages/rmseq/rmseq-0.0.5b0.tar.gz/rmseq-0.0.5b0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import RMseq
+import os
+
+
+LONG_DESCRIPTION = 'RM-seq is a bioinformatics tool for for ' +\
+                   'assessing resistance mutations from PE short-reads.'
+
+
+setup(
+    name = 'rmseq',
+    version = RMseq.__version__,
+    description = RMseq.__description__,
+    long_description=LONG_DESCRIPTION,
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'License :: OSI Approved :: GNU Affero General ' +
+                   'Public License v3 or later (AGPLv3+)',
+                   'Programming Language :: Python :: 3.5',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics',
+                   'Topic :: Scientific/Engineering :: Medical Science Apps.',
+                   'Intended Audience :: Science/Research'],
+    keywords = ['resistance',
+                'mutation'],
+    download_url = RMseq.__download_url__,
+    author = RMseq.__author__,
+    author_email = RMseq.__author_email__,
+    license = RMseq.__license__,
+    packages = ['RMseq'],
+    scripts = ['RMseq/rmseq'],
+    include_package_data = True,
+    install_requires = []
+    )
