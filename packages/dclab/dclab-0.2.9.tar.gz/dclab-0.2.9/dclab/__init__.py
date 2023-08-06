5870a1fb3994f6f72efb2d5b1d5d35c8882d5a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:16 +0000

    Add package dclab/0.2.9/dclab-0.2.9.tar.gz

---
+++ packages/dclab/dclab-0.2.9.tar.gz/dclab-0.2.9/dclab/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+This library contains classes and methods for the analysis
+of real-time deformability cytometry (RT-DC) data sets.
+"""
+from __future__ import division, print_function, unicode_literals
+
+import os
+
+# Definitions
+from . import definitions as dfn
+from ._version import version as __version__
+from .rtdc_dataset import new_dataset
+from .polygon_filter import PolygonFilter
+from . import statistics
