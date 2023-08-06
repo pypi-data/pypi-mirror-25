1d1c78d232ab306cd574e091e44a8ade06b5b555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:55 +0000

    Add package sxs/2017.10.10.12.9.59.dev30902516/sxs-2017.10.10.12.9.59.dev30902516-py2.py3-none-any.whl

---
+++ packages/sxs/sxs-2017.10.10.12.9.59.dev30902516-py2.py3-none-any.whl/sxs/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import division, print_function, absolute_import
+
+__doc_title__ = "SXS python code"
+__doc__ = "A collection of python code used by the SXS collaboration"
+
+from ._version import __version__
