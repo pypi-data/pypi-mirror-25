96e396c1e9eb7b4d75570ebc7dc0210cd35e2727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:18 +0000

    Add package lecsim-fibu/1.0.2/lecsim_fibu-1.0.2.tar.gz

---
+++ packages/lecsim-fibu/lecsim_fibu-1.0.2.tar.gz/lecsim_fibu-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name       = 'lecsim_fibu',
+    version    = '1.0.2',
+    py_modules = ['lecsim_fibu'],
+    author     = 'lecsim',
+    author_email = 'lechler@gmx.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'Fibu-Folgen',
+)
