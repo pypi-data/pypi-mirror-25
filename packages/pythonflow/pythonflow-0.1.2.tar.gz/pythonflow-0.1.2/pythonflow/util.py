0196f3e683c6c7c7973336d996b76f75a5bb49fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:35 +0000

    Add package pythonflow/0.1.2/pythonflow-0.1.2.tar.gz

---
+++ packages/pythonflow/pythonflow-0.1.2.tar.gz/pythonflow-0.1.2/pythonflow/util.py
@@ -0,0 +1,40 @@
+# pylint: disable=missing-docstring
+# pylint: enable=missing-docstring
+# Copyright 2017 Spotify AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import collections
+
+
+class lazy_import:  # pylint: disable=invalid-name, too-few-public-methods
+    """
+    Lazily import the given module.
+
+    Parameters
+    ----------
+    module : str
+        name of the module to import
+    """
+    def __init__(self, module):
+        self.module = module
+        self._module = None
+
+    def __getattr__(self, name):
+        if self._module is None:
+            self._module = __import__(self.module)
+        return getattr(self._module, name)
+
+
+Quantity = collections.namedtuple('Quantity', ['value', 'unit'])
