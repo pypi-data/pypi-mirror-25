94f5cc62faadc407baec363db0b9c5393b533abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:38 +0000

    Add package felapps/1.5.15/felapps-1.5.15-py2-none-any.whl

---
+++ packages/felapps/felapps-1.5.15-py2-none-any.whl/felapps/utils/miscutils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# class to control app version number
+class AppVersions():
+    def __init__(self):
+        self.versions = {
+                         'imageviewer'  : '1.4.1',
+                         'cornalyzer'   : '2.0',
+                         'felformula'   : '1.4',
+                         'dataworkshop' : '1.1',
+                         'matchwizard'  : '1.0',
+                         'wxmpv'        : '0.2',
+                         'appdrawer'    : '1.5.15',
+                         }
+
+    def setVersion(self, verNum, appName = 'imageviewer'):
+        self.versions.update({appName: verNum})
+
+    def getVersion(self, appName = 'imageviewer'):
+        return self.versions.get(appName)
+
