23eb678e0d257b336df38c0ec230834b741ba7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:41 +0000

    Add package APNGLib/1.0/APNGLib-1.0.tar.gz

---
+++ packages/APNGLib/APNGLib-1.0.tar.gz/APNGLib-1.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup, Extension
+
+module = Extension('APNGLib',
+
+					libraries = ['stdc++', 'm', 'png', 'z'],
+                    sources = ['APNGLib/APNGLib.cpp'])
+
+setup (name = 'APNGLib',
+	   packages = ['APNGLib'],
+       version = '1.0',
+       description = 'APNGLib a few tools for working with APNG files',
+	   author = 'Tim Collins',
+	   author_email = 'tscollins321@aim.com',
+	   url = 'https://github.com/slicer4ever/APNGLib',
+	   download_url = 'https://github.com/slicer4ever/APNGLib/archive/1.0.tar.gz',
+	   keywords = ['APNG'],
+	   classifiers = [],
+       ext_modules = [module])
