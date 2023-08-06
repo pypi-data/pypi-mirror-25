3b3529b6277ed9e6ae004cecc3142e7a5babef57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:38 +0000

    Add package scitokens/0.1.6/scitokens-0.1.6.tar.gz

---
+++ packages/scitokens/scitokens-0.1.6.tar.gz/scitokens-0.1.6/setup.py
@@ -0,0 +1,19 @@
+
+"""
+Install file for SciTokens project.
+"""
+
+import setuptools
+
+setuptools.setup(name="scitokens",
+                 version="0.1.6",
+                 description="SciToken reference implementation library",
+                 author_email="team@scitokens.org",
+                 author="Brian Bockelman",
+                 url="https://scitokens.org",
+                 package_dir={"": "src"},
+                 packages=["scitokens"],
+                 scripts=['tools/scitokens-admin-create'],
+                 install_requires=['cryptography',
+                                   'PyJWT'],
+                )
