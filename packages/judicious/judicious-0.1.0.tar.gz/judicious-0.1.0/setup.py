66a2005cb709a701a946806cd6a4b62ccc566875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:13 +0000

    Add package judicious/0.1.0/judicious-0.1.0.tar.gz

---
+++ packages/judicious/judicious-0.1.0.tar.gz/judicious-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name='judicious',
+  packages=['judicious'],
+  version='0.1.0',
+  description='judicious',
+  author='Jordan Suchow',
+  author_email='jwsuchow@gmail.com',
+  url='https://suchow.com/',
+)
