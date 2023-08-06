cafe757a2d032d7b9d97547b3f95959228246b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:22 +0000

    Add package linkero/0.9.2/linkero-0.9.2.tar.gz

---
+++ packages/linkero/linkero-0.9.2.tar.gz/linkero-0.9.2/linkero/core/gateway/waitress_service.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import linkero.core.linkero as linkero
+from waitress import serve
+
+def run(app):
+    linkero.printWellcome()
+    linkero.createDB()
+    serve(app, host=linkero.config["host"]["ip"], port=linkero.config["host"]["port"],
+          threads=linkero.config["waitress"]["threads"], connection_limit=linkero.config["waitress"]["spawn"])
