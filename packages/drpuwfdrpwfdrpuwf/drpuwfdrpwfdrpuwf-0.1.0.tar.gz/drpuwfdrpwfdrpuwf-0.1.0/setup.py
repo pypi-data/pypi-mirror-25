e0e0841479cbd3b21814d5bdfb7540ae2934f1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:12 +0000

    Add package drpuwfdrpwfdrpuwf/0.1.0/drpuwfdrpwfdrpuwf-0.1.0.tar.gz

---
+++ packages/drpuwfdrpwfdrpuwf/drpuwfdrpwfdrpuwf-0.1.0.tar.gz/drpuwfdrpwfdrpuwf-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name='drpuwfdrpwfdrpuwf',
+  packages=['drpuwfdrpwfdrpuwf'],
+  version='0.1.0',
+  description='drpuwfdrpwfdrpuwf',
+  author='Jordan Suchow',
+  author_email='jwsuchow@gmail.com',
+  url='https://suchow.com/',
+)
