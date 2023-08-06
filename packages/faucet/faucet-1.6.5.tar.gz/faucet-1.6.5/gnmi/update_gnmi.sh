3dcaa6c1410a268c7c0abfbe4faba85b32ae8b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:49 +0000

    Add package faucet/1.6.5/faucet-1.6.5.tar.gz

---
+++ packages/faucet/faucet-1.6.5.tar.gz/faucet-1.6.5/gnmi/update_gnmi.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Updating gNMI sources..."
+go get -u github.com/google/gnxi/gnmi_get
+go get -u github.com/google/gnxi/gnmi_target
+echo "Rebuilding gNMI binaries..."
+go install github.com/google/gnxi/gnmi_get
+go install github.com/google/gnxi/gnmi_target
+echo "Restarting gNMI Target..."
+fuser -k -n tcp $GNMI_PORT
+nohup ./run_target.sh > $HOME/target.log &
+sleep 2 # wait for noup log message
+echo "Done."
