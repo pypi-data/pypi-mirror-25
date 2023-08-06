05b4671b6b8f59c9a52afaef8b9b1823446e06c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:53 +0000

    Add package ngtsio/1.3.6/ngtsio-1.3.6.tar.gz

---
+++ packages/ngtsio/ngtsio-1.3.6.tar.gz/ngtsio-1.3.6/ngtsio/test_ngtsio_find.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  5 16:07:19 2016
+
+@author:
+Maximilian N. Guenther
+Battcock Centre for Experimental Astrophysics,
+Cavendish Laboratory,
+JJ Thomson Avenue
+Cambridge CB3 0HE
+Email: mg719@cam.ac.uk
+"""
+
+
+
+import ngtsio
+
+    
+if __name__ == '__main__':  
+#    ngtsio.find('03 05 00', '-11 55 00', ngts_version='TEST18')
+#    ngtsio.find(0.82299971580505371, -0.17282542586326599, ngts_version='TEST18', unit='rad')
+#    ngtsio.find_list('/Users/mx/Desktop/Astrometric_Binaries_coords.txt')
+    
+    ngtsio.find('05 23 31', '-25 08 48', ngts_version='TEST18')
