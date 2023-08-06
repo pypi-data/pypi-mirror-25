f1fb8be64795fec0de8fa73da25c794f5ad93291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:20 +0000

    Add package pymesos/0.2.15/pymesos-0.2.15.tar.gz

---
+++ packages/pymesos/pymesos-0.2.15.tar.gz/pymesos-0.2.15/pymesos/__init__.py
@@ -0,0 +1,15 @@
+from .interface import Scheduler, Executor
+from .scheduler import MesosSchedulerDriver
+from .executor import MesosExecutorDriver
+from .utils import encode_data, decode_data
+
+__VERSION__ = '0.2.15'
+
+__all__ = (
+    'Scheduler',
+    'MesosSchedulerDriver',
+    'Executor',
+    'MesosExecutorDriver',
+    'encode_data',
+    'decode_data',
+)
---
+++ packages/pymesos/pymesos-0.2.15.tar.gz/pymesos-0.2.15/pymesos/utils.py
@@ -0,0 +1,39 @@
+from binascii import b2a_base64, a2b_base64
+
+POSTFIX = {
+    'ns': 1e-9,
+    'us': 1e-6,
+    'ms': 1e-3,
+    'secs': 1,
+    'mins': 60,
+    'hrs': 60 * 60,
+    'days': 24 * 60 * 60,
+    'weeks': 7 * 24 * 60 * 60
+}
+
+
+def parse_duration(s):
+    s = s.strip()
+    unit = None
+    postfix = None
+    for n, u in POSTFIX.items():
+        if s.endswith(n):
+            unit = u
+            postfix = n
+            break
+
+    assert unit is not None, \
+        'Unknown duration \'%s\'; supported units are %s' % (
+            s, ','.join('\'%s\'' % n for n in POSTFIX)
+        )
+
+    n = float(s[:-len(postfix)])
+    return n * unit
+
+
+def encode_data(data):
+    return b2a_base64(data).strip().decode('ascii')
+
+
+def decode_data(data):
+    return a2b_base64(data)
