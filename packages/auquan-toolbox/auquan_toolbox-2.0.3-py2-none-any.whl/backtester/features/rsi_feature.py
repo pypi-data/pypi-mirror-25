c393c0ca8ae98d5b015a2c1ecad39590df68ab86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:56 +0000

    Add package auquan-toolbox/2.0.3/auquan_toolbox-2.0.3-py2-none-any.whl

---
+++ packages/auquan-toolbox/auquan_toolbox-2.0.3-py2-none-any.whl/backtester/features/rsi_feature.py
@@ -0,0 +1,36 @@
+from backtester.features.feature import Feature
+from backtester.financial_fn import ma
+
+
+class RSIFeature(Feature):
+
+    @classmethod
+    def computeForInstrument(cls, updateNum, time, featureParams, featureKey, instrumentManager):
+        instrumentLookbackData = instrumentManager.getLookbackInstrumentFeatures()
+        data = instrumentLookbackData.getFeatureDf(featureParams['featureName'])
+        data_upside = data.sub(data.shift(1), fill_value=0)
+        data_downside = data_upside.copy()
+        data_downside[data_upside > 0] = 0
+        data_upside[data_upside < 0] = 0
+        avg_upside = data_upside[-featureParams['period']:].mean()
+        avg_downside = - data_downside[-featureParams['period']:].mean()
+        rs = avg_upside / avg_downside
+
+        return 100 - (100 * avg_downside / (avg_downside + avg_upside))
+
+    @classmethod
+    def computeForMarket(cls, updateNum, time, featureParams, featureKey, currentMarketFeatures, instrumentManager):
+        lookbackDataDf = instrumentManager.getDataDf()
+        data = lookbackDataDf[featureParams['featureName']]
+        data_upside = data.sub(data.shift(1), fill_value=0)
+        data_downside = data_upside.copy()
+        data_downside[data_upside > 0] = 0
+        data_upside[data_upside < 0] = 0
+        if len(data.index) > 0:
+            avg_upside = data_upside[-featureParams['period']:].mean()
+            avg_downside = - data_downside[-featureParams['period']:].mean()
+            rs = avg_upside / avg_downside
+        else:
+            return 0
+
+        return 100 - (100 * avg_downside / (avg_downside + avg_upside))
