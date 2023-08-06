f0fedb76ea724d9a287dce9b3aadce6934912208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:35 +0000

    Add package chinesecalendar/0.0.3/chinesecalendar-0.0.3.tar.gz

---
+++ packages/chinesecalendar/chinesecalendar-0.0.3.tar.gz/chinesecalendar-0.0.3/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='chinesecalendar',
+    version='0.0.3',
+    description='check if some day is holiday in China',
+    author='Lirian Su',
+    author_email='liriansu@gmail.com',
+    url='https://github.com/LKI/chinese-calendar',
+    license='WTFPL',
+    packages=['chinese_calendar'],
+)
