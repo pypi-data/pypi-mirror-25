734e36af9dca6a17c5fe0453ecd45e9240bf1ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:27 +0000

    Add package django-chatwork/0.2.0/django-chatwork-0.2.0.tar.gz

---
+++ packages/django-chatwork/django-chatwork-0.2.0.tar.gz/django-chatwork-0.2.0/chatwork/utils.py
@@ -0,0 +1,37 @@
+from django.utils.encoding import force_text
+from django.template.loader import render_to_string
+
+from .api import ChatworkApiClient
+
+
+client = ChatworkApiClient()
+api_account_info = client.get_my_profile()
+api_account_id = getattr(api_account_info, 'account_id', '0')
+api_room_id = getattr(api_account_info, 'room_id', '0')
+
+
+def get_rooms(room_type='group'):
+    """ 所属するルームを取得する """
+    rooms = client.get_rooms()
+    return [room for room in rooms if room['type'] == room_type]
+
+
+def send_chatwork(text, room, title=None, to_all=None):
+    """ 一つのルームにメッセージを送信する """
+    context = {}
+    context['members'] = [m for m in client.get_members(room)
+                          if m.get('account_id') != api_account_id]
+    context['body'] = text
+    context['title'] = title
+    message = force_text(render_to_string('chatwork/message.txt', context))
+
+    return client.add_messages(room, message.strip())
+
+
+def send_chatwork_many(text, rooms, title=None, to_all=None):
+    """ 複数のルームにメッセージを送信する """
+    results = []
+    for room in rooms:
+        result = send_chatwork(text, room, title=title, to_all=to_all)
+        results.append(result)
+    return results
