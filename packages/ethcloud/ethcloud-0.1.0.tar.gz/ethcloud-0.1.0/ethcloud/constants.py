86ae44a86d299d83c103a7ce8c5e755d9a682f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:14 +0000

    Add package ethcloud/0.1.0/ethcloud-0.1.0.tar.gz

---
+++ packages/ethcloud/ethcloud-0.1.0.tar.gz/ethcloud-0.1.0/ethcloud/constants.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+PROVISION_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'provision'
+)
---
+++ packages/ethcloud/ethcloud-0.1.0.tar.gz/ethcloud-0.1.0/setup.py
@@ -0,0 +1,56 @@
+"""
+ A cli tool too launch, manage and use ethereum client on the cloud
+"""
+import os
+import sys
+from setuptools import find_packages, setup
+
+version = '0.1.0'
+dependencies = ['click', 'ansible', 'PyYaml', 'boto3']
+
+if sys.argv[-1] == 'tag':
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push origin master --tags")
+    sys.exit()
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
+    sys.exit()
+
+
+setup(
+    name='ethcloud',
+    version=version,
+    url='https://github.com/kgritesh/ethcloud',
+    license='MIT',
+    author='Ritesh Kadmawala',
+    author_email='k.g.ritesh@gmail.com',
+    description=' A cli tool too launch, manage and use ethereum client on the cloud',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'ethcloud = ethcloud.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
