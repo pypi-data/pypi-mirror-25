f815bd16dd4d450a6cebf93c047c176759330640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:44 +0000

    Add package essential-auth/0.1.1/essential_auth-0.1.1.tar.gz

---
+++ packages/essential-auth/essential_auth-0.1.1.tar.gz/essential_auth-0.1.1/essential_auth/__init__.py
@@ -0,0 +1,5 @@
+from .phrasemetrics import Metrics, PhraseMetrics
+from .essentialauth import EssentialAuth, SessionAssurance, SessionAlreadyExistsException, ProfileAlreadyExistsException, ProfileNotFoundException, LoginAlreadyExistsException
+
+
+
