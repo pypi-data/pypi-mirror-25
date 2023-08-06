7b1f839459a9a7c61307f33732881443dd31c21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:29 +0000

    Add package asn1tools/0.11.0/asn1tools-0.11.0-py2.py3-none-any.whl

---
+++ packages/asn1tools/asn1tools-0.11.0-py2.py3-none-any.whl/asn1tools/__init__.py
@@ -0,0 +1,12 @@
+"""The top level of the ASN.1 tools package contains commonly used
+functions and classes.
+
+"""
+
+__author__ = 'Erik Moqvist'
+__version__ = '0.11.0'
+
+
+from .compiler import compile_dict, compile_string, compile_file
+from .parser import parse_string, parse_file
+from .codecs import EncodeError, DecodeError
