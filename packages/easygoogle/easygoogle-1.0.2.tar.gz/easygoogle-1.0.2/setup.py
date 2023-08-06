a9e5c5c5c62bcb0a7894201821ae3311fec2468f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:05 +0000

    Add package easygoogle/1.0.2/easygoogle-1.0.2.tar.gz

---
+++ packages/easygoogle/easygoogle-1.0.2.tar.gz/easygoogle-1.0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="easygoogle",
+    packages=["easygoogle"],
+    license="GNU General Public License v3.0",
+    version='1.0.2',
+    description="Easy to use wrapper to google APIs client library",
+    package_data={
+        'easygoogle': ['*.pk']
+    },
+    author="Luiz Augusto Ferraz",
+    author_email="adm.fryuni@gmail.com",
+    install_requires=["google-api-python-client (>=1.6.2)"],
+    url="https://github.com/Fryuni/easy_google",
+    download_url='https://github.com/Fryuni/easy_google/archive/1.0.1.tar.gz',
+    keywords="google apis google-apis",
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Intended Audience :: Developers",
+                 "Intended Audience :: Education",
+                 "Intended Audience :: Information Technology",
+                 "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                 "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+                 "Topic :: Utilities"]
+)
