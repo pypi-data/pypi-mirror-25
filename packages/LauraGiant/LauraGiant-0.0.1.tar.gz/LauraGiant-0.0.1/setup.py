52d7783d17bf320b09f27f6da771422807b447bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:32 +0000

    Add package LauraGiant/0.0.1/LauraGiant-0.0.1.tar.gz

---
+++ packages/LauraGiant/LauraGiant-0.0.1.tar.gz/LauraGiant-0.0.1/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='LauraGiant',
+    version='0.0.1',
+    url='https://github.com/brunofisico/laura_gigantes',
+    license='MIT License',
+    author='Bruno Lustosa de Moura ',
+    author_email='bruno.moura@ifrn.edu.br',
+    keywords='Red Giants Analysis Time-Series Asteroseismology',
+    description=u'Código aberto para uso em analise de estrelas gigantes observadas pelo Kepler',
+    packages=find_packages(),
+    install_requires=[],
+    test_suite='test',
+)
