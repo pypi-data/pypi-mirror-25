65a91f8b5fe02ea9e96b000e6b2a87242957a958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:38 +0000

    Add package randomcolor/0.4.4.5/randomcolor-0.4.4.5.tar.gz

---
+++ packages/randomcolor/randomcolor-0.4.4.5.tar.gz/randomcolor-0.4.4.5/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import os
+
+setup(
+    name='randomcolor',
+    version='0.4.4.5',
+    description='For generating attractive random colors',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    keywords='python random color generator',
+    author='Kevin Wu',
+    author_email='me@kevinformatics.com',
+    url='https://github.com/kevinwuhoo/randomcolor-py',
+    packages=['randomcolor'],
+    package_data={'randomcolor': ['lib/colormap.json']},
+    install_requires=[],
+    test_suite="tests",
+)
