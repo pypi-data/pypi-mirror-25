57ac34bbfe32dbbe7e3d89387ea509faf74a9425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:24 +0000

    Add package public_health_cis/0.1.5/public_health_cis-0.1.5.tar.gz

---
+++ packages/public_health_cis/public_health_cis-0.1.5.tar.gz/public_health_cis-0.1.5/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+    name='public_health_cis',
+    packages=['public_health_cis'], # this must be the same as the name above
+    py_modules=['public_health_cis'],
+    version='0.1.5',
+    description='A lib to calculate specific confidence intervals that are used in Public Health',
+    author='Russell Plunkett',
+    author_email='russ.plunkett@gmail.com',
+    url='https://github.com/RustyBrain/PublicHealthCIs.git', # use the URL to the github repo
+    download_url='https://github.com/RustyBrain/PublicHealthCIs/archive/0.1.5.tar.gz', # I'll explain this in a second
+    keywords=['confidence interval', 'wilsons', 'byars', 'statistics'], # arbitrary keywords
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+    ],
+)
