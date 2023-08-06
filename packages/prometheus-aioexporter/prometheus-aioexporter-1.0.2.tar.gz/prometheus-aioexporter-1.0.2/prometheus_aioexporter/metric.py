4e2c9d835635a2939ac1072ddc4f3f1976d31ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:22 +0000

    Add package prometheus-aioexporter/1.0.2/prometheus-aioexporter-1.0.2.tar.gz

---
+++ packages/prometheus-aioexporter/prometheus-aioexporter-1.0.2.tar.gz/prometheus-aioexporter-1.0.2/prometheus_aioexporter/metric.py
@@ -0,0 +1,75 @@
+"""Helpers around prometheus-client to create and register metrics."""
+
+from collections import namedtuple
+
+from prometheus_client import Counter, Gauge, Histogram, Summary
+
+
+# Map metric types to classes and allowed options
+METRIC_TYPES = {
+    'counter': {
+        'class': Counter,
+        'options': {
+            'labels': 'labelnames'}},
+    'gauge': {
+        'class': Gauge,
+        'options': {
+            'labels': 'labelnames'}},
+    'histogram': {
+        'class': Histogram,
+        'options': {
+            'labels': 'labelnames',
+            'buckets': 'buckets'}},
+    'summary': {
+        'class': Summary,
+        'options': {
+            'labels': 'labelnames'}}}
+
+
+class MetricConfig(namedtuple(
+        'MetricConfig', ['name', 'description', 'type', 'config'])):
+    """Configuration for a metric."""
+
+    def __new__(cls, name, description, typ, config):
+        if typ not in METRIC_TYPES:
+            raise InvalidMetricType(name, typ)
+        return super().__new__(cls, name, description, typ, config)
+
+
+class InvalidMetricType(Exception):
+    """Raised when invalid metric type is found."""
+
+    def __init__(self, name, invalid_type):
+        self.name = name
+        self.invalid_type = invalid_type
+        super().__init__(
+            'Invalid type for {}: must be one of {}'.format(
+                self.name, ', '.join(sorted(METRIC_TYPES))))
+
+
+def create_metrics(configs, registry):
+    """Create and register Prometheus metrics from a list of MetricConfigs."""
+    return {
+        config.name: _register_metric(config, registry)
+        for config in configs}
+
+
+def get_registry_metrics(registry):
+    """Return a dict mapping names to metrics from a CollectorRegistry."""
+    metrics = {}
+    for collector in registry._collector_to_names:
+        if hasattr(collector, 'describe'):
+            metrics.update(
+                (metric.name, collector) for metric in collector.describe())
+    return metrics
+
+
+def _register_metric(config, registry):
+    """Register and return a Prometheus metric."""
+    metric_info = METRIC_TYPES[config.type]
+    options = {
+        metric_info['options'][key]: value
+        for key, value in config.config.items()
+        if key in metric_info['options']}
+    return metric_info['class'](
+        config.name, config.description, registry=registry, **options)
---
+++ packages/prometheus-aioexporter/prometheus-aioexporter-1.0.2.tar.gz/prometheus-aioexporter-1.0.2/prometheus_aioexporter/tests/test_metric.py
@@ -0,0 +1,79 @@
+from unittest import TestCase
+
+from prometheus_client import CollectorRegistry, Gauge, Histogram
+
+from ..metric import (
+    MetricConfig,
+    InvalidMetricType,
+    create_metrics,
+    get_registry_metrics
+)
+
+
+class MetricConfigTests(TestCase):
+
+    def test_invalid_metric_type(self):
+        """An invalid metric type raises an error."""
+        with self.assertRaises(InvalidMetricType) as cm:
+            MetricConfig('m1', 'desc1', 'unknown', {})
+        self.assertEqual(
+            str(cm.exception),
+            'Invalid type for m1: must be one of counter, gauge,'
+            ' histogram, summary')
+
+
+class CreateMetricsTests(TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.registry = CollectorRegistry()
+
+    def test_metrics_from_config(self):
+        """Prometheus metrics are created from the specified config."""
+        configs = [
+            MetricConfig('m1', 'desc1', 'counter', {}),
+            MetricConfig('m2', 'desc2', 'histogram', {})]
+        metrics = create_metrics(configs, self.registry)
+        self.assertEqual(len(metrics), 2)
+        self.assertEqual(metrics['m1']._type, 'counter')
+        self.assertEqual(metrics['m2']._type, 'histogram')
+
+    def test_metrics_config(self):
+        """Metric configs are applied."""
+        configs = [
+            MetricConfig('m1', 'desc1', 'histogram', {'buckets': [10, 20]})]
+        metrics = create_metrics(configs, self.registry)
+        # The two specified bucket plus +Inf
+        self.assertEqual(len(metrics['m1']._buckets), 3)
+
+    def test_metrics_config_ignores_unknown(self):
+        """Unknown metric configs are ignored and don't cause an error."""
+        configs = [
+            MetricConfig('m1', 'desc1', 'gauge', {'unknown': 'value'})]
+        metrics = create_metrics(configs, self.registry)
+        self.assertEqual(len(metrics), 1)
+
+
+class GetRegistryMetricsTests(TestCase):
+
+    def test_get_registry_metrics(self):
+        """get_registry_metrics returns a dict with metrics."""
+        registry = CollectorRegistry()
+        metric1 = Gauge('metric1', 'A test gauge', registry=registry)
+        metric2 = Histogram('metric2', 'A test histogram', registry=registry)
+        metrics = get_registry_metrics(registry)
+        self.assertEqual(metrics, {'metric1': metric1, 'metric2': metric2})
+
+    def test_collector_without_describe(self):
+        """Collectors without a describe() method are skipped."""
+
+        class SampleCollector:
+
+            def __init__(self, registry):
+                registry.register(self)
+
+        registry = CollectorRegistry()
+        metric1 = Gauge('metric1', 'A test gauge', registry=registry)
+        SampleCollector(registry)
+        metrics = get_registry_metrics(registry)
+        self.assertEqual(metrics, {'metric1': metric1})
