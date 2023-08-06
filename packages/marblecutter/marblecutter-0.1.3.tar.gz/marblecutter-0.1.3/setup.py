544df7b0f1fafe2c697c1abb43ebc7a1107b871b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:52 +0000

    Add package marblecutter/0.1.3/marblecutter-0.1.3.tar.gz

---
+++ packages/marblecutter/marblecutter-0.1.3.tar.gz/marblecutter-0.1.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+version = '0.1.3'
+
+setup(
+    name='marblecutter',
+    version=version,
+    description='Raster manipulation',
+    url='https://github.com/mojodna/marblecutter',
+    author='Seth Fitzsimmons',
+    author_email='seth@mojodna.net',
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=False,
+    package_data={
+        'marblecutter': ['static/images/*', 'templates/*'],
+    },
+    install_requires=[
+        'haversine',
+        'mercantile',
+        'numpy',
+        'Pillow',
+        'scipy',
+    ],
+    dependency_links=[
+        'https://github.com/mapbox/rasterio/archive/92d5e81.tar.gz#egg=rasterio-1.0a10[s3]'
+    ],
+    extras_require={
+        'color_ramp': 'matplotlib',
+        'postgis': 'psycopg2',
+        'web': [
+            'flask',
+            'flask-cors',
+        ],
+    },
+)
