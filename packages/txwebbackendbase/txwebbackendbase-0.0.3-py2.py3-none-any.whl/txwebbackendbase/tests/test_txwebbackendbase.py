0deea6d224a66ef1ca17a1f5bcdaf13d7efda05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:21 +0000

    Add package txwebbackendbase/0.0.3/txwebbackendbase-0.0.3-py2.py3-none-any.whl

---
+++ packages/txwebbackendbase/txwebbackendbase-0.0.3-py2.py3-none-any.whl/txwebbackendbase/tests/test_txwebbackendbase.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_txwebbackendbase
+----------------------------------
+Tests for `txwebbackendbase` module.
+"""
+
+# import pytest
+
+# from txwebbackendbase import txwebbackendbase
+
+
+class TestTxwebbackendbase(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def test_something(self):
+        pass
