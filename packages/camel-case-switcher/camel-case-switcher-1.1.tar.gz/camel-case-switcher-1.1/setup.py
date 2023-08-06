71620ca6a86f8c87229a5406096b34e9e6a6daa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:17 +0000

    Add package camel-case-switcher/1.1/camel-case-switcher-1.1.tar.gz

---
+++ packages/camel-case-switcher/camel-case-switcher-1.1.tar.gz/camel-case-switcher-1.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup \
+(
+    name='camel-case-switcher',
+    version='1.1',
+    packages=['camel_case_switcher'],
+    test_suite = 'tests',
+    url='https://gitlab.com/Hares/camel-case-switcher',
+    download_url='https://gitlab.com/Hares/camel-case-switcher/repository/master/archive.tar.gz',
+    license='MIT',
+    author='USSX Hares / Peter Zaitcev',
+    author_email='ussx.hares@yandex.ru',
+    description='Python tool for changing style in name of functions etc. from camelCase/CamelCase to the underscope_style.',
+    keywords=['camel_case', 'strings', 'undescope_style'],
+)
