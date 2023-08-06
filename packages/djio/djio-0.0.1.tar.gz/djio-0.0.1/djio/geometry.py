9d518869ab6f0b6660b60461a03db2f46efe49b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:17 +0000

    Add package djio/0.0.1/djio-0.0.1.tar.gz

---
+++ packages/djio/djio-0.0.1.tar.gz/djio-0.0.1/djio/geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. currentmodule:: djio.geometry
+.. moduleauthor:: Pat Daburu <pat@daburu.net>
+
+Working with geometries?  Need help?  Here it is!
+"""
