62037a2b90004d10fac91abed1de5cf55f43727a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:10 +0000

    Add package numerishy/1.7/numerishy-1.7.tar.gz

---
+++ packages/numerishy/numerishy-1.7.tar.gz/numerishy-1.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'numerishy',
+  scripts = ['numerishy'],
+  version = '1.7',
+  description = 'Strongest numbers survive. ',
+  author = 'Jan Kaifer',
+  author_email = 'kaifer741@gmail.com',
+  url = 'https://github.com/jankaifer/numerishy',
+  download_url = 'https://github.com/JanKaifer/Numerishy/archive/v1.7.tar.gz',
+  keywords = ['game', 'curses', 'best', 'must'],
+  classifiers = [],
+)
