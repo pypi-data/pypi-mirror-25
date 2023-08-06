90076739b173b738bcc30eb17bce3915ad0f062d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:30 +0000

    Add package aiozipkin/0.0.1a0/aiozipkin-0.0.1a0.tar.gz

---
+++ packages/aiozipkin/aiozipkin-0.0.1a0.tar.gz/aiozipkin-0.0.1a0/aiozipkin/aiohttp_helpers.py
@@ -0,0 +1,59 @@
+from .helpers import make_context, SERVER, parse_debug, parse_sampled
+
+
+APP_AIOZIPKIN_KEY = "aiozipkin_tracer"
+REQUEST_AIOZIPKIN_KEY = "aiozipkin_span"
+
+PATH_TAG = 'path'
+STATUS_CODE_TAG = 'status_code'
+
+
+def middleware_maker(tracer_key=APP_AIOZIPKIN_KEY,
+                     request_key=REQUEST_AIOZIPKIN_KEY):
+    async def middleware_factory(app, handler):
+        async def aiozipkin_middleware(request):
+            context = make_context(request.headers)
+            tracer = app[tracer_key]
+
+            if context is None:
+                sampled = parse_sampled(request.headers)
+                debug = parse_debug(request.headers)
+                span = tracer.new_trace(sampled=sampled, debug=debug)
+            else:
+                span = tracer.join_span(context)
+
+            with span:
+                span_name = '{0} {1}'.format(request.method, request.path)
+                span.kind(SERVER)
+                span.name(span_name)
+                span.tag(PATH_TAG, request.path)
+                request[request_key] = span
+
+                resp = await handler(request)
+                span.tag(STATUS_CODE_TAG, resp.status)
+                return resp
+
+        return aiozipkin_middleware
+    return middleware_factory
+
+
+def setup(app, tracer,
+          tracer_key=APP_AIOZIPKIN_KEY,
+          request_key=REQUEST_AIOZIPKIN_KEY):
+    app[tracer_key] = tracer
+    app.middlewares.append(middleware_maker(tracer_key, request_key))
+
+    # register cleanup signal to close zipkin connections
+    async def close_aiozipkin(app):
+        app[tracer_key].close()
+    app.on_cleanup.append(close_aiozipkin)
+
+    return app
+
+
+def get_tracer(app, tracer_key=APP_AIOZIPKIN_KEY):
+    return app[tracer_key]
+
+
+def request_span(request, request_key=REQUEST_AIOZIPKIN_KEY):
+    return request[request_key]
---
+++ packages/aiozipkin/aiozipkin-0.0.1a0.tar.gz/aiozipkin-0.0.1a0/aiozipkin/transport.py
@@ -0,0 +1,45 @@
+import asyncio
+import aiohttp
+
+
+class Transport:
+
+    def __init__(self, address, send_inteval=10, loop=None):
+        self._address = address
+        self._session = aiohttp.ClientSession(loop=loop)
+        self._queue = []
+        self._closing = False
+        self._send_interval = send_inteval
+        self._loop = loop
+        self._sender_task = asyncio.ensure_future(
+            self._sender_loop(), loop=loop)
+
+    def send(self, record):
+        print(record)
+        data = record.asdict()
+        self._queue.append(data)
+        # self.http_transport(data)
+
+    async def _sender_loop(self):
+        while not self._closing:
+            if len(self._queue) != 0:
+                payload = self._queue[:]
+                self._queue = []
+                await self._send(payload)
+            await asyncio.sleep(self._send_interval, loop=self._loop)
+
+    async def _send(self, data):
+        # TODO: add retries
+        print(data)
+        async with self._session.post(self._address, json=data) as resp:
+            await resp.read()
+
+    async def close(self):
+        # TODO: make sure queue is empty before closing
+        self._closing = True
+        self._sender_task.cancel()
+        try:
+            await self._sender_task
+        except asyncio.CancelledError:
+            pass
+        await self._session.close()
