31bdabc5be420b582a34d83167d65102e6cf7a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:58 +0000

    Add package confcollect/0.3.1/confcollect-0.3.1.tar.gz

---
+++ packages/confcollect/confcollect-0.3.1.tar.gz/confcollect-0.3.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name="confcollect",
+    description="Helpers to collect configuration from os environment, files etc.",
+    author='Michael Elsdoerfer',
+    author_email='michael@elsdoerfer.com',
+    url='https://github.com/miracle2k/confcollect',
+    version="0.3.1",
+    license='BSD',
+    py_modules=['confcollect'],
+    install_requires=[
+       'six'
+    ],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: BSD License',
+    ]
+)
