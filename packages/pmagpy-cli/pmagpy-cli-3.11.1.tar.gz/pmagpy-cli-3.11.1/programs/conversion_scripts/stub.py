6432ac3956593d7e2040c5567eb29eb633481724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:18 +0000

    Add package pmagpy-cli/3.11.1/pmagpy-cli-3.11.1.tar.gz

---
+++ packages/pmagpy-cli/pmagpy-cli-3.11.1.tar.gz/pmagpy-cli-3.11.1/programs/conversion_scripts/stub.py
@@ -0,0 +1,16 @@
+#/usr/bin/env python
+
+from __future__ import division
+from __future__ import print_function
+from builtins import str
+from past.utils import old_div
+import sys, os
+import numpy as np
+import pmagpy.pmag as pmag
+import pmagpy.new_builder as nb
+import pytz, datetime
+import pandas as pd
+
+if __name__ == "__main__":
+    print('running stub')
+    print(pd)
