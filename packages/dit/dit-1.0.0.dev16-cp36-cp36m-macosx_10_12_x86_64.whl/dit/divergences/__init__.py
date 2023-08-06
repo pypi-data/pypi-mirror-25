642112ce78fe80528421bd0a0729b12de4ac5130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:46 +0000

    Add package dit/1.0.0.dev16/dit-1.0.0.dev16-cp36-cp36m-macosx_10_12_x86_64.whl

---
+++ packages/dit/dit-1.0.0.dev16-cp36-cp36m-macosx_10_12_x86_64.whl/dit/divergences/__init__.py
@@ -0,0 +1,28 @@
+"""
+Divergences, measuring the distance between distributions. They are not
+necessarily true metrics, but some are.
+"""
+
+from .jensen_shannon_divergence import (
+    jensen_shannon_divergence,
+)
+
+from .cross_entropy import (
+    cross_entropy,
+)
+
+from .kullback_leibler_divergence import (
+    kullback_leibler_divergence,
+    relative_entropy,
+)
+
+from .generalized_divergences import (
+    alpha_divergence,
+    hellinger_divergence,
+    renyi_divergence,
+    tsallis_divergence,
+    f_divergence,
+    hellinger_sum,
+)
+
+from . import pmf
