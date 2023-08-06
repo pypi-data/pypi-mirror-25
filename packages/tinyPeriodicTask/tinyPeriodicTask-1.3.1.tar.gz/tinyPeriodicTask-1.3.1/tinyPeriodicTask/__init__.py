0334e11c4d450a8903a9e348d5a2f71290cc2ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:18 +0000

    Add package tinyPeriodicTask/1.3.1/tinyPeriodicTask-1.3.1.tar.gz

---
+++ packages/tinyPeriodicTask/tinyPeriodicTask-1.3.1.tar.gz/tinyPeriodicTask-1.3.1/tinyPeriodicTask/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from .TinyPeriodicTask import TinyPeriodicTask
+
+__author__ = "Frederick Lussier <frederick.lussier@hotmail.com>"
+__status__ = "dev"
+__version__ = "1.3.1"
+__date__ = "september 30th 2017"
+
+__all__ = ["TinyPeriodicTask"]
