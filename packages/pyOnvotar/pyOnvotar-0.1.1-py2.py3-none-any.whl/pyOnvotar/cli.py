c4e02629096d07b7522035ee9cc95b94fa785338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:56 +0000

    Add package pyOnvotar/0.1.1/pyOnvotar-0.1.1-py2.py3-none-any.whl

---
+++ packages/pyOnvotar/pyOnvotar-0.1.1-py2.py3-none-any.whl/pyOnvotar/cli.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for pyOnvotar."""
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for pyOnvotar."""
+    click.echo("Replace this message by putting your code into "
+               "pyOnvotar.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+
+
+if __name__ == "__main__":
+    main()
