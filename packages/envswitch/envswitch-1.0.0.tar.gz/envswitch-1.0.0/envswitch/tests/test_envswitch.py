9943a211fd6f4b66d1b06306c84910cbf1673f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:47 +0000

    Add package envswitch/1.0.0/envswitch-1.0.0.tar.gz

---
+++ packages/envswitch/envswitch-1.0.0.tar.gz/envswitch-1.0.0/envswitch/tests/test_envswitch.py
@@ -0,0 +1,41 @@
+import os
+from os import getenv
+
+import envswitch as es
+
+
+def test_env_switch():
+    """
+    reads the value for the http_proxy environment variable, replaces it with 'blah', checks that it has been set
+    correctly, and sets it back to initial value.
+    :return:
+    """
+
+    # this environment variable needs to have been set before the test runs
+    env_var = 'FOO'
+
+    # (1) get and save initial value
+    init_val = getenv(env_var)
+
+    # print the initial value
+    print(env_var + '=' + init_val)
+    es.print_external_env_var(env_var)
+
+    # assert getting from external provides the same value
+    assert es.get_external_env_var(env_var) == init_val
+
+    # (2) set a new value permanently
+    es.set_env_permanently(env_var, 'blah')
+
+    # print the changed value and assert
+    es.print_external_env_var(env_var)
+    assert es.get_external_env_var(env_var) == 'blah'
+    # TODO later version
+    # assert os.getenv(env_http_proxy) == 'blah'
+
+    # (3) set to initial value permanently
+    es.set_env_permanently(env_var, init_val)
+
+    # get and assert
+    es.print_external_env_var(env_var)
+    assert es.get_external_env_var(env_var) == init_val
---
+++ packages/envswitch/envswitch-1.0.0.tar.gz/envswitch-1.0.0/setup_cx_app.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+
+import os
+import importlib.util
+import sys
+# DO NOT REMOVE setuptools IMPORT: IT PREVENTS AN ERROR SEE https://github.com/anthony-tuininga/cx_Freeze/issues/308
+import setuptools
+from cx_app_unpack_eggs import unpackEgg
+from envswitch.utils import version_file_cx_freeze
+from setuptools_scm import version_from_scm
+from cx_Freeze import setup, Executable
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QLibraryInfo
+
+# To build
+# - the executable distribution, do:  python setup_cx_app.py build   >> result is in build/exe.<target>/
+# - the msi installer, do: python setup_cx_app.py bdist_msi          >> result is in dist/envswitch-<ver>-<target>.msi
+
+# First import setup.py so that we can get all description, metadata, etc.
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+print('Importing definitions from setup.py...')
+spec = importlib.util.spec_from_file_location("setup", os.path.join(THIS_DIR, "setup.py"))
+setup_py = importlib.util.module_from_spec(spec)
+tmp = sys.argv  # temporarily store the sys.argv to replace them during this call
+sys.argv = [tmp[0].replace('setup_cx_app.py', 'setup.py'), '--quiet', '--dry-run', 'clean']  # performs nothing, simply loads the variables defined in the script
+spec.loader.exec_module(setup_py)
+print('DONE, now executing setup_cx_app.py...')
+sys.argv = tmp
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+# *** find the folder containing the qt platform plugins ****
+# -- Create a dummy app in order to load the appropriate qt configuration for the system.
+app = QApplication(sys.argv)
+plugins_path = QLibraryInfo.location(QLibraryInfo.PluginsPath)
+qt_platforms_folder = os.path.join(plugins_path, 'platforms')
+# unfortunately for some reason the dll files from while the one from Anaconda/Library/plugins/platforms work,
+# so we included the latter in the sources just in case..
+qt_platforms_folder = os.path.join(THIS_DIR, 'qt_resources', 'platforms')
+
+# # you should pip install -e . before running this
+# import pkg_resources  # part of setuptools
+# _app_version = pkg_resources.require("envswitch")[0].version
+# we have to manually create a version number compliant with cx_Freeze (no letters, just numbers)
+my_version = version_from_scm(THIS_DIR)
+THIS_TAG_OR_NEXT_TAG_VERSION = my_version.format_with("{tag}")
+with open('./' + version_file_cx_freeze, 'wt') as f:
+    f.write(THIS_TAG_OR_NEXT_TAG_VERSION)
+
+# Dependencies are automatically detected, but it might need fine tuning.
+# unpackEgg('setuptools', 'eggs_tmp')  # setuptools contains 'pkg_resources'
+options = {
+    # see http://cx-freeze.readthedocs.io/en/latest/distutils.html#build-exe
+    'build_exe': {
+        'includes': [],
+        'packages': ['os'],  #, 'pkg_resources'],
+        'excludes': ['*'],
+        "include_files": [# (os.path.join(THIS_DIR, 'qt_resources', 'qt.conf'), 'qt.conf'),  not needed, default is ok
+                          (qt_platforms_folder, 'platforms'),  # in order to override the one gathered by cx_Freeze
+                          'LICENSE',
+                          '_TMP_VERSION_',
+                          'README.md'],  # relative paths only
+        # "include_msvcr"=True
+        # 'path': sys.path + ['eggs_tmp']
+    }
+}
+
+executables = [
+    # see http://cx-freeze.readthedocs.io/en/latest/distutils.html#cx-freeze-executable
+    Executable(script='envswitch/gui.py',
+               # initScript= (executed before script)
+               targetName='envswitch.exe',
+               base=base,
+               # icon=
+               )
+]
+
+import PyQt5
+
+setup(name=setup_py.DISTNAME,
+      description=setup_py.DESCRIPTION,
+      long_description=setup_py.LONG_DESCRIPTION,
+
+      # Versions should comply with PEP440.  For a discussion on single-sourcing
+      # the version across setup_py.py and the project code, see
+      # https://packaging.python.org/en/latest/single_source_version.html
+      # version=setup_py.THIS_TAG_OR_NEXT_TAG_VERSION, NOW HANDLED BY GIT
+
+      maintainer=setup_py.MAINTAINER,
+      maintainer_email=setup_py.MAINTAINER_EMAIL,
+
+      license=setup_py.LICENSE,
+      url=setup_py.URL,
+      download_url=setup_py.DOWNLOAD_URL,
+
+      # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 4 - Beta',
+
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Topic :: Desktop Environment',
+          # 'Topic :: Software Development :: Libraries :: Python Modules',
+
+          # Pick your license as you wish (should match "license" above)
+          setup_py.LICENSE_LONG,
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          # 'Programming Language :: Python :: 2',
+          # 'Programming Language :: Python :: 2.6',
+          # 'Programming Language :: Python :: 2.7',
+          # 'Programming Language :: Python :: 3',
+          # 'Programming Language :: Python :: 3.3',
+          # 'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+
+      # What does your project relate to?
+      keywords=setup_py.KEYWORDS,
+
+      # setuptools_scm to detect git version. Unfortunately this does not work with cx_Freeze (unsupported dev)
+      # use_scm_version=True,
+      version=THIS_TAG_OR_NEXT_TAG_VERSION,
+
+      # cx_Freeze specific
+      options=options,
+      executables=executables,
+      )
