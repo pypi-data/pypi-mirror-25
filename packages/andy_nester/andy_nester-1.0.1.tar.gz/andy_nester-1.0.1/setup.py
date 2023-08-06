d76d23013770878c48aa70c6f6f35308c4aa048c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:16 +0000

    Add package andy_nester/1.0.1/andy_nester-1.0.1.tar.gz

---
+++ packages/andy_nester/andy_nester-1.0.1.tar.gz/andy_nester-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name       = 'andy_nester',
+    version    = '1.0.1',
+    py_modules = ['andy_nester'],
+    author     = 'hfpython',
+    author_email = 'neolithe@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+)
