957fc04a39f65131672da94ebd4bae6ae6ccc051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:43 +0000

    Add package pcart-basic-theme/2.1/pcart-basic-theme-2.1.tar.gz

---
+++ packages/pcart-basic-theme/pcart-basic-theme-2.1.tar.gz/pcart-basic-theme-2.1/pcart_basic_theme/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'pcart_basic_theme.apps.PCartBasicThemeConfig'
---
+++ packages/pcart-basic-theme/pcart-basic-theme-2.1.tar.gz/pcart-basic-theme-2.1/pcart_basic_theme/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class PCartBasicThemeConfig(AppConfig):
+    name = 'pcart_basic_theme'
+    verbose_name = _('Basic theme')
