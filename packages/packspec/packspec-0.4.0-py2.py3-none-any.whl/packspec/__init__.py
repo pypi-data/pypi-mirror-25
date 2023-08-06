57207e11cc815e9e4ed9f1e5c41ab2af7f83b853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:31 +0000

    Add package packspec/0.4.0/packspec-0.4.0-py2.py3-none-any.whl

---
+++ packages/packspec/packspec-0.4.0-py2.py3-none-any.whl/packspec/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+# Version
+
+import io
+import os
+__version__ = io.open(
+    os.path.join(os.path.dirname(__file__), 'VERSION'),
+    encoding='utf-8').read().strip()
