6a2d718f8031539d69ef367ebc5ab16557e7b2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:49 +0000

    Add package django-error-report/0.1.6/django-error-report-0.1.6.tar.gz

---
+++ packages/django-error-report/django-error-report-0.1.6.tar.gz/django-error-report-0.1.6/error_report/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+from models import Error
+
+
+class ErrorAdmin(admin.ModelAdmin):
+    list_display = ('path', 'kind', 'info', 'when')
+    list_display_links = ('path',)
+    ordering = ('-id',)
+    search_fields = ('path', 'kind', 'info', 'data')
+    readonly_fields = ('path', 'kind', 'info', 'data', 'when', 'html_iframe')
+    fieldsets = (
+        (None, {
+            'fields': ('kind', 'path', 'info', 'when', 'data', 'html_iframe')
+        }),
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        """
+        Disabling the delete permissions
+        """
+        return True
+
+    def has_add_permission(self, request):
+        """
+        Disabling the create permissions
+        """
+        return False
+
+admin.site.register(Error, ErrorAdmin)
