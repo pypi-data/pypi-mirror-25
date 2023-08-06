c44b7814f72edc10fcd5d26d474f832dfabe9f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:27 +0000

    Add package moxel-http-driver/0.0.1.post24/moxel-http-driver-0.0.1.post24.tar.gz

---
+++ packages/moxel-http-driver/moxel-http-driver-0.0.1.post24.tar.gz/moxel-http-driver-0.0.1.post24/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='moxel-http-driver',
+      version='0.0.1.post24',
+      description='generic http driver for moxel models',
+      url='http://moxel.ai',
+      author='moxel',
+      author_email='support@moxel.ai',
+      license='MIT',
+      zip_safe=False,
+      packages=['moxel_http_driver'],
+      entry_points={
+          "console_scripts": [
+              "moxel-http-driver = moxel_http_driver.driver:main",
+          ]
+      }
+)
