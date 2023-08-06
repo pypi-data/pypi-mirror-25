f2f3c34839e68fbec30f11363ede400125365b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:07 +0000

    Add package ttnmqtt/0.7.0/ttnmqtt-0.7.0.tar.gz

---
+++ packages/ttnmqtt/ttnmqtt-0.7.0.tar.gz/ttnmqtt-0.7.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='ttnmqtt',
+      version='0.7.0',
+      description='small package to make mqtt connection to ttn',
+      author='Emmanuelle Lejeail',
+      author_email='manu.lejeail@gmail.com',
+      license='MIT',
+      packages=['ttnmqtt'],
+      install_requires=[
+          'paho-mqtt',
+          'pydispatch',
+      ],
+      zip_safe=False)
