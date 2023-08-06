217307047e4e9193450a8cd0d2cb05be394e4e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:20 +0000

    Add package pipsearch/2/pipsearch-2.tar.gz

---
+++ packages/pipsearch/pipsearch-2.tar.gz/pipsearch-2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name="pipsearch",
+	  version="2",
+	  description="Search pypi modules",
+	  url="https://github.com/biggydbs/pipsearch",
+	  author="Hitesh Jain",
+	  author_email="jain.hitesh30695@gmail.com",
+	  packages=["pipsearch"],
+	  scripts=["bin/api"],
+	  download_url="https://github.com/biggydbs/pipsearch/archive/1.tar.gz",
+	  install_requires=['Click'],
+	  py_modules=["bin/api"],
+	  entry_points='''
+        [console_scripts]
+        pipsearch=api:search
+    	''',
+	  zip_safe=False)
