b2e46bd82d464f4afac1d6ea55cf67e7a388857b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:41 +0000

    Add package ebcli/4.0.17/ebcli-4.0.17.tar.gz

---
+++ packages/ebcli/ebcli-4.0.17.tar.gz/ebcli-4.0.17/setup.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+from setuptools import setup
+from setuptools import find_packages
+
+read_me = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(read_me, 'README.rst')) as f:
+    README = f.read()
+
+REQUIREMENTS = [
+    'Jinja2 >=2.8',
+    'keyring >= 8.0, <=9.0',
+    'PyYAML >= 3.11',
+    'requests >= 2.8.1',
+    'keyrings.alt >= 1.1'
+]
+
+if sys.version_info[:2] == (2, 6):
+    # For python2.6 we have to require argparse since it
+    # was not in stdlib until 2.7.
+    REQUIREMENTS.append('argparse >= 1.1')
+
+
+setup(
+    name='ebcli',
+    version='4.0.17',
+    description='API Client library for ElasticBox',
+    long_description=README,
+    author='ElasticBox Inc',
+    author_email='operations@elasticbox.com',
+    url='https://cam.ctl.io',
+    packages=find_packages(),
+    package_data={
+        'ebcli': [
+            'build/*.jinja'
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'ebcli=ebcli.entrypoint:main'
+        ]
+    },
+    install_requires=REQUIREMENTS,
+    license='Apache License 2.0',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
