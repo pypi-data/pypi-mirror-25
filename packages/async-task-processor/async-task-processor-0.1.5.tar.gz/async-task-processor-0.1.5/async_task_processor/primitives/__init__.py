e838f2c3b3cf7a634fe45dda3c907b20a8939476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:12 +0000

    Add package async-task-processor/0.1.5/async-task-processor-0.1.5.tar.gz

---
+++ packages/async-task-processor/async-task-processor-0.1.5.tar.gz/async-task-processor-0.1.5/async_task_processor/primitives/__init__.py
@@ -0,0 +1,4 @@
+from .task import *
+from .tarantool_task import *
+
+__all__ = ['Task', 'TarantoolTask']
---
+++ packages/async-task-processor/async-task-processor-0.1.5.tar.gz/async-task-processor-0.1.5/async_task_processor/processors/__init__.py
@@ -0,0 +1,5 @@
+from .simple_processor import SimpleProcessor
+from .periodic_processor import PeriodicProcessor
+from .tarantool_processor import TarantoolProcessor
+
+__all__ = ['SimpleProcessor', 'TarantoolProcessor', 'PeriodicProcessor']
