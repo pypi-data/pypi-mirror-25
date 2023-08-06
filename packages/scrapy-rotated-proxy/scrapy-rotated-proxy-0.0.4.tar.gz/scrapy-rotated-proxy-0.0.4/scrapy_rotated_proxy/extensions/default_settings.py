f992d84b89ecf764913cee99a39bdf092ff63380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:54 +0000

    Add package scrapy-rotated-proxy/0.0.4/scrapy-rotated-proxy-0.0.4.tar.gz

---
+++ packages/scrapy-rotated-proxy/scrapy-rotated-proxy-0.0.4.tar.gz/scrapy-rotated-proxy-0.0.4/scrapy_rotated_proxy/extensions/default_settings.py
@@ -0,0 +1,25 @@
+# DOWNLOADER_MIDDLEWARES = {}
+
+PROXY_STORAGE = 'scrapy_rotated_proxy.extensions.file_storage.FileProxyStorage'
+PROXY_FILE_PATH = ''
+# PROXY_STORAGE = 'scrapy_rotated_proxy.extensions.mongodb_storage.MongoDBProxyStorage'
+PROXY_MONGODB_STORAGE_URI = 'mongodb://127.0.0.1:27017'
+PROXY_MONGODB_STORAGE_DB = 'vps_management'
+PROXY_MONGODB_STORAGE_COLL = 'service'
+PROXY_MONGODB_STORAGE_COLL_INDEX = []
+# DOWNLOADER_MIDDLEWARES.update({
+#     'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': None,
+#     'scrapy_rotated_proxy.downloadmiddlwares.proxy.RotatedProxy': 750,
+# })
+
+
+# settings_proxy.txt
+# {
+#     "HTTP_PROXIES": [
+#         "http://host:ip"
+#     ],
+#     "HTTPS_PROXIES": [
+#         "http://auth@host:ip"
+#     ]
+# }
+
