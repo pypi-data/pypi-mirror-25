e16b6803be82263e19893e2c1f8e6c6daca31b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:05 +0000

    Add package hotfeng-test/1.3.0/hotfeng_test-1.3.0.tar.gz

---
+++ packages/hotfeng-test/hotfeng_test-1.3.0.tar.gz/hotfeng_test-1.3.0/hotfeng_test.py
@@ -0,0 +1,11 @@
+"""这是hotfeng_test.py模块，提供了一个名为print_lol()的函数
+这个函数作用是打印列表，其中有可能包含嵌套列表"""
+def print_lol(the_list, indent=False, level=0):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item, indent, level+1)
+		else:
+			if indent:
+				for tab_stop in range(level):
+					print('\t', end='')
+			print(each_item)
