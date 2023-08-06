056d7a93a82a72ec2070af8640074b9565117203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:59 +0000

    Add package pydotexe/1.0.0/pydotexe-1.0.0.tar.gz

---
+++ packages/pydotexe/pydotexe-1.0.0.tar.gz/pydotexe-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="pydotexe",
+    version="1.0.0",
+    url='https://github.com/betacode-project/pyDotexe',
+    author='betacode_',
+    author_email='betacode.net@gmail.com',
+    maintainer='betacode_',
+    maintainer_email='betacode.net@gmail.com',
+    description='pyDotexe is a program that freeze Python programs into stand-alone application tool.',
+    packages=["pydotexe"],
+    license="MIT",
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Build Tools', 
+        'License :: OSI Approved :: MIT License',
+    ]
+)
