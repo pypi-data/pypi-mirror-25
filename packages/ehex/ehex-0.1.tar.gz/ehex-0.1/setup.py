fab78d7f64df8482d997bb956bab9f88a9523dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:23 +0000

    Add package ehex/0.1/ehex-0.1.tar.gz

---
+++ packages/ehex/ehex-0.1.tar.gz/ehex-0.1/setup.py
@@ -0,0 +1,13 @@
+'''setup.py for ehex'''
+
+from setuptools import setup
+
+setup(name='ehex',
+      version='0.1',
+      description='eHex module (from Traveller/Cepheus System)',
+      url='http://github.com/storborg/funniest',
+      author='Arthur Green',
+      author_email='arthur.green045@gmail.com',
+      license='MIT',
+      packages=['ehex'],
+      zip_safe=False)
