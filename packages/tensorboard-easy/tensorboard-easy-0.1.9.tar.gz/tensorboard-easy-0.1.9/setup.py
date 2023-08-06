86c4cd34f58398663c0dd24e29f731cb9bac9377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:43 +0000

    Add package tensorboard-easy/0.1.9/tensorboard-easy-0.1.9.tar.gz

---
+++ packages/tensorboard-easy/tensorboard-easy-0.1.9.tar.gz/tensorboard-easy-0.1.9/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='tensorboard-easy',
+    packages=['tensorboard_easy'],
+    include_package_data=True,
+    version='0.1.9',
+    description='A tensorflow-independent tensorboard logger',
+    author='maxme1',
+    author_email='maxs987@gmail.com',
+    license='MIT',
+    url='https://github.com/maxme1/tensorboard-easy',
+    download_url='https://github.com/maxme1/tensorboard-easy/archive/0.1.9.tar.gz',
+    keywords=[
+        'tensorboard', 'logging'
+    ],
+    classifiers=[],
+    install_requires=[
+        'Pillow>=4.3',
+        'protobuf>=3.4',
+        'crccheck>=0.6',
+        'numpy>=1.11',
+    ]
+)
