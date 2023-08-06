4585c116c418f6bf9bb919bed5f18b1532885e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:27 +0000

    Add package diogobaeder.webfaction/0.0.2/diogobaeder.webfaction-0.0.2.tar.gz

---
+++ packages/diogobaeder.webfaction/diogobaeder.webfaction-0.0.2.tar.gz/diogobaeder.webfaction-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='diogobaeder.webfaction',
+    use_scm_version=True,
+    description="Tools to help with deploying to Webfaction",
+    long_description='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='diogobaeder webfaction python django',
+    author='Diogo Baeder',
+    author_email='diogobaeder@yahoo.com.br',
+    url='https://github.com/diogobaeder/diogobaeder.webfaction',
+    license='BSD 2-Clause License',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'cryptography',
+        'Fabric',
+        'keyring',
+        'keyrings.alt',
+        'SecretStorage',
+        'setuptools_scm',
+        'wfcli',
+    ],
+    namespace_packages=['diogobaeder'],
+    entry_points='',
+)
