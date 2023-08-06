4e7748fa4c79d2a9c4eb0936684ac6996949aa06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:16 +0000

    Add package pycrutils/0.0.0.dev2/pycrutils-0.0.0.dev2-py3-none-any.whl

---
+++ packages/pycrutils/pycrutils-0.0.0.dev2-py3-none-any.whl/pycrutils/__init__.py
@@ -0,0 +1,2 @@
+'''This package contains a set of tools for application development'''
+__all__ = []
---
+++ packages/pycrutils/pycrutils-0.0.0.dev2-py3-none-any.whl/pycrutils/streams/__init__.py
@@ -0,0 +1,4 @@
+'''This package contains the pycrutils stream implementation'''
+__all__ = ['streams']
+
+from .streams import Stream, IStream, OStream, IOStream
