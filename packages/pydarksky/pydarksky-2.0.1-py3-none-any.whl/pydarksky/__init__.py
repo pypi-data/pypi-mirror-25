44a62d2fb03d0eec3d02483f27307f3a79b64ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:21 +0000

    Add package pydarksky/2.0.1/pydarksky-2.0.1-py3-none-any.whl

---
+++ packages/pydarksky/pydarksky-2.0.1-py3-none-any.whl/pydarksky/__init__.py
@@ -0,0 +1,21 @@
+import logging
+
+from .darksky import DarkSky
+from .darksky import weather
+from .weather import Weather
+from .weather import DataBlock
+from .weather import Now
+from .weather import Day
+from .weather import Hour
+from .weather import Minute
+from .weather import Alert
+from .weather import Flag
+
+from .weather import NoDataError
+from .weather import RequiredDataError
+
+__version__ = "2.0.1"
+__status__ = "Release"
+
+logging.getLogger(__name__).setLevel(logging.DEBUG)
+logging.getLogger(__name__).addHandler(logging.NullHandler())
