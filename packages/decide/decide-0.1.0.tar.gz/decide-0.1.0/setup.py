01385159d491fc0fc655bfdcb890e2e237fad627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:13 +0000

    Add package decide/0.1.0/decide-0.1.0.tar.gz

---
+++ packages/decide/decide-0.1.0.tar.gz/decide-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name='decide',
+  packages=['decide'],
+  version='0.1.0',
+  description='decide',
+  author='Jordan Suchow',
+  author_email='jwsuchow@gmail.com',
+  url='https://suchow.com/',
+)
