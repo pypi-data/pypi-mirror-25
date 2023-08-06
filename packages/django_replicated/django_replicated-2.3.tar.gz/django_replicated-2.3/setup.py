f98d0d8f2dd5feec10ab58be222f0e839e090ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:24 +0000

    Add package django_replicated/2.3/django_replicated-2.3.tar.gz

---
+++ packages/django_replicated/django_replicated-2.3.tar.gz/django_replicated-2.3/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='django_replicated',
+    version='2.3',
+    description='Django DB router for stateful master-slave replication',
+    packages=[
+        'django_replicated',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
