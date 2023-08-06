585c4a2ce2b8688687cc58c64253f4df41f794da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:03 +0000

    Add package trailblazer/2.0.0/trailblazer-2.0.0-py2.py3-none-any.whl

---
+++ packages/trailblazer/trailblazer-2.0.0-py2.py3-none-any.whl/trailblazer-2.0.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,12 @@
+
+Trailblazer is a tool to manage and track state of analyses.
+
+You can install it from PyPI as follows::
+    pip install trailblazer
+
+For more detailed instructions, run :code:`trailblazer --help`
+
+See the GitHub repo for documentation:
+`Clinical-Genomics/analysis <https://github.com/Clinical-Genomics/analysis>`_
+
+
---
+++ packages/trailblazer/trailblazer-2.0.0-py2.py3-none-any.whl/trailblazer/add/utils.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from trailblazer.store import Analysis
+
+FINISHED_STATUSES = ('finished', 'archived', 'archiving')
+
+
+def same_entry(entry: Analysis, other_entry: Analysis) -> bool:
+    """Compare two analysis entries if they are the same."""
+    statuses = set()
+    steps = set()
+    for aentry in [entry, other_entry]:
+        steps.add('na' if aentry.failed_step is None else aentry.failed_step)
+        # collapse into "failed" status
+        statuses.add('failed' if aentry.status in ('error', 'canceled') else
+                     aentry.status)
+
+    if entry.case_id != other_entry.case_id:
+        return False
+    elif entry.started_at != other_entry.started_at:
+        return False
+    elif len(statuses) > 1:
+        return False
+    elif len(steps) > 1:
+        return False
+    else:
+        return True
+
+
+def is_latest_mip(sampleinfo: dict) -> bool:
+    """Check if the analysis is up to date."""
+    version = sampleinfo.get('mip_version')
+    return version is not None and version.startswith('v4.')
