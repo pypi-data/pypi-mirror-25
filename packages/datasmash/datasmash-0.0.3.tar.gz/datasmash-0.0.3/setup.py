a75a7000b7e01e1e3b5f7acd4b03034a0f99c0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:12 +0000

    Add package datasmash/0.0.3/datasmash-0.0.3.tar.gz

---
+++ packages/datasmash/datasmash-0.0.3.tar.gz/datasmash-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='datasmash',
+      version='0.0.3',
+      packages=['datasmash.bin', 'datasmash',
+                'datasmash.primitive_interfaces',
+                'datasmash.primitive_interfaces.types'],
+      install_requires=['pandas', 'numpy', 'scikit-learn'],
+
+      # metadata for PyPI upload
+      url='https://gitlab.datadrivendiscovery.org/uchicago/datasmash',
+      download_url='https://gitlab.datadrivendiscovery.org/uchicago/datasmash/archive/0.0.3.tar.gz',
+
+      maintainer_email='warrenmo@uchicago.edu',
+      maintainer='Warren Mo',
+
+      description='Quantifier of universal similarity amongst arbitrary data streams without a priori knowledge, features, or training.',
+
+      classifiers=[
+          "Programming Language :: Python :: 3"
+      ]
+)
