26fa17a37992f5280ebb68ea795d7ba94fa6162b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:28 +0000

    Add package sunpy-sphinx-theme/1.0.7/sunpy-sphinx-theme-1.0.7.tar.gz

---
+++ packages/sunpy-sphinx-theme/sunpy-sphinx-theme-1.0.7.tar.gz/sunpy-sphinx-theme-1.0.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sunpy-sphinx-theme",
+    version="1.0.7",
+    use_2to3=False,
+    description="The sphinx theme for the SunPy website and documentation.",
+    long_description="",
+    author="The SunPy Developers",
+    install_requires=[
+        "setuptools",
+        "sphinx",
+        "sphinx-bootstrap-theme"
+    ],
+    packages=['sunpy_sphinx_theme'],
+    include_package_data=True,
+)
---
+++ packages/sunpy-sphinx-theme/sunpy-sphinx-theme-1.0.7.tar.gz/sunpy-sphinx-theme-1.0.7/sunpy_sphinx_theme/conf.py
@@ -0,0 +1,22 @@
+import os
+try:
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urljoin
+from . import get_html_theme_path
+
+html_theme_path = get_html_theme_path()
+html_theme = "sunpy"
+html_favicon = os.path.join(html_theme_path[0], html_theme, "static", "img", "favicon-32.ico")
+
+def page_url(page):
+    sunpy_website_url_base = "http://sunpy.org/"
+    return urljoin(sunpy_website_url_base, page)
+
+html_theme_options = {
+    'navbar_links': [
+        ("Support Us", page_url("contribute.html"), 1),
+        ("Get Help", page_url("help.html"), 1),
+        ("SunPy Project", page_url("team.html"), 1),
+    ]
+}
