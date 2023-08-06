ffd681bdb0ce4803fb35a70abba6e0b3028b6974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:50 +0000

    Add package stmbplus-thinclient-primitive/1.0/stmbplus_thinclient_primitive-1.0.tar.gz

---
+++ packages/stmbplus-thinclient-primitive/stmbplus_thinclient_primitive-1.0.tar.gz/stmbplus_thinclient_primitive-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+	name='stmbplus_thinclient_primitive',  # This is the name of your PyPI-package. 
+	version= '1.0',  # Update the version number for new releases
+	author= 'Keyi Liu',
+	packages=['stmbplus_thinclient_primitive']
+)
