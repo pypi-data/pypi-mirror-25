dea2a3f1dfe1ecc45c6cbd33760286ac77113fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:27 +0000

    Add package plasmapy/0.1.dev1/plasmapy-0.1.dev1.tar.gz

---
+++ packages/plasmapy/plasmapy-0.1.dev1.tar.gz/plasmapy-0.1.dev1/plasmapy/__init__.py
@@ -0,0 +1,20 @@
+from ._metadata import (
+    name as __name__,
+    version as __version__,
+    description as __doc__,
+    author as __author__,
+)
+
+from .classes import Plasma
+from . import classes
+from . import constants
+from . import atomic
+from . import math
+from . import physics
+from . import utils
+
+import sys
+import warnings
+
+if sys.version_info[:2] < (3, 6):  # coveralls: ignore
+    warnings.warn("PlasmaPy does not support Python 3.5 and below")
