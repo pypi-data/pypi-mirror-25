e3c4e3115da8c8f166469baa8eed13fcab1a49af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:08 +0000

    Add package pyshifu/0.0.4/pyshifu-0.0.4-py2.py3-none-any.whl

---
+++ packages/pyshifu/pyshifu-0.0.4-py2.py3-none-any.whl/shifu/core/enums.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+class Platform(object):
+    WINDOWS, LINUX, MAC = range(3)
+
+    @staticmethod
+    def contain(_platform):
+        if _platform == Platform.WINDOWS or _platform == Platform.LINUX or _platform == Platform.MAC:
+            return True
+        else:
+            return False
+
+
+class Step(object):
+    NEW, INIT, STATS, NORMALIZE, VARSELECT, TRAIN, POSTTRAIN, EVAL = range(8)
+
+
+class CommandRunningStatus(object):
+    SUCCESS, FAILED = range(2)
