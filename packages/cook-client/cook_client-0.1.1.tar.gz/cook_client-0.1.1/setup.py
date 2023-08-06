bc4044db5d6d4c3624aa7f5ae43d71aa90269f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:50 +0000

    Add package cook-client/0.1.1/cook_client-0.1.1.tar.gz

---
+++ packages/cook-client/cook_client-0.1.1.tar.gz/cook_client-0.1.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+requirements = [
+    'arrow',
+    'blessings',
+    'humanfriendly',
+    'requests',
+    'tabulate'
+]
+
+test_requirements = [
+    'nose',
+    'requests-mock'
+]
+
+setup(
+    name='cook_client',
+    version='0.1.1',
+    description="Two Sigma's Cook CLI",
+    long_description="This package contains Two Sigma's Cook Scheduler command line interface, cs. cs allows you to "
+                     "submit jobs and view jobs, job instances, and job groups across multiple Cook clusters.",
+    packages=['cook', 'cook.subcommands'],
+    entry_points={'console_scripts': ['cs = cook.__main__:main']},
+    install_requires=requirements,
+    tests_require=test_requirements
+)
