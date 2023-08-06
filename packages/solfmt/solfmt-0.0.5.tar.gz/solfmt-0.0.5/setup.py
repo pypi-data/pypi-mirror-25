ea64555922a277aaa042a6aed8606122839f31e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:10 +0000

    Add package solfmt/0.0.5/solfmt-0.0.5.tar.gz

---
+++ packages/solfmt/solfmt-0.0.5.tar.gz/solfmt-0.0.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from solfmt import version
+try:
+    long_description = open("Readme.md").read()
+except IOError:
+    long_description = """
+"""
+
+setup(
+    name="solfmt",
+    version=version,
+    description="Solfmt a code formatter for Solidity",
+    license="MIT",
+    author="Alex Myasoedov",
+    author_email="msoedov@gmail.com",
+    packages=['.'],
+    install_requires=['fire'],
+    entry_points={
+        'console_scripts': ['solfmt=solfmt:entrypoint'],
+    },
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ])
