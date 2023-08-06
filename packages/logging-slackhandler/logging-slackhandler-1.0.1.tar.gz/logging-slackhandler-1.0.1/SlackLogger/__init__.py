d18527d8498929180c9d9507851adcca71228e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:48 +0000

    Add package logging-slackhandler/1.0.1/logging-slackhandler-1.0.1.tar.gz

---
+++ packages/logging-slackhandler/logging-slackhandler-1.0.1.tar.gz/logging-slackhandler-1.0.1/SlackLogger/__init__.py
@@ -0,0 +1,54 @@
+from logging import Handler, Formatter
+from requests_futures.sessions import FuturesSession
+from SlackLogger.version import get_version
+
+__version__ = get_version()
+
+LEVEL_TO_COLOR = {
+    'DEBUG': '#AC92EC',
+    'INFO': '#48CFAD',
+    'WARNING': '#FFCE54',
+    'ERROR': '#FC6E51',
+    'CRITICAL': '#DA4453'
+}
+
+
+class SlackHandler(Handler):
+    def __init__(self, webhook_url, username=None, channel=None, icon_emoji=':snake:'):
+        super(SlackHandler, self).__init__()
+
+        self.webhook_url = webhook_url
+        self.username = username
+        self.channel = channel
+        self.icon_emoji = icon_emoji
+
+        self.session = FuturesSession()
+
+    def emit(self, record):
+        if isinstance(self.formatter, SlackFormatter):
+            attachment = self.format(record)
+        else:
+            attachment = {'text': self.format(record)}
+
+        payload = {
+            'channel': self.channel,
+            'icon_emoji': self.icon_emoji,
+            'username': self.username,
+            'attachments': [
+                attachment
+            ]
+        }
+
+        self.session.post(self.webhook_url, json=payload)
+
+
+class SlackFormatter(Formatter):
+    def format(self, record):
+        return {
+            'author_name': record.levelname,
+            'color': LEVEL_TO_COLOR[record.levelname],
+            'mrkdwn': True,
+            'text': super(SlackFormatter, self).format(record),
+            'title': record.name,
+            'ts': record.created
+        }
---
+++ packages/logging-slackhandler/logging-slackhandler-1.0.1.tar.gz/logging-slackhandler-1.0.1/SlackLogger/version.py
@@ -0,0 +1,5 @@
+VERSION = '1.0.1'
+
+
+def get_version():
+    return VERSION
