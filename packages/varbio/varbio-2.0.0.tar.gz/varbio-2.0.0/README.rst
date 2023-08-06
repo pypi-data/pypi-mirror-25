d41f714f4018aaf7776804b5630933ccf58fb484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:19 +0000

    Add package varbio/2.0.0/varbio-2.0.0.tar.gz

---
+++ packages/varbio/varbio-2.0.0.tar.gz/varbio-2.0.0/README.rst
@@ -0,0 +1,9 @@
+varbio (various bioinformatics) is a bioinformatics utility library, not
+recommended for public use due to frequent major releases without backports.
+
+Links
+=====
+- `Documentation <http://varbio.readthedocs.io/en/2.0.0/>`_
+- `PyPI <https://pypi.python.org/pypi/varbio>`_
+- `GitLab <https://gitlab.psb.ugent.be/deep_genome/varbio>`_
+
---
+++ packages/varbio/varbio-2.0.0.tar.gz/varbio-2.0.0/setup.py
@@ -0,0 +1,98 @@
+from setuptools import setup, find_packages
+from collections import defaultdict
+from pathlib import Path
+import os
+
+setup_args = dict(
+    version='2.0.0',
+    name='varbio',
+    description='Bioinformatics utility library',
+    long_description=Path('README.rst').read_text(),
+    url='https://gitlab.psb.ugent.be/deep_genome/varbio',
+    author='Tim Diels',
+    author_email='timdiels.m@gmail.com',
+    license='LGPL3',
+    keywords='bioinformatics parsing',
+    packages=find_packages(),
+    install_requires=[
+        'pandas==0.*',
+        'plumbum==1.*',
+        'pytil[test,data_frame]==6.*',
+        'scikit-learn==0.*',
+        'scipy==0.*',
+        'numpy==1.*',
+    ],
+    extras_require={
+        'dev': [
+            'numpydoc==0.*',
+            'sphinx==1.*',
+            'sphinx-rtd-theme==0.*',
+            'coverage-pth==0.*',
+            'pytest==3.*',
+            'pytest-cov==2.*',
+            'pytest-env==0.*',
+        ],
+    },
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: AIX',
+        'Operating System :: POSIX :: BSD',
+        'Operating System :: POSIX :: BSD :: BSD/OS',
+        'Operating System :: POSIX :: BSD :: FreeBSD',
+        'Operating System :: POSIX :: BSD :: NetBSD',
+        'Operating System :: POSIX :: BSD :: OpenBSD',
+        'Operating System :: POSIX :: GNU Hurd',
+        'Operating System :: POSIX :: HP-UX',
+        'Operating System :: POSIX :: IRIX',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX :: Other',
+        'Operating System :: POSIX :: SCO',
+        'Operating System :: POSIX :: SunOS/Solaris',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: Stackless',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
+
+# Generate extras_require['all'], union of all extras
+all_extra_dependencies = []
+for dependencies in setup_args['extras_require'].values():
+    all_extra_dependencies.extend(dependencies)
+all_extra_dependencies = list(set(all_extra_dependencies))
+setup_args['extras_require']['all'] = all_extra_dependencies
+
+# Generate package data
+#
+# Anything placed underneath a directory named 'data' in a package, is added to
+# the package_data of that package; i.e. included in the sdist/bdist and
+# accessible via pkg_resources.resource_*
+project_root = Path(__file__).parent
+package_data = defaultdict(list)
+for package in setup_args['packages']:
+    package_dir = project_root / package.replace('.', '/')
+    data_dir = package_dir / 'data'
+    if data_dir.exists() and not (data_dir / '__init__.py').exists():
+        # Found a data dir
+        for parent, _, files in os.walk(str(data_dir)):
+            package_data[package].extend(str((data_dir / parent / file).relative_to(package_dir)) for file in files)
+setup_args['package_data'] = {k: sorted(v) for k,v in package_data.items()}  # sort to avoid unnecessary git diffs
+
+# setup
+setup(**setup_args)
+
