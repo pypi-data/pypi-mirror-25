3023b468a20e66ef5417f9afe421734082d4fe91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:19 +0000

    Add package odoo8-addon-l10n-it-fiscal-document-type/8.0.0.1.0.99.dev3/odoo8_addon_l10n_it_fiscal_document_type-8.0.0.1.0.99.dev3-py2-none-any.whl

---
+++ packages/odoo8-addon-l10n-it-fiscal-document-type/odoo8_addon_l10n_it_fiscal_document_type-8.0.0.1.0.99.dev3-py2-none-any.whl/odoo_addons/l10n_it_fiscal_document_type/model/fiscal_document_type.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields
+
+
+class FiscalDocumentType(models.Model):
+    _name = 'fiscal.document.type'
+
+    code = fields.Char(string='Codice', size=5)
+    name = fields.Char(string='Nome', size=100)
+    code = fields.Char(string='Codice', size=5)
+    name = fields.Char(string='Nome', size=100)
+    out_invoice = fields.Boolean(string='Fattura vendita')
+    in_invoice = fields.Boolean(string='Fattura acquisto')
+    out_refund = fields.Boolean(string='Nota di credito')
+    in_refund = fields.Boolean(string='Nota di debito')
+    priority = fields.Integer(string='Priority', default='3')
+
+    journal_ids = fields.Many2many(
+        'account.journal',
+        'account_journal_fiscal_doc_type_rel',
+        'fiscal_document_type_id',
+        'journal_id',
+        'Sezionali'
+    )
+
+    _order = 'code, priority asc'
