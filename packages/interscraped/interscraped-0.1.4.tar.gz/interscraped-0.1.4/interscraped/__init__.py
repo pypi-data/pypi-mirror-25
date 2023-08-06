6d1f202c9d7b435fe957adbc74356c7f0b6da482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:54 +0000

    Add package interscraped/0.1.4/interscraped-0.1.4.tar.gz

---
+++ packages/interscraped/interscraped-0.1.4.tar.gz/interscraped-0.1.4/interscraped/__init__.py
@@ -0,0 +1,7 @@
+from .consistency_ratings import consistency_scraper
+from .def_vs import def_vs_scraper
+from .fanduel_salaries import fanduel_salaries_scraper
+from .player_stats import player_stats_scraper
+from .player_projections import player_projections_scraper
+from .roto_guru import roto_guru_scraper
+from .snap_counts import snap_counts_scraper
