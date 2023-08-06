66a9f3fd6058a532a610d3c57efa287c70f71298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:05 +0000

    Add package MPInterfaces_Latest/1.7.9/MPInterfaces_Latest-1.7.9.tar.gz

---
+++ packages/MPInterfaces_Latest/MPInterfaces_Latest-1.7.9.tar.gz/MPInterfaces_Latest-1.7.9/setup.py
@@ -0,0 +1,48 @@
+import glob
+import os
+import shutil as shu
+
+from setuptools import setup, find_packages
+
+MPINT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    name="MPInterfaces_Latest",
+    version="1.7.9",
+    install_requires=["pymatgen>=4.7.0", "FireWorks>=1.4.0",
+                      "custodian>=1.0.1", "pymatgen-db>=0.5.1",
+                      "ase>=3.11.0", "six", "pyhull>=1.5.3"],
+    extras_require={"babel": ["openbabel", "pybel"],
+                    "remote": ["fabric"],
+                    "doc": ["sphinx>=1.3.1", "sphinx-rtd-theme>=0.1.8"]
+                    },
+    author="Kiran Mathew, Joshua J. Gabriel, Michael Ashton, "
+           "Arunima K. Singh, Joshua T. Paul, Seve G. Monahan, "
+           "Richard G. Hennig",
+    author_email="joshgabriel92@gmail.com, kmathew@lbl.gov, ashtonmv@gmail.com",
+    maintainer="Joshua J. Gabriel, Michael Ashton",
+    maintainer_email="joshgabriel92@ufl.edu, ashtonmv@gmail.com",
+    description=(
+        "High throughput analysis of interfaces using VASP and Materials Project tools"),
+    license="MIT",
+    url="https://github.com/henniggroup/MPInterfaces",
+    packages=find_packages(),
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",        
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+    ],
+    scripts=glob.glob(os.path.join(MPINT_DIR, "mpinterfaces", "scripts", "*"))
+)
+
+# create the config file in the users home directory
+config_template = os.path.join(MPINT_DIR, "config_files", "mpint_config.yaml")
+os.system('cp {} ~/.mpint_config.yaml'.format(config_template))
