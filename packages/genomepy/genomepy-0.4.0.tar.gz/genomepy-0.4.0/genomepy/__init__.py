f4d23b441c232125515947b858858bd40c3904f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:25 +0000

    Add package genomepy/0.4.0/genomepy-0.4.0.tar.gz

---
+++ packages/genomepy/genomepy-0.4.0.tar.gz/genomepy-0.4.0/genomepy/__init__.py
@@ -0,0 +1,12 @@
+"""Search, download and use genome FASTA files."""
+import genomepy.__about__
+import genomepy.functions
+__version__ = genomepy.__about__.__version__
+__author__ = genomepy.__about__.__author__
+
+list_available_providers = genomepy.functions.list_available_providers
+list_available_genomes = genomepy.functions.list_available_genomes
+list_installed_genomes = genomepy.functions.list_installed_genomes
+search = genomepy.functions.search
+install_genome = genomepy.functions.install_genome
+Genome = genomepy.functions.Genome
