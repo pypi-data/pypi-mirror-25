641dcd376eddad52b1aef0d46cbf343f79b898d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:40 +0000

    Add package cosmosid-cli/0.0.7/cosmosid-cli-0.0.7.tar.gz

---
+++ packages/cosmosid-cli/cosmosid-cli-0.0.7.tar.gz/cosmosid-cli-0.0.7/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+``CosmosID``
+------------
+
+``cosmosid`` provides a command line client and Python library for CosmosID
+
+"""
+
+from setuptools import setup, find_packages
+
+__version__ = '0.0.7'
+
+setup(
+    name='cosmosid-cli',
+    version=__version__,
+    description='API Client and Pyhthon 3 libraries for CosmosID',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['contrib', 'docs', '*tests*']),
+    install_requires=['requests==2.17', 'cliff==2.8.0',
+                      'six', 'boto3==1.4.2',
+                      's3transfer==0.1.10', 'tzlocal==1.4'],
+    include_package_data=True,
+    dependency_links=[],
+    author='CosmosID',
+    author_email='support@cosmosid.com',
+    keywords='API Client for CosmosID',
+    url='https://www.cosmosid.com/',
+    classifiers=[
+        'Environment :: Console',
+    ],
+    entry_points={
+        'console_scripts': ['cosmosid = cosmosid.cli:main'],
+        'cosmosid': [
+            'files = cosmosid.command:Files',
+            'upload = cosmosid.command:Upload',
+            'analysis = cosmosid.command:Analysis',
+            'reports = cosmosid.command:Reports',
+        ],
+
+    },
+    test_suite=''
+)
