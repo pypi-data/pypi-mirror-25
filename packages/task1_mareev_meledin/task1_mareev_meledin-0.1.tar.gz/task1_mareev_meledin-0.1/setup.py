dd1f055d66293304e563c0f857754001a50b78fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:14 +0000

    Add package task1_mareev_meledin/0.1/task1_mareev_meledin-0.1.tar.gz

---
+++ packages/task1_mareev_meledin/task1_mareev_meledin-0.1.tar.gz/task1_mareev_meledin-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='task1_mareev_meledin',
+      version='0.1',
+      description='Workshop 3 course. Autumn 17-18. Exercise 1.',
+      #url='https://github.com/GlebOlegovich/prac-2017-2018/tree/master/submissions/task1',
+      author='Mareev Gleb and Meledin Stanislav',
+      author_email='wowmagic.gm@gmail.com',
+      license='MIT',
+      packages=['task1_mareev_meledin'],
+      zip_safe=False)
