88aa72b87f071e9de5af765d4bef4959f1a31daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:21 +0000

    Add package tagnews/0.3.6/tagnews-0.3.6.tar.gz

---
+++ packages/tagnews/tagnews-0.3.6.tar.gz/tagnews-0.3.6/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+
+# required_nltk_packages = ['punkt', 'wordnet']
+#
+# class Install(_install):
+#     def run(self):
+#         _install.do_egg_install(self)
+#         try:
+#             import nltk
+#             for nltk_package in required_nltk_packages:
+#                 nltk.download(nltk_package)
+#         except Exception as e:
+#             print('Failed to download NLTK packages.')
+
+setup(name='tagnews',
+      version='0.3.6',
+      description='automatically tag articles with justice-related categories',
+      author='Kevin Rose',
+      url='https://github.com/chicago-justice-project/article-tagging',
+      package_dir={'': 'lib'},
+      packages=['tagnews',
+                'tagnews.utils',
+                'tagnews.crimetype',
+                'tagnews.crimetype.models.binary_stemmed_logistic',
+                'tagnews.tests'],
+      install_requires=['nltk', 'numpy>=1.13', 'scikit-learn', 'pandas'],
+      tests_require=['pytest'],
+      package_data={'tagnews': ['crimetype/models/binary_stemmed_logistic/*.pkl']},
+    #   cmdclass={'install': Install},
+    #   setup_requires=['nltk'],
+      python_requires=">=3.5", # for now
+      zip_safe=False, # force source installation
+     )
