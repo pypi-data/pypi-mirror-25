37acdd3138c714a58123758de2211941644fdfaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:06 +0000

    Add package bubuku/0.9.35/bubuku-0.9.35.tar.gz

---
+++ packages/bubuku/bubuku-0.9.35.tar.gz/bubuku-0.9.35/instance_control/command/get.py
@@ -0,0 +1,32 @@
+import logging
+
+from instance_control.aws import AWSResources
+from instance_control.command import Command
+
+_LOG = logging.getLogger('bubuku.cluster.command.get')
+
+
+class GetCommand(Command):
+    def __init__(self, cluster_config_path: str):
+        super().__init__(cluster_config_path)
+
+    def execute(self):
+        aws_ = AWSResources(region=self.cluster_config['region'])
+        instances = list(aws_.ec2_resource.instances.filter(Filters=[
+            {'Name': 'instance-state-name', 'Values': ['running', 'pending']},
+            {'Name': 'tag:Name', 'Values': [self.cluster_config['cluster_name']]}]))
+
+        if len(instances) == 0:
+            _LOG.info('Cluster has no running instances')
+            return
+
+        max_ip = max([len(instance.private_ip_address) for instance in instances])
+        max_id = max([len(instance.instance_id) for instance in instances])
+
+        pattern = '{0:' + str(max_ip) + '}   {1:' + str(max_id) + '}   {2}'
+        print(pattern.format('ip', 'id', 'volume'))
+        for instance in instances:
+            print(pattern.format(instance.private_ip_address,
+                                 instance.instance_id,
+                                 [x['Ebs']['VolumeId'] for x in instance.block_device_mappings if
+                                  x['DeviceName'] == '/dev/xvdk'][0]))
