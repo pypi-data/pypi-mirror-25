05df5de034513791e15cc4c76cc7a644a91c1b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:34 +0000

    Add package bottery/0.0.1a3/bottery-0.0.1a3-py2.py3-none-any.whl

---
+++ packages/bottery/bottery-0.0.1a3-py2.py3-none-any.whl/bottery/platform/__init__.py
@@ -0,0 +1,48 @@
+import importlib
+import logging
+import os
+
+from bottery.conf import settings
+from bottery.exceptions import ImproperlyConfigured
+
+
+logger = logging.getLogger('bottery.platforms')
+
+
+def discover_view(message):
+    base = os.getcwd()
+    patterns_path = os.path.join(base, 'patterns.py')
+    if not os.path.isfile(patterns_path):
+        raise ImproperlyConfigured('Could not find patterns module')
+
+    patterns = importlib.import_module('patterns').patterns
+    for pattern in patterns:
+        if pattern.check(message):
+            logger.debug('[%s] Pattern found', message.platform)
+            if isinstance(pattern.view, str):
+                return importlib.import_module(pattern.view)
+            return pattern.view
+
+    # raise Exception('No Pattern found!')
+    return None
+
+
+class BasePlataform:
+
+    def __init__(self, **kw):
+        for item, value in kw.items():
+            setattr(self, item, value)
+
+    @property
+    def webhook_endpoint(self):
+        return '/hook/{}'.format(self.platform)
+
+    @property
+    def webhook_url(self):
+        return 'https://{}{}'.format(settings.HOSTNAME, self.webhook_endpoint)
+
+    def build_message(self):
+        raise NotImplementedError('create_message not implemented')
+
+    def tasks(self):
+        return None
