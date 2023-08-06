38523590ddf8f970ef5ab390e654b99c0f8e6fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:15 +0000

    Add package elementium/1.1.9/elementium-1.1.9.tar.gz

---
+++ packages/elementium/elementium-1.1.9.tar.gz/elementium-1.1.9/elementium/__init__.py
@@ -0,0 +1,7 @@
+"""Elementium provides a wrapper around existing web testing drivers to enable
+more a more consistent, less frustrating experience."""
+
+from . import elements
+
+__version__ = '1.1.9'
+
