5c928f35662608693ca19d7e6256ee6819cf0fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:23 +0000

    Add package tagnews/0.4.0/tagnews-0.4.0.tar.gz

---
+++ packages/tagnews/tagnews-0.4.0.tar.gz/tagnews-0.4.0/lib/tagnews/__init__.py
@@ -0,0 +1,19 @@
+from . import utils
+from . import crimetype
+
+__version__ = '0.4.0'
+
+def test(verbosity=None, **kwargs):
+    """run the test suite"""
+
+    import pytest
+
+    args = kwargs.pop('argv', [])
+
+    if verbosity:
+        args += ['-' + 'v' * verbosity]
+
+    return pytest.main(args, **kwargs)
+
+
+test.__test__ = False  # pytest: this function is not a test
