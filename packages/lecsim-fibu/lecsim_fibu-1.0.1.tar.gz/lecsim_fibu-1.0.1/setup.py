a6432627c7c007b36da992407ff03370497e8351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:08 +0000

    Add package lecsim-fibu/1.0.1/lecsim_fibu-1.0.1.tar.gz

---
+++ packages/lecsim-fibu/lecsim_fibu-1.0.1.tar.gz/lecsim_fibu-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name       = 'lecsim_fibu',
+    version    = '1.0.1',
+    py_modules = ['lecsim_fibu'],
+    author     = 'lecsim',
+    author_email = 'lechler@gmx.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+)
