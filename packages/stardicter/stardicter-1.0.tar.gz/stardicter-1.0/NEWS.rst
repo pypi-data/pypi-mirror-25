64a90b4d6249fd913f1256e52dfa51fbd01e02a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:34 +0000

    Add package stardicter/1.0/stardicter-1.0.tar.gz

---
+++ packages/stardicter/stardicter-1.0.tar.gz/stardicter-1.0/NEWS.rst
@@ -0,0 +1,19 @@
+Changes in stardicter
+=====================
+
+Version 0.11
+------------
+
+* Improved deaccent filter.
+* Fixed sorting of ASCII dictionaries.
+
+Version 0.10
+------------
+
+* Changed URL for English-Czech dictionary to https://www.svobodneslovniky.cz/.
+* Various improvements in deaccent filter.
+
+Version 0.9
+-----------
+
+* Initial release to pypi.
---
+++ packages/stardicter/stardicter-1.0.tar.gz/stardicter-1.0/cizislova_tarball.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# Script to create tarballs of Slovník cizích slov
+#
+# Copyright (c) 2006 - 2017 Michal Čihař
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see http://www.gnu.org/licenses/.
+
+set -e
+
+NAME=stardict-czech
+dir="$NAME-`date +%Y%m%d`"
+
+if [ "x$1" = 'x--wrap' ] ; then
+    WRAP="$2"
+    shift 2
+fi
+
+rm -rf $dir
+mkdir $dir
+
+$WRAP ./sdgen.py --change --directory $dir "$@" czech
+
+if [ ! -f $dir/README ] ; then
+    rm -rf $dir
+    exit 0
+fi
+
+# Compress
+dictzip $dir/*.dict
+
+# Create tarball
+tar --owner=root --group=root --numeric-owner -czf $dir.tar.gz $dir
+rm -rf $dir
