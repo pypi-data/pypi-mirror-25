bafbe3ef408eb0558e1b30f939f178c0d68a6200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:16 +0000

    Add package pyarxiv/1.0.2/pyarxiv-1.0.2.tar.gz

---
+++ packages/pyarxiv/pyarxiv-1.0.2.tar.gz/pyarxiv-1.0.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name='pyarxiv',
+      version='1.0.2',
+      install_requires=requirements,
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Environment :: Console'
+      ],
+      description='Python Client Library and CLI client for the ArXiv.org API',
+      long_description="See source on GitHub: https://github.com/culshoefer/pyarxiv\n\n" + readme,
+      author='Christoph Ulshoefer',
+      license='MIT',
+      author_email='c@culshoefer.com',
+      packages=['pyarxiv'],
+      url='https://github.com/culshoefer/pyarxiv/',
+      scripts=['scripts/pyarxiv-cli'],
+      test_suite='tests'
+      )
