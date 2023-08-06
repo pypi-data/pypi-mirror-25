e13440806c122c09c436436911ac44d1a0ea71e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:17 +0000

    Add package FMPy/0.0.9/FMPy-0.0.9.tar.gz

---
+++ packages/FMPy/FMPy-0.0.9.tar.gz/FMPy-0.0.9/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+def readme():
+    """ Get the content of README.rst without the CI badges """
+    with open('README.rst') as f:
+        lines = f.readlines()
+        while not lines[0].startswith('FMPy'):
+            lines = lines[1:]
+        return ''.join(lines)
+
+setup(name="FMPy",
+      version="0.0.9",
+      description="Simulate Functional Mockup Units (FMUs) in Python",
+      long_description=readme(),
+      author="Torsten Sommer",
+      author_email="torsten.sommer@3ds.com",
+      url="https://github.com/CATIA-Systems/FMPy",
+      license="Standard 3-clause BSD",
+      packages=['fmpy', 'fmpy.cross_check', 'fmpy.examples', 'fmpy.sundials'],
+      package_data={'fmpy': ['schema/fmi1/*.xsd',
+                             'schema/fmi2/*.xsd',
+                             'sundials/darwin64/*.dylib',
+                             'sundials/linux64/*.so',
+                             'sundials/win32/*.dll',
+                             'sundials/win64/*.dll']})
