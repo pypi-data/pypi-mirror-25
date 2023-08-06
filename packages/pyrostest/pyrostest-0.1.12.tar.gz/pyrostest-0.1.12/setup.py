2d1e4a505131c1c8d0fd58c1d7df06273b47694c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:33 +0000

    Add package pyrostest/0.1.12/pyrostest-0.1.12.tar.gz

---
+++ packages/pyrostest/pyrostest-0.1.12.tar.gz/pyrostest-0.1.12/setup.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+required = [
+    'psutil==5.1.3'
+    ]
+
+test = [
+    'pytest'
+    ]
+
+setup(
+    name='pyrostest',
+    version='0.1.12',
+    description='The most lit ros testing framework',
+    long_description=long_description,
+    packages=['pyrostest'],
+    package_data={
+        'pyrostest': ['data/*']
+    },
+    install_requires=required,
+    tests_require=test,
+    url='https://github.com/gtagency/pyrostest',
+    author='Joshua Morton',
+    author_email='joshua.morton13@gmail.com',
+    maintainer='Raphael Gontijo Lopes',
+    maintainer_email='raphaelgontijolopes@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        ],
+    )
+
+
