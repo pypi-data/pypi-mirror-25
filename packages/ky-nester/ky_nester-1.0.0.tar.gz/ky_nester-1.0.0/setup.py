ffd76f2821691a54f1ca07598cb90716ee1317ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:31 +0000

    Add package ky-nester/1.0.0/ky_nester-1.0.0.tar.gz

---
+++ packages/ky-nester/ky_nester-1.0.0.tar.gz/ky_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='ky_nester',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='hypython',
+    author_email='hypython@headfirstlabs.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+    )
