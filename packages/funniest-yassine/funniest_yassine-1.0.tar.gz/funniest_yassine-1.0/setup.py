e98d834a30bb94d26e01da4c16f7c564cb12910b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:05 +0000

    Add package funniest-yassine/1.0/funniest_yassine-1.0.tar.gz

---
+++ packages/funniest-yassine/funniest_yassine-1.0.tar.gz/funniest_yassine-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='funniest_yassine',
+    version='1.0',
+    packages=['funniest_yassine'],
+    url='https://github.com/yassineameur/funniest_yassine',
+    license='MIT',
+    author='yassineameur',
+    author_email='yassine.ameur@chauffeur-prive.com',
+    description='this is a test project'
+)
