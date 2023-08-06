7b637b95fd02aee773bbe35210e016c91ebb82ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:18 +0000

    Add package bibtex2html.py/1.0.0/bibtex2html.py-1.0.0-py2.py3-none-any.whl

---
+++ packages/bibtex2html.py/bibtex2html.py-1.0.0-py2.py3-none-any.whl/bibtex2html/bibtex2latex.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import bibtex2html as bl
+from bibtex2html import params
+
+
+def main():
+    print params
+
+
+if __name__ == '__main__':
+    main()
+
