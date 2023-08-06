58a8bb3322a9c1d8f5e31106a7142491127559e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:26 +0000

    Add package nester9527/1.0.9527/nester9527-1.0.9527.tar.gz

---
+++ packages/nester9527/nester9527-1.0.9527.tar.gz/nester9527-1.0.9527/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'nester9527',
+		version = '1.0.9527',
+		py_modules = ['nester'],
+		author = 'li.shuhao',
+		author_email = '1446529778@qq.com',
+		url = 'http://www.baidu.com',
+		description = 'A simple printer of nested lists',
+)
