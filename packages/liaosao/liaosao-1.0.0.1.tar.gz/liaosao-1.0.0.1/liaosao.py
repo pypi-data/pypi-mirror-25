30bb03b779acd1e3d298ba9604239f844770ff39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:49 +0000

    Add package liaosao/1.0.0.1/liaosao-1.0.0.1.tar.gz

---
+++ packages/liaosao/liaosao-1.0.0.1.tar.gz/liaosao-1.0.0.1/liaosao.py
@@ -0,0 +1,5 @@
+# coding: UTF-8
+__author__ = 'elegant_ft'
+
+def LiaoSao():
+	print("shoushoushigechusheng")
---
+++ packages/liaosao/liaosao-1.0.0.1.tar.gz/liaosao-1.0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	    name         = 'liaosao',
+		version      = '1.0.0.1',
+		py_modules   = ['liaosao'],
+		author       = 'elegant_ft',
+		author_email = '376512812@qq.com',
+		url          = 'http://www.baidu.com',
+		description  = 'liaosao',
+     )
