323433167d9939700e255e1ab3af23a4cd03af52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:59 +0000

    Add package sunpy/0.8.2/sunpy-0.8.2-cp36-cp36m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/sunpy/sunpy-0.8.2-cp36-cp36m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl/sunpy/instr/rhessi.py
@@ -0,0 +1,523 @@
+# -*- coding: utf-8 -*-
+"""
+    Provides programs to process and analyze RHESSI data.
+
+    .. warning:: This module is in development.
+
+"""
+from __future__ import absolute_import, print_function
+
+import csv
+import socket
+from datetime import datetime
+from datetime import timedelta
+
+import numpy as np
+
+from astropy.io import fits
+from astropy import units as u
+
+from sunpy.time import TimeRange, parse_time
+from sunpy.sun.sun import solar_semidiameter_angular_size
+from sunpy.coordinates import get_sunearth_distance
+import sunpy.map
+
+from sunpy.extern.six.moves import urllib
+from sunpy.extern.six.moves.urllib.request import urlopen
+from sunpy.extern.six.moves.urllib.error import URLError
+
+__all__ = ['get_obssumm_dbase_file', 'parse_obssumm_dbase_file',
+           'get_obssum_filename', 'get_obssumm_file', 'parse_obssumm_file',
+           'backprojection']
+
+# Measured fixed grid parameters
+grid_pitch = (4.52467, 7.85160, 13.5751, 23.5542, 40.7241, 70.5309, 122.164,
+              211.609, 366.646)
+grid_orientation = (3.53547, 2.75007, 3.53569, 2.74962, 3.92596, 2.35647,
+                    0.786083, 0.00140674, 1.57147)
+
+data_servers = ('https://hesperia.gsfc.nasa.gov/hessidata/',
+                'http://hessi.ssl.berkeley.edu/hessidata/',
+                'http://soleil.i4ds.ch/hessidata/')
+
+lc_linecolors = ('black', 'pink', 'green', 'blue', 'brown', 'red',
+                 'navy', 'orange', 'green')
+
+
+def get_base_url():
+    """
+    Find the first mirror which is online
+    """
+
+    for server in data_servers:
+        try:
+            urlopen(server, timeout=1)
+        except (URLError, socket.timeout):
+            pass
+        else:
+            return server
+
+
+def get_obssumm_dbase_file(time_range):
+    """
+    Download the RHESSI observing summary database file. This file lists the
+    name of observing summary files for specific time ranges.
+
+    Parameters
+    ----------
+    time_range : `str`, `sunpy.time.TimeRange`
+        A `~sunpy.time.TimeRange` or `~sunpy.time.TimeRange` compatible string.
+
+    Returns
+    -------
+    value : `tuple`
+        Return a `tuple` (filename, headers) where filename is the local file
+        name under which the object can be found, and headers is
+        whatever the info() method of the object returned by urlopen.
+
+    Examples
+    --------
+    >>> import sunpy.instr.rhessi as rhessi
+    >>> rhessi.get_obssumm_dbase_file(('2011/04/04', '2011/04/05'))   # doctest: +SKIP
+
+    References
+    ----------
+    | http://hesperia.gsfc.nasa.gov/ssw/hessi/doc/guides/hessi_data_access.htm#Observing Summary Data
+
+    .. note::
+        This API is currently limited to providing data from whole days only.
+
+    """
+
+    #    http://hesperia.gsfc.nasa.gov/hessidata/dbase/hsi_obssumm_filedb_200311.txt
+
+    _time_range = TimeRange(time_range)
+    data_location = 'dbase/'
+
+    if _time_range.start < parse_time("2002/02/01"):
+        raise ValueError("RHESSI summary files are not available for before 2002-02-01")
+
+    url_root = get_base_url() + data_location
+    url = url_root + _time_range.start.strftime("hsi_obssumm_filedb_%Y%m.txt")
+
+    f = urllib.request.urlretrieve(url)
+
+    return f
+
+
+def parse_obssumm_dbase_file(filename):
+    """
+    Parse the RHESSI observing summary database file. This file lists the
+    name of observing summary files for specific time ranges along with other
+    info
+
+    Parameters
+    ----------
+    filename : `str`
+        The filename of the obssumm dbase file.
+
+    Returns
+    -------
+    out : `dict`
+        Return a `dict` containing the parsed data in the dbase file.
+
+    Examples
+    --------
+    >>> import sunpy.instr.rhessi as rhessi
+    >>> f = rhessi.get_obssumm_dbase_file(('2011/04/04', '2011/04/05'))   # doctest: +SKIP
+    >>> rhessi.parse_obssumm_dbase_file(f[0])   # doctest: +SKIP
+
+    References
+    ----------
+    | http://hesperia.gsfc.nasa.gov/ssw/hessi/doc/guides/hessi_data_access.htm#Observing Summary Data
+
+    .. note::
+        This API is currently limited to providing data from whole days only.
+
+    """
+    with open(filename, "rt") as fd:
+        reader = csv.reader(fd, delimiter=' ', skipinitialspace=True)
+        headerline = next(reader)
+        headerline = next(reader)
+        headerline = next(reader)
+        headerline = next(reader)
+
+        obssumm_filename = []
+        orbit_start = []
+        orbit_end = []
+        start_time = []
+        end_time = []
+        status_flag = []
+        number_of_packets = []
+
+        for row in reader:
+            obssumm_filename.append(row[0])
+            orbit_start.append(int(row[1]))
+            orbit_end.append(int(row[2]))
+            start_time.append(datetime.strptime(row[3], '%d-%b-%y'))
+            end_time.append(datetime.strptime(row[5], '%d-%b-%y'))
+            status_flag.append(int(row[7]))
+            number_of_packets.append(int(row[8]))
+
+        return {
+            headerline[0].lower(): obssumm_filename,
+            headerline[1].lower(): orbit_start,
+            headerline[2].lower(): orbit_end,
+            headerline[3].lower(): start_time,
+            headerline[4].lower(): end_time,
+            headerline[5].lower(): status_flag,
+            headerline[6].lower(): number_of_packets
+        }
+
+
+def get_obssum_filename(time_range):
+    """
+    Download the RHESSI observing summary data from one of the RHESSI
+    servers, parses it, and returns the name of the obssumm files relevant for
+    the time range.
+
+    Parameters
+    ----------
+    time_range : str, TimeRange
+        A TimeRange or time range compatible string
+
+    Returns
+    -------
+    out : list
+        Returns the filenames of the observation summary file
+
+    Examples
+    --------
+    >>> import sunpy.instr.rhessi as rhessi
+    >>> rhessi.get_obssum_filename(('2011/04/04', '2011/04/05'))   # doctest: +SKIP
+
+    .. note::
+        This API is currently limited to providing data from whole days only.
+
+    """
+    # need to download and inspect the dbase file to determine the filename
+    # for the observing summary data
+    f = get_obssumm_dbase_file(time_range)
+    data_location = 'metadata/catalog/'
+
+    result = parse_obssumm_dbase_file(f[0])
+    _time_range = TimeRange(time_range)
+
+    index_number_start = _time_range.start.day - 1
+    index_number_end = _time_range.end.day - 1
+
+    return [get_base_url() + data_location + filename
+            + 's' for filename in result.get('filename')[index_number_start:index_number_end]]
+
+
+def get_obssumm_file(time_range):
+    """
+    Download the RHESSI observing summary data from one of the RHESSI
+    servers.
+
+    Parameters
+    ----------
+    time_range : `str`, `sunpy.time.TimeRange`
+        A TimeRange or time range compatible string
+
+    Returns
+    -------
+    out : tuple
+        Return a tuple (filename, headers) where filename is the local file
+        name under which the object can be found, and headers is
+        whatever the info() method of the object returned by urlopen.
+
+    Examples
+    --------
+    >>> import sunpy.instr.rhessi as rhessi
+    >>> rhessi.get_obssumm_file(('2011/04/04', '2011/04/05'))   # doctest: +SKIP
+
+    .. note::
+        This API is currently limited to providing data from whole days only.
+
+    """
+
+    time_range = TimeRange(time_range)
+
+    url = get_obssum_filename(time_range)[0]
+
+    print('Downloading file: ' + url)
+    f = urllib.request.urlretrieve(url)
+
+    return f
+
+
+def parse_obssumm_file(filename):
+    """
+    Parse a RHESSI observation summary file.
+    Note: this is for the Lightcurve datatype only, the TimSeries uses the
+    parse_obssumm_hdulist(hdulist) method to enable implicit source detection.
+
+    Parameters
+    ----------
+    filename : str
+        The filename of a RHESSI fits file.
+
+    Returns
+    -------
+    out : `dict`
+        Returns a dictionary.
+
+    Examples
+    --------
+    >>> import sunpy.instr.rhessi as rhessi
+    >>> f = rhessi.get_obssumm_file(('2011/04/04', '2011/04/05'))   # doctest: +SKIP
+    >>> data = rhessi.parse_obssumm_file(f[0])   # doctest: +SKIP
+
+    """
+
+    afits = fits.open(filename)
+    header = afits[0].header
+
+    reference_time_ut = parse_time(afits[5].data.field('UT_REF')[0])
+    time_interval_sec = afits[5].data.field('TIME_INTV')[0]
+    # label_unit = fits[5].data.field('DIM1_UNIT')[0]
+    # labels = fits[5].data.field('DIM1_IDS')
+    labels = ['3 - 6 keV', '6 - 12 keV', '12 - 25 keV', '25 - 50 keV',
+              '50 - 100 keV', '100 - 300 keV', '300 - 800 keV', '800 - 7000 keV',
+              '7000 - 20000 keV']
+
+    # The data stored in the FITS file are "compressed" countrates stored as
+    # one byte
+    compressed_countrate = np.array(afits[6].data.field('countrate'))
+
+    countrate = uncompress_countrate(compressed_countrate)
+    dim = np.array(countrate[:, 0]).size
+
+    time_array = [reference_time_ut + timedelta(0, time_interval_sec * a) for a in np.arange(dim)]
+
+    # TODO generate the labels for the dict automatically from labels
+    data = {'time': time_array, 'data': countrate, 'labels': labels}
+
+    return header, data
+
+
+def parse_obssumm_hdulist(hdulist):
+    """
+    Parse a RHESSI observation summary file.
+
+    Parameters
+    ----------
+    hdulist : list
+        The HDU list from the fits file.
+
+    Returns
+    -------
+    out : `dict`
+        Returns a dictionary.
+
+    """
+    header = hdulist[0].header
+
+    reference_time_ut = parse_time(hdulist[5].data.field('UT_REF')[0])
+    time_interval_sec = hdulist[5].data.field('TIME_INTV')[0]
+    # label_unit = fits[5].data.field('DIM1_UNIT')[0]
+    # labels = fits[5].data.field('DIM1_IDS')
+    labels = ['3 - 6 keV', '6 - 12 keV', '12 - 25 keV', '25 - 50 keV',
+              '50 - 100 keV', '100 - 300 keV', '300 - 800 keV',
+              '800 - 7000 keV', '7000 - 20000 keV']
+
+    # The data stored in the fits file are "compressed" countrates stored as
+    # one byte
+    compressed_countrate = np.array(hdulist[6].data.field('countrate'))
+
+    countrate = uncompress_countrate(compressed_countrate)
+    dim = np.array(countrate[:, 0]).size
+
+    time_array = [reference_time_ut + timedelta(0, time_interval_sec * a) for a in np.arange(dim)]
+
+    #  TODO generate the labels for the dict automatically from labels
+    data = {'time': time_array, 'data': countrate, 'labels': labels}
+
+    return header, data
+
+
+def uncompress_countrate(compressed_countrate):
+    """Convert the compressed count rate inside of observing summary file from
+    a compressed byte to a true count rate
+
+    Parameters
+    ----------
+    compressed_countrate : byte array
+        A compressed count rate returned from an observing summary file.
+
+    References
+    ----------
+    Hsi_obs_summ_decompress.pro `<http://hesperia.gsfc.nasa.gov/ssw/hessi/idl/qlook_archive/hsi_obs_summ_decompress.pro>`_
+    """
+
+    # Ensure uncompressed counts are between 0 and 255
+    if (compressed_countrate.min() < 0) or (compressed_countrate.max() > 255):
+        raise ValueError(
+            'Exepected uncompressed counts {} to in range 0-255'.format(compressed_countrate))
+
+    # TODO Must be a better way than creating entire lookup table on each call
+    ll = np.arange(0, 16, 1)
+    lkup = np.zeros(256, dtype='int')
+    _sum = 0
+    for i in range(0, 16):
+        lkup[16 * i:16 * (i + 1)] = ll * 2 ** i + _sum
+        if i < 15:
+            _sum = lkup[16 * (i + 1) - 1] + 2 ** i
+
+    return lkup[compressed_countrate]
+
+
+def hsi_linecolors():
+    """Define discrete colors to use for RHESSI plots
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    tuple : matplotliblib color list
+
+    References
+    ----------
+    hsi_linecolors.pro `<http://hesperia.gsfc.nasa.gov/ssw/hessi/idl/gen/hsi_linecolors.pro`_
+    """
+    return ('black', 'magenta', 'lime', 'cyan', 'y', 'red', 'blue', 'orange',
+            'olive')
+
+
+def _backproject(calibrated_event_list, detector=8, pixel_size=(1., 1.),
+                 image_dim=(64, 64)):
+    """
+    Given a stacked calibrated event list fits file create a back
+    projection image for an individual detectors. This function is used by
+    backprojection.
+
+    Parameters
+    ----------
+    calibrated_event_list : string
+        filename of a RHESSI calibrated event list
+    detector : int
+        the detector number
+    pixel_size : 2-tuple
+        the size of the pixels in arcseconds. Default is (1,1).
+    image_dim : 2-tuple
+        the size of the output image in number of pixels
+
+    Returns
+    -------
+    out : ndarray
+        Return a backprojection image.
+
+    Examples
+    --------
+    >>> import sunpy.instr.rhessi as rhessi
+
+    """
+    # info_parameters = fits[2]
+    # detector_efficiency = info_parameters.data.field('cbe_det_eff$$REL')
+
+    afits = fits.open(calibrated_event_list)
+
+    fits_detector_index = detector + 2
+    detector_index = detector - 1
+    grid_angle = np.pi/2. - grid_orientation[detector_index]
+    harm_ang_pitch = grid_pitch[detector_index]/1
+
+    phase_map_center = afits[fits_detector_index].data.field('phase_map_ctr')
+    this_roll_angle = afits[fits_detector_index].data.field('roll_angle')
+    modamp = afits[fits_detector_index].data.field('modamp')
+    grid_transmission = afits[fits_detector_index].data.field('gridtran')
+    count = afits[fits_detector_index].data.field('count')
+
+    tempa = (np.arange(image_dim[0] * image_dim[1]) % image_dim[0]) - (image_dim[0]-1)/2.
+    tempb = tempa.reshape(image_dim[0], image_dim[1]).transpose().reshape(image_dim[0]*image_dim[1])
+
+    pixel = np.array(list(zip(tempa, tempb)))*pixel_size[0]
+    phase_pixel = (2 * np.pi/harm_ang_pitch) *\
+                  (np.outer(pixel[:, 0], np.cos(this_roll_angle - grid_angle)) -
+                   np.outer(pixel[:, 1], np.sin(this_roll_angle - grid_angle))) + phase_map_center
+    phase_modulation = np.cos(phase_pixel)
+    gridmod = modamp * grid_transmission
+    probability_of_transmission = gridmod * phase_modulation + grid_transmission
+    bproj_image = np.inner(probability_of_transmission, count).reshape(image_dim)
+
+    return bproj_image
+
+
+@u.quantity_input(pixel_size=u.arcsec, image_dim=u.pix)
+def backprojection(calibrated_event_list, pixel_size=(1., 1.) * u.arcsec,
+                   image_dim=(64, 64) * u.pix):
+    """
+    Given a stacked calibrated event list fits file create a back
+    projection image.
+
+    .. warning:: The image is not in the right orientation!
+
+    Parameters
+    ----------
+    calibrated_event_list : string
+        filename of a RHESSI calibrated event list
+    pixel_size : `~astropy.units.Quantity` instance
+        the size of the pixels in arcseconds. Default is (1,1).
+    image_dim : `~astropy.units.Quantity` instance
+        the size of the output image in number of pixels
+
+    Returns
+    -------
+    out : RHESSImap
+        Return a backprojection map.
+
+    Examples
+    --------
+    >>> import sunpy.data
+    >>> import sunpy.data.sample
+    >>> import sunpy.instr.rhessi as rhessi
+    >>> sunpy.data.download_sample_data(overwrite=False)   # doctest: +SKIP
+    >>> map = rhessi.backprojection(sunpy.data.sample.RHESSI_EVENT_LIST)   # doctest: +SKIP
+    >>> map.peek()   # doctest: +SKIP
+
+    """
+    pixel_size = pixel_size.to(u.arcsec)
+    image_dim = np.array(image_dim.to(u.pix).value, dtype=int)
+
+    afits = fits.open(calibrated_event_list)
+    info_parameters = afits[2]
+    xyoffset = info_parameters.data.field('USED_XYOFFSET')[0]
+    time_range = TimeRange(info_parameters.data.field('ABSOLUTE_TIME_RANGE')[0])
+
+    image = np.zeros(image_dim)
+
+    # find out what detectors were used
+    det_index_mask = afits[1].data.field('det_index_mask')[0]
+    detector_list = (np.arange(9)+1) * np.array(det_index_mask)
+    for detector in detector_list:
+        if detector > 0:
+            image = image + _backproject(calibrated_event_list, detector=detector,
+                                         pixel_size=pixel_size.value, image_dim=image_dim)
+
+    dict_header = {
+        "DATE-OBS": time_range.center.strftime("%Y-%m-%d %H:%M:%S"),
+        "CDELT1": pixel_size[0],
+        "NAXIS1": image_dim[0],
+        "CRVAL1": xyoffset[0],
+        "CRPIX1": image_dim[0]/2 + 0.5,
+        "CUNIT1": "arcsec",
+        "CTYPE1": "HPLN-TAN",
+        "CDELT2": pixel_size[1],
+        "NAXIS2": image_dim[1],
+        "CRVAL2": xyoffset[1],
+        "CRPIX2": image_dim[0]/2 + 0.5,
+        "CUNIT2": "arcsec",
+        "CTYPE2": "HPLT-TAN",
+        "HGLT_OBS": 0,
+        "HGLN_OBS": 0,
+        "RSUN_OBS": solar_semidiameter_angular_size(time_range.center).value,
+        "RSUN_REF": sunpy.sun.constants.radius.value,
+        "DSUN_OBS": get_sunearth_distance(time_range.center).value * sunpy.sun.constants.au.value
+    }
+
+    result_map = sunpy.map.Map(image, dict_header)
+
+    return result_map
