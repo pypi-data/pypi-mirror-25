e58579154ee27169216ef4aad221add1916c8328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:18 +0000

    Add package PyMines/1.1.1/PyMines-1.1.1.tar.gz

---
+++ packages/PyMines/PyMines-1.1.1.tar.gz/PyMines-1.1.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='PyMines',
+      version='1.1.1',
+      description='Very simple minesweeper game',
+      author='Vilsol',
+      author_email='me@vil.so',
+      url='https://github.com/Vilsol/PyMines',
+      packages=[
+        "pymines"
+      ],
+      package_data={
+          "pymines": ["images/*.*"]
+      },
+      entry_points={
+          'console_scripts': [
+              'pymines = pymines.pymines:execute'
+          ]
+      },
+      )
