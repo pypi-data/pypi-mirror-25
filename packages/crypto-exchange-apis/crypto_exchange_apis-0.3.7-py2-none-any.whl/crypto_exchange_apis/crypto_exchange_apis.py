6b8fe8f1b0a9fd8f1343a083d439504be9e4edbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:22 +0000

    Add package crypto-exchange-apis/0.3.7/crypto_exchange_apis-0.3.7-py2-none-any.whl

---
+++ packages/crypto-exchange-apis/crypto_exchange_apis-0.3.7-py2-none-any.whl/crypto_exchange_apis/crypto_exchange_apis.py
@@ -0,0 +1,11 @@
+'''
+  Cryptocurrency exchange API wrappers tested on Python 2.7.12
+  https://github.com/init-industries/crypto-exchange-
+
+  See docs folder
+'''
+
+from itbit import *
+from bittrex import *
+from poloniex import *
+from tuxexchange import *
