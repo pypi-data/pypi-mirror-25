8dc17683420936aaa081422b0b560d12bacbb115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:01 +0000

    Add package Flask-WebSub/0.2/Flask-WebSub-0.2.tar.gz

---
+++ packages/Flask-WebSub/Flask-WebSub-0.2.tar.gz/Flask-WebSub-0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='UTF-8') as f:
+    description = f.read()
+
+setup(
+    name='Flask-WebSub',
+    version='0.2',
+    url='https://github.com/marten-de-vries/Flask-WebSub',
+    license='ISC',
+    author='Marten de Vries',
+    author_email='m@rtendevri.es',
+    description='A WebSub hub, publisher and subscriber using Flask',
+    long_description=description,
+    packages=find_packages(),
+    zip_safe=False,
+    platforms='any',
+    install_requires=['Flask', 'requests'],
+    extras_require={
+        'celery': ['celery'],
+        'redis': ['redis'],
+    },
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
