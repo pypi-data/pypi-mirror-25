31f21e89a7096500a3c452284e28b46542218da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:07 +0000

    Add package wpm/1.21/wpm-1.21.tar.gz

---
+++ packages/wpm/wpm-1.21.tar.gz/wpm-1.21/wpm/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+Measures your typing speed in words per minute (WPM).
+"""
+
+__author__ = "Christian Stigen Larsen"
+__copyright__ = "Copyright 2017 Christian Stigen Larsen"
+__license__ = "GNU GPL v3 or later"
+__version__ = "1.21"
