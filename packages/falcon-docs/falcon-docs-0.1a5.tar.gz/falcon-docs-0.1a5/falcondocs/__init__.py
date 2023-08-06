358ace138ceb0f1eab1feb7d05a6762f7e81b9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:17 +0000

    Add package falcon-docs/0.1a5/falcon-docs-0.1a5.tar.gz

---
+++ packages/falcon-docs/falcon-docs-0.1a5.tar.gz/falcon-docs-0.1a5/falcondocs/__init__.py
@@ -0,0 +1,9 @@
+# coding=UTF-8
+from __future__ import print_function, absolute_import, division
+import six
+import logging
+from .docs import FalconDocumentationResource, FalconDocumentationRouter
+
+__all__ = [
+    'FalconDocumentationResource', 'FalconDocumentationRouter'
+]
