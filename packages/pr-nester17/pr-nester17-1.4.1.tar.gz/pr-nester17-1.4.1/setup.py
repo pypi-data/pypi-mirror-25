8a9168a8d97f3293b3e69abc67be5a098c42637b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:59 +0000

    Add package pr-nester17/1.4.1/pr-nester17-1.4.1.tar.gz

---
+++ packages/pr-nester17/pr-nester17-1.4.1.tar.gz/pr-nester17-1.4.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name          = 'pr-nester17',
+        version       = '1.4.1',
+        py_modules    = ['pr-nester17'],
+        author        = 'Garry Meng',
+        author_email  = 'menggaoling@163.com',
+        url           = 'http://blog.csdn.net/garrymeng',
+        description   = 'Enhanced print nested list function',
+    )
