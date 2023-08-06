262a4ecdfe92cecd06e23f543ae8aa68fe220b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:11 +0000

    Add package django-watermark-remastered/0.1.8/django-watermark-remastered-0.1.8.tar.gz

---
+++ packages/django-watermark-remastered/django-watermark-remastered-0.1.8.tar.gz/django-watermark-remastered-0.1.8/setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+from watermarker import __version__
+
+setup(
+    name='django-watermark-remastered',
+    version=__version__,
+    packages=find_packages(exclude=['example']),
+    include_package_data=True,
+    license='BSD License',
+    description="Overrided django-watermark from origin",
+    long_description=README,
+    keywords='django, watermark, image, photo, logo',
+    url='https://github.com/papenx/django-watermark/',
+    author='Josh VanderLinden',
+    author_email='codekoala@gmail.com',
+    maintainer='Basil Shubin',
+    maintainer_email='basil.shubin@gmail.com',
+    install_requires=[
+        'django>=1.4',
+        'django-appconf',
+        'pillow',
+        'six',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Artistic Software',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Multimedia :: Graphics'
+    ],
+    zip_safe=False
+)
