bc0dc2515da45283d50887d0dbaf3a382234deba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:40 +0000

    Add package tsp-rest-api-server/3.5/tsp_rest_api_server-3.5-py2-none-any.whl

---
+++ packages/tsp-rest-api-server/tsp_rest_api_server-3.5-py2-none-any.whl/tsp_api/__main__.py
@@ -0,0 +1,83 @@
+from optparse import OptionParser
+
+CONFIG_TEMPLATE = """
+DOMAIN = {
+    'maps': {
+        'additional_lookup': {
+            'url': 'regex("[\w]+")',
+            'field': 'title',
+        },
+        'schema': {
+            'title': {
+                'type':'string'
+            },
+            'routes': {
+                'type':'list',
+                'schema': {
+                    'type':'dict',
+                    'schema': {
+                        'origin': {'type':'string'},
+                        'destiny': {'type':'string'},
+                        'distance': {'type':'float'}
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+MONGO_HOST = %(default_key)r
+MONGO_PORT = 27017
+MONGO_DBNAME = 'tsp_rest_api'
+
+RESOURCE_METHODS = ['GET', 'POST']
+XML = False
+
+"""
+
+
+def generate_settings(parser, options, args):
+    output = CONFIG_TEMPLATE % dict(
+        default_key='0.0.0.0',
+    )
+
+    with open('settings.py', "a+") as a_file:
+        a_file.write(output)
+        a_file.close()
+    return
+
+
+def runserver(parser, options, args):
+    from api import app
+    app.debug = True
+    app.run()
+
+
+COMMANDS = {
+    'runserver': runserver,
+    'settings': generate_settings,
+}
+
+
+def main():
+    # Parse options
+    parser = OptionParser(usage="Usage: %prog runserver")
+    (options, args) = parser.parse_args()
+
+    # Find command
+    try:
+        command = args[0]
+    except IndexError:
+        parser.print_help()
+        return
+
+    if COMMANDS:
+        if command in COMMANDS:
+            COMMANDS[command](parser, options, args)
+        else:
+            parser.error("Unrecognised command: " + command)
+
+
+if __name__ == '__main__':
+    main()
