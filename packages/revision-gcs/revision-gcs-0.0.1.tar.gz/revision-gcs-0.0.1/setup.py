e30437594824c481f96e28e558b427df5bc55ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:03 +0000

    Add package revision-gcs/0.0.1/revision-gcs-0.0.1.tar.gz

---
+++ packages/revision-gcs/revision-gcs-0.0.1.tar.gz/revision-gcs-0.0.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import ast
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('revision_gcs/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')
+    ).group(1)))
+
+setup(
+    name='revision-gcs',
+    version=version,
+    packages=['revision_gcs'],
+    install_requires=[
+        'revision',
+        'google-cloud-storage==1.4.0'
+    ],
+    extras_require={
+        'dev': [
+            'flake8==3.4.1',
+            'pytest==3.2.2'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
