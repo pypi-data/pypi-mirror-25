16e117c2a30c34ac536716ba8536a7e0adcced99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:47 +0000

    Add package print-listasass/1.0/print_listasass-1.0.tar.gz

---
+++ packages/print-listasass/print_listasass-1.0.tar.gz/print_listasass-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='print_listasass',
+      version='1.0',
+      description='print list with indentation',
+      author='zhou',
+      pymodules=['print_listasass']
+     )
