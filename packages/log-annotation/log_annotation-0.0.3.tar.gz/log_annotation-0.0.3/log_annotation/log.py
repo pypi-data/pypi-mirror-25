9993ff08d3d3fb3412ab9fc541c8b55935a3092a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:58 +0000

    Add package log-annotation/0.0.3/log_annotation-0.0.3.tar.gz

---
+++ packages/log-annotation/log_annotation-0.0.3.tar.gz/log_annotation-0.0.3/log_annotation/log.py
@@ -0,0 +1,29 @@
+import logging
+import graypy
+import requests
+def log(name='', level=logging.INFO, **graylog):
+    import time
+    import logging
+    def decorator(function):
+        def log_work():
+            try:
+                # Init logger
+                logger = logging.getLogger(name)
+                logger.setLevel(level)
+                # Log Init
+                if graylog.get("host",None) != None and graylog.get("port",None) != None:
+                    handler = graypy.GELFHandler(graylog["loghub"], graylog["logport"])
+                    logger.addHandler(handler)
+                else:
+                    logger.addHandler(logging.StreamHandler())
+                start_time = time.time()
+                res = function()
+                elapsed_time = str(round(time.time() - start_time,2))
+                logger.info({"function":function.__name__, "elapsed_time":elapsed_time})
+                return res
+            except requests.exceptions.HTTPError as err:
+                logger.error({'error': {'description': str(err), 'response': err.response.json()}})
+            except Exception as e:
+                print('Error in ' + function.__name__ + ' :'  + str(e))
+        return log_work
+    return log_decorator #returning the decorator function
