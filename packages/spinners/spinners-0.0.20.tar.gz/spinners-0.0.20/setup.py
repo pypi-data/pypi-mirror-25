51b96c1af5258f7fc26df931597695329bc8dc0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:58 +0000

    Add package spinners/0.0.20/spinners-0.0.20.tar.gz

---
+++ packages/spinners/spinners-0.0.20.tar.gz/spinners-0.0.20/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages # pylint: disable=no-name-in-module,import-error
+
+def dependencies(file):
+    with open(file) as f:
+        return f.read().splitlines()
+
+setup(
+    name='spinners',
+    packages=find_packages(exclude=('tests', 'examples')) + ['cli-spinners'],
+    version='0.0.20',
+    license='MIT',
+    description='Spinners for terminals',
+    long_description='More than 60 spinners for terminal, this is python port of amazing node library cli-spinners. Find the documentation here: https://github.com/ManrajGrover/py-spinners.',
+    author='Manraj Singh',
+    author_email='manrajsinghgrover@gmail.com',
+    url='https://github.com/ManrajGrover/py-spinners',
+    keywords=[
+        'cli',
+        'spinner',
+        'spinners',
+        'terminal',
+        'term',
+        'console',
+        'ascii',
+        'unicode',
+        'loading',
+        'indicator',
+        'progress',
+        'busy',
+        'wait',
+        'idle',
+        'json'
+    ],
+    install_requires=dependencies('requirements.txt'),
+    tests_require=dependencies('requirements-dev.txt'),
+    include_package_data=True
+)
